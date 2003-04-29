d34720fd7ddfe88ea99da0c06bcd60d6aa1a55cd
bellard <bellard@c046a42c-6fe2-441c-8c8c-71466251a162>
2003-04-29 20:37:44 +0000

    comment
    git-svn-id: svn://svn.savannah.nongnu.org/qemu/trunk@100 c046a42c-6fe2-441c-8c8c-71466251a162

--- cpu-i386.h
+++ cpu-i386.h
@@ -163,7 +163,7 @@ typedef struct CPUX86State {
     uint32_t eip;
     uint32_t eflags; /* eflags register. During CPU emulation, CC
                         flags and DF are set to zero because they are
-                        store elsewhere */
+                        stored elsewhere */
 
     /* emulator internal eflags handling */
     uint32_t cc_src;
