3b0dca51b0cefb662f1d2757fcccfd563c58b2b9
bellard <bellard@c046a42c-6fe2-441c-8c8c-71466251a162>
2003-06-27 18:52:23 +0000

    gdb stub defines
    git-svn-id: svn://svn.savannah.nongnu.org/qemu/trunk@290 c046a42c-6fe2-441c-8c8c-71466251a162

--- cpu-all.h
+++ cpu-all.h
@@ -304,7 +304,14 @@ void page_unprotect_range(uint8_t *data, unsigned long data_size);
 
 #endif /* SINGLE_CPU_DEFINES */
 
+#define DEFAULT_GDBSTUB_PORT 1234
+
 void cpu_abort(CPUState *env, const char *fmt, ...);
 extern CPUState *cpu_single_env;
+
+/* gdb stub API */
+extern int gdbstub_fd;
+CPUState *cpu_gdbstub_get_env(void *opaque);
+int cpu_gdbstub(void *opaque, void (*main_loop)(void *opaque), int port);
 
 #endif /* CPU_ALL_H */
