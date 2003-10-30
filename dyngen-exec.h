c970a162e7b152622b394f9268fc4aeb0f0e3b20
bellard <bellard@c046a42c-6fe2-441c-8c8c-71466251a162>
2003-10-30 01:21:44 +0000

    temporary hack for PowerPC system emulation
    git-svn-id: svn://svn.savannah.nongnu.org/qemu/trunk@436 c046a42c-6fe2-441c-8c8c-71466251a162

--- dyngen-exec.h
+++ dyngen-exec.h
@@ -54,6 +54,8 @@ extern int printf(const char *, ...);
 #define AREG1 "r24"
 #define AREG2 "r25"
 #define AREG3 "r26"
+/* XXX: suppress this hack */
+#if defined(CONFIG_USER_ONLY)
 #define AREG4 "r16"
 #define AREG5 "r17"
 #define AREG6 "r18"
@@ -62,6 +64,7 @@ extern int printf(const char *, ...);
 #define AREG9 "r21"
 #define AREG10 "r22"
 #define AREG11 "r23"
+#endif
 #define USE_INT_TO_FLOAT_HELPERS
 #define BUGGY_GCC_DIV64
 #endif
