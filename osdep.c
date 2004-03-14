07d898662d9aa173ac2d5c0aa4c9a1cf39caa703
bellard <bellard@c046a42c-6fe2-441c-8c8c-71466251a162>
2004-03-14 21:41:12 +0000

    added qemu_mallocz()
    git-svn-id: svn://svn.savannah.nongnu.org/qemu/trunk@660 c046a42c-6fe2-441c-8c8c-71466251a162

--- osdep.c
+++ osdep.c
@@ -281,6 +281,16 @@ void *qemu_malloc(size_t size)
 
 #endif
 
+void *qemu_mallocz(size_t size)
+{
+    void *ptr;
+    ptr = qemu_malloc(size);
+    if (!ptr)
+        return NULL;
+    memset(ptr, 0, size);
+    return ptr;
+}
+
 /****************************************************************/
 /* printf support */
 
