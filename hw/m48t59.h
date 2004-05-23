b69fedff841fc01fd96843ab38912031e1defb33
bellard <bellard@c046a42c-6fe2-441c-8c8c-71466251a162>
2004-05-23 21:17:08 +0000

    PowerPC merge
    git-svn-id: svn://svn.savannah.nongnu.org/qemu/trunk@859 c046a42c-6fe2-441c-8c8c-71466251a162

--- hw/m48t59.h
+++ hw/m48t59.h
@@ -6,6 +6,7 @@ typedef struct m48t59_t m48t59_t;
 void m48t59_write (m48t59_t *NVRAM, uint32_t val);
 uint32_t m48t59_read (m48t59_t *NVRAM);
 void m48t59_set_addr (m48t59_t *NVRAM, uint32_t addr);
+void m48t59_toggle_lock (m48t59_t *NVRAM, int lock);
 m48t59_t *m48t59_init (int IRQ, uint32_t io_base, uint16_t size);
 
 #endif /* !defined (__M48T59_H__) */
