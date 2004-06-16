05efe46eaa337ce7680a22c5e034686957dc3032
bellard <bellard@c046a42c-6fe2-441c-8c8c-71466251a162>
2004-06-16 20:34:33 +0000

    VMware 4 disk images support
    git-svn-id: svn://svn.savannah.nongnu.org/qemu/trunk@927 c046a42c-6fe2-441c-8c8c-71466251a162

--- vmdk.h
+++ vmdk.h
@@ -32,12 +32,11 @@
 
 struct cowdisk_header
 {
-    char magic[4]; /* COWD */
     uint32_t version;
     uint32_t flags;
     uint32_t disk_sectors;
     uint32_t granularity;
-    uint32_t l1dir_sector;
+    uint32_t l1dir_offset;
     uint32_t l1dir_size;
     uint32_t file_sectors;
     uint32_t cylinders;
@@ -49,4 +48,25 @@ struct cowdisk_header2
 {
    uint32_t parent_ts;
    uint32_t timestamp;
+};
+
+/* based on vdk 3.1 10-11-2003 by Ken Kato */
+
+struct vmdisk_header
+{
+    uint32_t version;
+    uint32_t flags;
+    
+    int64_t capacity;
+    int64_t granularity;
+    int64_t desc_offset;
+    int64_t desc_size;
+    int32_t num_gtes_per_gte;
+    int64_t rgd_offset;
+    int64_t gd_offset;
+    int64_t grain_offset;
+
+    char filler[1];
+
+    char check_bytes[4];
 };
--- vmdk2raw.c
+++ vmdk2raw.c
@@ -30,131 +30,210 @@
 #include "vmdk.h"
 #include "config-host.h"
 
-struct cowdisk_header header;
-struct cowdisk_header2 header2;
-off_t disk_base, disk_limit;
-unsigned int granule_size;
-uint32_t l1dir[L1_SIZE];
+static struct cowdisk_header header;
+static struct vmdisk_header header4;
+static off64_t disk_limit;
+static unsigned int granule_size;
+static uint32_t *l1dir;
 
-unsigned int cached_l2dir;
-uint32_t l2dir[L2_SIZE];
+static unsigned int cached_l2dir;
+static uint32_t l2dir[L2_SIZE];
 
-size_t read_physical(int fd, off64_t offset, size_t length, void *buffer)
+static struct vmdk_prm {
+    uint32_t grain_table_size;
+    uint32_t sectors_per_grain;
+    uint32_t sectors_per_table;
+    uint32_t directory_size;
+} vdsk;
+
+static size_t read_physical(int fd, off64_t offset, size_t length, void *buffer)
 {
     size_t n;
 
-    if (lseek64(fd, offset, SEEK_SET) == -1)
-    {
-        perror("lseek");
+    if (lseek64(fd, offset, SEEK_SET) == -1) {
+        printf(" error trying to seek lseek to %lld", offset);
         return -1;
     }
 
     n = read(fd, buffer, length);
-    if (n == -1)
-    {
-        perror("read from disk");
+    
+    if (n == -1) {
+        printf("read from disk %lld", offset);
         return -1;
     }
 
     return n;
 }
 
-size_t copy_virtual(int in_fd, int out_fd, off64_t offset, void *buffer, size_t length)
+static int read_l1dir(int fd, size_t offset, int num)
 {
-    unsigned int granule_index, granule_offset;
-    unsigned int l1index, l2index;
+    l1dir = malloc(sizeof(*l1dir) * num);
+    if (!l1dir)
+        return -1;
+    return read_physical(fd, offset << SECTOR_BITS, sizeof(*l1dir) * num, (char *)l1dir) != (sizeof(*l1dir) * num);
+}
 
-    granule_index = offset / granule_size;
+static int read_l2dir(int fd, size_t offset, int num)
+{
+    return read_physical(fd, offset << SECTOR_BITS, sizeof(l2dir[0]) * num, (char *)l2dir) != sizeof(l2dir);
+}
+
+static size_t copy_virtual(struct vmdk_prm *dsk, int in_fd, int out_fd, off64_t offset, void *buffer, size_t length)
+{
+    
+    unsigned int granule_offset;
+    unsigned int grain_index;
+    unsigned int sector_map_idx;
+    
     granule_offset = offset % granule_size;
     length = MIN(length, granule_size - granule_offset);
     length = MIN(length, disk_limit - offset);
 
-    l1index = (granule_index >> L2_BITS) & L1_MASK;
-    l2index = granule_index & L2_MASK;
-
-    if (l1dir[l1index] == 0)
-        goto zero_fill;
-
-    if (l1index != cached_l2dir)
-    {
-        if (read_physical(in_fd, (l1dir[l1index] << SECTOR_BITS), sizeof(l2dir), (char *)l2dir) != sizeof(l2dir))
-            return 0;
-
-        cached_l2dir = l1index;
+    sector_map_idx = (offset >> SECTOR_BITS) / dsk->sectors_per_table;
+    
+    if (sector_map_idx >= dsk->directory_size) {
+        fprintf(stderr, "cannot locate grain table for %d in %d\n", sector_map_idx, dsk->directory_size);
+        return -1;
     }
 
-    if (l2dir[l2index] == 0)
+    if (l1dir[sector_map_idx] == 0)
+        goto zero_fill;
+   
+    if (sector_map_idx != cached_l2dir) {
+        if (read_l2dir(in_fd, l1dir[sector_map_idx], dsk->grain_table_size)) {
+            fprintf(stderr, "read failed\n");
+            return -1;
+        }
+        cached_l2dir = sector_map_idx;
+    }
+
+    grain_index = ((offset >> SECTOR_BITS) % dsk->sectors_per_table) / dsk->sectors_per_grain;
+    
+    if (grain_index >= dsk->grain_table_size) {
+        fprintf(stderr, "grain to large");
+        return -1;
+    }
+
+    if (l2dir[grain_index] == 0)
         goto zero_fill;
 
-    if (read_physical(in_fd, (l2dir[l2index] << SECTOR_BITS) + granule_offset, length, buffer) != length)
-        return 0;
-
+    if (read_physical(in_fd, (l2dir[grain_index] << SECTOR_BITS) + granule_offset, length, buffer) != length) {
+        fprintf(stderr, "read error 2\n");
+        return -1;
+    }
+    
     write(out_fd, buffer, length);
     return length;
 
 zero_fill:
     /* the last chunk of the file can not be sparse
      * or the file will be truncated */
-    if (offset + length < disk_limit) {
-        memset(buffer, 0, length);
-        write(out_fd, buffer, length);
+    if (offset + length >= disk_limit) {
+        if (lseek64(out_fd, length-1, SEEK_CUR) == (off_t)-1)
+            perror("lseek");
+        /* write the last NULL byte instead of seeking */
+        const char nil = 0;
+        write(out_fd, &nil, 1);
     } else {
-        if (lseek(out_fd, length, SEEK_CUR) == (off_t)-1)
+        if (lseek64(out_fd, length, SEEK_CUR) == (off_t)-1)
             perror("lseek");
     }
     return length;
 }
 
-
-int open_vmdk(const char *filename)
+static int open_vmdk4(int fd)
 {
-    int fd = open(filename, O_RDONLY | O_LARGEFILE);
-    if (fd == -1)
-    {
-        perror(filename);
-        return -1;
-    }
-
-    if (read(fd, &header, sizeof(header)) != sizeof(header))
-    {
+    if (read(fd, &header4, sizeof(header4)) != sizeof(header4)) {
         perror("read from disk");
         return -1;
     }
+    
+    granule_size = header4.granularity << SECTOR_BITS;
+    disk_limit = header4.capacity << SECTOR_BITS; 
+    
+    cached_l2dir = -1;
+    vdsk.grain_table_size = header4.num_gtes_per_gte;
+    vdsk.sectors_per_grain = header4.granularity;
+    vdsk.sectors_per_table = vdsk.grain_table_size * vdsk.sectors_per_grain;
+    vdsk.directory_size = (header4.capacity + vdsk.sectors_per_table - 1) / vdsk.sectors_per_table + 1;
 
-    if (memcmp(header.magic, "COWD", 4) != 0)
-    {
-        fprintf(stderr, "%s is not a VMware virtual disk image\n", filename);
+    if (read_l1dir(fd, header4.rgd_offset, vdsk.directory_size))
+        return -1;
+    
+    return 0;
+    
+}
+
+static int open_vmdk3(int fd)
+{
+    if (read(fd, &header, sizeof(header)) != sizeof(header)) {
+        perror("read from disk\n");
         return -1;
     }
-
     granule_size = header.granularity << SECTOR_BITS;
-    if (read_physical(fd, header.l1dir_sector << SECTOR_BITS, sizeof(l1dir), (char *)l1dir) != sizeof(l1dir))
+    vdsk.sectors_per_grain = header.granularity;
+    vdsk.grain_table_size = L2_SIZE;
+    vdsk.sectors_per_table = vdsk.grain_table_size * vdsk.sectors_per_grain;
+    vdsk.directory_size = L1_SIZE;
+    if (read_l1dir(fd, header.l1dir_offset, L1_SIZE))
         return -1;
 
     disk_limit = header.disk_sectors << SECTOR_BITS;
 
+    return fd;
+}
+
+static int open_vmdk(const char *filename)
+{
+    int fd = open(filename, O_RDONLY | O_LARGEFILE);
+    if (fd == -1) {
+        perror(filename);
+        return -1;
+    }
+
+    char magic[4];
+    if (read(fd, &magic, sizeof(magic)) != sizeof(magic)) {
+        perror("read from disk");
+        return -1;
+    }
+    
+    if (!memcmp(magic, "KDMV", sizeof(magic))) {
+        open_vmdk4(fd);
+    } else if (!memcmp(magic, "COWD", sizeof(magic))) {
+        open_vmdk3(fd);
+    } else {
+        fprintf(stderr, "%s is not a VMware virtual disk image\n", filename);
+        return -1;
+    }
+    
     cached_l2dir = -1;
     return fd;
 }
 
-void help(void)
+static void help(void)
 {
     printf("vmdk2raw\n"
            "usage: vmdk2raw vmware_image output_image\n"
            "\n"
-           "vmware_image   a vmware 2.x/3.x cow image\n"
+           "vmware_image   a vmware cow image\n"
            "output_image   the created disk image\n" 
           );
     exit(1);
 }
 
-#define BUF_SIZE granule_size
-void copy_disk(in_fd, out_fd)
+#define BUF_SIZE 0x10000
+static void copy_disk(in_fd, out_fd)
 {
     char buf[BUF_SIZE];
     off64_t i = 0;
+    int ret;
     while (i < disk_limit) {
-        i += copy_virtual(in_fd, out_fd, i, buf, sizeof(buf));
+        ret = copy_virtual(&vdsk, in_fd, out_fd, i, buf, sizeof(buf));
+        if (ret < 0) {
+            fprintf(stderr, "copying failed\n");
+            exit(-1);
+        }
+        i += ret;
     }
 }
 
@@ -170,7 +249,7 @@ int main(int argc, char **argv)
         return -1;
     }
 
-    out_fd = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR);
+    out_fd = open(argv[2], O_WRONLY | O_LARGEFILE | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR);
     if (out_fd < 0) {
         perror(argv[2]);
         return -1;
