979a54fb20c6461669d8e620cd9760544c84254b
bellard <bellard@c046a42c-6fe2-441c-8c8c-71466251a162>
2004-06-20 12:36:04 +0000

    new reset API
    git-svn-id: svn://svn.savannah.nongnu.org/qemu/trunk@936 c046a42c-6fe2-441c-8c8c-71466251a162

--- sdl.c
+++ sdl.c
@@ -318,7 +318,7 @@ static void sdl_refresh(DisplayState *ds)
             sdl_process_key(&ev->key);
             break;
         case SDL_QUIT:
-            reset_requested = 1;
+            qemu_system_shutdown_request();
             break;
         case SDL_MOUSEMOTION:
             if (gui_grab) {
