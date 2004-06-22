dc5d0b3d1b2e0fdb48fcf4033058bc351580959d
bellard <bellard@c046a42c-6fe2-441c-8c8c-71466251a162>
2004-06-22 18:43:30 +0000

    disable buggy tb_invalidate_page_range()
    git-svn-id: svn://svn.savannah.nongnu.org/qemu/trunk@968 c046a42c-6fe2-441c-8c8c-71466251a162

--- cpu-exec.c
+++ cpu-exec.c
@@ -602,9 +602,13 @@ int cpu_exec(CPUState *env1)
    generated */
 void tb_invalidate_page_range(target_ulong start, target_ulong end)
 {
+    /* XXX: cannot enable it yet because it yields to MMU exception
+       where NIP != read address on PowerPC */
+#if 0
     target_ulong phys_addr;
     phys_addr = get_phys_addr_code(env, start);
     tb_invalidate_phys_page_range(phys_addr, phys_addr + end - start, 0);
+#endif
 }
 
 #if defined(TARGET_I386) && defined(CONFIG_USER_ONLY)
