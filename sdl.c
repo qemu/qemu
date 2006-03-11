5b31187812694ef575c20fc3fe3123685a306fde
pbrook <pbrook@c046a42c-6fe2-441c-8c8c-71466251a162>
2006-03-11 20:07:45 +0000

    Make sure gui_key_modifier_pressed is cleared correctly. Use correct event fields for SDL_ACTIVEEVENT.
    git-svn-id: svn://svn.savannah.nongnu.org/qemu/trunk@1780 c046a42c-6fe2-441c-8c8c-71466251a162

--- sdl.c
+++ sdl.c
@@ -404,6 +404,7 @@ static void sdl_refresh(DisplayState *ds)
                 mod_state = (ev->key.keysym.mod & gui_grab_code);
                 if (!mod_state) {
                     if (gui_key_modifier_pressed) {
+                        gui_key_modifier_pressed = 0;
                         if (gui_keysym == 0) {
                             /* exit/enter grab if pressing Ctrl-Alt */
                             if (!gui_grab)
@@ -415,7 +416,6 @@ static void sdl_refresh(DisplayState *ds)
                             reset_keys();
                             break;
                         }
-                        gui_key_modifier_pressed = 0;
                         gui_keysym = 0;
                     }
                 }
@@ -456,8 +456,8 @@ static void sdl_refresh(DisplayState *ds)
             }
             break;
         case SDL_ACTIVEEVENT:
-            if (gui_grab && (ev->active.gain & SDL_ACTIVEEVENTMASK) == 0 &&
-                !gui_fullscreen_initial_grab) {
+            if (gui_grab && ev->active.state == SDL_APPINPUTFOCUS &&
+                !ev->active.gain && !gui_fullscreen_initial_grab) {
                 sdl_grab_end();
             }
             break;
