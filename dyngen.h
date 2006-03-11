f5ba07d399ff205da3e91de4389e936027856b48
pbrook <pbrook@c046a42c-6fe2-441c-8c8c-71466251a162>
2006-03-11 20:43:48 +0000

    Fix typo in ppc icache flush.
    git-svn-id: svn://svn.savannah.nongnu.org/qemu/trunk@1782 c046a42c-6fe2-441c-8c8c-71466251a162

--- dyngen.h
+++ dyngen.h
@@ -59,7 +59,7 @@ static void inline flush_icache_range(unsigned long start, unsigned long stop)
 {
     unsigned long p;
 
-    p = start & ~(MIN_CACHE_LINE_SIZE - 1);
+    start &= ~(MIN_CACHE_LINE_SIZE - 1);
     stop = (stop + MIN_CACHE_LINE_SIZE - 1) & ~(MIN_CACHE_LINE_SIZE - 1);
     
     for (p = start; p < stop; p += MIN_CACHE_LINE_SIZE) {
