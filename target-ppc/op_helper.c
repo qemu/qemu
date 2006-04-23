f9ebe432db4e0933bdedae021d3b1e23f188be12
bellard <bellard@c046a42c-6fe2-441c-8c8c-71466251a162>
2006-04-23 18:18:10 +0000

    removed unnecessary header
    git-svn-id: svn://svn.savannah.nongnu.org/qemu/trunk@1835 c046a42c-6fe2-441c-8c8c-71466251a162

--- target-ppc/op_helper.c
+++ target-ppc/op_helper.c
@@ -17,7 +17,6 @@
  * License along with this library; if not, write to the Free Software
  * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
  */
-#include <math.h>
 #include "exec.h"
 
 #define MEMSUFFIX _raw
