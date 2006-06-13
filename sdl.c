8785a8ddcc6e772d82744279447bb9541da5a407
bellard <bellard@c046a42c-6fe2-441c-8c8c-71466251a162>
2006-06-13 10:49:12 +0000

    'invisible wall' patch (Anthony Liguori)
    git-svn-id: svn://svn.savannah.nongnu.org/qemu/trunk@1953 c046a42c-6fe2-441c-8c8c-71466251a162

--- sdl.c
+++ sdl.c
@@ -285,13 +285,18 @@ static void sdl_update_caption(void)
 
 static void sdl_hide_cursor(void)
 {
-    SDL_SetCursor(sdl_cursor_hidden);
+    if (kbd_mouse_is_absolute()) {
+        SDL_ShowCursor(1);
+        SDL_SetCursor(sdl_cursor_hidden);
+    } else {
+        SDL_ShowCursor(0);
+    }
 }
 
 static void sdl_show_cursor(void)
 {
     if (!kbd_mouse_is_absolute()) {
-	SDL_SetCursor(sdl_cursor_normal);
+        SDL_ShowCursor(1);
     }
 }
 
