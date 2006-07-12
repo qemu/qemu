26f1eb27f542894a304b8aa8abfd03362defa12d
weil <weil@11fd3d49-aaf6-0310-8052-ea9566248e5e>
2006-07-12 21:00:13 +0000

    added CFI 1.0 support and use it for AR7 simulation
    git-svn-id: svn+ssh://svn.berlios.de/svnroot/repos/ar7-firmware/qemu/trunk/qemu@94 11fd3d49-aaf6-0310-8052-ea9566248e5e

--- hw/pflash_cfi01.c
+++ hw/pflash_cfi01.c
@@ -32,26 +32,25 @@
 #define FlashCommandPrefix1     0xAA
 #define FlashCommandPrefix2     0x55
 
-
 /*
- *  CFI parallel flash with Intel command set emulation
+ * CFI parallel flash with Intel command set emulation
  * 
- *  Copyright (c) 2005 Jocelyn Mayer
- *  Copyright (c) 2006 Stefan Weil (TE28F160C3-B emulation)
+ * Copyright (c) 2005 Jocelyn Mayer (CFI 2.0, see original cfi02.c)
+ * Copyright (c) 2006 Stefan Weil (CFI 1.0, TE28F160C3-B emulation)
  *
- * This library is free software; you can redistribute it and/or
- * modify it under the terms of the GNU Lesser General Public
- * License as published by the Free Software Foundation; either
- * version 2 of the License, or (at your option) any later version.
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
  *
- * This library is distributed in the hope that it will be useful,
+ * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
- * Lesser General Public License for more details.
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
  *
- * You should have received a copy of the GNU Lesser General Public
- * License along with this library; if not, write to the Free Software
- * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
  */
 
 /*
@@ -64,13 +63,13 @@
  * - chip erase
  * - unlock bypass command
  * - CFI queries
+ * - JEDEC ID (partially)
  *
  * It does not support flash interleaving.
  * It does not implement boot blocs with reduced size
  * It does not implement software data protection as found in many real chips
  * It does not implement erase suspend/resume commands
  * It does not implement multiple sectors erase
- * It does not implement JEDEC ID.
  */
 
 #include <assert.h>
