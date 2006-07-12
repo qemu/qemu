1ba4ff9c0376e5665abc920956a296a8da6adfa2
weil <weil@11fd3d49-aaf6-0310-8052-ea9566248e5e>
2006-07-12 21:00:13 +0000

    added CFI 1.0 support and use it for AR7 simulation
    git-svn-id: svn+ssh://svn.berlios.de/svnroot/repos/ar7-firmware/qemu/trunk@94 11fd3d49-aaf6-0310-8052-ea9566248e5e

--- qemu/hw/pflash.h
+++ qemu/hw/pflash.h
@@ -1,4 +1,21 @@
-/* declarations from Linux header files */
+/*
+ * This file contains parts from Linux header files (several authors)
+ * Copyright (c) 2006 Stefan Weil (modifications for QEMU integration)
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+ */
 
 #define P_ID_NONE 0
 #define P_ID_INTEL_EXT 1
@@ -178,3 +195,5 @@ typedef enum {
 
 /* Winbond */
 #define W49V002A	0x00b0
+
+/* eof */
