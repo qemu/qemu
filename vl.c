e50527e70dff9202fea22ffa1e879ddcd7accf5c
weil <weil@11fd3d49-aaf6-0310-8052-ea9566248e5e>
2006-11-01 21:35:39 +0000

    cvs update
    git-svn-id: svn+ssh://svn.berlios.de/svnroot/repos/ar7-firmware/qemu/trunk/qemu@229 11fd3d49-aaf6-0310-8052-ea9566248e5e

--- vl.c
+++ vl.c
@@ -1228,6 +1228,11 @@ static void fd_chr_read(void *opaque)
     if (len == 0)
         return;
     size = read(s->fd_in, buf, len);
+    if (size == 0) {
+        /* FD has been closed. Remove it from the active list.  */
+        qemu_set_fd_handler2(s->fd_in, NULL, NULL, NULL, NULL);
+        return;
+    }
     if (size > 0) {
         s->fd_read(s->fd_opaque, buf, size);
     }
