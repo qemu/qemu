7f1c9da99804235212888abda7ba7f06ee3afc70
ths <ths@c046a42c-6fe2-441c-8c8c-71466251a162>
2007-01-18 22:43:16 +0000

    Darwin build fix, by Pierre d'Herbemont.
    git-svn-id: svn://svn.savannah.nongnu.org/qemu/trunk@2334 c046a42c-6fe2-441c-8c8c-71466251a162

--- block.c
+++ block.c
@@ -35,6 +35,10 @@
 #define SECTOR_BITS 9
 #define SECTOR_SIZE (1 << SECTOR_BITS)
 
+#ifndef ENOMEDIUM
+#define ENOMEDIUM ENODEV
+#endif
+
 typedef struct BlockDriverAIOCBSync {
     BlockDriverAIOCB common;
     QEMUBH *bh;
