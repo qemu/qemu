c2403882535f50208a90001e9d18c33882a1366f
weil <weil@11fd3d49-aaf6-0310-8052-ea9566248e5e>
2007-02-20 19:41:34 +0000

    cvs update
    git-svn-id: svn+ssh://svn.berlios.de/svnroot/repos/ar7-firmware/qemu/trunk@465 11fd3d49-aaf6-0310-8052-ea9566248e5e

--- hw/mips_malta.c
+++ hw/mips_malta.c
@@ -735,6 +735,7 @@ void mips_malta_init (int ram_size, int vga_ram_size, int boot_device,
     kbd_init();
     rtc_state = rtc_init(0x70, 8);
     serial_init(&pic_set_irq_new, isa_pic, 0x3f8, 4, serial_hds[0]);
+    serial_init(&pic_set_irq_new, isa_pic, 0x2f8, 4, serial_hds[0]);
     parallel_init(0x378, 7, parallel_hds[0]);
     /* XXX: The floppy controller does not work correctly, something is
        probably wrong.
