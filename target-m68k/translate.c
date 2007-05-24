986b002367c3261f3fe19210ef3aefcfce05d1be
weil <weil@11fd3d49-aaf6-0310-8052-ea9566248e5e>
2007-05-24 19:17:18 +0000

    added missing include
    git-svn-id: svn+ssh://svn.berlios.de/svnroot/repos/ar7-firmware/qemu/trunk@683 11fd3d49-aaf6-0310-8052-ea9566248e5e

--- target-m68k/translate.c
+++ target-m68k/translate.c
@@ -19,6 +19,7 @@
  * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
  */
 #include <stdarg.h>
+#include <stddef.h>
 #include <stdlib.h>
 #include <stdio.h>
 #include <string.h>
