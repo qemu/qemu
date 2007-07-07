90508d9b6fc3ccf5073ad3133709021e08775a82
weil <weil@11fd3d49-aaf6-0310-8052-ea9566248e5e>
2007-07-07 17:07:15 +0000

    update to GPL 3
    git-svn-id: svn+ssh://svn.berlios.de/svnroot/repos/ar7-firmware/qemu/trunk@750 11fd3d49-aaf6-0310-8052-ea9566248e5e

--- hw/dp8381x.c
+++ hw/dp8381x.c
@@ -1,11 +1,11 @@
 /*
  * QEMU emulation for National Semiconductor DP83815 / DP83816.
  * 
- * Copyright (c) 2006 Stefan Weil
- * 
- * This program is free software; you can redistribute it and/or modify
+ * Copyright (C) 2006-2007 Stefan Weil
+ *
+ * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation; either version 2 of the License, or
+ * the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
  *
  * This program is distributed in the hope that it will be useful,
@@ -14,8 +14,7 @@
  * GNU General Public License for more details.
  *
  * You should have received a copy of the GNU General Public License
- * along with this program; if not, write to the Free Software
- * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  *
  * Datasheets are available from National Semiconductor, see
  * http://www.national.com/pf/DP/DP83815.html
