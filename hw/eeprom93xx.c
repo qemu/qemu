358db7f1900d4ac155cdd5a318e52d51438c3d34
weil <weil@11fd3d49-aaf6-0310-8052-ea9566248e5e>
2007-07-21 16:16:04 +0000

    update to GPL 3
    git-svn-id: svn+ssh://svn.berlios.de/svnroot/repos/ar7-firmware/qemu/trunk@778 11fd3d49-aaf6-0310-8052-ea9566248e5e

--- hw/eeprom93xx.c
+++ hw/eeprom93xx.c
@@ -1,11 +1,11 @@
 /*
  * QEMU EEPROM 93xx emulation
- * 
+ *
  * Copyright (c) 2006-2007 Stefan Weil
- * 
- * This program is free software; you can redistribute it and/or modify
+ *
+ * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation; either version 2 of the License, or
+ * the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
  *
  * This program is distributed in the hope that it will be useful,
@@ -14,8 +14,7 @@
  * GNU General Public License for more details.
  *
  * You should have received a copy of the GNU General Public License
- * along with this program; if not, write to the Free Software
- * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
 /* Emulation for serial EEPROMs:
@@ -31,6 +30,8 @@
  * eeprom93xx_read  - read data from the EEPROM
  * eeprom93xx_write - write data to the EEPROM
  * eeprom93xx_data  - get EEPROM data array for external manipulation
+ *
+ * Hint: This driver always uses host endianess!
  *
  * Todo list:
  * - No emulation of EEPROM timings.
