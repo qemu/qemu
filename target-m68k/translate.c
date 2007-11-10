8fc7cc580f94dff04dd3d72d912bae38f0b614f9
pbrook <pbrook@c046a42c-6fe2-441c-8c8c-71466251a162>
2007-11-10 17:43:49 +0000

    Fix m68k FP register debug dumps.
    git-svn-id: svn://svn.savannah.nongnu.org/qemu/trunk@3566 c046a42c-6fe2-441c-8c8c-71466251a162

--- target-m68k/translate.c
+++ target-m68k/translate.c
@@ -3291,13 +3291,13 @@ void cpu_dump_state(CPUState *env, FILE *f,
         u.d = env->fregs[i];
         cpu_fprintf (f, "D%d = %08x   A%d = %08x   F%d = %08x%08x (%12g)\n",
                      i, env->dregs[i], i, env->aregs[i],
-                     i, u.l.upper, u.l.lower, u.d);
+                     i, u.l.upper, u.l.lower, *(double *)&u.d);
       }
     cpu_fprintf (f, "PC = %08x   ", env->pc);
     sr = env->sr;
     cpu_fprintf (f, "SR = %04x %c%c%c%c%c ", sr, (sr & 0x10) ? 'X' : '-',
                  (sr & CCF_N) ? 'N' : '-', (sr & CCF_Z) ? 'Z' : '-',
                  (sr & CCF_V) ? 'V' : '-', (sr & CCF_C) ? 'C' : '-');
-    cpu_fprintf (f, "FPRESULT = %12g\n", env->fp_result);
+    cpu_fprintf (f, "FPRESULT = %12g\n", *(double *)&env->fp_result);
 }
 
