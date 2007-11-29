c308917e01aa9b30e87104343ae23ee5c14ee243
weil <weil@11fd3d49-aaf6-0310-8052-ea9566248e5e>
2007-11-29 20:31:43 +0000

    fixed for my pflash interface
    git-svn-id: svn+ssh://svn.berlios.de/svnroot/repos/ar7-firmware/qemu/trunk@958 11fd3d49-aaf6-0310-8052-ea9566248e5e

--- hw/gumstix.c
+++ hw/gumstix.c
@@ -103,7 +103,7 @@ static void verdex_init(int ram_size, int vga_ram_size,
         exit(1);
     }
 
-    if (!pflash_register(0x00000000, qemu_ram_alloc(verdex_rom),
+    if (!pflash_cfi01_register(0x00000000, qemu_ram_alloc(verdex_rom),
             pflash_table[0], sector_len, verdex_rom / sector_len,
             2, 0, 0, 0, 0)) {
         fprintf(stderr, "qemu: Error registering flash memory.\n");
