eb24dadde5f62b532bb887ba38e468f65692d96b
aurel32 <aurel32@c046a42c-6fe2-441c-8c8c-71466251a162>
2008-03-18 06:55:27 +0000

    Typo in curses_keys.h.
    (Dan Nelson)
    
    
    git-svn-id: svn://svn.savannah.nongnu.org/qemu/trunk@4084 c046a42c-6fe2-441c-8c8c-71466251a162

--- curses_keys.h
+++ curses_keys.h
@@ -198,7 +198,7 @@ int curses2keycode[CURSES_KEYS] = {
 
     [0x001] = 30 | CNTRL, /* Control + a */
     [0x013] = 31 | CNTRL, /* Control + s */
-    [0x014] = 32 | CNTRL, /* Control + d */
+    [0x004] = 32 | CNTRL, /* Control + d */
     [0x006] = 33 | CNTRL, /* Control + f */
     [0x007] = 34 | CNTRL, /* Control + g */
     [0x008] = 35 | CNTRL, /* Control + h */
