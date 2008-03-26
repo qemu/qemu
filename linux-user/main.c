44de1b33694ae45431743000fdd8f719a0b6e1cf
pbrook <pbrook@c046a42c-6fe2-441c-8c8c-71466251a162>
2008-03-26 22:40:25 +0000

    Revert accidental commit.
    git-svn-id: svn://svn.savannah.nongnu.org/qemu/trunk@4112 c046a42c-6fe2-441c-8c8c-71466251a162

--- linux-user/main.c
+++ linux-user/main.c
@@ -1945,7 +1945,7 @@ int main(int argc, char **argv)
     char **target_environ, **wrk, **dst;
 
     if (argc <= 1)
-        usage(1);
+        usage();
 
     /* init debug */
     cpu_set_log_filename(DEBUG_LOGFILE);
@@ -1983,7 +1983,7 @@ int main(int argc, char **argv)
             r = argv[optind++];
             x86_stack_size = strtol(r, (char **)&r, 0);
             if (x86_stack_size <= 0)
-                usage(1);
+                usage();
             if (*r == 'M')
                 x86_stack_size *= 1024 * 1024;
             else if (*r == 'k' || *r == 'K')
