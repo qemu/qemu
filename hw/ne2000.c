e89f00e6a3fcba77ce5ffd616a2ca93de5c8fcab
aurel32 <aurel32@c046a42c-6fe2-441c-8c8c-71466251a162>
2008-03-28 22:57:48 +0000

    Revert "Fix ne2000_can_receive() function".
    When the card is stopped, it should always accept packets, and then
    discard them. Thanks to Paul Brook for the explanations.
    
    
    git-svn-id: svn://svn.savannah.nongnu.org/qemu/trunk@4128 c046a42c-6fe2-441c-8c8c-71466251a162

--- hw/ne2000.c
+++ hw/ne2000.c
@@ -217,7 +217,7 @@ static int ne2000_can_receive(void *opaque)
     NE2000State *s = opaque;
 
     if (s->cmd & E8390_STOP)
-        return 0;
+        return 1;
     return !ne2000_buffer_full(s);
 }
 
