603d3e79b2c64bceb8826d3d65b525188db47797
aurel32 <aurel32@c046a42c-6fe2-441c-8c8c-71466251a162>
2008-05-05 21:26:43 +0000

    qemu: generate signals on tap I/O
    Currently tap does not generate signals on I/O; this causes
    network latency to be dependent on the timer tick (1ms without
    dyntick, guest dependent with dyntick).  By generating a signal
    on I/O, we can inform the guest immediately that a packet has
    arrived.
    
    Signed-off-by: Aurelien Jarno <aurelien@aurel32.net>
    
    
    git-svn-id: svn://svn.savannah.nongnu.org/qemu/trunk@4341 c046a42c-6fe2-441c-8c8c-71466251a162

--- vl.c
+++ vl.c
@@ -4030,6 +4030,7 @@ static TAPState *net_tap_fd_init(VLANState *vlan, int fd)
     if (!s)
         return NULL;
     s->fd = fd;
+    enable_sigio_timer(fd);
     s->vc = qemu_new_vlan_client(vlan, tap_receive, NULL, s);
     qemu_set_fd_handler(s->fd, tap_send, NULL, s);
     snprintf(s->vc->info_str, sizeof(s->vc->info_str), "tap: fd=%d", fd);
