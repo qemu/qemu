d478990a524d5f61986512afb260b56bb120702d
aurel32 <aurel32@c046a42c-6fe2-441c-8c8c-71466251a162>
2008-05-05 21:27:01 +0000

    Don't stop translation for mtc0 compare
    Signed-off-by: Aurelien Jarno <aurelien@aurel32.net>
    
    
    git-svn-id: svn://svn.savannah.nongnu.org/qemu/trunk@4343 c046a42c-6fe2-441c-8c8c-71466251a162

--- target-mips/translate.c
+++ target-mips/translate.c
@@ -2883,8 +2883,6 @@ static void gen_mtc0 (CPUState *env, DisasContext *ctx, int reg, int sel)
         default:
             goto die;
         }
-        /* Stop translation as we may have switched the execution mode */
-        ctx->bstate = BS_STOP;
         break;
     case 12:
         switch (sel) {
