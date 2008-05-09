e83a86737fa1569df3f198d912b8e51de2edf63a
edgar_igl <edgar_igl@c046a42c-6fe2-441c-8c8c-71466251a162>
2008-05-09 05:55:18 +0000

    Make a safer guess for max generated ops per guest insn (spotted by Jan Kiszka).
    git-svn-id: svn://svn.savannah.nongnu.org/qemu/trunk@4388 c046a42c-6fe2-441c-8c8c-71466251a162

--- exec-all.h
+++ exec-all.h
@@ -30,7 +30,7 @@
 struct TranslationBlock;
 
 /* XXX: make safe guess about sizes */
-#define MAX_OP_PER_INSTR 32
+#define MAX_OP_PER_INSTR 64
 /* A Call op needs up to 6 + 2N parameters (N = number of arguments).  */
 #define MAX_OPC_PARAM 10
 #define OPC_BUF_SIZE 512
