5cbdd273fbf5e977d14b1f06976489d8e4625a68
ths <ths@c046a42c-6fe2-441c-8c8c-71466251a162>
2008-05-22 13:51:48 +0000

    Fix block-vmdk.c:vmdk_close() use-after-free, by Ed Maste.
    git-svn-id: svn://svn.savannah.nongnu.org/qemu/trunk@4526 c046a42c-6fe2-441c-8c8c-71466251a162

--- block-vmdk.c
+++ block-vmdk.c
@@ -808,9 +808,9 @@ static void vmdk_close(BlockDriverState *bs)
 
     qemu_free(s->l1_table);
     qemu_free(s->l2_cache);
-    bdrv_delete(s->hd);
     // try to close parent image, if exist
     vmdk_parent_close(s->hd);
+    bdrv_delete(s->hd);
 }
 
 static void vmdk_flush(BlockDriverState *bs)
