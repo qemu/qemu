a5973fbff16ec5f58b648929beb885da3f6e2373
bellard <bellard@c046a42c-6fe2-441c-8c8c-71466251a162>
2008-05-28 12:34:49 +0000

    more tests
    git-svn-id: svn://svn.savannah.nongnu.org/qemu/trunk@4601 c046a42c-6fe2-441c-8c8c-71466251a162

--- tests/test-i386-shift.h
+++ tests/test-i386-shift.h
@@ -145,8 +145,7 @@ void exec_op(long s2, long s0, long s1)
 #endif
     exec_opl(s2, s0, s1, 0);
 #ifdef OP_SHIFTD
-    if (s1 <= 15)
-        exec_opw(s2, s0, s1, 0);
+    exec_opw(s2, s0, s1, 0);
 #else
     exec_opw(s2, s0, s1, 0);
 #endif
