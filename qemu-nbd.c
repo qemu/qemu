315bc7aa0eb8a0d30a2ffb4f8bfc605575629d61
ths <ths@c046a42c-6fe2-441c-8c8c-71466251a162>
2008-07-18 18:06:23 +0000

    Allow qemu-nbd --version to show the application name dynamically, by Carlo Marcelo Arenas Belon.
    git-svn-id: svn://svn.savannah.nongnu.org/qemu/trunk@4893 c046a42c-6fe2-441c-8c8c-71466251a162

--- qemu-nbd.c
+++ qemu-nbd.c
@@ -68,13 +68,13 @@ static void usage(const char *name)
 static void version(const char *name)
 {
     printf(
-"qemu-nbd version 0.0.1\n"
+"%s version 0.0.1\n"
 "Written by Anthony Liguori.\n"
 "\n"
 "Copyright (C) 2006 Anthony Liguori <anthony@codemonkey.ws>.\n"
 "This is free software; see the source for copying conditions.  There is NO\n"
 "warranty; not even for MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.\n"
-    );
+    , name);
 }
 
 struct partition_record
