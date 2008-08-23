492b239197b1695df12cab1f05a72d1cd2c14058
aurel32 <aurel32@c046a42c-6fe2-441c-8c8c-71466251a162>
2008-08-23 17:22:19 +0000

    MIPS: don't free TCG temporary variable twice
    In gen_dmtc0 function, TCG temporary variable t0 is freed at the end of
    the function. Variable is freed again in the gen_dmtc0 caller.
    I removed the free in gen_dmtc0, to do like in gen_dmfc0, gen_mfc0,
    gen_mtc0.
    
    (Hervé Poussineau)
    
    git-svn-id: svn://svn.savannah.nongnu.org/qemu/trunk@5075 c046a42c-6fe2-441c-8c8c-71466251a162

--- target-mips/translate.c
+++ target-mips/translate.c
@@ -5188,7 +5188,6 @@ static void gen_dmtc0 (CPUState *env, DisasContext *ctx, TCGv t0, int reg, int s
                 rn, reg, sel);
     }
 #endif
-    tcg_temp_free(t0);
     /* For simplicity assume that all writes can cause interrupts.  */
     if (use_icount) {
         gen_io_end();
@@ -5197,7 +5196,6 @@ static void gen_dmtc0 (CPUState *env, DisasContext *ctx, TCGv t0, int reg, int s
     return;
 
 die:
-    tcg_temp_free(t0);
 #if defined MIPS_DEBUG_DISAS
     if (loglevel & CPU_LOG_TB_IN_ASM) {
         fprintf(logfile, "dmtc0 %s (reg %d sel %d)\n",
