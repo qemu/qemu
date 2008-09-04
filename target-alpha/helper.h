59df7f62a9ba5358e7f2e2498d38d5f8719cc05a
aurel32 <aurel32@c046a42c-6fe2-441c-8c8c-71466251a162>
2008-09-04 20:24:14 +0000

    alpha: add target-alpha/helper.h (missing from commit r5150)
    git-svn-id: svn://svn.savannah.nongnu.org/qemu/trunk@5161 c046a42c-6fe2-441c-8c8c-71466251a162

---
+++ target-alpha/helper.h
@@ -0,0 +1,10 @@
+#ifndef DEF_HELPER
+#define DEF_HELPER(ret, name, params) ret name params;
+#endif
+
+DEF_HELPER(void, helper_tb_flush, (void))
+#ifndef DEF_HELPER
+#define DEF_HELPER(ret, name, params) ret name params;
+#endif
+
+DEF_HELPER(void, helper_tb_flush, (void))
