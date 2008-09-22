48d7c50f843141b743d2d212ed1275d037f5a247
blueswir1 <blueswir1@c046a42c-6fe2-441c-8c8c-71466251a162>
2008-09-22 16:29:18 +0000

    Remove unnecessary call to qemu_aio_init (called from bdrv_init)
    git-svn-id: svn://svn.savannah.nongnu.org/qemu/trunk@5294 c046a42c-6fe2-441c-8c8c-71466251a162

--- vl.c
+++ vl.c
@@ -8981,7 +8981,6 @@ int main(int argc, char **argv)
 
     init_timers();
     init_timer_alarm();
-    qemu_aio_init();
     if (use_icount && icount_time_shift < 0) {
         use_icount = 2;
         /* 125MIPS seems a reasonable initial guess at the guest speed.
