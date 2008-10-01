1cbed99ca2887d1940daab44a9e1adccd087783a
weil <weil@11fd3d49-aaf6-0310-8052-ea9566248e5e>
2008-10-01 20:43:33 +0000

    add attribute noreturn
    git-svn-id: svn+ssh://svn.berlios.de/svnroot/repos/ar7-firmware/qemu/trunk@1222 11fd3d49-aaf6-0310-8052-ea9566248e5e

--- exec-all.h
+++ exec-all.h
@@ -75,7 +75,7 @@ int cpu_restore_state_copy(struct TranslationBlock *tb,
                            CPUState *env, unsigned long searched_pc,
                            void *puc);
 void cpu_resume_from_signal(CPUState *env1, void *puc) __attribute__((__noreturn__));
-void cpu_io_recompile(CPUState *env, void *retaddr);
+void cpu_io_recompile(CPUState *env, void *retaddr) __attribute__((__noreturn__));
 TranslationBlock *tb_gen_code(CPUState *env, 
                               target_ulong pc, target_ulong cs_base, int flags,
                               int cflags);
