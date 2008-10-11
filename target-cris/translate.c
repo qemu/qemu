a855593c33fcc6efd5d9033a0942b56a08a8f8be
edgar_igl <edgar_igl@c046a42c-6fe2-441c-8c8c-71466251a162>
2008-10-11 19:36:17 +0000

    CRIS: Initialize the arch version preg to v32.
    git-svn-id: svn://svn.savannah.nongnu.org/qemu/trunk@5462 c046a42c-6fe2-441c-8c8c-71466251a162

--- target-cris/translate.c
+++ target-cris/translate.c
@@ -3411,6 +3411,7 @@ void cpu_reset (CPUCRISState *env)
 	memset(env, 0, offsetof(CPUCRISState, breakpoints));
 	tlb_flush(env, 1);
 
+	env->pregs[PR_VR] = 32;
 #if defined(CONFIG_USER_ONLY)
 	/* start in user mode with interrupts enabled.  */
 	env->pregs[PR_CCS] |= U_FLAG | I_FLAG;
