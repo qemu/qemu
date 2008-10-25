653a07b222f76c832625cccbb1a097dde3048422
blueswir1 <blueswir1@c046a42c-6fe2-441c-8c8c-71466251a162>
2008-10-25 13:48:59 +0000

    Change freeing method because the allocation function was changed in r5532
    git-svn-id: svn://svn.savannah.nongnu.org/qemu/trunk@5536 c046a42c-6fe2-441c-8c8c-71466251a162

--- hw/bt-hci.c
+++ hw/bt-hci.c
@@ -1135,7 +1135,7 @@ void bt_hci_reset(struct bt_hci_s *hci)
     hci->device.inquiry_scan = 0;
     hci->device.page_scan = 0;
     if (hci->device.lmp_name)
-        free((void *) hci->device.lmp_name);
+        qemu_free((void *) hci->device.lmp_name);
     hci->device.lmp_name = 0;
     hci->device.class[0] = 0x00;
     hci->device.class[1] = 0x00;
@@ -1813,7 +1813,7 @@ static void bt_submit_hci(struct HCIInfo *info,
         LENGTH_CHECK(change_local_name);
 
         if (hci->device.lmp_name)
-            free((void *) hci->device.lmp_name);
+            qemu_free((void *) hci->device.lmp_name);
         hci->device.lmp_name = pstrdup(PARAM(change_local_name, name),
                         sizeof(PARAM(change_local_name, name)));
         bt_hci_event_complete_status(hci, HCI_SUCCESS);
@@ -2189,7 +2189,7 @@ static void bt_hci_done(struct HCIInfo *info)
     bt_device_done(&hci->device);
 
     if (hci->device.lmp_name)
-        free((void *) hci->device.lmp_name);
+        qemu_free((void *) hci->device.lmp_name);
 
     /* Be gentle and send DISCONNECT to all connected peers and those
      * currently waiting for us to accept or reject a connection request.
