41a3965359e922d0ae1847a4358a7feabfe16f7f
weil <weil@11fd3d49-aaf6-0310-8052-ea9566248e5e>
2008-10-27 19:10:34 +0000

    update copyright
    git-svn-id: svn+ssh://svn.berlios.de/svnroot/repos/ar7-firmware/qemu/trunk@1257 11fd3d49-aaf6-0310-8052-ea9566248e5e

--- hw/eepro100.c
+++ hw/eepro100.c
@@ -1,15 +1,15 @@
 /*
  * QEMU i8255x (PRO100) emulation
  *
- * Copyright (C) 2006-2007 Stefan Weil
+ * Copyright (C) 2006-2008 Stefan Weil
  *
  * Portions of the code are copies from grub / etherboot eepro100.c
  * and linux e100.c.
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
+ * the Free Software Foundation, either version 2 of the License, or
+ * (at your option) version 3 or any later version.
  *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
