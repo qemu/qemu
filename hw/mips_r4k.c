2fa0541578dd44f19446badc2c62f93ba866bf1c
weil <weil@11fd3d49-aaf6-0310-8052-ea9566248e5e>
2008-11-13 18:00:03 +0000

    fix function prototype
    git-svn-id: svn+ssh://svn.berlios.de/svnroot/repos/ar7-firmware/qemu/trunk@1270 11fd3d49-aaf6-0310-8052-ea9566248e5e

--- hw/mips_r4k.c
+++ hw/mips_r4k.c
@@ -292,8 +292,8 @@ static void mips_init(ram_addr_t ram_size, int vga_ram_size,
 }
 
 static
-void mips_r4k_init (int ram_size, int vga_ram_size, const char *boot_device,
-                    DisplayState *ds,
+void mips_r4k_init(ram_addr_t ram_size, int vga_ram_size,
+                    const char *boot_device, DisplayState *ds,
                     const char *kernel_filename, const char *kernel_cmdline,
                     const char *initrd_filename, const char *cpu_model)
 {
@@ -304,8 +304,8 @@ void mips_r4k_init (int ram_size, int vga_ram_size, const char *boot_device,
 }
 
 static
-void mipsel_r4k_init (int ram_size, int vga_ram_size, const char *boot_device,
-                    DisplayState *ds, const char **fd_filename, int snapshot,
+void mipsel_r4k_init(ram_addr_t ram_size, int vga_ram_size,
+                    const char *boot_device, DisplayState *ds,
                     const char *kernel_filename, const char *kernel_cmdline,
                     const char *initrd_filename, const char *cpu_model)
 {
