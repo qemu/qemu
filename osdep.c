48253bd84ee24182dd8a1e236094eb8649da3b54
malc <malc@c046a42c-6fe2-441c-8c8c-71466251a162>
2008-11-18 01:42:15 +0000

    Use qemu_memalign instead of memalign in qemu_vmalloc
    git-svn-id: svn://svn.savannah.nongnu.org/qemu/trunk@5730 c046a42c-6fe2-441c-8c8c-71466251a162

--- osdep.c
+++ osdep.c
@@ -200,7 +200,7 @@ void *qemu_vmalloc(size_t size)
 #ifdef _BSD
     return valloc(size);
 #else
-    return memalign(getpagesize(), size);
+    return qemu_memalign(getpagesize(), size);
 #endif
 }
 
