196e3422f7179b4a9e5ab95847e750e4576ae94f
aurel32 <aurel32@c046a42c-6fe2-441c-8c8c-71466251a162>
2008-12-15 17:14:50 +0000

    target-ppc: enable PRECISE_EMULATION by default
    With this option enabled, all glibc math tests pass.
    
    Signed-off-by: Aurelien Jarno <aurelien@aurel32.net>
    
    git-svn-id: svn://svn.savannah.nongnu.org/qemu/trunk@6054 c046a42c-6fe2-441c-8c8c-71466251a162

--- target-ppc/exec.h
+++ target-ppc/exec.h
@@ -29,7 +29,7 @@
 
 /* For normal operations, precise emulation should not be needed */
 //#define USE_PRECISE_EMULATION 1
-#define USE_PRECISE_EMULATION 0
+#define USE_PRECISE_EMULATION 1
 
 register struct CPUPPCState *env asm(AREG0);
 #define TDX "%016" PRIx64
