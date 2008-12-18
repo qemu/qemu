a9c11522bb4daabc43b3fe12d08c8db6deb2ac79
aurel32 <aurel32@c046a42c-6fe2-441c-8c8c-71466251a162>
2008-12-18 22:42:51 +0000

    KVM: Silence unused s warning
    Move s under #ifdef to avoid compiler warning.
    
    Signed-off-by: Jan Kiszka <jan.kiszka@siemens.com>
    Signed-off-by: Aurelien Jarno <aurelien@aurel32.net>
    
    git-svn-id: svn://svn.savannah.nongnu.org/qemu/trunk@6086 c046a42c-6fe2-441c-8c8c-71466251a162

--- kvm-all.c
+++ kvm-all.c
@@ -654,9 +654,9 @@ int kvm_vcpu_ioctl(CPUState *env, int type, ...)
 
 int kvm_has_sync_mmu(void)
 {
+#ifdef KVM_CAP_SYNC_MMU
     KVMState *s = kvm_state;
 
-#ifdef KVM_CAP_SYNC_MMU
     if (kvm_ioctl(s, KVM_CHECK_EXTENSION, KVM_CAP_SYNC_MMU) > 0)
         return 1;
 #endif
