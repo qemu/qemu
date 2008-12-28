60cbfb95522b33c3ec1dd4fa32da261c6c3d6a9d
blueswir1 <blueswir1@c046a42c-6fe2-441c-8c8c-71466251a162>
2008-12-28 13:14:48 +0000

    Fix -Werror=format-security warning (Frederik Himpe/Lennert Buytenhek)
    git-svn-id: svn://svn.savannah.nongnu.org/qemu/trunk@6134 c046a42c-6fe2-441c-8c8c-71466251a162

--- monitor.c
+++ monitor.c
@@ -1936,10 +1936,9 @@ static const MonitorDef monitor_defs[] = {
     { NULL },
 };
 
-static void expr_error(const char *fmt)
+static void expr_error(const char *msg)
 {
-    term_printf(fmt);
-    term_printf("\n");
+    term_printf("%s\n", msg);
     longjmp(expr_env, 1);
 }
 
