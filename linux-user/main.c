2e77eac62db82164e4c6200c928a315cd86bf7c4
blueswir1 <blueswir1@c046a42c-6fe2-441c-8c8c-71466251a162>
2009-01-20 16:57:34 +0000

    Fix user emulator breakage, based on patch by Riku Voipio
    git-svn-id: svn://svn.savannah.nongnu.org/qemu/trunk@6371 c046a42c-6fe2-441c-8c8c-71466251a162

--- linux-user/main.c
+++ linux-user/main.c
@@ -2394,19 +2394,21 @@ int main(int argc, char **argv, char **envp)
 
     free(target_environ);
 
-    log_page_dump();
+    if (qemu_log_enabled()) {
+        log_page_dump();
 
-    qemu_log("start_brk   0x" TARGET_ABI_FMT_lx "\n", info->start_brk);
-    qemu_log("end_code    0x" TARGET_ABI_FMT_lx "\n", info->end_code);
-    qemu_log("start_code  0x" TARGET_ABI_FMT_lx "\n",
-            info->start_code);
-    qemu_log("start_data  0x" TARGET_ABI_FMT_lx "\n",
-            info->start_data);
-    qemu_log("end_data    0x" TARGET_ABI_FMT_lx "\n", info->end_data);
-    qemu_log("start_stack 0x" TARGET_ABI_FMT_lx "\n",
-            info->start_stack);
-    qemu_log("brk         0x" TARGET_ABI_FMT_lx "\n", info->brk);
-    qemu_log("entry       0x" TARGET_ABI_FMT_lx "\n", info->entry);
+        qemu_log("start_brk   0x" TARGET_ABI_FMT_lx "\n", info->start_brk);
+        qemu_log("end_code    0x" TARGET_ABI_FMT_lx "\n", info->end_code);
+        qemu_log("start_code  0x" TARGET_ABI_FMT_lx "\n",
+                 info->start_code);
+        qemu_log("start_data  0x" TARGET_ABI_FMT_lx "\n",
+                 info->start_data);
+        qemu_log("end_data    0x" TARGET_ABI_FMT_lx "\n", info->end_data);
+        qemu_log("start_stack 0x" TARGET_ABI_FMT_lx "\n",
+                 info->start_stack);
+        qemu_log("brk         0x" TARGET_ABI_FMT_lx "\n", info->brk);
+        qemu_log("entry       0x" TARGET_ABI_FMT_lx "\n", info->entry);
+    }
 
     target_set_brk(info->brk);
     syscall_init();
