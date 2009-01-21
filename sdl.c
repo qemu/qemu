413bfb7cfc4e7f886ad35d610056ad8da8deab8a
malc <malc@c046a42c-6fe2-441c-8c8c-71466251a162>
2009-01-21 00:34:46 +0000

    Remove real_screen->pixels checks
    git-svn-id: svn://svn.savannah.nongnu.org/qemu/trunk@6372 c046a42c-6fe2-441c-8c8c-71466251a162

--- sdl.c
+++ sdl.c
@@ -93,18 +93,8 @@ static void sdl_resize(DisplayState *ds)
     if (gui_noframe)
         flags |= SDL_NOFRAME;
 
- again:
     real_screen = SDL_SetVideoMode(ds_get_width(ds), ds_get_height(ds), 0, flags);
     if (!real_screen) {
-        fprintf(stderr, "Could not open SDL display\n");
-        exit(1);
-    }
-    if (!real_screen->pixels && (flags & SDL_HWSURFACE) && (flags & SDL_FULLSCREEN)) {
-        flags &= ~SDL_HWSURFACE;
-        goto again;
-    }
-
-    if (!real_screen->pixels) {
         fprintf(stderr, "Could not open SDL display\n");
         exit(1);
     }
