206ab6e090eeddce71372041454d50d93a63017d
Jan Kiszka <jan.kiszka@web.de>
2009-04-28 21:25:58 +0200

    net: Avoid gcc'ism in net_host_device_add
    >> +    if (net_client_init(device, opts ? : "") < 0) {
    >
    > Is this a gcc extension?  Do we want to introduce this construct to the
    > code base.
    
    Valid remark, fix below.
    
    Thanks,
    Jan
    
    -------->
    
    Signed-off-by: Jan Kiszka <jan.kiszka@siemens.com>
    Signed-off-by: Anthony Liguori <aliguori@us.ibm.com>

--- net.c
+++ net.c
@@ -2085,7 +2085,7 @@ void net_host_device_add(Monitor *mon, const char *device, const char *opts)
         monitor_printf(mon, "invalid host network device %s\n", device);
         return;
     }
-    if (net_client_init(device, opts ? : "") < 0) {
+    if (net_client_init(device, opts ? opts : "") < 0) {
         monitor_printf(mon, "adding host network device %s failed\n", device);
     }
 }
