3a8bae3e057a775070b5da887ce506a5f9ec6139
malc <av1474@comtv.ru>
2009-05-20 03:25:22 +0400

    Remove dead code
    Signed-off-by: malc <av1474@comtv.ru>

--- hw/pc.c
+++ hw/pc.c
@@ -778,21 +778,14 @@ static int parallel_irq[MAX_PARALLEL_PORTS] = { 7, 7, 7 };
 static void audio_init (PCIBus *pci_bus, qemu_irq *pic)
 {
     struct soundhw *c;
-    int audio_enabled = 0;
 
-    for (c = soundhw; !audio_enabled && c->name; ++c) {
-        audio_enabled = c->enabled;
-    }
-
-    if (audio_enabled) {
-        for (c = soundhw; c->name; ++c) {
-            if (c->enabled) {
-                if (c->isa) {
-                    c->init.init_isa(pic);
-                } else {
-                    if (pci_bus) {
-                        c->init.init_pci(pci_bus);
-                    }
+    for (c = soundhw; c->name; ++c) {
+        if (c->enabled) {
+            if (c->isa) {
+                c->init.init_isa(pic);
+            } else {
+                if (pci_bus) {
+                    c->init.init_pci(pci_bus);
                 }
             }
         }
