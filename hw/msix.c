72755a709619b1db204c675df8f1093d54e38128
Michael S. Tsirkin <mst@redhat.com>
2009-07-05 15:58:52 +0300

    qemu/msi: missing braces
    MSIX present bit is tested incorrectly, and only happens to work because
    the bit we are testing is 0x1.  Add braces to fix this.
    
    Reported-by: Blue Swirl <blauwirbel@gmail.com>
    Signed-off-by: Michael S. Tsirkin <mst@redhat.com>
    Signed-off-by: Anthony Liguori <aliguori@us.ibm.com>

--- hw/msix.c
+++ hw/msix.c
@@ -286,8 +286,9 @@ void msix_save(PCIDevice *dev, QEMUFile *f)
 {
     unsigned n = dev->msix_entries_nr;
 
-    if (!dev->cap_present & QEMU_PCI_CAP_MSIX)
+    if (!(dev->cap_present & QEMU_PCI_CAP_MSIX)) {
         return;
+    }
 
     qemu_put_buffer(f, dev->msix_table_page, n * MSIX_ENTRY_SIZE);
     qemu_put_buffer(f, dev->msix_table_page + MSIX_PAGE_PENDING, (n + 7) / 8);
