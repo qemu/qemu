113c066d2ef8fbdf6c100286e7e5fd797624afde
Stefan Weil <weil@mail.berlios.de>
2009-07-10 21:17:10 +0200

    block/vdi.c: Removed bad workaround
    Signed-off-by: Stefan Weil <weil@mail.berlios.de>

--- block/vdi.c
+++ block/vdi.c
@@ -303,16 +303,6 @@ static int vdi_open(BlockDriverState *bs, const char *filename, int flags)
 
     logout("\n");
 
-    /* Performance is terrible right now with cache=writethrough due mainly
-     * to reference count updates.  If the user does not explicitly specify
-     * a caching type, force to writeback caching.
-     * TODO: This is a copy from qcow2.c. Remove it when it is no longer needed.
-     */
-    if ((flags & BDRV_O_CACHE_DEF)) {
-        flags |= BDRV_O_CACHE_WB;
-        flags &= ~BDRV_O_CACHE_DEF;
-    }
-
     ret = bdrv_file_open(&s->hd, filename, flags);
     if (ret < 0) {
         return ret;
