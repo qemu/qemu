e6a0575e98179167bc5f7ab792c3d19f9852ee5f
Anthony Liguori <aliguori@us.ibm.com>
2009-07-10 13:39:34 -0500

    Make sure to mark MCE defines as ULL
    Fixes build on 32-bit
    
    Signed-off-by: Anthony Liguori <aliguori@us.ibm.com>

--- target-i386/cpu.h
+++ target-i386/cpu.h
@@ -256,11 +256,11 @@
 #define MCE_CAP_DEF	MCG_CTL_P
 #define MCE_BANKS_DEF	10
 
-#define MCG_STATUS_MCIP	(1UL<<2)   /* machine check in progress */
+#define MCG_STATUS_MCIP	(1ULL<<2)   /* machine check in progress */
 
-#define MCI_STATUS_VAL	(1UL<<63)  /* valid error */
-#define MCI_STATUS_OVER	(1UL<<62)  /* previous errors lost */
-#define MCI_STATUS_UC	(1UL<<61)  /* uncorrected error */
+#define MCI_STATUS_VAL	(1ULL<<63)  /* valid error */
+#define MCI_STATUS_OVER	(1ULL<<62)  /* previous errors lost */
+#define MCI_STATUS_UC	(1ULL<<61)  /* uncorrected error */
 
 #define MSR_IA32_TSC                    0x10
 #define MSR_IA32_APICBASE               0x1b
