7d93b1fa188a7e32bd5e53fce819d39cf6d90fcc
Beth Kon <eak@us.ibm.com>
2009-07-13 19:43:13 -0400

    Reset HPET config register on hpet_reset
    Without this, after system reset, hpet does not detect transition from
    non-legacy to legacy mode.
    
    Signed-off-by: Beth Kon <eak@us.ibm.com>
    Signed-off-by: Anthony Liguori <aliguori@us.ibm.com>

--- hw/hpet.c
+++ hw/hpet.c
@@ -553,6 +553,7 @@ static void hpet_reset(void *opaque) {
     /* 64-bit main counter; 3 timers supported; LegacyReplacementRoute. */
     s->capability = 0x8086a201ULL;
     s->capability |= ((HPET_CLK_PERIOD) << 32);
+    s->config = 0ULL;
     if (count > 0)
         /* we don't enable pit when hpet_reset is first called (by hpet_init)
          * because hpet is taking over for pit here. On subsequent invocations,
