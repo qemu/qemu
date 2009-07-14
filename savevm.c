0bb05eaff04d30609a98c0dae80bb5dba3e4e799
Glauber Costa <glommer@redhat.com>
2009-07-14 18:26:51 -0400

    fix segfault in setting migration speed
    Hi,
    
    Whoever wrote this migrate_set_speed function is totally stupid.
    
    Any failed or completed migration keeps its state to allow probing of
    migration data, but has no associated file anymore. It is, thus,
    possible to crash qemu by calling migrate_set_speed after a migration
    is finished (or failed, or cancelled), but before another one starts.
    
    This patch fixes it.
    
    Signed-off-by: Glauber Costa <glommer@redhat.com>
    Signed-off-by: Anthony Liguori <aliguori@us.ibm.com>

--- savevm.c
+++ savevm.c
@@ -556,7 +556,9 @@ int qemu_file_rate_limit(QEMUFile *f)
 
 size_t qemu_file_set_rate_limit(QEMUFile *f, size_t new_rate)
 {
-    if (f->set_rate_limit)
+    /* any failed or completed migration keeps its state to allow probing of
+     * migration data, but has no associated file anymore */
+    if (f && f->set_rate_limit)
         return f->set_rate_limit(f->opaque, new_rate);
 
     return 0;
