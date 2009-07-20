a7824a886ed50eb4fe3c6fcd6afd8814a6973583
Kevin Wolf <kwolf@redhat.com>
2009-07-20 16:48:43 +0200

    qemu-io: Rework alloc command
    The alloc command in qemu-io is mostly useless currently. Instead of doing a
    single call to bdrv_is_allocated, we must call bdrv_is_allocated in a loop
    until we have found out for each requested sector if it is allocated or not
    (bdrv_is_allocated returns a number of sectors that are known to be in the same
    state as the first one, but it is not required to include all of them)
    
    This changes the output format of the alloc command so that a change to the
    expected output of qemu-iotests 019 is necessary once this is included.
    
    Signed-off-by: Kevin Wolf <kwolf@redhat.com>
    Signed-off-by: Anthony Liguori <aliguori@us.ibm.com>

--- qemu-io.c
+++ qemu-io.c
@@ -1170,11 +1170,10 @@ static int
 alloc_f(int argc, char **argv)
 {
 	int64_t offset;
-	int nb_sectors;
+	int nb_sectors, remaining;
 	char s1[64];
-	int num;
+	int num, sum_alloc;
 	int ret;
-	const char *retstr;
 
 	offset = cvtnum(argv[1]);
 	if (offset & 0x1ff) {
@@ -1188,16 +1187,23 @@ alloc_f(int argc, char **argv)
 	else
 		nb_sectors = 1;
 
-	ret = bdrv_is_allocated(bs, offset >> 9, nb_sectors, &num);
+	remaining = nb_sectors;
+	sum_alloc = 0;
+	while (remaining) {
+		ret = bdrv_is_allocated(bs, offset >> 9, nb_sectors, &num);
+		remaining -= num;
+		if (ret) {
+			sum_alloc += num;
+		}
+	}
 
 	cvtstr(offset, s1, sizeof(s1));
 
-	retstr = ret ? "allocated" : "not allocated";
 	if (nb_sectors == 1)
-		printf("sector %s at offset %s\n", retstr, s1);
+		printf("sector allocated at offset %s\n", s1);
 	else
-		printf("%d/%d sectors %s at offset %s\n",
-			num, nb_sectors, retstr, s1);
+		printf("%d/%d sectors allocated at offset %s\n",
+			sum_alloc, nb_sectors, s1);
 	return 0;
 }
 
