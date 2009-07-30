52c18be9e99dabe295321153fda7fce9f76647ac
Zachary Amsden <zamsden@redhat.com>
2009-07-30 00:15:01 -1000

    When using stdio monitor and VNC display, one can set or clear a VNC password; this should set or turn off VNC authentication as well.
    Signed-off-by: Zachary Amsden <zamsden@redhat.com>
    Signed-off-by: Anthony Liguori <aliguori@us.ibm.com>

--- vnc.c
+++ vnc.c
@@ -2274,6 +2274,11 @@ int vnc_display_password(DisplayState *ds, const char *password)
     if (password && password[0]) {
         if (!(vs->password = qemu_strdup(password)))
             return -1;
+        if (vs->auth == VNC_AUTH_NONE) {
+            vs->auth = VNC_AUTH_VNC;
+        }
+    } else {
+        vs->auth = VNC_AUTH_NONE;
     }
 
     return 0;
