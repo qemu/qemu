b8630e45f0ff32f90f9ab637cd2ff7d687f5a3af
Luiz Capitulino <lcapitulino@redhat.com>
2009-08-03 13:56:59 -0300

    net: Fix do_set_link() return type
    do_set_link() returns int, but Monitor handler functions should
    always return void.
    
    Signed-off-by: Luiz Capitulino <lcapitulino@redhat.com>
    Signed-off-by: Anthony Liguori <aliguori@us.ibm.com>
    Message-Id:

--- net.c
+++ net.c
@@ -2934,7 +2934,7 @@ void do_info_network(Monitor *mon)
     }
 }
 
-int do_set_link(Monitor *mon, const char *name, const char *up_or_down)
+void do_set_link(Monitor *mon, const char *name, const char *up_or_down)
 {
     VLANState *vlan;
     VLANClientState *vc = NULL;
@@ -2947,7 +2947,7 @@ done:
 
     if (!vc) {
         monitor_printf(mon, "could not find network device '%s'", name);
-        return 0;
+        return;
     }
 
     if (strcmp(up_or_down, "up") == 0)
@@ -2960,8 +2960,6 @@ done:
 
     if (vc->link_status_changed)
         vc->link_status_changed(vc);
-
-    return 1;
 }
 
 void net_cleanup(void)
