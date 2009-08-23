9f2f0bc6bc5189bae5d038819b6cd52fe57ce83d
Blue Swirl <blauwirbel@gmail.com>
2009-08-23 20:10:28 +0000

    Fix device name completion for 'eject'
    Signed-off-by: Blue Swirl <blauwirbel@gmail.com>
    Signed-off-by: Anthony Liguori <aliguori@us.ibm.com>

--- monitor.c
+++ monitor.c
@@ -3059,6 +3059,9 @@ static void monitor_find_completion(const char *cmdline)
             }
         }
         str = args[nb_args - 1];
+        if (*ptype == '-' && ptype[1] != '\0') {
+            ptype += 2;
+        }
         switch(*ptype) {
         case 'F':
             /* file completion */
