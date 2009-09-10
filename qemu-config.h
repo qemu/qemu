9d868d4517be12226991b2cc55d11bc0f83ea7b2
Gerd Hoffmann <kraxel@redhat.com>
2009-09-10 10:58:34 +0200

    qemu-option.h include protectors
    qemu-option.h has no protection against including it twice.
    This patch adds the usual "#ifndef header" bits.
    
    Signed-off-by: Gerd Hoffmann <kraxel@redhat.com>
    Signed-off-by: Anthony Liguori <aliguori@us.ibm.com>

--- qemu-config.h
+++ qemu-config.h
@@ -1,4 +1,9 @@
+#ifndef QEMU_CONFIG_H
+#define QEMU_CONFIG_H
+
 extern QemuOptsList qemu_drive_opts;
 extern QemuOptsList qemu_device_opts;
 
 int qemu_set_option(const char *str);
+
+#endif /* QEMU_CONFIG_H */
