47faadc676b9ade4259119e4e3f1b5ac3e28e090
Blue Swirl <blauwirbel@gmail.com>
2009-09-12 06:19:14 +0000

    Unbreak BSD: use qemu_fdatasync instead of fdatasync
    Signed-off-by: Blue Swirl <blauwirbel@gmail.com>

--- posix-aio-compat.c
+++ posix-aio-compat.c
@@ -138,7 +138,7 @@ static size_t handle_aiocb_flush(struct qemu_paiocb *aiocb)
 {
     int ret;
 
-    ret = fdatasync(aiocb->aio_fildes);
+    ret = qemu_fdatasync(aiocb->aio_fildes);
     if (ret == -1)
         return -errno;
     return 0;
