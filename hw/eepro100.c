3262e5488d9376cf89b13199dfebe8ede5d55a31
=?UTF-8?q?Reimar=20D=C3=B6ffinger?= <Reimar.Doeffinger@gmx.de>
2009-09-12 15:42:01 +0200

    Set revision in eeprom correctly for 82557 versions.
    This is necessary to make FreeBSD recognize the device as 82557 - otherwise its
    driver will use unsupported features and fail to work.
    
    Signed-off-by: Reimar Döffinger <Reimar.Doeffinger@gmx.de>
    Signed-off-by: Anthony Liguori <aliguori@us.ibm.com>

--- hw/eepro100.c
+++ hw/eepro100.c
@@ -726,6 +726,8 @@ static void nic_selective_reset(EEPRO100State * s)
     }
     /* TODO: source of next statement? */
     eeprom_contents[0xa] = 0x4000;
+    if (s->device == i82557B || s->device == i82557C)
+        eeprom_contents[5] = 0x0100;
     uint16_t sum = 0;
     for (i = 0; i < EEPROM_SIZE - 1; i++) {
         sum += eeprom_contents[i];
