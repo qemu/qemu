41cbc23c5ca37a8b841915d7d252a02106d58b1e
Stefan Weil <weil@mail.berlios.de>
2009-09-19 12:29:59 +0200

    eepro100: Replace sprintf by snprintf
    Signed-off-by: Stefan Weil <weil@mail.berlios.de>
    Signed-off-by: Aurelien Jarno <aurelien@aurel32.net>

--- hw/eepro100.c
+++ hw/eepro100.c
@@ -537,12 +537,12 @@ static char *regname(uint32_t addr)
     if (addr < PCI_IO_SIZE) {
         const char *r = reg[addr / 4];
         if (r != 0) {
-            sprintf(buf, "%s+%u", r, addr % 4);
+            snprintf(buf, sizeof(buf), "%s+%u", r, addr % 4);
         } else {
-            sprintf(buf, "0x%02x", addr);
+            snprintf(buf, sizeof(buf), "0x%02x", addr);
         }
     } else {
-        sprintf(buf, "??? 0x%08x", addr);
+        snprintf(buf, sizeof(buf), "??? 0x%08x", addr);
     }
     return buf;
 }
