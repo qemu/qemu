b689c622a69841abd91cb47b9c6deb0f735d2a3e
Aurelien Jarno <aurelien@aurel32.net>
2009-09-22 23:26:21 +0200

    exec-all.h: increase MAX_OP_PER_INSTR to 96 from 64
    The x86_64 ror instruction on a 32-bit host can generate up to 77 TCG
    ops. Some more space should be left for opc that are added at the end
    of the translation.
    
    Thanks to Laurent Desnogues for the debugging help.
    
    Signed-off-by: Aurelien Jarno <aurelien@aurel32.net>

--- exec-all.h
+++ exec-all.h
@@ -34,7 +34,7 @@
 typedef struct TranslationBlock TranslationBlock;
 
 /* XXX: make safe guess about sizes */
-#define MAX_OP_PER_INSTR 64
+#define MAX_OP_PER_INSTR 96
 /* A Call op needs up to 6 + 2N parameters (N = number of arguments).  */
 #define MAX_OPC_PARAM 10
 #define OPC_BUF_SIZE 512
