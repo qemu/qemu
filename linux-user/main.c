48a74d5513ca4c87bcecae02de4926aa89a927fe
Stefan Weil <weil@mail.berlios.de>
2009-09-25 22:53:18 +0200

    linux-user: Add command line option to clear environment
    Command line option -Uall resets the environment.
    This is useful because it allows a well defined environment.
    
    Signed-off-by: Stefan Weil <weil@mail.berlios.de>

--- linux-user/main.c
+++ linux-user/main.c
@@ -2501,6 +2501,12 @@ int main(int argc, char **argv, char **envp)
             r = argv[optind++];
             if (envlist_setenv(envlist, r) != 0)
                 usage();
+        } else if (!strcmp(r, "Uall")) {
+            envlist_free(envlist);
+            if ((envlist = envlist_create()) == NULL) {
+                (void) fprintf(stderr, "Unable to allocate envlist\n");
+                exit(1);
+            }
         } else if (!strcmp(r, "U")) {
             r = argv[optind++];
             if (envlist_unsetenv(envlist, r) != 0)
