5c45c53c5244290e6d2204df657992ef93e6f515
Juan Quintela <quintela@redhat.com>
2009-09-29 22:48:52 +0200

    x86: add fptag_vmstate to the state
    It is needed to store fptags
    
    Signed-off-by: Juan Quintela <quintela@redhat.com>
    Signed-off-by: Anthony Liguori <aliguori@us.ibm.com>

--- target-i386/cpu.h
+++ target-i386/cpu.h
@@ -595,6 +595,7 @@ typedef struct CPUX86State {
     unsigned int fpstt; /* top of stack index */
     uint16_t fpus;
     uint16_t fpus_vmstate;
+    uint16_t fptag_vmstate;
     uint16_t fpuc;
     uint8_t fptags[8];   /* 0 = valid, 1 = empty */
     union {
