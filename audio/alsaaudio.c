de2ca4fbb4cc1fdb31a9a1136b9b6bf9d045dd41
malc <av1474@comtv.ru>
2009-10-02 03:19:30 +0400

    alsa: Change default buffer/period size
    Increase buffer size but do not rely on ALSA picking up default period
    size.
    
    Signed-off-by: malc <av1474@comtv.ru>

--- audio/alsaaudio.c
+++ audio/alsaaudio.c
@@ -74,7 +74,8 @@ static struct {
     int period_size_out_overridden;
     int verbose;
 } conf = {
-    .buffer_size_out = 1024,
+    .buffer_size_out = 4096,
+    .period_size_out = 1024,
     .pcm_name_out = "default",
     .pcm_name_in = "default",
 };
