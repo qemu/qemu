5f769e5497f7f61deeacf9c702a263396fa04582
Stefan Weil <weil@mail.berlios.de>
2009-10-02 23:38:31 +0200

    tcg: Fix initial value for TCGv variables
    Normally, TCGv is a simple int, but with --enable-debug-tcg,
    it is a structure. So a simple assignment to 0 is invalid.
    
    Signed-off-by: Stefan Weil <weil@mail.berlios.de>

--- target-ppc/translate.c
+++ target-ppc/translate.c
@@ -762,7 +762,7 @@ static inline void gen_op_arith_add(DisasContext *ctx, TCGv ret, TCGv arg1,
                                     TCGv arg2, int add_ca, int compute_ca,
                                     int compute_ov)
 {
-    TCGv t0, t1 = 0;
+    TCGv t0, t1;
 
     if ((!compute_ca && !compute_ov) ||
         (!TCGV_EQUAL(ret,arg1) && !TCGV_EQUAL(ret, arg2)))  {
@@ -1183,7 +1183,7 @@ static inline void gen_op_arith_subf(DisasContext *ctx, TCGv ret, TCGv arg1,
                                      TCGv arg2, int add_ca, int compute_ca,
                                      int compute_ov)
 {
-    TCGv t0, t1 = 0;
+    TCGv t0, t1;
 
     if ((!compute_ca && !compute_ov) ||
         (!TCGV_EQUAL(ret, arg1) && !TCGV_EQUAL(ret, arg2)))  {
@@ -3399,7 +3399,7 @@ static inline void gen_bcond(DisasContext *ctx, int type)
 {
     uint32_t bo = BO(ctx->opcode);
     int l1 = gen_new_label();
-    TCGv target = 0;
+    TCGv target;
 
     ctx->exception = POWERPC_EXCP_BRANCH;
     if (type == BCOND_LR || type == BCOND_CTR) {
