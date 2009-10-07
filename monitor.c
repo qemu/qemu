a2876f595bb9661806f577cc3c0f9789975b18df
Luiz Capitulino <lcapitulino@redhat.com>
2009-10-07 13:41:53 -0300

    monitor: Add user_print() to mon_cmd_t
    This new struct member will store a pointer to a function that
    should be used to output data in the user protocol format.
    
    It will also serve as a flag to say if a given handler has already
    been converted to the new QObject style.
    
    Patchworks-ID: 35339
    Signed-off-by: Luiz Capitulino <lcapitulino@redhat.com>
    Signed-off-by: Anthony Liguori <aliguori@us.ibm.com>

--- monitor.c
+++ monitor.c
@@ -73,6 +73,7 @@ typedef struct mon_cmd_t {
     const char *args_type;
     const char *params;
     const char *help;
+    void (*user_print)(Monitor *mon, const QObject *data);
     union {
         void (*info)(Monitor *mon);
         void (*cmd)(Monitor *mon, const QDict *qdict);
