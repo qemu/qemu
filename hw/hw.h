c29d8b1e9b927606b83cd7c56ddb350c9a9fbd5f
Juan Quintela <quintela@redhat.com>
2009-10-16 12:35:27 +0200

    vmstate: factor vmstate_offset_pointer
    Signed-off-by: Juan Quintela <quintela@redhat.com>
    Signed-off-by: Anthony Liguori <aliguori@us.ibm.com>

--- hw/hw.h
+++ hw/hw.h
@@ -342,6 +342,10 @@ extern const VMStateInfo vmstate_info_buffer;
     (offsetof(_state, _field) +                                      \
      type_check(_type, typeof_field(_state, _field)))
 
+#define vmstate_offset_pointer(_state, _field, _type)                \
+    (offsetof(_state, _field) +                                      \
+     type_check_pointer(_type, typeof_field(_state, _field)))
+
 #define VMSTATE_SINGLE(_field, _state, _version, _info, _type) {     \
     .name       = (stringify(_field)),                               \
     .version_id = (_version),                                        \
@@ -398,8 +402,7 @@ extern const VMStateInfo vmstate_info_buffer;
     .info       = &(_info),                                          \
     .size       = sizeof(_type),                                     \
     .flags      = VMS_VARRAY_INT32|VMS_POINTER,                      \
-    .offset     = offsetof(_state, _field)                           \
-        + type_check_pointer(_type,typeof_field(_state, _field))     \
+    .offset     = vmstate_offset_pointer(_state, _field, _type),     \
 }
 
 #define VMSTATE_STRUCT(_field, _state, _version, _vmsd, _type) {     \
