976b2037e5eaa7dc18ca7062c9276ec03aaa37a9
Blue Swirl <blauwirbel@gmail.com>
2009-10-18 14:51:10 +0000

    x86: add 'static' to please Sparse
    Signed-off-by: Blue Swirl <blauwirbel@gmail.com>

--- target-i386/machine.c
+++ target-i386/machine.c
@@ -116,7 +116,7 @@ static void put_fpreg(QEMUFile *f, void *opaque, size_t size)
     qemu_put_be16s(f, &exp);
 }
 
-const VMStateInfo vmstate_fpreg = {
+static const VMStateInfo vmstate_fpreg = {
     .name = "fpreg",
     .get  = get_fpreg,
     .put  = put_fpreg,
@@ -133,7 +133,7 @@ static int get_fpreg_1_mmx(QEMUFile *f, void *opaque, size_t size)
     return 0;
 }
 
-const VMStateInfo vmstate_fpreg_1_mmx = {
+static const VMStateInfo vmstate_fpreg_1_mmx = {
     .name = "fpreg_1_mmx",
     .get  = get_fpreg_1_mmx,
     .put  = put_fpreg_error,
@@ -149,7 +149,7 @@ static int get_fpreg_1_no_mmx(QEMUFile *f, void *opaque, size_t size)
     return 0;
 }
 
-const VMStateInfo vmstate_fpreg_1_no_mmx = {
+static const VMStateInfo vmstate_fpreg_1_no_mmx = {
     .name = "fpreg_1_no_mmx",
     .get  = get_fpreg_1_no_mmx,
     .put  = put_fpreg_error,
@@ -307,7 +307,7 @@ static void put_uint64_as_uint32(QEMUFile *f, void *pv, size_t size)
     qemu_put_be32(f, *v);
 }
 
-const VMStateInfo vmstate_hack_uint64_as_uint32 = {
+static const VMStateInfo vmstate_hack_uint64_as_uint32 = {
     .name = "uint64_as_uint32",
     .get  = get_uint64_as_uint32,
     .put  = put_uint64_as_uint32,
@@ -387,7 +387,7 @@ static int cpu_post_load(void *opaque, int version_id)
     return 0;
 }
 
-const VMStateDescription vmstate_cpu = {
+static const VMStateDescription vmstate_cpu = {
     .name = "cpu",
     .version_id = CPU_SAVE_VERSION,
     .minimum_version_id = 3,
