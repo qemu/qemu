792773b2255d25c6f5fe9dfa0ae200debab92de4
Jan Kiszka <jan.kiszka@web.de>
2009-11-30 20:34:55 +0100

    block migration: Skip zero-sized disks
    No need to migrate emptiness (risking divide by zero later on).
    
    Signed-off-by: Jan Kiszka <jan.kiszka@siemens.com>
    Signed-off-by: Anthony Liguori <aliguori@us.ibm.com>

--- block-migration.c
+++ block-migration.c
@@ -222,6 +222,7 @@ static void init_blk_migration(Monitor *mon, QEMUFile *f)
 {
     BlkMigDevState *bmds;
     BlockDriverState *bs;
+    int64_t sectors;
 
     block_mig_state.submitted = 0;
     block_mig_state.read_done = 0;
@@ -231,14 +232,19 @@ static void init_blk_migration(Monitor *mon, QEMUFile *f)
 
     for (bs = bdrv_first; bs != NULL; bs = bs->next) {
         if (bs->type == BDRV_TYPE_HD) {
+            sectors = bdrv_getlength(bs) >> BDRV_SECTOR_BITS;
+            if (sectors == 0) {
+                continue;
+            }
+
             bmds = qemu_mallocz(sizeof(BlkMigDevState));
             bmds->bs = bs;
             bmds->bulk_completed = 0;
-            bmds->total_sectors = bdrv_getlength(bs) >> BDRV_SECTOR_BITS;
+            bmds->total_sectors = sectors;
             bmds->completed_sectors = 0;
             bmds->shared_base = block_mig_state.shared_base;
 
-            block_mig_state.total_sector_sum += bmds->total_sectors;
+            block_mig_state.total_sector_sum += sectors;
 
             if (bmds->shared_base) {
                 monitor_printf(mon, "Start migration for %s with shared base "
