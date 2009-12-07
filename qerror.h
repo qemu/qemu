c7c338c49712eceef0ef584249856928de30cb45
Markus Armbruster <armbru@redhat.com>
2009-12-07 21:37:10 +0100

    QError: New QERR_FD_NOT_FOUND
    Signed-off-by: Markus Armbruster <armbru@redhat.com>
    Signed-off-by: Anthony Liguori <aliguori@us.ibm.com>

--- qerror.h
+++ qerror.h
@@ -56,6 +56,9 @@ QError *qobject_to_qerror(const QObject *obj);
 #define QERR_DEVICE_NOT_REMOVABLE \
     "{ 'class': 'DeviceNotRemovable', 'data': { 'device': %s } }"
 
+#define QERR_FD_NOT_FOUND \
+    "{ 'class': 'fd_not_found', 'data': { 'name': %s } }"
+
 #define QERR_INVALID_BLOCK_FORMAT \
     "{ 'class': 'InvalidBlockFormat', 'data': { 'name': %s } }"
 
