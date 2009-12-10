b81ebc69900638e199e4cd1f4f00ba60917ff685
Michael S. Tsirkin <mst@redhat.com>
2009-12-10 19:28:53 +0200

    pci: remove unused macro
    PCI_STATUS_DEVSEL is unused, and it also
    has a different name in pci_regs.h
    Remove.
    
    Signed-off-by: Michael S. Tsirkin <mst@redhat.com>
    Acked-by: Juan Quintela <quintela@redhat.com>
    Acked-by: Glauber Costa <glommer@gmail.com>

--- hw/pci.h
+++ hw/pci.h
@@ -167,7 +167,6 @@ typedef struct PCIIORegion {
 #define PCI_STATUS_FAST_BACK	0x080
 #define PCI_STATUS_DEVSEL_MEDIUM 0x200
 #define PCI_STATUS_DEVSEL_SLOW	0x400
-#define PCI_STATUS_DEVSEL	0x600
 #define  PCI_STATUS_SIG_TARGET_ABORT	0x800 /* Set on target abort */
 #define  PCI_STATUS_REC_MASTER_ABORT	0x2000 /* Set on master abort */
 
