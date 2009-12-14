d587e0787153f0224a6140c5015609963ceaabfb
Anthony Liguori <aliguori@us.ibm.com>
2009-12-14 11:36:53 -0600

    Revert "pci: interrupt disable bit support"
    This reverts commit 0ea5709a32085f7d14901a09d12bd35f9b267607.
    
    Per discussion with Michael Tsirkin, this is too risky for 0.12
    
    Signed-off-by: Anthony Liguori <aliguori@us.ibm.com>

--- hw/pci.h
+++ hw/pci.h
@@ -101,7 +101,6 @@ typedef struct PCIIORegion {
 #define  PCI_COMMAND_IO		0x1	/* Enable response in I/O space */
 #define  PCI_COMMAND_MEMORY	0x2	/* Enable response in Memory space */
 #define  PCI_COMMAND_MASTER	0x4	/* Enable bus master */
-#define  PCI_COMMAND_INTX_DISABLE 0x400 /* INTx Emulation Disable */
 #define PCI_STATUS              0x06    /* 16 bits */
 #define  PCI_STATUS_INTERRUPT   0x08
 #define PCI_REVISION_ID         0x08    /* 8 bits  */
