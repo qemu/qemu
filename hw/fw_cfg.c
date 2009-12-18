e6ea832410be570d8b6f9ba9285c9fec2174de3a
Gerd Hoffmann <kraxel@redhat.com>
2009-12-18 12:01:10 +0100

    fw_cfg: add API for file transfer.
    This patch adds a file transfer interface to fw_cfg.  Intended to be
    used for passing non-pci option roms and vgabios to seabios.  Namespace
    is modeled after the existing cbfs filesystem support in seabios.
    
    Reading the new FW_CFG_FILE_DIR entry returns a file list.
    Fields there are in network byte order (aka bigendian).
    
    aliguori: fix fw_cfg.h for multiboot.bin, add proper fw_cfg.h declarations,
              quiet fprintf() in fw_cfg.c
    
    Signed-off-by: Gerd Hoffmann <kraxel@redhat.com>
    Signed-off-by: Anthony Liguori <aliguori@us.ibm.com>
    (cherry picked from commit abe147e0ce41c07e789f6627bcb01c905d1f9bbf)

--- hw/fw_cfg.c
+++ hw/fw_cfg.c
@@ -47,6 +47,7 @@ typedef struct _FWCfgEntry {
 
 struct _FWCfgState {
     FWCfgEntry entries[2][FW_CFG_MAX_ENTRY];
+    FWCfgFiles *files;
     uint16_t cur_entry;
     uint32_t cur_offset;
 };
@@ -270,6 +271,48 @@ int fw_cfg_add_callback(FWCfgState *s, uint16_t key, FWCfgCallback callback,
     s->entries[arch][key].callback_opaque = callback_opaque;
     s->entries[arch][key].callback = callback;
 
+    return 1;
+}
+
+int fw_cfg_add_file(FWCfgState *s,  const char *dir, const char *filename,
+                    uint8_t *data, uint32_t len)
+{
+    const char *basename;
+    int index;
+
+    if (!s->files) {
+        int dsize = sizeof(uint32_t) + sizeof(FWCfgFile) * FW_CFG_FILE_SLOTS;
+        s->files = qemu_mallocz(dsize);
+        fw_cfg_add_bytes(s, FW_CFG_FILE_DIR, (uint8_t*)s->files, dsize);
+    }
+
+    index = be32_to_cpu(s->files->count);
+    if (index == FW_CFG_FILE_SLOTS) {
+        fprintf(stderr, "fw_cfg: out of file slots\n");
+        return 0;
+    }
+
+    fw_cfg_add_bytes(s, FW_CFG_FILE_FIRST + index, data, len);
+
+    basename = strrchr(filename, '/');
+    if (basename) {
+        basename++;
+    } else {
+        basename = filename;
+    }
+    if (dir) {
+        snprintf(s->files->f[index].name, sizeof(s->files->f[index].name),
+                 "%s/%s", dir, basename);
+    } else {
+        snprintf(s->files->f[index].name, sizeof(s->files->f[index].name),
+                 "%s", basename);
+    }
+    s->files->f[index].size   = cpu_to_be32(len);
+    s->files->f[index].select = cpu_to_be16(FW_CFG_FILE_FIRST + index);
+    FW_CFG_DPRINTF("%s: #%d: %s (%d bytes)\n", __FUNCTION__,
+                   index, s->files->f[index].name, len);
+
+    s->files->count = cpu_to_be32(index+1);
     return 1;
 }
 
