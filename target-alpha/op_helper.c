eacdccbb3e79deb04056bd2096ec9f679b6761ae
Blue Swirl <blauwirbel@gmail.com>
2009-12-20 10:27:44 +0000

    alpha: fix compile
    Signed-off-by: Blue Swirl <blauwirbel@gmail.com>

--- target-alpha/op_helper.c
+++ target-alpha/op_helper.c
@@ -46,10 +46,10 @@ uint64_t helper_load_fpcr (void)
 {
     uint64_t ret = 0;
 #ifdef CONFIG_SOFTFLOAT
-    ret |= env->fp_status.float_exception_flags << 52;
+    ret |= (uint64_t)env->fp_status.float_exception_flags << 52;
     if (env->fp_status.float_exception_flags)
         ret |= 1ULL << 63;
-    env->ipr[IPR_EXC_SUM] &= ~0x3E:
+    env->ipr[IPR_EXC_SUM] &= ~0x3E;
     env->ipr[IPR_EXC_SUM] |= env->fp_status.float_exception_flags << 1;
 #endif
     switch (env->fp_status.float_rounding_mode) {
