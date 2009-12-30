302698e882aacc6b5e1ff1b80bff0b691c69ce2f
Blue Swirl <blauwirbel@gmail.com>
2009-12-30 12:26:01 +0000

    Sparc64: fix compile with DEBUG_IRQ enabled
    Signed-off-by: Blue Swirl <blauwirbel@gmail.com>

--- hw/sun4u.c
+++ hw/sun4u.c
@@ -347,7 +347,8 @@ void cpu_tick_set_limit(void *opaque, uint64_t limit)
 static void ebus_mmio_mapfunc(PCIDevice *pci_dev, int region_num,
                               pcibus_t addr, pcibus_t size, int type)
 {
-    DPRINTF("Mapping region %d registers at %08x\n", region_num, addr);
+    DPRINTF("Mapping region %d registers at %" FMT_PCIBUS "\n", region_num,
+            addr);
     switch (region_num) {
     case 0:
         isa_mmio_init(addr, 0x1000000);
