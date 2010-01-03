dffbe21746fc7a8b66fbbdf15f74eefccd8edafd
Igor V. Kovalenko <igor.v.kovalenko@gmail.com>
2010-01-03 15:09:27 +0300

    pass env to raise_exception if called outside of op_helper code
    - this fixes stepping with gdb, where do_unassigned_access
      may be called from gdb handler, outside of generated code
    
    Signed-off-by: Igor V. Kovalenko <igor.v.kovalenko@gmail.com>
    Signed-off-by: Blue Swirl <blauwirbel@gmail.com>

--- target-sparc/op_helper.c
+++ target-sparc/op_helper.c
@@ -3686,21 +3686,24 @@ void do_unassigned_access(target_phys_addr_t addr, int is_write, int is_exec,
 void do_unassigned_access(target_phys_addr_t addr, int is_write, int is_exec,
                           int is_asi, int size)
 {
-#ifdef DEBUG_UNASSIGNED
     CPUState *saved_env;
 
     /* XXX: hack to restore env in all cases, even if not called from
        generated code */
     saved_env = env;
     env = cpu_single_env;
+
+#ifdef DEBUG_UNASSIGNED
     printf("Unassigned mem access to " TARGET_FMT_plx " from " TARGET_FMT_lx
            "\n", addr, env->pc);
-    env = saved_env;
 #endif
+
     if (is_exec)
         raise_exception(TT_CODE_ACCESS);
     else
         raise_exception(TT_DATA_ACCESS);
+
+    env = saved_env;
 }
 #endif
 
