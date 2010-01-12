dbd483242c2e6dfaacb9fd3d20c333bbdad87243
Michael S. Tsirkin <mst@redhat.com>
2010-01-12 21:16:59 +0200

    qdev: fix thinko leading to guest crashes
    Without this fix, guest crashes with drive=virtio.
    
    Signed-off-by: Michael S. Tsirkin <mst@redhat.com>
    Signed-off-by: Anthony Liguori <aliguori@us.ibm.com>

--- hw/qdev-properties.c
+++ hw/qdev-properties.c
@@ -20,7 +20,7 @@ static void bit_prop_set(DeviceState *dev, Property *props, bool val)
     uint32_t *p = qdev_get_prop_ptr(dev, props);
     uint32_t mask = qdev_get_prop_mask(props);
     if (val)
-        *p |= ~mask;
+        *p |= mask;
     else
         *p &= ~mask;
 }
