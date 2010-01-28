afc535acb579a7808d9ff170088c839a29c61dc9
Evgeniy Dushistov <dushistov@mail.ru>
2010-01-28 21:44:46 +0300

    Do not ignore error, if open file failed (-serial /dev/tty)
    In case, when qemu is executed with option like
    -serial /dev/ttyS0, report if there are problems with
    opening of devices. At now errors are silently ignoring.
    
    Signed-off-by: Evgeniy Dushistov <dushistov@mail.ru>
    Signed-off-by: Anthony Liguori <aliguori@us.ibm.com>

--- qemu-char.c
+++ qemu-char.c
@@ -1180,6 +1180,9 @@ static CharDriverState *qemu_chr_open_tty(QemuOpts *opts)
     int fd;
 
     TFR(fd = open(filename, O_RDWR | O_NONBLOCK));
+    if (fd < 0) {
+        return NULL;
+    }
     tty_serial_init(fd, 115200, 'N', 8, 1);
     chr = qemu_chr_open_fd(fd, fd);
     if (!chr) {
