cc0c4185e54867ef93c4b4dc0a8f3ecbc20ece42
Markus Armbruster <armbru@redhat.com>
2010-01-29 10:34:00 +0100

    monitor: Use QERR_INVALID_PARAMETER instead of QERR_INVALID_CPU_INDEX
    This changes the error message from "Invalid CPU index" to "Invalid
    parameter index" in the human monitor.
    
    Signed-off-by: Markus Armbruster <armbru@redhat.com>
    Signed-off-by: Anthony Liguori <aliguori@us.ibm.com>

--- monitor.c
+++ monitor.c
@@ -907,7 +907,7 @@ static void do_cpu_set(Monitor *mon, const QDict *qdict, QObject **ret_data)
 {
     int index = qdict_get_int(qdict, "index");
     if (mon_set_cpu(index) < 0)
-        qemu_error_new(QERR_INVALID_CPU_INDEX);
+        qemu_error_new(QERR_INVALID_PARAMETER, "index");
 }
 
 static void do_info_jit(Monitor *mon)
