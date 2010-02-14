204ff57128532207893f170dab4db158f2ca7024
Blue Swirl <blauwirbel@gmail.com>
2010-02-14 09:01:14 +0000

    dec: use PCI accessors
    Signed-off-by: Blue Swirl <blauwirbel@gmail.com>

--- hw/dec_pci.c
+++ hw/dec_pci.c
@@ -81,9 +81,8 @@ static int dec_21154_pci_host_init(PCIDevice *d)
     /* PCI2PCI bridge same values as PearPC - check this */
     pci_config_set_vendor_id(d->config, PCI_VENDOR_ID_DEC);
     pci_config_set_device_id(d->config, PCI_DEVICE_ID_DEC_21154);
-    d->config[0x08] = 0x02; // revision
+    pci_set_byte(d->config + PCI_REVISION_ID, 0x02);
     pci_config_set_class(d->config, PCI_CLASS_BRIDGE_PCI);
-    d->config[PCI_HEADER_TYPE] = PCI_HEADER_TYPE_BRIDGE; // header_type
     return 0;
 }
 
