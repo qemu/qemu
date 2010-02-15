d76fa62dba54a156ca0f5e79eb33756c9015e02c
Amit Shah <amit.shah@redhat.com>
2010-02-15 20:43:54 +0530

    pc: Bump up pc version to 0.13 and add a 0.12 compat version
    The version 0.13 will be the new default and compatibility options will
    be added to the 0.12 version.
    
    Signed-off-by: Amit Shah <amit.shah@redhat.com>
    Signed-off-by: Anthony Liguori <aliguori@us.ibm.com>

--- hw/pc.c
+++ hw/pc.c
@@ -1052,12 +1052,19 @@ void cmos_set_s3_resume(void)
 }
 
 static QEMUMachine pc_machine = {
-    .name = "pc-0.12",
+    .name = "pc-0.13",
     .alias = "pc",
     .desc = "Standard PC",
     .init = pc_init_pci,
     .max_cpus = 255,
     .is_default = 1,
+};
+
+static QEMUMachine pc_machine_v0_12 = {
+    .name = "pc-0.12",
+    .desc = "Standard PC",
+    .init = pc_init_pci,
+    .max_cpus = 255,
 };
 
 static QEMUMachine pc_machine_v0_11 = {
@@ -1136,6 +1143,7 @@ static QEMUMachine isapc_machine = {
 static void pc_machine_init(void)
 {
     qemu_register_machine(&pc_machine);
+    qemu_register_machine(&pc_machine_v0_12);
     qemu_register_machine(&pc_machine_v0_11);
     qemu_register_machine(&pc_machine_v0_10);
     qemu_register_machine(&isapc_machine);
