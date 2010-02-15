ee4d45be0d791eb8bb0f767cd0f17ea8f697281b
Michael S. Tsirkin <mst@redhat.com>
2010-02-15 20:43:56 +0530

    virtio-serial: don't set MULTIPORT for 1 port dev
    Since commit 98b19252cf1bd97c54bc4613f3537c5ec0aae263, all
    serial devices declare MULTIPORT feature.
    To allow 0.12 compatibility, we should clear this when
    max_nr_ports is 1.
    
    Signed-off-by: Michael S. Tsirkin <mst@redhat.com>
    Signed-off-by: Amit Shah <amit.shah@redhat.com>
    Signed-off-by: Anthony Liguori <aliguori@us.ibm.com>

--- hw/virtio-serial-bus.c
+++ hw/virtio-serial-bus.c
@@ -335,8 +335,10 @@ static void handle_input(VirtIODevice *vdev, VirtQueue *vq)
 
 static uint32_t get_features(VirtIODevice *vdev, uint32_t features)
 {
-    features |= (1 << VIRTIO_CONSOLE_F_MULTIPORT);
-
+    VirtIOSerial *vser = DO_UPCAST(VirtIOSerial, vdev, vdev);
+    if (vser->bus->max_nr_ports > 1) {
+        features |= (1 << VIRTIO_CONSOLE_F_MULTIPORT);
+    }
     return features;
 }
 
