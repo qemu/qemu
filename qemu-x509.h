de7890db9ed3baa07d3fcdd4beef1bdfc430ac2b
Gerd Hoffmann <kraxel@redhat.com>
2010-02-25 09:41:36 +0100

    move x509 file name defines to qemu-x509.h
    Want share them with vnc and spice.
    
    Signed-off-by: Gerd Hoffmann <kraxel@redhat.com>
    Signed-off-by: Anthony Liguori <aliguori@us.ibm.com>

---
+++ qemu-x509.h
@@ -0,0 +1,9 @@
+#ifndef QEMU_X509_H
+#define QEMU_X509_H
+
+#define X509_CA_CERT_FILE "ca-cert.pem"
+#define X509_CA_CRL_FILE "ca-crl.pem"
+#define X509_SERVER_KEY_FILE "server-key.pem"
+#define X509_SERVER_CERT_FILE "server-cert.pem"
+
+#endif /* QEMU_X509_H */
