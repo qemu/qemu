6b8f8fff78185cb260d1ca3c30352c0bf5601d36
Luiz Capitulino <lcapitulino@redhat.com>
2010-02-25 12:06:59 -0300

    QMP: Move STOP event into do_vm_stop()
    I've introduced the STOP event in the main loop, this is wrong
    as it will be only emitted if the io thread is enabled.
    
    This fixes that by moving the STOP event to do_vm_stop().
    
    Signed-off-by: Luiz Capitulino <lcapitulino@redhat.com>
    Signed-off-by: Anthony Liguori <aliguori@us.ibm.com>

--- vl.c
+++ vl.c
@@ -3137,6 +3137,7 @@ static void do_vm_stop(int reason)
         vm_running = 0;
         pause_all_vcpus();
         vm_state_notify(0, reason);
+        monitor_protocol_event(QEVENT_STOP, NULL);
     }
 }
 
@@ -4105,7 +4106,6 @@ static void main_loop(void)
             qemu_irq_raise(qemu_system_powerdown);
         }
         if ((r = qemu_vmstop_requested())) {
-            monitor_protocol_event(QEVENT_STOP, NULL);
             vm_stop(r);
         }
     }
