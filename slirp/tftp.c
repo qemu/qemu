aca9fcd28e8a40f2555f8d6d113e4988335aa629
Blue Swirl <blauwirbel@gmail.com>
2010-03-07 13:13:05 +0000

    slirp: remove dead assignments, spotted by clang
    Value stored is never read.
    
    Signed-off-by: Blue Swirl <blauwirbel@gmail.com>

--- slirp/tftp.c
+++ slirp/tftp.c
@@ -161,7 +161,6 @@ static void tftp_send_error(struct tftp_session *spt,
   struct sockaddr_in saddr, daddr;
   struct mbuf *m;
   struct tftp_t *tp;
-  int nobytes;
 
   m = m_get(spt->slirp);
 
@@ -184,8 +183,6 @@ static void tftp_send_error(struct tftp_session *spt,
 
   daddr.sin_addr = spt->client_ip;
   daddr.sin_port = spt->client_port;
-
-  nobytes = 2;
 
   m->m_len = sizeof(struct tftp_t) - 514 + 3 + strlen(msg) -
         sizeof(struct ip) - sizeof(struct udphdr);
