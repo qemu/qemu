cc39a92cbfc80c70d2b83708a4c9b309c3126ac3
Chris Webb <chris@arachsys.com>
2010-03-08 14:34:49 +0000

    Fix SIGFPE for vnc display of width/height = 1
    During boot, the screen gets resized to height 1 and a mouse click at this
    point will cause a division by zero when calculating the absolute pointer
    position from the pixel (x, y). Return a click in the middle of the screen
    instead in this case.
    
    Signed-off-by: Chris Webb <chris@arachsys.com>
    Signed-off-by: Anthony Liguori <aliguori@us.ibm.com>

--- vnc.c
+++ vnc.c
@@ -1457,8 +1457,10 @@ static void pointer_event(VncState *vs, int button_mask, int x, int y)
         dz = 1;
 
     if (vs->absolute) {
-        kbd_mouse_event(x * 0x7FFF / (ds_get_width(vs->ds) - 1),
-                        y * 0x7FFF / (ds_get_height(vs->ds) - 1),
+        kbd_mouse_event(ds_get_width(vs->ds) > 1 ?
+                          x * 0x7FFF / (ds_get_width(vs->ds) - 1) : 0x4000,
+                        ds_get_height(vs->ds) > 1 ?
+                          y * 0x7FFF / (ds_get_height(vs->ds) - 1) : 0x4000,
                         dz, buttons);
     } else if (vnc_has_feature(vs, VNC_FEATURE_POINTER_TYPE_CHANGE)) {
         x -= 0x7FFF;
