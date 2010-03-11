e117f7725af8416a0c6e416fe18549d1625f7498
Andre Przywara <andre.przywara@amd.com>
2010-03-11 14:38:59 +0100

    x86/cpuid: add missing CPUID feature flag names
    Some CPUID feature flags had no string value, so they could not be
    switched on or off from the command line.
    Add names for the missing ones mentioned in the current public CPUID
    specification from both Intel and AMD. Those only mentioned in the
    Linux kernel source I put as comments.
    
    Signed-off-by: Andre Przywara <andre.przywara@amd.com>
    Signed-off-by: Aurelien Jarno <aurelien@aurel32.net>

--- target-i386/cpuid.c
+++ target-i386/cpuid.c
@@ -42,14 +42,14 @@ static const char *feature_name[] = {
     "ht" /* Intel htt */, "tm", "ia64", "pbe",
 };
 static const char *ext_feature_name[] = {
-    "pni|sse3" /* Intel,AMD sse3 */, NULL, NULL, "monitor",
-    "ds_cpl", "vmx", NULL /* Linux smx */, "est",
+    "pni|sse3" /* Intel,AMD sse3 */, "pclmuldq", "dtes64", "monitor",
+    "ds_cpl", "vmx", "smx", "est",
     "tm2", "ssse3", "cid", NULL,
-    NULL, "cx16", "xtpr", NULL,
+    "fma", "cx16", "xtpr", "pdcm",
     NULL, NULL, "dca", "sse4.1|sse4_1",
-    "sse4.2|sse4_2", "x2apic", NULL, "popcnt",
-    NULL, NULL, NULL, NULL,
-    NULL, NULL, NULL, "hypervisor",
+    "sse4.2|sse4_2", "x2apic", "movbe", "popcnt",
+    NULL, "aes", "xsave", "osxsave",
+    "avx", NULL, NULL, "hypervisor",
 };
 static const char *ext2_feature_name[] = {
     "fpu", "vme", "de", "pse",
@@ -64,9 +64,9 @@ static const char *ext2_feature_name[] = {
 static const char *ext3_feature_name[] = {
     "lahf_lm" /* AMD LahfSahf */, "cmp_legacy", "svm", "extapic" /* AMD ExtApicSpace */,
     "cr8legacy" /* AMD AltMovCr8 */, "abm", "sse4a", "misalignsse",
-    "3dnowprefetch", "osvw", NULL /* Linux ibs */, NULL,
+    "3dnowprefetch", "osvw", "ibs", "xop",
     "skinit", "wdt", NULL, NULL,
-    NULL, NULL, NULL, NULL,
+    "fma4", NULL, "cvt16", "nodeid_msr",
     NULL, NULL, NULL, NULL,
     NULL, NULL, NULL, NULL,
     NULL, NULL, NULL, NULL,
