090414a30c6c1d276eca48e76945f2cfcf0cccf9
Blue Swirl <blauwirbel@gmail.com>
2010-03-13 11:36:09 +0000

    Fix a typo in error message
    Signed-off-by: Blue Swirl <blauwirbel@gmail.com>

--- savevm.c
+++ savevm.c
@@ -356,7 +356,7 @@ QEMUFile *qemu_fopen(const char *filename, const char *mode)
     if (mode == NULL ||
 	(mode[0] != 'r' && mode[0] != 'w') ||
 	mode[1] != 'b' || mode[2] != 0) {
-        fprintf(stderr, "qemu_fdopen: Argument validity check failed\n");
+        fprintf(stderr, "qemu_fopen: Argument validity check failed\n");
         return NULL;
     }
 
