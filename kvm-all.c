ca82180603446831187203383de4b5ede4e2c346
Michael S. Tsirkin <mst@redhat.com>
2010-03-17 13:07:54 +0200

    kvm: add API to set ioeventfd
    Comment on kvm usage: rather than require users to do if (kvm_enabled())
    and/or ifdefs, this patch adds an API that, internally, is defined to
    stub function on non-kvm build, and checks kvm_enabled for non-kvm
    run.
    
    While rest of qemu code still uses if (kvm_enabled()), I think this
    approach is cleaner, and we should convert rest of code to it
    long term.
    
    Signed-off-by: Michael S. Tsirkin <mst@redhat.com>
    Signed-off-by: Anthony Liguori <aliguori@us.ibm.com>

--- kvm-all.c
+++ kvm-all.c
@@ -1151,3 +1151,25 @@ int kvm_set_signal_mask(CPUState *env, const sigset_t *sigset)
 
     return r;
 }
+
+#ifdef KVM_IOEVENTFD
+int kvm_set_ioeventfd_pio_word(int fd, uint16_t addr, uint16_t val, bool assign)
+{
+    struct kvm_ioeventfd kick = {
+        .datamatch = val,
+        .addr = addr,
+        .len = 2,
+        .flags = KVM_IOEVENTFD_FLAG_DATAMATCH | KVM_IOEVENTFD_FLAG_PIO,
+        .fd = fd,
+    };
+    int r;
+    if (!kvm_enabled())
+        return -ENOSYS;
+    if (!assign)
+        kick.flags |= KVM_IOEVENTFD_FLAG_DEASSIGN;
+    r = kvm_vm_ioctl(kvm_state, KVM_IOEVENTFD, &kick);
+    if (r < 0)
+        return r;
+    return 0;
+}
+#endif
--- kvm.h
+++ kvm.h
@@ -14,12 +14,17 @@
 #ifndef QEMU_KVM_H
 #define QEMU_KVM_H
 
+#include <stdbool.h>
+#include <errno.h>
 #include "config-host.h"
 #include "qemu-queue.h"
 
-extern int kvm_allowed;
+#ifdef CONFIG_KVM
+#include <linux/kvm.h>
+#endif
 
 #ifdef CONFIG_KVM
+extern int kvm_allowed;
 #define kvm_enabled() (kvm_allowed)
 #else
 #define kvm_enabled() (0)
@@ -161,5 +166,16 @@ static inline void cpu_synchronize_post_init(CPUState *env)
         kvm_cpu_synchronize_post_init(env);
     }
 }
+
+#if defined(KVM_IOEVENTFD) && defined(CONFIG_KVM)
+int kvm_set_ioeventfd_pio_word(int fd, uint16_t adr, uint16_t val, bool assign);
+#else
+static inline
+int kvm_set_ioeventfd_pio_word(int fd, uint16_t adr, uint16_t val, bool assign)
+{
+    return -ENOSYS;
+}
+#endif
+
 #endif
 #endif
