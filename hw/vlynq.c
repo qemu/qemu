b080e775e344b934df9345c6a4175af23f597a34
Stefan Weil <weil@mail.berlios.de>
2010-03-20 23:10:05 +0100

    vlynq: Add copyright
    Signed-off-by: Stefan Weil <weil@mail.berlios.de>

--- hw/vlynq.c
+++ hw/vlynq.c
@@ -1,7 +1,23 @@
 /*
- * QEMU VLYNQ Interface support
+ * QEMU VLYNQ Serial Interface support.
  *
- * This code is licenced under the GNU GPL v2.
+ * Copyright (C) 2009-2010 Stefan Weil
+ *
+ * Portions of the code are copies from ssi.h.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 2 of the License, or
+ * (at your option) version 3 or any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
  */
 
 #include "vlynq.h"
--- hw/vlynq.h
+++ hw/vlynq.h
@@ -1,4 +1,24 @@
-/* QEMU Synchronous Serial Interface support.  */
+/*
+ * QEMU VLYNQ Serial Interface support.
+ *
+ * Copyright (C) 2009-2010 Stefan Weil
+ *
+ * Portions of the code are copies from ssi.h.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 2 of the License, or
+ * (at your option) version 3 or any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
 
 /* In principle VLYNQ is a point-point interface.  As such the qemu
    implementation has a single slave device on a "bus".
@@ -29,6 +49,17 @@ struct VLYNQSlave {
     DeviceState qdev;
     VLYNQSlaveInfo *info;
 };
+
+typedef struct {
+    DeviceInfo qdev;
+    pci_qdev_initfn init;
+    PCIUnregisterFunc *exit;
+    //~ PCIConfigReadFunc *config_read;
+    //~ PCIConfigWriteFunc *config_write;
+
+    /* pci config header type */
+    //~ uint8_t header_type;
+} VLYNQDeviceInfo;
 
 #define VLYNQ_SLAVE_FROM_QDEV(dev) DO_UPCAST(VLYNQSlave, qdev, dev)
 #define FROM_VLYNQ_SLAVE(type, dev) DO_UPCAST(type, vlynqdev, dev)
