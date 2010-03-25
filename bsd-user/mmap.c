b035ffd11813524d7c0e44354f5c4bdd281f4b37
Juergen Lock <nox@jelal.kn-bremen.de>
2010-03-25 22:11:17 +0100

    Fix bsd-user qemu_vmalloc() host page protection code
    Just do the same as linux-user does.
    
    Signed-off-by: Juergen Lock <nox@jelal.kn-bremen.de>
    Signed-off-by: Blue Swirl <blauwirbel@gmail.com>

--- bsd-user/mmap.c
+++ bsd-user/mmap.c
@@ -77,16 +77,15 @@ void mmap_unlock(void)
 void *qemu_vmalloc(size_t size)
 {
     void *p;
-    unsigned long addr;
     mmap_lock();
     /* Use map and mark the pages as used.  */
     p = mmap(NULL, size, PROT_READ | PROT_WRITE,
              MAP_PRIVATE | MAP_ANON, -1, 0);
 
-    addr = (unsigned long)p;
-    if (addr == (target_ulong) addr) {
+    if (h2g_valid(p)) {
         /* Allocated region overlaps guest address space.
            This may recurse.  */
+        abi_ulong addr = h2g(p);
         page_set_flags(addr & TARGET_PAGE_MASK, TARGET_PAGE_ALIGN(addr + size),
                        PAGE_RESERVED);
     }
