c8160fab31e7a27979196c338a305e7095dd8aea
Stefan Weil <weil@mail.berlios.de>
2010-04-09 22:49:52 +0200

    sh4: Fix compiler warning (fprintf format string)
    When argument checking is enabled, gcc throws this error:
    
    error: format not a string literal and no format arguments
    
    The patch rewrites the statement to satisfy the compiler.
    
    Signed-off-by: Stefan Weil <weil@mail.berlios.de>
    Signed-off-by: Aurelien Jarno <aurelien@aurel32.net>

--- sh4-dis.c
+++ sh4-dis.c
@@ -1493,10 +1493,10 @@ print_insn_ppi (int field_b, struct disassemble_info *info)
 		  print_dsp_reg (field_b & 0xf, fprintf_fn, stream);
 		  break;
 		case DSP_REG_X:
-		  fprintf_fn (stream, sx_tab[(field_b >> 6) & 3]);
+		  fprintf_fn (stream, "%s", sx_tab[(field_b >> 6) & 3]);
 		  break;
 		case DSP_REG_Y:
-		  fprintf_fn (stream, sy_tab[(field_b >> 4) & 3]);
+		  fprintf_fn (stream, "%s", sy_tab[(field_b >> 4) & 3]);
 		  break;
 		case A_MACH:
 		  fprintf_fn (stream, "mach");
