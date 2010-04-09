2488b41bb2d03c96ffdd69a0c16b585e90697951
Aurelien Jarno <aurelien@aurel32.net>
2010-04-09 20:52:48 +0200

    tcg/arm: align 64-bit arguments in function calls
    As specified by the "Procedure Call Standard for the ARM Architecture".
    
    Signed-off-by: Aurelien Jarno <aurelien@aurel32.net>

--- tcg/arm/tcg-target.h
+++ tcg/arm/tcg-target.h
@@ -54,6 +54,7 @@ enum {
 /* used for function call generation */
 #define TCG_REG_CALL_STACK		TCG_REG_R13
 #define TCG_TARGET_STACK_ALIGN		8
+#define TCG_TARGET_CALL_ALIGN_ARGS	1
 #define TCG_TARGET_CALL_STACK_OFFSET	0
 
 /* optional instructions */
