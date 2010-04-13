72a769a186dd18b44d7cced6fb91c6318b4d9c96
Stefan Weil <weil@mail.berlios.de>
2010-04-13 18:34:37 +0200

    tcg/bytecode: Don't set TCG_TARGET_WORDS_BIGENDIAN
    The bytecode interpreter tries to emulate a little endian machine
    (which stores register values in the host's native format), so
    from a TCG point of view, it is never big endian.
    
    Signed-off-by: Stefan Weil <weil@mail.berlios.de>

--- tcg/bytecode/tcg-target.h
+++ tcg/bytecode/tcg-target.h
@@ -44,8 +44,10 @@
 #define CONFIG_DEBUG_TCG_INTERPRETER
 #endif
 
+#if 0 /* TCI tries to emulate a little endian host. */
 #if defined(HOST_WORDS_BIGENDIAN)
 # define TCG_TARGET_WORDS_BIGENDIAN
+#endif
 #endif
 
 /* Target word size (must be identical to pointer size). */
