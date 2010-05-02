0c459361a1117a6c434c7b2b008a4c6c035eb4bf
Rabin Vincent <rabin@rab.in>
2010-05-02 15:20:52 +0530

    arm_timer: fix oneshot mode
    In oneshot mode, the delta needs to come from the TimerLoad register,
    not the maximum limit.
    
    Signed-off-by: Rabin Vincent <rabin@rab.in>
    Signed-off-by: Aurelien Jarno <aurelien@aurel32.net>
    (cherry picked from commit a9cf98d939c4f6539fad7e7d812ea16d96ba3dc9)

--- hw/arm_timer.c
+++ hw/arm_timer.c
@@ -71,7 +71,7 @@ static void arm_timer_recalibrate(arm_timer_state *s, int reload)
 {
     uint32_t limit;
 
-    if ((s->control & TIMER_CTRL_PERIODIC) == 0) {
+    if ((s->control & (TIMER_CTRL_PERIODIC | TIMER_CTRL_ONESHOT)) == 0) {
         /* Free running.  */
         if (s->control & TIMER_CTRL_32BIT)
             limit = 0xffffffff;
