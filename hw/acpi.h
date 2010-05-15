1012e960c747849e53db702c96b4c0f25a9bda95
Blue Swirl <blauwirbel@gmail.com>
2010-05-15 17:52:49 +0000

    Update to a hopefully more future proof FSF address
    See also 70539e1850ddd3a7ee6f9a8db7bd8e81b85225a4,
    8167ee883931cb20c6264fc19d040ce2dc6ceaaa,
    530e7615ce3c01882e582c84dc6304ab98a3d5c5 and
    fad6cb1a565bb73f83fc0e2654489457b489e436.
    
    Signed-off-by: Blue Swirl <blauwirbel@gmail.com>

--- hw/acpi.h
+++ hw/acpi.h
@@ -15,8 +15,8 @@
  * Lesser General Public License for more details.
  *
  * You should have received a copy of the GNU Lesser General Public
- * License along with this library; if not, write to the Free Software
- * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston MA  02110-1301 USA
+ * License along with this library; if not, see
+ * <http://www.gnu.org/licenses/>.
  */
 
 /* from linux include/acpi/actype.h */
