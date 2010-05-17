396759ad4ad5289623eb7e1993c433ad4e7b13a1
Nicholas Bellinger <nab@linux-iscsi.org>
2010-05-17 09:46:04 -0700

    block: Add SG_IO device check in refresh_total_sectors()
    This patch adds a special case check for scsi-generic devices in
    refresh_total_sectors() to skip the subsequent BlockDriver->bdrv_getlength()
    that will be returning -ESPIPE from block/raw-posic.c:raw_getlength() for
    BlockDriverState->sg=1 devices.
    
    Signed-off-by: Nicholas A. Bellinger <nab@linux-iscsi.org>
    Signed-off-by: Kevin Wolf <kwolf@redhat.com>

--- block.c
+++ block.c
@@ -361,6 +361,10 @@ static int refresh_total_sectors(BlockDriverState *bs, int64_t hint)
 {
     BlockDriver *drv = bs->drv;
 
+    /* Do not attempt drv->bdrv_getlength() on scsi-generic devices */
+    if (bs->sg)
+        return 0;
+
     /* query actual device if possible, otherwise just trust the hint */
     if (drv->bdrv_getlength) {
         int64_t length = drv->bdrv_getlength(bs);
