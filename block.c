1fb9798b69d7a2fccce70aebe5c5162cf069d596
Christoph Hellwig <hch@lst.de>
2010-05-19 20:53:10 +0200

    block: fix sector comparism in multiwrite_req_compare
    The difference between the start sectors of two requests can be larger
    than the size of the "int" type, which can lead to a not correctly
    sorted multiwrite array and thus spurious I/O errors and filesystem
    corruption due to incorrect request merges.
    
    So instead of doing the cute sector arithmetics trick spell out the
    exact comparisms.
    
    Spotted by Kevin Wolf based on a testcase from Michael Tokarev.
    
    Signed-off-by: Christoph Hellwig <hch@lst.de>
    Signed-off-by: Kevin Wolf <kwolf@redhat.com>
    (cherry picked from commit 77be4366baface6613cfc312ba281f8e5860997c)

--- block.c
+++ block.c
@@ -1636,7 +1636,19 @@ static void multiwrite_cb(void *opaque, int ret)
 
 static int multiwrite_req_compare(const void *a, const void *b)
 {
-    return (((BlockRequest*) a)->sector - ((BlockRequest*) b)->sector);
+    const BlockRequest *req1 = a, *req2 = b;
+
+    /*
+     * Note that we can't simply subtract req2->sector from req1->sector
+     * here as that could overflow the return value.
+     */
+    if (req1->sector > req2->sector) {
+        return 1;
+    } else if (req1->sector < req2->sector) {
+        return -1;
+    } else {
+        return 0;
+    }
 }
 
 /*
