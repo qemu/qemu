c63782cbe8bdc2c401ea710cef427de0214c5900
Jes Sorensen <Jes.Sorensen@redhat.com>
2010-05-27 15:46:55 +0200

    block.h: Make BDRV_SECTOR_SIZE 64 bit safe
    C defaults to int, so make definition of BDRV_SECTOR_SIZE 64 bit
    safe as it and BDRV_SECTOR_MASK may be used against 64 bit addresses.
    
    Signed-off-by: Jes Sorensen <Jes.Sorensen@redhat.com>
    Signed-off-by: Kevin Wolf <kwolf@redhat.com>

--- block.h
+++ block.h
@@ -38,7 +38,7 @@ typedef struct QEMUSnapshotInfo {
 #define BDRV_O_CACHE_MASK  (BDRV_O_NOCACHE | BDRV_O_CACHE_WB)
 
 #define BDRV_SECTOR_BITS   9
-#define BDRV_SECTOR_SIZE   (1 << BDRV_SECTOR_BITS)
+#define BDRV_SECTOR_SIZE   (1ULL << BDRV_SECTOR_BITS)
 #define BDRV_SECTOR_MASK   ~(BDRV_SECTOR_SIZE - 1)
 
 typedef enum {
