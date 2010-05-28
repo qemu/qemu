81bf96d3d299a7f88bf3e2ece4f795a9949db5f7
Markus Armbruster <armbru@redhat.com>
2010-05-28 17:03:22 +0200

    usb-serial: Fail instead of crash when chardev is missing
    Signed-off-by: Markus Armbruster <armbru@redhat.com>
    Acked-by: Gerd Hoffmann <kraxel@redhat.com>
    Signed-off-by: Aurelien Jarno <aurelien@aurel32.net>

--- hw/usb-serial.c
+++ hw/usb-serial.c
@@ -545,6 +545,11 @@ static int usb_serial_initfn(USBDevice *dev)
     USBSerialState *s = DO_UPCAST(USBSerialState, dev, dev);
     s->dev.speed = USB_SPEED_FULL;
 
+    if (!s->cs) {
+        error_report("Property chardev is required");
+        return -1;
+    }
+
     qemu_chr_add_handlers(s->cs, usb_serial_can_read, usb_serial_read,
                           usb_serial_event, s);
     usb_serial_handle_reset(dev);
