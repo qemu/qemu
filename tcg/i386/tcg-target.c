qemu/qemu

commit c28b14c694d759f39fe3ae4f8d03b567da5b93f8

tcg-i386: declare tcg_out_tlb_load() inline
Declare tcg_out_tlb_load() inline so that we don't loose optimisations
with commit 8516a04467cb7954cdc32e8b79b4b7df56dccb16.

Signed-off-by: Aurelien Jarno <aurelien@aurel32.net>

tcg/i386/tcg-target.c
@@ -724,9 +724,10 @@ static void *qemu_st_helpers[4] = {
 
    EDX is clobbered.  */
 
-static void tcg_out_tlb_load(TCGContext *s, int addrlo_idx, int mem_index,
-                             int s_bits, const TCGArg *args,
-                             uint8_t **label_ptr, int which)
+static inline void tcg_out_tlb_load(TCGContext *s, int addrlo_idx,
+                                    int mem_index, int s_bits,
+                                    const TCGArg *args
+                                    uint8_t **label_ptr, int which)
 {
     const int addrlo = args[addrlo_idx];
     const int r0 = TCG_REG_EAX;

commit 4309a79bffce10d6d8de82c5ee403ffa4f45db64

tcg-i386: fix a typo
Fix a typo introduced by c28b14c694d759f39fe3ae4f8d03b567da5b93f8.

Signed-off-by: Aurelien Jarno <aurelien@aurel32.net>

tcg/i386/tcg-target.c
@@ -726,7 +726,7 @@ static void *qemu_st_helpers[4] = {
 
 static inline void tcg_out_tlb_load(TCGContext *s, int addrlo_idx,
                                     int mem_index, int s_bits,
-                                    const TCGArg *args
+                                    const TCGArg *args,
                                     uint8_t **label_ptr, int which)
 {
     const int addrlo = args[addrlo_idx];