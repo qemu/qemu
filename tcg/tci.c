49467ce3c89c3a2449dba7018255ee6a017b9581
Stefan Weil <weil@mail.berlios.de>
2010-06-13 21:19:40 +0200

    tcg/tci: stdbool.h now included from qemu-common.h
    Signed-off-by: Stefan Weil <weil@mail.berlios.de>

--- tcg/tci.c
+++ tcg/tci.c
@@ -17,8 +17,6 @@
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
-#include <stdbool.h>    /* bool */
-
 #include "config.h"
 #include "qemu-common.h"
 #include "tcg-op.h"
