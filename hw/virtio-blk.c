20a81e4d178379381fbd522eda5f664ba2ecdaaa
Yoshiaki Tamura <tamura.yoshiaki@lab.ntt.co.jp>
2010-06-21 17:50:01 +0900

    virtio-blk: fix the list operation in virtio_blk_load().
    Although it is really rare to get in to the while loop, the list
    operation in the loop is obviously wrong.
    
    Signed-off-by: Yoshiaki Tamura <tamura.yoshiaki@lab.ntt.co.jp>
    Signed-off-by: Kevin Wolf <kwolf@redhat.com>

--- hw/virtio-blk.c
+++ hw/virtio-blk.c
@@ -470,7 +470,7 @@ static int virtio_blk_load(QEMUFile *f, void *opaque, int version_id)
         VirtIOBlockReq *req = virtio_blk_alloc_request(s);
         qemu_get_buffer(f, (unsigned char*)&req->elem, sizeof(req->elem));
         req->next = s->rq;
-        s->rq = req->next;
+        s->rq = req;
     }
 
     return 0;
