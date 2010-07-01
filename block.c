bb44e0bbcea21e50aaed1bd54d0aa53c46cecbf1
Kevin Wolf <kwolf@redhat.com>
2010-07-01 16:08:51 +0200

    block: Handle multiwrite errors only when all requests have completed
    Don't try to be clever by freeing all temporary data and calling all callbacks
    when the return value (an error) is certain. Doing so has at least two
    important problems:
    
    * The temporary data that is freed (qiov, possibly zero buffer) is still used
      by the requests that have not yet completed.
    * Calling the callbacks for all requests in the multiwrite means for the caller
      that it may free buffers etc. which are still in use.
    
    Just remember the error value and do the cleanup when all requests have
    completed.
    
    Signed-off-by: Kevin Wolf <kwolf@redhat.com>
    (cherry picked from commit de189a1b4a471d37a2909e97646654fc9751b52f)

--- block.c
+++ block.c
@@ -1661,14 +1661,11 @@ static void multiwrite_cb(void *opaque, int ret)
 
     if (ret < 0 && !mcb->error) {
         mcb->error = ret;
-        multiwrite_user_cb(mcb);
     }
 
     mcb->num_requests--;
     if (mcb->num_requests == 0) {
-        if (mcb->error == 0) {
-            multiwrite_user_cb(mcb);
-        }
+        multiwrite_user_cb(mcb);
         qemu_free(mcb);
     }
 }
