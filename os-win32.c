0a1574bb1347f856524014ef23cb2d686361075c
Stefan Weil <weil@mail.berlios.de>
2010-07-01 00:47:49 +0000

    win32: Add missing function setenv
    Mingw32 does not provide a declaration and implementation of function
    setenv (which is used in sdl.c), so this patch adds both.
    
    Signed-off-by: Stefan Weil <weil@mail.berlios.de>
    Signed-off-by: Blue Swirl <blauwirbel@gmail.com>

--- os-win32.c
+++ os-win32.c
@@ -34,6 +34,21 @@
 #include "qemu-options.h"
 
 /***********************************************************/
+/* Functions missing in mingw */
+
+int setenv(const char *name, const char *value, int overwrite)
+{
+    int result = 0;
+    if (overwrite || !getenv(name)) {
+        size_t length = strlen(name) + strlen(value) + 2;
+        char *string = qemu_malloc(length);
+        snprintf(string, length, "%s=%s", name, value);
+        result = putenv(string);
+    }
+    return result;
+}
+
+/***********************************************************/
 /* Polling handling */
 
 typedef struct PollingEntry {
