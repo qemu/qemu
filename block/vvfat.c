f891f9f74dac3622848ea3c10d11ac1c0b81869c
Loïc Minier <loic.minier@linaro.org>
2010-08-22 00:47:23 +0200

    vvfat: fat_chksum(): fix access above array bounds
    Signed-off-by: Loïc Minier <loic.minier@linaro.org>
    Signed-off-by: Kevin Wolf <kwolf@redhat.com>
    (cherry picked from commit 2aa326be0d2039f51192707bdb2fc935d0e87c21)

--- block/vvfat.c
+++ block/vvfat.c
@@ -512,7 +512,7 @@ static inline uint8_t fat_chksum(const direntry_t* entry)
     for(i=0;i<11;i++) {
         unsigned char c;
 
-        c = (i <= 8) ? entry->name[i] : entry->extension[i-8];
+        c = (i < 8) ? entry->name[i] : entry->extension[i-8];
         chksum=(((chksum&0xfe)>>1)|((chksum&0x01)?0x80:0)) + c;
     }
 
