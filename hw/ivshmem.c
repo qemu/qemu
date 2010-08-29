ad0a4ac1c0e1859eb0c67900dba696cc459b42a7
Avi Kivity <avi@redhat.com>
2010-08-29 12:43:15 +0300

    Fix ivshmem build on 32-bit hosts
    stat() fields can be more or less anything depending on configuration, cast
    explicitly to uint64_t to avoid printf() format mismatches.
    
    Signed-off-by: Avi Kivity <avi@redhat.com>
    Signed-off-by: Blue Swirl <blauwirbel@gmail.com>

--- hw/ivshmem.c
+++ hw/ivshmem.c
@@ -351,9 +351,10 @@ static int check_shm_size(IVShmemState *s, int fd) {
     fstat(fd, &buf);
 
     if (s->ivshmem_size > buf.st_size) {
-        fprintf(stderr, "IVSHMEM ERROR: Requested memory size greater");
-        fprintf(stderr, " than shared object size (%" PRIu64 " > %ld)\n",
-                                          s->ivshmem_size, buf.st_size);
+        fprintf(stderr,
+                "IVSHMEM ERROR: Requested memory size greater"
+                " than shared object size (%" PRIu64 " > %" PRIu64")\n",
+                s->ivshmem_size, (uint64_t)buf.st_size);
         return -1;
     } else {
         return 0;
