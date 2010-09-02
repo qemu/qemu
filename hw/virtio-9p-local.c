5c0f255dd4c8b8f3349b6ed14233f77948c5a9a6
Aneesh Kumar K.V <aneesh.kumar@linux.vnet.ibm.com>
2010-09-02 11:09:07 +0530

    virtio-9p: Use lchown which won't follow symlink
    We should always use functions which don't follow
    symlink on the server
    
    Signed-off-by: Aneesh Kumar K.V <aneesh.kumar@linux.vnet.ibm.com>

--- hw/virtio-9p-local.c
+++ hw/virtio-9p-local.c
@@ -101,7 +101,7 @@ static int local_post_create_passthrough(FsContext *fs_ctx, const char *path,
     if (chmod(rpath(fs_ctx, path), credp->fc_mode & 07777) < 0) {
         return -1;
     }
-    if (chown(rpath(fs_ctx, path), credp->fc_uid, credp->fc_gid) < 0) {
+    if (lchown(rpath(fs_ctx, path), credp->fc_uid, credp->fc_gid) < 0) {
         /*
          * If we fail to change ownership and if we are
          * using security model none. Ignore the error
