14a0b95684d5e3588cab88c4900433746ab56c58
Anthony Liguori <aliguori@us.ibm.com>
2010-09-08 17:09:15 -0500

    Revert "Make default invocation of block drivers safer (v3)"
    This reverts commit 79368c81bf8cf93864d7afc88b81b05d8f0a2c90.
    
    Conflicts:
    
    	block.c
    
    I haven't been able to come up with a solution yet for the corruption caused by
    unaligned requests from the IDE disk so revert until a solution can be written.
    
    Signed-off-by: Anthony Liguori <aliguori@us.ibm.com>
    (cherry picked from commit 8b33d9eeba91422ee2d73b6936ad57262d18cf5a)

--- block.c
+++ block.c
@@ -523,7 +523,6 @@ int bdrv_open(BlockDriverState *bs, const char *filename, int flags,
               BlockDriver *drv)
 {
     int ret;
-    int probed = 0;
 
     if (flags & BDRV_O_SNAPSHOT) {
         BlockDriverState *bs1;
@@ -584,7 +583,6 @@ int bdrv_open(BlockDriverState *bs, const char *filename, int flags,
     /* Find the right image format driver */
     if (!drv) {
         ret = find_image_format(filename, &drv);
-        probed = 1;
     }
 
     if (!drv) {
@@ -596,8 +594,6 @@ int bdrv_open(BlockDriverState *bs, const char *filename, int flags,
     if (ret < 0) {
         goto unlink_and_fail;
     }
-
-    bs->probed = probed;
 
     /* If there is a backing file, use it */
     if ((flags & BDRV_O_NO_BACKING) == 0 && bs->backing_file[0] != '\0') {
