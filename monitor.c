de79ba6f53fa78a6132a67c7ff134014dc8479e7
Luiz Capitulino <lcapitulino@redhat.com>
2010-09-16 11:06:11 -0300

    Monitor: Directly call QObject handlers
    This avoids handle_user_command() calling monitor_call_handler(),
    which is currently shared with QMP.
    
    Signed-off-by: Luiz Capitulino <lcapitulino@redhat.com>

--- monitor.c
+++ monitor.c
@@ -3917,7 +3917,15 @@ static void handle_user_command(Monitor *mon, const char *cmdline)
     if (handler_is_async(cmd)) {
         user_async_cmd_handler(mon, cmd, qdict);
     } else if (handler_is_qobject(cmd)) {
-        monitor_call_handler(mon, cmd, qdict);
+        QObject *data = NULL;
+
+        /* XXX: ignores the error code */
+        cmd->mhandler.cmd_new(mon, qdict, &data);
+        assert(!monitor_has_error(mon));
+        if (data) {
+            cmd->user_print(mon, data);
+            qobject_decref(data);
+        }
     } else {
         cmd->mhandler.cmd(mon, qdict);
     }
