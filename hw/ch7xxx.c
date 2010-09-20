38d18224e9d5d764924ed3b839c8250e15a1b616
Stefan Weil <weil@mail.berlios.de>
2010-09-20 20:33:25 +0200

    ch7xxx: Convert device to qdev
    Signed-off-by: Stefan Weil <weil@mail.berlios.de>

--- hw/ch7xxx.c
+++ hw/ch7xxx.c
@@ -7,21 +7,20 @@
 
 #include "qemu-common.h"
 #include "i2c.h"
-#include <stdio.h>
 
 //#define DBF(X...) fprintf(stderr, "QEMU:ch7xxx:" X)
 #define DBF(X...)
 
-typedef struct ch7xxx_state_s {
+typedef struct {
   i2c_slave i2c;
   int reg;
   int wridx;
-} ch7xxx_state_t;
+} CH7xxxState;
 
 static int
 ch7xxx_rx(i2c_slave *i2c)
 {
-    ch7xxx_state_t *s = (ch7xxx_state_t *)i2c;
+    CH7xxxState *s = (CH7xxxState *)i2c;
     DBF("RX?\n");
 
     switch (s->reg) {
@@ -36,7 +35,7 @@ static int
 ch7xxx_tx(i2c_slave *i2c, uint8_t data)
 {
   DBF("TX: %d\n", data);
-  ch7xxx_state_t *s = (ch7xxx_state_t *)i2c;
+  CH7xxxState *s = (CH7xxxState *)i2c;
   if (s->wridx == 0) {
     s->reg = data;
     s->wridx++;
@@ -49,7 +48,7 @@ ch7xxx_tx(i2c_slave *i2c, uint8_t data)
 static void
 ch7xxx_event(i2c_slave *i2c, enum i2c_event event)
 {
-  ch7xxx_state_t *s = (ch7xxx_state_t *)i2c;
+  CH7xxxState *s = (CH7xxxState *)i2c;
   DBF("EV? %d\n", event);
   switch (event) {
   case I2C_START_RECV:
@@ -62,15 +61,41 @@ ch7xxx_event(i2c_slave *i2c, enum i2c_event event)
   }
 }
 
-i2c_slave *
-ch7xxx_init(i2c_bus *bus, int addr)
+static int ch7xxx_init(i2c_slave *i2c)
 {
-  ch7xxx_state_t *s = (ch7xxx_state_t *)
-    i2c_slave_init(bus, addr, sizeof(ch7xxx_state_t));
+    //~ StcPMUState *s = FROM_I2C_SLAVE(StcPMUState, i2c);
+  //~ StcPMUState *s = (StcPMUState *)
+    //~ i2c_slave_init(bus, addr, sizeof(StcPMUState));
 
-  s->i2c.event = ch7xxx_event;
-  s->i2c.recv = ch7xxx_rx;
-  s->i2c.send = ch7xxx_tx;
-
-  return &s->i2c;
+  return 0;
 }
+
+static const VMStateDescription vmstate_ch7xxx = {
+    .name = "ch7xxx",
+    .version_id = 0,
+    .minimum_version_id = 0,
+    .minimum_version_id_old = 0,
+    //~ .pre_save = menelaus_pre_save,
+    //~ .post_load = menelaus_post_load,
+    .fields      = (VMStateField []) {
+        VMSTATE_I2C_SLAVE(i2c, CH7xxxState),
+        VMSTATE_END_OF_LIST()
+    }
+};
+
+static I2CSlaveInfo ch7xxx_info = {
+    .qdev.name ="ch7xxx",
+    .qdev.size = sizeof(CH7xxxState),
+    .qdev.vmsd = &vmstate_ch7xxx,
+    .init = ch7xxx_init,
+    .event = ch7xxx_event,
+    .recv = ch7xxx_rx,
+    .send = ch7xxx_tx
+};
+
+static void ch7xxx_register_devices(void)
+{
+    i2c_register_slave(&ch7xxx_info);
+}
+
+device_init(ch7xxx_register_devices)
