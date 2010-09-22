687db4ed2ecd5fd74c94fbb420482823cca4ab7e
Anthony Liguori <aliguori@us.ibm.com>
2010-09-22 14:46:33 -0500

    block-verify: fix 32-bit build
    Reported-by: Peter Lemenkov <lemenkov@gmail.com>
    Signed-off-by: Anthony Liguori <aliguori@us.ibm.com>

--- block/blkverify.c
+++ block/blkverify.c
@@ -58,7 +58,7 @@ static void blkverify_err(BlkverifyAIOCB *acb, const char *fmt, ...)
     va_list ap;
 
     va_start(ap, fmt);
-    fprintf(stderr, "blkverify: %s sector_num=%ld nb_sectors=%d ",
+    fprintf(stderr, "blkverify: %s sector_num=%" PRId64 " nb_sectors=%d ",
             acb->is_write ? "write" : "read", acb->sector_num,
             acb->nb_sectors);
     vfprintf(stderr, fmt, ap);
