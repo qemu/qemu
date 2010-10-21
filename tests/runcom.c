e311248b6442827820bfc7340ea1401caee3a0f1
Paolo Bonzini <pbonzini@redhat.com>
2010-10-21 10:18:38 +0200

    make runcom compile on recent distributions
    Signed-off-by: Paolo Bonzini <pbonzini@redhat.com>
    Signed-off-by: Blue Swirl <blauwirbel@gmail.com>

--- tests/runcom.c
+++ tests/runcom.c
@@ -13,15 +13,12 @@
 #include <linux/unistd.h>
 #include <asm/vm86.h>
 
+extern int vm86 (unsigned long int subfunction,
+		 struct vm86plus_struct *info);
+
+#define VIF_MASK                0x00080000
+
 //#define SIGTEST
-
-#undef __syscall_return
-#define __syscall_return(type, res) \
-do { \
-	return (type) (res); \
-} while (0)
-
-_syscall2(int, vm86, int, func, struct vm86plus_struct *, v86)
 
 #define COM_BASE_ADDR    0x10100
 
