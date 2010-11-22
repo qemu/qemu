954773230484f5afeb675e9ff814c97e54e69e17
Michael S. Tsirkin <mst@redhat.com>
2010-11-22 19:52:19 +0200

    virtio-net: don't dma while vm is stopped
    DMA into memory while VM is stopped makes it
    hard to debug migration (consequitive saves
    result in different files).
    Fixing this completely is a large effort,
    this patch does this for virtio-net.
    
    Signed-off-by: Michael S. Tsirkin <mst@redhat.com>
    Tested-by: Jason Wang <jasowang@redhat.com>

--- hw/virtio-net.c
+++ hw/virtio-net.c
@@ -424,6 +424,9 @@ static void virtio_net_handle_rx(VirtIODevice *vdev, VirtQueue *vq)
 static int virtio_net_can_receive(VLANClientState *nc)
 {
     VirtIONet *n = DO_UPCAST(NICState, nc, nc)->opaque;
+    if (!n->vm_running) {
+        return 0;
+    }
 
     if (!virtio_queue_ready(n->rx_vq) ||
         !(n->vdev.status & VIRTIO_CONFIG_S_DRIVER_OK))
