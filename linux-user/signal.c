5f0b7c888b5e626d61e0969a6c0dbf4fcf0f522c
Peter Maydell <peter.maydell@linaro.org>
2010-11-24 15:20:03 +0000

    ARM: linux-user: Correct size of padding in target_ucontext_v2
    The padding in the target_ucontext_v2 is defined by the size of
    the target's sigset_t type, not the host's. (This bug only causes
    problems when we start using the uc_regspace[] array to expose
    VFP registers to userspace signal handlers.)
    
    Signed-off-by: Peter Maydell <peter.maydell@linaro.org>
    Signed-off-by: Riku Voipio <riku.voipio@nokia.com>

--- linux-user/signal.c
+++ linux-user/signal.c
@@ -1108,7 +1108,7 @@ struct target_ucontext_v2 {
     target_stack_t tuc_stack;
     struct target_sigcontext tuc_mcontext;
     target_sigset_t  tuc_sigmask;	/* mask last for extensibility */
-    char __unused[128 - sizeof(sigset_t)];
+    char __unused[128 - sizeof(target_sigset_t)];
     abi_ulong tuc_regspace[128] __attribute__((__aligned__(8)));
 };
 
