53016fa69c1fc7ed257272f36cb84b6623f73332
Peter Maydell <peter.maydell@linaro.org>
2010-12-01 19:44:38 +0000

    Remove unused spin_trylock() function
    Remove the spin_trylock() function, as it is not used anywhere,
    and is not even implemented if CONFIG_USE_NPTL is defined.
    
    Signed-off-by: Peter Maydell <peter.maydell@linaro.org>
    Signed-off-by: Blue Swirl <blauwirbel@gmail.com>

--- qemu-lock.h
+++ qemu-lock.h
@@ -224,11 +224,6 @@ static inline void spin_unlock(spinlock_t *lock)
 {
     resetlock(lock);
 }
-
-static inline int spin_trylock(spinlock_t *lock)
-{
-    return !testandset(lock);
-}
 #else
 static inline void spin_lock(spinlock_t *lock)
 {
@@ -236,11 +231,6 @@ static inline void spin_lock(spinlock_t *lock)
 
 static inline void spin_unlock(spinlock_t *lock)
 {
-}
-
-static inline int spin_trylock(spinlock_t *lock)
-{
-    return 1;
 }
 #endif
 
