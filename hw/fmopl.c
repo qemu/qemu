871c6abf458f2f9a9e6f7532ae614e1bbe19b5dd
Stefan Weil <weil@mail.berlios.de>
2011-01-15 17:52:09 +0100

    hw/fmopl: Fix buffer access out-of-bounds errors
    Index 75 is one too large for AR_TABLE[75], DR_TABLE[75].
    This error was reported by cppcheck.
    
    hw/fmopl.c:600: error: Buffer access out-of-bounds: OPL.AR_TABLE
    hw/fmopl.c:601: error: Buffer access out-of-bounds: OPL.DR_TABLE
    
    Fix this by limiting the access to the allowed range.
    MultiArcadeMachineEmulator has newer versions of fmopl,
    but using these requires more efforts.
    
    Cc: Blue Swirl <blauwirbel@gmail.com>
    Signed-off-by: Stefan Weil <weil@mail.berlios.de>

--- hw/fmopl.c
+++ hw/fmopl.c
@@ -45,6 +45,10 @@
 #define PI 3.14159265358979323846
 #endif
 
+#ifndef ARRAY_SIZE
+#define ARRAY_SIZE(x) (sizeof(x) / sizeof((x)[0]))
+#endif
+
 /* -------------------- for debug --------------------- */
 /* #define OPL_OUTPUT_LOG */
 #ifdef OPL_OUTPUT_LOG
@@ -595,7 +599,7 @@ static void init_timetables( FM_OPL *OPL , int ARRATE , int DRRATE )
 		OPL->AR_TABLE[i] = rate / ARRATE;
 		OPL->DR_TABLE[i] = rate / DRRATE;
 	}
-	for (i = 60;i < 76;i++)
+	for (i = 60; i < ARRAY_SIZE(OPL->AR_TABLE); i++)
 	{
 		OPL->AR_TABLE[i] = EG_AED-1;
 		OPL->DR_TABLE[i] = OPL->DR_TABLE[60];
