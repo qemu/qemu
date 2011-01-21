17268d54be2e15b0d8ccfc4102bc48c84299b027
Gerd Hoffmann <kraxel@redhat.com>
2011-01-21 10:32:03 +0100

    qxl: locking fix
    One spice worker call lacks the unlock/relock calls,
    which may lead to deadlocks, add them.
    
    Signed-off-by: Gerd Hoffmann <kraxel@redhat.com>

--- hw/qxl.c
+++ hw/qxl.c
@@ -866,7 +866,9 @@ static void qxl_destroy_primary(PCIQXLDevice *d)
     dprint(d, 1, "%s\n", __FUNCTION__);
 
     d->mode = QXL_MODE_UNDEFINED;
+    qemu_mutex_unlock_iothread();
     d->ssd.worker->destroy_primary_surface(d->ssd.worker, 0);
+    qemu_mutex_lock_iothread();
 }
 
 static void qxl_set_mode(PCIQXLDevice *d, int modenr, int loadvm)
