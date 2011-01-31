0fba9fd6c305fd1fbe950eb4136dc9edc36cb3fd
Dmitry Eremin-Solenikov <dbaryshkov@gmail.com>
2011-01-31 18:20:41 +0300

    sysbus: print number of irqs in dev_print
    Signed-off-by: Dmitry Eremin-Solenikov <dbaryshkov@gmail.com>
    Signed-off-by: Andrzej Zaborowski <andrew.zaborowski@intel.com>

--- hw/sysbus.c
+++ hw/sysbus.c
@@ -178,6 +178,7 @@ static void sysbus_dev_print(Monitor *mon, DeviceState *dev, int indent)
     SysBusDevice *s = sysbus_from_qdev(dev);
     int i;
 
+    monitor_printf(mon, "%*sirq %d\n", indent, "", s->num_irq);
     for (i = 0; i < s->num_mmio; i++) {
         monitor_printf(mon, "%*smmio " TARGET_FMT_plx "/" TARGET_FMT_plx "\n",
                        indent, "", s->mmio[i].addr, s->mmio[i].size);
