1d810aeb4eda548e8a875db9e364732b8765d894
M. Mohan Kumar <mohan@in.ibm.com>
2011-02-01 14:21:41 +0530

    virtio-9p: Bugfix to send correct iounit
    LCREATE function packs address of iounit in the pdu, fix that to send
    actual iounit itself.
    
    Signed-off-by: M. Mohan Kumar <mohan@in.ibm.com>
    Acked-by: Aneesh Kumar K.V <aneesh.kumar@linux.vnet.ibm.com>
    Signed-off-by: Venkateswararao Jujjuri <jvrao@linux.vnet.ibm.com>

--- hw/9pfs/virtio-9p.c
+++ hw/9pfs/virtio-9p.c
@@ -1771,7 +1771,7 @@ static void v9fs_post_lcreate(V9fsState *s, V9fsLcreateState *vs, int err)
         v9fs_string_copy(&vs->fidp->path, &vs->fullname);
         stat_to_qid(&vs->stbuf, &vs->qid);
         vs->offset += pdu_marshal(vs->pdu, vs->offset, "Qd", &vs->qid,
-                &vs->iounit);
+                vs->iounit);
         err = vs->offset;
     } else {
         vs->fidp->fid_type = P9_FID_NONE;
