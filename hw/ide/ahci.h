8f7dfd6bdae5a9cd0ec7f2533379350e21fff869
Alexander Graf <agraf@suse.de>
2011-02-01 15:51:27 +0100

    ahci: add license header in ahci.h
    Due to popular request, this patch adds a license header to ahci.h
    
    Signed-off-by: Alexander Graf <agraf@suse.de>
    Signed-off-by: Kevin Wolf <kwolf@redhat.com>
    (cherry picked from commit f83a40dcd7c38aef8cb4aa93e1f6f0e21c750992)

--- hw/ide/ahci.h
+++ hw/ide/ahci.h
@@ -1,3 +1,26 @@
+/*
+ * QEMU AHCI Emulation
+ *
+ * Copyright (c) 2010 qiaochong@loongson.cn
+ * Copyright (c) 2010 Roland Elek <elek.roland@gmail.com>
+ * Copyright (c) 2010 Sebastian Herbszt <herbszt@gmx.de>
+ * Copyright (c) 2010 Alexander Graf <agraf@suse.de>
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+
 #ifndef HW_IDE_AHCI_H
 #define HW_IDE_AHCI_H
 
