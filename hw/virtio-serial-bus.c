cb5281b1998c12b8d93d0c6a29aeae13934ceb1a
Amit Shah <amit.shah@redhat.com>
2011-02-04 14:24:18 +0530

    virtio-serial: Make sure virtqueue is ready before discarding data
    This can happen if a port gets unplugged before guest has chance to
    initialise vqs.
    
    Reported-by: Juan Quintela <quintela@redhat.com>
    Signed-off-by: Amit Shah <amit.shah@redhat.com>
    Signed-off-by: Anthony Liguori <aliguori@us.ibm.com>
    (cherry picked from commit 7185f9315bcf90e99b123370cf4d19b8c20afbd5)

--- hw/virtio-serial-bus.c
+++ hw/virtio-serial-bus.c
@@ -117,6 +117,9 @@ static void discard_vq_data(VirtQueue *vq, VirtIODevice *vdev)
 {
     VirtQueueElement elem;
 
+    if (!virtio_queue_ready(vq)) {
+        return;
+    }
     while (virtqueue_pop(vq, &elem)) {
         virtqueue_push(vq, &elem, 0);
     }
