8e045ac407029c004e31eb46b0586309d618fbbd
Jan Kiszka <jan.kiszka@siemens.com>
2011-02-07 12:19:22 +0100

    kvm: Remove unneeded memory slot reservation
    The number of slots and the location of private ones changed several
    times in KVM's early days. However, it's stable since 2.6.29 (our
    required baseline), and slots 8..11 are no longer reserved since then.
    So remove this unneeded restriction.
    
    Signed-off-by: Jan Kiszka <jan.kiszka@siemens.com>
    CC: Alex Williamson <alex.williamson@redhat.com>
    Signed-off-by: Marcelo Tosatti <mtosatti@redhat.com>

--- kvm-all.c
+++ kvm-all.c
@@ -91,10 +91,6 @@ static KVMSlot *kvm_alloc_slot(KVMState *s)
     int i;
 
     for (i = 0; i < ARRAY_SIZE(s->slots); i++) {
-        /* KVM private memory slots */
-        if (i >= 8 && i < 12) {
-            continue;
-        }
         if (s->slots[i].memory_size == 0) {
             return &s->slots[i];
         }
