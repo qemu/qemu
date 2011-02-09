cc2212c2f851291929becc3f4fd153d05ca4c54a
Peter Maydell <peter.maydell@linaro.org>
2011-02-09 15:42:33 +0000

    target-arm: Fix 32 bit signed saturating narrow
    The returned value when doing saturating signed 64->32 bit
    conversion of a negative number was incorrect due to a missing cast.
    
    Signed-off-by: Peter Maydell <peter.maydell@linaro.org>
    Signed-off-by: Aurelien Jarno <aurelien@aurel32.net>

--- target-arm/neon_helper.c
+++ target-arm/neon_helper.c
@@ -1209,7 +1209,7 @@ uint32_t HELPER(neon_narrow_sat_s32)(CPUState *env, uint64_t x)
 {
     if ((int64_t)x != (int32_t)x) {
         SET_QC();
-        return (x >> 63) ^ 0x7fffffff;
+        return ((int64_t)x >> 63) ^ 0x7fffffff;
     }
     return x;
 }
