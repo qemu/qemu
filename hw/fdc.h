17801c78fe01ddab7ae9092190d17cc77bd5ef66
Blue Swirl <blauwirbel@gmail.com>
2011-02-12 22:25:26 +0000

    fdc: make optional
    Ignore failure with fdc device creation.
    
    Signed-off-by: Blue Swirl <blauwirbel@gmail.com>

--- hw/fdc.h
+++ hw/fdc.h
@@ -11,7 +11,10 @@ static inline void fdctrl_init_isa(DriveInfo **fds)
 {
     ISADevice *dev;
 
-    dev = isa_create("isa-fdc");
+    dev = isa_try_create("isa-fdc");
+    if (!dev) {
+        return;
+    }
     if (fds[0]) {
         qdev_prop_set_drive_nofail(&dev->qdev, "driveA", fds[0]->bdrv);
     }
