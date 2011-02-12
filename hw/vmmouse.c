8668f61d20eac971d116ebbe8436b4ae963884a8
Blue Swirl <blauwirbel@gmail.com>
2011-02-12 17:44:11 +0000

    vmmouse: fix queue_size field initialization
    Initialize the field queue_size, dropped by
    91c9e09147ba1f3604a3d5d29b4de7702082a33f.
    
    Signed-off-by: Blue Swirl <blauwirbel@gmail.com>

--- hw/vmmouse.c
+++ hw/vmmouse.c
@@ -253,6 +253,7 @@ static void vmmouse_reset(DeviceState *d)
     VMMouseState *s = container_of(d, VMMouseState, dev.qdev);
 
     s->status = 0xffff;
+    s->queue_size = VMMOUSE_QUEUE_SIZE;
 }
 
 static int vmmouse_initfn(ISADevice *dev)
