74b121a007c52b435870def4b1f1e6c42042bf51
Stefan Weil <weil@mail.berlios.de>
2011-02-16 20:48:00 +0100

    vnc: Fix fatal crash with vnc reverse mode
    Reverse mode is unusable:
    
    	qemu -vnc localhost:5500,reverse
    
    crashes in vnc_refresh_server_surface because some pointers are NULL.
    
    Fix this by calling vnc_dpy_resize (which initializes these pointers)
    before calling vnc_refresh.
    
    Cc: Anthony Liguori <aliguori@us.ibm.com>
    Signed-off-by: Stefan Weil <weil@mail.berlios.de>
    Signed-off-by: Anthony Liguori <aliguori@us.ibm.com>

--- ui/vnc.c
+++ ui/vnc.c
@@ -2349,6 +2349,7 @@ static void vnc_init_timer(VncDisplay *vd)
     vd->timer_interval = VNC_REFRESH_INTERVAL_BASE;
     if (vd->timer == NULL && !QTAILQ_EMPTY(&vd->clients)) {
         vd->timer = qemu_new_timer(rt_clock, vnc_refresh, vd);
+        vnc_dpy_resize(vd->ds);
         vnc_refresh(vd);
     }
 }
