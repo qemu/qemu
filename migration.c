2350e13c93c28f717e2ba1b31560b49ac6f81d4d
Yoshiaki Tamura <tamura.yoshiaki@lab.ntt.co.jp>
2011-02-23 00:01:24 +0900

    migration: add error handling to migrate_fd_put_notify().
    Although migrate_fd_put_buffer() sets MIG_STATE_ERROR if it failed,
    since migrate_fd_put_notify() isn't checking error of underlying
    QEMUFile, those resources are kept open.  This patch checks it and
    calls migrate_fd_error() in case of error.
    
    Signed-off-by: Yoshiaki Tamura <tamura.yoshiaki@lab.ntt.co.jp>
    Signed-off-by: Juan Quintela <quintela@redhat.com>
    Reviewed-by: Anthony Liguori <aliguori@us.ibm.com>

--- migration.c
+++ migration.c
@@ -313,6 +313,9 @@ void migrate_fd_put_notify(void *opaque)
 
     qemu_set_fd_handler2(s->fd, NULL, NULL, NULL, NULL);
     qemu_file_put_notify(s->file);
+    if (qemu_file_has_error(s->file)) {
+        migrate_fd_error(s);
+    }
 }
 
 ssize_t migrate_fd_put_buffer(void *opaque, const void *data, size_t size)
@@ -329,9 +332,6 @@ ssize_t migrate_fd_put_buffer(void *opaque, const void *data, size_t size)
 
     if (ret == -EAGAIN) {
         qemu_set_fd_handler2(s->fd, NULL, NULL, migrate_fd_put_notify, s);
-    } else if (ret < 0) {
-        s->state = MIG_STATE_ERROR;
-        notifier_list_notify(&migration_state_notifiers, NULL);
     }
 
     return ret;
