b52d60ddb248ab6b4c063dab1215d7bdd2e89035
Stefan Weil <weil@mail.berlios.de>
2011-02-26 20:20:12 +0100

    w32: Add macro timersub to sys/time.h
    timersub is needed by tha latest vnc code.
    
    Signed-off-by: Stefan Weil <weil@mail.berlios.de>

---
+++ hosts/w32/include/sys/time.h
@@ -0,0 +1,24 @@
+/*
+ * Extensions of MinGW sys/time.h
+ *
+ * Copyright (C) 2011 Stefan Weil
+ *
+ * This work is licensed under the terms of the GNU LGPL, version 2.1 or later.
+ * See the COPYING.LIB file in the top-level directory.
+ *
+ */
+
+#include_next <sys/time.h>
+
+#ifndef timersub
+/* This is a copy from GNU C Library (GNU LGPL 2.1), sys/time.h. */
+# define timersub(a, b, result)                                               \
+  do {                                                                        \
+    (result)->tv_sec = (a)->tv_sec - (b)->tv_sec;                             \
+    (result)->tv_usec = (a)->tv_usec - (b)->tv_usec;                          \
+    if ((result)->tv_usec < 0) {                                              \
+      --(result)->tv_sec;                                                     \
+      (result)->tv_usec += 1000000;                                           \
+    }                                                                         \
+  } while (0)
+#endif
