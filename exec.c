fd28aa132362320f9f3a30b23f634bb14dee528e
Jan Kiszka <jan.kiszka@siemens.com>
2011-03-15 12:26:14 +0100

    s390: Detect invalid invocations of qemu_ram_free/remap
    This both detects invalid invocations of qemu_ram_free and
    qemu_ram_remap when mem_path is non-NULL and fixes a build error on
    s390 ("'area' may be used uninitialized in this function").
    
    Signed-off-by: Jan Kiszka <jan.kiszka@siemens.com>
    CC: Alexander Graf <agraf@suse.de>
    Signed-off-by: Marcelo Tosatti <mtosatti@redhat.com>

--- exec.c
+++ exec.c
@@ -2931,6 +2931,8 @@ void qemu_ram_free(ram_addr_t addr)
                 } else {
                     qemu_vfree(block->host);
                 }
+#else
+                abort();
 #endif
             } else {
 #if defined(TARGET_S390X) && defined(CONFIG_KVM)
@@ -2979,6 +2981,8 @@ void qemu_ram_remap(ram_addr_t addr, ram_addr_t length)
                         area = mmap(vaddr, length, PROT_READ | PROT_WRITE,
                                     flags, -1, 0);
                     }
+#else
+                    abort();
 #endif
                 } else {
 #if defined(TARGET_S390X) && defined(CONFIG_KVM)
