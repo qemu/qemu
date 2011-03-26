71d2b725e1f38162872b198992a61fa460978d77
Stefan Weil <weil@mail.berlios.de>
2011-03-26 21:06:56 +0100

    exec: Remove a type cast which is no longer needed
    All other type casts in calls of cpu_physical_memory_write are
    used by hardware emulations and will be fixed by separate patches.
    
    Cc: Blue Swirl <blauwirbel@gmail.com>
    Signed-off-by: Stefan Weil <weil@mail.berlios.de>
    Signed-off-by: Aurelien Jarno <aurelien@aurel32.net>

--- exec.c
+++ exec.c
@@ -4253,7 +4253,7 @@ void stw_phys(target_phys_addr_t addr, uint32_t val)
 void stq_phys(target_phys_addr_t addr, uint64_t val)
 {
     val = tswap64(val);
-    cpu_physical_memory_write(addr, (const uint8_t *)&val, 8);
+    cpu_physical_memory_write(addr, &val, 8);
 }
 
 /* virtual memory access for debug (includes writing to ROM) */
