c8f930c0eeb696d638f4d4bf654e955fa44ff40f
Peter Maydell <peter.maydell@linaro.org>
2011-04-04 12:09:22 +0100

    cpu-all.h: Remove unnecessary target-specific ifdef for CPU_QuadU
    CPU_QuadU isn't used on all targets, but there's no harm in defining the
    typedef anyway. It only needs to be guarded by CONFIG_SOFTFLOAT, because
    softfloat-native doesn't have a float128 type. This avoids the need for
    every new target which uses CPU_QuadU to add itself to an #ifdef in
    what ought to be target-agnostic code.
    
    Signed-off-by: Peter Maydell <peter.maydell@linaro.org>
    Signed-off-by: Aurelien Jarno <aurelien@aurel32.net>

--- cpu-all.h
+++ cpu-all.h
@@ -138,11 +138,10 @@ typedef union {
     uint64_t ll;
 } CPU_DoubleU;
 
-#if defined(TARGET_SPARC) || defined(TARGET_S390X)
+#if defined(CONFIG_SOFTFLOAT)
 typedef union {
     float128 q;
-#if defined(HOST_WORDS_BIGENDIAN) \
-    || (defined(__arm__) && !defined(__VFP_FP__) && !defined(CONFIG_SOFTFLOAT))
+#if defined(HOST_WORDS_BIGENDIAN)
     struct {
         uint32_t upmost;
         uint32_t upper;
