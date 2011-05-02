45fe15c25a5c9feea6e0f78434f5e9f632de9d94
Jan Kiszka <jan.kiszka@siemens.com>
2011-05-02 20:00:47 +0200

    MSI: Robust resource release
    msi_init may fail, so we need to check on uninit if the cap was
    actually installed. This also avoids that the users need to check.
    
    Signed-off-by: Jan Kiszka <jan.kiszka@siemens.com>
    Signed-off-by: Michael S. Tsirkin <mst@redhat.com>

--- hw/ide/ich.c
+++ hw/ide/ich.c
@@ -110,10 +110,7 @@ static int pci_ich9_uninit(PCIDevice *dev)
     struct AHCIPCIState *d;
     d = DO_UPCAST(struct AHCIPCIState, card, dev);
 
-    if (msi_enabled(dev)) {
-        msi_uninit(dev);
-    }
-
+    msi_uninit(dev);
     qemu_unregister_reset(ahci_reset, d);
     ahci_uninit(&d->ahci);
 
--- hw/intel-hda.c
+++ hw/intel-hda.c
@@ -1165,9 +1165,7 @@ static int intel_hda_exit(PCIDevice *pci)
 {
     IntelHDAState *d = DO_UPCAST(IntelHDAState, pci, pci);
 
-    if (d->msi) {
-        msi_uninit(&d->pci);
-    }
+    msi_uninit(&d->pci);
     cpu_unregister_io_memory(d->mmio_addr);
     return 0;
 }
