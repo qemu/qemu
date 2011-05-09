4d29b50a41810684ad34e44352a630eb1dd94b58
Jan Kiszka <jan.kiszka@siemens.com>
2011-05-09 17:48:19 +0200

    ahci: Fix crashes on duplicate BH registration
    If ahci_dma_set_inactive is called a while there is still a pending BH
    from a previous run, we will crash on the second run of
    ahci_check_cmd_bh as it overwrites AHCIDevice::check_bh. Avoid this
    broken and redundant duplicate registration.
    
    Signed-off-by: Jan Kiszka <jan.kiszka@siemens.com>
    Signed-off-by: Kevin Wolf <kwolf@redhat.com>

--- hw/ide/ahci.c
+++ hw/ide/ahci.c
@@ -1066,9 +1066,11 @@ static int ahci_dma_set_inactive(IDEDMA *dma)
 
     ad->dma_cb = NULL;
 
-    /* maybe we still have something to process, check later */
-    ad->check_bh = qemu_bh_new(ahci_check_cmd_bh, ad);
-    qemu_bh_schedule(ad->check_bh);
+    if (!ad->check_bh) {
+        /* maybe we still have something to process, check later */
+        ad->check_bh = qemu_bh_new(ahci_check_cmd_bh, ad);
+        qemu_bh_schedule(ad->check_bh);
+    }
 
     return 0;
 }
