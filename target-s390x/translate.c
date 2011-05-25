0b3bb28e077fb7430e223e051ebdc62908753022
Stefan Weil <weil@mail.berlios.de>
2011-05-25 20:47:44 +0200

    target-s390x: Add missing tcg_temp_free_i64()
    load_reg() needs a matching tcg_temp_free_i64().
    
    Signed-off-by: Stefan Weil <weil@mail.berlios.de>

--- target-s390x/translate.c
+++ target-s390x/translate.c
@@ -2367,6 +2367,7 @@ static void disas_a5(DisasContext *s, int op, int r1, int i2)
         tcg_gen_shri_i64(tmp2, tmp, 48);
         tcg_gen_trunc_i64_i32(tmp32, tmp2);
         set_cc_nz_u32(s, tmp32);
+        tcg_temp_free_i64(tmp);
         tcg_temp_free_i64(tmp2);
         tcg_temp_free_i32(tmp32);
         break;
