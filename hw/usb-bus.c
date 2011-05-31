290d26d2e7510fbbcb68a765695b5838af60b3f0
Hans de Goede <hdegoede@redhat.com>
2011-05-31 11:35:27 +0200

    usb-bus: Add knowledge of USB_SPEED_SUPER to usb_speed helper
    Signed-off-by: Gerd Hoffmann <kraxel@redhat.com>

--- hw/usb-bus.c
+++ hw/usb-bus.c
@@ -273,6 +273,7 @@ static const char *usb_speed(unsigned int speed)
         [ USB_SPEED_LOW  ] = "1.5",
         [ USB_SPEED_FULL ] = "12",
         [ USB_SPEED_HIGH ] = "480",
+        [ USB_SPEED_SUPER ] = "5000",
     };
     if (speed >= ARRAY_SIZE(txt))
         return "?";
