f97742d0d36810ea72a2bd40b3abb890589ea3b8
Alexandre Raymond <cerbere@gmail.com>
2011-06-06 23:34:10 -0400

    Darwin: Fix compilation warning regarding the deprecated daemon() function
    Changes since v1: create a wrapper function named qemu_daemon() in oslib-posix.c
    instead of putting the OS specific workaround in qemu-nbd.c directly.
    
    On OSX >= 10.5, daemon() is deprecated, resulting in the following warning:
    ----8<----
    qemu-nbd.c: In function ‘main’:
    qemu-nbd.c:371: warning: ‘daemon’ is deprecated (declared at /usr/include/stdlib.h:289)
    ----8<----
    
    The following trick, used in mDNSResponder, takes care of this warning:
    http://www.opensource.apple.com/source/mDNSResponder/mDNSResponder-258.18/mDNSPosix/PosixDaemon.c
    
    On OSX, it temporarily renames the daemon() function before including stdlib.h
    and declares it manually as an extern function. This way, the compiler does not
    see the declaration from stdlib.h and thus does not display the warning.
    
    Signed-off-by: Alexandre Raymond <cerbere@gmail.com>
    Cc: Blue Swirl <blauwirbel@gmail.com>
    Signed-off-by: Andreas Färber <andreas.faerber@web.de>

--- oslib-posix.c
+++ oslib-posix.c
@@ -26,10 +26,26 @@
  * THE SOFTWARE.
  */
 
+/* The following block of code temporarily renames the daemon() function so the
+   compiler does not see the warning associated with it in stdlib.h on OSX */
+#ifdef __APPLE__
+#define daemon qemu_fake_daemon_function
+#include <stdlib.h>
+#undef daemon
+extern int daemon(int, int);
+#endif
+
 #include "config-host.h"
 #include "sysemu.h"
 #include "trace.h"
 #include "qemu_socket.h"
+
+
+
+int qemu_daemon(int nochdir, int noclose)
+{
+    return daemon(nochdir, noclose);
+}
 
 void *qemu_oom_check(void *ptr)
 {
