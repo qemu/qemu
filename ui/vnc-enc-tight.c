f26e428da505709ec03b2ed2c9eb3db82b30bd7b
Roy Tam <roytam@gmail.com>
2011-06-18 13:13:39 +0800

    Fix MinGW compilation when --enable-vnc-jpeg is specified
    Fix conflicting types for 'INT32' in basetsd.h and jmorecfg.h by
    including qemu-common.h first.
    
    Signed-off-by: Roy Tam <roytam@gmail.com>
    Acked-by: Stefan Weil <weil@mail.berlios.de>
    Signed-off-by: Blue Swirl <blauwirbel@gmail.com>

--- ui/vnc-enc-tight.c
+++ ui/vnc-enc-tight.c
@@ -28,6 +28,11 @@
 
 #include "config-host.h"
 
+/* This needs to be before jpeglib.h line because of conflict with
+   INT32 definitions between jmorecfg.h (included by jpeglib.h) and
+   Win32 basetsd.h (included by windows.h). */
+#include "qemu-common.h"
+
 #ifdef CONFIG_VNC_PNG
 #include <png.h>
 #endif
@@ -35,8 +40,6 @@
 #include <stdio.h>
 #include <jpeglib.h>
 #endif
-
-#include "qemu-common.h"
 
 #include "bswap.h"
 #include "qint.h"
