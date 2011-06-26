136a0b5a66d9bdf4e2167964db2af9dbed967c85
Blue Swirl <blauwirbel@gmail.com>
2011-06-26 22:23:54 +0300

    TCG/PPC: use stack for TCG temps
    Use stack instead of temp_buf array in CPUState for TCG temps.
    
    Signed-off-by: Blue Swirl <blauwirbel@gmail.com>
    Signed-off-by: malc <av1474@comtv.ru>

--- tcg/ppc/tcg-target.c
+++ tcg/ppc/tcg-target.c
@@ -907,8 +907,13 @@ static void tcg_target_qemu_prologue (TCGContext *s)
         + LINKAGE_AREA_SIZE
         + TCG_STATIC_CALL_ARGS_SIZE
         + ARRAY_SIZE (tcg_target_callee_save_regs) * 4
+        + CPU_TEMP_BUF_NLONGS * sizeof(long)
         ;
     frame_size = (frame_size + 15) & ~15;
+
+    tcg_set_frame(s, TCG_REG_CALL_STACK, frame_size
+                  - CPU_TEMP_BUF_NLONGS * sizeof(long),
+                  CPU_TEMP_BUF_NLONGS * sizeof(long));
 
 #ifdef _CALL_AIX
     {
@@ -1914,6 +1919,4 @@ static void tcg_target_init(TCGContext *s)
 #endif
 
     tcg_add_target_add_op_defs(ppc_op_defs);
-    tcg_set_frame(s, TCG_AREG0, offsetof(CPUState, temp_buf),
-                  CPU_TEMP_BUF_NLONGS * sizeof(long));
 }
