1dfdcaa83f9ce34aded8bc0669e81753d94f1b7d
Edgar E. Iglesias <edgar.iglesias@gmail.com>
2011-06-28 20:57:09 +0200

    user: Fix -d debug logging for usermode emulation
    Signed-off-by: Edgar E. Iglesias <edgar.iglesias@gmail.com>

--- linux-user/main.c
+++ linux-user/main.c
@@ -3030,7 +3030,7 @@ int main(int argc, char **argv, char **envp)
         int mask;
         const CPULogItem *item;
 
-        mask = cpu_str_to_log_mask(r);
+        mask = cpu_str_to_log_mask(log_mask);
         if (!mask) {
             printf("Log items (comma separated):\n");
             for (item = cpu_log_items; item->mask != 0; item++) {
