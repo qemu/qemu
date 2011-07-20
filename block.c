a19712b0dbe43016fb17ec48bfff2f360225fe97
Markus Armbruster <armbru@redhat.com>
2011-07-20 18:23:36 +0200

    block: Reset device model callbacks on detach
    BlockDriverState members change_cb and change_opaque are initially
    null.  The device model may set them, with bdrv_set_change_cb().  If
    the device model gets detached (hot unplug), they're left dangling.
    Only safe because device hot unplug automatically destroys the
    BlockDriverState.  But that's a questionable feature, best not to rely
    on it.
    
    Signed-off-by: Markus Armbruster <armbru@redhat.com>
    Signed-off-by: Kevin Wolf <kwolf@redhat.com>

--- block.c
+++ block.c
@@ -730,6 +730,8 @@ void bdrv_detach(BlockDriverState *bs, DeviceState *qdev)
 {
     assert(bs->peer == qdev);
     bs->peer = NULL;
+    bs->change_cb = NULL;
+    bs->change_opaque = NULL;
 }
 
 DeviceState *bdrv_get_attached(BlockDriverState *bs)
