fd2a9d2fc719d53caeb37c28440a0e9d34c1b563
Avi Kivity <avi@redhat.com>
2011-07-20 12:14:06 +0300

    xen: fix xen-mapcache build on non-Xen capable targets
    Signed-off-by: Avi Kivity <avi@redhat.com>
    Signed-off-by: Blue Swirl <blauwirbel@gmail.com>

--- xen-mapcache.h
+++ xen-mapcache.h
@@ -9,11 +9,43 @@
 #ifndef XEN_MAPCACHE_H
 #define XEN_MAPCACHE_H
 
+#include <stdlib.h>
+
+#ifdef CONFIG_XEN
+
 void xen_map_cache_init(void);
 uint8_t *xen_map_cache(target_phys_addr_t phys_addr, target_phys_addr_t size,
                        uint8_t lock);
 ram_addr_t xen_ram_addr_from_mapcache(void *ptr);
 void xen_invalidate_map_cache_entry(uint8_t *buffer);
 void xen_invalidate_map_cache(void);
+
+#else
+
+static inline void xen_map_cache_init(void)
+{
+}
+
+static inline uint8_t *xen_map_cache(target_phys_addr_t phys_addr,
+                                     target_phys_addr_t size,
+                                     uint8_t lock)
+{
+    abort();
+}
+
+static inline ram_addr_t xen_ram_addr_from_mapcache(void *ptr)
+{
+    abort();
+}
+
+static inline void xen_invalidate_map_cache_entry(uint8_t *buffer)
+{
+}
+
+static inline void xen_invalidate_map_cache(void)
+{
+}
+
+#endif
 
 #endif /* !XEN_MAPCACHE_H */
