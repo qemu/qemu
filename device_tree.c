80ad781643b68c91316687051965e12021abeab6
Alexander Graf <agraf@suse.de>
2011-07-22 13:55:37 +0200

    device tree: add add_subnode command
    We want to be able to create subnodes in our device tree, so export it through
    the qemu device tree abstraction framework.
    
    Signed-off-by: Alexander Graf <agraf@suse.de>

--- device_tree.c
+++ device_tree.c
@@ -118,3 +118,27 @@ int qemu_devtree_nop_node(void *fdt, const char *node_path)
 
     return fdt_nop_node(fdt, offset);
 }
+
+int qemu_devtree_add_subnode(void *fdt, const char *name)
+{
+    int offset;
+    char *dupname = g_strdup(name);
+    char *basename = strrchr(dupname, '/');
+    int retval;
+
+    if (!basename) {
+        return -1;
+    }
+
+    basename[0] = '\0';
+    basename++;
+
+    offset = fdt_path_offset(fdt, dupname);
+    if (offset < 0) {
+        return offset;
+    }
+
+    retval = fdt_add_subnode(fdt, offset, basename);
+    g_free(dupname);
+    return retval;
+}
