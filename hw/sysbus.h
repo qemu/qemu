d40b2af8baca0a400ff7a3f75199f083ff0babff
Avi Kivity <avi@redhat.com>
2011-07-25 15:02:17 +0300

    sysbus: add sysbus_add_memory_overlap()
    Signed-off-by: Avi Kivity <avi@redhat.com>

--- hw/sysbus.h
+++ hw/sysbus.h
@@ -60,6 +60,8 @@ void sysbus_connect_irq(SysBusDevice *dev, int n, qemu_irq irq);
 void sysbus_mmio_map(SysBusDevice *dev, int n, target_phys_addr_t addr);
 void sysbus_add_memory(SysBusDevice *dev, target_phys_addr_t addr,
                        MemoryRegion *mem);
+void sysbus_add_memory_overlap(SysBusDevice *dev, target_phys_addr_t addr,
+                               MemoryRegion *mem, unsigned priority);
 void sysbus_del_memory(SysBusDevice *dev, MemoryRegion *mem);
 void sysbus_add_io(SysBusDevice *dev, target_phys_addr_t addr,
                    MemoryRegion *mem);
