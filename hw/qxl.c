be48e9951214a78ebef025cefecfc77be3d1c13c
Yonit Halperin <yhalperi@redhat.com>
2011-08-09 16:12:40 +0300

    qxl: allowing the command rings to be not empty when spice worker is stopped RHBZ #728984
    same as 8927cfbba232e28304734f7afd463c1b84134031, but for qxl_check_state, that was
    triggered by qxl_pre_load (which calls qxl_hard_reset, which calls qxl_soft_reset),
    and caused the migration target to crash.
    
    Signed-off-by: Gerd Hoffmann <kraxel@redhat.com>

--- hw/qxl.c
+++ hw/qxl.c
@@ -821,17 +821,15 @@ static void qxl_check_state(PCIQXLDevice *d)
 {
     QXLRam *ram = d->ram;
 
-    assert(SPICE_RING_IS_EMPTY(&ram->cmd_ring));
-    assert(SPICE_RING_IS_EMPTY(&ram->cursor_ring));
+    assert(!d->ssd.running || SPICE_RING_IS_EMPTY(&ram->cmd_ring));
+    assert(!d->ssd.running || SPICE_RING_IS_EMPTY(&ram->cursor_ring));
 }
 
 static void qxl_reset_state(PCIQXLDevice *d)
 {
-    QXLRam *ram = d->ram;
     QXLRom *rom = d->rom;
 
-    assert(!d->ssd.running || SPICE_RING_IS_EMPTY(&ram->cmd_ring));
-    assert(!d->ssd.running || SPICE_RING_IS_EMPTY(&ram->cursor_ring));
+    qxl_check_state(d);
     d->shadow_rom.update_id = cpu_to_le32(0);
     *rom = d->shadow_rom;
     qxl_rom_set_dirty(d);
