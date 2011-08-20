02fa69b6e8de1742c60ce3a050ed397102122e6b
Blue Swirl <blauwirbel@gmail.com>
2011-08-20 09:21:42 +0000

    scsi-disk: fix DPRINTF
    The variable 'status' does not exist anymore, adjust DPRINTF
    accordingly.
    
    Signed-off-by: Blue Swirl <blauwirbel@gmail.com>

--- hw/scsi-disk.c
+++ hw/scsi-disk.c
@@ -86,8 +86,8 @@ static void scsi_free_request(SCSIRequest *req)
 /* Helper function for command completion with sense.  */
 static void scsi_check_condition(SCSIDiskReq *r, SCSISense sense)
 {
-    DPRINTF("Command complete tag=0x%x status=%d sense=%d/%d/%d\n",
-            r->req.tag, status, sense.key, sense.asc, sense.ascq);
+    DPRINTF("Command complete tag=0x%x sense=%d/%d/%d\n",
+            r->req.tag, sense.key, sense.asc, sense.ascq);
     scsi_req_build_sense(&r->req, sense);
     scsi_req_complete(&r->req, CHECK_CONDITION);
 }
