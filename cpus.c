eae74cf906942999bf70e94f034f95c7f831ec63
Jan Kiszka <jan.kiszka@siemens.com>
2011-08-22 17:46:03 +0200

    Do not kick vcpus in TCG mode
    In TCG mode, iothread and vcpus run in lock-step. So it's pointless to
    send a signal from qemu_cpu_kick to the vcpu thread - if we got here,
    the receiver already left the vcpu loop.
    
    Signed-off-by: Jan Kiszka <jan.kiszka@siemens.com>
    Signed-off-by: Anthony Liguori <aliguori@us.ibm.com>

--- cpus.c
+++ cpus.c
@@ -869,7 +869,7 @@ void qemu_cpu_kick(void *_env)
     CPUState *env = _env;
 
     qemu_cond_broadcast(env->halt_cond);
-    if (!env->thread_kicked) {
+    if (kvm_enabled() && !env->thread_kicked) {
         qemu_cpu_kick_thread(env);
         env->thread_kicked = true;
     }
