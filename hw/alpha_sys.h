80bb2ff770e3e6ff2922a7b60978394571ec64cb
Richard Henderson <rth@twiddle.net>
2011-08-25 11:38:59 -1000

    target-alpha: Add CLIPPER emulation.
    This is a DP264 variant, SMP capable, no unusual hardware present.
    
    The emulation does not currently include any PCI IOMMU code.
    Hopefully the generic support for that can be merged to HEAD soon.
    
    Signed-off-by: Richard Henderson <rth@twiddle.net>

---
+++ hw/alpha_sys.h
@@ -0,0 +1,24 @@
+/* Alpha cores and system support chips.  */
+
+#ifndef HW_ALPHA_H
+#define HW_ALPHA_H 1
+
+#include "pci.h"
+#include "pci_host.h"
+#include "ide.h"
+#include "net.h"
+#include "pc.h"
+#include "usb-ohci.h"
+#include "irq.h"
+
+
+PCIBus *typhoon_init(ram_addr_t, qemu_irq *, CPUState *[4], pci_map_irq_fn);
+
+/* alpha_pci.c.  */
+extern const MemoryRegionOps alpha_pci_bw_io_ops;
+extern const MemoryRegionOps alpha_pci_conf1_ops;
+extern const MemoryRegionOps alpha_pci_iack_ops;
+
+void alpha_pci_vga_setup(PCIBus *pci_bus);
+
+#endif
