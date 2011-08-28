9c8255e12422771cdecbb3bf538f0732be629e2a
Peter Maydell <peter.maydell@linaro.org>
2011-08-28 16:22:18 +0000

    omap_gpmc: GPMC_IRQSTATUS is write-one-to-clear
    Fix a bug in the handling of writes to GPMC_IRQSTATUS:
    it behaves as "write one to clear, writing zero is ignored".
    
    Signed-off-by: Peter Maydell <peter.maydell@linaro.org>

--- hw/omap_gpmc.c
+++ hw/omap_gpmc.c
@@ -284,7 +284,7 @@ static void omap_gpmc_write(void *opaque, target_phys_addr_t addr,
         break;
 
     case 0x018:	/* GPMC_IRQSTATUS */
-        s->irqen = ~value;
+        s->irqen &= ~value;
         omap_gpmc_int_update(s);
         break;
 
