7f6f0ae5b95adfa76e10eabe2c34424a955fd10c
Stefan Weil <weil@mail.berlios.de>
2011-09-17 22:00:30 +0200

    tcg: Add some assertions
    Signed-off-by: Stefan Weil <weil@mail.berlios.de>
    Signed-off-by: Blue Swirl <blauwirbel@gmail.com>

--- tcg/tcg.c
+++ tcg/tcg.c
@@ -794,7 +794,9 @@ static char *tcg_get_arg_str_idx(TCGContext *s, char *buf, int buf_size,
 {
     TCGTemp *ts;
 
+    assert(idx >= 0 && idx < s->nb_temps);
     ts = &s->temps[idx];
+    assert(ts);
     if (idx < s->nb_globals) {
         pstrcpy(buf, buf_size, ts->name);
     } else {
