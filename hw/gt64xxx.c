f6c610653799749cdb5bcd4799ca3ef61b974eb7
Antony Pavlov <antonynpavlov@gmail.com>
2011-09-26 09:02:38 +0400

    gt64xxx.c: remove reference to non-existing ISD_handle field
    The commit fc2bf44972349b078d8310466c3866615500e67f
    removed ISD_handle field from struct GT64120State,
    so remove the field from DPRINTF too.
    
    Signed-off-by: Antony Pavlov <antonynpavlov@gmail.com>
    Signed-off-by: Stefan Hajnoczi <stefanha@linux.vnet.ibm.com>

--- hw/gt64xxx.c
+++ hw/gt64xxx.c
@@ -275,8 +275,9 @@ static void gt64120_isd_mapping(GT64120State *s)
     check_reserved_space(&start, &length);
     length = 0x1000;
     /* Map new address */
-    DPRINTF("ISD: "TARGET_FMT_plx"@"TARGET_FMT_plx" -> "TARGET_FMT_plx"@"TARGET_FMT_plx", %x\n", s->ISD_length, s->ISD_start,
-            length, start, s->ISD_handle);
+    DPRINTF("ISD: "TARGET_FMT_plx"@"TARGET_FMT_plx
+        " -> "TARGET_FMT_plx"@"TARGET_FMT_plx"\n",
+        s->ISD_length, s->ISD_start, length, start);
     s->ISD_start = start;
     s->ISD_length = length;
     memory_region_add_subregion(get_system_memory(), s->ISD_start, &s->ISD_mem);
