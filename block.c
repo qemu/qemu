a652d16025030013116c4d63883b9e1ec08a4359
Stefan Hajnoczi <stefanha@linux.vnet.ibm.com>
2011-10-05 17:17:02 +0100

    block: directly invoke .bdrv_aio_*() in bdrv_co_io_em()
    We will unify block layer request processing across sync, aio, and
    coroutines and this means a .bdrv_co_*() emulation function should not
    call back into the public interface.  There's no need here, just call
    .bdrv_aio_*() directly.
    
    The gory details: bdrv_co_io_em() cannot call back into the public
    bdrv_aio_*() interface since that will be handled using coroutines,
    which causes us to call into bdrv_co_io_em() again in an infinite loop
    :).
    
    Signed-off-by: Stefan Hajnoczi <stefanha@linux.vnet.ibm.com>
    Signed-off-by: Kevin Wolf <kwolf@redhat.com>

--- block.c
+++ block.c
@@ -3011,11 +3011,11 @@ static int coroutine_fn bdrv_co_io_em(BlockDriverState *bs, int64_t sector_num,
     BlockDriverAIOCB *acb;
 
     if (is_write) {
-        acb = bdrv_aio_writev(bs, sector_num, iov, nb_sectors,
-                              bdrv_co_io_em_complete, &co);
+        acb = bs->drv->bdrv_aio_writev(bs, sector_num, iov, nb_sectors,
+                                       bdrv_co_io_em_complete, &co);
     } else {
-        acb = bdrv_aio_readv(bs, sector_num, iov, nb_sectors,
-                             bdrv_co_io_em_complete, &co);
+        acb = bs->drv->bdrv_aio_readv(bs, sector_num, iov, nb_sectors,
+                                      bdrv_co_io_em_complete, &co);
     }
 
     trace_bdrv_co_io_em(bs, sector_num, nb_sectors, is_write, acb);
