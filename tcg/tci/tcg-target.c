ee398805033aef11d9dcfdce36493a6d12faa06e
Stefan Weil <sw@weilnetz.de>
2011-10-17 20:54:35 +0200

    tci: Rename tcg/bytecode to tcg/tci
    Also changes the license for tcg/tci/tcg-target.[ch].
    
    Signed-off-by: Stefan Weil <sw@weilnetz.de>

--- tcg/tci/tcg-target.c
+++ tcg/tci/tcg-target.c
@@ -3,18 +3,23 @@
  *
  * Copyright (c) 2009, 2011 Stefan Weil
  *
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 2 of the License, or
- * (at your option) any later version.
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
  *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
  *
- * You should have received a copy of the GNU General Public License
- * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
+ * THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
  */
 
 /* TODO list:
@@ -777,10 +782,11 @@ static void tcg_out_movi(TCGContext *s, TCGType type,
 static void tcg_out_op(TCGContext *s, TCGOpcode opc, const TCGArg *args,
                        const int *const_args)
 {
-    tcg_disas3(s, opc, args);
     uint8_t *old_code_ptr = s->code_ptr;
 
+    tcg_disas3(s, opc, args);
     tcg_out_op_t(s, opc);
+
     switch (opc) {
     case INDEX_op_exit_tb:
         tcg_out64(s, args[0]);
@@ -839,11 +845,6 @@ static void tcg_out_op(TCGContext *s, TCGOpcode opc, const TCGArg *args,
     case INDEX_op_ld16u_i32:
     case INDEX_op_ld16s_i32:
     case INDEX_op_ld_i32:
-        tcg_out_r(s, args[0]);
-        tcg_out_r(s, args[1]);
-        assert(args[2] == (uint32_t)args[2]);
-        tcg_out32(s, args[2]);
-        break;
     case INDEX_op_st8_i32:
     case INDEX_op_st16_i32:
     case INDEX_op_st_i32:
