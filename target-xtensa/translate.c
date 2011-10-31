53a72dfda584c8d54df61f7c1aea9f9ff86b138c
Max Filippov <jcmvbkbc@gmail.com>
2011-11-01 01:51:02 +0400

    target-xtensa: mask out undefined bits of WINDOWSTART SR
    According to ISA, table 5-156, bits 32:NAREG/4 of the WINDOWSTART SR
    must be zero.
    
    Signed-off-by: Max Filippov <jcmvbkbc@gmail.com>

--- target-xtensa/translate.c
+++ target-xtensa/translate.c
@@ -466,7 +466,7 @@ static void gen_wsr_windowbase(DisasContext *dc, uint32_t sr, TCGv_i32 v)
 
 static void gen_wsr_windowstart(DisasContext *dc, uint32_t sr, TCGv_i32 v)
 {
-    tcg_gen_mov_i32(cpu_SR[sr], v);
+    tcg_gen_andi_i32(cpu_SR[sr], v, (1 << dc->config->nareg / 4) - 1);
     reset_used_window(dc);
 }
 
