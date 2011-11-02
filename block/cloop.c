756f51e408febecdaff041f096527b820e857762
Dong Xu Wang <wdongxu@linux.vnet.ibm.com>
2011-11-02 16:36:21 +0800

    block/cloop: Use g_free instead of free
    Fix mismatching allocation and deallocation: g_free should be used to pair with
    g_malloc.
    
    Reviewed-by: Andreas Färber <afaerber@suse.de>
    Reviewed_by: Ray Wang <raywang@linux.vnet.ibm.com>
    Signed-off-by: Dong Xu Wang <wdongxu@linux.vnet.ibm.com>
    Signed-off-by: Kevin Wolf <kwolf@redhat.com>

--- block/cloop.c
+++ block/cloop.c
@@ -171,10 +171,10 @@ static void cloop_close(BlockDriverState *bs)
 {
     BDRVCloopState *s = bs->opaque;
     if (s->n_blocks > 0) {
-        free(s->offsets);
+        g_free(s->offsets);
     }
-    free(s->compressed_block);
-    free(s->uncompressed_block);
+    g_free(s->compressed_block);
+    g_free(s->uncompressed_block);
     inflateEnd(&s->zstream);
 }
 
