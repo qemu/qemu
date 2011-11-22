6ac5f3881fd9880f55e30c5f5bd9984d9d47fd62
Kevin Wolf <kwolf@redhat.com>
2011-11-22 16:57:34 +0100

    vdi: Fix memory leak
    The block map is allocated in vdi_open, but was never freed.
    
    Signed-off-by: Kevin Wolf <kwolf@redhat.com>

--- block/vdi.c
+++ block/vdi.c
@@ -949,6 +949,9 @@ static int vdi_create(const char *filename, QEMUOptionParameter *options)
 static void vdi_close(BlockDriverState *bs)
 {
     BDRVVdiState *s = bs->opaque;
+
+    g_free(s->bmap);
+
     migrate_del_blocker(s->migration_blocker);
     error_free(s->migration_blocker);
 }
