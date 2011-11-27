a1fd24af4d4d5f96283ba48e7b1e83ffe3b72273
Anthony Liguori <aliguori@us.ibm.com>
2011-11-27 11:13:01 -0600

    Revert "i386: derive '-cpu host' from KVM_GET_SUPPORTED_CPUID"
    This reverts commit 66e3dd9282141b5ae75637c9676002cf3ceeb988.
    
    From Avi,
    
     "Anthony, I think we should revert that commit and refactor cpuid for
      1.1.  The logic is spread over too many places which makes it hard to
      reason about."
    
    Signed-off-by: Anthony Liguori <aliguori@us.ibm.com>

--- target-i386/cpuid.c
+++ target-i386/cpuid.c
@@ -107,14 +107,33 @@ void host_cpuid(uint32_t function, uint32_t count,
                 uint32_t *eax, uint32_t *ebx, uint32_t *ecx, uint32_t *edx)
 {
 #if defined(CONFIG_KVM)
+    uint32_t vec[4];
+
+#ifdef __x86_64__
+    asm volatile("cpuid"
+                 : "=a"(vec[0]), "=b"(vec[1]),
+                   "=c"(vec[2]), "=d"(vec[3])
+                 : "0"(function), "c"(count) : "cc");
+#else
+    asm volatile("pusha \n\t"
+                 "cpuid \n\t"
+                 "mov %%eax, 0(%2) \n\t"
+                 "mov %%ebx, 4(%2) \n\t"
+                 "mov %%ecx, 8(%2) \n\t"
+                 "mov %%edx, 12(%2) \n\t"
+                 "popa"
+                 : : "a"(function), "c"(count), "S"(vec)
+                 : "memory", "cc");
+#endif
+
     if (eax)
-        *eax = kvm_arch_get_supported_cpuid(kvm_state, function, count, R_EAX);
+        *eax = vec[0];
     if (ebx)
-        *ebx = kvm_arch_get_supported_cpuid(kvm_state, function, count, R_EBX);
+        *ebx = vec[1];
     if (ecx)
-        *ecx = kvm_arch_get_supported_cpuid(kvm_state, function, count, R_ECX);
+        *ecx = vec[2];
     if (edx)
-        *edx = kvm_arch_get_supported_cpuid(kvm_state, function, count, R_EDX);
+        *edx = vec[3];
 #endif
 }
 
