5d32da32f549a7eecc31dd863b085c340ee04ccd
Stefan Weil <sw@weilnetz.de>
2011-12-07 22:34:00 +0100

    bast: serial ports (WiP)
    Bast emulation is still not working since latest
    QEMU memory API changes.
    
    Signed-off-by: Stefan Weil <sw@weilnetz.de>

--- hw/bast.c
+++ hw/bast.c
@@ -10,7 +10,10 @@
  * TODO:
  * * Undefined r/w at address 0x118002f9 (serial i/o?).
  * * Undefined r/w at address 0x118003f9 (serial i/o?).
- * * Undefined r/w at address 0x29000000 ff.
+ * * Undefined r/w at address 0x29000000 ff (ax88796).
+ * * Undefined r/w at address 0x4b000000 ff.
+ * * Undefined r/w at address 0x55000000 ff (iis).
+ * * eth1 is 10 Mbps half duplex only.
  */
 
 #include "hw.h"
@@ -23,7 +26,9 @@
 #include "smbus.h"
 #include "devices.h"
 #include "boards.h"
+#include "pc.h"                 /* serial_isa_init */
 #include "dma.h"                /* QEMUSGList (in ide/internal.h) */
+#include "exec-memory.h"        /* get_system_memory */
 #include "ide/internal.h"       /* ide_cmd_write, ... */
 
 #include "s3c2410x.h"
@@ -36,8 +41,8 @@ static int bigendian = 0;
 
 typedef struct {
     S3CState *soc;
-    unsigned char cpld_ctrl2;
     DeviceState *nand[4];
+    uint8_t cpld_ctrl2;
 } STCBState;
 
 /* Useful defines */
@@ -46,30 +51,24 @@ typedef struct {
 #define BAST_NOR_SIZE    (2 * MiB)
 #define BAST_BOARD_ID 331
 
-#define BAST_CS1_CPLD_BASE ((target_phys_addr_t)(CPU_S3C2410X_CS1 | (0xc << 23)))
-#define BAST_CS5_CPLD_BASE ((target_phys_addr_t)(CPU_S3C2410X_CS5 | (0xc << 23)))
+#define BAST_CS1_CPLD_BASE (CPU_S3C2410X_CS1 | (0xc << 23))
+#define BAST_CS5_CPLD_BASE (CPU_S3C2410X_CS5 | (0xc << 23))
 #define BAST_CPLD_SIZE (4<<23)
-
-/* GPIO */
-#define CPU_S3C2410X_GPIO_BASE (CPU_S3C2410X_PERIPHERAL + 0x16000000)
-
-/* S3C2410 SoC IDs */
-#define CPU_S3C2410X_IDENT_S3C2410X 0x32410000
-#define CPU_S3C2410X_IDENT_S3C2410A 0x32410002
 
 static uint32_t cpld_read(void *opaque, target_phys_addr_t address)
 {
-    STCBState *stcb = (STCBState *)opaque;
+    STCBState *stcb = opaque;
     int reg = (address >> 23) & 0xf;
-    if (reg == 0xc)
+    if (reg == 0xc) {
         return stcb->cpld_ctrl2;
+    }
     return 0;
 }
 
 static void cpld_write(void *opaque, target_phys_addr_t address,
                        uint32_t value)
 {
-    STCBState *stcb = (STCBState *)opaque;
+    STCBState *stcb = opaque;
     int reg = (address >> 23) & 0xf;
     if (reg == 0xc) {
         stcb->cpld_ctrl2 = value;
@@ -218,6 +217,156 @@ static void stcb_register_ide(STCBState *stcb)
     cpu_register_physical_memory(BAST_IDE_SEC_FAST_BYTE, 0x1000000, ide1_mem);
 }
 
+#define BAST_PA_ASIXNET 0x01000000
+#define BAST_PA_SUPERIO 0x01800000
+
+#define SERIAL_BASE     (CPU_S3C2410X_CS2 + BAST_PA_SUPERIO)
+#define SERIAL_CLK      1843200
+
+#define ASIXNET_BASE    (CPU_S3C2410X_CS5 + BAST_PA_ASIXNET)
+#define ASIXNET_SIZE    (0x400)
+#define AX88796_BASE    (CPU_S3C2410X_CS5 + BAST_PA_ASIXNET + (0x18 * 0x20))
+#define AX88796_SIZE    (3 * 0x20)
+
+#define DM9000_BASE     (0x2d000000)
+#define DM9000_IRQ      10
+
+#define logout(fmt, ...) \
+    fprintf(stderr, "AX88796\t%-24s" fmt, __func__, ##__VA_ARGS__)
+
+typedef struct {
+    SysBusDevice busdev;
+    MemoryRegion mmio;
+    NICState *nic;
+    NICConf conf;
+} AX88796State;
+
+static uint64_t ax88796_read(void *opaque, target_phys_addr_t offset,
+                             unsigned size)
+{
+    //~ AX88796State *s = opaque;
+    uint32_t value = 0;
+
+    switch (offset) {
+        case 0x0000:
+        case 0x000d:
+        case 0x0020:
+        case 0x0040:
+        case 0x0060:
+        case 0x0080:
+        case 0x00a0:
+        case 0x00c0:
+        case 0x00e0:
+        case 0x02e0:
+        case 0x03e0:
+            ;
+            //~ return 0; // FIXME
+    }
+
+    logout("0x" TARGET_FMT_plx " 0x%08x\n", offset, value);
+    return value;
+}
+
+static void ax88796_write(void *opaque, target_phys_addr_t offset,
+                          uint64_t value, unsigned size)
+{
+    //~ AX88796State *s = opaque;
+    switch (offset) {
+        case 0x0000:
+        case 0x000d:
+        case 0x0020:
+        case 0x0040:
+        case 0x0060:
+        case 0x0080:
+        case 0x00a0:
+        case 0x00c0:
+        case 0x00e0:
+        case 0x02e0:
+        case 0x03e0:
+            ;
+            //~ return; // FIXME
+    }
+    logout("0x" TARGET_FMT_plx " 0x%08" PRIx64 "\n", offset, value);
+}
+
+static const MemoryRegionOps ax88796_ops = {
+    .read = ax88796_read,
+    .write = ax88796_write,
+    .endianness = DEVICE_NATIVE_ENDIAN,
+    .valid = {
+        .min_access_size = 4,
+        .max_access_size = 4
+    }
+};
+
+static int ax88796_init(SysBusDevice *dev)
+{
+    AX88796State *s = FROM_SYSBUS(AX88796State, dev);
+
+    logout("\n");
+
+    memory_region_init_io(&s->mmio, &ax88796_ops, s, "ax88796", ASIXNET_SIZE);
+    //~ sysbus_init_mmio(dev, AX88796_SIZE, iomemtype);
+    sysbus_init_mmio(dev, &s->mmio);
+    //~ sysbus_init_irq(dev, &s->irq);
+    //~ ax88796_reset(s);
+#if 0
+    isa_ne2000_init(ne2000_io[i], ne2000_irq[i], &nd_table[i]);
+    ISANE2000State *isa = DO_UPCAST(ISANE2000State, dev, dev);
+    NE2000State *s = &isa->ne2000;
+
+    register_ioport_write(isa->iobase, 16, 1, ne2000_ioport_write, s);
+    register_ioport_read(isa->iobase, 16, 1, ne2000_ioport_read, s);
+
+    register_ioport_write(isa->iobase + 0x10, 1, 1, ne2000_asic_ioport_write, s);
+    register_ioport_read(isa->iobase + 0x10, 1, 1, ne2000_asic_ioport_read, s);
+    register_ioport_write(isa->iobase + 0x10, 2, 2, ne2000_asic_ioport_write, s);
+    register_ioport_read(isa->iobase + 0x10, 2, 2, ne2000_asic_ioport_read, s);
+
+    register_ioport_write(isa->iobase + 0x1f, 1, 1, ne2000_reset_ioport_write, s);
+    register_ioport_read(isa->iobase + 0x1f, 1, 1, ne2000_reset_ioport_read, s);
+
+    isa_init_irq(dev, &s->irq, isa->isairq);
+
+    qemu_macaddr_default_if_unset(&s->c.macaddr);
+    ne2000_reset(s);
+
+    s->nic = qemu_new_nic(&net_ne2000_isa_info, &s->c,
+                          dev->qdev.info->name, dev->qdev.id, s);
+    qemu_format_nic_info_str(&s->nic->nc, s->c.macaddr.a);
+#endif
+    return 0;
+}
+
+static const VMStateDescription ax88796_vmsd = {
+    .name = "ax88796",
+    .version_id = 1,
+    .minimum_version_id = 1,
+    .minimum_version_id_old = 1,
+    .fields = (VMStateField[]) {
+        VMSTATE_END_OF_LIST()
+    }
+};
+
+static SysBusDeviceInfo ax88796_info = {
+    .init = ax88796_init,
+    .qdev.name = "ax88796",
+    .qdev.size = sizeof(AX88796State),
+    .qdev.vmsd = &ax88796_vmsd,
+    .qdev.props = (Property[]) {
+        DEFINE_NIC_PROPERTIES(AX88796State, conf),
+        DEFINE_PROP_END_OF_LIST(),
+    },
+};
+
+static void ax88796_register_device(void)
+{
+    sysbus_register_withprop(&ax88796_info);
+}
+
+device_init(ax88796_register_device)
+
+
 static void stcb_i2c_setup(STCBState *stcb)
 {
     i2c_bus *bus = s3c24xx_i2c_bus(stcb->soc->iic);
@@ -232,6 +381,7 @@ static void stcb_i2c_setup(STCBState *stcb)
     i2c_create_slave(bus, "stcpmu", 0x6B);
 }
 
+
 static struct arm_boot_info bast_binfo = {
     .board_id = BAST_BOARD_ID,
     .ram_size = 0x10000000, /* 256MB */
@@ -242,11 +392,17 @@ static void stcb_init(ram_addr_t _ram_size,
                       const char *kernel_filename, const char *kernel_cmdline,
                       const char *initrd_filename, const char *cpu_model)
 {
+    MemoryRegion *address_space_mem = get_system_memory();
     STCBState *stcb;
+    CharDriverState *chr;
+    DeviceState *dev;
     DriveInfo *dinfo;
     NICInfo *nd;
+    SysBusDevice *s;
+    int i;
     int ret;
     BlockDriverState *flash_bds = NULL;
+    //~ qemu_irq *i8259;
 
     /* ensure memory is limited to 256MB */
     if (_ram_size > (256 * MiB)) {
@@ -264,6 +420,15 @@ static void stcb_init(ram_addr_t _ram_size,
 
     /* allocate storage for board state */
     stcb = g_malloc0(sizeof(STCBState));
+
+    /* Make sure all serial ports are associated with a device. */
+    for (i = 0; i < MAX_SERIAL_PORTS; i++) {
+        if (!serial_hds[i]) {
+            char label[32];
+            snprintf(label, sizeof(label), "serial%d", i);
+            serial_hds[i] = qemu_chr_new(label, "vc:80Cx24C", NULL);
+        }
+    }
 
     /* initialise SOC */
     stcb->soc = s3c2410x_init(ram_size);
@@ -308,15 +473,26 @@ static void stcb_init(ram_addr_t _ram_size,
 
     nd = &nd_table[0];
     if (nd->vlan) {
-        DeviceState *dev;
-        SysBusDevice *s;
         qemu_check_nic_model(nd, "dm9000");
         dev = qdev_create(NULL, "dm9000");
         qdev_set_nic_properties(dev, nd);
         qdev_init_nofail(dev);
         s = sysbus_from_qdev(dev);
-        sysbus_mmio_map(s, 0, 0x2d000000);
-        sysbus_connect_irq(s, 0, s3c24xx_get_eirq(stcb->soc->gpio, 10));
+        sysbus_mmio_map(s, 0, DM9000_BASE);
+        sysbus_connect_irq(s, 0, s3c24xx_get_eirq(stcb->soc->gpio, DM9000_IRQ));
+    }
+
+    nd = &nd_table[1];
+    if (nd->vlan) {
+        qemu_check_nic_model(nd, "ax88796");
+        dev = qdev_create(NULL, "ax88796");
+        qdev_set_nic_properties(dev, nd);
+        qdev_init_nofail(dev);
+        s = sysbus_from_qdev(dev);
+        sysbus_mmio_map(s, 0, ASIXNET_BASE);
+        logout("ASIXNET_BASE = 0x%08x\n", ASIXNET_BASE);
+        logout("AX88796_BASE = 0x%08x\n", AX88796_BASE);
+        //~ sysbus_connect_irq(s, 0, s3c24xx_get_eirq(stcb->soc->gpio, AX88796_IRQ));
     }
 
     /* Initialise the BAST CPLD */
@@ -334,6 +510,24 @@ static void stcb_init(ram_addr_t _ram_size,
     } else {
         stcb->nand[2] = nand_init(NULL, 0xEC, 0x79); /* 128MiB small-page */
     }
+
+    chr = qemu_chr_new("uart0", "vc:80Cx24C", NULL);
+    serial_mm_init(address_space_mem, SERIAL_BASE + 0x2f8, 0,
+                   s3c24xx_get_eirq(stcb->soc->gpio, 15),
+                   SERIAL_CLK, chr, 0);
+    chr = qemu_chr_new("uart1", "vc:80Cx24C", NULL);
+    serial_mm_init(address_space_mem, SERIAL_BASE + 0x3f8, 0,
+                   s3c24xx_get_eirq(stcb->soc->gpio, 14),
+                   SERIAL_CLK, chr, 0);
+#if 0
+    /* Super I/O */
+    isa_bus_new(NULL);
+    i8259 = i8259_init(s3c24xx_get_eirq(stcb->soc->gpio, 4));
+    isa_bus_irqs(i8259);
+    /*isa_dev =*/ isa_create_simple("i8042");
+    serial_isa_init(0, serial_hds[0]);
+    serial_isa_init(1, serial_hds[1]);
+#endif
 }
 
 static QEMUMachine bast_machine = {
