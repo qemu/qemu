222f23f508a8d778f56eddef14752dfd26d225b4
Dr. David Alan Gilbert <david.gilbert@linaro.org>
2011-12-12 16:37:31 +0100

    tcg/arm: remove fixed map code buffer restriction
    On ARM, don't map the code buffer at a fixed location, and fix up the
    call/goto tcg routines to let it do long jumps.
    
    Mapping the code buffer at a fixed address could sometimes result in it being
    mapped over the top of the heap with pretty random results.
    
    Signed-off-by: Dr. David Alan Gilbert <david.gilbert@linaro.org>
    Signed-off-by: Andrzej Zaborowski <andrew.zaborowski@intel.com>

--- exec.c
+++ exec.c
@@ -497,9 +497,7 @@ static void code_gen_alloc(unsigned long tb_size)
         if (code_gen_buffer_size > (512 * 1024 * 1024))
             code_gen_buffer_size = (512 * 1024 * 1024);
 #elif defined(__arm__)
-        /* Map the buffer below 32M, so we can use direct calls and branches */
-        flags |= MAP_FIXED;
-        start = (void *) 0x01000000UL;
+        /* Keep the buffer no bigger than 16GB to branch between blocks */
         if (code_gen_buffer_size > 16 * 1024 * 1024)
             code_gen_buffer_size = 16 * 1024 * 1024;
 #elif defined(__s390x__)
