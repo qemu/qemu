qemu/qemu

commit c5705a7728b4a6bc9e4f2d35911adbaf28042b25

vmstate, memory: decouple vmstate from memory API
Currently creating a memory region automatically registers it for
live migration.  This differs from other state (which is enumerated
in a VMStateDescription structure) and ties the live migration code
into the memory core.

Decouple the two by introducing a separate API, vmstate_register_ram(),
for registering a RAM block for migration.  Currently the same
implementation is reused, but later it can be moved into a separate list,
and registrations can be moved to VMStateDescription blocks.

Signed-off-by: Avi Kivity <avi@redhat.com>

memory.c
@@ -1033,7 +1033,6 @@ void memory_region_init_io(MemoryRegion *mr,
 }
 
 void memory_region_init_ram(MemoryRegion *mr,
-                            DeviceState *dev,
                             const char *name,
                             uint64_t size)
 {
@@ -1041,12 +1040,11 @@ void memory_region_init_ram(MemoryRegion *mr,
     mr->ram = true;
     mr->terminates = true;
     mr->destructor = memory_region_destructor_ram;
-    mr->ram_addr = qemu_ram_alloc(dev, name, size, mr);
+    mr->ram_addr = qemu_ram_alloc(size, mr);
     mr->backend_registered = true;
 }
 
 void memory_region_init_ram_ptr(MemoryRegion *mr,
-                                DeviceState *dev,
                                 const char *name,
                                 uint64_t size,
                                 void *ptr)
@@ -1055,7 +1053,7 @@ void memory_region_init_ram_ptr(MemoryRegion *mr,
     mr->ram = true;
     mr->terminates = true;
     mr->destructor = memory_region_destructor_ram_from_ptr;
-    mr->ram_addr = qemu_ram_alloc_from_ptr(dev, name, size, ptr, mr);
+    mr->ram_addr = qemu_ram_alloc_from_ptr(size, ptr, mr);
     mr->backend_registered = true;
 }
 
@@ -1073,7 +1071,6 @@ void memory_region_init_alias(MemoryRegion *mr,
 void memory_region_init_rom_device(MemoryRegion *mr,
                                    const MemoryRegionOps *ops,
                                    void *opaque,
-                                   DeviceState *dev,
                                    const char *name,
                                    uint64_t size)
 {
@@ -1082,7 +1079,7 @@ void memory_region_init_rom_device(MemoryRegion *mr,
     mr->opaque = opaque;
     mr->terminates = true;
     mr->destructor = memory_region_destructor_rom_device;
-    mr->ram_addr = qemu_ram_alloc(dev, name, size, mr);
+    mr->ram_addr = qemu_ram_alloc(size, mr);
     mr->ram_addr |= cpu_register_io_memory(memory_region_read_thunk,
                                            memory_region_write_thunk,
                                            mr,

commit b8a42ff13b73cc9a851b39752f1bc47a5cffda7d

memory: Fix assertion for flash devices
There is a regression since commit c5705a7728b4a6bc9e4f2d35911adbaf28042b25
or some other recent change.

System emulation with a flash device raises an assertion in function
qemu_ram_set_idstr because no new_block is found at the requested addr.

The address of the memory region (mr->ram_addr) is set by
memory_region_init_rom_device:

    mr->ram_addr = qemu_ram_alloc(size, mr);
    mr->ram_addr |= cpu_register_io_memory(mr);

The 2nd line adds a small offset to mr->ram_addr, and this offset
makes ram_set_idstr fail with an assertion.

Masking mr->ram_addr with the target's page mask removes the small
offset and makes flash devices work again.

Cc: Avi Kivity <avi@redhat.com>
Signed-off-by: Stefan Weil <sw@weilnetz.de>

memory.c
@@ -1386,7 +1386,7 @@ void memory_region_set_alias_offset(MemoryRegion *mr, target_phys_addr_t offset)
 
 ram_addr_t memory_region_get_ram_addr(MemoryRegion *mr)
 {
-    return mr->ram_addr;
+    return mr->ram_addr & TARGET_PAGE_MASK;
 }
 
 static int cmp_flatrange_addr(const void *addr_, const void *fr_)
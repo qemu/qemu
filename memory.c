b8a42ff13b73cc9a851b39752f1bc47a5cffda7d
Stefan Weil <sw@weilnetz.de>
2012-01-07 12:11:14 +0100

    memory: Fix assertion for flash devices
    There is a regression since commit c5705a7728b4a6bc9e4f2d35911adbaf28042b25
    or some other recent change.
    
    System emulation with a flash device raises an assertion in function
    qemu_ram_set_idstr because no new_block is found at the requested addr.
    
    The address of the memory region (mr->ram_addr) is set by
    memory_region_init_rom_device:
    
        mr->ram_addr = qemu_ram_alloc(size, mr);
        mr->ram_addr |= cpu_register_io_memory(mr);
    
    The 2nd line adds a small offset to mr->ram_addr, and this offset
    makes ram_set_idstr fail with an assertion.
    
    Masking mr->ram_addr with the target's page mask removes the small
    offset and makes flash devices work again.
    
    Cc: Avi Kivity <avi@redhat.com>
    Signed-off-by: Stefan Weil <sw@weilnetz.de>

--- memory.c
+++ memory.c
@@ -1386,7 +1386,7 @@ void memory_region_set_alias_offset(MemoryRegion *mr, target_phys_addr_t offset)
 
 ram_addr_t memory_region_get_ram_addr(MemoryRegion *mr)
 {
-    return mr->ram_addr;
+    return mr->ram_addr & TARGET_PAGE_MASK;
 }
 
 static int cmp_flatrange_addr(const void *addr_, const void *fr_)
