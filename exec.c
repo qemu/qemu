673641504780300be2f0553b76bee34e3d643e57
Max Filippov <jcmvbkbc@gmail.com>
2012-01-29 00:01:40 +0400

    exec: add missing breaks to the watch_mem_write
    Signed-off-by: Max Filippov <jcmvbkbc@gmail.com>
    Reviewed-by: Andreas Färber <afaerber@suse.de>
    Reviewed-by: Meador Inge <meadori@codesourcery.com>

--- exec.c
+++ exec.c
@@ -3289,9 +3289,15 @@ static void watch_mem_write(void *opaque, target_phys_addr_t addr,
 {
     check_watchpoint(addr & ~TARGET_PAGE_MASK, ~(size - 1), BP_MEM_WRITE);
     switch (size) {
-    case 1: stb_phys(addr, val);
-    case 2: stw_phys(addr, val);
-    case 4: stl_phys(addr, val);
+    case 1:
+        stb_phys(addr, val);
+        break;
+    case 2:
+        stw_phys(addr, val);
+        break;
+    case 4:
+        stl_phys(addr, val);
+        break;
     default: abort();
     }
 }
