eaa2e027b73c9afca623d877c91150a94c83049d
Jan Kiszka <jan.kiszka@siemens.com>
2012-01-31 13:45:29 +0100

    Revert "Handle SDL grabs failing (Mark McLoughlin)"
    This reverts commit 6bb816031f8bc0aafc3476e6dfa4293ee3a5f106.
    
    SDL_WM_GrabInput does not reliably bail out if grabbing is impossible.
    So if we get here, we already lost and will block. But this can no
    longer happen due to the check in sdl_grab_start. So this patch became
    obsolete.
    
    Conflicts:
    
    	sdl.c
    
    Signed-off-by: Jan Kiszka <jan.kiszka@siemens.com>
    Signed-off-by: Anthony Liguori <aliguori@us.ibm.com>

--- ui/sdl.c
+++ ui/sdl.c
@@ -475,12 +475,9 @@ static void sdl_grab_start(void)
             SDL_WarpMouse(guest_x, guest_y);
     } else
         sdl_hide_cursor();
-
-    if (SDL_WM_GrabInput(SDL_GRAB_ON) == SDL_GRAB_ON) {
-        gui_grab = 1;
-        sdl_update_caption();
-    } else
-        sdl_show_cursor();
+    SDL_WM_GrabInput(SDL_GRAB_ON);
+    gui_grab = 1;
+    sdl_update_caption();
 }
 
 static void sdl_grab_end(void)
