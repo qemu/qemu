0056c093b683879535f99a0ab7ccccaa5d1fefc6
Stefan Weil <sw@weilnetz.de>
2012-02-01 20:56:03 +0100

    target-mips: Clean includes
    Remove some include statements which are not needed.
    
    Cc: Aurelien Jarno <aurelien@aurel32.net>
    Signed-off-by: Stefan Weil <sw@weilnetz.de>

--- target-mips/translate.c
+++ target-mips/translate.c
@@ -20,16 +20,9 @@
  * License along with this library; if not, see <http://www.gnu.org/licenses/>.
  */
 
-#include <stdarg.h>
-#include <stdlib.h>
-#include <stdio.h>
-#include <string.h>
-#include <inttypes.h>
-
 #include "cpu.h"
 #include "disas.h"
 #include "tcg-op.h"
-#include "qemu-common.h"
 
 #include "helper.h"
 #define GEN_HELPER 1
