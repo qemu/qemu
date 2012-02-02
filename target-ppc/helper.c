550a82ec32c1afe6518bfbe8c811d34d85518cdc
Stefan Weil <sw@weilnetz.de>
2012-02-02 21:40:37 +0100

    target-ppc: Clean includes
    Remove some include statements which are not needed.
    
    Acked-by: Alexander Graf <agraf@suse.de>
    Signed-off-by: Stefan Weil <sw@weilnetz.de>

--- target-ppc/helper.c
+++ target-ppc/helper.c
@@ -16,15 +16,9 @@
  * You should have received a copy of the GNU Lesser General Public
  * License along with this library; if not, see <http://www.gnu.org/licenses/>.
  */
-#include <stdarg.h>
-#include <stdlib.h>
-#include <stdio.h>
-#include <string.h>
-#include <inttypes.h>
 
 #include "cpu.h"
 #include "helper_regs.h"
-#include "qemu-common.h"
 #include "kvm.h"
 #include "kvm_ppc.h"
 #include "cpus.h"
