d7378ee1c159ee0bf6deffa9e87020cb820af45c
Markus Armbruster <armbru@redhat.com>
2012-02-07 15:09:11 +0100

    qemu-char: qemu_chr_open_fd() can't fail, don't check
    Cleaned up silently in commit aad04cd0, but that just got reverted.
    Re-apply this part.
    
    Reviewed-by: Anthony Liguori <aliguori@us.ibm.com>
    Signed-off-by: Markus Armbruster <armbru@redhat.com>
    Signed-off-by: Anthony Liguori <aliguori@us.ibm.com>

--- qemu-char.c
+++ qemu-char.c
@@ -1226,10 +1226,6 @@ static CharDriverState *qemu_chr_open_tty(QemuOpts *opts)
     }
     tty_serial_init(fd, 115200, 'N', 8, 1);
     chr = qemu_chr_open_fd(fd, fd);
-    if (!chr) {
-        close(fd);
-        return NULL;
-    }
     chr->chr_ioctl = tty_serial_ioctl;
     chr->chr_close = qemu_chr_close_tty;
     return chr;
