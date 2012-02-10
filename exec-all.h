194de0a85662cd9c7ec462896e0474d821d9a219
Stefan Weil <sw@weilnetz.de>
2012-02-10 22:46:37 +0100

    exec-all: Fix data type of local variable
    Array tb_jmp_offset has elements of type uint16_t,
    so use the same type for variable offset.
    
    Signed-off-by: Stefan Weil <sw@weilnetz.de>

--- exec-all.h
+++ exec-all.h
@@ -239,9 +239,7 @@ static inline void tb_set_jmp_target1(uintptr_t jmp_addr, uintptr_t addr)
 static inline void tb_set_jmp_target(TranslationBlock *tb,
                                      int n, uintptr_t addr)
 {
-    uintptr_t offset;
-
-    offset = tb->tb_jmp_offset[n];
+    uint16_t offset = tb->tb_jmp_offset[n];
     tb_set_jmp_target1((uintptr_t)(tb->tc_ptr + offset), addr);
 }
 
