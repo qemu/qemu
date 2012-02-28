95f4f404e108f8c6b937ddf2ba973f3799b51fb5
Luiz Capitulino <lcapitulino@redhat.com>
2012-02-28 11:03:05 -0300

    qemu-ga: add guest-suspend-hybrid
    Signed-off-by: Luiz Capitulino <lcapitulino@redhat.com>

--- qga/commands-posix.c
+++ qga/commands-posix.c
@@ -715,6 +715,16 @@ void qmp_guest_suspend_ram(Error **err)
     guest_suspend("pm-suspend", "mem", err);
 }
 
+void qmp_guest_suspend_hybrid(Error **err)
+{
+    bios_supports_mode("pm-is-supported", "--suspend-hybrid", NULL, err);
+    if (error_is_set(err)) {
+        return;
+    }
+
+    guest_suspend("pm-suspend-hybrid", NULL, err);
+}
+
 /* register init/cleanup routines for stateful command groups */
 void ga_command_state_init(GAState *s, GACommandState *cs)
 {
