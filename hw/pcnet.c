6655124ddd6442b19a4b43b27e7d5a3846c4e6a8
Jan Kiszka <jan.kiszka@siemens.com>
2012-02-29 15:33:48 +0100

    pcnet: Clear ERR in CSR0 on stop
    pcnet_stop already clears any reason (BABL, CERR, MISS, MERR) why ERR
    (bit 15) should be set in CRS0. So we have to clear that bit as well.
    
    Signed-off-by: Jan Kiszka <jan.kiszka@siemens.com>

--- hw/pcnet.c
+++ hw/pcnet.c
@@ -884,7 +884,7 @@ static void pcnet_stop(PCNetState *s)
 #ifdef PCNET_DEBUG
     printf("pcnet_stop\n");
 #endif
-    s->csr[0] &= ~0x7feb;
+    s->csr[0] &= ~0xffeb;
     s->csr[0] |= 0x0014;
     s->csr[4] &= ~0x02c2;
     s->csr[5] &= ~0x0011;
