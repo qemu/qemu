4055299ef0e1c6e4a9b09ce000757b1274129991
Kirill Batuzov <batuzovk@ispras.ru>
2012-03-02 13:22:17 +0400

    Fix large memory chunks allocation with tcg_malloc.
    An attempt to allocate a large memory chunk after a small one resulted in
    circular links in list of pools.  It caused the same memory being
    allocated twice for different arrays.
    
    Now pools for large memory chunks are kept in separate list and are
    freed during pool reset because current allocator can not reuse them.
    
    Signed-off-by: Kirill Batuzov <batuzovk@ispras.ru>
    Signed-off-by: Blue Swirl <blauwirbel@gmail.com>

--- tcg/tcg.h
+++ tcg/tcg.h
@@ -337,7 +337,7 @@ typedef struct TCGContext TCGContext;
 
 struct TCGContext {
     uint8_t *pool_cur, *pool_end;
-    TCGPool *pool_first, *pool_current;
+    TCGPool *pool_first, *pool_current, *pool_first_large;
     TCGLabel *labels;
     int nb_labels;
     TCGTemp *temps; /* globals first, temps after */
