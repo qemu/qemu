021ecd8b9db37927059f5d3234b51ed766706437
Stefan Weil <sw@weilnetz.de>
2012-03-02 23:30:06 +0100

    cache-utils: Change data type of parameters for flush_icache_range
    The TCG targets i386 and tci needed a change of the function
    prototype for w64.
    
    This change is currently not needed here, but it can be applied
    to avoid code differences.
    
    Cc: Alexander Graf <agraf@suse.de>
    Signed-off-by: Stefan Weil <sw@weilnetz.de>
    Signed-off-by: Blue Swirl <blauwirbel@gmail.com>

--- cache-utils.h
+++ cache-utils.h
@@ -12,7 +12,7 @@ extern struct qemu_cache_conf qemu_cache_conf;
 void qemu_cache_utils_init(char **envp);
 
 /* mildly adjusted code from tcg-dyngen.c */
-static inline void flush_icache_range(unsigned long start, unsigned long stop)
+static inline void flush_icache_range(uintptr_t start, uintptr_t stop)
 {
     unsigned long p, start1, stop1;
     unsigned long dsize = qemu_cache_conf.dcache_bsize;
