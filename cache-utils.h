e04b28996110bd6acfc059e9f2c8c5aba5119a46
Stefan Weil <sw@weilnetz.de>
2012-03-05 21:15:19 +0100

    cache-utils: Add missing include file for uintptr_t
    Commit 021ecd8b9db37927059f5d3234b51ed766706437 breaks the build for
    PPC hosts because it uses uintptr_t without the necessary include file.
    
    uintptr_t is defined in stdint.h, so add this include.
    
    Cc: Alexander Graf <agraf@suse.de>
    Signed-off-by: Stefan Weil <sw@weilnetz.de>
    Signed-off-by: Blue Swirl <blauwirbel@gmail.com>

--- cache-utils.h
+++ cache-utils.h
@@ -2,6 +2,9 @@
 #define QEMU_CACHE_UTILS_H
 
 #if defined(_ARCH_PPC)
+
+#include <stdint.h> /* uintptr_t */
+
 struct qemu_cache_conf {
     unsigned long dcache_bsize;
     unsigned long icache_bsize;
