ee032ca146e932ce616aff7762596a10bf24ecac
Stefan Weil <sw@weilnetz.de>
2012-03-08 22:58:06 +0100

    vnc: Fix packed boolean struct members
    This patch fixes warnings reported by splint:
    
    For variables which are packed in a single bit, a signed data type
    like 'int' does not make much sense.
    
    There is no obvious reason why the two values should be packed,
    so I removed the packing and changed the data type to bool
    because both are used as boolean values.
    
    v2:
    
    Some versions of gcc complain after this modification,
    for example gcc (Debian 4.4.5-8) 4.4.5):
    
    ui/vnc-auth-sasl.c: In function ‘vnc_sasl_client_cleanup’:
    ui/vnc-auth-sasl.c:34: error: suggest parentheses around assignment used as truth value
    
    Obviously, the compiler does not like code which does
    bool = unsigned = bool = 0
    
    Splitting that code in three statements works.
    
    Cc: Anthony Liguori <aliguori@us.ibm.com>
    Signed-off-by: Stefan Weil <sw@weilnetz.de>
    Signed-off-by: Stefan Hajnoczi <stefanha@linux.vnet.ibm.com>

--- ui/vnc-auth-sasl.c
+++ ui/vnc-auth-sasl.c
@@ -31,7 +31,9 @@
 void vnc_sasl_client_cleanup(VncState *vs)
 {
     if (vs->sasl.conn) {
-        vs->sasl.runSSF = vs->sasl.waitWriteSSF = vs->sasl.wantSSF = 0;
+        vs->sasl.runSSF = false;
+        vs->sasl.wantSSF = false;
+        vs->sasl.waitWriteSSF = 0;
         vs->sasl.encodedLength = vs->sasl.encodedOffset = 0;
         vs->sasl.encoded = NULL;
         g_free(vs->sasl.username);
