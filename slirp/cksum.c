2d26512b45b5236fa521c4492608fe9fb5bedf46
Stefan Weil <sw@weilnetz.de>
2012-03-10 21:20:53 +0100

    slirp: Fix compiler warning for w64
    Casting a pointer to an integer value must use uintptr_t or intptr_t
    (not long) for portable code. MinGW-w64 requires this because
    sizeof(long) != sizeof(void *) for w64 hosts, so casting to long
    raises a compiler warning.
    
    I use uintptr_t instead of intptr_t because changing the sign does not
    matter here and casting pointers to unsigned values seems more
    reasonable (the unsigned value is a non negative offset.
    
    Cc: Jan Kiszka <jan.kiszka@siemens.com>
    Signed-off-by: Stefan Weil <sw@weilnetz.de>
    Signed-off-by: Jan Kiszka <jan.kiszka@siemens.com>

--- slirp/cksum.c
+++ slirp/cksum.c
@@ -75,7 +75,7 @@ int cksum(struct mbuf *m, int len)
 	/*
 	 * Force to even boundary.
 	 */
-	if ((1 & (long) w) && (mlen > 0)) {
+	if ((1 & (uintptr_t)w) && (mlen > 0)) {
 		REDUCE;
 		sum <<= 8;
 		s_util.c[0] = *(uint8_t *)w;
