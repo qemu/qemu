2b3bc6c0f56968631c4d92b7e06ec969ed0700bb
Andreas Färber <afaerber@suse.de>
2012-03-13 16:48:19 +0100

    target-unicore32: Relicense to GPLv2+
    Adopt the license text suggested by Guan Xue-tao (with a minor
    simplification) for all target-unicore/ files except helper.c.
    
    To helper.c Anthony Liguori contributed a qemu_malloc() -> g_malloc()
    conversion, still pending IBM relicensing approval, so that remains
    GPLv2 for now.
    
    By relicensing all possible parts now, we avoid having to formally
    relicense new, e.g., QOM code.
    
    Signed-off-by: Andreas Färber <afaerber@suse.de>
    Signed-off-by: Guan Xuetao <gxt@mprc.pku.edu.cn>
    Signed-off-by: Stefan Weil <sw@weilnetz.de>
    Signed-off-by: Blue Swirl <blauwirbel@gmail.com>
    Signed-off-by: Dor Laor <dlaor@redhat.com>

--- target-unicore32/op_helper.c
+++ target-unicore32/op_helper.c
@@ -5,7 +5,8 @@
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License version 2 as
- * published by the Free Software Foundation.
+ * published by the Free Software Foundation, or (at your option) any
+ * later version. See the COPYING file in the top-level directory.
  */
 #include "cpu.h"
 #include "dyngen-exec.h"
