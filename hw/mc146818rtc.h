fd94be7ae65195c1fad2b740e0cd4620b25cc5b5
Anthony Liguori <aliguori@us.ibm.com>
2012-03-28 15:42:06 +0200

    rtc: split out macros into a header file and use in test case
    Signed-off-by: Anthony Liguori <aliguori@us.ibm.com>
    
    Signed-off-by: Paolo Bonzini <pbonzini@redhat.com>
    Signed-off-by: Anthony Liguori <aliguori@us.ibm.com>

--- hw/mc146818rtc.h
+++ hw/mc146818rtc.h
@@ -2,8 +2,7 @@
 #define MC146818RTC_H
 
 #include "isa.h"
-
-#define RTC_ISA_IRQ 8
+#include "mc146818rtc_regs.h"
 
 ISADevice *rtc_init(ISABus *bus, int base_year, qemu_irq intercept_irq);
 void rtc_set_memory(ISADevice *dev, int addr, int val);
