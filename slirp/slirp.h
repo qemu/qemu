56688961634a2d05fb5841a149bb5c1a17239e4c
Jan Kiszka <jan.kiszka@siemens.com>
2012-03-28 21:03:42 +0200

    w32/slirp: Undefine error constants before their redefinition
    Less warnings for your console.
    
    Signed-off-by: Jan Kiszka <jan.kiszka@siemens.com>

--- slirp/slirp.h
+++ slirp/slirp.h
@@ -15,6 +15,12 @@ typedef char *caddr_t;
 # include <sys/timeb.h>
 # include <iphlpapi.h>
 
+# undef EWOULDBLOCK
+# undef EINPROGRESS
+# undef ENOTCONN
+# undef EHOSTUNREACH
+# undef ENETUNREACH
+# undef ECONNREFUSED
 # define EWOULDBLOCK WSAEWOULDBLOCK
 # define EINPROGRESS WSAEINPROGRESS
 # define ENOTCONN WSAENOTCONN
