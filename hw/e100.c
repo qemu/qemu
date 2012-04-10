6b369c4d1d3254eae894f28e36e94436df21efe5
Stefan Weil <sw@weilnetz.de>
2012-04-10 20:53:46 +0200

    e100: Remove duplicate code
    Signed-off-by: Stefan Weil <sw@weilnetz.de>

--- hw/e100.c
+++ hw/e100.c
@@ -2115,28 +2115,6 @@ static const MemoryRegionOps e100_ops = {
     .endianness = DEVICE_LITTLE_ENDIAN,
 };
 
-/* From FreeBSD */
-#define POLYNOMIAL 0x04c11db6
-static int compute_mcast_idx(const uint8_t *ep)
-{
-    uint32_t crc;
-    int carry, i, j;
-    uint8_t b;
-
-    crc = 0xffffffff;
-    for (i = 0; i < 6; i++) {
-        b = *ep++;
-        for (j = 0; j < 8; j++) {
-            carry = ((crc & 0x80000000L) ? 1 : 0) ^ (b & 0x01);
-            crc <<= 1;
-            b >>= 1;
-            if (carry)
-                crc = ((crc ^ POLYNOMIAL) | carry);
-        }
-    }
-    return (crc >> 26);
-}
-
 #ifdef USE_BUFFER_TCP
 #define BUFFER_TCP_ENABLE_VALVE 3
 #define BUFFER_TCP_FLUSH_VALVE 5
