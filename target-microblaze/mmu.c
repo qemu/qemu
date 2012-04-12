dadc1064c348545695b8a14d9dc72ccaa2983be7
Peter A. G. Crosthwaite <peter.crosthwaite@petalogix.com>
2012-04-12 14:30:30 +1000

    target-microblaze: added PetaLogix copyright
    Microblaze cpu development has been driven and funded by PetaLogix. Added (c)
    PetaLogix line accordingly.
    
    Signed-off-by: Peter A. G. Crosthwaite <peter.crosthwaite@petalogix.com>
    Signed-off-by: Edgar E. Iglesias <edgar.iglesias@gmail.com>

--- target-microblaze/mmu.c
+++ target-microblaze/mmu.c
@@ -2,6 +2,7 @@
  *  Microblaze MMU emulation for qemu.
  *
  *  Copyright (c) 2009 Edgar E. Iglesias
+ *  Copyright (c) 2009-2012 PetaLogix Qld Pty Ltd.
  *
  * This library is free software; you can redistribute it and/or
  * modify it under the terms of the GNU Lesser General Public
