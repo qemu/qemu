90449c388711c3defdc76da490926d1eca177b06
Blue Swirl <blauwirbel@gmail.com>
2012-04-19 18:33:05 +0000

    sparc: fix qtest
    Initialize TCG only when enabled.
    
    Signed-off-by: Blue Swirl <blauwirbel@gmail.com>

--- target-sparc/cpu.c
+++ target-sparc/cpu.c
@@ -119,7 +119,9 @@ CPUSPARCState *cpu_sparc_init(const char *cpu_model)
     cpu = SPARC_CPU(object_new(TYPE_SPARC_CPU));
     env = &cpu->env;
 
-    gen_intermediate_code_init(env);
+    if (tcg_enabled()) {
+        gen_intermediate_code_init(env);
+    }
 
     if (cpu_sparc_register(env, cpu_model) < 0) {
         object_delete(OBJECT(cpu));
