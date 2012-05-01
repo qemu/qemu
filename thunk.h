b754e4fc1e8e68af975c545c38ebc3b001ebc98f
malc <av1474@comtv.ru>
2012-05-01 18:23:04 +0400

    Remove stray HOST_LONG_SIZE
    Signed-off-by: malc <av1474@comtv.ru>

--- thunk.h
+++ thunk.h
@@ -113,7 +113,7 @@ static inline int thunk_type_size(const argtype *type_ptr, int is_host)
       defined(HOST_PARISC) || defined(HOST_SPARC64)
             return 4;
 #elif defined(HOST_PPC)
-            return HOST_LONG_SIZE;
+            return TARGET_ABI_BITS / 8;
 #else
             return 2;
 #endif
