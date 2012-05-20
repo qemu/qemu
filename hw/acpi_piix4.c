a6de8ed80e1cf47d0f372e0856e6eeb6de9bbf0e
Amos Kong <kongjianjun@gmail.com>
2012-05-20 17:57:45 +0800

    pci: call object_unparent() before free_qdev()
    Start VM with 8 multiple-function block devs, hot-removing
    those block devs by 'device_del ...' would cause qemu abort.
    
    | (qemu) device_del virti0-0-0
    | (qemu) **
    |ERROR:qom/object.c:389:object_delete: assertion failed: (obj->ref == 0)
    
    It's a regression introduced by commit 57c9fafe
    
    The whole PCI slot should be removed once. Currently only one func
    is cleaned in pci_unplug_device(), if you try to remove a single
    func by monitor cmd.
    
    free_qdev() are called for all functions in slot,
    but unparent_delete() is only called for one
    function.
    
    Signed-off-by: XXXX
    Signed-off-by: Anthony Liguori <aliguori@us.ibm.com>

--- hw/acpi_piix4.c
+++ hw/acpi_piix4.c
@@ -299,6 +299,7 @@ static void acpi_piix_eject_slot(PIIX4PMState *s, unsigned slots)
             if (pc->no_hotplug) {
                 slot_free = false;
             } else {
+                object_unparent(OBJECT(dev));
                 qdev_free(qdev);
             }
         }
