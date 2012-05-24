e9c6149f6ae6873f14a12eea554925b6aa4c4dec
Gerd Hoffmann <kraxel@redhat.com>
2012-05-24 09:16:29 +0200

    vga: raise xres+yres limits
    The vgabios will check whenever any given video mode will fit into the
    given video memory before adding it to the list of available modes, so
    there is no need to keep xmax * ymax * 32bpp lower than VGA_RAM_SIZE.
    
    Lets raise the limits a bit.  Should be good for a few years, display
    sizes are not growing that fast.
    
    Signed-off-by: Gerd Hoffmann <kraxel@redhat.com>

--- hw/vga_int.h
+++ hw/vga_int.h
@@ -31,8 +31,8 @@
 /* bochs VBE support */
 #define CONFIG_BOCHS_VBE
 
-#define VBE_DISPI_MAX_XRES              1600
-#define VBE_DISPI_MAX_YRES              1200
+#define VBE_DISPI_MAX_XRES              16000
+#define VBE_DISPI_MAX_YRES              12000
 #define VBE_DISPI_MAX_BPP               32
 
 #define VBE_DISPI_INDEX_ID              0x0
