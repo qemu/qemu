1643f2b232628905e8f32965ff36a87bd53b93c5
Gerd Hoffmann <kraxel@redhat.com>
2012-05-24 10:55:01 +0200

    vnc: fix segfault in vnc_display_pw_expire()
    NULL pointer dereference in case no vnc server is configured.
    Catch this and return -EINVAL like vnc_display_password() does.
    
    Signed-off-by: Gerd Hoffmann <kraxel@redhat.com>
    Signed-off-by: Anthony Liguori <aliguori@us.ibm.com>

--- ui/vnc.c
+++ ui/vnc.c
@@ -2849,6 +2849,10 @@ int vnc_display_pw_expire(DisplayState *ds, time_t expires)
 {
     VncDisplay *vs = ds ? (VncDisplay *)ds->opaque : vnc_display;
 
+    if (!vs) {
+        return -EINVAL;
+    }
+
     vs->expires = expires;
     return 0;
 }
