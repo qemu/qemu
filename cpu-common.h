e58998c66d158dea6221e84c2738301ff7a7c434
Peter Maydell <peter.maydell@linaro.org>
2012-06-22 12:30:59 +0100

    cpu-common.h: Remove a pointless ifndef CONFIG_USER_ONLY
    Remove an ifndef CONFIG_USER_ONLY guard that was pointless
    because it is already inside an if !defined(CONFIG_USER_ONLY).
    
    Signed-off-by: Peter Maydell <peter.maydell@linaro.org>
    Acked-by: Andreas Färber <afaerber@suse.de>
    Signed-off-by: Stefan Hajnoczi <stefanha@linux.vnet.ibm.com>

--- cpu-common.h
+++ cpu-common.h
@@ -69,9 +69,7 @@ void cpu_physical_memory_unmap(void *buffer, target_phys_addr_t len,
 void *cpu_register_map_client(void *opaque, void (*callback)(void *opaque));
 void cpu_unregister_map_client(void *cookie);
 
-#ifndef CONFIG_USER_ONLY
 bool cpu_physical_memory_is_io(target_phys_addr_t phys_addr);
-#endif
 
 /* Coalesced MMIO regions are areas where write operations can be reordered.
  * This usually implies that write operations are side-effect free.  This allows
