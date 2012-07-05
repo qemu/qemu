55e64b6dddfd71953f4eb7ad6d0bcfc8d92500e5
Jan Kiszka <jan.kiszka@siemens.com>
2012-07-05 19:35:57 +0200

    slirp: Enforce host-side user of smb share
    Windows 7 (and possibly other versions) cannot connect to the samba
    share if the exported host directory is not world-readable. This can be
    resolved by forcing the username used for access checks to the one
    under which QEMU and smbd are running.
    
    Signed-off-by: Jan Kiszka <jan.kiszka@siemens.com>
    (cherry picked from commit 1cb1c5d10bb9e180bd3f7be2c10b212ed86a97b4)
    
    Signed-off-by: Michael Roth <mdroth@linux.vnet.ibm.com>

--- net/slirp.c
+++ net/slirp.c
@@ -26,6 +26,7 @@
 #include "config-host.h"
 
 #ifndef _WIN32
+#include <pwd.h>
 #include <sys/wait.h>
 #endif
 #include "net.h"
@@ -487,7 +488,14 @@ static int slirp_smb(SlirpState* s, const char *exported_dir,
     static int instance;
     char smb_conf[128];
     char smb_cmdline[128];
+    struct passwd *passwd;
     FILE *f;
+
+    passwd = getpwuid(geteuid());
+    if (!passwd) {
+        error_report("failed to retrieve user name");
+        return -1;
+    }
 
     snprintf(s->smb_dir, sizeof(s->smb_dir), "/tmp/qemu-smb.%ld-%d",
              (long)getpid(), instance++);
@@ -517,14 +525,16 @@ static int slirp_smb(SlirpState* s, const char *exported_dir,
             "[qemu]\n"
             "path=%s\n"
             "read only=no\n"
-            "guest ok=yes\n",
+            "guest ok=yes\n"
+            "force user=%s\n",
             s->smb_dir,
             s->smb_dir,
             s->smb_dir,
             s->smb_dir,
             s->smb_dir,
             s->smb_dir,
-            exported_dir
+            exported_dir,
+            passwd->pw_name
             );
     fclose(f);
 
