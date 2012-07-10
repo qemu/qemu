856dcba23ad2aeea4d98d5d3c97cd46aac0cd073
Markus Armbruster <armbru@redhat.com>
2012-07-10 11:12:49 +0200

    qtest: Cover qdev property for BIOS CHS translation
    Signed-off-by: Markus Armbruster <armbru@redhat.com>
    Signed-off-by: Kevin Wolf <kwolf@redhat.com>

--- tests/hd-geo-test.c
+++ tests/hd-geo-test.c
@@ -321,15 +321,16 @@ static void test_ide_drive_user(const char *dev, bool trans)
     const CHST expected_chst = { secs / (4 * 32) , 4, 32, trans };
 
     argc = setup_common(argv, ARRAY_SIZE(argv));
-    opts = g_strdup_printf("%s,cyls=%d,heads=%d,secs=%d%s",
-                           dev && !trans ? dev : "",
+    opts = g_strdup_printf("%s,%s%scyls=%d,heads=%d,secs=%d",
+                           dev ?: "",
+                           trans && dev ? "bios-chs-" : "",
+                           trans ? "trans=lba," : "",
                            expected_chst.cyls, expected_chst.heads,
-                           expected_chst.secs,
-                           trans ? ",trans=lba" : "");
+                           expected_chst.secs);
     cur_ide[0] = &expected_chst;
     argc = setup_ide(argc, argv, ARRAY_SIZE(argv),
-                     0, dev && !trans ? opts : NULL, backend_small, mbr_chs,
-                     dev && !trans ? "" : opts);
+                     0, dev ? opts : NULL, backend_small, mbr_chs,
+                     dev ? "" : opts);
     g_free(opts);
     qtest_start(g_strjoinv(" ", argv));
     test_cmos();
