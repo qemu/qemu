69b67646bc7ae7f3d28b278e6ae4435a767450ec
Tyler Hall <tylerwhall@gmail.com>
2012-07-25 18:45:04 -0400

    exec.c: Use subpages for large unaligned mappings
    Registering a multi-page memory region that is non-page-aligned results
    in a subpage from the start to the page boundary, some number of full
    pages, and possibly another subpage from the last page boundary to the
    end. The full pages will have a value for offset_within_region that is
    not a multiple of TARGET_PAGE_SIZE. Accesses through softmmu are unable
    to handle this and will segfault.
    
    Handling full pages through subpages is not optimal, but only
    non-page-aligned mappings take the penalty.
    
    Signed-off-by: Tyler Hall <tylerwhall@gmail.com>
    Reviewed-by: Peter Maydell <peter.maydell@linaro.org>
    Reviewed-by: Avi Kivity <avi@redhat.com>
    Signed-off-by: Stefan Hajnoczi <stefanha@linux.vnet.ibm.com>

--- exec.c
+++ exec.c
@@ -2305,10 +2305,15 @@ void cpu_register_physical_memory_log(MemoryRegionSection *section,
         remain.offset_within_address_space += now.size;
         remain.offset_within_region += now.size;
     }
-    now = remain;
-    now.size &= TARGET_PAGE_MASK;
-    if (now.size) {
-        register_multipage(&now);
+    while (remain.size >= TARGET_PAGE_SIZE) {
+        now = remain;
+        if (remain.offset_within_region & ~TARGET_PAGE_MASK) {
+            now.size = TARGET_PAGE_SIZE;
+            register_subpage(&now);
+        } else {
+            now.size &= TARGET_PAGE_MASK;
+            register_multipage(&now);
+        }
         remain.size -= now.size;
         remain.offset_within_address_space += now.size;
         remain.offset_within_region += now.size;
