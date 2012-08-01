47fb65ce3e15762064dd6d93834e3ce923ef6f89
Stefano Stabellini <stefano.stabellini@eu.citrix.com>
2012-08-01 13:44:11 +0000

    fix Xen compilation
    xen_pt_unregister_device is used as PCIUnregisterFunc, so it should
    match the type.
    
    Signed-off-by: Stefano Stabellini <stefano.stabellini@eu.citrix.com>
    Tested-by: Andreas Färber <afaerber@suse.de>

--- hw/xen_pt.c
+++ hw/xen_pt.c
@@ -764,7 +764,7 @@ out:
     return 0;
 }
 
-static int xen_pt_unregister_device(PCIDevice *d)
+static void xen_pt_unregister_device(PCIDevice *d)
 {
     XenPCIPassthroughState *s = DO_UPCAST(XenPCIPassthroughState, dev, d);
     uint8_t machine_irq = s->machine_irq;
@@ -814,8 +814,6 @@ static int xen_pt_unregister_device(PCIDevice *d)
     memory_listener_unregister(&s->memory_listener);
 
     xen_host_pci_device_put(&s->real_device);
-
-    return 0;
 }
 
 static Property xen_pci_passthrough_properties[] = {
