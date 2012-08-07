a2950fb6e795e3e10fed35d347a7aa28a44be2ff
Alexey Kardashevskiy <aik@ozlabs.ru>
2012-08-07 16:10:36 +0000

    pseries: Add trace event for PCI irqs
    This adds a trace event in the pseries PCI specific set_irq() function to
    assist in debugging.
    
    Signed-off-by: Alexey Kardashevskiy <aik@ozlabs.ru>
    Signed-off-by: David Gibson <david@gibson.dropbear.id.au>
    [agraf: add trace.h include]
    Signed-off-by: Alexander Graf <agraf@suse.de>

--- hw/spapr_pci.c
+++ hw/spapr_pci.c
@@ -29,6 +29,7 @@
 #include "hw/spapr_pci.h"
 #include "exec-memory.h"
 #include <libfdt.h>
+#include "trace.h"
 
 #include "hw/pci_internals.h"
 
@@ -234,6 +235,7 @@ static void pci_spapr_set_irq(void *opaque, int irq_num, int level)
      */
     sPAPRPHBState *phb = opaque;
 
+    trace_spapr_pci_lsi_set(phb->busname, irq_num, phb->lsi_table[irq_num].irq);
     qemu_set_irq(spapr_phb_lsi_qirq(phb, irq_num), level);
 }
 
