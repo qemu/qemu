ffc7565c81b010683c3cab8efe9accd0eb90aa0d
Jan Kiszka <jan.kiszka@siemens.com>
2012-08-14 10:24:47 +0200

    kvm: i8254: Finish time conversion fix
    0cdd3d1444 fixed reading back the counter load time from the kernel
    while assuming the kernel would always update its load time on writing
    the state. That is only true for channel 1, and so pit_get_channel_info
    returned wrong output pin states for high counter values.
    
    Fix this by applying the offset also on kvm_pit_put. Now we also need to
    update the offset when we write the state while the VM is stopped as it
    keeps on changing in that state.
    
    Signed-off-by: Jan Kiszka <jan.kiszka@siemens.com>
    Signed-off-by: Marcelo Tosatti <mtosatti@redhat.com>
    (cherry picked from commit 050a46065de8e3d4ee5a04f5598d666f63d34800)
    
    Signed-off-by: Michael Roth <mdroth@linux.vnet.ibm.com>

--- hw/kvm/i8254.c
+++ hw/kvm/i8254.c
@@ -122,17 +122,23 @@ static void kvm_pit_get(PITCommonState *pit)
         pit_get_next_transition_time(sc, sc->count_load_time);
 }
 
-static void kvm_pit_put(PITCommonState *s)
+static void kvm_pit_put(PITCommonState *pit)
 {
+    KVMPITState *s = DO_UPCAST(KVMPITState, pit, pit);
     struct kvm_pit_state2 kpit;
     struct kvm_pit_channel_state *kchan;
     struct PITChannelState *sc;
     int i, ret;
 
-    kpit.flags = s->channels[0].irq_disabled ? KVM_PIT_FLAGS_HPET_LEGACY : 0;
+    /* The offset keeps changing as long as the VM is stopped. */
+    if (s->vm_stopped) {
+        kvm_pit_update_clock_offset(s);
+    }
+
+    kpit.flags = pit->channels[0].irq_disabled ? KVM_PIT_FLAGS_HPET_LEGACY : 0;
     for (i = 0; i < 3; i++) {
         kchan = &kpit.channels[i];
-        sc = &s->channels[i];
+        sc = &pit->channels[i];
         kchan->count = sc->count;
         kchan->latched_count = sc->latched_count;
         kchan->count_latched = sc->count_latched;
@@ -145,7 +151,7 @@ static void kvm_pit_put(PITCommonState *s)
         kchan->mode = sc->mode;
         kchan->bcd = sc->bcd;
         kchan->gate = sc->gate;
-        kchan->count_load_time = sc->count_load_time;
+        kchan->count_load_time = sc->count_load_time - s->kernel_clock_offset;
     }
 
     ret = kvm_vm_ioctl(kvm_state,
