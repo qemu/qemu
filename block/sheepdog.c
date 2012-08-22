b68e45c68667dce43341c94c69126852496ddbaf
Jim Meyering <meyering@redhat.com>
2012-08-22 13:55:54 +0200

    sheepdog: don't leak socket file descriptor upon connection failure
    Signed-off-by: Jim Meyering <meyering@redhat.com>
    Signed-off-by: Anthony Liguori <aliguori@us.ibm.com>
    (cherry picked from commit a7e47d4bfcbf256fae06891a8599950ff8e1b61b)
    
    Signed-off-by: Michael Roth <mdroth@linux.vnet.ibm.com>

--- block/sheepdog.c
+++ block/sheepdog.c
@@ -489,6 +489,7 @@ static int connect_to_sdog(const char *addr, const char *port)
             if (errno == EINTR) {
                 goto reconnect;
             }
+            close(fd);
             break;
         }
 
