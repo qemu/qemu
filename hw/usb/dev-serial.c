fbcb89ddc9808ad4bb63c46d6e47ea088516a7a7
Samuel Thibault <samuel.thibault@ens-lyon.org>
2012-08-23 09:59:27 +0200

    Better name usb braille device
    Windows users need to know that they have to use the Baum driver to make
    the qemu braille device work.
    
    Signed-off-by: Samuel Thibault <samuel.thibault@ens-lyon.org>
    Signed-off-by: Gerd Hoffmann <kraxel@redhat.com>
    (cherry picked from commit 2964cd9bfa5100e433471d3e3fedcc9d62891894)
    
    Signed-off-by: Michael Roth <mdroth@linux.vnet.ibm.com>

--- hw/usb/dev-serial.c
+++ hw/usb/dev-serial.c
@@ -113,7 +113,7 @@ enum {
 static const USBDescStrings desc_strings = {
     [STR_MANUFACTURER]    = "QEMU",
     [STR_PRODUCT_SERIAL]  = "QEMU USB SERIAL",
-    [STR_PRODUCT_BRAILLE] = "QEMU USB BRAILLE",
+    [STR_PRODUCT_BRAILLE] = "QEMU USB BAUM BRAILLE",
     [STR_SERIALNUMBER]    = "1",
 };
 
