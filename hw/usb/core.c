d780116ab095a3ac1e2a6b98f13ee4effde01544
Hans de Goede <hdegoede@redhat.com>
2012-08-28 09:43:18 +0200

    usb-core: Add a usb_ep_find_packet_by_id() helper function
    Signed-off-by: Hans de Goede <hdegoede@redhat.com>
    Signed-off-by: Gerd Hoffmann <kraxel@redhat.com>
    (cherry picked from commit c13a9e61366cc3e28299d8faeb65e65c6e5964cf)
    
    Signed-off-by: Michael Roth <mdroth@linux.vnet.ibm.com>

--- hw/usb/core.c
+++ hw/usb/core.c
@@ -726,3 +726,18 @@ void usb_ep_set_pipeline(USBDevice *dev, int pid, int ep, bool enabled)
     struct USBEndpoint *uep = usb_ep_get(dev, pid, ep);
     uep->pipeline = enabled;
 }
+
+USBPacket *usb_ep_find_packet_by_id(USBDevice *dev, int pid, int ep,
+                                    uint64_t id)
+{
+    struct USBEndpoint *uep = usb_ep_get(dev, pid, ep);
+    USBPacket *p;
+
+    while ((p = QTAILQ_FIRST(&uep->queue)) != NULL) {
+        if (p->id == id) {
+            return p;
+        }
+    }
+
+    return NULL;
+}
