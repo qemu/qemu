3c12193d998249bcbe5099edf85039bcd25c3a80
Juan Quintela <quintela@redhat.com>
2012-09-04 13:08:57 +0200

    ram: create trace event for migration sync bitmap
    Signed-off-by: Juan Quintela <quintela@redhat.com>
    
    Reviewed-by: Paolo Bonzini <pbonzini@redhat.com>

--- arch_init.c
+++ arch_init.c
@@ -45,6 +45,7 @@
 #include "hw/pcspk.h"
 #include "qemu/page_cache.h"
 #include "qmp-commands.h"
+#include "trace.h"
 
 #ifdef DEBUG_ARCH_INIT
 #define DPRINTF(fmt, ...) \
@@ -358,7 +359,12 @@ static inline void migration_bitmap_set_dirty(MemoryRegion *mr, int length)
 
 static void migration_bitmap_sync(void)
 {
+    uint64_t num_dirty_pages_init = ram_list.dirty_pages;
+
+    trace_migration_bitmap_sync_start();
     memory_global_sync_dirty_bitmap(get_system_memory());
+    trace_migration_bitmap_sync_end(ram_list.dirty_pages
+                                    - num_dirty_pages_init);
 }
 
 
