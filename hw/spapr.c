1ddcae82a2509668b94a13e84921bdcafddcdfff
Aurelien Jarno <aurelien@aurel32.net>
2012-09-08 12:40:45 +0200

    ppc/pSeries: use the new pci_vga_init() function
    Keep the case to prevent some vga card to be selected.
    
    Cc: Alexander Graf <agraf@suse.de>
    Cc: David Gibson <david@gibson.dropbear.id.au>
    Signed-off-by: Aurelien Jarno <aurelien@aurel32.net>

--- hw/spapr.c
+++ hw/spapr.c
@@ -46,7 +46,6 @@
 #include "kvm.h"
 #include "kvm_ppc.h"
 #include "pci.h"
-#include "vga-pci.h"
 
 #include "exec-memory.h"
 #include "hw/usb.h"
@@ -589,11 +588,9 @@ static void spapr_cpu_reset(void *opaque)
 static int spapr_vga_init(PCIBus *pci_bus)
 {
     switch (vga_interface_type) {
-    case VGA_STD:
-        pci_std_vga_init(pci_bus);
-        return 1;
     case VGA_NONE:
-        return 0;
+    case VGA_STD:
+        return pci_vga_init(pci_bus) != NULL;
     default:
         fprintf(stderr, "This vga model is not supported,"
                 "currently it only supports -vga std\n");
