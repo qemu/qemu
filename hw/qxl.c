958c2bceba06696e9c223498aaf81d06ce95f608
Gerd Hoffmann <kraxel@redhat.com>
2012-09-14 22:17:44 +0200

    qxl: fix cursor reset
    When resetting the qxl cursor notify the qemu displaystate too.
    
    Signed-off-by: Gerd Hoffmann <kraxel@redhat.com>

--- hw/qxl.c
+++ hw/qxl.c
@@ -293,6 +293,10 @@ void qxl_spice_reset_cursor(PCIQXLDevice *qxl)
     qemu_mutex_lock(&qxl->track_lock);
     qxl->guest_cursor = 0;
     qemu_mutex_unlock(&qxl->track_lock);
+    if (qxl->ssd.cursor) {
+        cursor_put(qxl->ssd.cursor);
+    }
+    qxl->ssd.cursor = cursor_builtin_hidden();
 }
 
 
