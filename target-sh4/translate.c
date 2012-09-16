f16640f47bb0b8f907fff08a2e8d1c891ce82480
Aurelien Jarno <aurelien@aurel32.net>
2012-09-16 13:12:20 +0200

    target-sh4: optimize xtrct
    The register being 32 bit long, after a shift to the right by 16 bits,
    the upper 16 bit are already cleared. There is no need to call ext16u
    to clear them.
    
    Signed-off-by: Aurelien Jarno <aurelien@aurel32.net>

--- target-sh4/translate.c
+++ target-sh4/translate.c
@@ -751,7 +751,6 @@ static void _decode_opc(DisasContext * ctx)
 	    tcg_gen_shli_i32(high, REG(B7_4), 16);
 	    low = tcg_temp_new();
 	    tcg_gen_shri_i32(low, REG(B11_8), 16);
-	    tcg_gen_ext16u_i32(low, low);
 	    tcg_gen_or_i32(REG(B11_8), high, low);
 	    tcg_temp_free(low);
 	    tcg_temp_free(high);
