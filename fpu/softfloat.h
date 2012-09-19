66176802960ad1f7c3d061d17b784088c2e28d59
Max Filippov <jcmvbkbc@gmail.com>
2012-09-19 04:23:50 +0400

    softfloat: make float_muladd_negate_* flags independent
    Flags passed into float{32,64}_muladd are treated as bits; assign
    independent bits to float_muladd_negate_* to allow precise control over
    what gets negated in float{32,64}_muladd.
    
    Signed-off-by: Max Filippov <jcmvbkbc@gmail.com>
    Reviewed-by: Aurelien Jarno <aurelien@aurel32.net>
    Signed-off-by: Blue Swirl <blauwirbel@gmail.com>

--- fpu/softfloat.h
+++ fpu/softfloat.h
@@ -219,7 +219,7 @@ void float_raise( int8 flags STATUS_PARAM);
 enum {
     float_muladd_negate_c = 1,
     float_muladd_negate_product = 2,
-    float_muladd_negate_result = 3,
+    float_muladd_negate_result = 4,
 };
 
 /*----------------------------------------------------------------------------
