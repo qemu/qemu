3b63392482a72f89e08dbbd221f56fda8907803c
Aurelien Jarno <aurelien@aurel32.net>
2012-09-21 18:20:26 +0200

    tcg/mips: optimize bswap{16,16s,32} on MIPS32R2
    bswap operations can be optimized on MIPS32 Release 2 using the ROTR,
    WSBH and SEH instructions. We can't use the non-R2 code to implement the
    ops due to registers constraints, so don't define the corresponding
    TCG_TARGET_HAS_bswap* values.
    
    Also bswap16* operations are supposed to be called with the 16 high bits
    zeroed. This is the case everywhere (including for TCG by definition)
    except when called from the store helper. Remove the AND instructions from
    bswap16* and move it there.
    
    Reviewed-by: Richard Henderson <rth@twiddle.net>
    Signed-off-by: Aurelien Jarno <aurelien@aurel32.net>
    Signed-off-by: Michael Roth <mdroth@linux.vnet.ibm.com>

--- tcg/mips/tcg-target.h
+++ tcg/mips/tcg-target.h
@@ -83,14 +83,21 @@ typedef enum {
 #define TCG_TARGET_HAS_rot_i32          0
 #define TCG_TARGET_HAS_ext8s_i32        1
 #define TCG_TARGET_HAS_ext16s_i32       1
-#define TCG_TARGET_HAS_bswap32_i32      0
-#define TCG_TARGET_HAS_bswap16_i32      0
 #define TCG_TARGET_HAS_andc_i32         0
 #define TCG_TARGET_HAS_orc_i32          0
 #define TCG_TARGET_HAS_eqv_i32          0
 #define TCG_TARGET_HAS_nand_i32         0
 #define TCG_TARGET_HAS_deposit_i32      0
 #define TCG_TARGET_HAS_movcond_i32      0
+
+/* optional instructions only implemented on MIPS32R2 */
+#ifdef _MIPS_ARCH_MIPS32R2
+#define TCG_TARGET_HAS_bswap16_i32      1
+#define TCG_TARGET_HAS_bswap32_i32      1
+#else
+#define TCG_TARGET_HAS_bswap16_i32      0
+#define TCG_TARGET_HAS_bswap32_i32      0
+#endif
 
 /* optional instructions automatically implemented */
 #define TCG_TARGET_HAS_neg_i32          0 /* sub  rd, zero, rt   */
