a8f2299d1b170b6100b0e9ad00788a7a7e31719b
Peter Maydell <peter.maydell@linaro.org>
2012-09-28 16:17:03 +0100

    fpu/softfloat.c: Return correctly signed values from uint64_to_float32
    The uint64_to_float32() conversion function was incorrectly always
    returning numbers with the sign bit set (ie negative numbers). Correct
    this so we return positive numbers instead.
    
    Signed-off-by: Peter Maydell <peter.maydell@linaro.org>
    Signed-off-by: Aurelien Jarno <aurelien@aurel32.net>
    (cherry picked from commit e744c06fca438dc08271e626034e632a270c91c8)
    
    Signed-off-by: Michael Roth <mdroth@linux.vnet.ibm.com>

--- fpu/softfloat.c
+++ fpu/softfloat.c
@@ -1238,7 +1238,7 @@ float32 uint64_to_float32( uint64 a STATUS_PARAM )
     if ( a == 0 ) return float32_zero;
     shiftCount = countLeadingZeros64( a ) - 40;
     if ( 0 <= shiftCount ) {
-        return packFloat32( 1 > 0, 0x95 - shiftCount, a<<shiftCount );
+        return packFloat32(0, 0x95 - shiftCount, a<<shiftCount);
     }
     else {
         shiftCount += 7;
@@ -1248,7 +1248,7 @@ float32 uint64_to_float32( uint64 a STATUS_PARAM )
         else {
             a <<= shiftCount;
         }
-        return roundAndPackFloat32( 1 > 0, 0x9C - shiftCount, a STATUS_VAR );
+        return roundAndPackFloat32(0, 0x9C - shiftCount, a STATUS_VAR);
     }
 }
 
