4a1def4e4ec2f0eb72b15596a04a030cdc889370
Alexander Graf <agraf@suse.de>
2012-09-29 15:32:38 +0000

    linux-user: ppc: mark as long long aligned
    The SysV PPC32 ABI dictates that long long (64bit) parameters are pass in odd/even
    register pairs. Because unlike ARM and MIPS we start at an odd register number,
    we can reuse the same aligning code that ARM and MIPS use.
    
    Clarified inline comment that it is SysV ABI that requires long long aligned
    parameters - Riku
    
    Signed-off-by: Alexander Graf <agraf@suse.de>
    Signed-off-by: Riku Voipio <riku.voipio@linaro.org>

--- linux-user/syscall.c
+++ linux-user/syscall.c
@@ -587,11 +587,16 @@ extern int setfsgid(int);
 extern int setgroups(int, gid_t *);
 
 /* ARM EABI and MIPS expect 64bit types aligned even on pairs or registers */
-#ifdef TARGET_ARM 
+#ifdef TARGET_ARM
 static inline int regpairs_aligned(void *cpu_env) {
     return ((((CPUARMState *)cpu_env)->eabi) == 1) ;
 }
 #elif defined(TARGET_MIPS)
+static inline int regpairs_aligned(void *cpu_env) { return 1; }
+#elif defined(TARGET_PPC) && !defined(TARGET_PPC64)
+/* SysV AVI for PPC32 expects 64bit parameters to be passed on odd/even pairs
+ * of registers which translates to the same as ARM/MIPS, because we start with
+ * r3 as arg1 */
 static inline int regpairs_aligned(void *cpu_env) { return 1; }
 #else
 static inline int regpairs_aligned(void *cpu_env) { return 0; }
