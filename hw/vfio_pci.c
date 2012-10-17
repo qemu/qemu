a011b10e0c385c58469e654595801a592fb4137a
Alex Williamson <alex.williamson@redhat.com>
2012-10-17 11:20:11 -0600

    vfio-pci: Fix debug build
    Stray variable from before MSI-X rework
    
    Signed-off-by: Alex Williamson <alex.williamson@redhat.com>

--- hw/vfio_pci.c
+++ hw/vfio_pci.c
@@ -639,9 +639,8 @@ static void vfio_disable_msix(VFIODevice *vdev)
 
     vfio_disable_msi_common(vdev);
 
-    DPRINTF("%s(%04x:%02x:%02x.%x, msi%s)\n", __func__,
-            vdev->host.domain, vdev->host.bus, vdev->host.slot,
-            vdev->host.function, msix ? "x" : "");
+    DPRINTF("%s(%04x:%02x:%02x.%x)\n", __func__, vdev->host.domain,
+            vdev->host.bus, vdev->host.slot, vdev->host.function);
 }
 
 static void vfio_disable_msi(VFIODevice *vdev)
