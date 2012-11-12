fc6467eaf22f974620b5dd046afca04e619f9be9
Paolo Bonzini <pbonzini@redhat.com>
2012-11-12 14:12:54 +0100

    nbd: fix nbd_server_stop crash when no server was running
    This failed on the new assertion of qemu_set_fd_handler2:
    
    qemu-system-x86_64: /home/pbonzini/work/upstream/qemu/iohandler.c:60: qemu_set_fd_handler2: Assertion `fd >= 0' failed.
    
    Signed-off-by: Paolo Bonzini <pbonzini@redhat.com>

--- blockdev-nbd.c
+++ blockdev-nbd.c
@@ -113,7 +113,9 @@ void qmp_nbd_server_stop(Error **errp)
         nbd_close_notifier(&cn->n, nbd_export_get_blockdev(cn->exp));
     }
 
-    qemu_set_fd_handler2(server_fd, NULL, NULL, NULL, NULL);
-    close(server_fd);
-    server_fd = -1;
+    if (server_fd != -1) {
+        qemu_set_fd_handler2(server_fd, NULL, NULL, NULL, NULL);
+        close(server_fd);
+        server_fd = -1;
+    }
 }
