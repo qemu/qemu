daa8e5a077e9d8ca2615f5590fa98b3fcc4c9f47
Gerd Hoffmann <kraxel@redhat.com>
2012-11-13 09:38:06 +0100

    pixman: add licensing info
    Signed-off-by: Gerd Hoffmann <kraxel@redhat.com>

--- qemu-pixman.h
+++ qemu-pixman.h
@@ -1,3 +1,8 @@
+/*
+ * This work is licensed under the terms of the GNU GPL, version 2 or later.
+ * See the COPYING file in the top-level directory.
+ */
+
 #ifndef QEMU_PIXMAN_H
 #define QEMU_PIXMAN_H
 
