773de5c786a6050bbf3b33c0e29d1bd519a40b4b
Cornelia Huck <cornelia.huck@de.ibm.com>
2012-11-23 00:18:12 +0000

    sclp: Fix uninitialized var in handle_write_event_buf().
    Some gcc versions rightly complain about a possibly unitialized rc,
    so let's move setting it before the QTAILQ_FOREACH().
    
    Signed-off-by: Cornelia Huck <cornelia.huck@de.ibm.com>
    Signed-off-by: Christian Borntraeger <borntraeger@de.ibm.com>
    Signed-off-by: Jens Freimann <jfrei@linux.vnet.ibm.com>
    Signed-off-by: Alexander Graf <agraf@suse.de>

--- hw/s390x/event-facility.c
+++ hw/s390x/event-facility.c
@@ -112,12 +112,13 @@ static uint16_t handle_write_event_buf(SCLPEventFacility *ef,
     SCLPEvent *event;
     SCLPEventClass *ec;
 
+    rc = SCLP_RC_INVALID_FUNCTION;
+
     QTAILQ_FOREACH(kid, &ef->sbus.qbus.children, sibling) {
         DeviceState *qdev = kid->child;
         event = (SCLPEvent *) qdev;
         ec = SCLP_EVENT_GET_CLASS(event);
 
-        rc = SCLP_RC_INVALID_FUNCTION;
         if (ec->write_event_data &&
             ec->event_type() == event_buf->type) {
             rc = ec->write_event_data(event, event_buf);
