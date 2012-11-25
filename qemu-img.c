a12d975f65bf278769896755af1de08bc518e957
Stefan Weil <sw@weilnetz.de>
2012-11-25 21:33:23 +0100

    qemu-img: Fix compilation (missing parameter)
    Commit f0536bb8 added a parameter to function bdrv_new_open.
    
    Signed-off-by: Stefan Weil <sw@weilnetz.de>

--- qemu-img.c
+++ qemu-img.c
@@ -1497,7 +1497,8 @@ static int img_update(int argc, char **argv)
         help();
     filename = argv[optind++];
 
-    bs = bdrv_new_open(filename, fmt, BDRV_O_FLAGS | BDRV_O_NO_BACKING | BDRV_O_RDWR);
+    bs = bdrv_new_open(filename, fmt,
+                       BDRV_O_FLAGS | BDRV_O_NO_BACKING | BDRV_O_RDWR, true);
     if (!bs) {
         return 1;
     }
