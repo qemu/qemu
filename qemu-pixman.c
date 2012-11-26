6e72719e721a40fe1224701ca10edc1caf0cd708
Alexander Graf <agraf@suse.de>
2012-11-26 19:49:58 +0100

    fbdev: fix pixman compile on old pixman
    My QEMU compile failed with the following error:
    
    qemu-pixman.c: In function ‘qemu_pixman_get_type’:
    qemu-pixman.c:24: error: ‘PIXMAN_TYPE_BGRA’ undeclared (first use in this function)
    qemu-pixman.c:24: error: (Each undeclared identifier is reported only once
    qemu-pixman.c:24: error: for each function it appears in.)
    
    Guard the PIXMAN_TYPE_BGRA branch like in the case right above the failing
    case, so that compilation is fixed. Functionality on such old pixman is a
    different question ;-).
    
    Signed-off-by: Alexander Graf <agraf@suse.de>

--- qemu-pixman.c
+++ qemu-pixman.c
@@ -21,7 +21,9 @@ int qemu_pixman_get_type(int rshift, int gshift, int bshift)
         if (rshift == 0) {
             type = PIXMAN_TYPE_ABGR;
         } else {
+#if PIXMAN_VERSION >= PIXMAN_VERSION_ENCODE(0, 21, 8)
             type = PIXMAN_TYPE_BGRA;
+#endif
         }
     }
     return type;
