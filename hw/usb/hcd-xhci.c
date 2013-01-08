f3dcf6384cc94b6a688f3a366c20642f36247b68
Gerd Hoffmann <kraxel@redhat.com>
2013-01-08 13:06:57 +0100

    xhci: call xhci_detach_slot on root port detach too
    Signed-off-by: Gerd Hoffmann <kraxel@redhat.com>

--- hw/usb/hcd-xhci.c
+++ hw/usb/hcd-xhci.c
@@ -2957,6 +2957,7 @@ static void xhci_detach(USBPort *usbport)
     XHCIState *xhci = usbport->opaque;
     XHCIPort *port = xhci_lookup_port(xhci, usbport);
 
+    xhci_detach_slot(xhci, usbport);
     xhci_port_update(port, 1);
 }
 
