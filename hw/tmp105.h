6d0b430176e3571af0e1596276078f05bfe1c5a5
Andreas Färber <andreas.faerber@web.de>
2013-01-16 01:57:55 +0100

    tmp105: Split out I2C message constants from header
    Allows value sharing with qtest.
    
    Signed-off-by: Andreas Färber <andreas.faerber@web.de>
    Reviewed-by: Anthony Liguori <aliguori@us.ibm.com>
    Signed-off-by: Anthony Liguori <aliguori@us.ibm.com>

--- hw/tmp105.h
+++ hw/tmp105.h
@@ -15,39 +15,7 @@
 #define QEMU_TMP105_H
 
 #include "i2c.h"
-
-/**
- * TMP105Reg:
- * @TMP105_REG_TEMPERATURE: Temperature register
- * @TMP105_REG_CONFIG: Configuration register
- * @TMP105_REG_T_LOW: Low temperature register (also known as T_hyst)
- * @TMP105_REG_T_HIGH: High temperature register (also known as T_OS)
- *
- * The following temperature sensors are
- * compatible with the TMP105 registers:
- * - adt75
- * - ds1775
- * - ds75
- * - lm75
- * - lm75a
- * - max6625
- * - max6626
- * - mcp980x
- * - stds75
- * - tcn75
- * - tmp100
- * - tmp101
- * - tmp105
- * - tmp175
- * - tmp275
- * - tmp75
- **/
-typedef enum TMP105Reg {
-    TMP105_REG_TEMPERATURE = 0,
-    TMP105_REG_CONFIG,
-    TMP105_REG_T_LOW,
-    TMP105_REG_T_HIGH,
-} TMP105Reg;
+#include "tmp105_regs.h"
 
 /**
  * tmp105_set:
