0dac84597a66eb8f416972faef451ce786b3cdd4
Andreas Färber <afaerber@suse.de>
2013-02-02 12:10:37 +0100

    target-s390x: Drop unused cpu_s390x_close() prototype
    It was never implemented.
    
    Signed-off-by: Andreas Färber <afaerber@suse.de>

--- target-s390x/cpu.h
+++ target-s390x/cpu.h
@@ -315,7 +315,6 @@ static inline int get_ilen(uint8_t opc)
 S390CPU *cpu_s390x_init(const char *cpu_model);
 void s390x_translate_init(void);
 int cpu_s390x_exec(CPUS390XState *s);
-void cpu_s390x_close(CPUS390XState *s);
 void do_interrupt (CPUS390XState *env);
 
 /* you can call this signal handler from your SIGBUS and SIGSEGV
