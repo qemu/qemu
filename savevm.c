13c7b2da073ec83cb47f9582149c8d28bb038e73
Paolo Bonzini <pbonzini@redhat.com>
2013-02-22 17:36:38 +0100

    qemu-file: check exit status when closing a pipe QEMUFile
    This is what exec_close does.  Move this to the underlying QEMUFile.
    
    Reviewed-by: Orit Wasserman <owasserm@redhat.com>
    Reviewed-by: Juan Quintela <quintela@redhat.com>
    Signed-off-by: Paolo Bonzini <pbonzini@redhat.com>
    Signed-off-by: Juan Quintela <quintela@redhat.com>

--- savevm.c
+++ savevm.c
@@ -247,6 +247,9 @@ static int stdio_pclose(void *opaque)
     ret = pclose(s->stdio_file);
     if (ret == -1) {
         ret = -errno;
+    } else if (!WIFEXITED(ret) || WEXITSTATUS(ret) != 0) {
+        /* close succeeded, but non-zero exit code: */
+        ret = -EIO; /* fake errno value */
     }
     g_free(s);
     return ret;
