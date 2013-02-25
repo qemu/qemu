cba68834c69f2d0fd04127301171fedac63d9b67
Daniel P. Berrange <berrange@redhat.com>
2013-02-25 15:20:34 +0000

    Add compat for gdk_drawable_get_size on GTK3
    GTK3 lacks the gdk_drawable_get_size method, so we create a
    stub impl which gets the get_width/get_height mehtods instead
    
    Signed-off-by: Daniel P. Berrange <berrange@redhat.com>
    Message-id: 1361805646-6425-2-git-send-email-berrange@redhat.com
    Signed-off-by: Anthony Liguori <aliguori@us.ibm.com>

--- ui/gtk.c
+++ ui/gtk.c
@@ -74,6 +74,16 @@
 
 #define MAX_VCS 10
 
+
+/* Compatibility define to let us build on both Gtk2 and Gtk3 */
+#if GTK_CHECK_VERSION(3, 0, 0)
+static inline void gdk_drawable_get_size(GdkWindow *w, gint *ww, gint *wh)
+{
+    *ww = gdk_window_get_width(w);
+    *wh = gdk_window_get_height(w);
+}
+#endif
+
 typedef struct VirtualConsole
 {
     GtkWidget *menu_item;
