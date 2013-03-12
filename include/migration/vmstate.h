8474a9dd6757be064bf4b35f422b4640d1cca0a5
David Gibson <david@gibson.dropbear.id.au>
2013-03-12 14:06:03 +1100

    savevm: Add VMSTATE_STRUCT_VARRAY_POINTER_UINT32
    Currently the savevm code contains a VMSTATE_STRUCT_VARRAY_POINTER_INT32
    helper (a variably sized array with the number of elements in an int32_t),
    but not VMSTATE_STRUCT_VARRAY_POINTER_UINT32 (... with the number of
    elements in a uint32_t).  This patch (trivially) fixes the deficiency.
    
    Signed-off-by: David Gibson <david@gibson.dropbear.id.au>
    Signed-off-by: Juan Quintela <quintela@redhat.com>

--- include/migration/vmstate.h
+++ include/migration/vmstate.h
@@ -343,6 +343,16 @@ extern const VMStateInfo vmstate_info_bitmap;
     .offset     = vmstate_offset_pointer(_state, _field, _type),     \
 }
 
+#define VMSTATE_STRUCT_VARRAY_POINTER_UINT32(_field, _state, _field_num, _vmsd, _type) { \
+    .name       = (stringify(_field)),                               \
+    .version_id = 0,                                                 \
+    .num_offset = vmstate_offset_value(_state, _field_num, uint32_t),\
+    .size       = sizeof(_type),                                     \
+    .vmsd       = &(_vmsd),                                          \
+    .flags      = VMS_POINTER | VMS_VARRAY_INT32 | VMS_STRUCT,       \
+    .offset     = vmstate_offset_pointer(_state, _field, _type),     \
+}
+
 #define VMSTATE_STRUCT_VARRAY_POINTER_UINT16(_field, _state, _field_num, _vmsd, _type) { \
     .name       = (stringify(_field)),                               \
     .version_id = 0,                                                 \
