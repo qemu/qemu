e998fa8df828ef68ea540a12917d10b4d335c1dd
Michael S. Tsirkin <mst@redhat.com>
2013-03-18 21:01:37 +0200

    qom: call class destructor before unparent
    It seems more logical to have destruction flow start with the subclass
    and move up to the base class.  This ensures object has a valid
    canonical path when destructor is called.
    
    Signed-off-by: Michael S. Tsirkin <mst@redhat.com>

--- qom/object.c
+++ qom/object.c
@@ -363,11 +363,11 @@ static void object_property_del_child(Object *obj, Object *child, Error **errp)
 void object_unparent(Object *obj)
 {
     object_ref(obj);
-    if (obj->parent) {
-        object_property_del_child(obj->parent, obj, NULL);
-    }
     if (obj->class->unparent) {
         (obj->class->unparent)(obj);
+    }
+    if (obj->parent) {
+        object_property_del_child(obj->parent, obj, NULL);
     }
     object_unref(obj);
 }
