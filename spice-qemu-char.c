e280ff5e9159ed227a117339c1157143627cab96
Hans de Goede <hdegoede@redhat.com>
2013-03-26 11:08:03 +0100

    spice-qemu-char: Drop hackish vmc_register on spice_chr_write
    Now that the core takes care of fe_open tracking we no longer need this hack.
    
    Signed-off-by: Hans de Goede <hdegoede@redhat.com>
    Message-id: 1364292483-16564-12-git-send-email-hdegoede@redhat.com
    Signed-off-by: Anthony Liguori <aliguori@us.ibm.com>

--- spice-qemu-char.c
+++ spice-qemu-char.c
@@ -149,7 +149,6 @@ static int spice_chr_write(CharDriverState *chr, const uint8_t *buf, int len)
 {
     SpiceCharDriver *s = chr->opaque;
 
-    vmc_register_interface(s);
     assert(s->datalen == 0);
     if (s->bufsize < len) {
         s->bufsize = len;
