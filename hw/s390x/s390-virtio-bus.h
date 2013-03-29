d6e51919a7e3250bbfb4bb0ad0f208ab6fd688a4
Paolo Bonzini <pbonzini@redhat.com>
2013-03-29 01:08:16 +0000

    vhost-scsi-s390: new device supporting the tcm_vhost Linux kernel module
    Signed-off-by: Paolo Bonzini <pbonzini@redhat.com>
    Signed-off-by: Asias He <asias@redhat.com>
    Cc: Michael S. Tsirkin <mst@redhat.com>
    Signed-off-by: Nicholas Bellinger <nab@linux-iscsi.org>
    Signed-off-by: Paolo Bonzini <pbonzini@redhat.com>

--- hw/s390x/s390-virtio-bus.h
+++ hw/s390x/s390-virtio-bus.h
@@ -25,6 +25,9 @@
 #include "hw/virtio/virtio-serial.h"
 #include "hw/virtio/virtio-scsi.h"
 #include "hw/virtio/virtio-bus.h"
+#ifdef CONFIG_VHOST_SCSI
+#include "hw/virtio/vhost-scsi.h"
+#endif
 
 #define VIRTIO_DEV_OFFS_TYPE		0	/* 8 bits */
 #define VIRTIO_DEV_OFFS_NUM_VQ		1	/* 8 bits */
@@ -159,5 +162,18 @@ typedef struct VirtIONetS390 {
     VirtIOS390Device parent_obj;
     VirtIONet vdev;
 } VirtIONetS390;
+
+/* vhost-scsi-s390 */
+
+#ifdef CONFIG_VHOST_SCSI
+#define TYPE_VHOST_SCSI_S390 "vhost-scsi-s390"
+#define VHOST_SCSI_S390(obj) \
+        OBJECT_CHECK(VHostSCSIS390, (obj), TYPE_VHOST_SCSI_S390)
+
+typedef struct VHostSCSIS390 {
+    VirtIOS390Device parent_obj;
+    VHostSCSI vdev;
+} VHostSCSIS390;
+#endif
 
 #endif
