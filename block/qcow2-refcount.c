c2bc78b6a975ea2dcd7eee9f0dce22cc060cdcdc
Kevin Wolf <kwolf@redhat.com>
2013-04-05 12:51:31 +0200

    qcow2: Return real error in qcow2_update_snapshot_refcount
    This fixes the error message triggered by the following script:
    
        cat > /tmp/blkdebug.cfg <<EOF
        [inject-error]
        event = "cluster_free"
        errno = "28"
        immediately = "off"
        EOF
    
        $qemu_img create -f qcow2 test.qcow2 10G
        $qemu_img snapshot -c snap test.qcow2
        $qemu_img snapshot -d snap blkdebug:/tmp/blkdebug.cfg:test.qcow2
    
    Signed-off-by: Kevin Wolf <kwolf@redhat.com>

--- block/qcow2-refcount.c
+++ block/qcow2-refcount.c
@@ -747,10 +747,9 @@ int qcow2_update_snapshot_refcount(BlockDriverState *bs,
     if (l1_table_offset != s->l1_table_offset) {
         l1_table = g_malloc0(align_offset(l1_size2, 512));
         l1_allocated = 1;
-        if (bdrv_pread(bs->file, l1_table_offset,
-                       l1_table, l1_size2) != l1_size2)
-        {
-            ret = -EIO;
+
+        ret = bdrv_pread(bs->file, l1_table_offset, l1_table, l1_size2);
+        if (ret < 0) {
             goto fail;
         }
 
@@ -802,7 +801,7 @@ int qcow2_update_snapshot_refcount(BlockDriverState *bs,
                         }
 
                         if (refcount < 0) {
-                            ret = -EIO;
+                            ret = refcount;
                             goto fail;
                         }
                     }
@@ -833,7 +832,7 @@ int qcow2_update_snapshot_refcount(BlockDriverState *bs,
                 refcount = get_refcount(bs, l2_offset >> s->cluster_bits);
             }
             if (refcount < 0) {
-                ret = -EIO;
+                ret = refcount;
                 goto fail;
             } else if (refcount == 1) {
                 l2_offset |= QCOW_OFLAG_COPIED;
