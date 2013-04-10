0ca5aa4f4c4a8bcc73988dd52a536241d35e5223
Paolo Bonzini <pbonzini@redhat.com>
2013-04-10 15:23:27 +0200

    qemu-char: another io_add_watch_poll fix
    After attaching the source, we have to remove the reference we hold
    to it, because we do not hold anymore a pointer to the source.
    
    If we do not do this, removing the source will not finalize it and
    will not drop the "real" I/O watch source.
    
    This showed up when backporting the new flow control patches to older
    versions of QEMU that still used select.  The whole select then failed
    with EBADF (poll instead will reporting POLLNVAL on a single pollfd)
    and QEMU froze.
    
    Signed-off-by: Paolo Bonzini <pbonzini@redhat.com>
    Message-id: 1365600207-21685-1-git-send-email-pbonzini@redhat.com
    Signed-off-by: Anthony Liguori <aliguori@us.ibm.com>

--- qemu-char.c
+++ qemu-char.c
@@ -663,6 +663,7 @@ static guint io_add_watch_poll(GIOChannel *channel,
                                gpointer user_data)
 {
     IOWatchPoll *iwp;
+    int tag;
 
     iwp = (IOWatchPoll *) g_source_new(&io_watch_poll_funcs, sizeof(IOWatchPoll));
     iwp->fd_can_read = fd_can_read;
@@ -671,7 +672,9 @@ static guint io_add_watch_poll(GIOChannel *channel,
     iwp->fd_read = (GSourceFunc) fd_read;
     iwp->src = NULL;
 
-    return g_source_attach(&iwp->parent, NULL);
+    tag = g_source_attach(&iwp->parent, NULL);
+    g_source_unref(&iwp->parent);
+    return tag;
 }
 
 #ifndef _WIN32
