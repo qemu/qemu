0e41271ec4944ea5b63c98b8e497618709e954c3
Andreas Färber <afaerber@suse.de>
2013-04-27 22:18:43 +0200

    mc146818rtc: QOM'ify
    Introduce type constant and cast macro to obsolete DO_UPCAST().
    
    Prepares for ISA realizefn.
    
    Signed-off-by: Andreas Färber <afaerber@suse.de>
    Reviewed-by: Igor Mammedov <imammedo@redhat.com>
    Signed-off-by: Andreas Färber <afaerber@suse.de>
    Message-id: 1367093935-29091-9-git-send-email-afaerber@suse.de
    Signed-off-by: Anthony Liguori <aliguori@us.ibm.com>

--- include/hw/timer/mc146818rtc.h
+++ include/hw/timer/mc146818rtc.h
@@ -4,6 +4,8 @@
 #include "hw/isa/isa.h"
 #include "hw/timer/mc146818rtc_regs.h"
 
+#define TYPE_MC146818_RTC "mc146818rtc"
+
 ISADevice *rtc_init(ISABus *bus, int base_year, qemu_irq intercept_irq);
 void rtc_set_memory(ISADevice *dev, int addr, int val);
 void rtc_set_date(ISADevice *dev, const struct tm *tm);
