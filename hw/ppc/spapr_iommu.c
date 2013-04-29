b55519a0f29fca0ef7ebc5d87ee4bfa7304dc219
David Gibson <david@gibson.dropbear.id.au>
2013-04-29 18:33:52 +0000

    pseries: Fix debug message for out-of-bounds address in H_PUT_TCE
    Due to a brain outage, this message says "out-of-boards" instead of
    "out-of-bounds".
    
    Signed-off-by: David Gibson <david@gibson.dropbear.id.au>
    Signed-off-by: Alexey Kardashevskiy <aik@ozlabs.ru>
    Signed-off-by: Alexander Graf <agraf@suse.de>

--- hw/ppc/spapr_iommu.c
+++ hw/ppc/spapr_iommu.c
@@ -205,7 +205,7 @@ static target_ulong put_tce_emu(sPAPRTCETable *tcet, target_ulong ioba,
     sPAPRTCE *tcep;
 
     if (ioba >= tcet->window_size) {
-        hcall_dprintf("spapr_vio_put_tce on out-of-boards IOBA 0x"
+        hcall_dprintf("spapr_vio_put_tce on out-of-bounds IOBA 0x"
                       TARGET_FMT_lx "\n", ioba);
         return H_PARAMETER;
     }
