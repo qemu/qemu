d2ad50210b35c2685640f7df931a0fccb49334f7
Hu Tao <hutao@cn.fujitsu.com>
2013-05-15 14:04:29 +0800

    target-moxie: replace target_phys_addr_t with hwaddr
    target_phys_addr_t has been already replaced by hwaddr, but this
    one is introduced after.
    
    Signed-off-by: Hu Tao <hutao@cn.fujitsu.com>
    Reviewed-by: Peter Maydell <peter.maydell@linaro.org>
    Signed-off-by: Michael Tokarev <mjt@tls.msk.ru>

--- target-moxie/helper.c
+++ target-moxie/helper.c
@@ -116,7 +116,7 @@ int cpu_moxie_handle_mmu_fault(CPUMoxieState *env, target_ulong address,
     return 1;
 }
 
-target_phys_addr_t cpu_get_phys_page_debug(CPUState *env, target_ulong addr)
+hwaddr cpu_get_phys_page_debug(CPUState *env, target_ulong addr)
 {
     return addr;
 }
