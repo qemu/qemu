08521e28c7e6e8cc1f53424a0f845f58d2ed9546
Paolo Bonzini <pbonzini@redhat.com>
2013-05-24 12:54:01 +0200

    memory: add big endian support to access_with_adjusted_size
    This will be used to split 8-byte access down to two four-byte accesses.
    
    Reviewed-by: Richard Henderson <rth@twiddle.net>
    Signed-off-by: Paolo Bonzini <pbonzini@redhat.com>

--- memory.c
+++ memory.c
@@ -362,8 +362,12 @@ static void access_with_adjusted_size(hwaddr addr,
     access_size = MAX(MIN(size, access_size_max), access_size_min);
     access_mask = -1ULL >> (64 - access_size * 8);
     for (i = 0; i < size; i += access_size) {
-        /* FIXME: big-endian support */
+#ifdef TARGET_WORDS_BIGENDIAN
+        access(opaque, addr + i, value, access_size,
+               (size - access_size - i) * 8, access_mask);
+#else
         access(opaque, addr + i, value, access_size, i * 8, access_mask);
+#endif
     }
 }
 
