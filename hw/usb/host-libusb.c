bc45de8c21dfc3bc3896a78b33d5a6d9e710dfff
Hans de Goede <hdegoede@redhat.com>
2013-06-06 16:39:34 +0200

    usb/host-libusb: Fix building with libusb git master code
    The next libusb release will deprecate libusb_get_port_path, and since
    we compile with -Werror, this breaks the build.
    
    Signed-off-by: Hans de Goede <hdegoede@redhat.com>
    Signed-off-by: Gerd Hoffmann <kraxel@redhat.com>

--- hw/usb/host-libusb.c
+++ hw/usb/host-libusb.c
@@ -241,7 +241,11 @@ static int usb_host_get_port(libusb_device *dev, char *port, size_t len)
     size_t off;
     int rc, i;
 
+#if LIBUSBX_API_VERSION >= 0x01000102
+    rc = libusb_get_port_numbers(dev, path, 7);
+#else
     rc = libusb_get_port_path(ctx, dev, path, 7);
+#endif
     if (rc < 0) {
         return 0;
     }
