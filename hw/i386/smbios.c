93bc62438402a21c6b6fedc3c11c951438d506e0
Markus Armbruster <armbru@redhat.com>
2013-06-07 15:00:35 +0200

    smbios: Fix -smbios type=0, release=... for big endian hosts
    Classic endianness bug due to careless dirty coding: assuming reading
    a byte from an int variable gets the least significant byte.
    
    Signed-off-by: Markus Armbruster <armbru@redhat.com>
    Reviewed-by: Laszlo "ever the optimist" Ersek <lersek@redhat.com>
    Message-id: 1370610036-10577-6-git-send-email-armbru@redhat.com
    Signed-off-by: Anthony Liguori <aliguori@us.ibm.com>
    (cherry picked from commit 527cd96f15348f4454d5bb71f69aaeb95461c90e)
    
    Signed-off-by: Michael Roth <mdroth@linux.vnet.ibm.com>

--- hw/i386/smbios.c
+++ hw/i386/smbios.c
@@ -127,6 +127,7 @@ void smbios_add_field(int type, int offset, const void *data, size_t len)
 static void smbios_build_type_0_fields(const char *t)
 {
     char buf[1024];
+    unsigned char major, minor;
 
     if (get_param_value(buf, sizeof(buf), "vendor", t))
         smbios_add_field(0, offsetof(struct smbios_type_0, vendor_str),
@@ -139,8 +140,7 @@ static void smbios_build_type_0_fields(const char *t)
                                      bios_release_date_str),
                          buf, strlen(buf) + 1);
     if (get_param_value(buf, sizeof(buf), "release", t)) {
-        int major, minor;
-        sscanf(buf, "%d.%d", &major, &minor);
+        sscanf(buf, "%hhu.%hhu", &major, &minor);
         smbios_add_field(0, offsetof(struct smbios_type_0,
                                      system_bios_major_release),
                          &major, 1);
