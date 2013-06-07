fef7fbc92496f5f6d2b7395263830bce15ebf410
Andreas Färber <afaerber@suse.de>
2013-06-07 14:45:17 +0200

    qdev: Drop FROM_QBUS() macro
    Use QOM cast macros I2C_BUS(), SSI_BUS(), PCI_BUS() instead.
    
    Signed-off-by: Andreas Färber <afaerber@suse.de>

--- hw/pci/pci.c
+++ hw/pci/pci.c
@@ -1700,7 +1700,7 @@ static int pci_qdev_init(DeviceState *qdev)
         pci_dev->cap_present |= QEMU_PCI_CAP_EXPRESS;
     }
 
-    bus = FROM_QBUS(PCIBus, qdev_get_parent_bus(qdev));
+    bus = PCI_BUS(qdev_get_parent_bus(qdev));
     pci_dev = do_pci_register_device(pci_dev, bus,
                                      object_get_typename(OBJECT(qdev)),
                                      pci_dev->devfn);
