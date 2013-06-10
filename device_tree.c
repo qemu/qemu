909a196d2754388d3b15a34b688095c655792124
Stefan Weil <sw@weilnetz.de>
2013-06-10 22:12:25 +0200

    device tree: Fix cppcheck warning
    Fix this cppcheck warning:
    
    Checking device_tree.c...
    device_tree.c:216: style:
     Checking if unsigned variable 'r' is less than zero.
    
    Signed-off-by: Stefan Weil <sw@weilnetz.de>
    Signed-off-by: Michael Tokarev <mjt@tls.msk.ru>

--- device_tree.c
+++ device_tree.c
@@ -213,7 +213,7 @@ uint32_t qemu_devtree_get_phandle(void *fdt, const char *path)
     uint32_t r;
 
     r = fdt_get_phandle(fdt, findnode_nofail(fdt, path));
-    if (r <= 0) {
+    if (r == 0) {
         fprintf(stderr, "%s: Couldn't get phandle for %s: %s\n", __func__,
                 path, fdt_strerror(r));
         exit(1);
