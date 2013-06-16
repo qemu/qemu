bad76c33fe97dc91b8d7726f354b4cb1356c46da
Stefan Weil <sw@weilnetz.de>
2013-06-16 17:38:23 +0200

    Revert "audio: Replace macro GCC_ATTR by GCC_FMT_ATTR and use inline functions"
    This reverts commit 4c540edebe40a3c3606c46e9ed1f26c2293eade8.

--- audio/audio_int.h
+++ audio/audio_int.h
@@ -243,7 +243,7 @@ static inline int audio_ring_dist (int dst, int src, int len)
     return (dst >= src) ? (dst - src) : (len - src + dst);
 }
 
-static inline void GCC_FMT_ATTR(1, 2) dolog (const char *fmt, ...)
+static void GCC_ATTR dolog (const char *fmt, ...)
 {
     va_list ap;
 
@@ -253,7 +253,7 @@ static inline void GCC_FMT_ATTR(1, 2) dolog (const char *fmt, ...)
 }
 
 #ifdef DEBUG
-static inline void GCC_FMT_ATTR(1, 2) ldebug (const char *fmt, ...)
+static void GCC_ATTR ldebug (const char *fmt, ...)
 {
     va_list ap;
 
@@ -267,12 +267,14 @@ static inline void GCC_FMT_ATTR(1, 2) ldebug (const char *fmt, ...)
 #elif defined NDEBUG && defined _MSC_VER
 #define ldebug __noop
 #else
-static inline void GCC_FMT_ATTR(1, 2) ldebug (const char *fmt, ...)
+static void GCC_ATTR ldebug (const char *fmt, ...)
 {
     (void) fmt;
 }
 #endif
 #endif
+
+#undef GCC_ATTR
 
 #define AUDIO_STRINGIFY_(n) #n
 #define AUDIO_STRINGIFY(n) AUDIO_STRINGIFY_(n)
