670599a08c052f6ef841743731a8f06d4b50ec99
Stefan Hajnoczi <stefanha@redhat.com>
2013-06-27 15:32:26 +0200

    block: ensure bdrv_drain_all() works during bdrv_delete()
    In bdrv_delete() make sure to call bdrv_make_anon() *after* bdrv_close()
    so that the device is still seen by bdrv_drain_all() when iterating
    bdrv_states.
    
    Cc: qemu-stable@nongnu.org
    Signed-off-by: Stefan Hajnoczi <stefanha@redhat.com>
    (cherry picked from commit e1b5c52e04d04bb93546c6e37e8884889d047cb1)
    
    Signed-off-by: Michael Roth <mdroth@linux.vnet.ibm.com>

--- block.c
+++ block.c
@@ -1606,10 +1606,10 @@ void bdrv_delete(BlockDriverState *bs)
     assert(!bs->job);
     assert(!bs->in_use);
 
+    bdrv_close(bs);
+
     /* remove from list, if necessary */
     bdrv_make_anon(bs);
-
-    bdrv_close(bs);
 
     g_free(bs);
 }
