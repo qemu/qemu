4be1db86060d803f2335c08a483218eb6a8bd9eb
Alexander Graf <agraf@suse.de>
2013-06-28 13:47:15 +0200

    PPC: Add non-kvm stub file
    There are cases where a kvm provided function is called from generic
    hw code that doesn't know whether kvm is available or not. Provide
    a stub file which can provide simple replacement functions for those
    cases.
    
    Signed-off-by: Alexander Graf <agraf@suse.de>
    Reviewed-by: Paolo Bonzini <pbonzini@redhat.com>

---
+++ target-ppc/kvm-stub.c
@@ -0,0 +1,12 @@
+/*
+ * QEMU KVM PPC specific function stubs
+ *
+ * Copyright Freescale Inc. 2013
+ *
+ * Author: Alexander Graf <agraf@suse.de>
+ *
+ * This work is licensed under the terms of the GNU GPL, version 2 or later.
+ * See the COPYING file in the top-level directory.
+ *
+ */
+#include "qemu-common.h"
