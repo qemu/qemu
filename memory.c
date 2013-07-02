b476c99d01519277e3494a10dc0329d07157ae02
Jan Kiszka <jan.kiszka@siemens.com>
2013-07-02 16:51:15 +0200

    memory: remove assertion on memory_region_destroy
    Now that memory_region_destroy can be called from an RCU callback,
    checking the BQL-protected global memory_region_transaction_depth
    does not make much sense.
    
    Signed-off-by: Jan Kiszka <jan.kiszka@siemens.com>
    Reviewed-by: Fam Zheng <famz@redhat.com>
    Signed-off-by: Paolo Bonzini <pbonzini@redhat.com>

--- memory.c
+++ memory.c
@@ -1263,7 +1263,6 @@ static void memory_region_finalize(Object *obj)
     MemoryRegion *mr = MEMORY_REGION(obj);
 
     assert(QTAILQ_EMPTY(&mr->subregions));
-    assert(memory_region_transaction_depth == 0);
     mr->destructor(mr);
     memory_region_clear_coalescing(mr);
     g_free((char *)mr->name);
