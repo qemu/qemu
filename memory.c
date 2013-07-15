b4afea11aafe85975e74dd562bb94f7ce3de1ef1
Paolo Bonzini <pbonzini@redhat.com>
2013-07-15 15:48:50 +0200

    memory: actually set the owner
    Brown paper bag for me.  Originally commit 803c0816 came before commit
    2c9b15c.  When the order was inverted, I left in the NULL initialization
    of mr->owner.
    
    Reviewed-by: Hu Tao <hutao@cn.fujitsu.com>
    Signed-off-by: Paolo Bonzini <pbonzini@redhat.com>

--- memory.c
+++ memory.c
@@ -805,7 +805,6 @@ void memory_region_init(MemoryRegion *mr,
     mr->owner = owner;
     mr->iommu_ops = NULL;
     mr->parent = NULL;
-    mr->owner = NULL;
     mr->size = int128_make64(size);
     if (size == UINT64_MAX) {
         mr->size = int128_2_64();
