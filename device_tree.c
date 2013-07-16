97c38f8c279ae2f71cb0f6aed11aa94ef59b2955
Peter Maydell <peter.maydell@linaro.org>
2013-07-16 13:25:05 +0100

    device_tree: Add qemu_devtree_setprop_sized_cells() utility functions
    We already have a qemu_devtree_setprop_cells() which sets a dtb
    property to an array of cells whose values are specified by varargs.
    However for the fairly common case of setting a property to a list
    of addresses or of address,size pairs the number of cells used by
    each element in the list depends on the parent's #address-cells
    and #size-cells properties. To make this easier we provide an analogous
    qemu_devtree_setprop_sized_cells() macro which allows the number
    of cells used by each element to be specified. This is implemented
    using an underlying qemu_devtree_setprop_sized_cells_from_array()
    function which takes the values and sizes as an array; this may
    also be directly useful for cases where the cell contents are
    constructed programmatically.
    
    Signed-off-by: Peter Maydell <peter.maydell@linaro.org>
    Reviewed-by: Peter Crosthwaite <peter.crosthwaite@xilinx.com>
    Message-id: 1373977512-28932-2-git-send-email-peter.maydell@linaro.org

--- device_tree.c
+++ device_tree.c
@@ -308,3 +308,36 @@ void qemu_devtree_dumpdtb(void *fdt, int size)
         exit(g_file_set_contents(dumpdtb, fdt, size, NULL) ? 0 : 1);
     }
 }
+
+int qemu_devtree_setprop_sized_cells_from_array(void *fdt,
+                                                const char *node_path,
+                                                const char *property,
+                                                int numvalues,
+                                                uint64_t *values)
+{
+    uint32_t *propcells;
+    uint64_t value;
+    int cellnum, vnum, ncells;
+    uint32_t hival;
+
+    propcells = g_new0(uint32_t, numvalues * 2);
+
+    cellnum = 0;
+    for (vnum = 0; vnum < numvalues; vnum++) {
+        ncells = values[vnum * 2];
+        if (ncells != 1 && ncells != 2) {
+            return -1;
+        }
+        value = values[vnum * 2 + 1];
+        hival = cpu_to_be32(value >> 32);
+        if (ncells > 1) {
+            propcells[cellnum++] = hival;
+        } else if (hival != 0) {
+            return -1;
+        }
+        propcells[cellnum++] = cpu_to_be32(value);
+    }
+
+    return qemu_devtree_setprop(fdt, node_path, property, propcells,
+                                cellnum * sizeof(uint32_t));
+}
