9213aa5391f7c8d3766420d96888f1353af4c890
Markus Armbruster <armbru@redhat.com>
2013-07-27 17:41:57 +0200

    qapi.py: Reject invalid characters in schema file
    Signed-off-by: Markus Armbruster <armbru@redhat.com>
    Reviewed-by: Eric Blake <eblake@redhat.com>
    Message-id: 1374939721-7876-6-git-send-email-armbru@redhat.com
    Signed-off-by: Anthony Liguori <aliguori@us.ibm.com>

--- scripts/qapi.py
+++ scripts/qapi.py
@@ -101,6 +101,8 @@ class QAPISchema:
                 if self.cursor == len(self.src):
                     self.tok = None
                     return
+            elif not self.tok.isspace():
+                raise QAPISchemaError(self, 'Stray "%s"' % self.tok)
 
     def get_members(self):
         expr = OrderedDict()
