e0d479445876676f3f92e1bc6d5f92d4bb2500f8
Andreas Färber <afaerber@suse.de>
2013-07-29 04:07:50 +0200

    cpu: Fix VMSTATE_CPU() semantics
    Commit 1a1562f5ea3da17d45d3829e35b5f49da9ec2db5 prepared a VMSTATE_CPU()
    macro for device-style VMStateDescription registration, but missed to
    adapt cpu_exec_init(), so that the "cpu_common" VMStateDescription was
    still registered for AlphaCPU (fe31e7374299c0c6172ce618b29bf2fecbd881c7)
    and OpenRISCCPU (da69721460e652072b6a3dd52b7693da21ffe237). Fix this.
    
    Cc: Richard Henderson <rth@twiddle.net>
    Tested-by: Jia Liu <proljc@gmail.com>
    Signed-off-by: Andreas Färber <afaerber@suse.de>

--- exec.c
+++ exec.c
@@ -402,11 +402,14 @@ void cpu_exec_init(CPUArchState *env)
 #if defined(CONFIG_USER_ONLY)
     cpu_list_unlock();
 #endif
-    vmstate_register(NULL, cpu_index, &vmstate_cpu_common, cpu);
+    if (qdev_get_vmsd(DEVICE(cpu)) == NULL) {
+        vmstate_register(NULL, cpu_index, &vmstate_cpu_common, cpu);
+    }
 #if defined(CPU_SAVE_VERSION) && !defined(CONFIG_USER_ONLY)
     register_savevm(NULL, "cpu", cpu_index, CPU_SAVE_VERSION,
                     cpu_save, cpu_load, env);
     assert(cc->vmsd == NULL);
+    assert(qdev_get_vmsd(DEVICE(cpu)) == NULL);
 #endif
     if (cc->vmsd != NULL) {
         vmstate_register(NULL, cpu_index, cc->vmsd, cpu);
