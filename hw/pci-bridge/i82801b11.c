ccf279824cf78faa3e33450f715f1d7792b46bd0
Gerd Hoffmann <kraxel@redhat.com>
2013-08-05 16:36:40 +0200

    i82801b11: Fix i82801b11 PCI host bridge config space
    pci_bridge_write_config() was not being used.
    
    Cc: qemu-stable@nongnu.org
    Signed-off-by: Gerd Hoffmann <kraxel@redhat.com>
    Signed-off-by: Andreas Färber <afaerber@suse.de>
    Signed-off-by: Michael S. Tsirkin <mst@redhat.com>
    (cherry picked from commit 4965b7f056177ddfb816319e9ff5e766898d0bc5)
    
    Conflicts:
    
    	hw/pci-bridge/i82801b11.c
    
    * modified to avoid dependency on 125ee0ed
    
    Signed-off-by: Michael Roth <mdroth@linux.vnet.ibm.com>

--- hw/pci-bridge/i82801b11.c
+++ hw/pci-bridge/i82801b11.c
@@ -87,6 +87,7 @@ static void i82801b11_bridge_class_init(ObjectClass *klass, void *data)
     k->device_id = PCI_DEVICE_ID_INTEL_82801BA_11;
     k->revision = ICH9_D2P_A2_REVISION;
     k->init = i82801b11_bridge_initfn;
+    k->config_write = pci_bridge_write_config;
 }
 
 static const TypeInfo i82801b11_bridge_info = {
