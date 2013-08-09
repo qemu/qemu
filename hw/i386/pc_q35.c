56383703c060777fd01aaf8d63d5f46d660e9fb9
Eduardo Habkost <ehabkost@redhat.com>
2013-08-09 11:11:36 -0300

    pc: Remove PCLMULQDQ from Westmere on pc-*-1.4 and older
    Commit 41cb383f42d0cb51d8e3e25e3ecebc954dd4196f made a guest-visible
    change by adding the PCLMULQDQ bit to Westmere without adding
    compatibility code to keep the ABI for older machine-types.
    Fix it by adding the missing compat code.
    
    Signed-off-by: Eduardo Habkost <ehabkost@redhat.com>
    Signed-off-by: Andreas Färber <afaerber@suse.de>

--- hw/i386/pc_q35.c
+++ hw/i386/pc_q35.c
@@ -233,6 +233,7 @@ static void pc_q35_init_1_4(QEMUMachineInitArgs *args)
 {
     has_pvpanic = false;
     x86_cpu_compat_set_features("n270", FEAT_1_ECX, 0, CPUID_EXT_MOVBE);
+    x86_cpu_compat_set_features("Westmere", FEAT_1_ECX, 0, CPUID_EXT_PCLMULQDQ);
     pc_q35_init_1_5(args);
 }
 
