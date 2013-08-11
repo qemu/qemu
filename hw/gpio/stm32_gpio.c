0f5572b8da6abfe04c8cdf48cb09c8f77bc923fd
Andre Beckus <mikemail98-qemu@yahoo.com>
2013-08-10 23:20:31 -0400

    Clean old comment

--- hw/gpio/stm32_gpio.c
+++ hw/gpio/stm32_gpio.c
@@ -19,7 +19,6 @@
  * You should have received a copy of the GNU General Public License along
  * with this program; if not, see <http://www.gnu.org/licenses/>.
  */
-/* use PL061 for inspiration. */
 
 #include "hw/sysbus.h"
 #include "hw/arm/stm32.h"
