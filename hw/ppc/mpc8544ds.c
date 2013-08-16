ee87e32f830b4e22c02038de963bb9f72b55896f
Markus Armbruster <armbru@redhat.com>
2013-08-16 13:13:48 +0200

    ppc: Don't explode QEMUMachineInitArgs into local variables needlessly
    Don't explode when the variable is used just once, and never changed.
    
    Signed-off-by: Markus Armbruster <armbru@redhat.com>
    Acked-by: Alexander Graf <agraf@suse.de>
    Reviewed-by: Laszlo Ersek <lersek@redhat.com>
    Signed-off-by: Michael S. Tsirkin <mst@redhat.com>

--- hw/ppc/mpc8544ds.c
+++ hw/ppc/mpc8544ds.c
@@ -28,19 +28,13 @@ static void mpc8544ds_fixup_devtree(PPCE500Params *params, void *fdt)
 
 static void mpc8544ds_init(QEMUMachineInitArgs *args)
 {
-    ram_addr_t ram_size = args->ram_size;
-    const char *boot_device = args->boot_device;
-    const char *cpu_model = args->cpu_model;
-    const char *kernel_filename = args->kernel_filename;
-    const char *kernel_cmdline = args->kernel_cmdline;
-    const char *initrd_filename = args->initrd_filename;
     PPCE500Params params = {
-        .ram_size = ram_size,
-        .boot_device = boot_device,
-        .kernel_filename = kernel_filename,
-        .kernel_cmdline = kernel_cmdline,
-        .initrd_filename = initrd_filename,
-        .cpu_model = cpu_model,
+        .ram_size = args->ram_size,
+        .boot_device = args->boot_device,
+        .kernel_filename = args->kernel_filename,
+        .kernel_cmdline = args->kernel_cmdline,
+        .initrd_filename = args->initrd_filename,
+        .cpu_model = args->cpu_model,
         .pci_first_slot = 0x11,
         .pci_nr_slots = 2,
         .fixup_devtree = mpc8544ds_fixup_devtree,
