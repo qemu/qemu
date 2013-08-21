396f79f45ea75bd1c421522f29b4f91d490df7cc
Eduardo Habkost <ehabkost@redhat.com>
2013-08-21 15:14:44 -0300

    pc: pc_compat_1_4() now can call pc_compat_1_5()
    It just needs to set has_pvpanic=false after calling it. This way, it
    won't be a special case anymore.
    
    Signed-off-by: Eduardo Habkost <ehabkost@redhat.com>
    Signed-off-by: Michael S. Tsirkin <mst@redhat.com>

--- hw/i386/pc_q35.c
+++ hw/i386/pc_q35.c
@@ -229,8 +229,8 @@ static void pc_compat_1_5(QEMUMachineInitArgs *args)
 
 static void pc_compat_1_4(QEMUMachineInitArgs *args)
 {
-    /* 1.5 was special - it enabled pvpanic in builtin machine */
-    pc_compat_1_6(args);
+    pc_compat_1_5(args);
+    has_pvpanic = false;
     x86_cpu_compat_set_features("n270", FEAT_1_ECX, 0, CPUID_EXT_MOVBE);
     x86_cpu_compat_set_features("Westmere", FEAT_1_ECX, 0, CPUID_EXT_PCLMULQDQ);
 }
