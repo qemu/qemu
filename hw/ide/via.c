c6baf942e084e0bc40ee37c8d8672ac9c5ea270b
Andreas Färber <afaerber@suse.de>
2013-08-23 20:18:50 +0200

    ide: Pass size to ide_bus_new()
    To be passed to qbus_create_inplace().
    
    Reviewed-by: Wenchao Xia <xiawenc@linux.vnet.ibm.com>
    Signed-off-by: Andreas Färber <afaerber@suse.de>

--- hw/ide/via.c
+++ hw/ide/via.c
@@ -159,7 +159,7 @@ static void vt82c686b_init_ports(PCIIDEState *d) {
     int i;
 
     for (i = 0; i < 2; i++) {
-        ide_bus_new(&d->bus[i], DEVICE(d), i, 2);
+        ide_bus_new(&d->bus[i], sizeof(d->bus[i]), DEVICE(d), i, 2);
         ide_init_ioport(&d->bus[i], NULL, port_info[i].iobase,
                         port_info[i].iobase2);
         ide_init2(&d->bus[i], isa_get_irq(NULL, port_info[i].isairq));
