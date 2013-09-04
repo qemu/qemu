da4e203efa76f2d2ee0a17670c241881963d033d
Paolo Bonzini <pbonzini@redhat.com>
2013-09-04 19:00:25 +0200

    block: expect errors from bdrv_co_is_allocated
    Some bdrv_is_allocated callers do not expect errors, but the fallback
    in qcow2.c might make other callers trip on assertion failures or
    infinite loops.
    
    Fix the callers to always look for errors.
    
    Cc: qemu-stable@nongnu.org
    Reviewed-by: Eric Blake <eblake@redhat.com>
    Signed-off-by: Paolo Bonzini <pbonzini@redhat.com>
    Signed-off-by: Stefan Hajnoczi <stefanha@redhat.com>
    (cherry picked from commit d663640c04f2aab810915c556390211d75457704)
    
    Conflicts:
    
    	block/cow.c
    
    *modified to avoid dependency on upstream's e641c1e8
    
    Signed-off-by: Michael Roth <mdroth@linux.vnet.ibm.com>

--- block/stream.c
+++ block/stream.c
@@ -120,7 +120,7 @@ wait:
         if (ret == 1) {
             /* Allocated in the top, no need to copy.  */
             copy = false;
-        } else {
+        } else if (ret >= 0) {
             /* Copy if allocated in the intermediate images.  Limit to the
              * known-unallocated area [sector_num, sector_num+n).  */
             ret = bdrv_co_is_allocated_above(bs->backing_hd, base,
