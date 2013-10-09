b4350deed67b95651896ddb60cf9f765093a4848
Hans de Goede <hdegoede@redhat.com>
2013-10-09 21:33:44 +0200

    audio: honor QEMU_AUDIO_TIMER_PERIOD instead of waking up every *nano* second
    Now that we no longer have MIN_REARM_TIMER_NS a bug in the audio subsys has
    clearly shown it self by trying to make a timer fire every nano second.
    
    Note we have a similar problem in 1.6, 1.5 and older but there
    MIN_REARM_TIMER_NS limits the wakeups caused by audio being active to
    4000 times / second. This still causes a host cpu load of 50 % for simply
    playing audio, where as with this patch git master is at 13%, so we should
    backport this to 1.5 and 1.6 too.
    
    Note this will not apply to 1.5 and 1.6 as is.
    
    Cc: qemu-stable@nongnu.org
    Signed-off-by: Hans de Goede <hdegoede@redhat.com>
    Signed-off-by: Gerd Hoffmann <kraxel@redhat.com>

--- audio/audio.c
+++ audio/audio.c
@@ -1124,7 +1124,8 @@ static int audio_is_timer_needed (void)
 static void audio_reset_timer (AudioState *s)
 {
     if (audio_is_timer_needed ()) {
-        timer_mod (s->ts, qemu_clock_get_ns(QEMU_CLOCK_VIRTUAL) + 1);
+        timer_mod (s->ts,
+            qemu_clock_get_ns(QEMU_CLOCK_VIRTUAL) + conf.period.ticks);
     }
     else {
         timer_del (s->ts);
