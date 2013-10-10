33876dfad64bc481f59c5e9ccf60db78624c4b93
Roger Pau Monné <roger.pau@citrix.com>
2013-10-10 14:25:52 +0000

    qemu/xen: make use of xenstore relative paths
    Qemu has several hardcoded xenstore paths that are only valid on Dom0.
    Attempts to launch a Qemu instance (to act as a userspace backend for
    PV disks) will fail because Qemu is not able to access those paths
    when running on a domain different than Dom0.
    
    Instead make the xenstore paths relative to the domain where Qemu is
    actually running.
    
    Signed-off-by: Roger Pau Monné <roger.pau@citrix.com>
    Signed-off-by: Stefano Stabellini <stefano.stabellini@eu.citrix.com>
    Reviewed-by: Anthony PERARD <anthony.perard@citrix.com>
    Cc: xen-devel@lists.xenproject.org
    Cc: Anthony PERARD <anthony.perard@citrix.com>

--- xen-all.c
+++ xen-all.c
@@ -949,7 +949,7 @@ static void xenstore_record_dm_state(struct xs_handle *xs, const char *state)
         exit(1);
     }
 
-    snprintf(path, sizeof (path), "/local/domain/0/device-model/%u/state", xen_domid);
+    snprintf(path, sizeof (path), "device-model/%u/state", xen_domid);
     if (!xs_write(xs, XBT_NULL, path, state, strlen(state))) {
         fprintf(stderr, "error recording dm state\n");
         exit(1);
