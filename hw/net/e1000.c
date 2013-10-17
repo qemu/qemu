7c36507c2b8776266f50c5e2739bd18279953b93
Amos Kong <akong@redhat.com>
2013-10-17 15:02:49 +0800

    net/e1000: update network information when macaddr is changed in guest
    If we change macaddr in guest by 'ifconfig eth0 hw ether 12:12:12:34:35:36',
    the mac register of e1000 is already updated, but we don't update
    network information in qemu. Therefor, the information in monitor
    is wrong.
    
    This patch updates nic info when the second part of macaddr is written.
    
    Signed-off-by: Amos Kong <akong@redhat.com>
    Signed-off-by: Stefan Hajnoczi <stefanha@redhat.com>

--- hw/net/e1000.c
+++ hw/net/e1000.c
@@ -1106,7 +1106,15 @@ mac_read_clr8(E1000State *s, int index)
 static void
 mac_writereg(E1000State *s, int index, uint32_t val)
 {
+    uint32_t macaddr[2];
+
     s->mac_reg[index] = val;
+
+    if (index == RA + 1) {
+        macaddr[0] = cpu_to_le32(s->mac_reg[RA]);
+        macaddr[1] = cpu_to_le32(s->mac_reg[RA + 1]);
+        qemu_format_nic_info_str(qemu_get_queue(s->nic), (uint8_t *)macaddr);
+    }
 }
 
 static void
