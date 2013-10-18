c777d5d62a729fd8b19847aaa0aad3d7a1f73f47
Stefan Weil <sw@weilnetz.de>
2013-10-18 22:12:22 +0200

    w32: Add assertion
    This assertion also works around a crash (compiler error?).
    
    Signed-off-by: Stefan Weil <sw@weilnetz.de>

--- coroutine-win32.c
+++ coroutine-win32.c
@@ -42,6 +42,7 @@ CoroutineAction qemu_coroutine_switch(Coroutine *from_, Coroutine *to_,
     CoroutineWin32 *from = DO_UPCAST(CoroutineWin32, base, from_);
     CoroutineWin32 *to = DO_UPCAST(CoroutineWin32, base, to_);
 
+    g_assert(current == from_);
     current = to_;
 
     to->action = action;
