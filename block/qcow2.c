6e13610aa454beba52944e8df6d93158d68ab911
Max Reitz <mreitz@redhat.com>
2013-10-20 21:52:35 +0200

    qcow2: Unset zero_beyond_eof in save_vmstate
    Saving the VM state is done using bdrv_pwrite. This function may perform
    a read-modify-write, which in this case results in data being read from
    beyond the end of the virtual disk. Since we are actually trying to
    access an area which is not a part of the virtual disk, zero_beyond_eof
    has to be set to false before performing the partial write, otherwise
    the VM state may become corrupted.
    
    Signed-off-by: Max Reitz <mreitz@redhat.com>
    Reviewed-by: Eric Blake <eblake@redhat.com>
    Signed-off-by: Kevin Wolf <kwolf@redhat.com>

--- block/qcow2.c
+++ block/qcow2.c
@@ -1941,12 +1941,15 @@ static int qcow2_save_vmstate(BlockDriverState *bs, QEMUIOVector *qiov,
     BDRVQcowState *s = bs->opaque;
     int64_t total_sectors = bs->total_sectors;
     int growable = bs->growable;
+    bool zero_beyond_eof = bs->zero_beyond_eof;
     int ret;
 
     BLKDBG_EVENT(bs->file, BLKDBG_VMSTATE_SAVE);
     bs->growable = 1;
+    bs->zero_beyond_eof = false;
     ret = bdrv_pwritev(bs, qcow2_vm_state_offset(s) + pos, qiov);
     bs->growable = growable;
+    bs->zero_beyond_eof = zero_beyond_eof;
 
     /* bdrv_co_do_writev will have increased the total_sectors value to include
      * the VM state - the VM state is however not an actual part of the block
