3ab7bd1917d210eac5d2a80593899ec70c4e998c
MORITA Kazutaka <morita.kazutaka@lab.ntt.co.jp>
2013-10-24 16:01:14 +0900

    coroutine: add co_aio_sleep_ns() to allow sleep in block drivers
    This helper function behaves similarly to co_sleep_ns(), but the
    sleeping coroutine will be resumed when using qemu_aio_wait().
    
    Signed-off-by: MORITA Kazutaka <morita.kazutaka@lab.ntt.co.jp>
    Tested-by: Liu Yuan <namei.unix@gmail.com>
    Reviewed-by: Liu Yuan <namei.unix@gmail.com>
    Signed-off-by: Kevin Wolf <kwolf@redhat.com>

--- qemu-coroutine-sleep.c
+++ qemu-coroutine-sleep.c
@@ -13,6 +13,7 @@
 
 #include "block/coroutine.h"
 #include "qemu/timer.h"
+#include "block/aio.h"
 
 typedef struct CoSleepCB {
     QEMUTimer *ts;
@@ -32,6 +33,19 @@ void coroutine_fn co_sleep_ns(QEMUClockType type, int64_t ns)
         .co = qemu_coroutine_self(),
     };
     sleep_cb.ts = timer_new(type, SCALE_NS, co_sleep_cb, &sleep_cb);
+    timer_mod(sleep_cb.ts, qemu_clock_get_ns(type) + ns);
+    qemu_coroutine_yield();
+    timer_del(sleep_cb.ts);
+    timer_free(sleep_cb.ts);
+}
+
+void coroutine_fn co_aio_sleep_ns(AioContext *ctx, QEMUClockType type,
+                                  int64_t ns)
+{
+    CoSleepCB sleep_cb = {
+        .co = qemu_coroutine_self(),
+    };
+    sleep_cb.ts = aio_timer_new(ctx, type, SCALE_NS, co_sleep_cb, &sleep_cb);
     timer_mod(sleep_cb.ts, qemu_clock_get_ns(type) + ns);
     qemu_coroutine_yield();
     timer_del(sleep_cb.ts);
