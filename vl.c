8111d0287821a9b2a2faefbf0be1f345dbe93222
Antony Pavlov <antonynpavlov@gmail.com>
2013-10-29 08:36:31 +0400

    vl: fix build when configured with no graphic support
    The following error occurs when building with no graphic output support:
    
      vl.c: In function ‘main’:
      vl.c:2829:19: error: variable ‘ds’ set but not used [-Werror=unused-but-set-variable]
           DisplayState *ds;
                         ^
      cc1: all warnings being treated as errors
    
    To reproduce this issue, just run:
    
      $ ./configure \
          --disable-curses \
          --disable-sdl \
          --disable-cocoa \
          --disable-gtk \
          --disable-vnc \
          --enable-werror
      $ make vl.o
    
    Signed-off-by: Antony Pavlov <antonynpavlov@gmail.com>
    Reviewed-by: Stefan Weil <sw@weilnetz.de>
    Signed-off-by: Michael Tokarev <mjt@tls.msk.ru>

--- vl.c
+++ vl.c
@@ -4267,6 +4267,7 @@ int main(int argc, char **argv, char **envp)
     /* init local displays */
     switch (display_type) {
     case DT_NOGRAPHIC:
+        (void)ds;	/* avoid warning if no display is configured */
         break;
 #if defined(CONFIG_CURSES)
     case DT_CURSES:
