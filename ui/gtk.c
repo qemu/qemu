c1678ec6de00534b37b185e13f3d3590b6276133
Stefan Weil <sw@weilnetz.de>
2013-11-14 22:07:44 +0100

    gtk: Fix key code for AltGr (MinGW)
    This allows using special characters like @ or € with German keyboards.
    
    Signed-off-by: Stefan Weil <sw@weilnetz.de>

--- ui/gtk.c
+++ ui/gtk.c
@@ -722,6 +722,11 @@ static gboolean gd_key_event(GtkWidget *widget, GdkEventKey *key, void *opaque)
 #ifdef _WIN32
     /* MapVirtualKey doesn't return scancode with needed higher byte */
     UINT qemu_keycode = MapVirtualKey(gdk_keycode, MAPVK_VK_TO_VSC);
+    switch (qemu_keycode) {
+    case 103:   /* alt gr */
+        qemu_keycode = 56 | SCANCODE_GREY;
+        break;
+    }
 #else
     int qemu_keycode;
 
