0b06ef3bdd17742ae50c0662d3fe8ed944648890
Stefan Hajnoczi <stefanha@redhat.com>
2013-11-26 16:18:00 +0100

    block: clean up bdrv_drain_all() throttling comments
    Since cc0681c45430a1f1a4c2d06e9499b7775afc9a18 ("block: Enable the new
    throttling code in the block layer.") bdrv_drain_all() no longer spins.
    The code used to look as follows:
    
      do {
          busy = qemu_aio_wait();
    
          /* FIXME: We do not have timer support here, so this is effectively
           * a busy wait.
           */
          QTAILQ_FOREACH(bs, &bdrv_states, list) {
              while (qemu_co_enter_next(&bs->throttled_reqs)) {
                  busy = true;
              }
          }
      } while (busy);
    
    Note that throttle requests are kicked but I/O throttling limits are
    still in effect.  The loop spins until the vm_clock time allows the
    request to make progress and complete.
    
    The new throttling code introduced bdrv_start_throttled_reqs().  This
    function not only kicks throttled requests but also temporarily disables
    throttling so requests can run.
    
    The outdated FIXME comment can be removed.  Also drop the busy = true
    assignment since we overwrite it immediately afterwards.
    
    Reviewed-by: Alex Bligh <alex@alex.org.uk>
    Signed-off-by: Stefan Hajnoczi <stefanha@redhat.com>

--- block.c
+++ block.c
@@ -1557,13 +1557,8 @@ void bdrv_drain_all(void)
     BlockDriverState *bs;
 
     while (busy) {
-        /* FIXME: We do not have timer support here, so this is effectively
-         * a busy wait.
-         */
         QTAILQ_FOREACH(bs, &bdrv_states, list) {
-            if (bdrv_start_throttled_reqs(bs)) {
-                busy = true;
-            }
+            bdrv_start_throttled_reqs(bs);
         }
 
         busy = bdrv_requests_pending_all();
