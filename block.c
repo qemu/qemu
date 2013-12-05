1ff735bdc417945bc6df1857861b127644b3f461
Kevin Wolf <kwolf@redhat.com>
2013-12-05 13:01:46 +0100

    block: Detect unaligned length in bdrv_qiov_is_aligned()
    For an O_DIRECT request to succeed, it's not only necessary that all
    base addresses in the qiov are aligned, but also that each length in it
    is aligned.
    
    Signed-off-by: Kevin Wolf <kwolf@redhat.com>
    Reviewed-by: Wenchao Xia <xiawenc@linux.vnet.ibm.com>
    Reviewed-by: Max Reitz <mreitz@redhat.com>

--- block.c
+++ block.c
@@ -4811,6 +4811,9 @@ bool bdrv_qiov_is_aligned(BlockDriverState *bs, QEMUIOVector *qiov)
         if ((uintptr_t) qiov->iov[i].iov_base % bs->buffer_alignment) {
             return false;
         }
+        if (qiov->iov[i].iov_len % bs->buffer_alignment) {
+            return false;
+        }
     }
 
     return true;
