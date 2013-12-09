e5187b561ff7b241d609a2848131879072169565
Amos Kong <akong@redhat.com>
2013-12-09 19:53:15 +0800

    fix -boot strict regressed in commit 6ef4716
    Commit 6ef4716 cleaned up parsing of -boot option argument, but
    accidentally dropped parameter strict.  It should have been updated
    exactly like parameter menu. Do that.
    
    Signed-off-by: Amos Kong <akong@redhat.com>
    Reviewed-by: Markus Armbruster <armbru@redhat.com>
    Signed-off-by: Michael Tokarev <mjt@tls.msk.ru>

--- vl.c
+++ vl.c
@@ -461,7 +461,7 @@ static QemuOptsList qemu_boot_opts = {
             .type = QEMU_OPT_STRING,
         }, {
             .name = "strict",
-            .type = QEMU_OPT_STRING,
+            .type = QEMU_OPT_BOOL,
         },
         { /*End of list */ }
     },
@@ -4081,6 +4081,7 @@ int main(int argc, char **argv, char **envp)
         }
 
         boot_menu = qemu_opt_get_bool(opts, "menu", boot_menu);
+        boot_strict = qemu_opt_get_bool(opts, "strict", false);
     }
 
     if (!kernel_cmdline) {
