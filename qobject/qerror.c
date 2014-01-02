594278718323ca7bffaab0fb7fc6c82fa2c1cd5f
Peter Crosthwaite <peter.crosthwaite@xilinx.com>
2014-01-01 18:49:52 -0800

    qerror: Remove assert_no_error()
    This is no longer needed, and is obsoleted by error_abort. Remove.
    
    Signed-off-by: Peter Crosthwaite <peter.crosthwaite@xilinx.com>
    Reviewed-by: Markus Armbruster <armbru@redhat.com>
    Signed-off-by: Luiz Capitulino <lcapitulino@redhat.com>

--- qobject/qerror.c
+++ qobject/qerror.c
@@ -121,14 +121,6 @@ void qerror_report_err(Error *err)
     }
 }
 
-void assert_no_error(Error *err)
-{
-    if (err) {
-        qerror_report_err(err);
-        abort();
-    }
-}
-
 /**
  * qobject_to_qerror(): Convert a QObject into a QError
  */
