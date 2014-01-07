a824bc191a90d86980a9ed090cb1d1219faaf8e7
Tom Musta <tommusta@gmail.com>
2014-01-07 10:05:50 -0600

    target-ppc: Add Flag for ISA2.06 Divide Extended Instructions
    This patch adds a flag for the Divide Extended instructions that
    were introduced in Power ISA V2.06B.  The flag is added to the
    Power7 and Power8 models.
    
    Signed-off-by: Tom Musta <tommusta@gmail.com>
    Reviewed-by: Richard Henderson <rth@twiddle.net>
    Signed-off-by: Alexander Graf <agraf@suse.de>

--- target-ppc/cpu.h
+++ target-ppc/cpu.h
@@ -1879,9 +1879,12 @@ enum {
     PPC2_VSX207        = 0x0000000000000040ULL,
     /* ISA 2.06B bpermd                                                      */
     PPC2_PERM_ISA206   = 0x0000000000000080ULL,
+    /* ISA 2.06B divide extended variants                                    */
+    PPC2_DIVE_ISA206   = 0x0000000000000100ULL,
 
 #define PPC_TCG_INSNS2 (PPC2_BOOKE206 | PPC2_VSX | PPC2_PRCNTL | PPC2_DBRX | \
-                        PPC2_ISA205 | PPC2_VSX207 | PPC2_PERM_ISA206)
+                        PPC2_ISA205 | PPC2_VSX207 | PPC2_PERM_ISA206 | \
+                        PPC2_DIVE_ISA206)
 };
 
 /*****************************************************************************/
