863d7c91050551def59116f4f3b39fab7f1568f7
Peter Lieven <pl@kamp.de>
2014-01-08 10:08:36 +0100

    ui/vnc: optimize clearing in find_and_clear_dirty_height()
    The following artifical test (just the bitmap operation part) running
    vnc_update_client 65536 times on a 2560x2048 surface illustrates the
    performance difference:
    
    All bits clean - vnc_update_client_new: 0.07 secs
                     vnc_update_client_new2: 0.07 secs
                     vnc_update_client_old: 10.98 secs
    
    All bits dirty - vnc_update_client_new: 11.26 secs
                   - vnc_update_client_new2: 0.29 secs
                     vnc_update_client_old: 20.19 secs
    
    Few bits dirty - vnc_update_client_new: 0.07 secs
                   - vnc_update_client_new2: 0.07 secs
                     vnc_update_client_old: 10.98 secs
    
    vnc_update_client_new2 shows the performance of vnc_update_client
    with this patch added.
    
    Comparing with the test run of the last patch the performance
    is at least unchanged while it is significantly improved
    for the all bits dirty case.
    
    Signed-off-by: Peter Lieven <pl@kamp.de>
    Reviewed-by: Wenchao Xia <xiawenc@linux.vnet.ibm.com>
    Signed-off-by: Gerd Hoffmann <kraxel@redhat.com>

--- ui/vnc.c
+++ ui/vnc.c
@@ -879,13 +879,10 @@ static int find_and_clear_dirty_height(struct VncState *vs,
     int h;
 
     for (h = 1; h < (height - y); h++) {
-        int tmp_x;
         if (!test_bit(last_x, vs->dirty[y + h])) {
             break;
         }
-        for (tmp_x = last_x; tmp_x < x; tmp_x++) {
-            clear_bit(tmp_x, vs->dirty[y + h]);
-        }
+        bitmap_clear(vs->dirty[y + h], last_x, x - last_x);
     }
 
     return h;
