5c1904f103cbc5a7353fba8c8ca3cb16a0281392
Markus Armbruster <armbru@redhat.com>
2014-01-10 14:31:38 +0100

    qom-test: Run for all available machines
    Get available machines via QMP instead of hardcoding a list that's
    perpetually out of date.
    
    Xen machines can work only when running under the Xen hypervisor.
    Blacklist them.
    
    Signed-off-by: Markus Armbruster <armbru@redhat.com>
    Signed-off-by: Andreas Färber <afaerber@suse.de>

--- tests/qom-test.c
+++ tests/qom-test.c
@@ -11,6 +11,36 @@
 #include <glib.h>
 #include <string.h>
 #include "qemu/osdep.h"
+#include "qapi/qmp/types.h"
+
+static const char *blacklist_x86[] = {
+    "xenfv", "xenpv", NULL
+};
+
+static const struct {
+    const char *arch;
+    const char **machine;
+} blacklists[] = {
+    { "i386", blacklist_x86 },
+    { "x86_64", blacklist_x86 },
+};
+
+static bool is_blacklisted(const char *arch, const char *mach)
+{
+    int i;
+    const char **p;
+
+    for (i = 0; i < ARRAY_SIZE(blacklists); i++) {
+        if (!strcmp(blacklists[i].arch, arch)) {
+            for (p = blacklists[i].machine; *p; p++) {
+                if (!strcmp(*p, mach)) {
+                    return true;
+                }
+            }
+        }
+    }
+    return false;
+}
 
 static void test_nop(gconstpointer data)
 {
@@ -26,227 +56,43 @@ static void test_nop(gconstpointer data)
     g_free(args);
 }
 
-static const char *x86_machines[] = {
-    "pc",
-    "isapc",
-    "q35",
-};
-
-static const char *alpha_machines[] = {
-    "clipper",
-};
-
-static const char *arm_machines[] = {
-    "integratorcp",
-    "versatilepb",
-    "versatileab",
-    "lm3s811evb",
-    "lm3s6965evb",
-    "collie",
-    "akita",
-    "spitz",
-    "borzoi",
-    "terrier",
-    "tosa",
-    "cheetah",
-    "sx1-v1",
-    "sx1",
-    "realview-eb",
-    "realview-eb-mpcore",
-    "realview-pb-a8",
-    "realview-pbx-a9",
-    "musicpal",
-    "mainstone",
-    "connex",
-    "verdex",
-    "z2",
-    "n800",
-    "n810",
-    "kzm",
-    "vexpress-a9",
-    "vexpress-a15",
-    "smdkc210",
-    "nuri",
-    "xilinx-zynq-a9",
-    "highbank",
-    "midway",
-    "canon-a1100",
-    "cubieboard",
-};
-
-static const char *cris_machines[] = {
-    "axis-dev88",
-};
-
-static const char *lm32_machines[] = {
-    "lm32-evr",
-    "lm32-uclinux",
-    "milkymist",
-};
-
-static const char *m68k_machines[] = {
-    "mcf5208evb",
-    "an5206",
-    "dummy",
-};
-
-static const char *microblaze_machines[] = {
-    "petalogix-ml605",
-    "petalogix-s3adsp1800",
-};
-
-static const char *mips_machines[] = {
-    "malta",
-    "magnum",
-    "mips",
-    "mipssim",
-    "pica61",
-};
-
-static const char *moxie_machines[] = {
-    "moxiesim",
-};
-
-static const char *openrisc_machines[] = {
-    "or32-sim",
-};
-
-static const char *ppc_machines[] = {
-    "g3beige",
-    "mac99",
-    "prep",
-    "mpc8544ds",
-    "ppce500",
-};
-
-static const char *ppc64_machines[] = {
-    "pseries",
-};
-
-static const char *ppc405_machines[] = {
-    "ref405ep",
-    "taihu",
-};
-
-static const char *ppc440_machines[] = {
-    "bamboo",
-    "virtex-ml507",
-};
-
-static const char *s390_machines[] = {
-    "s390-virtio",
-    "s390-ccw-virtio",
-};
-
-static const char *superh_machines[] = {
-    "r2d",
-    "shix",
-};
-
-static const char *sparc_machines[] = {
-    "SS-4",
-    "SS-5",
-    "SS-10",
-    "SS-20",
-    "SS-600MP",
-    "LX",
-    "SPARCClassic",
-    "SPARCbook",
-    "leon3_generic",
-};
-
-static const char *sparc64_machines[] = {
-    "sun4u",
-    "sun4v",
-    "Niagara",
-};
-
-static const char *unicore32_machines[] = {
-    "puv3",
-};
-
-static const char *xtensa_machines[] = {
-    "sim",
-    "lx60",
-    "lx200",
-};
-
-static void add_test_cases(const char *arch, const char *machine)
+static void add_machine_test_cases(void)
 {
-    char *path;
-    path = g_strdup_printf("/%s/qom/%s", arch, machine);
-    g_test_add_data_func(path, machine, test_nop);
-}
+    const char *arch = qtest_get_arch();
+    QDict *response, *minfo;
+    QList *list;
+    const QListEntry *p;
+    QObject *qobj;
+    QString *qstr;
+    const char *mname, *path;
 
-#define ADD_MACHINE_TESTS(arch, array) do { \
-    int i; \
-    for (i = 0; i < ARRAY_SIZE(array); i++) { \
-        add_test_cases((arch), (array)[i]); \
-    } \
-} while (false)
+    qtest_start("-machine none");
+    response = qmp("{ 'execute': 'query-machines' }");
+    g_assert(response);
+    list = qdict_get_qlist(response, "return");
+    g_assert(list);
+
+    for (p = qlist_first(list); p; p = qlist_next(p)) {
+        minfo = qobject_to_qdict(qlist_entry_obj(p));
+        g_assert(minfo);
+        qobj = qdict_get(minfo, "name");
+        g_assert(qobj);
+        qstr = qobject_to_qstring(qobj);
+        g_assert(qstr);
+        mname = qstring_get_str(qstr);
+        if (!is_blacklisted(arch, mname)) {
+            path = g_strdup_printf("/%s/qom/%s", arch, mname);
+            g_test_add_data_func(path, mname, test_nop);
+        }
+    }
+    qtest_end();
+}
 
 int main(int argc, char **argv)
 {
-    const char *arch = qtest_get_arch();
-
     g_test_init(&argc, &argv, NULL);
 
-    add_test_cases(arch, "none");
-
-    if (strcmp(arch, "i386") == 0 ||
-        strcmp(arch, "x86_64") == 0) {
-        ADD_MACHINE_TESTS(arch, x86_machines);
-    } else if (strcmp(arch, "alpha") == 0) {
-        ADD_MACHINE_TESTS(arch, alpha_machines);
-    } else if (strcmp(arch, "arm") == 0) {
-        ADD_MACHINE_TESTS(arch, arm_machines);
-    } else if (strcmp(arch, "cris") == 0) {
-        ADD_MACHINE_TESTS(arch, cris_machines);
-    } else if (strcmp(arch, "lm32") == 0) {
-        ADD_MACHINE_TESTS(arch, lm32_machines);
-    } else if (strcmp(arch, "m68k") == 0) {
-        ADD_MACHINE_TESTS(arch, m68k_machines);
-    } else if (strcmp(arch, "microblaze") == 0 ||
-               strcmp(arch, "microblazeel") == 0) {
-        ADD_MACHINE_TESTS(arch, microblaze_machines);
-    } else if (strcmp(arch, "mips") == 0 ||
-               strcmp(arch, "mipsel") == 0 ||
-               strcmp(arch, "mips64") == 0) {
-        ADD_MACHINE_TESTS(arch, mips_machines);
-    } else if (strcmp(arch, "mips64el") == 0) {
-        ADD_MACHINE_TESTS(arch, mips_machines);
-        add_test_cases(arch, "fulong2e");
-    } else if (strcmp(arch, "moxie") == 0) {
-        ADD_MACHINE_TESTS(arch, moxie_machines);
-    } else if (strcmp(arch, "or32") == 0) {
-        ADD_MACHINE_TESTS(arch, openrisc_machines);
-    } else if (strcmp(arch, "ppcemb") == 0) {
-        ADD_MACHINE_TESTS(arch, ppc405_machines);
-        ADD_MACHINE_TESTS(arch, ppc440_machines);
-    } else if (strcmp(arch, "ppc") == 0) {
-        ADD_MACHINE_TESTS(arch, ppc405_machines);
-        ADD_MACHINE_TESTS(arch, ppc440_machines);
-        ADD_MACHINE_TESTS(arch, ppc_machines);
-    } else if (strcmp(arch, "ppc64") == 0) {
-        ADD_MACHINE_TESTS(arch, ppc405_machines);
-        ADD_MACHINE_TESTS(arch, ppc440_machines);
-        ADD_MACHINE_TESTS(arch, ppc_machines);
-        ADD_MACHINE_TESTS(arch, ppc64_machines);
-    } else if (strcmp(arch, "s390x") == 0) {
-        ADD_MACHINE_TESTS(arch, s390_machines);
-    } else if (strcmp(arch, "sh4") == 0 ||
-               strcmp(arch, "sh4eb") == 0) {
-        ADD_MACHINE_TESTS(arch, superh_machines);
-    } else if (strcmp(arch, "sparc") == 0) {
-        ADD_MACHINE_TESTS(arch, sparc_machines);
-    } else if (strcmp(arch, "sparc64") == 0) {
-        ADD_MACHINE_TESTS(arch, sparc64_machines);
-    } else if (strcmp(arch, "unicore32") == 0) {
-        ADD_MACHINE_TESTS(arch, unicore32_machines);
-    } else if (strcmp(arch, "xtensa") == 0 ||
-               strcmp(arch, "xtensaeb") == 0) {
-        ADD_MACHINE_TESTS(arch, xtensa_machines);
-    }
+    add_machine_test_cases();
 
     return g_test_run();
 }
