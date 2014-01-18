dc9a353cf7de78b9fae3e46410803d487fd2fc8f
Martin Husemann <martin@duskware.de>
2014-01-18 14:47:23 +0100

    qemu 1.7.0 does not build on NetBSD
     Do not rely on int8_t (and friends) not being preprocessor
     symbols (or symbols expanding to themselves). On NetBSD (for example) the
     glue(u, SDATA_TYPE) results in u__int8_t, which is undefined. There is no way
     to stop cpp expanding inner macros, so just add the few lines explicitly and
     get rid of the magic.
    
    Signed-off-by: Martin Husemann <martin@NetBSD.org>
    Reviewed-by: Peter Maydell <peter.maydell@linaro.org>
    Reviewed-by: Andreas Färber <afaerber@suse.de>
    Signed-off-by: Michael Tokarev <mjt@tls.msk.ru>

--- include/exec/softmmu_template.h
+++ include/exec/softmmu_template.h
@@ -30,23 +30,26 @@
 #define SUFFIX q
 #define LSUFFIX q
 #define SDATA_TYPE  int64_t
+#define DATA_TYPE  uint64_t
 #elif DATA_SIZE == 4
 #define SUFFIX l
 #define LSUFFIX l
 #define SDATA_TYPE  int32_t
+#define DATA_TYPE  uint32_t
 #elif DATA_SIZE == 2
 #define SUFFIX w
 #define LSUFFIX uw
 #define SDATA_TYPE  int16_t
+#define DATA_TYPE  uint16_t
 #elif DATA_SIZE == 1
 #define SUFFIX b
 #define LSUFFIX ub
 #define SDATA_TYPE  int8_t
+#define DATA_TYPE  uint8_t
 #else
 #error unsupported data size
 #endif
 
-#define DATA_TYPE   glue(u, SDATA_TYPE)
 
 /* For the benefit of TCG generated code, we want to avoid the complication
    of ABI-specific return type promotion and always return a value extended
