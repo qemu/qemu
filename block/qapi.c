c8059b97e1f9b4635b836ee98373a0f72f9fc0b4
Fam Zheng <famz@redhat.com>
2014-01-23 10:03:26 +0800

    qapi: Add "backing" to BlockStats
    Currently there is no way to query BlockStats of the backing chain. This
    adds "backing" field into BlockStats to make it possible.
    
    The comment of "parent" is reworded.
    
    Signed-off-by: Fam Zheng <famz@redhat.com>
    Reviewed-by: Benoit Canet <benoit@irqsave.net>
    Reviewed-by: Eric Blake <eblake@redhat.com>
    Signed-off-by: Kevin Wolf <kwolf@redhat.com>

--- block/qapi.c
+++ block/qapi.c
@@ -318,6 +318,11 @@ BlockStats *bdrv_query_stats(const BlockDriverState *bs)
         s->parent = bdrv_query_stats(bs->file);
     }
 
+    if (bs->backing_hd) {
+        s->has_backing = true;
+        s->backing = bdrv_query_stats(bs->backing_hd);
+    }
+
     return s;
 }
 
