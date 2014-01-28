5736245c80bb277f3de7f37d9482c906743612ad
Aneesh Kumar K.V <aneesh.kumar@linux.vnet.ibm.com>
2014-01-28 13:29:59 +0530

    target-ppc: Update external_htab even when HTAB is managed by kernel
    We will use this in later patches to make sure we use the right load
    functions when copying hpte entries.
    
    Signed-off-by: Aneesh Kumar K.V <aneesh.kumar@linux.vnet.ibm.com>
    Signed-off-by: Alexander Graf <agraf@suse.de>

--- hw/ppc/spapr.c
+++ hw/ppc/spapr.c
@@ -741,6 +741,13 @@ static void spapr_cpu_reset(void *opaque)
     env->spr[SPR_HIOR] = 0;
 
     env->external_htab = (uint8_t *)spapr->htab;
+    if (kvm_enabled() && !env->external_htab) {
+        /*
+         * HV KVM, set external_htab to 1 so our ppc_hash64_load_hpte*
+         * functions do the right thing.
+         */
+        env->external_htab = (void *)1;
+    }
     env->htab_base = -1;
     env->htab_mask = HTAB_SIZE(spapr) - 1;
     env->spr[SPR_SDR1] = (target_ulong)(uintptr_t)spapr->htab |
