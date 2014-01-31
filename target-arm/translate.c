664c6733d72c589cd9f6ccee305e7b7ce36ea06d
Will Newton <will.newton@linaro.org>
2014-01-31 14:47:34 +0000

    target-arm: Add support for AArch32 FP VRINTR
    Add support for the AArch32 floating-point VRINTR instruction.
    
    Signed-off-by: Will Newton <will.newton@linaro.org>
    Signed-off-by: Peter Maydell <peter.maydell@linaro.org>

--- target-arm/translate.c
+++ target-arm/translate.c
@@ -3379,6 +3379,17 @@ static int disas_vfp_insn(CPUARMState * env, DisasContext *s, uint32_t insn)
                         gen_vfp_F1_ld0(dp);
                         gen_vfp_cmpe(dp);
                         break;
+                    case 12: /* vrintr */
+                    {
+                        TCGv_ptr fpst = get_fpstatus_ptr(0);
+                        if (dp) {
+                            gen_helper_rintd(cpu_F0d, cpu_F0d, fpst);
+                        } else {
+                            gen_helper_rints(cpu_F0s, cpu_F0s, fpst);
+                        }
+                        tcg_temp_free_ptr(fpst);
+                        break;
+                    }
                     case 15: /* single<->double conversion */
                         if (dp)
                             gen_helper_vfp_fcvtsd(cpu_F0s, cpu_F0d, cpu_env);
