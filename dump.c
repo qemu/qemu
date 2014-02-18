b5ba1cc6260917926781fb79fbb05d53bf586d53
qiaonuohan <qiaonuohan@cn.fujitsu.com>
2014-02-18 14:11:25 +0800

    dump: const-qualify the buf of WriteCoreDumpFunction
    WriteCoreDumpFunction is a function pointer that points to the function used to
    write content in "buf" into core file, so "buf" should be const-qualify.
    
    Signed-off-by: Qiao Nuohan <qiaonuohan@cn.fujitsu.com>
    Reviewed-by: Laszlo Ersek <lersek@redhat.com>
    Signed-off-by: Luiz Capitulino <lcapitulino@redhat.com>

--- dump.c
+++ dump.c
@@ -99,7 +99,7 @@ static void dump_error(DumpState *s, const char *reason)
     dump_cleanup(s);
 }
 
-static int fd_write_vmcore(void *buf, size_t size, void *opaque)
+static int fd_write_vmcore(const void *buf, size_t size, void *opaque)
 {
     DumpState *s = opaque;
     size_t written_size;
