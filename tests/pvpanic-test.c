627b1a17ced89e13df34a85feef54a50e9b94191
Andreas Färber <afaerber@suse.de>
2014-02-21 21:17:17 +0100

    pvpanic-test: Assert pause event
    Signed-off-by: Andreas Färber <afaerber@suse.de>

--- tests/pvpanic-test.c
+++ tests/pvpanic-test.c
@@ -15,11 +15,20 @@
 static void test_panic(void)
 {
     uint8_t val;
+    QDict *response, *data;
 
     val = inb(0x505);
     g_assert_cmpuint(val, ==, 1);
 
     outb(0x505, 0x1);
+
+    response = qmp_receive();
+    g_assert(qdict_haskey(response, "event"));
+    g_assert_cmpstr(qdict_get_str(response, "event"), ==, "GUEST_PANICKED");
+    g_assert(qdict_haskey(response, "data"));
+    data = qdict_get_qdict(response, "data");
+    g_assert(qdict_haskey(data, "action"));
+    g_assert_cmpstr(qdict_get_str(data, "action"), ==, "pause");
 }
 
 int main(int argc, char **argv)
