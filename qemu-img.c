626f84f39d4ae365a44dbbc0d0dd3c7739c3971a
Kevin Wolf <kwolf@redhat.com>
2014-02-21 16:24:06 +0100

    qemu-img amend: Support multiple -o options
    Instead of ignoring all option values but the last one, multiple -o
    options now have the same meaning as having a single option with all
    settings in the order of their respective -o options.
    
    Signed-off-by: Kevin Wolf <kwolf@redhat.com>
    Reviewed-by: Jeff Cody <jcody@redhat.com>
    Reviewed-by: Eric Blake <eblake@redhat.com>

--- qemu-img.c
+++ qemu-img.c
@@ -2667,7 +2667,18 @@ static int img_amend(int argc, char **argv)
                 help();
                 break;
             case 'o':
-                options = optarg;
+                if (!is_valid_option_list(optarg)) {
+                    error_report("Invalid option list: %s", optarg);
+                    ret = -1;
+                    goto out;
+                }
+                if (!options) {
+                    options = g_strdup(optarg);
+                } else {
+                    char *old_options = options;
+                    options = g_strdup_printf("%s,%s", options, optarg);
+                    g_free(old_options);
+                }
                 break;
             case 'f':
                 fmt = optarg;
@@ -2697,7 +2708,7 @@ static int img_amend(int argc, char **argv)
 
     fmt = bs->drv->format_name;
 
-    if (is_help_option(options)) {
+    if (has_help_option(options)) {
         ret = print_block_option_help(filename, fmt);
         goto out;
     }
@@ -2724,6 +2735,8 @@ out:
     }
     free_option_parameters(create_options);
     free_option_parameters(options_param);
+    g_free(options);
+
     if (ret) {
         return 1;
     }
