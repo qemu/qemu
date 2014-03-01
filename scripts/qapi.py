678e48a2e43d1cafbb510c60d7597897b0d90256
Markus Armbruster <armbru@redhat.com>
2014-03-01 08:40:34 +0100

    qapi: Fix licensing of scripts
    The scripts carry this copyright notice:
    
        # This work is licensed under the terms of the GNU GPLv2.
        # See the COPYING.LIB file in the top-level directory.
    
    The sentences contradict each other, as COPYING.LIB contains the LGPL
    2.1.  Michael Roth says this was a simple pasto, and he meant to refer
    COPYING.  Let's fix that.
    
    Signed-off-by: Markus Armbruster <armbru@redhat.com>
    Reviewed-by: Eric Blake <eblake@redhat.com>
    Signed-off-by: Luiz Capitulino <lcapitulino@redhat.com>

--- scripts/qapi.py
+++ scripts/qapi.py
@@ -8,8 +8,8 @@
 #  Anthony Liguori <aliguori@us.ibm.com>
 #  Markus Armbruster <armbru@redhat.com>
 #
-# This work is licensed under the terms of the GNU GPLv2.
-# See the COPYING.LIB file in the top-level directory.
+# This work is licensed under the terms of the GNU GPL, version 2.
+# See the COPYING file in the top-level directory.
 
 from ordereddict import OrderedDict
 import sys
