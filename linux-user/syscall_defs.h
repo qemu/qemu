cd98d390ae009c5c1be5ae9a656e943bb47e14f0
Andreas Schwab <schwab@suse.de>
2014-03-02 19:36:40 +0000

    linux-user: Don't use UID16 on AArch64
    The AArch64 kernel defines its __kernel_uid_t type as 32 bits, unlike
    32 bit ARM, so don't enable our 16-bit UID wrapper handling.
    
    Signed-off-by: Andreas Schwab <schwab@suse.de>
    Signed-off-by: Peter Maydell <peter.maydell@linaro.org>
    Signed-off-by: Riku Voipio <riku.voipio@linaro.org>
    Reviewed-by: Richard Henderson <rth@twiddle.net>

--- linux-user/syscall_defs.h
+++ linux-user/syscall_defs.h
@@ -53,7 +53,8 @@
 #define TARGET_IOC_NRBITS	8
 #define TARGET_IOC_TYPEBITS	8
 
-#if defined(TARGET_I386) || defined(TARGET_ARM) || defined(TARGET_SPARC) \
+#if defined(TARGET_I386) || (defined(TARGET_ARM) && defined(TARGET_ABI32)) \
+    || defined(TARGET_SPARC) \
     || defined(TARGET_M68K) || defined(TARGET_SH4) || defined(TARGET_CRIS)
     /* 16 bit uid wrappers emulation */
 #define USE_UID16
