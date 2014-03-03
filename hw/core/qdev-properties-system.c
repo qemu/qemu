6e4a876b433f78f72724f45ae3f9e26596da1b4d
Stefan Hajnoczi <stefanha@redhat.com>
2014-03-03 11:30:07 +0100

    iothread: add "iothread" qdev property type
    Add a "iothread" qdev property type so devices can be hooked up to an
    IOThread from the comand-line:
    
      qemu -object iothread,id=iothread0 \
           -device some-device,x-iothread=iothread0
    
    Note that Paolo Bonzini <pbonzini@redhat.com> has suggested using QOM
    links instead.  This way the relationship between the objects is
    reflected in QOM.  There are currently shortcomings of
    object_property_add_link() which prevent this use case.  I will attempt
    to fix them and move to QOM links in a separate series.
    
    Signed-off-by: Stefan Hajnoczi <stefanha@redhat.com>

--- hw/core/qdev-properties-system.c
+++ hw/core/qdev-properties-system.c
@@ -18,6 +18,7 @@
 #include "net/hub.h"
 #include "qapi/visitor.h"
 #include "sysemu/char.h"
+#include "sysemu/iothread.h"
 
 static void get_pointer(Object *obj, Visitor *v, Property *prop,
                         char *(*print)(void *ptr),
@@ -384,6 +385,56 @@ void qdev_set_nic_properties(DeviceState *dev, NICInfo *nd)
     }
     nd->instantiated = 1;
 }
+
+/* --- iothread --- */
+
+static char *print_iothread(void *ptr)
+{
+    return iothread_get_id(ptr);
+}
+
+static int parse_iothread(DeviceState *dev, const char *str, void **ptr)
+{
+    IOThread *iothread;
+
+    iothread = iothread_find(str);
+    if (!iothread) {
+        return -ENOENT;
+    }
+    object_ref(OBJECT(iothread));
+    *ptr = iothread;
+    return 0;
+}
+
+static void get_iothread(Object *obj, struct Visitor *v, void *opaque,
+                         const char *name, Error **errp)
+{
+    get_pointer(obj, v, opaque, print_iothread, name, errp);
+}
+
+static void set_iothread(Object *obj, struct Visitor *v, void *opaque,
+                         const char *name, Error **errp)
+{
+    set_pointer(obj, v, opaque, parse_iothread, name, errp);
+}
+
+static void release_iothread(Object *obj, const char *name, void *opaque)
+{
+    DeviceState *dev = DEVICE(obj);
+    Property *prop = opaque;
+    IOThread **ptr = qdev_get_prop_ptr(dev, prop);
+
+    if (*ptr) {
+        object_unref(OBJECT(*ptr));
+    }
+}
+
+PropertyInfo qdev_prop_iothread = {
+    .name = "iothread",
+    .get = get_iothread,
+    .set = set_iothread,
+    .release = release_iothread,
+};
 
 static int qdev_add_one_global(QemuOpts *opts, void *opaque)
 {
