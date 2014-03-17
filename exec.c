c01a71c1a56fa27f43449ff59e5d03b2483658a2
Christian Borntraeger <borntraeger@de.ibm.com>
2014-03-17 17:13:12 +0100

    exec: Fix CPU rework fallout
    Commit 259186a7d2f7184efc96ae99bc5658e6159f53ad (cpu: Move halted and
    interrupt_request fields to CPUState) passed CPUState::env_ptr to
    tlb_flush() directory rather than through a typed variable.
    
    Commit 00c8cb0a36f51a6866a83c08962d12a0eb21864b (cputlb: Change
    tlb_flush() argument to CPUState) now changed the argument type.
    This was unnoticed by gcc because env_ptr is a void pointer.
    
    Signed-off-by: Christian Borntraeger <borntraeger@de.ibm.com>
    Signed-off-by: Andreas Färber <afaerber@suse.de>

--- exec.c
+++ exec.c
@@ -420,7 +420,7 @@ static int cpu_common_post_load(void *opaque, int version_id)
     /* 0x01 was CPU_INTERRUPT_EXIT. This line can be removed when the
        version_id is increased. */
     cpu->interrupt_request &= ~0x01;
-    tlb_flush(cpu->env_ptr, 1);
+    tlb_flush(cpu, 1);
 
     return 0;
 }
