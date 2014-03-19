b0f49d138777fb6609aa2ea96d2c59fb872d2c2d
Prasad Joshi <prasadjoshi.linux@gmail.com>
2014-03-19 07:10:32 +0530

    scsi: check req pointer before dereferencing it
    Signed-off-by: Prasad Joshi <prasadjoshi.linux@gmail.com>
    Signed-off-by: Paolo Bonzini <pbonzini@redhat.com>

--- hw/scsi/vmw_pvscsi.c
+++ hw/scsi/vmw_pvscsi.c
@@ -479,12 +479,13 @@ static void
 pvscsi_command_complete(SCSIRequest *req, uint32_t status, size_t resid)
 {
     PVSCSIRequest *pvscsi_req = req->hba_private;
-    PVSCSIState *s = pvscsi_req->dev;
+    PVSCSIState *s;
 
     if (!pvscsi_req) {
         trace_pvscsi_command_complete_not_found(req->tag);
         return;
     }
+    s = pvscsi_req->dev;
 
     if (resid) {
         /* Short transfer.  */
