0419f78fae1d70bb5de0d44be62ec9741c5a742b
Hani Benhabiles <kroosec@gmail.com>
2014-03-31 23:09:06 +0100

    input: mouse_set should check input device type.
    Otherwise, the index of an input device like a usb-kbd is silently accepted.
    
    (qemu) info mice
      Mouse #2: QEMU PS/2 Mouse
    * Mouse #3: QEMU HID Mouse
    (qemu) mouse_set 1
    (qemu) info mice
      Mouse #2: QEMU PS/2 Mouse
    * Mouse #3: QEMU HID Mouse
    
    Also replace monitor_printf() call in do_mouse_set() with error_report() and
    adjust error message.
    
    Signed-off-by: Hani Benhabiles <hani@linux.com>
    Signed-off-by: Gerd Hoffmann <kraxel@redhat.com>

--- ui/input.c
+++ ui/input.c
@@ -342,15 +342,21 @@ void do_mouse_set(Monitor *mon, const QDict *qdict)
     int found = 0;
 
     QTAILQ_FOREACH(s, &handlers, node) {
-        if (s->id == index) {
-            found = 1;
-            qemu_input_handler_activate(s);
-            break;
+        if (s->id != index) {
+            continue;
         }
+        if (!(s->handler->mask & (INPUT_EVENT_MASK_REL |
+                                  INPUT_EVENT_MASK_ABS))) {
+            error_report("Input device '%s' is not a mouse", s->handler->name);
+            return;
+        }
+        found = 1;
+        qemu_input_handler_activate(s);
+        break;
     }
 
     if (!found) {
-        monitor_printf(mon, "Mouse at given index not found\n");
+        error_report("Mouse at index '%d' not found", index);
     }
 
     qemu_input_check_mode_change();
