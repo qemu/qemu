4e48018ae9edfafa25fb4ddca745307bd66cfc63
Michael S. Tsirkin <mst@redhat.com>
2014-04-03 19:51:53 +0300

    virtio: validate num_sg when mapping
    CVE-2013-4535
    CVE-2013-4536
    
    Both virtio-block and virtio-serial read,
    VirtQueueElements are read in as buffers, and passed to
    virtqueue_map_sg(), where num_sg is taken from the wire and can force
    writes to indicies beyond VIRTQUEUE_MAX_SIZE.
    
    To fix, validate num_sg.
    
    Reported-by: Michael Roth <mdroth@linux.vnet.ibm.com>
    Signed-off-by: Michael S. Tsirkin <mst@redhat.com>
    Cc: Amit Shah <amit.shah@redhat.com>
    Signed-off-by: Juan Quintela <quintela@redhat.com>
    (cherry picked from commit 36cf2a37132c7f01fa9adb5f95f5312b27742fd4)
    Signed-off-by: Michael Roth <mdroth@linux.vnet.ibm.com>

--- hw/virtio/virtio.c
+++ hw/virtio/virtio.c
@@ -430,6 +430,12 @@ void virtqueue_map_sg(struct iovec *sg, hwaddr *addr,
     unsigned int i;
     hwaddr len;
 
+    if (num_sg >= VIRTQUEUE_MAX_SIZE) {
+        error_report("virtio: map attempt out of bounds: %zd > %d",
+                     num_sg, VIRTQUEUE_MAX_SIZE);
+        exit(1);
+    }
+
     for (i = 0; i < num_sg; i++) {
         len = sg[i].iov_len;
         sg[i].iov_base = cpu_physical_memory_map(addr[i], &len, is_write);
