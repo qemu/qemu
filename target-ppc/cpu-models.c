70d246c335d7fef745c3068f109926a8cee08220
Alexey Kardashevskiy <aik@ozlabs.ru>
2014-04-12 03:34:27 +1000

    target-ppc: Remove redundant POWER7 declarations
    At the moment there are 3 versions of POWER7 CPUs defined. However
    we do not emulate these CPUs diffent and it does not make much
    sense to keep them all.
    
    This removes POWER7_v2.0 and POWER7_v2.1 and leaves just one versioned
    CPU per family which is POWER7_v2.3 with POWER7 alias.
    
    Signed-off-by: Alexey Kardashevskiy <aik@ozlabs.ru>
    Signed-off-by: Alexander Graf <agraf@suse.de>

--- target-ppc/cpu-models.c
+++ target-ppc/cpu-models.c
@@ -1134,10 +1134,6 @@
     POWERPC_DEF("POWER6A",       CPU_POWERPC_POWER6A,                POWER6,
                 "POWER6A")
 #endif
-    POWERPC_DEF("POWER7_v2.0",   CPU_POWERPC_POWER7_v20,             POWER7,
-                "POWER7 v2.0")
-    POWERPC_DEF("POWER7_v2.1",   CPU_POWERPC_POWER7_v21,             POWER7,
-                "POWER7 v2.1")
     POWERPC_DEF("POWER7_v2.3",   CPU_POWERPC_POWER7_v23,             POWER7,
                 "POWER7 v2.3")
     POWERPC_DEF("POWER7+_v2.1",  CPU_POWERPC_POWER7P_v21,            POWER7P,
