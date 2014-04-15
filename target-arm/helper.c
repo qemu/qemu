9225d739e7f6ec8d2139f79c3d2e3282cc725364
Peter Maydell <peter.maydell@linaro.org>
2014-04-15 19:18:41 +0100

    target-arm: Don't mention PMU in debug feature register
    Suppress the ID_AA64DFR0_EL1 PMUVer field, even if the CPU specific
    value claims that it exists. QEMU doesn't currently implement it,
    and not advertising it prevents the guest from trying to use it
    and getting UNDEFs on unimplemented registers.
    
    Signed-off-by: Peter Maydell <peter.maydell@linaro.org>
    Reviewed-by: Peter Crosthwaite <peter.crosthwaite@xilinx.com>
    ---
    This is arguably a hack, but otherwise Linux tries to prod
    half a dozen PMU sysregs.

--- target-arm/helper.c
+++ target-arm/helper.c
@@ -2036,7 +2036,12 @@ void register_cp_regs_for_features(ARMCPU *cpu)
             { .name = "ID_AA64DFR0_EL1", .state = ARM_CP_STATE_AA64,
               .opc0 = 3, .opc1 = 0, .crn = 0, .crm = 5, .opc2 = 0,
               .access = PL1_R, .type = ARM_CP_CONST,
-              .resetvalue = cpu->id_aa64dfr0 },
+              /* We mask out the PMUVer field, beacuse we don't currently
+               * implement the PMU. Not advertising it prevents the guest
+               * from trying to use it and getting UNDEFs on registers we
+               * don't implement.
+               */
+              .resetvalue = cpu->id_aa64dfr0 & ~0xf00 },
             { .name = "ID_AA64DFR1_EL1", .state = ARM_CP_STATE_AA64,
               .opc0 = 3, .opc1 = 0, .crn = 0, .crm = 5, .opc2 = 1,
               .access = PL1_R, .type = ARM_CP_CONST,
