85acfa9c38880d5d2b3cc71a5e541a267c9f7396
Peter Maydell <peter.maydell@linaro.org>
2014-04-15 19:18:46 +0100

    target-arm: Remove THUMB2EE feature from AArch64 'any' CPU
    The AArch64 usermode 'any' CPU type was accidentally specified
    with the ARM_FEATURE_THUMB2EE bit set. This is incorrect since
    ARMv8 removes Thumb2EE completely. Since we never implemented
    Thumb2EE anyway having the feature bit set was fairly harmless
    for user-mode, but the correct thing is to not set it at all.
    
    Signed-off-by: Peter Maydell <peter.maydell@linaro.org>
    Reviewed-by: Peter Crosthwaite <peter.crosthwaite@xilinx.com>

--- target-arm/cpu64.c
+++ target-arm/cpu64.c
@@ -83,7 +83,6 @@ static void aarch64_any_initfn(Object *obj)
     set_feature(&cpu->env, ARM_FEATURE_VFP4);
     set_feature(&cpu->env, ARM_FEATURE_VFP_FP16);
     set_feature(&cpu->env, ARM_FEATURE_NEON);
-    set_feature(&cpu->env, ARM_FEATURE_THUMB2EE);
     set_feature(&cpu->env, ARM_FEATURE_ARM_DIV);
     set_feature(&cpu->env, ARM_FEATURE_V7MP);
     set_feature(&cpu->env, ARM_FEATURE_AARCH64);
