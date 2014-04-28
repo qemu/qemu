e855e4fb7b97f7f605e1f44427b98022e39e6f8f
Jeff Cody <jcody@redhat.com>
2014-04-28 18:29:54 -0400

    block: Ignore duplicate or NULL format_name in bdrv_iterate_format
    Some block drivers have multiple BlockDriver instances with identical
    format_name fields (e.g. gluster, nbd).
    
    Both qemu-img and qemu will use bdrv_iterate_format() to list the
    supported formats when a help option is invoked.  As protocols and
    formats may register multiple drivers, redundant listings of formats
    occur (e.g., "Supported formats: ... gluster gluster gluster gluster ...
    ").
    
    Since the list of driver formats will be small, this performs a simple
    linear search on format_name, and ignores any duplicates.
    
    The end result change is that the iterator will no longer receive
    duplicate string names, nor will it receive NULL pointers.
    
    Signed-off-by: Jeff Cody <jcody@redhat.com>
    Signed-off-by: Kevin Wolf <kwolf@redhat.com>

--- block.c
+++ block.c
@@ -3601,10 +3601,25 @@ void bdrv_iterate_format(void (*it)(void *opaque, const char *name),
                          void *opaque)
 {
     BlockDriver *drv;
+    int count = 0;
+    const char **formats = NULL;
 
     QLIST_FOREACH(drv, &bdrv_drivers, list) {
-        it(opaque, drv->format_name);
+        if (drv->format_name) {
+            bool found = false;
+            int i = count;
+            while (formats && i && !found) {
+                found = !strcmp(formats[--i], drv->format_name);
+            }
+
+            if (!found) {
+                formats = g_realloc(formats, (count + 1) * sizeof(char *));
+                formats[count++] = drv->format_name;
+                it(opaque, drv->format_name);
+            }
+        }
     }
+    g_free(formats);
 }
 
 /* This function is to find block backend bs */
