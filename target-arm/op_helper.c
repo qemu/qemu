37f0806ed9ebaa3b7a7840048818b8916639f2db
Edgar E. Iglesias <edgar.iglesias@xilinx.com>
2014-05-01 15:24:46 +0100

    target-arm: Correct a comment refering to EL0
    Signed-off-by: Edgar E. Iglesias <edgar.iglesias@xilinx.com>
    Message-id: 1398926097-28097-5-git-send-email-edgar.iglesias@gmail.com
    Signed-off-by: Peter Maydell <peter.maydell@linaro.org>

--- target-arm/op_helper.c
+++ target-arm/op_helper.c
@@ -418,7 +418,7 @@ void HELPER(exception_return)(CPUARMState *env)
             goto illegal_return;
         }
         if (new_el == 0 && (spsr & PSTATE_SP)) {
-            /* Return to EL1 with M[0] bit set */
+            /* Return to EL0 with M[0] bit set */
             goto illegal_return;
         }
         env->aarch64 = 1;
