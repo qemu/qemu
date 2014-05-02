415168e0c7bda5371a876914d4fdb68c4556f28d
Markus Armbruster <armbru@redhat.com>
2014-05-02 13:26:34 +0200

    hmp: Guard against misuse of hmp_handle_error()
    Null errp argument makes no sense.  Assert it's not null, to make this
    explicit, and guard against misuse.  All current callers pass non-null
    errp.
    
    Signed-off-by: Markus Armbruster <armbru@redhat.com>
    Reviewed-by: Eric Blake <eblake@redhat.com>
    Signed-off-by: Luiz Capitulino <lcapitulino@redhat.com>

--- hmp.c
+++ hmp.c
@@ -28,7 +28,8 @@
 
 static void hmp_handle_error(Monitor *mon, Error **errp)
 {
-    if (error_is_set(errp)) {
+    assert(errp);
+    if (*errp) {
         monitor_printf(mon, "%s\n", error_get_pretty(*errp));
         error_free(*errp);
     }
