fbdb664cb69730d043e7eb3b89db1cc8b419080f
Stefan Weil <sw@weilnetz.de>
2014-05-03 08:12:15 +0200

    qemu-timer: Add missing 'static' attribute
    This fixes a warning from the static code analysis (smatch).
    
    Signed-off-by: Stefan Weil <sw@weilnetz.de>
    Signed-off-by: Michael Tokarev <mjt@tls.msk.ru>

--- qemu-timer.c
+++ qemu-timer.c
@@ -56,7 +56,7 @@ typedef struct QEMUClock {
 } QEMUClock;
 
 QEMUTimerListGroup main_loop_tlg;
-QEMUClock qemu_clocks[QEMU_CLOCK_MAX];
+static QEMUClock qemu_clocks[QEMU_CLOCK_MAX];
 
 /* A QEMUTimerList is a list of timers attached to a clock. More
  * than one QEMUTimerList can be attached to each clock, for instance
