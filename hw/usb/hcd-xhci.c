463c534db516701ac732ac606c33c7101cf22e56
Gerd Hoffmann <kraxel@redhat.com>
2014-05-12 14:43:51 +0200

    xhci: child detach fix
    xhci_child_detach() zaps the wrong slot when unplugging a device
    connected via usb-hub:  Instead of the device's slot the slot of the
    usb-hub is used.  Fix it.
    
    https://bugzilla.redhat.com/show_bug.cgi?id=1075846
    
    Signed-off-by: Gerd Hoffmann <kraxel@redhat.com>
    Reviewed-by: Gonglei <arei.gonglei@huawei.com>

--- hw/usb/hcd-xhci.c
+++ hw/usb/hcd-xhci.c
@@ -3435,7 +3435,7 @@ static void xhci_child_detach(USBPort *uport, USBDevice *child)
     USBBus *bus = usb_bus_from_device(child);
     XHCIState *xhci = container_of(bus, XHCIState, bus);
 
-    xhci_detach_slot(xhci, uport);
+    xhci_detach_slot(xhci, child->port);
 }
 
 static USBPortOps xhci_uport_ops = {
