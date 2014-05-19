b44672849ad9baaa6734f16bfd7aa0d6b0780065
Jiri Pirko <jiri@resnulli.us>
2014-05-19 15:47:16 +0200

    vmxnet3: fix msix vectors unuse
    In vmxnet3_cleanup_msix(), there is called msix_vector_unuse() with
    VMXNET3_MAX_INTRS. That is not correct since vector of
    value VMXNET3_MAX_INTRS was never used. Also all the used vectors
    are not un-used. So call vmxnet3_unuse_msix_vectors() instead which
    does the correct job.
    
    Signed-off-by: Jiri Pirko <jiri@resnulli.us>
    Acked-by: Dmitry Fleytman <dmitry@daynix.com>
    Signed-off-by: Stefan Hajnoczi <stefanha@redhat.com>

--- hw/net/vmxnet3.c
+++ hw/net/vmxnet3.c
@@ -2050,7 +2050,7 @@ vmxnet3_cleanup_msix(VMXNET3State *s)
     PCIDevice *d = PCI_DEVICE(s);
 
     if (s->msix_used) {
-        msix_vector_unuse(d, VMXNET3_MAX_INTRS);
+        vmxnet3_unuse_msix_vectors(s, VMXNET3_MAX_INTRS);
         msix_uninit(d, &s->msix_bar, &s->msix_bar);
     }
 }
