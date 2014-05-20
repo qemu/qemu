ae2e18e0cab1715e03ce4ff90e8362507b2f325d
Max Filippov <jcmvbkbc@gmail.com>
2014-05-20 14:48:45 +0400

    target-xtensa: fix cross-page jumps/calls at the end of TB
    Use tb->pc instead of dc->pc to check for cross-page jumps.
    When TB translation stops at the page boundary dc->pc points to the next
    page allowing chaining to TBs in it, which is wrong.
    
    Cc: qemu-stable@nongnu.org
    Signed-off-by: Max Filippov <jcmvbkbc@gmail.com>
    (cherry picked from commit 433d33c555deeed375996e338df1a9510df401c6)
    Signed-off-by: Michael Roth <mdroth@linux.vnet.ibm.com>

--- target-xtensa/translate.c
+++ target-xtensa/translate.c
@@ -419,7 +419,7 @@ static void gen_jump(DisasContext *dc, TCGv dest)
 static void gen_jumpi(DisasContext *dc, uint32_t dest, int slot)
 {
     TCGv_i32 tmp = tcg_const_i32(dest);
-    if (((dc->pc ^ dest) & TARGET_PAGE_MASK) != 0) {
+    if (((dc->tb->pc ^ dest) & TARGET_PAGE_MASK) != 0) {
         slot = -1;
     }
     gen_jump_slot(dc, tmp, slot);
@@ -447,7 +447,7 @@ static void gen_callw(DisasContext *dc, int callinc, TCGv_i32 dest)
 static void gen_callwi(DisasContext *dc, int callinc, uint32_t dest, int slot)
 {
     TCGv_i32 tmp = tcg_const_i32(dest);
-    if (((dc->pc ^ dest) & TARGET_PAGE_MASK) != 0) {
+    if (((dc->tb->pc ^ dest) & TARGET_PAGE_MASK) != 0) {
         slot = -1;
     }
     gen_callw_slot(dc, callinc, tmp, slot);
