b167383ffbe995c6d48fbe8ca2bab5eb667e45dc
Gabriel L. Somlo <gsomlo@gmail.com>
2014-06-02 09:33:28 -0400

    tests: e1000: test additional device IDs
    Update e1000-test.c to check all currently supported devices.
    
    Suggested-by: Andreas Färber <afaerber@suse.de>
    Signed-off-by: Gabriel Somlo <somlo@cmu.edu>
    Reviewed-by: Michael S. Tsirkin <mst@redhat.com>
    Reviewed-by: Peter Crosthwaite <peter.crosthwaite@xilinx.com>
    Reviewed-by: Stefan Hajnoczi <stefanha@redhat.com>
    Signed-off-by: Stefan Hajnoczi <stefanha@redhat.com>

--- tests/e1000-test.c
+++ tests/e1000-test.c
@@ -13,21 +13,41 @@
 #include "qemu/osdep.h"
 
 /* Tests only initialization so far. TODO: Replace with functional tests */
-static void nop(void)
+static void test_device(gconstpointer data)
 {
+    const char *model = data;
+    QTestState *s;
+    char *args;
+
+    args = g_strdup_printf("-device %s", model);
+    s = qtest_start(args);
+
+    if (s) {
+        qtest_quit(s);
+    }
+    g_free(args);
 }
+
+static const char *models[] = {
+    "e1000",
+    "e1000-82540em",
+    "e1000-82544gc",
+    "e1000-82545em",
+    "e1000-82573l",
+};
 
 int main(int argc, char **argv)
 {
-    int ret;
+    int i;
 
     g_test_init(&argc, &argv, NULL);
-    qtest_add_func("/e1000/nop", nop);
 
-    qtest_start("-device e1000");
-    ret = g_test_run();
+    for (i = 0; i < ARRAY_SIZE(models); i++) {
+        char *path;
 
-    qtest_end();
+        path = g_strdup_printf("/%s/e1000/%s", qtest_get_arch(), models[i]);
+        g_test_add_data_func(path, models[i], test_device);
+    }
 
-    return ret;
+    return g_test_run();
 }
