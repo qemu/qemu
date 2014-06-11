d13fc32ecf8d810ec9894a35e1cfae81f7d88039
Alexander Graf <agraf@suse.de>
2014-06-11 12:19:03 +0200

    PPC: KVM: Make pv hcall endian agnostic
    There were a few revisions of the Linux kernel that incorrectly swapped
    the hcall instructions when they saw ePAPR compliant hypercalls.
    
    We already have fixups for those in place when running with PR KVM, but
    HV KVM and systems that don't implement hypercalls at all are still broken
    because they fall back to the QEMU implementation of fallback hypercalls.
    
    So let's make the fallback hypercall instruction path endian agnostic. This
    only really works well for 64bit guests, but I don't think there are any 32bit
    systems left that don't implement real pv hcall support, so we'll never get
    into this code path.
    
    Signed-off-by: Alexander Graf <agraf@suse.de>

--- target-ppc/kvm.c
+++ target-ppc/kvm.c
@@ -1525,18 +1525,18 @@ int kvmppc_get_hypercall(CPUPPCState *env, uint8_t *buf, int buf_len)
     }
 
     /*
-     * Fallback to always fail hypercalls:
+     * Fallback to always fail hypercalls regardless of endianness:
      *
+     *     tdi 0,r0,72 (becomes b .+8 in wrong endian, nop in good endian)
      *     li r3, -1
-     *     nop
-     *     nop
-     *     nop
+     *     b .+8       (becomes nop in wrong endian)
+     *     bswap32(li r3, -1)
      */
 
-    hc[0] = 0x3860ffff;
-    hc[1] = 0x60000000;
-    hc[2] = 0x60000000;
-    hc[3] = 0x60000000;
+    hc[0] = cpu_to_be32(0x08000048);
+    hc[1] = cpu_to_be32(0x3860ffff);
+    hc[2] = cpu_to_be32(0x48000008);
+    hc[3] = cpu_to_be32(bswap32(0x3860ffff));
 
     return 0;
 }
