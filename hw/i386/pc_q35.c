48cb7f3c1526b4632bd63d945cac80d26616d6f5
Eduardo Habkost <ehabkost@redhat.com>
2014-06-13 00:28:32 -0300

    q35: Use PC_Q35_COMPAT_1_4 on pc-q35-1.4 compat_props
    pc-q35-1.4 was incorrectly using PC_COMPAT_1_4 instead of
    PC_Q35_COMPAT_1_4.
    
    The only side-effect was that the hpet compat property (inherited from
    PC_Q35_COMPAT_1_7) was missing.
    
    Without this patch, pc-q35-1.4 inicorrectly initializes hpet-intcap to
    0xff0104 (behavior introduced in QEMU 2.0, by commit
    7a10ef51c2397ac4323bc786af02c58b413b5cd2).
    
    Signed-off-by: Eduardo Habkost <ehabkost@redhat.com>
    Reviewed-by: Michael S. Tsirkin <mst@redhat.com>
    Signed-off-by: Michael S. Tsirkin <mst@redhat.com>
    
    Reviewed-by: Markus Armbruster <armbru@redhat.com>

--- hw/i386/pc_q35.c
+++ hw/i386/pc_q35.c
@@ -388,7 +388,7 @@ static QEMUMachine pc_q35_machine_v1_4 = {
     .name = "pc-q35-1.4",
     .init = pc_q35_init_1_4,
     .compat_props = (GlobalProperty[]) {
-        PC_COMPAT_1_4,
+        PC_Q35_COMPAT_1_4,
         { /* end of list */ }
     },
 };
