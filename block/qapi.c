13344f3a17e0a785c0eb8e36f69518f21aa8a91a
Stefan Hajnoczi <stefanha@redhat.com>
2014-06-17 14:32:05 +0800

    block: acquire AioContext in qmp_query_blockstats()
    Make query-blockstats safe for dataplane by acquiring the
    BlockDriverState's AioContext.  This ensures that the dataplane IOThread
    and the main loop's monitor code do not race.
    
    Note the assumption that acquiring the drive's BDS AioContext also
    protects ->file and ->backing_hd.  This assumption is made by other
    aio_context_acquire() callers too.
    
    Signed-off-by: Stefan Hajnoczi <stefanha@redhat.com>
    Signed-off-by: Fam Zheng <famz@redhat.com>
    Tested-by: Paolo Bonzini <pbonzini@redhat.com>
    Reviewed-by: Stefan Hajnoczi <stefanha@redhat.com>
    Signed-off-by: Kevin Wolf <kwolf@redhat.com>

--- block/qapi.c
+++ block/qapi.c
@@ -360,7 +360,11 @@ BlockStatsList *qmp_query_blockstats(Error **errp)
 
      while ((bs = bdrv_next(bs))) {
         BlockStatsList *info = g_malloc0(sizeof(*info));
+        AioContext *ctx = bdrv_get_aio_context(bs);
+
+        aio_context_acquire(ctx);
         info->value = bdrv_query_stats(bs);
+        aio_context_release(ctx);
 
         *p_next = info;
         p_next = &info->next;
