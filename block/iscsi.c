9d2e256e62738f17d067258012f9a32e74bb9a8e
Peter Lieven <pl@kamp.de>
2014-06-18 18:40:11 +0200

    block/iscsi: fix init value for iTask->retries
    during rebasing the changed init value for the
    retry counter was missed. This resulted in no retries
    being performed at all.
    
    Signed-off-by: Peter Lieven <pl@kamp.de>
    Signed-off-by: Paolo Bonzini <pbonzini@redhat.com>

--- block/iscsi.c
+++ block/iscsi.c
@@ -216,7 +216,6 @@ static void iscsi_co_init_iscsitask(IscsiLun *iscsilun, struct IscsiTask *iTask)
 {
     *iTask = (struct IscsiTask) {
         .co         = qemu_coroutine_self(),
-        .retries    = ISCSI_CMD_RETRIES,
         .iscsilun   = iscsilun,
     };
 }
