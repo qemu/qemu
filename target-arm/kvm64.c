73542cf6900146f4a203007388e224e5dc25c7da
Pranavkumar Sawargaonkar <pranavkumar@linaro.org>
2014-06-19 18:06:26 +0100

    target-arm: Implement kvm_arch_reset_vcpu() for KVM ARM64
    To implement kvm_arch_reset_vcpu(), we simply re-init the VCPU
    using kvm_arm_vcpu_init() so that all registers of VCPU are set
    to their reset values by in-kernel KVM code.
    
    Signed-off-by: Pranavkumar Sawargaonkar <pranavkumar@linaro.org>
    Signed-off-by: Anup Patel <anup.patel@linaro.org>
    Reviewed-by: Peter Maydell <peter.maydell@linaro.org>
    Message-id: 1402901605-24551-7-git-send-email-pranavkumar@linaro.org
    Signed-off-by: Peter Maydell <peter.maydell@linaro.org>

--- target-arm/kvm64.c
+++ target-arm/kvm64.c
@@ -269,4 +269,8 @@ int kvm_arch_get_registers(CPUState *cs)
 
 void kvm_arm_reset_vcpu(ARMCPU *cpu)
 {
+    /* Re-init VCPU so that all registers are set to
+     * their respective reset values.
+     */
+    kvm_arm_vcpu_init(CPU(cpu));
 }
