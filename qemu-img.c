fdcf6e65bce1f8972030fed7af5e8aa5f6ae92c6
Chen Gang <gang.chen.5i5j@gmail.com>
2014-07-06 16:43:33 +0800

    qemu-img: Check getchar() return value in read_password() for WIN32
    getchar() is a standard c library function which may return with failure
    (e.g. -1), so like another platforms, also need check it under WIN32.
    
    And make the related code match current qemu code styles, too.
    
    Signed-off-by: Chen Gang <gang.chen.5i5j@gmail.com>
    Signed-off-by: Michael Tokarev <mjt@tls.msk.ru>

--- qemu-img.c
+++ qemu-img.c
@@ -185,15 +185,20 @@ static int GCC_FMT_ATTR(2, 3) qprintf(bool quiet, const char *fmt, ...)
 static int read_password(char *buf, int buf_size)
 {
     int c, i;
+
     printf("Password: ");
     fflush(stdout);
     i = 0;
     for(;;) {
         c = getchar();
-        if (c == '\n')
+        if (c < 0) {
+            buf[i] = '\0';
+            return -1;
+        } else if (c == '\n') {
             break;
-        if (i < (buf_size - 1))
+        } else if (i < (buf_size - 1)) {
             buf[i++] = c;
+        }
     }
     buf[i] = '\0';
     return 0;
