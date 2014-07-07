5508099397c480f1c3b4f14b0e64593ebe284b26
Benjamin Herrenschmidt <benh@kernel.crashing.org>
2014-07-07 10:17:44 +1000

    vga: Add mechanism to force the use of a shadow surface
    This prevents surface sharing which will be necessary to
    fix cirrus HW cursor support.
    
    Signed-off-by: Benjamin Herrenschmidt <benh@kernel.crashing.org>
    Signed-off-by: Gerd Hoffmann <kraxel@redhat.com>

--- hw/display/vga_int.h
+++ hw/display/vga_int.h
@@ -151,6 +151,7 @@ typedef struct VGACommonState {
     uint32_t last_scr_width, last_scr_height; /* in pixels */
     uint32_t last_depth; /* in bits */
     bool last_byteswap;
+    bool force_shadow;
     uint8_t cursor_start, cursor_end;
     bool cursor_visible_phase;
     int64_t cursor_blink_time;
