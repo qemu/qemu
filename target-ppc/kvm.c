6fd33a750214a866772dd77573cfa24c27ad956d
Alexander Graf <agraf@suse.de>
2014-07-14 19:17:35 +0200

    PPC: KVM: Use vm check_extension for pv hcall
    To find out whether we support the KVM hypercall interface we need to ask KVM
    on the VM level rather than the global KVM level, because Book3S HV KVM does
    not support it and we play conservative when both HV and PR are loaded.
    
    So instead, use the VM helper that falls back to global KVM enumeration. That
    should cover all cases.
    
    Signed-off-by: Alexander Graf <agraf@suse.de>

--- target-ppc/kvm.c
+++ target-ppc/kvm.c
@@ -1835,7 +1835,7 @@ static int kvmppc_get_pvinfo(CPUPPCState *env, struct kvm_ppc_pvinfo *pvinfo)
      PowerPCCPU *cpu = ppc_env_get_cpu(env);
      CPUState *cs = CPU(cpu);
 
-    if (kvm_check_extension(cs->kvm_state, KVM_CAP_PPC_GET_PVINFO) &&
+    if (kvm_vm_check_extension(cs->kvm_state, KVM_CAP_PPC_GET_PVINFO) &&
         !kvm_vm_ioctl(cs->kvm_state, KVM_PPC_GET_PVINFO, pvinfo)) {
         return 0;
     }
