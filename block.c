9a4d5ca60772e09d0cbac01f1b4778aa68e00eaa
Jeff Cody <jcody@redhat.com>
2014-07-23 17:22:57 -0400

    block: allow bdrv_unref() to be passed NULL pointers
    If bdrv_unref() is passed a NULL BDS pointer, it is safe to
    exit with no operation.  This will allow cleanup code to blindly
    call bdrv_unref() on a BDS that has been initialized to NULL.
    
    Reviewed-by: Max Reitz <mreitz@redhat.com>
    Signed-off-by: Jeff Cody <jcody@redhat.com>
    Signed-off-by: Kevin Wolf <kwolf@redhat.com>

--- block.c
+++ block.c
@@ -5381,6 +5381,9 @@ void bdrv_ref(BlockDriverState *bs)
  * deleted. */
 void bdrv_unref(BlockDriverState *bs)
 {
+    if (!bs) {
+        return;
+    }
     assert(bs->refcnt > 0);
     if (--bs->refcnt == 0) {
         bdrv_delete(bs);
