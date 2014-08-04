a7afc6b8c13c70e9c40b4f666be80600f8ad0b3d
John Snow <jsnow@redhat.com>
2014-08-04 17:11:22 -0400

    libqtest: Correct small memory leak.
    Fixes a small memory leak inside of libqtest.
    After we produce a test path and glib copies the string
    for itself, we should clean up our temporary copy.
    
    Signed-off-by: John Snow <jsnow@redhat.com>
    Signed-off-by: Stefan Hajnoczi <stefanha@redhat.com>

--- tests/libqtest.c
+++ tests/libqtest.c
@@ -644,6 +644,7 @@ void qtest_add_func(const char *str, void (*fn))
 {
     gchar *path = g_strdup_printf("/%s/%s", qtest_get_arch(), str);
     g_test_add_func(path, fn);
+    g_free(path);
 }
 
 void qtest_memwrite(QTestState *s, uint64_t addr, const void *data, size_t size)
