552b48f44df99e095f04245a48d3b839bbe9912c
John Snow <jsnow@redhat.com>
2014-08-04 17:11:19 -0400

    q35: Enable the ioapic device to be seen by qtest.
    Currently, the ioapic device can not be found in a qtest environment
    when requesting "irq_interrupt_in ioapic" via the qtest socket.
    
    By mirroring how the ioapic is added in i44ofx (hw/i440/pc_piix.c),
    as a child of "q35," the device is able to be seen by qtest.
    
    Signed-off-by: John Snow <jsnow@redhat.com>
    Signed-off-by: Stefan Hajnoczi <stefanha@redhat.com>

--- hw/i386/pc_q35.c
+++ hw/i386/pc_q35.c
@@ -236,7 +236,7 @@ static void pc_q35_init(MachineState *machine)
         gsi_state->i8259_irq[i] = i8259[i];
     }
     if (pci_enabled) {
-        ioapic_init_gsi(gsi_state, NULL);
+        ioapic_init_gsi(gsi_state, "q35");
     }
     qdev_init_nofail(icc_bridge);
 
