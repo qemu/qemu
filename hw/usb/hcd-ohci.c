cae7f29c47dee0bd0474fa7f1dda28b115a34d33
Jack Un <jack.un@gmail.com>
2014-08-09 23:34:36 +0300

    Fix OHCI ISO TD state never being written back.
    There appears to be typo in OHCI with isochronous transfers
    resulting in isoch. transfer descriptor state never being written back.
    The'put_words' function is in a OR statement hence it is never called.
    
    Signed-off-by: Jack Un <jack.un@gmail.com>
    Signed-off-by: Gerd Hoffmann <kraxel@redhat.com>

--- hw/usb/hcd-ohci.c
+++ hw/usb/hcd-ohci.c
@@ -619,8 +619,8 @@ static inline int ohci_put_td(OHCIState *ohci,
 static inline int ohci_put_iso_td(OHCIState *ohci,
                                   dma_addr_t addr, struct ohci_iso_td *td)
 {
-    return put_dwords(ohci, addr, (uint32_t *)td, 4 ||
-           put_words(ohci, addr + 16, td->offset, 8));
+    return put_dwords(ohci, addr, (uint32_t *)td, 4) ||
+           put_words(ohci, addr + 16, td->offset, 8);
 }
 
 static inline int ohci_put_hcca(OHCIState *ohci,
