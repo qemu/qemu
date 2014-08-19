aad4dce934649b3a398396fc2a76f215bb194ea4
Jason Wang <jasowang@redhat.com>
2014-08-19 12:56:29 +0800

    vhost_net: start/stop guest notifiers properly
    commit a9f98bb5ebe6fb1869321dcc58e72041ae626ad8 vhost: multiqueue
    support changed the order of stopping the device. Previously
    vhost_dev_stop would disable backend and only afterwards, unset guest
    notifiers. We now unset guest notifiers while vhost is still
    active. This can lose interrupts causing guest networking to fail. In
    particular, this has been observed during migration.
    
    To adapt this, several other changes are needed:
    - remove the hdev->started assertion in vhost.c since we may want to
    start the guest notifiers before vhost starts and stop the guest
    notifiers after vhost is stopped.
    - introduce the vhost_net_set_vq_index() and call it before setting
    guest notifiers. This is used to guarantee vhost_net has the correct
    virtqueue index when setting guest notifiers.
    
    Cc: qemu-stable@nongnu.org
    Reported-by: "Zhangjie (HZ)" <zhangjie14@huawei.com>
    Tested-by: William Dauchy <wdauchy@gmail.com>
    Signed-off-by: Michael S. Tsirkin <mst@redhat.com>
    Signed-off-by: Jason Wang <jasowang@redhat.com>
    Reviewed-by: Michael S. Tsirkin <mst@redhat.com>
    Signed-off-by: Michael S. Tsirkin <mst@redhat.com>

--- hw/net/vhost_net.c
+++ hw/net/vhost_net.c
@@ -188,16 +188,19 @@ bool vhost_net_query(VHostNetState *net, VirtIODevice *dev)
     return vhost_dev_query(&net->dev, dev);
 }
 
+static void vhost_net_set_vq_index(struct vhost_net *net, int vq_index)
+{
+    net->dev.vq_index = vq_index;
+}
+
 static int vhost_net_start_one(struct vhost_net *net,
-                               VirtIODevice *dev,
-                               int vq_index)
+                               VirtIODevice *dev)
 {
     struct vhost_vring_file file = { };
     int r;
 
     net->dev.nvqs = 2;
     net->dev.vqs = net->vqs;
-    net->dev.vq_index = vq_index;
 
     r = vhost_dev_enable_notifiers(&net->dev, dev);
     if (r < 0) {
@@ -301,17 +304,21 @@ int vhost_net_start(VirtIODevice *dev, NetClientState *ncs,
     }
 
     for (i = 0; i < total_queues; i++) {
-        r = vhost_net_start_one(get_vhost_net(ncs[i].peer), dev, i * 2);
-
-        if (r < 0) {
-            goto err;
-        }
+        vhost_net_set_vq_index(get_vhost_net(ncs[i].peer), i * 2);
     }
 
     r = k->set_guest_notifiers(qbus->parent, total_queues * 2, true);
     if (r < 0) {
         error_report("Error binding guest notifier: %d", -r);
         goto err;
+    }
+
+    for (i = 0; i < total_queues; i++) {
+        r = vhost_net_start_one(get_vhost_net(ncs[i].peer), dev);
+
+        if (r < 0) {
+            goto err;
+        }
     }
 
     return 0;
@@ -331,16 +338,16 @@ void vhost_net_stop(VirtIODevice *dev, NetClientState *ncs,
     VirtioBusClass *k = VIRTIO_BUS_GET_CLASS(vbus);
     int i, r;
 
+    for (i = 0; i < total_queues; i++) {
+        vhost_net_stop_one(get_vhost_net(ncs[i].peer), dev);
+    }
+
     r = k->set_guest_notifiers(qbus->parent, total_queues * 2, false);
     if (r < 0) {
         fprintf(stderr, "vhost guest notifier cleanup failed: %d\n", r);
         fflush(stderr);
     }
     assert(r >= 0);
-
-    for (i = 0; i < total_queues; i++) {
-        vhost_net_stop_one(get_vhost_net(ncs[i].peer), dev);
-    }
 }
 
 void vhost_net_cleanup(struct vhost_net *net)
