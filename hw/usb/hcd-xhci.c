8c244210d842b71e838c8fc5219b6ab3e4063d71
Gonglei <arei.gonglei@huawei.com>
2014-08-21 20:48:58 +0800

    xhci: fix debug print compiling error
    after commit 003e15a180373048f0c1f4df0bfe303746eb2676
    the DPRINTF will broke compiling, adjust its location.
    
    Signed-off-by: Gonglei <arei.gonglei@huawei.com>
    Signed-off-by: Gerd Hoffmann <kraxel@redhat.com>

--- hw/usb/hcd-xhci.c
+++ hw/usb/hcd-xhci.c
@@ -1380,14 +1380,11 @@ static void xhci_init_epctx(XHCIEPContext *epctx,
     dequeue = xhci_addr64(ctx[2] & ~0xf, ctx[3]);
 
     epctx->type = (ctx[1] >> EP_TYPE_SHIFT) & EP_TYPE_MASK;
-    DPRINTF("xhci: endpoint %d.%d type is %d\n", epid/2, epid%2, epctx->type);
     epctx->pctx = pctx;
     epctx->max_psize = ctx[1]>>16;
     epctx->max_psize *= 1+((ctx[1]>>8)&0xff);
     epctx->max_pstreams = (ctx[0] >> 10) & 0xf;
     epctx->lsa = (ctx[0] >> 15) & 1;
-    DPRINTF("xhci: endpoint %d.%d max transaction (burst) size is %d\n",
-            epid/2, epid%2, epctx->max_psize);
     if (epctx->max_pstreams) {
         xhci_alloc_streams(epctx, dequeue);
     } else {
@@ -1417,6 +1414,9 @@ static TRBCCode xhci_enable_ep(XHCIState *xhci, unsigned int slotid,
     epctx = xhci_alloc_epctx(xhci, slotid, epid);
     slot->eps[epid-1] = epctx;
     xhci_init_epctx(epctx, pctx, ctx);
+
+    DPRINTF("xhci: endpoint %d.%d type is %d, max transaction (burst) "
+            "size is %d\n", epid/2, epid%2, epctx->type, epctx->max_psize);
 
     epctx->mfindex_last = 0;
 
