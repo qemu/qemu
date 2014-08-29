f90e160b502fb5c464eb9417ac075a78f13e9801
Gerd Hoffmann <kraxel@redhat.com>
2014-08-29 12:40:55 +0200

    Revert "xhci: Fix number of streams allocated when using streams"
    This reverts commit d063c3112c4cd23a479ee18720c2bd119da2d315.
    
    "2 << x" is the same as "2 ^ (x + 1)", so the old code is correct.
    
    Signed-off-by: Gerd Hoffmann <kraxel@redhat.com>

--- hw/usb/hcd-xhci.c
+++ hw/usb/hcd-xhci.c
@@ -1151,7 +1151,7 @@ static void xhci_reset_streams(XHCIEPContext *epctx)
 static void xhci_alloc_streams(XHCIEPContext *epctx, dma_addr_t base)
 {
     assert(epctx->pstreams == NULL);
-    epctx->nr_pstreams = 2 << (epctx->max_pstreams + 1);
+    epctx->nr_pstreams = 2 << epctx->max_pstreams;
     epctx->pstreams = xhci_alloc_stream_contexts(epctx->nr_pstreams, base);
 }
 
