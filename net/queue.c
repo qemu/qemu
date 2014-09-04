d9c06c0d794a3729be7f9772b0ee654ddfc7f5a7
Michael S. Tsirkin <mst@redhat.com>
2014-09-04 11:39:10 +0300

    net: invoke callback when purging queue
    devices rely on packet callbacks eventually running,
    but we violate this rule whenever we purge the queue.
    To fix, invoke callbacks on all packets on purge.
    Set length to 0, this way callers can detect that
    this happened and re-queue if necessary.
    
    Cc: qemu-stable@nongnu.org
    Cc: Jason Wang <jasowang@redhat.com>
    Signed-off-by: Michael S. Tsirkin <mst@redhat.com>
    Acked-by: Jason Wang <jasowang@redhat.com>
    Signed-off-by: Stefan Hajnoczi <stefanha@redhat.com>
    (cherry picked from commit 07d8084624b3f5cbde7777849147a6a3a862e90a)
    Signed-off-by: Michael Roth <mdroth@linux.vnet.ibm.com>

--- net/queue.c
+++ net/queue.c
@@ -233,6 +233,9 @@ void qemu_net_queue_purge(NetQueue *queue, NetClientState *from)
         if (packet->sender == from) {
             QTAILQ_REMOVE(&queue->packets, packet, entry);
             queue->nq_count--;
+            if (packet->sent_cb) {
+                packet->sent_cb(packet->sender, 0);
+            }
             g_free(packet);
         }
     }
