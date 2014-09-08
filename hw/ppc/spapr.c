4aee73623d0141c5d4ce4754fc054689fb92f0e5
David Gibson <david@gibson.dropbear.id.au>
2014-09-08 15:30:31 +1000

    spapr: Cleanup machine naming conventions, and prepare for 2.2 release
    As of qemu-2.1, spapr/pseries, has a set of versioned machine classes to
    represent the machine type as it appeared to the guest in different qemu
    versions.  This allows for safe migration of guests between current and
    future qemu versions.
    
    However, these are organized a bit differently from those for PC: on PC,
    the default plain "pc" machine type is just an alias for the most recent
    versioned machine type.  In sPAPR, it names the base machine class from
    which the versioned types are derived.
    
    The PC approach is preferable; it makes it clearer which explicit version
    is the current one.  Additionally updating the "current" machine as the
    base class makes it even more likely than otherwise to incorrectly alter
    the versioned machines' behaviour when updating the current machine.
    
    Therefore this patch changes sPAPR to the PC approach - the base class
    becomes abstract, and plain "pseries" becomes an alias for the most
    recent versioned machine class.  Since qemu-2.1 is now released, we also
    create a new pseries-2.2 machine type, to incorporate changes during this
    development cycle (for now it is identical to pseries-2.1).
    
    Signed-off-by: David Gibson <david@gibson.dropbear.id.au>
    Signed-off-by: Alexander Graf <agraf@suse.de>

--- hw/ppc/spapr.c
+++ hw/ppc/spapr.c
@@ -1660,9 +1660,6 @@ static void spapr_machine_class_init(ObjectClass *oc, void *data)
     FWPathProviderClass *fwc = FW_PATH_PROVIDER_CLASS(oc);
     NMIClass *nc = NMI_CLASS(oc);
 
-    mc->name = "pseries";
-    mc->desc = "pSeries Logical Partition (PAPR compliant)";
-    mc->is_default = 1;
     mc->init = ppc_spapr_init;
     mc->reset = ppc_spapr_reset;
     mc->block_default_type = IF_SCSI;
@@ -1678,6 +1675,7 @@ static void spapr_machine_class_init(ObjectClass *oc, void *data)
 static const TypeInfo spapr_machine_info = {
     .name          = TYPE_SPAPR_MACHINE,
     .parent        = TYPE_MACHINE,
+    .abstract      = true,
     .instance_size = sizeof(sPAPRMachineState),
     .instance_init = spapr_machine_initfn,
     .class_init    = spapr_machine_class_init,
@@ -1698,7 +1696,6 @@ static void spapr_machine_2_1_class_init(ObjectClass *oc, void *data)
 
     mc->name = "pseries-2.1";
     mc->desc = "pSeries Logical Partition (PAPR compliant) v2.1";
-    mc->is_default = 0;
     mc->compat_props = compat_props;
 }
 
@@ -1708,10 +1705,27 @@ static const TypeInfo spapr_machine_2_1_info = {
     .class_init    = spapr_machine_2_1_class_init,
 };
 
+static void spapr_machine_2_2_class_init(ObjectClass *oc, void *data)
+{
+    MachineClass *mc = MACHINE_CLASS(oc);
+
+    mc->name = "pseries-2.2";
+    mc->desc = "pSeries Logical Partition (PAPR compliant) v2.2";
+    mc->alias = "pseries";
+    mc->is_default = 1;
+}
+
+static const TypeInfo spapr_machine_2_2_info = {
+    .name          = TYPE_SPAPR_MACHINE "2.2",
+    .parent        = TYPE_SPAPR_MACHINE,
+    .class_init    = spapr_machine_2_2_class_init,
+};
+
 static void spapr_machine_register_types(void)
 {
     type_register_static(&spapr_machine_info);
     type_register_static(&spapr_machine_2_1_info);
+    type_register_static(&spapr_machine_2_2_info);
 }
 
 type_init(spapr_machine_register_types)
