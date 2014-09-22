5a6e8ba64fb236068fef0f2ce26fe6579cf6a75c
Pavel Dovgalyuk <Pavel.Dovgaluk@ispras.ru>
2014-09-22 16:14:24 +0400

    kvmvapic: fix migration when VM paused and when not running Windows
    This patch fixes migration by extending do_vapic_enable function. This function
    called vapic_enable which read cpu number from the guest memory. When cpu
    number could not be read, vapic was not enabled while loading the VM state.
    This patch adds required code for cpu_number=0 to do_vapic_enable function,
    because it is called only when cpu_number=0.
    
    Signed-off-by: Pavel Dovgalyuk <Pavel.Dovgaluk@ispras.ru>
    Signed-off-by: Paolo Bonzini <pbonzini@redhat.com>

--- hw/i386/kvmvapic.c
+++ hw/i386/kvmvapic.c
@@ -732,7 +732,11 @@ static void do_vapic_enable(void *data)
     VAPICROMState *s = data;
     X86CPU *cpu = X86_CPU(first_cpu);
 
-    vapic_enable(s, cpu);
+    static const uint8_t enabled = 1;
+    cpu_physical_memory_write(s->vapic_paddr + offsetof(VAPICState, enabled),
+                              &enabled, sizeof(enabled));
+    apic_enable_vapic(cpu->apic_state, s->vapic_paddr);
+    s->state = VAPIC_ACTIVE;
 }
 
 static void kvmvapic_vm_state_change(void *opaque, int running,
@@ -777,7 +781,10 @@ static int vapic_post_load(void *opaque, int version_id)
         }
     }
 
-    s->vmsentry = qemu_add_vm_change_state_handler(kvmvapic_vm_state_change, s);
+    if (!s->vmsentry) {
+        s->vmsentry =
+            qemu_add_vm_change_state_handler(kvmvapic_vm_state_change, s);
+    }
     return 0;
 }
 
