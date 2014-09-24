eda470e41a753070e057380a9a71e2ad7347f667
Fam Zheng <famz@redhat.com>
2014-09-24 16:27:52 +0800

    scsi: Drop scsi_req_abort
    The only user of this function is spapr_vscsi.c. We can convert to
    scsi_req_cancel plus adding a check in vscsi_request_cancelled.
    
    Suggested-by: Paolo Bonzini <pbonzini@redhat.com>
    Signed-off-by: Fam Zheng <famz@redhat.com>
    [Drop prototype. - Paolo]
    Signed-off-by: Paolo Bonzini <pbonzini@redhat.com>

--- hw/scsi/scsi-bus.c
+++ hw/scsi/scsi-bus.c
@@ -1736,21 +1736,6 @@ void scsi_req_cancel(SCSIRequest *req)
     scsi_req_unref(req);
 }
 
-void scsi_req_abort(SCSIRequest *req, int status)
-{
-    if (!req->enqueued) {
-        return;
-    }
-    scsi_req_ref(req);
-    scsi_req_dequeue(req);
-    req->io_canceled = true;
-    if (req->ops->cancel_io) {
-        req->ops->cancel_io(req);
-    }
-    scsi_req_complete(req, status);
-    scsi_req_unref(req);
-}
-
 static int scsi_ua_precedence(SCSISense sense)
 {
     if (sense.key != UNIT_ATTENTION) {
--- include/hw/scsi/scsi.h
+++ include/hw/scsi/scsi.h
@@ -265,7 +265,6 @@ void scsi_req_data(SCSIRequest *req, int len);
 void scsi_req_complete(SCSIRequest *req, int status);
 uint8_t *scsi_req_get_buf(SCSIRequest *req);
 int scsi_req_get_sense(SCSIRequest *req, uint8_t *buf, int len);
-void scsi_req_abort(SCSIRequest *req, int status);
 void scsi_req_cancel(SCSIRequest *req);
 void scsi_req_retry(SCSIRequest *req);
 void scsi_device_purge_requests(SCSIDevice *sdev, SCSISense sense);
