af01492755b82ccaf0d15014477b34ba3ea643fc
Igor Mammedov <imammedo@redhat.com>
2014-09-26 09:28:36 +0000

    usb-bot: Drop not needed "allow_hotplug = 0"
    Drop useless hack that disables hotplug on bus by setting
    "allow_hotplug = 0". Even if bus is hotpluggable, It won't
    be possible to add another SCSI device to bus since its
    realization will fail early with error "no free target"
    in scsi_qdev_realize() method.
    
    Signed-off-by: Igor Mammedov <imammedo@redhat.com>
    Signed-off-by: Andreas Färber <afaerber@suse.de>

--- hw/usb/dev-storage.c
+++ hw/usb/dev-storage.c
@@ -653,7 +653,6 @@ static void usb_msd_realize_bot(USBDevice *dev, Error **errp)
     usb_desc_init(dev);
     scsi_bus_new(&s->bus, sizeof(s->bus), DEVICE(dev),
                  &usb_msd_scsi_info_bot, NULL);
-    s->bus.qbus.allow_hotplug = 0;
     usb_msd_handle_reset(dev);
 }
 
