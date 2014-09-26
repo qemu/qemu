45c46f20c62bd7be98f774f2740ec775654e2668
zhanghailiang <zhang.zhanghailiang@huawei.com>
2014-09-26 17:16:12 +0800

    pc-dimm: Don't check dimm->node when there is non-NUMA config
    It should not break memory hotplug feature if there is non-NUMA option.
    
    This patch would also allow to use pc-dimm as replacement for initial memory
    for non-NUMA configs.
    
    Note: After this patch, the memory hotplug can work normally for Linux guest OS
    when there is non-NUMA option and NUMA option. But not support Windows guest OS
    to hotplug memory with no-NUMA config, actully, it's Windows limitation.
    
    Reviewed-By: Igor Mammedov <imammedo@redhat.com>
    Signed-off-by: zhanghailiang <zhang.zhanghailiang@huawei.com>
    Acked-by: Michael S. Tsirkin <mst@redhat.com>
    Signed-off-by: Michael S. Tsirkin <mst@redhat.com>
    (cherry picked from commit fc50ff0666315be5120c70ad00cd0b0097484b84)
    Signed-off-by: Michael Roth <mdroth@linux.vnet.ibm.com>

--- hw/mem/pc-dimm.c
+++ hw/mem/pc-dimm.c
@@ -252,7 +252,7 @@ static void pc_dimm_realize(DeviceState *dev, Error **errp)
         error_setg(errp, "'" PC_DIMM_MEMDEV_PROP "' property is not set");
         return;
     }
-    if (dimm->node >= nb_numa_nodes) {
+    if ((nb_numa_nodes > 0) && (dimm->node >= nb_numa_nodes)) {
         error_setg(errp, "'DIMM property " PC_DIMM_NODE_PROP " has value %"
                    PRIu32 "' which exceeds the number of numa nodes: %d",
                    dimm->node, nb_numa_nodes);
