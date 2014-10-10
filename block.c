573742a5431a99ceaba6968ae269cee247727cce
Peter Maydell <peter.maydell@linaro.org>
2014-10-10 20:33:03 +0100

    block.c: Fix type of IoOperationType variable in send_qmp_error_event()
    The local variable 'ac' in send_qmp_error_event() is declared with the
    wrong type, which causes clang to complain when it is initialized
    and again when it is used:
    
    block.c:3655:20: warning: implicit conversion from enumeration type 'enum IoOperationType' to different enumeration type 'BlockErrorAction' (aka 'enum BlockErrorAction') [-Wenum-conversion]
        ac = is_read ? IO_OPERATION_TYPE_READ : IO_OPERATION_TYPE_WRITE;
           ~           ^~~~~~~~~~~~~~~~~~~~~~
    block.c:3655:45: warning: implicit conversion from enumeration type 'enum IoOperationType' to different enumeration type 'BlockErrorAction' (aka 'enum BlockErrorAction') [-Wenum-conversion]
        ac = is_read ? IO_OPERATION_TYPE_READ : IO_OPERATION_TYPE_WRITE;
           ~                                    ^~~~~~~~~~~~~~~~~~~~~~~
    block.c:3656:62: warning: implicit conversion from enumeration type 'BlockErrorAction' (aka 'enum BlockErrorAction') to different enumeration type 'IoOperationType' (aka 'enum IoOperationType') [-Wenum-conversion]
        qapi_event_send_block_io_error(bdrv_get_device_name(bs), ac, action,
        ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~                           ^~
    
    Correct the type to IoOperationType, and rename the variable
    to 'optype' to match its correct type.
    
    Signed-off-by: Peter Maydell <peter.maydell@linaro.org>
    Reviewed-by: Eric Blake <eblake@redhat.com>
    Reviewed-by: Luiz Capitulino <lcapitulino@redhat.com>
    Message-id: 1412969583-21045-1-git-send-email-peter.maydell@linaro.org
    Signed-off-by: Stefan Hajnoczi <stefanha@redhat.com>

--- block.c
+++ block.c
@@ -3544,10 +3544,10 @@ static void send_qmp_error_event(BlockDriverState *bs,
                                  BlockErrorAction action,
                                  bool is_read, int error)
 {
-    BlockErrorAction ac;
+    IoOperationType optype;
 
-    ac = is_read ? IO_OPERATION_TYPE_READ : IO_OPERATION_TYPE_WRITE;
-    qapi_event_send_block_io_error(bdrv_get_device_name(bs), ac, action,
+    optype = is_read ? IO_OPERATION_TYPE_READ : IO_OPERATION_TYPE_WRITE;
+    qapi_event_send_block_io_error(bdrv_get_device_name(bs), optype, action,
                                    bdrv_iostatus_is_enabled(bs),
                                    error == ENOSPC, strerror(error),
                                    &error_abort);
