69691e72708603592b1618f1a68d2a3f07db853d
Stefan Hajnoczi <stefanha@redhat.com>
2014-10-21 12:03:51 +0100

    blockdev: acquire AioContext in do_qmp_query_block_jobs_one()
    Make sure that query-block-jobs acquires the BlockDriverState
    AioContext so that the blockjob isn't running in another thread while we
    access its state.
    
    Signed-off-by: Stefan Hajnoczi <stefanha@redhat.com>
    Reviewed-by: Max Reitz <mreitz@redhat.com>
    Message-id: 1413889440-32577-3-git-send-email-stefanha@redhat.com

--- blockdev.c
+++ blockdev.c
@@ -2628,12 +2628,18 @@ BlockJobInfoList *qmp_query_block_jobs(Error **errp)
     BlockDriverState *bs;
 
     for (bs = bdrv_next(NULL); bs; bs = bdrv_next(bs)) {
+        AioContext *aio_context = bdrv_get_aio_context(bs);
+
+        aio_context_acquire(aio_context);
+
         if (bs->job) {
             BlockJobInfoList *elem = g_new0(BlockJobInfoList, 1);
             elem->value = block_job_query(bs->job);
             *p_next = elem;
             p_next = &elem->next;
         }
+
+        aio_context_release(aio_context);
     }
 
     return head;
