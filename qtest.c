107684c05d80c457aa6e81d090b36a1a294110ec
Li Liu <john.liuli@huawei.com>
2014-10-22 10:26:47 +0800

    qtest: fix qtest log fd should be initialized before qtest chardev
    qtest_log_fp should be inited before qemu_chr_add_handlers.
    If not the log dumped from callback functions may be lost.
    
    easy to reproduce it by command:
    "QTEST_LOG=1 QTEST_QEMU_BINARY=x86_64-softmmu/qemu-system-x86_64
    gtester -k --verbose -m=quick tests/qdev-monitor-test"
    
    The log "[I xxxxxx] OPENED" should be printed out by
    qtest_event, but does not.
    
    Signed-off-by: Li Liu <john.liuli@huawei.com>
    Reviewed-by: Stefan Hajnoczi <stefanha@redhat.com>
    Signed-off-by: Paolo Bonzini <pbonzini@redhat.com>

--- qtest.c
+++ qtest.c
@@ -545,11 +545,6 @@ void qtest_init(const char *qtest_chrdev, const char *qtest_log, Error **errp)
         return;
     }
 
-    qemu_chr_add_handlers(chr, qtest_can_read, qtest_read, qtest_event, chr);
-    qemu_chr_fe_set_echo(chr, true);
-
-    inbuf = g_string_new("");
-
     if (qtest_log) {
         if (strcmp(qtest_log, "none") != 0) {
             qtest_log_fp = fopen(qtest_log, "w+");
@@ -558,6 +553,10 @@ void qtest_init(const char *qtest_chrdev, const char *qtest_log, Error **errp)
         qtest_log_fp = stderr;
     }
 
+    qemu_chr_add_handlers(chr, qtest_can_read, qtest_read, qtest_event, chr);
+    qemu_chr_fe_set_echo(chr, true);
+
+    inbuf = g_string_new("");
     qtest_chr = chr;
 }
 
