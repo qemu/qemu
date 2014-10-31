f18a768efb990d9cd02911159de02e8ff5475239
SeokYeon Hwang <syeon.hwang@samsung.com>
2014-10-31 17:02:05 +0900

    vdi: wrapped uuid_unparse() in #ifdef
    Wrapped uuid_unparse() in #ifdef to avoid "-Wunused-function"
    on clang 3.4 or later.
    
    Signed-off-by: SeokYeon Hwang <syeon.hwang@samsung.com>
    Reviewed-by: Stefan Weil <sw@weilnetz.de>
    Signed-off-by: Michael Tokarev <mjt@tls.msk.ru>

--- block/vdi.c
+++ block/vdi.c
@@ -137,12 +137,14 @@ static inline int uuid_is_null(const uuid_t uu)
     return memcmp(uu, null_uuid, sizeof(uuid_t)) == 0;
 }
 
+# if defined(CONFIG_VDI_DEBUG)
 static inline void uuid_unparse(const uuid_t uu, char *out)
 {
     snprintf(out, 37, UUID_FMT,
             uu[0], uu[1], uu[2], uu[3], uu[4], uu[5], uu[6], uu[7],
             uu[8], uu[9], uu[10], uu[11], uu[12], uu[13], uu[14], uu[15]);
 }
+# endif
 #endif
 
 typedef struct {
