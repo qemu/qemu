2b09f94cdbf5c54e2278d7f3aed2eceff3494790
Maciej W. Rozycki <macro@codesourcery.com>
2014-11-05 15:35:59 +0000

    target-mips: Make `helper_float_cvtw_s' consistent with the remaining helpers
    Move the call to `update_fcr31' in `helper_float_cvtw_s' after the
    exception flag check, for consistency with the remaining helpers that do
    it last too.
    
    Signed-off-by: Maciej W. Rozycki <macro@codesourcery.com>
    Signed-off-by: Leon Alrae <leon.alrae@imgtec.com>

--- target-mips/op_helper.c
+++ target-mips/op_helper.c
@@ -2659,11 +2659,11 @@ uint32_t helper_float_cvtw_s(CPUMIPSState *env, uint32_t fst0)
     uint32_t wt2;
 
     wt2 = float32_to_int32(fst0, &env->active_fpu.fp_status);
-    update_fcr31(env, GETPC());
     if (get_float_exception_flags(&env->active_fpu.fp_status)
         & (float_flag_invalid | float_flag_overflow)) {
         wt2 = FP_TO_INT32_OVERFLOW;
     }
+    update_fcr31(env, GETPC());
     return wt2;
 }
 
