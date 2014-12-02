9f02cfc84b85929947b32fe1674fbc6a429f332a
Stefan Hajnoczi <stefanha@redhat.com>
2014-12-02 11:23:14 +0000

    bitmap: add atomic set functions
    Use atomic_or() for atomic bitmaps where several threads may set bits at
    the same time.  This avoids the race condition between threads loading
    an element, bitwise ORing, and then storing the element.
    
    When setting all bits in a word we can avoid atomic ops and instead just
    use an smp_mb() at the end.
    
    Most bitmap users don't need atomicity so introduce new functions.
    
    Signed-off-by: Stefan Hajnoczi <stefanha@redhat.com>
    Message-Id: <1417519399-3166-2-git-send-email-stefanha@redhat.com>
    [Avoid barrier in the single word case, use full barrier instead of write.
     - Paolo]
    Reviewed-by: Fam Zheng <famz@redhat.com>
    Signed-off-by: Paolo Bonzini <pbonzini@redhat.com>

--- include/qemu/bitmap.h
+++ include/qemu/bitmap.h
@@ -39,6 +39,7 @@
  * bitmap_empty(src, nbits)			Are all bits zero in *src?
  * bitmap_full(src, nbits)			Are all bits set in *src?
  * bitmap_set(dst, pos, nbits)			Set specified bit area
+ * bitmap_set_atomic(dst, pos, nbits)   Set specified bit area with atomic ops
  * bitmap_clear(dst, pos, nbits)		Clear specified bit area
  * bitmap_find_next_zero_area(buf, len, pos, n, mask)	Find bit free area
  */
@@ -226,6 +227,7 @@ static inline int bitmap_intersects(const unsigned long *src1,
 }
 
 void bitmap_set(unsigned long *map, long i, long len);
+void bitmap_set_atomic(unsigned long *map, long i, long len);
 void bitmap_clear(unsigned long *map, long start, long nr);
 unsigned long bitmap_find_next_zero_area(unsigned long *map,
                                          unsigned long size,
