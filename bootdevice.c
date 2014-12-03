3b08098b409c0fb28f85436ba1adeb1d401ec8f7
Gonglei <arei.gonglei@huawei.com>
2014-12-03 18:25:46 +0000

    bootdevice: add validate check for qemu_boot_set()
    Signed-off-by: Gonglei <arei.gonglei@huawei.com>
    Reviewed-by: Markus Armbruster <armbru@redhat.com>

--- bootdevice.c
+++ bootdevice.c
@@ -49,9 +49,17 @@ void qemu_register_boot_set(QEMUBootSetHandler *func, void *opaque)
 
 void qemu_boot_set(const char *boot_order, Error **errp)
 {
+    Error *local_err = NULL;
+
     if (!boot_set_handler) {
         error_setg(errp, "no function defined to set boot device list for"
                          " this architecture");
+        return;
+    }
+
+    validate_bootdevices(boot_order, &local_err);
+    if (local_err) {
+        error_propagate(errp, local_err);
         return;
     }
 
