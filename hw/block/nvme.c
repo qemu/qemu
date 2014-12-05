e7026f1953c16d1cc3e8310cc27db6acf61d9def
Alex Friedman <alex@e8storage.com>
2014-12-05 14:40:24 +0200

    nvme: Fix get/set number of queues feature
    According to the specification, the low 16 bits should contain the number of
    I/O submission queues, and the high 16 bits should contain the number of
    I/O completion queues.
    
    Signed-off-by: Alex Friedman <alex@e8storage.com>
    Acked-by: Keith Busch <keith.busch@intel.com>
    Signed-off-by: Stefan Hajnoczi <stefanha@redhat.com>

--- hw/block/nvme.c
+++ hw/block/nvme.c
@@ -476,7 +476,8 @@ static uint16_t nvme_get_feature(NvmeCtrl *n, NvmeCmd *cmd, NvmeRequest *req)
 
     switch (dw10) {
     case NVME_NUMBER_OF_QUEUES:
-        req->cqe.result = cpu_to_le32(n->num_queues);
+        req->cqe.result =
+            cpu_to_le32((n->num_queues - 1) | ((n->num_queues - 1) << 16));
         break;
     default:
         return NVME_INVALID_FIELD | NVME_DNR;
@@ -490,7 +491,8 @@ static uint16_t nvme_set_feature(NvmeCtrl *n, NvmeCmd *cmd, NvmeRequest *req)
 
     switch (dw10) {
     case NVME_NUMBER_OF_QUEUES:
-        req->cqe.result = cpu_to_le32(n->num_queues);
+        req->cqe.result =
+            cpu_to_le32((n->num_queues - 1) | ((n->num_queues - 1) << 16));
         break;
     default:
         return NVME_INVALID_FIELD | NVME_DNR;
