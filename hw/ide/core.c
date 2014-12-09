819fa276311ce328a8e38ad9306c1093961b3f4b
Dr. David Alan Gilbert <dgilbert@redhat.com>
2014-12-09 18:15:08 +0000

    Restore atapi_dma flag across migration
    If a migration happens just after the guest has kicked
    off an ATAPI command and kicked off DMA, we lose the atapi_dma
    flag, and the destination tries to complete the command as PIO
    rather than DMA.  This upsets Linux; modern libata based kernels
    stumble and recover OK, older kernels end up passing bad data
    to userspace.
    
    Signed-off-by: Dr. David Alan Gilbert <dgilbert@redhat.com>
    Reviewed-by: John Snow <jsnow@redhat.com>
    Signed-off-by: Kevin Wolf <kwolf@redhat.com>

--- hw/ide/core.c
+++ hw/ide/core.c
@@ -2417,6 +2417,7 @@ static int ide_drive_pio_post_load(void *opaque, int version_id)
     s->end_transfer_func = transfer_end_table[s->end_transfer_fn_idx];
     s->data_ptr = s->io_buffer + s->cur_io_buffer_offset;
     s->data_end = s->data_ptr + s->cur_io_buffer_len;
+    s->atapi_dma = s->feature & 1; /* as per cmd_packet */
 
     return 0;
 }
