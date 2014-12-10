9a502563eef7d7c2c9120b237059426e229eefe9
John Snow <jsnow@redhat.com>
2014-12-10 13:17:07 -0500

    ide: Implement VPD response for ATAPI
    SCSI devices have multiple kinds of queries they need to respond
    to, as defined in the "cmd inquiry" section in MMC-6 and SPC-3.
    
    Relevent sections:
    MMC-6 revision 2g:
          Non-VPD response data and pointer to SPC-3;
          Section 6.8 "Inquiry Command"
    SPC-3 revision 23:
          Inquiry command and error handling:
          Section 6.4 "INQUIRY command"
          VPD data pages format:
          Section 7.6 "Vital product data parameters"
    
    We implement these Vital Product Data queries for SCSI, but not for
    ATAPI through IDE. The result is that if you are looking for the WWN
    identifier via tools such as sg3_utils, you will be unable to query
    our CD/DVD rom device to obtain it.
    
    This patch adds the minimum number of mandatory responses as defined
    by SPC-3, which include the "supported pages" response (page 0x00)
    and the "Device Identification" response (page 0x83). It also correctly
    responds when it receives a request for an illegal page to improve
    error output from related tools.
    
    The Device ID page contains an arbitrary list of identification
    strings of various formats; the ID strings included in this patch
    were chosen to mimic those provided by the libata driver when
    emulating this SCSI query (model, serial, and wwn when present.)
    
    Example:
    
    # libata emulated response
    [root@localhost ~]# sg_inq --id /dev/sda
    VPD INQUIRY: Device Identification page
      Designation descriptor number 1, descriptor length: 24
        designator_type: vendor specific [0x0],  code_set: ASCII
        associated with the addressed logical unit
          vendor specific: QM00001
      Designation descriptor number 2, descriptor length: 72
        designator_type: T10 vendor identification,  code_set: ASCII
        associated with the addressed logical unit
          vendor id: ATA
          vendor specific: QEMU HARDDISK                           QM00001
    
    # QEMU generated ATAPI response, with WWN
    [root@localhost ~]# sg_inq --id /dev/sr0
    VPD INQUIRY: Device Identification page
      Designation descriptor number 1, descriptor length: 24
        designator_type: vendor specific [0x0],  code_set: ASCII
        associated with the addressed logical unit
          vendor specific: QM00005
      Designation descriptor number 2, descriptor length: 72
        designator_type: T10 vendor identification,  code_set: ASCII
        associated with the addressed logical unit
          vendor id: ATA
          vendor specific: QEMU DVD-ROM                            QM00005
      Designation descriptor number 3, descriptor length: 12
        designator_type: NAA,  code_set: Binary
        associated with the addressed logical unit
          NAA 5, IEEE Company_id: 0xc50
          Vendor Specific Identifier: 0x15ea71bb
          [0x5000c50015ea71bb]
    
    See also: hw/scsi/scsi-disk.c, scsi_disk_emulate_inquiry()
    
    Signed-off-by: John Snow <jsnow@redhat.com>
    Signed-off-by: Stefan Hajnoczi <stefanha@redhat.com>

--- hw/ide/atapi.c
+++ hw/ide/atapi.c
@@ -621,20 +621,107 @@ static void cmd_request_sense(IDEState *s, uint8_t *buf)
 
 static void cmd_inquiry(IDEState *s, uint8_t *buf)
 {
+    uint8_t page_code = buf[2];
     int max_len = buf[4];
 
-    buf[0] = 0x05; /* CD-ROM */
-    buf[1] = 0x80; /* removable */
-    buf[2] = 0x00; /* ISO */
-    buf[3] = 0x21; /* ATAPI-2 (XXX: put ATAPI-4 ?) */
-    buf[4] = 31; /* additional length */
-    buf[5] = 0; /* reserved */
-    buf[6] = 0; /* reserved */
-    buf[7] = 0; /* reserved */
-    padstr8(buf + 8, 8, "QEMU");
-    padstr8(buf + 16, 16, "QEMU DVD-ROM");
-    padstr8(buf + 32, 4, s->version);
-    ide_atapi_cmd_reply(s, 36, max_len);
+    unsigned idx = 0;
+    unsigned size_idx;
+    unsigned preamble_len;
+
+    /* If the EVPD (Enable Vital Product Data) bit is set in byte 1,
+     * we are being asked for a specific page of info indicated by byte 2. */
+    if (buf[1] & 0x01) {
+        preamble_len = 4;
+        size_idx = 3;
+
+        buf[idx++] = 0x05;      /* CD-ROM */
+        buf[idx++] = page_code; /* Page Code */
+        buf[idx++] = 0x00;      /* reserved */
+        idx++;                  /* length (set later) */
+
+        switch (page_code) {
+        case 0x00:
+            /* Supported Pages: List of supported VPD responses. */
+            buf[idx++] = 0x00; /* 0x00: Supported Pages, and: */
+            buf[idx++] = 0x83; /* 0x83: Device Identification. */
+            break;
+
+        case 0x83:
+            /* Device Identification. Each entry is optional, but the entries
+             * included here are modeled after libata's VPD responses.
+             * If the response is given, at least one entry must be present. */
+
+            /* Entry 1: Serial */
+            if (idx + 24 > max_len) {
+                /* Not enough room for even the first entry: */
+                /* 4 byte header + 20 byte string */
+                ide_atapi_cmd_error(s, ILLEGAL_REQUEST,
+                                    ASC_DATA_PHASE_ERROR);
+                return;
+            }
+            buf[idx++] = 0x02; /* Ascii */
+            buf[idx++] = 0x00; /* Vendor Specific */
+            buf[idx++] = 0x00;
+            buf[idx++] = 20;   /* Remaining length */
+            padstr8(buf + idx, 20, s->drive_serial_str);
+            idx += 20;
+
+            /* Entry 2: Drive Model and Serial */
+            if (idx + 72 > max_len) {
+                /* 4 (header) + 8 (vendor) + 60 (model & serial) */
+                goto out;
+            }
+            buf[idx++] = 0x02; /* Ascii */
+            buf[idx++] = 0x01; /* T10 Vendor */
+            buf[idx++] = 0x00;
+            buf[idx++] = 68;
+            padstr8(buf + idx, 8, "ATA"); /* Generic T10 vendor */
+            idx += 8;
+            padstr8(buf + idx, 40, s->drive_model_str);
+            idx += 40;
+            padstr8(buf + idx, 20, s->drive_serial_str);
+            idx += 20;
+
+            /* Entry 3: WWN */
+            if (s->wwn && (idx + 12 <= max_len)) {
+                /* 4 byte header + 8 byte wwn */
+                buf[idx++] = 0x01; /* Binary */
+                buf[idx++] = 0x03; /* NAA */
+                buf[idx++] = 0x00;
+                buf[idx++] = 0x08;
+                stq_be_p(&buf[idx], s->wwn);
+                idx += 8;
+            }
+            break;
+
+        default:
+            /* SPC-3, revision 23 sec. 6.4 */
+            ide_atapi_cmd_error(s, ILLEGAL_REQUEST,
+                                ASC_INV_FIELD_IN_CMD_PACKET);
+            return;
+        }
+    } else {
+        preamble_len = 5;
+        size_idx = 4;
+
+        buf[0] = 0x05; /* CD-ROM */
+        buf[1] = 0x80; /* removable */
+        buf[2] = 0x00; /* ISO */
+        buf[3] = 0x21; /* ATAPI-2 (XXX: put ATAPI-4 ?) */
+        /* buf[size_idx] set below. */
+        buf[5] = 0;    /* reserved */
+        buf[6] = 0;    /* reserved */
+        buf[7] = 0;    /* reserved */
+        padstr8(buf + 8, 8, "QEMU");
+        padstr8(buf + 16, 16, "QEMU DVD-ROM");
+        padstr8(buf + 32, 4, s->version);
+        idx = 36;
+    }
+
+ out:
+    buf[size_idx] = idx - preamble_len;
+    ide_atapi_cmd_reply(s, idx, max_len);
+    return;
 }
 
 static void cmd_get_configuration(IDEState *s, uint8_t *buf)
