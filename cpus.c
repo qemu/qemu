1979b908b6e99cc24c1c0746060422f8778aa9d9
Pavel Dovgalyuk <Pavel.Dovgaluk@ispras.ru>
2015-01-12 15:00:43 +0300

    cpus: consistently use QEMU_CLOCK_VIRTUAL_RT for icount_warp_rt timer
    Fix mismatch between timer_new_ms and timer_mod.
    
    Signed-off-by: Pavel Dovgalyuk <Pavel.Dovgaluk@ispras.ru>
    Signed-off-by: Paolo Bonzini <pbonzini@redhat.com>

--- cpus.c
+++ cpus.c
@@ -324,7 +324,7 @@ static void icount_adjust(void)
 static void icount_adjust_rt(void *opaque)
 {
     timer_mod(icount_rt_timer,
-                   qemu_clock_get_ms(QEMU_CLOCK_REALTIME) + 1000);
+              qemu_clock_get_ms(QEMU_CLOCK_VIRTUAL_RT) + 1000);
     icount_adjust();
 }
 
