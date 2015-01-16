5b324bbafc4fe367bd9c5bfa6cff071081fb8b0e
Frank Blaschka <blaschka@linux.vnet.ibm.com>
2015-01-16 14:55:21 +0100

    s390x/pci: check for invalid function handle
    broken guest may provide 0 (invalid) function handle to zpci
    instructions. Since we use function handle 0 to indicate an empty
    slot in the PHB we have to add an additional check to spot this
    kind of error.
    
    Signed-off-by: Frank Blaschka <blaschka@linux.vnet.ibm.com>
    Signed-off-by: Cornelia Huck <cornelia.huck@de.ibm.com>

--- hw/s390x/s390-pci-bus.c
+++ hw/s390x/s390-pci-bus.c
@@ -170,7 +170,7 @@ S390PCIBusDevice *s390_pci_find_dev_by_fh(uint32_t fh)
     S390pciState *s = S390_PCI_HOST_BRIDGE(
         object_resolve_path(TYPE_S390_PCI_HOST_BRIDGE, NULL));
 
-    if (!s) {
+    if (!s || !fh) {
         return NULL;
     }
 
