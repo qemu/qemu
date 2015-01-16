d98bc0b654b97d130338e76e0928296f84e6d6fd
Gerd Hoffmann <kraxel@redhat.com>
2015-01-16 13:59:17 +0100

    opengl: add shader build infrastructure
    perl script to transform shader programs into c include files with
    static string constands containing the shader programs, so we can
    easily embed them into qemu.  Also some Makefile logic for them.
    
    Signed-off-by: Gerd Hoffmann <kraxel@redhat.com>
    Reviewed-by: Max Reitz <mreitz@redhat.com>

---
+++ scripts/shaderinclude.pl
@@ -0,0 +1,16 @@
+#!/usr/bin/perl
+use strict;
+use warnings;
+
+my $file = shift;
+open FILE, "<", $file or die "open $file: $!";
+my $name = $file;
+$name =~ s|.*/||;
+$name =~ s/[-.]/_/g;
+print "static GLchar ${name}_src[] =\n";
+while (<FILE>) {
+    chomp;
+    printf "    \"%s\\n\"\n", $_;
+}
+print "    \"\\n\";\n";
+close FILE;
