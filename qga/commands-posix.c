ef82b60be13b18198b84a2157f59c50fd53f5408
zhanghailiang <zhang.zhanghailiang@huawei.com>
2015-01-22 10:40:05 +0800

    qga: implement qmp_guest_get_memory_block_info() for Linux with sysfs
    This conveys general information about guest memory blocks. Currently,
    just the memory block size.
    
    The size of a memory block is architecture dependent, it represents the logical
    unit upon which memory online/offline operations are to be performed.
    
    Signed-off-by: zhanghailiang <zhang.zhanghailiang@huawei.com>
    
    *generalized guest-get-memory-block-size to get-get-memory-block-info
     for future extensibility
    
    Signed-off-by: Michael Roth <mdroth@linux.vnet.ibm.com>

--- qga/commands-posix.c
+++ qga/commands-posix.c
@@ -2266,8 +2266,35 @@ err:
 
 GuestMemoryBlockInfo *qmp_guest_get_memory_block_info(Error **errp)
 {
-    error_set(errp, QERR_UNSUPPORTED);
-    return NULL;
+    Error *local_err = NULL;
+    char *dirpath;
+    int dirfd;
+    char *buf;
+    GuestMemoryBlockInfo *info;
+
+    dirpath = g_strdup_printf("/sys/devices/system/memory/");
+    dirfd = open(dirpath, O_RDONLY | O_DIRECTORY);
+    if (dirfd == -1) {
+        error_setg_errno(errp, errno, "open(\"%s\")", dirpath);
+        g_free(dirpath);
+        return NULL;
+    }
+    g_free(dirpath);
+
+    buf = g_malloc0(20);
+    ga_read_sysfs_file(dirfd, "block_size_bytes", buf, 20, &local_err);
+    if (local_err) {
+        g_free(buf);
+        error_propagate(errp, local_err);
+        return NULL;
+    }
+
+    info = g_new0(GuestMemoryBlockInfo, 1);
+    info->size = strtol(buf, NULL, 16); /* the unit is bytes */
+
+    g_free(buf);
+
+    return info;
 }
 
 #else /* defined(__linux__) */
