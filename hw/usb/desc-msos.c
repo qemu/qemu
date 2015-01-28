0c6f807f4a98e7e258765dcf22619a582995fce0
Markus Armbruster <armbru@redhat.com>
2015-01-28 15:54:04 +0100

    usb: Pair g_malloc() with g_free(), not free()
    Spotted by Coverity with preview checker ALLOC_FREE_MISMATCH enabled
    and my "coverity: Model g_free() isn't necessarily free()" model patch
    applied.
    
    Signed-off-by: Markus Armbruster <armbru@redhat.com>
    Reviewed-by: Eric Blake <eblake@redhat.com>
    Signed-off-by: Michael Tokarev <mjt@tls.msk.ru>

--- hw/usb/desc-msos.c
+++ hw/usb/desc-msos.c
@@ -231,7 +231,7 @@ int usb_desc_msos(const USBDesc *desc,  USBPacket *p,
         length = len;
     }
     memcpy(dest, buf, length);
-    free(buf);
+    g_free(buf);
 
     p->actual_length = length;
     return 0;
