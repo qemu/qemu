a44145e4294eb3a377d0e748c59f8b0c82d18895
zhanghailiang <zhang.zhanghailiang@huawei.com>
2015-02-09 17:26:21 +0000

    migration: Introduce capability 'x-colo' to migration
    We add helper function colo_supported() to indicate whether
    colo is supported or not, with which we use to control whether or not
    showing 'x-colo' string to users, they can use qmp command
    'query-migrate-capabilities' or hmp command 'info migrate_capabilities'
    to learn if colo is supported.
    
    Cc: Juan Quintela <quintela@redhat.com>
    Cc: Amit Shah <amit.shah@redhat.com>
    Cc: Eric Blake <eblake@redhat.com>
    Cc: Markus Armbruster <armbru@redhat.com>
    Signed-off-by: zhanghailiang <zhang.zhanghailiang@huawei.com>
    Signed-off-by: Li Zhijian <lizhijian@cn.fujitsu.com>
    Signed-off-by: Gonglei <arei.gonglei@huawei.com>
    Reviewed-by: Eric Blake <eblake@redhat.com>

---
+++ stubs/migration-colo.c
@@ -0,0 +1,18 @@
+/*
+ * COarse-grain LOck-stepping Virtual Machines for Non-stop Service (COLO)
+ * (a.k.a. Fault Tolerance or Continuous Replication)
+ *
+ * Copyright (c) 2015 HUAWEI TECHNOLOGIES CO., LTD.
+ * Copyright (c) 2015 FUJITSU LIMITED
+ * Copyright (c) 2015 Intel Corporation
+ *
+ * This work is licensed under the terms of the GNU GPL, version 2 or
+ * later.  See the COPYING file in the top-level directory.
+ */
+
+#include "migration/colo.h"
+
+bool colo_supported(void)
+{
+    return false;
+}
