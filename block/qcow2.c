0709c5a1530b046183b6e96d9631affcff76c1fc
Max Reitz <mreitz@redhat.com>
2015-02-10 15:28:44 -0500

    qcow2: Add refcount_bits to format-specific info
    Add the bit width of every refcount entry to the format-specific
    information.
    
    In contrast to lazy_refcounts and the corrupt flag, this should be
    always emitted, even for compat=0.10 although it does not support any
    refcount width other than 16 bits. This is because if a boolean is
    optional, one normally assumes it to be false when omitted; but if an
    integer is not specified, it is rather difficult to guess its value.
    
    This new field breaks some test outputs, fix them.
    
    Signed-off-by: Max Reitz <mreitz@redhat.com>
    Reviewed-by: Eric Blake <eblake@redhat.com>
    Reviewed-by: Stefan Hajnoczi <stefanha@redhat.com>
    Signed-off-by: Kevin Wolf <kwolf@redhat.com>

--- block/qcow2.c
+++ block/qcow2.c
@@ -2482,7 +2482,8 @@ static ImageInfoSpecific *qcow2_get_specific_info(BlockDriverState *bs)
     };
     if (s->qcow_version == 2) {
         *spec_info->qcow2 = (ImageInfoSpecificQCow2){
-            .compat = g_strdup("0.10"),
+            .compat             = g_strdup("0.10"),
+            .refcount_bits      = s->refcount_bits,
         };
     } else if (s->qcow_version == 3) {
         *spec_info->qcow2 = (ImageInfoSpecificQCow2){
@@ -2493,6 +2494,7 @@ static ImageInfoSpecific *qcow2_get_specific_info(BlockDriverState *bs)
             .corrupt            = s->incompatible_features &
                                   QCOW2_INCOMPAT_CORRUPT,
             .has_corrupt        = true,
+            .refcount_bits      = s->refcount_bits,
         };
     }
 
