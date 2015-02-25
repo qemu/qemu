c65476612aa4842785939fa3fbea2123cc980c75
Gonglei <arei.gonglei@huawei.com>
2015-02-25 12:22:37 +0800

    vhost: Remove superfluous '\n' around error_report()
    Signed-off-by: Gonglei <arei.gonglei@huawei.com>
    Reviewed-by: Markus Armbruster <armbru@redhat.com>
    Signed-off-by: Michael Tokarev <mjt@tls.msk.ru>

--- net/vhost-user.c
+++ net/vhost-user.c
@@ -121,12 +121,12 @@ static void net_vhost_user_event(void *opaque, int event)
     case CHR_EVENT_OPENED:
         vhost_user_start(s);
         net_vhost_link_down(s, false);
-        error_report("chardev \"%s\" went up\n", s->chr->label);
+        error_report("chardev \"%s\" went up", s->chr->label);
         break;
     case CHR_EVENT_CLOSED:
         net_vhost_link_down(s, true);
         vhost_user_stop(s);
-        error_report("chardev \"%s\" went down\n", s->chr->label);
+        error_report("chardev \"%s\" went down", s->chr->label);
         break;
     }
 }
