6a50636f35ba677c747f2f6127b0dba994b039ca
Markus Armbruster <armbru@redhat.com>
2015-03-06 19:49:41 +0100

    monitor: Drop do_qmp_capabilities()'s superfluous QMP check
    Superfluous since commit 30f5041 removed it from HMP.
    
    Signed-off-by: Markus Armbruster <armbru@redhat.com>
    Reviewed-by: Eric Blake <eblake@redhat.com>
    Reviewed-by: Luiz Capitulino <lcapitulino@redhat.com>

--- monitor.c
+++ monitor.c
@@ -566,11 +566,7 @@ static void monitor_qapi_event_init(void)
 static int do_qmp_capabilities(Monitor *mon, const QDict *params,
                                QObject **ret_data)
 {
-    /* Will setup QMP capabilities in the future */
-    if (monitor_ctrl_mode(mon)) {
-        mon->qmp.command_mode = 1;
-    }
-
+    mon->qmp.command_mode = 1;
     return 0;
 }
 
