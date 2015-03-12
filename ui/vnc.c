81607cbfa433272d1f09bd0f0ae6c3b14f818972
Gonglei <arei.gonglei@huawei.com>
2015-03-12 15:33:45 +0800

    vnc: fix segmentation fault when invalid vnc parameters are specified
    Reproducer:
     #./qemu-system-x86_64 -vnc :0,ip
    qemu-system-x86_64: -vnc :1,ip: Invalid parameter 'ip'
    Segmentation fault (core dumped)
    
    Signed-off-by: Gonglei <arei.gonglei@huawei.com>
    Signed-off-by: Gerd Hoffmann <kraxel@redhat.com>

--- ui/vnc.c
+++ ui/vnc.c
@@ -3703,8 +3703,13 @@ QemuOpts *vnc_parse_func(const char *str)
 {
     QemuOptsList *olist = qemu_find_opts("vnc");
     QemuOpts *opts = qemu_opts_parse(olist, str, 1);
-    const char *id = qemu_opts_id(opts);
+    const char *id;
 
+    if (!opts) {
+        return NULL;
+    }
+
+    id = qemu_opts_id(opts);
     if (!id) {
         /* auto-assign id if not present */
         vnc_auto_assign_id(olist, opts);
