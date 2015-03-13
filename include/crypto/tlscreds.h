a090187de116a3d0b8146ca481249c8fc83ad3ee
Daniel P. Berrange <berrange@redhat.com>
2015-03-13 17:39:26 +0000

    crypto: introduce new base module for TLS credentials
    Introduce a QCryptoTLSCreds class to act as the base class for
    storing TLS credentials. This will be later subclassed to provide
    handling of anonymous and x509 credential types. The subclasses
    will be user creatable objects, so instances can be created &
    deleted via 'object-add' and 'object-del' QMP commands respectively,
    or via the -object command line arg.
    
    If the credentials cannot be initialized an error will be reported
    as a QMP reply, or on stderr respectively.
    
    The idea is to make it possible to represent and manage TLS
    credentials independently of the network service that is using
    them. This will enable multiple services to use the same set of
    credentials and minimize code duplication. A later patch will
    convert the current VNC server TLS code over to use this object.
    
    The representation of credentials will be functionally equivalent
    to that currently implemented in the VNC server with one exception.
    The new code has the ability to (optionally) load a pre-generated
    set of diffie-hellman parameters, if the file dh-params.pem exists,
    whereas the current VNC server will always generate them on startup.
    This is beneficial for admins who wish to avoid the (small) time
    sink of generating DH parameters at startup and/or avoid depleting
    entropy.
    
    Signed-off-by: Daniel P. Berrange <berrange@redhat.com>

---
+++ include/crypto/tlscreds.h
@@ -0,0 +1,68 @@
+/*
+ * QEMU crypto TLS credential support
+ *
+ * Copyright (c) 2015 Red Hat, Inc.
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+
+#ifndef QCRYPTO_TLSCRED_H__
+#define QCRYPTO_TLSCRED_H__
+
+#include "qemu-common.h"
+#include "qapi/error.h"
+#include "qom/object.h"
+
+#ifdef CONFIG_GNUTLS
+#include <gnutls/gnutls.h>
+#endif
+
+#define TYPE_QCRYPTO_TLS_CREDS "tls-creds"
+#define QCRYPTO_TLS_CREDS(obj)                  \
+    OBJECT_CHECK(QCryptoTLSCreds, (obj), TYPE_QCRYPTO_TLS_CREDS)
+
+typedef struct QCryptoTLSCreds QCryptoTLSCreds;
+typedef struct QCryptoTLSCredsClass QCryptoTLSCredsClass;
+
+#define QCRYPTO_TLS_CREDS_DH_PARAMS "dh-params.pem"
+
+
+/**
+ * QCryptoTLSCreds:
+ *
+ * The QCryptoTLSCreds object is an abstract base for different
+ * types of TLS handshake credentials. Most commonly the
+ * QCryptoTLSCredsX509 subclass will be used to provide x509
+ * certificate credentials.
+ */
+
+struct QCryptoTLSCreds {
+    Object parent_obj;
+    char *dir;
+    QCryptoTLSCredsEndpoint endpoint;
+#ifdef CONFIG_GNUTLS
+    gnutls_dh_params_t dh_params;
+#endif
+    bool verifyPeer;
+};
+
+
+struct QCryptoTLSCredsClass {
+    ObjectClass parent_class;
+};
+
+
+#endif /* QCRYPTO_TLSCRED_H__ */
+
