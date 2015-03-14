dc488f888060afdc129e0cc8812cf50c4c083423
Scott Feldman <sfeldma@gmail.com>
2015-03-13 21:09:30 -0700

    rocker: add new rocker switch device
    Rocker is a simulated ethernet switch device.  The device supports up to 62
    front-panel ports and supports L2 switching and L3 routing functions, as well
    as L2/L3/L4 ACLs.  The device presents a single PCI device for each switch,
    with a memory-mapped register space for device driver access.
    
    Rocker device is invoked with -device, for example a 4-port switch:
    
      -device rocker,name=sw1,len-ports=4,ports[0]=dev0,ports[1]=dev1, \
             ports[2]=dev2,ports[3]=dev3
    
    Each port is a netdev and can be paired with using -netdev id=<port name>.
    
    Signed-off-by: Scott Feldman <sfeldma@gmail.com>
    Signed-off-by: Jiri Pirko <jiri@resnulli.us>
    Acked-by: Scott Feldman <sfeldma@gmail.com>
    Acked-by: Jiri Pirko <jiri@resnulli.us>
    Signed-off-by: David Ahern <dsahern@gmail.com>
    Message-id: 1426306173-24884-7-git-send-email-sfeldma@gmail.com
    
    rocker: fix clang compiler errors
    
    Consolidate all forward typedef declarations to rocker.h.
    
    Signed-off-by: David Ahern <dsahern@gmail.com>
    Acked-by: Scott Feldman <sfeldma@gmail.com>
    Acked-by: Jiri Pirko <jiri@resnulli.us>
    
    rocker: add support for flow modification
    
    We had support for flow add/del.  This adds support for flow mod.  I needed
    this for L3 support where an existing route is modified using NLM_F_REPLACE.
    For example:
    
      ip route add 12.0.0.0/30 nexthop via 11.0.0.1 dev swp1
      ip route change 12.0.0.0/30 nexthop via 11.0.0.9 dev swp2
    
    The first cmd adds the route.  The second cmd changes the existing route by
    changing its nexthop info.
    
    In the device, a mod operation results in the matching flow enty being modified
    with the new settings.  This is atomic to the device.
    
    Signed-off-by: Scott Feldman <sfeldma@gmail.com>
    Signed-off-by: Stefan Hajnoczi <stefanha@redhat.com>

---
+++ hw/net/rocker/rocker_of_dpa.h
@@ -0,0 +1,22 @@
+/*
+ * QEMU rocker switch emulation - OF-DPA flow processing support
+ *
+ * Copyright (c) 2014 Scott Feldman <sfeldma@gmail.com>
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ */
+
+#ifndef _ROCKER_OF_DPA_H_
+#define _ROCKER_OF_DPA_H_
+
+World *of_dpa_world_alloc(Rocker *r);
+
+#endif /* _ROCKER_OF_DPA_H_ */
