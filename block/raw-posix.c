965182549cee6fa337f0e72cde4d5787f35f33ac
Kevin Wolf <kwolf@redhat.com>
2015-03-17 13:45:21 +0100

    raw-posix: Deprecate aio=threads fallback without O_DIRECT
    Currently, if the user requests aio=native, but forgets to choose a
    cache mode that sets O_DIRECT, that request is silently ignored and raw
    falls back to aio=threads.
    
    Deprecate that behaviour so we can make it an error in future qemu
    versions.
    
    Signed-off-by: Kevin Wolf <kwolf@redhat.com>
    Reviewed-by: Markus Armbruster <armbru@redhat.com>

--- block/raw-posix.c
+++ block/raw-posix.c
@@ -503,6 +503,14 @@ static int raw_open_common(BlockDriverState *bs, QDict *options,
         error_setg_errno(errp, -ret, "Could not set AIO state");
         goto fail;
     }
+    if (!s->use_aio && (bdrv_flags & BDRV_O_NATIVE_AIO)) {
+        error_printf("WARNING: aio=native was specified for '%s', but "
+                     "it requires cache.direct=on, which was not "
+                     "specified. Falling back to aio=threads.\n"
+                     "         This will become an error condition in "
+                     "future QEMU versions.\n",
+                     bs->filename);
+    }
 #endif
 
     s->has_discard = true;
