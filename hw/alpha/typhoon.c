58c24a4775ef7ccf16cfcd695753dfdf149fe29d
Dirk Müller <dirk@dmllr.de>
2015-04-04 14:14:14 +0200

    alpha: memory: Replace memory_region_init_ram with memory_region_allocate_system_memory
    Commit 0b183fc871:"memory: move mem_path handling to
    memory_region_allocate_system_memory" split memory_region_init_ram and
    memory_region_init_ram_from_file. Also it moved mem-path handling a step
    up from memory_region_init_ram to memory_region_allocate_system_memory.
    
    Therefore for any board that uses memory_region_init_ram directly,
    -mem-path is not supported.
    
    Fix this by replacing memory_region_init_ram with
    memory_region_allocate_system_memory.
    
    Cc: Richard Henderson <rth@twiddle.net>
    Signed-off-by: Dirk Mueller <dmueller@suse.com>
    Acked-by: Richard Henderson <rth@twiddle.net>
    Message-id: CAL5wTH64_ykF17cw2T1Axq8P3vCWm=6WbUJ3qJrLF-u+-MmzUw@mail.gmail.com
    Signed-off-by: Peter Maydell <peter.maydell@linaro.org>

--- hw/alpha/typhoon.c
+++ hw/alpha/typhoon.c
@@ -844,9 +844,8 @@ PCIBus *typhoon_init(ram_addr_t ram_size, ISABus **isa_bus,
 
     /* Main memory region, 0x00.0000.0000.  Real hardware supports 32GB,
        but the address space hole reserved at this point is 8TB.  */
-    memory_region_init_ram(&s->ram_region, OBJECT(s), "ram", ram_size,
-                           &error_abort);
-    vmstate_register_ram_global(&s->ram_region);
+    memory_region_allocate_system_memory(&s->ram_region, OBJECT(s), "ram",
+                                         ram_size);
     memory_region_add_subregion(addr_space, 0, &s->ram_region);
 
     /* TIGbus, 0x801.0000.0000, 1GB.  */
