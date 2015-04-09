e3a0abfda71db1fa83be894dcff7c4871b36cc8d
Emilio G. Cota <cota@braap.org>
2015-04-09 16:07:33 -0400

    translate-all: use glib for all page descriptor allocations
    Since commit
    
      b7b5233a "bsd-user/mmap.c: Don't try to override g_malloc/g_free"
    
    the exception we make here for usermode has been unnecessary.
    Get rid of it.
    
    Signed-off-by: Emilio G. Cota <cota@braap.org>
    Message-Id: <1428610053-26148-1-git-send-email-cota@braap.org>
    Signed-off-by: Paolo Bonzini <pbonzini@redhat.com>

--- translate-all.c
+++ translate-all.c
@@ -389,18 +389,6 @@ static PageDesc *page_find_alloc(tb_page_addr_t index, int alloc)
     void **lp;
     int i;
 
-#if defined(CONFIG_USER_ONLY)
-    /* We can't use g_malloc because it may recurse into a locked mutex. */
-# define ALLOC(P, SIZE)                                 \
-    do {                                                \
-        P = mmap(NULL, SIZE, PROT_READ | PROT_WRITE,    \
-                 MAP_PRIVATE | MAP_ANONYMOUS, -1, 0);   \
-    } while (0)
-#else
-# define ALLOC(P, SIZE) \
-    do { P = g_malloc0(SIZE); } while (0)
-#endif
-
     /* Level 1.  Always allocated.  */
     lp = l1_map + ((index >> V_L1_SHIFT) & (V_L1_SIZE - 1));
 
@@ -412,7 +400,7 @@ static PageDesc *page_find_alloc(tb_page_addr_t index, int alloc)
             if (!alloc) {
                 return NULL;
             }
-            ALLOC(p, sizeof(void *) * V_L2_SIZE);
+            p = g_new0(void *, V_L2_SIZE);
             *lp = p;
         }
 
@@ -424,11 +412,9 @@ static PageDesc *page_find_alloc(tb_page_addr_t index, int alloc)
         if (!alloc) {
             return NULL;
         }
-        ALLOC(pd, sizeof(PageDesc) * V_L2_SIZE);
+        pd = g_new0(PageDesc, V_L2_SIZE);
         *lp = pd;
     }
-
-#undef ALLOC
 
     return pd + (index & (V_L2_SIZE - 1));
 }
