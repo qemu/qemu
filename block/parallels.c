dd3bed16ff229496b30cc77224b0c0ae645c4dae
Roman Kagan <rkagan@parallels.com>
2015-04-28 10:46:38 +0300

    block/parallels: add get_block_status
    Implement VFS method for get_block_status to Parallels format driver.
    
    qemu_co_mutex_lock is not necessary yet (the driver is read-only) but
    will be necessary very soon when write will be supported.
    
    Signed-off-by: Roman Kagan <rkagan@parallels.com>
    Reviewed-by: Denis V. Lunev <den@openvz.org>
    Signed-off-by: Denis V. Lunev <den@openvz.org>
    Message-id: 1430207220-24458-6-git-send-email-den@openvz.org
    CC: Kevin Wolf <kwolf@redhat.com>
    CC: Stefan Hajnoczi <stefanha@redhat.com>
    Signed-off-by: Stefan Hajnoczi <stefanha@redhat.com>

--- block/parallels.c
+++ block/parallels.c
@@ -166,6 +166,26 @@ static int cluster_remainder(BDRVParallelsState *s, int64_t sector_num,
     return MIN(nb_sectors, ret);
 }
 
+static int64_t coroutine_fn parallels_co_get_block_status(BlockDriverState *bs,
+        int64_t sector_num, int nb_sectors, int *pnum)
+{
+    BDRVParallelsState *s = bs->opaque;
+    int64_t offset;
+
+    qemu_co_mutex_lock(&s->lock);
+    offset = seek_to_sector(s, sector_num);
+    qemu_co_mutex_unlock(&s->lock);
+
+    *pnum = cluster_remainder(s, sector_num, nb_sectors);
+
+    if (offset < 0) {
+        return 0;
+    }
+
+    return (offset << BDRV_SECTOR_BITS) |
+        BDRV_BLOCK_DATA | BDRV_BLOCK_OFFSET_VALID;
+}
+
 static int parallels_read(BlockDriverState *bs, int64_t sector_num,
                     uint8_t *buf, int nb_sectors)
 {
@@ -213,6 +233,7 @@ static BlockDriver bdrv_parallels = {
     .bdrv_open		= parallels_open,
     .bdrv_read          = parallels_co_read,
     .bdrv_close		= parallels_close,
+    .bdrv_co_get_block_status = parallels_co_get_block_status,
 };
 
 static void bdrv_parallels_init(void)
