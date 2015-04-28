88dbed3f5946b74cf02c1bb0082b8c50037720ea
Gerd Hoffmann <kraxel@redhat.com>
2015-04-28 09:19:35 +0200

    Revert "xhci: generate a Transfer Event for each Transfer TRB with the IOC bit set"
    This makes xhci generate multiple short packet events in case of
    multi-trb transfers.  Which is wrong.  We need to fix this in a
    different way.
    
    This reverts commit aa6857891df614c620e6e9fc4bc4af6e0e49cafd.
    
    Signed-off-by: Gerd Hoffmann <kraxel@redhat.com>

--- hw/usb/hcd-xhci.c
+++ hw/usb/hcd-xhci.c
@@ -1767,18 +1767,9 @@ static void xhci_xfer_report(XHCITransfer *xfer)
             break;
         }
 
-        /*
-         * XHCI 1.1, 4.11.3.1 Transfer Event TRB -- "each Transfer TRB
-         * encountered with its IOC flag set to '1' shall generate a Transfer
-         * Event."
-         *
-         * Otherwise, longer transfers can have multiple data TRBs (for scatter
-         * gather). Short transfers and errors should be reported once per
-         * transfer only.
-         */
-        if ((trb->control & TRB_TR_IOC) ||
-            (!reported && ((shortpkt && (trb->control & TRB_TR_ISP)) ||
-                           (xfer->status != CC_SUCCESS && left == 0)))) {
+        if (!reported && ((trb->control & TRB_TR_IOC) ||
+                          (shortpkt && (trb->control & TRB_TR_ISP)) ||
+                          (xfer->status != CC_SUCCESS && left == 0))) {
             event.slotid = xfer->slotid;
             event.epid = xfer->epid;
             event.length = (trb->status & 0x1ffff) - chunk;
