a34944fe2e2457309bde74c1ffe3a1c60c6da018
Nikunj A Dadhania <nikunj@linux.vnet.ibm.com>
2015-05-07 15:33:58 +1000

    spapr: override default ram size to 512MB
    Signed-off-by: Nikunj A Dadhania <nikunj@linux.vnet.ibm.com>
    Reviewed-by: Igor Mammedov <imammedo@redhat.com>
    Reviewed-by: Thomas Huth <thuth@redhat.com>
    Acked-by: David Gibson <david@gibson.dropbear.id.au>
    Signed-off-by: David Gibson <david@gibson.dropbear.id.au>
    Signed-off-by: Alexander Graf <agraf@suse.de>

--- hw/ppc/spapr.c
+++ hw/ppc/spapr.c
@@ -1801,6 +1801,7 @@ static void spapr_machine_class_init(ObjectClass *oc, void *data)
     mc->max_cpus = MAX_CPUS;
     mc->no_parallel = 1;
     mc->default_boot_order = "";
+    mc->default_ram_size = 512 * M_BYTE;
     mc->kvm_type = spapr_kvm_type;
     mc->has_dynamic_sysbus = true;
 
