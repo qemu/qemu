6328d69de09243929d90665eb6a6e0c96c4d06fc
Shannon Zhao <shannon.zhao@linaro.org>
2015-05-12 10:25:18 +0800

    hw/virtio/virtio-pci: Remove meaningless blank Property
    Signed-off-by: Shannon Zhao <zhaoshenglong@huawei.com>
    Signed-off-by: Shannon Zhao <shannon.zhao@linaro.org>
    Signed-off-by: Michael Tokarev <mjt@tls.msk.ru>

--- hw/virtio/virtio-pci.c
+++ hw/virtio/virtio-pci.c
@@ -2007,10 +2007,6 @@ static const TypeInfo virtio_net_pci_info = {
 
 /* virtio-rng-pci */
 
-static Property virtio_rng_pci_properties[] = {
-    DEFINE_PROP_END_OF_LIST(),
-};
-
 static void virtio_rng_pci_realize(VirtIOPCIProxy *vpci_dev, Error **errp)
 {
     VirtIORngPCI *vrng = VIRTIO_RNG_PCI(vpci_dev);
@@ -2037,7 +2033,6 @@ static void virtio_rng_pci_class_init(ObjectClass *klass, void *data)
 
     k->realize = virtio_rng_pci_realize;
     set_bit(DEVICE_CATEGORY_MISC, dc->categories);
-    dc->props = virtio_rng_pci_properties;
 
     pcidev_k->vendor_id = PCI_VENDOR_ID_REDHAT_QUMRANET;
     pcidev_k->device_id = PCI_DEVICE_ID_VIRTIO_RNG;
