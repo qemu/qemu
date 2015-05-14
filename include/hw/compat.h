1edbde82b809f80b973978886d8232fbf280cb03
Eduardo Habkost <ehabkost@redhat.com>
2015-05-14 15:53:02 -0300

    hw: Define empty HW_COMPAT_2_[23] macros
    Now we can make everything consistent and define the macros even if they
    are still empty.
    
    Signed-off-by: Eduardo Habkost <ehabkost@redhat.com>
    Reviewed-by: Michael S. Tsirkin <mst@redhat.com>
    Signed-off-by: Michael S. Tsirkin <mst@redhat.com>

--- include/hw/compat.h
+++ include/hw/compat.h
@@ -1,6 +1,12 @@
 #ifndef HW_COMPAT_H
 #define HW_COMPAT_H
 
+#define HW_COMPAT_2_3 \
+        /* empty */
+
+#define HW_COMPAT_2_2 \
+        /* empty */
+
 #define HW_COMPAT_2_1 \
         {\
             .driver   = "intel-hda",\
