ee0d0be16819896cc6c8018cbe171a632b61489c
Aurelien Jarno <aurelien@aurel32.net>
2015-05-17 01:28:03 +0200

    target-s390x: fix CC computation for EX instruction
    Commit 7a6c7067f optimized CC computation by only saving cc_op before
    calling helpers as they either don't touch the CC or generate a new
    static value. This however doesn't work for the EX instruction as the
    helper changes or not the CC value depending on the actual executed
    instruction (e.g. MVC vs CLC).
    
    This patches force a CC computation before calling the helper. This
    fixes random memory corruption occuring in guests.
    
    Signed-off-by: Aurelien Jarno <aurelien@aurel32.net>
    [agraf: remove set_cc_static in op_ex as suggested by rth]
    Signed-off-by: Alexander Graf <agraf@suse.de>

--- target-s390x/translate.c
+++ target-s390x/translate.c
@@ -2101,13 +2101,12 @@ static ExitStatus op_ex(DisasContext *s, DisasOps *o)
     TCGv_i64 tmp;
 
     update_psw_addr(s);
-    update_cc_op(s);
+    gen_op_calc_cc(s);
 
     tmp = tcg_const_i64(s->next_pc);
     gen_helper_ex(cc_op, cpu_env, cc_op, o->in1, o->in2, tmp);
     tcg_temp_free_i64(tmp);
 
-    set_cc_static(s);
     return NO_EXIT;
 }
 
