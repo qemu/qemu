ea78deaf3b312ef6f58b7f795979faa9137844d0
zhanghailiang <zhang.zhanghailiang@huawei.com>
2015-05-20 15:18:04 +0000

    COLO: Implement colo checkpoint protocol
    We need communications protocol of user-defined to control the checkpoint
    process.
    
    The new checkpoint request is started by Primary VM, and the interactive process
    like below:
    Checkpoint synchronizing points:
    
                       Primary               Secondary
                                                initial work
    'checkpoint-ready'    <-------------------- @
    
    'checkpoint-request'  @ -------------------->
                                                Suspend (Only in hybrid mode)
    'checkpoint-reply'    <-------------------- @
                          Suspend&Save state
    'vmstate-send'        @ -------------------->
                          Send state            Receive state
    'vmstate-received'    <-------------------- @
                          Release packets       Load state
    'vmstate-load'        <-------------------- @
                          Resume                Resume (Only in hybrid mode)
    
                          Start Comparing (Only in hybrid mode)
    NOTE:
     1) '@' who sends the message
     2) Every sync-point is synchronized by two sides with only
        one handshake(single direction) for low-latency.
        If more strict synchronization is required, a opposite direction
        sync-point should be added.
     3) Since sync-points are single direction, the remote side may
        go forward a lot when this side just receives the sync-point.
     4) For now, we only support 'periodic' checkpoint, for which
       the Secondary VM is not running, later we will support 'hybrid' mode.
    
    Signed-off-by: zhanghailiang <zhang.zhanghailiang@huawei.com>
    Signed-off-by: Li Zhijian <lizhijian@cn.fujitsu.com>
    Signed-off-by: Gonglei <arei.gonglei@huawei.com>
    Cc: Eric Blake <eblake@redhat.com>
    Cc: Markus Armbruster <armbru@redhat.com>
    Cc: Dr. David Alan Gilbert <dgilbert@redhat.com>

--- migration/colo.c
+++ migration/colo.c
@@ -10,6 +10,7 @@
  * later.  See the COPYING file in the top-level directory.
  */
 
+#include <unistd.h>
 #include "sysemu/sysemu.h"
 #include "migration/colo.h"
 #include "trace.h"
@@ -34,11 +35,126 @@ bool migration_incoming_in_colo_state(void)
     return mis && (mis->state == MIGRATION_STATUS_COLO);
 }
 
+static void colo_put_cmd(QEMUFile *f, COLOCommand cmd,
+                         Error **errp)
+{
+    int ret;
+
+    if (cmd >= COLO_COMMAND__MAX) {
+        error_setg(errp, "%s: Invalid cmd", __func__);
+        return;
+    }
+    qemu_put_be32(f, cmd);
+    qemu_fflush(f);
+
+    ret = qemu_file_get_error(f);
+    if (ret < 0) {
+        error_setg_errno(errp, -ret, "Can't put COLO command");
+    }
+    trace_colo_put_cmd(COLOCommand_lookup[cmd]);
+}
+
+static COLOCommand colo_get_cmd(QEMUFile *f, Error **errp)
+{
+    COLOCommand cmd;
+    int ret;
+
+    cmd = qemu_get_be32(f);
+    ret = qemu_file_get_error(f);
+    if (ret < 0) {
+        error_setg_errno(errp, -ret, "Can't get COLO command");
+        return cmd;
+    }
+    if (cmd >= COLO_COMMAND__MAX) {
+        error_setg(errp, "%s: Invalid cmd", __func__);
+        return cmd;
+    }
+    trace_colo_get_cmd(COLOCommand_lookup[cmd]);
+    return cmd;
+}
+
+static void colo_get_check_cmd(QEMUFile *f, COLOCommand expect_cmd,
+                               Error **errp)
+{
+    COLOCommand cmd;
+    Error *local_err = NULL;
+
+    cmd = colo_get_cmd(f, &local_err);
+    if (local_err) {
+        error_propagate(errp, local_err);
+        return;
+    }
+    if (cmd != expect_cmd) {
+        error_setg(errp, "Unexpected COLO command %d, expected %d",
+                          expect_cmd, cmd);
+    }
+}
+
+static int colo_do_checkpoint_transaction(MigrationState *s)
+{
+    Error *local_err = NULL;
+
+    colo_put_cmd(s->to_dst_file, COLO_COMMAND_CHECKPOINT_REQUEST,
+                 &local_err);
+    if (local_err) {
+        goto out;
+    }
+
+    colo_get_check_cmd(s->rp_state.from_dst_file,
+                       COLO_COMMAND_CHECKPOINT_REPLY, &local_err);
+    if (local_err) {
+        goto out;
+    }
+
+    /* TODO: suspend and save vm state to colo buffer */
+
+    colo_put_cmd(s->to_dst_file, COLO_COMMAND_VMSTATE_SEND, &local_err);
+    if (local_err) {
+        goto out;
+    }
+
+    /* TODO: send vmstate to Secondary */
+
+    colo_get_check_cmd(s->rp_state.from_dst_file,
+                       COLO_COMMAND_VMSTATE_RECEIVED, &local_err);
+    if (local_err) {
+        goto out;
+    }
+
+    colo_get_check_cmd(s->rp_state.from_dst_file,
+                       COLO_COMMAND_VMSTATE_LOADED, &local_err);
+    if (local_err) {
+        goto out;
+    }
+
+    /* TODO: resume Primary */
+
+    return 0;
+out:
+    if (local_err) {
+        error_report_err(local_err);
+    }
+    return -EINVAL;
+}
+
 static void colo_process_checkpoint(MigrationState *s)
 {
+    Error *local_err = NULL;
+    int ret;
+
     s->rp_state.from_dst_file = qemu_file_get_return_path(s->to_dst_file);
     if (!s->rp_state.from_dst_file) {
         error_report("Open QEMUFile from_dst_file failed");
+        goto out;
+    }
+
+    /*
+     * Wait for Secondary finish loading vm states and enter COLO
+     * restore.
+     */
+    colo_get_check_cmd(s->rp_state.from_dst_file,
+                       COLO_COMMAND_CHECKPOINT_READY, &local_err);
+    if (local_err) {
         goto out;
     }
 
@@ -47,9 +163,19 @@ static void colo_process_checkpoint(MigrationState *s)
     qemu_mutex_unlock_iothread();
     trace_colo_vm_state_change("stop", "run");
 
-    /*TODO: COLO checkpoint savevm loop*/
+    while (s->state == MIGRATION_STATUS_COLO) {
+        /* start a colo checkpoint */
+        ret = colo_do_checkpoint_transaction(s);
+        if (ret < 0) {
+            goto out;
+        }
+    }
 
 out:
+    /* Throw the unreported error message after exited from loop */
+    if (local_err) {
+        error_report_err(local_err);
+    }
     migrate_set_state(&s->state, MIGRATION_STATUS_COLO,
                       MIGRATION_STATUS_COMPLETED);
 
@@ -67,9 +193,33 @@ void migrate_start_colo_process(MigrationState *s)
     qemu_mutex_lock_iothread();
 }
 
+static void colo_wait_handle_cmd(QEMUFile *f, int *checkpoint_request,
+                                 Error **errp)
+{
+    COLOCommand cmd;
+    Error *local_err = NULL;
+
+    cmd = colo_get_cmd(f, &local_err);
+    if (local_err) {
+        error_propagate(errp, local_err);
+        return;
+    }
+
+    switch (cmd) {
+    case COLO_COMMAND_CHECKPOINT_REQUEST:
+        *checkpoint_request = 1;
+        break;
+    default:
+        *checkpoint_request = 0;
+        error_setg(errp, "Got unknown COLO command: %d", cmd);
+        break;
+    }
+}
+
 void *colo_process_incoming_thread(void *opaque)
 {
     MigrationIncomingState *mis = opaque;
+    Error *local_err = NULL;
 
     migrate_set_state(&mis->state, MIGRATION_STATUS_ACTIVE,
                       MIGRATION_STATUS_COLO);
@@ -85,9 +235,56 @@ void *colo_process_incoming_thread(void *opaque)
     */
     qemu_file_set_blocking(mis->from_src_file, true);
 
-    /* TODO: COLO checkpoint restore loop */
+    colo_put_cmd(mis->to_src_file, COLO_COMMAND_CHECKPOINT_READY,
+                 &local_err);
+    if (local_err) {
+        goto out;
+    }
+
+    while (mis->state == MIGRATION_STATUS_COLO) {
+        int request;
+
+        colo_wait_handle_cmd(mis->from_src_file, &request, &local_err);
+        if (local_err) {
+            goto out;
+        }
+        assert(request);
+        /* FIXME: This is unnecessary for periodic checkpoint mode */
+        colo_put_cmd(mis->to_src_file, COLO_COMMAND_CHECKPOINT_REPLY,
+                     &local_err);
+        if (local_err) {
+            goto out;
+        }
+
+        colo_get_check_cmd(mis->from_src_file,
+                           COLO_COMMAND_VMSTATE_SEND, &local_err);
+        if (local_err) {
+            goto out;
+        }
+
+        /* TODO: read migration data into colo buffer */
+
+        colo_put_cmd(mis->to_src_file, COLO_COMMAND_VMSTATE_RECEIVED,
+                     &local_err);
+        if (local_err) {
+            goto out;
+        }
+
+        /* TODO: load vm state */
+
+        colo_put_cmd(mis->to_src_file, COLO_COMMAND_VMSTATE_LOADED,
+                     &local_err);
+        if (local_err) {
+            goto out;
+        }
+    }
 
 out:
+    /* Throw the unreported error message after exited from loop */
+    if (local_err) {
+        error_report_err(local_err);
+    }
+
     if (mis->to_src_file) {
         qemu_fclose(mis->to_src_file);
     }
