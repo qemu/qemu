38d40ff10f71657ea913a63d1f8477be368b92c1
Pavel Fedin <p.fedin@samsung.com>
2015-05-27 15:59:59 +0300

    Add stream ID to MSI write
    GICv3 ITS distinguishes between devices by using hardwired device IDs passed on the bus.
    This patch implements passing these IDs in qemu.
    SMMU is also known to use stream IDs, therefore this addition can also be useful for
    implementing platforms with SMMU.
    
    Signed-off-by: Pavel Fedin <p.fedin@samsung.com>
    Reviewed-by: Michael S. Tsirkin <mst@redhat.com>
    
     Changes from v1:
    - Added bus number to the stream ID
    - Added stream ID not only to MSI-X, but also to plain MSI. Some common code was made into
    msi_send_message() function.
    Reviewed-by: Michael S. Tsirkin <mst@redhat.com>
    Signed-off-by: Michael S. Tsirkin <mst@redhat.com>

--- include/hw/pci/msi.h
+++ include/hw/pci/msi.h
@@ -39,6 +39,7 @@ int msi_init(struct PCIDevice *dev, uint8_t offset,
 void msi_uninit(struct PCIDevice *dev);
 void msi_reset(PCIDevice *dev);
 void msi_notify(PCIDevice *dev, unsigned int vector);
+void msi_send_message(PCIDevice *dev, MSIMessage msg);
 void msi_write_config(PCIDevice *dev, uint32_t addr, uint32_t val, int len);
 unsigned int msi_nr_vectors_allocated(const PCIDevice *dev);
 
