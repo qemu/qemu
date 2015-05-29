8434488400971c6793893b8c9547bc6b97e076ce
Shannon Zhao <shannon.zhao@linaro.org>
2015-05-29 11:28:57 +0100

    hw/arm/virt-acpi-build: Generate MCFG table
    Generate MCFG table for PCIe controller.
    
    Signed-off-by: Shannon Zhao <zhaoshenglong@huawei.com>
    Signed-off-by: Shannon Zhao <shannon.zhao@linaro.org>
    Reviewed-by: Alex Bennée <alex.bennee@linaro.org>
    Message-id: 1432522520-8068-14-git-send-email-zhaoshenglong@huawei.com
    Signed-off-by: Peter Maydell <peter.maydell@linaro.org>

--- hw/arm/virt-acpi-build.c
+++ hw/arm/virt-acpi-build.c
@@ -39,6 +39,7 @@
 #include "hw/loader.h"
 #include "hw/hw.h"
 #include "hw/acpi/aml-build.h"
+#include "hw/pci/pcie_host.h"
 
 #define ARM_SPI_BASE 32
 
@@ -178,6 +179,25 @@ build_rsdp(GArray *rsdp_table, GArray *linker, unsigned rsdt)
                                     rsdp, rsdp, sizeof *rsdp, &rsdp->checksum);
 
     return rsdp_table;
+}
+
+static void
+build_mcfg(GArray *table_data, GArray *linker, VirtGuestInfo *guest_info)
+{
+    AcpiTableMcfg *mcfg;
+    const MemMapEntry *memmap = guest_info->memmap;
+    int len = sizeof(*mcfg) + sizeof(mcfg->allocation[0]);
+
+    mcfg = acpi_data_push(table_data, len);
+    mcfg->allocation[0].address = cpu_to_le64(memmap[VIRT_PCIE_ECAM].base);
+
+    /* Only a single allocation so no need to play with segments */
+    mcfg->allocation[0].pci_segment = cpu_to_le16(0);
+    mcfg->allocation[0].start_bus_number = 0;
+    mcfg->allocation[0].end_bus_number = (memmap[VIRT_PCIE_ECAM].size
+                                          / PCIE_MMCFG_SIZE_MIN) - 1;
+
+    build_header(linker, table_data, (void *)mcfg, "MCFG", len, 5);
 }
 
 /* GTDT */
@@ -349,6 +369,9 @@ void virt_acpi_build(VirtGuestInfo *guest_info, AcpiBuildTables *tables)
 
     acpi_add_table(table_offsets, tables_blob);
     build_gtdt(tables_blob, tables->linker);
+
+    acpi_add_table(table_offsets, tables_blob);
+    build_mcfg(tables_blob, tables->linker, guest_info);
 
     /* RSDT is pointed to by RSDP */
     rsdt = tables_blob->len;
