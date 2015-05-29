81cb0a565795de57672f761f04e29e08dfde067c
Jason Wang <jasowang@redhat.com>
2015-05-29 14:13:14 +0800

    vhost: correctly pass error to caller in vhost_dev_enable_notifiers()
    We override the error value r in fail_vq, this will cause the caller
    can't detect the failure which may cause the caller may disable the
    notifiers twice if vhost is failed to start. Fix this by using another
    variable to keep track the return value of set_host_notifier().
    
    Fixes b0b3db79559e57db340b292621c397e7a6cdbdc5 ("vhost-net: cleanup
    host notifiers at last step")
    
    Cc: qemu-stable@nongnu.org
    Cc: Michael S. Tsirkin <mst@redhat.com>
    Signed-off-by: Jason Wang <jasowang@redhat.com>
    Reviewed-by: Michael S. Tsirkin <mst@redhat.com>
    Signed-off-by: Michael S. Tsirkin <mst@redhat.com>
    (cherry picked from commit 16617e36b02ebdc83f215d89db9ac00f7d6d6d83)
    Signed-off-by: Michael Roth <mdroth@linux.vnet.ibm.com>

--- hw/virtio/vhost.c
+++ hw/virtio/vhost.c
@@ -921,7 +921,7 @@ int vhost_dev_enable_notifiers(struct vhost_dev *hdev, VirtIODevice *vdev)
     BusState *qbus = BUS(qdev_get_parent_bus(DEVICE(vdev)));
     VirtioBusState *vbus = VIRTIO_BUS(qbus);
     VirtioBusClass *k = VIRTIO_BUS_GET_CLASS(vbus);
-    int i, r;
+    int i, r, e;
     if (!k->set_host_notifier) {
         fprintf(stderr, "binding does not support host notifiers\n");
         r = -ENOSYS;
@@ -939,12 +939,12 @@ int vhost_dev_enable_notifiers(struct vhost_dev *hdev, VirtIODevice *vdev)
     return 0;
 fail_vq:
     while (--i >= 0) {
-        r = k->set_host_notifier(qbus->parent, hdev->vq_index + i, false);
-        if (r < 0) {
+        e = k->set_host_notifier(qbus->parent, hdev->vq_index + i, false);
+        if (e < 0) {
             fprintf(stderr, "vhost VQ %d notifier cleanup error: %d\n", i, -r);
             fflush(stderr);
         }
-        assert (r >= 0);
+        assert (e >= 0);
     }
 fail:
     return r;
