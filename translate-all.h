b6b099541d6cf3c50b0fb5af916fff0db6508805
Paolo Bonzini <pbonzini@redhat.com>
2015-06-01 09:53:55 +0200

    translate-all: delete prototype for non-existent function
    Missed in commit 3a808cc40
    
    Signed-off-by: Paolo Bonzini <pbonzini@redhat.com>
    Reviewed-by: Eric Blake <eblake@redhat.com>
    Reviewed-by: Peter Crosthwaite <peter.crosthwaite@xilinx.com>
    Signed-off-by: Michael Tokarev <mjt@tls.msk.ru>

--- translate-all.h
+++ translate-all.h
@@ -21,7 +21,6 @@
 
 /* translate-all.c */
 void tb_invalidate_phys_page_fast(tb_page_addr_t start, int len);
-void cpu_unlink_tb(CPUState *cpu);
 void tb_check_watchpoint(CPUState *cpu);
 
 #endif /* TRANSLATE_ALL_H */
