8772de2c53b44c75f18140646aa928e6d77cb9d8
Shannon Zhao <shannon.zhao@linaro.org>
2015-06-15 18:06:08 +0100

    hw/arm/virt: Add cortex-a53 cpu support in machine virt
    Add cortex-a53 cpu support in machine virt, so it can be used for TCG
    and KVM.
    
    Signed-off-by: Shannon Zhao <zhaoshenglong@huawei.com>
    Signed-off-by: Shannon Zhao <shannon.zhao@linaro.org>
    Reviewed-by: Peter Maydell <peter.maydell@linaro.org>
    Message-id: 1433207452-4512-3-git-send-email-shannon.zhao@linaro.org
    Signed-off-by: Peter Maydell <peter.maydell@linaro.org>

--- hw/arm/virt.c
+++ hw/arm/virt.c
@@ -145,6 +145,11 @@ static VirtBoardInfo machines[] = {
         .irqmap = a15irqmap,
     },
     {
+        .cpu_model = "cortex-a53",
+        .memmap = a15memmap,
+        .irqmap = a15irqmap,
+    },
+    {
         .cpu_model = "cortex-a57",
         .memmap = a15memmap,
         .irqmap = a15irqmap,
