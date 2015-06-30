d2b3f390d4e4b5d9dd74ae703d365a7b75a234ea
Olga Krishtal <okrishtal@virtuozzo.com>
2015-06-30 13:25:21 +0300

    qga: added mountpoint and filesystem type for single volume
    We should use GetVolumeXXX api to work with volumes. This will help us to
    resolve the situation with volumes without drive letter, i.e. when the
    volume is mounted as a folder. Such volume is called mounted folder.
    This volume is a regular mounted volume from all other points of view.
    The information about non mounted volume is reported as System Reserved.
    This volume is not mounted and thus it is not writable.
    
    GuestDiskAddressList API is not used because operations are performed with
    volumes but no with disks. This means that spanned disk will
    be counted and handled as a single volume. It is worth mentioning
    that the information about every disk in the volume can be queried
    via IOCTL_VOLUME_GET_VOLUME_DISK_EXTENTS.
    
    Signed-off-by: Olga Krishtal <okrishtal@virtuozzo.com>
    Signed-off-by: Denis V. Lunev <den@openvz.org>
    CC: Eric Blake <eblake@redhat.com>
    CC: Michael Roth <mdroth@linux.vnet.ibm.com>
    Signed-off-by: Michael Roth <mdroth@linux.vnet.ibm.com>

--- qga/commands-win32.c
+++ qga/commands-win32.c
@@ -387,6 +387,54 @@ static void guest_file_init(void)
     QTAILQ_INIT(&guest_file_state.filehandles);
 }
 
+static GuestFilesystemInfo *build_guest_fsinfo(char *guid, Error **errp)
+{
+    DWORD info_size;
+    char mnt, *mnt_point;
+    char fs_name[32];
+    char vol_info[MAX_PATH+1];
+    size_t len;
+    GuestFilesystemInfo *fs = NULL;
+
+    GetVolumePathNamesForVolumeName(guid, (LPCH)&mnt, 0, &info_size);
+    if (GetLastError() != ERROR_MORE_DATA) {
+        error_setg_win32(errp, GetLastError(), "failed to get volume name");
+        return NULL;
+    }
+
+    mnt_point = g_malloc(info_size + 1);
+    if (!GetVolumePathNamesForVolumeName(guid, mnt_point, info_size,
+                                         &info_size)) {
+        error_setg_win32(errp, GetLastError(), "failed to get volume name");
+        goto free;
+    }
+
+    len = strlen(mnt_point);
+    mnt_point[len] = '\\';
+    mnt_point[len+1] = 0;
+    if (!GetVolumeInformation(mnt_point, vol_info, sizeof(vol_info), NULL, NULL,
+                              NULL, (LPSTR)&fs_name, sizeof(fs_name))) {
+        if (GetLastError() != ERROR_NOT_READY) {
+            error_setg_win32(errp, GetLastError(), "failed to get volume info");
+        }
+        goto free;
+    }
+
+    fs_name[sizeof(fs_name) - 1] = 0;
+    fs = g_malloc(sizeof(*fs));
+    fs->name = g_strdup(guid);
+    if (len == 0) {
+        fs->mountpoint = g_strdup("System Reserved");
+    } else {
+        fs->mountpoint = g_strndup(mnt_point, len);
+    }
+    fs->type = g_strdup(fs_name);
+    fs->disk = NULL;
+free:
+    g_free(mnt_point);
+    return fs;
+}
+
 GuestFilesystemInfoList *qmp_guest_get_fsinfo(Error **errp)
 {
     HANDLE vol_h;
@@ -400,8 +448,12 @@ GuestFilesystemInfoList *qmp_guest_get_fsinfo(Error **errp)
     }
 
     do {
+        GuestFilesystemInfo *info = build_guest_fsinfo(guid, errp);
+        if (info == NULL) {
+            continue;
+        }
         new = g_malloc(sizeof(*ret));
-        new->value = build_guest_fsinfo(guid, errp);
+        new->value = info;
         new->next = ret;
         ret = new;
     } while (FindNextVolume(vol_h, guid, sizeof(guid)));
