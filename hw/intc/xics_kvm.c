a45863bda90daa8ec39e5a312b9734fd4665b016
Bharata B Rao <bharata@linux.vnet.ibm.com>
2015-07-02 16:23:20 +1000

    xics_kvm: Don't enable KVM_CAP_IRQ_XICS if already enabled
    When supporting CPU hot removal by parking the vCPU fd and reusing
    it during hotplug again, there can be cases where we try to reenable
    KVM_CAP_IRQ_XICS CAP for the vCPU for which it was already enabled.
    Introduce a boolean member in ICPState to track this and don't
    reenable the CAP if it was already enabled earlier.
    
    Re-enabling this CAP should ideally work, but currently it results in
    kernel trying to create and associate ICP with this vCPU and that
    fails since there is already an ICP associated with it. Hence this
    patch is needed to work around this problem in the kernel.
    
    This change allows CPU hot removal to work for sPAPR.
    
    Signed-off-by: Bharata B Rao <bharata@linux.vnet.ibm.com>
    Reviewed-by: David Gibson <david@gibson.dropbear.id.au>
    Signed-off-by: David Gibson <david@gibson.dropbear.id.au>
    Signed-off-by: Alexander Graf <agraf@suse.de>

--- hw/intc/xics_kvm.c
+++ hw/intc/xics_kvm.c
@@ -331,6 +331,15 @@ static void xics_kvm_cpu_setup(XICSState *icp, PowerPCCPU *cpu)
         abort();
     }
 
+    /*
+     * If we are reusing a parked vCPU fd corresponding to the CPU
+     * which was hot-removed earlier we don't have to renable
+     * KVM_CAP_IRQ_XICS capability again.
+     */
+    if (ss->cap_irq_xics_enabled) {
+        return;
+    }
+
     if (icpkvm->kernel_xics_fd != -1) {
         int ret;
 
@@ -343,6 +352,7 @@ static void xics_kvm_cpu_setup(XICSState *icp, PowerPCCPU *cpu)
                     kvm_arch_vcpu_id(cs), strerror(errno));
             exit(1);
         }
+        ss->cap_irq_xics_enabled = true;
     }
 }
 
