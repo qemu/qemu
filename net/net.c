625de449fc5597f2e1aff9cb586e249e198f03c9
Fam Zheng <famz@redhat.com>
2015-07-07 09:21:07 +0800

    net: Flush queued packets when guest resumes
    Since commit 6e99c63 "net/socket: Drop net_socket_can_send" and friends,
    net queues need to be explicitly flushed after qemu_can_send_packet()
    returns false, because the netdev side will disable the polling of fd.
    
    This fixes the case of "cont" after "stop" (or migration).
    
    Signed-off-by: Fam Zheng <famz@redhat.com>
    Message-id: 1436232067-29144-1-git-send-email-famz@redhat.com
    Signed-off-by: Stefan Hajnoczi <stefanha@redhat.com>

--- net/net.c
+++ net/net.c
@@ -1257,14 +1257,19 @@ void qmp_set_link(const char *name, bool up, Error **errp)
 static void net_vm_change_state_handler(void *opaque, int running,
                                         RunState state)
 {
-    /* Complete all queued packets, to guarantee we don't modify
-     * state later when VM is not running.
-     */
-    if (!running) {
-        NetClientState *nc;
-        NetClientState *tmp;
+    NetClientState *nc;
+    NetClientState *tmp;
 
-        QTAILQ_FOREACH_SAFE(nc, &net_clients, next, tmp) {
+    QTAILQ_FOREACH_SAFE(nc, &net_clients, next, tmp) {
+        if (running) {
+            /* Flush queued packets and wake up backends. */
+            if (nc->peer && qemu_can_send_packet(nc)) {
+                qemu_flush_queued_packets(nc->peer);
+            }
+        } else {
+            /* Complete all queued packets, to guarantee we don't modify
+             * state later when VM is not running.
+             */
             qemu_flush_or_purge_queued_packets(nc, true);
         }
     }
