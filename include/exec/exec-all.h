1c3c8af1fb40a481c07749e0448644d9b7700415
Pavel Dovgalyuk <Pavel.Dovgaluk@ispras.ru>
2015-07-10 12:57:02 +0300

    cpu-exec: introduce loop exit with restore function
    This patch introduces loop exit function, which also
    restores guest CPU state according to the value of host
    program counter.
    
    Reviewed-by: Aurelien Jarno <aurelien@aurel32.net>
    Signed-off-by: Pavel Dovgalyuk <pavel.dovgaluk@ispras.ru>
    Message-Id: <20150710095702.13280.97477.stgit@PASHA-ISP>
    Signed-off-by: Richard Henderson <rth@twiddle.net>

--- include/exec/exec-all.h
+++ include/exec/exec-all.h
@@ -90,6 +90,7 @@ TranslationBlock *tb_gen_code(CPUState *cpu,
                               int cflags);
 void cpu_exec_init(CPUState *cpu, Error **errp);
 void QEMU_NORETURN cpu_loop_exit(CPUState *cpu);
+void QEMU_NORETURN cpu_loop_exit_restore(CPUState *cpu, uintptr_t pc);
 
 #if !defined(CONFIG_USER_ONLY)
 bool qemu_in_vcpu_thread(void);
