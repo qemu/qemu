d345ed2da3279b015605823397235b8c5ca5251f
Michael S. Tsirkin <mst@redhat.com>
2015-07-15 13:47:31 +0300

    Revert "vhost-user: add multi queue support"
    This reverts commit 830d70db692e374b55555f4407f96a1ceefdcc97.
    
    The interface isn't fully backwards-compatible, which is bad.
    Let's redo this properly after 2.4.
    
    Signed-off-by: Michael S. Tsirkin <mst@redhat.com>

--- hw/net/vhost_net.c
+++ hw/net/vhost_net.c
@@ -160,7 +160,6 @@ struct vhost_net *vhost_net_init(VhostNetOptions *options)
 
     net->dev.nvqs = 2;
     net->dev.vqs = net->vqs;
-    net->dev.vq_index = net->nc->queue_index;
 
     r = vhost_dev_init(&net->dev, options->opaque,
                        options->backend_type);
@@ -287,7 +286,7 @@ static void vhost_net_stop_one(struct vhost_net *net,
         for (file.index = 0; file.index < net->dev.nvqs; ++file.index) {
             const VhostOps *vhost_ops = net->dev.vhost_ops;
             int r = vhost_ops->vhost_call(&net->dev, VHOST_RESET_OWNER,
-                                          &file);
+                                          NULL);
             assert(r >= 0);
         }
     }
