279621c046ce57de0af9e3c00663b48d3a7835ae
Alberto Garcia <berto@igalia.com>
2015-08-04 15:14:40 +0300

    qcow2: add option to clean unused cache entries after some time
    This adds a new 'cache-clean-interval' option that cleans all qcow2
    cache entries that haven't been used in a certain interval, given in
    seconds.
    
    This allows setting a large L2 cache size so it can handle scenarios
    with lots of I/O and at the same time use little memory during periods
    of inactivity.
    
    This feature currently relies on MADV_DONTNEED to free that memory, so
    it is not useful in systems that don't follow that behavior.
    
    Signed-off-by: Alberto Garcia <berto@igalia.com>
    Reviewed-by: Max Reitz <mreitz@redhat.com>
    Message-id: a70d12da60433df9360ada648b3f34b8f6f354ce.1438690126.git.berto@igalia.com
    Signed-off-by: Max Reitz <mreitz@redhat.com>

--- block/qcow2-cache.c
+++ block/qcow2-cache.c
@@ -49,6 +49,7 @@ struct Qcow2Cache {
     bool                    depends_on_flush;
     void                   *table_array;
     uint64_t                lru_counter;
+    uint64_t                cache_clean_lru_counter;
 };
 
 static inline void *qcow2_cache_get_table_addr(BlockDriverState *bs,
@@ -82,6 +83,40 @@ static void qcow2_cache_table_release(BlockDriverState *bs, Qcow2Cache *c,
         qemu_madvise((uint8_t *) t + offset, length, QEMU_MADV_DONTNEED);
     }
 #endif
+}
+
+static inline bool can_clean_entry(Qcow2Cache *c, int i)
+{
+    Qcow2CachedTable *t = &c->entries[i];
+    return t->ref == 0 && !t->dirty && t->offset != 0 &&
+        t->lru_counter <= c->cache_clean_lru_counter;
+}
+
+void qcow2_cache_clean_unused(BlockDriverState *bs, Qcow2Cache *c)
+{
+    int i = 0;
+    while (i < c->size) {
+        int to_clean = 0;
+
+        /* Skip the entries that we don't need to clean */
+        while (i < c->size && !can_clean_entry(c, i)) {
+            i++;
+        }
+
+        /* And count how many we can clean in a row */
+        while (i < c->size && can_clean_entry(c, i)) {
+            c->entries[i].offset = 0;
+            c->entries[i].lru_counter = 0;
+            i++;
+            to_clean++;
+        }
+
+        if (to_clean > 0) {
+            qcow2_cache_table_release(bs, c, i - to_clean, to_clean);
+        }
+    }
+
+    c->cache_clean_lru_counter = c->lru_counter;
 }
 
 Qcow2Cache *qcow2_cache_create(BlockDriverState *bs, int num_tables)
