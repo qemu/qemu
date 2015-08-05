b08a48145cb1dfc5b0689ea21ea19384251ebd17
Mixac1 <mixac1@yandex.ru>
2015-08-05 15:02:19 +0400

    update rtc add via

--- hw/m68k/sy6522.c
+++ hw/m68k/sy6522.c
@@ -11,7 +11,6 @@
 #include "qemu/timer.h"
 #include "exec/address-spaces.h"
 #include "mac128k.h"
-
 #include "sysemu/sysemu.h"
 
 /* register offsets */
@@ -35,7 +34,26 @@ enum
     VIA_REGS = 16
 };
 
-#define V_OVERLAY_MASK (1 << 4)
+#define REGA_OVERLAY_MASK (1 << 4)
+#define REGB_RTCDATA_MASK (1 << 0)
+#define REGB_RTCCLK_MASK (1 << 1)
+#define REGB_RTCENB_MASK (1 << 2)
+#define REGB_RTCRWBIT_MASK (1 << 7)
+#define REGB_RTCSEC_MASK 0x0C
+#define REGB_RTCRAMBUF1_MASK 0x3C
+#define REGB_RTCRAMBUF2_MASK 0x0C
+
+typedef struct {
+    uint8_t count;
+    uint8_t rw_flag;
+    uint8_t cmd;
+    uint8_t param;
+    uint8_t sec_reg[4];  
+    uint8_t test_reg;
+    uint8_t wr_pr_reg;
+    uint8_t buf[20];
+    QEMUTimer *timer;
+} rtc_state;
 
 typedef struct {
     M68kCPU *cpu;
@@ -46,18 +64,7 @@ typedef struct {
     target_ulong base;
     /* registers */
     uint8_t regs[VIA_REGS];
-    uint8_t RWcount;
-    uint8_t RWflag;
-    uint8_t cmd;
-    uint8_t param;
-    uint8_t secReg0; 
-    uint8_t secReg1; 
-    uint8_t secReg2; 
-    uint8_t secReg3; 
-    uint8_t testReg;
-    uint8_t wrPrReg;
-    uint8_t rTCbuff[20];
-    QEMUTimer *timer;
+	rtc_state rtc;
 } via_state;
 
 static void via_set_regAbuf(via_state *s, uint8_t val)
@@ -65,11 +72,13 @@ static void via_set_regAbuf(via_state *s, uint8_t val)
     uint8_t old = s->regs[vBufA];
 
     /* Switch vOverlay bit */
-    if ((old & V_OVERLAY_MASK) != (val & V_OVERLAY_MASK)) {
-        if (val & V_OVERLAY_MASK) {
+    if ((old & REGA_OVERLAY_MASK) != (val & REGA_OVERLAY_MASK)) {
+        if (val & REGA_OVERLAY_MASK) {
             /* map ROM and RAM */
-            memory_region_add_subregion_overlap(get_system_memory(), 0x0, &s->rom, 1);
-            memory_region_add_subregion_overlap(get_system_memory(), 0x600000, &s->ram, 1);
+            memory_region_add_subregion_overlap(get_system_memory(), 
+                                                0x0, &s->rom, 1);
+            memory_region_add_subregion_overlap(get_system_memory(), 
+                                                0x600000, &s->ram, 1);
             qemu_log("Map ROM at 0x0\n");
         } else {
             /* unmap ROM and RAM */
@@ -79,6 +88,7 @@ static void via_set_regAbuf(via_state *s, uint8_t val)
         }
         tlb_flush(CPU(s->cpu), 1);
     }
+
     /* TODO: other bits */
 
     s->regs[vBufA] = val;
@@ -86,133 +96,111 @@ static void via_set_regAbuf(via_state *s, uint8_t val)
 
 static void via_set_regBdir(via_state *s, uint8_t val)
 {
+
+    /* TODO: other bits */
+	
     s->regs[vDirB] = val;
 }
 
-static void sender(via_state *s, uint8_t *val)
+static void rtc_sender(rtc_state *rtc, uint8_t *val)
 {
-    *val &= 0xFE;
-    *val |= (s->param >> (7 - s->RWcount) & 1);
-    s->RWcount++;
+    *val &= ~REGB_RTCDATA_MASK;
+    *val |= (rtc->param >> (7 - rtc->count++)) & REGB_RTCDATA_MASK;
 }
 
-static void reset_parametrs(via_state *s)
+static void rtc_param_reset(rtc_state *rtc)
 {
-    s->param = 0;
-    s->cmd = 0;
-    s->RWflag = 1;
-    s->RWcount = 8;
+    rtc->param = 0;
+    rtc->cmd = 0;
+    rtc->rw_flag = 0;
+    rtc->count = 0;
 }
 
-static void cmdHandlerW(via_state *s, uint8_t val)
+static void rtc_cmd_handler_w(via_state *s, uint8_t val)
 {
-    if (s->cmd == 0x35) {
-        s->wrPrReg = s->param;
+    if (s->rtc.cmd == 0x35) {
+        s->rtc.wr_pr_reg = s->rtc.param;
     }
-    else if(!(s->wrPrReg & 0x80)) {
-        switch (s->cmd) {
-        case 0x01:
-            s->secReg0 = s->param;
+    else if (!(s->rtc.wr_pr_reg & 0x80)) {
+        if ((s->rtc.cmd & ~REGB_RTCSEC_MASK) == 0x01) {
+            s->rtc.sec_reg[(s->rtc.cmd & REGB_RTCSEC_MASK) >> 2] = s->rtc.param;
             m68k_set_irq_level(s->cpu, 1, 0x64 >> 2);
-            break;
-        case 0x05:
-            s->secReg1 = s->param;
-            m68k_set_irq_level(s->cpu, 1, 0x64 >> 2);
-            break;
-        case 0x09:
-            s->secReg2 = s->param;
-            m68k_set_irq_level(s->cpu, 1, 0x64 >> 2);
-            break;
-        case 0x0D:
-            s->secReg3 = s->param;
-            m68k_set_irq_level(s->cpu, 1, 0x64 >> 2);
-            break;
-        case 0x31:
-            s->testReg = s->param;
-            break;  
-        default: 
-            if ((s->cmd >= 0x41) && (s->cmd <= 0x7D)) {
-                s->rTCbuff[(s->cmd >> 2) - (0x41 >> 2)] = s->param;
-            }
-            else if ((s->cmd >= 0x21) && (s->cmd <= 0x2D)) {
-                s->rTCbuff[16 + (s->cmd >> 2) - (0x21 >> 2)] = s->param;
-            }
+            timer_mod_ns(s->rtc.timer, qemu_clock_get_ns(rtc_clock) + get_ticks_per_sec());
+        } else if ((s->rtc.cmd & ~REGB_RTCRAMBUF1_MASK) == 0x41) {
+            s->rtc.buf[(s->rtc.cmd & REGB_RTCRAMBUF1_MASK) >> 2] = s->rtc.param;
+        } else if ((s->rtc.cmd & ~REGB_RTCRAMBUF2_MASK) == 0x21) {
+             s->rtc.buf[16 + ((s->rtc.cmd & REGB_RTCRAMBUF2_MASK) >> 2)] = s->rtc.param;
+        } else if (s->rtc.cmd == 0x31) {
+             s->rtc.test_reg = s->rtc.param;
+        } else {
+            qemu_log("rtc error: unknown command\n");
         }
-    }
+    } else {
+        qemu_log("rtc error: write protect enabled\n");
+    }            
 }
 
-static void cmdHandlerR(via_state *s, uint8_t val)
-{
-    switch (s->cmd) {
-    case 0x81:
-        s->param = s->secReg0;
+static void rtc_cmd_handler_r(via_state *s, uint8_t val)
+{             
+    if ((s->rtc.cmd & ~REGB_RTCSEC_MASK & ~REGB_RTCRWBIT_MASK) == 0x01) {
+        s->rtc.param = s->rtc.sec_reg[(s->rtc.cmd & REGB_RTCSEC_MASK) >> 2];
         m68k_set_irq_level(s->cpu, 0, 0x64 >> 2);
-        break;
-    case 0x85:
-        s->param = s->secReg1;
-        m68k_set_irq_level(s->cpu, 0, 0x64 >> 2);
-        break;
-    case 0x89:
-        s->param = s->secReg2;
-        m68k_set_irq_level(s->cpu, 0, 0x64 >> 2);
-        break;
-    case 0x8D:
-        s->param = s->secReg3;
-        m68k_set_irq_level(s->cpu, 0, 0x64 >> 2);
-        break;
-    default: 
-        if ((s->cmd >= 0xC1) && (s->cmd <= 0xFD)) {
-            s->param = s->rTCbuff[(s->cmd >> 2) - (0xC1 >> 2)];
-        }
-        else if ((s->cmd >= 0xA1) && (s->cmd <= 0xAD)) {
-            s->param = s->rTCbuff[16 + (s->cmd >> 2) - (0xA1 >> 2)];
-        }
+    } else if ((s->rtc.cmd & ~REGB_RTCRAMBUF1_MASK& ~REGB_RTCRWBIT_MASK) == 0x41) {
+        s->rtc.param = s->rtc.buf[(s->rtc.cmd & REGB_RTCRAMBUF1_MASK) >> 2];
+    } else if ((s->rtc.cmd & ~REGB_RTCRAMBUF2_MASK& ~REGB_RTCRWBIT_MASK) == 0x21) {
+        s->rtc.param = s->rtc.buf[16 + ((s->rtc.cmd & REGB_RTCRAMBUF2_MASK) >> 2)];
+    } else {
+        qemu_log("rtc error: unknown command\n");
     }
 }
 
 static void via_set_regBbuf(via_state *s, uint8_t val)
 {
-    if (!(val & 0x4) && (!(s->regs[vBufB] & 0x2)) && (val & 0x2)) {
-        if (!(s->RWflag & 0x2)) { 	
-            s->RWcount--;
-            if (s->RWflag & 0x1)
-                s->cmd |= (val & 1) << s->RWcount;
-            else
-                s->param |= (val & 1) << s->RWcount;		
-            if (s->RWcount == 0)
-                s->RWflag = s->RWflag | 2;
+    uint8_t old = s->regs[vBufB];
+
+    if (!(val & REGB_RTCENB_MASK)) {
+        if (!(old & REGB_RTCCLK_MASK) && (val & REGB_RTCCLK_MASK) && (s->regs[vDirB] & REGB_RTCDATA_MASK)) {	
+            if (!(s->rtc.rw_flag)) {
+                s->rtc.cmd |= (val & REGB_RTCDATA_MASK) << (7 - s->rtc.count);
+            } else {
+                s->rtc.param |= (val & REGB_RTCDATA_MASK) << (7 - s->rtc.count);	
+            }
+            s->rtc.count++; 
+            if (s->rtc.count == 8) {   
+                if (!(s->rtc.cmd & REGB_RTCRWBIT_MASK) && !(s->rtc.rw_flag)) { 
+                    s->rtc.rw_flag = 1; 
+                    s->rtc.count = 0;
+                } else if (s->rtc.rw_flag) {
+                    rtc_cmd_handler_w(s, val);
+                    rtc_param_reset(&s->rtc);
+                } else {
+                    rtc_cmd_handler_r(s, val);
+                    s->rtc.count = 0;
+                }
+            }
+        } else if ((old & REGB_RTCCLK_MASK) && !(val & REGB_RTCCLK_MASK) && !(s->regs[vDirB] & REGB_RTCDATA_MASK)) {
+            rtc_sender(&s->rtc, &val);
+            if (s->rtc.count == 8) {
+                rtc_param_reset(&s->rtc);
+            }
         }
-        if ((s->RWflag & 0x2) != 0) {   
-            if (!(s->cmd & 128) && (s->RWflag & 0x1)) { 
-                s->RWflag = s->RWflag & 0xFC; 
-                s->RWcount = 8;
-            }
-            else if (!(s->RWflag & 0x1)) { 
-                cmdHandlerW(s, val);
-                reset_parametrs(s);
-            }
-            else { 
-                if (s->RWcount == 0) 
-                    cmdHandlerR(s, val);
-                sender(s, &val);
-                if (s->RWcount > 8)
-                    reset_parametrs(s);
-            }
-        }
+    } else if ((val & REGB_RTCENB_MASK) && !(s->regs[vBufB] & REGB_RTCENB_MASK)) { 
+        rtc_param_reset(&s->rtc);
     }
-    else if ((val & 0x4) && !(s->regs[vBufB] & 0x4)) { 
-        s->regs[vBufB] = val;
-        reset_parametrs(s);
-    }
+
+    /* TODO: other bits */
+
     s->regs[vBufB] = val;
 }
 
-static void via_writeb(void *opaque, hwaddr offset, uint32_t value)
+static void via_writeb(void *opaque, hwaddr offset, 
+                              uint32_t value)
 {
     via_state *s = (via_state *)opaque;
     offset = (offset - (s->base & ~TARGET_PAGE_MASK)) >> 9;
-    if (offset >= VIA_REGS)
+    if (offset >= VIA_REGS) {
         hw_error("Bad VIA write offset 0x%x", (int)offset);
+    }
     qemu_log("via_write offset=0x%x value=0x%x\n", (int)offset, value); 
     switch (offset) {
     case vBufA:
@@ -232,8 +220,9 @@ static uint32_t via_readb(void *opaque, hwaddr offset)
     via_state *s = (via_state *)opaque;
     uint32_t ret = 0;
     offset = (offset - (s->base & ~TARGET_PAGE_MASK)) >> 9;
-    if (offset >= VIA_REGS)
+    if (offset >= VIA_REGS) {
         hw_error("Bad VIA read offset 0x%x", (int)offset);
+    }
     ret = s->regs[offset];       
     qemu_log("via_read offset=0x%x val=0x%x\n", (int)offset, ret);
     return ret;
@@ -255,57 +244,68 @@ static const MemoryRegionOps via_ops = {
     .endianness = DEVICE_NATIVE_ENDIAN,
 };
 
+static void rtc_interrupt(void * opaque)
+{
+    via_state *s = opaque;
+    timer_mod_ns(s->rtc.timer, qemu_clock_get_ns(rtc_clock) + get_ticks_per_sec());
+    if (s->rtc.sec_reg[0] == 0xFF) {
+        if (s->rtc.sec_reg[1] == 0xFF) {
+            if (s->rtc.sec_reg[2] == 0xFF) {
+                s->rtc.sec_reg[3]++;
+            }
+            s->rtc.sec_reg[2]++;
+        }
+        s->rtc.sec_reg[1]++;
+    }
+    s->rtc.sec_reg[0]++;
+    m68k_set_irq_level(s->cpu, 1, 0x64 >> 2);  
+}
+
+static void rtc_reset(rtc_state *rtc)
+{
+    uint64_t now = qemu_clock_get_ns(rtc_clock);
+    uint8_t i;
+    for (i = 0; i < 4; ++i)
+    rtc->sec_reg[i] = (now >> (32 + 8 * i)) & 0xFF; 
+    rtc->wr_pr_reg = 0x80;
+    timer_mod_ns(rtc->timer, now + get_ticks_per_sec()); 
+}
+
+static void rtc_init(via_state *s)
+{
+    s->rtc.timer = timer_new_ms(rtc_clock, rtc_interrupt, s);  
+    rtc_reset(&s->rtc);
+}
+
 static void sy6522_reset(void *opaque)
 {
     via_state *s = opaque;
     /* Init registers */
-    via_set_regAbuf(s, V_OVERLAY_MASK);
+    via_set_regAbuf(s, REGA_OVERLAY_MASK);
+    via_set_regBbuf(s, 0);
     via_set_regBdir(s, 0);
-    reset_parametrs(s);
+    rtc_param_reset(&s->rtc);
 }
 
-static void interrupt(void * opaque)
-{
-    via_state *s = opaque;
-    int64_t now = qemu_clock_get_ns(rtc_clock);
-    timer_mod_ns(s->timer, now + get_ticks_per_sec());
-    if (s->secReg0 == 0xFF) {
-        if (s->secReg1 == 0xFF) {
-            if (s->secReg2 == 0xFF) {
-                s->secReg3++;
-            }
-            s->secReg2++;
-        }
-        s->secReg1++;
-    }
-    s->secReg0++;
-    m68k_set_irq_level(s->cpu, 1, 0x64 >> 2);  
-}
-
-void sy6522_init(MemoryRegion *rom, MemoryRegion *ram, uint32_t base, M68kCPU *cpu)
+void sy6522_init(MemoryRegion *rom, MemoryRegion *ram, 
+                 uint32_t base, M68kCPU *cpu)
 {
     via_state *s;
-
     s = (via_state *)g_malloc0(sizeof(via_state));
 
     s->base = base;
     s->cpu = cpu;
-    memory_region_init_io(&s->iomem, NULL, &via_ops, s, "sy6522 via", 0x2000);
-    memory_region_add_subregion(get_system_memory(), base & TARGET_PAGE_MASK, &s->iomem);
+    memory_region_init_io(&s->iomem, NULL, &via_ops, s, 
+                          "sy6522 via", 0x2000);
+    memory_region_add_subregion(get_system_memory(), 
+                                base & TARGET_PAGE_MASK, &s->iomem);
     /* TODO: Magic! */
     memory_region_init_alias(&s->rom, NULL, "ROM overlay", rom, 0x0, 0x10000);
     memory_region_set_readonly(&s->rom, true);
     memory_region_init_alias(&s->ram, NULL, "RAM overlay", ram, 0x0, 0x20000);
 
+    rtc_init(s);
+
     qemu_register_reset(sy6522_reset, s);
     sy6522_reset(s);
-
-    s->secReg0 = 0; 
-    s->secReg1 = 0;
-    s->secReg2 = 0;
-    s->secReg3 = 0;
-    s->wrPrReg = 0x80;
-    int64_t now = qemu_clock_get_ns(rtc_clock);
-    s->timer = timer_new_ms(rtc_clock, interrupt, s);  
-    timer_mod_ns(s->timer, now + get_ticks_per_sec());
 }
