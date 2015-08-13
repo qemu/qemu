af6b40e66661c7d3338495f6e079c1fef66fb716
espes <espes@pequalsnp.com>
2015-08-14 09:19:52 +1000

    carbon-copy from NV2A_GL_DPRINTF when gl debugging not enabled

--- hw/xbox/nv2a_debug.h
+++ hw/xbox/nv2a_debug.h
@@ -49,7 +49,9 @@ void gl_debug_label(GLenum target, GLuint name, const char *fmt, ...);
     gl_debug_label(target, name, "nv2a: { " format " }", ## __VA_ARGS__)
 
 #else
-# define NV2A_GL_DPRINTF(cc, format, ...)          do { } while (0)
+# define NV2A_GL_DPRINTF(cc, format, ...)          do { \
+        if (cc) NV2A_DPRINTF(format "\n", ##__VA_ARGS__ ); \
+    } while (0)
 # define NV2A_GL_DGROUP_BEGIN(format, ...)         do { } while (0)
 # define NV2A_GL_DGROUP_END()                      do { } while (0)
 # define NV2A_GL_DLABEL(target, name, format, ...) do { } while (0)
