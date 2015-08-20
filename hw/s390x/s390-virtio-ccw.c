af62e639fcc190f09c51e8b73dc6492b30ae2111
Eduardo Habkost <ehabkost@redhat.com>
2015-08-20 14:54:33 -0700

    s390: Rename s390-ccw-virtio-2.4 class name to use MACHINE_TYPE_NAME
    Machine class names should use the "-machine" suffix to allow
    class-name-based machine class lookup to work. Rename the
    s390-ccw-virtio-2.4 machine class using the MACHINE_TYPE_NAME macro.
    
    Signed-off-by: Eduardo Habkost <ehabkost@redhat.com>
    Reviewed-by: David Gibson <david@gibson.dropbear.id.au>
    [AF/ehabkost: Updated for 2.5 machine]
    Signed-off-by: Andreas Färber <afaerber@suse.de>

--- hw/s390x/s390-virtio-ccw.c
+++ hw/s390x/s390-virtio-ccw.c
@@ -258,7 +258,7 @@ static void ccw_machine_2_4_class_init(ObjectClass *oc, void *data)
 }
 
 static const TypeInfo ccw_machine_2_4_info = {
-    .name          = TYPE_S390_CCW_MACHINE "2.4",
+    .name          = MACHINE_TYPE_NAME("s390-ccw-virtio-2.4"),
     .parent        = TYPE_S390_CCW_MACHINE,
     .class_init    = ccw_machine_2_4_class_init,
 };
@@ -274,7 +274,7 @@ static void ccw_machine_2_5_class_init(ObjectClass *oc, void *data)
 }
 
 static const TypeInfo ccw_machine_2_5_info = {
-    .name          = TYPE_S390_CCW_MACHINE "2.5",
+    .name          = MACHINE_TYPE_NAME("s390-ccw-virtio-2.5"),
     .parent        = TYPE_S390_CCW_MACHINE,
     .class_init    = ccw_machine_2_5_class_init,
 };
