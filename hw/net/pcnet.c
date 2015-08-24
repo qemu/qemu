c6acbe861f1ed4203f4864baf756686064ba561f
Laurent Vivier <lvivier@redhat.com>
2015-08-24 19:29:45 +0200

    pcnet: remove muldiv64()
    Originally, timers were ticks based, and it made sense to
    add ticks to current time to know when to trigger an alarm.
    
    But since commit:
    
    7447545 change all other clock references to use nanosecond resolution accessors
    
    All timers use nanoseconds and we need to convert ticks to nanoseconds, by
    doing something like:
    
        y = muldiv64(x, get_ticks_per_sec(), PCI_FREQUENCY)
    
    where x is the number of device ticks and y the number of system ticks.
    
    y is used as nanoseconds in timer functions,
    it works because 1 tick is 1 nanosecond.
    (get_ticks_per_sec() is 10^9)
    
    But as PCI frequency is 33 MHz, we can also do:
    
        y = x * 30; /* 33 MHz PCI period is 30 ns */
    
    Which is much more simple.
    
    This implies a 33.333333 MHz PCI frequency,
    but this is correct.
    
    Signed-off-by: Laurent Vivier <lvivier@redhat.com>
    Reviewed-by: Stefan Hajnoczi <stefanha@redhat.com>

--- hw/net/pcnet.c
+++ hw/net/pcnet.c
@@ -670,8 +670,7 @@ static inline hwaddr pcnet_rdra_addr(PCNetState *s, int idx)
 static inline int64_t pcnet_get_next_poll_time(PCNetState *s, int64_t current_time)
 {
     int64_t next_time = current_time +
-        muldiv64(65536 - (CSR_SPND(s) ? 0 : CSR_POLL(s)),
-                 get_ticks_per_sec(), 33000000L);
+                        (65536 - (CSR_SPND(s) ? 0 : CSR_POLL(s))) * 30;
     if (next_time <= current_time)
         next_time = current_time + 1;
     return next_time;
