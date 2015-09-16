381a290266df4161de71afc8a44e67ae66bde5be
Paolo Bonzini <pbonzini@redhat.com>
2015-09-16 17:38:44 +0200

    trace: remove malloc tracing
    The malloc vtable is not supported anymore in glib, because it broke
    when constructors called g_malloc.  Remove tracing of g_malloc,
    g_realloc and g_free calls.
    
    Note that, for systemtap users, glib also provides tracepoints
    glib.mem_alloc, glib.mem_free, glib.mem_realloc, glib.slice_alloc
    and glib.slice_free.
    
    Signed-off-by: Paolo Bonzini <pbonzini@redhat.com>
    Reviewed-by: Alberto Garcia <berto@igalia.com>
    Message-id: 1442417924-25831-1-git-send-email-pbonzini@redhat.com
    Signed-off-by: Stefan Hajnoczi <stefanha@redhat.com>
    (cherry picked from commit 98cf48f60aa4999f5b2808569a193a401a390e6a)
    Signed-off-by: Michael Roth <mdroth@linux.vnet.ibm.com>

--- vl.c
+++ vl.c
@@ -2719,26 +2719,6 @@ static const QEMUOption *lookup_opt(int argc, char **argv,
     return popt;
 }
 
-static gpointer malloc_and_trace(gsize n_bytes)
-{
-    void *ptr = malloc(n_bytes);
-    trace_g_malloc(n_bytes, ptr);
-    return ptr;
-}
-
-static gpointer realloc_and_trace(gpointer mem, gsize n_bytes)
-{
-    void *ptr = realloc(mem, n_bytes);
-    trace_g_realloc(mem, n_bytes, ptr);
-    return ptr;
-}
-
-static void free_and_trace(gpointer mem)
-{
-    trace_g_free(mem);
-    free(mem);
-}
-
 static int machine_set_property(void *opaque,
                                 const char *name, const char *value,
                                 Error **errp)
@@ -2966,11 +2946,6 @@ int main(int argc, char **argv, char **envp)
     bool userconfig = true;
     const char *log_mask = NULL;
     const char *log_file = NULL;
-    GMemVTable mem_trace = {
-        .malloc = malloc_and_trace,
-        .realloc = realloc_and_trace,
-        .free = free_and_trace,
-    };
     const char *trace_events = NULL;
     const char *trace_file = NULL;
     ram_addr_t maxram_size;
@@ -2985,8 +2960,6 @@ int main(int argc, char **argv, char **envp)
     atexit(qemu_run_exit_notifiers);
     error_set_progname(argv[0]);
     qemu_init_exec_dir(argv[0]);
-
-    g_mem_set_vtable(&mem_trace);
 
     module_call_init(MODULE_INIT_QOM);
 
