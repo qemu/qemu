7735fedaf490cf9213cd8d487272b69a4987c851
Bharata B Rao <bharata@linux.vnet.ibm.com>
2015-09-24 13:52:48 +0530

    spapr: Abort when HTAB of requested size isn't allocated
    Terminate the guest when HTAB of requested size isn't allocated by
    the host.
    
    When memory hotplug is attempted on a guest that has booted with
    less than requested HTAB size, the guest kernel will not be able
    to gracefully fail the hotplug request. This patch will ensure that
    we never end up in a situation where memory hotplug fails due to
    less than requested HTAB size.
    
    Signed-off-by: Bharata B Rao <bharata@linux.vnet.ibm.com>
    Signed-off-by: David Gibson <david@gibson.dropbear.id.au>

--- hw/ppc/spapr.c
+++ hw/ppc/spapr.c
@@ -992,6 +992,10 @@ static void spapr_alloc_htab(sPAPRMachineState *spapr)
 
     if (shift > 0) {
         /* Kernel handles htab, we don't need to allocate one */
+        if (shift != spapr->htab_shift) {
+            error_setg(&error_abort, "Failed to allocate HTAB of requested size, try with smaller maxmem");
+        }
+
         spapr->htab_shift = shift;
         kvmppc_kern_htab = true;
     } else {
@@ -1020,6 +1024,10 @@ static void spapr_reset_htab(sPAPRMachineState *spapr)
 
     shift = kvmppc_reset_htab(spapr->htab_shift);
     if (shift > 0) {
+        if (shift != spapr->htab_shift) {
+            error_setg(&error_abort, "Requested HTAB allocation failed during reset");
+        }
+
         /* Tell readers to update their file descriptor */
         if (spapr->htab_fd >= 0) {
             spapr->htab_fd_stale = true;
