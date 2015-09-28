798595075bf51c7e3125d260a19d860b9aa63e69
Eduardo Habkost <ehabkost@redhat.com>
2015-09-28 15:07:21 -0300

    pc: Add a comment explaining why pc_compat_2_4() doesn't exist
    pc_compat_2_4() doesn't exist, and we shouldn't create one. Add a
    comment explaining why the function doesn't exist and why pc_compat_*()
    functions are deprecated.
    
    Signed-off-by: Eduardo Habkost <ehabkost@redhat.com>
    Reviewed-by: Michael S. Tsirkin <mst@redhat.com>
    Signed-off-by: Michael S. Tsirkin <mst@redhat.com>

--- hw/i386/pc_piix.c
+++ hw/i386/pc_piix.c
@@ -301,6 +301,13 @@ static void pc_init1(MachineState *machine,
     }
 }
 
+/* Looking for a pc_compat_2_4() function? It doesn't exist.
+ * pc_compat_*() functions that run on machine-init time and
+ * change global QEMU state are deprecated. Please don't create
+ * one, and implement any pc-*-2.4 (and newer) compat code in
+ * HW_COMPAT_*, PC_COMPAT_*, or * pc_*_machine_options().
+ */
+
 static void pc_compat_2_3(MachineState *machine)
 {
     PCMachineState *pcms = PC_MACHINE(machine);
