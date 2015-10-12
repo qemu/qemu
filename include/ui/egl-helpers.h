cb9ab7caae77ce8a99ac0e75312f2428d322958b
Gerd Hoffmann <kraxel@redhat.com>
2015-10-12 11:59:39 +0200

    zap qemu_egl_has_ext in include/ui/egl-helpers.h
    Drop leftover prototype which sneaked in by mistake
    
    Signed-off-by: Gerd Hoffmann <kraxel@redhat.com>
    Reviewed-by: Marc-André Lureau <marcandre.lureau@redhat.com>

--- include/ui/egl-helpers.h
+++ include/ui/egl-helpers.h
@@ -11,6 +11,5 @@ EGLSurface qemu_egl_init_surface_x11(EGLContext ectx, Window win);
 
 int qemu_egl_init_dpy(EGLNativeDisplayType dpy, bool gles, bool debug);
 EGLContext qemu_egl_init_ctx(void);
-bool qemu_egl_has_ext(const char *haystack, const char *needle);
 
 #endif /* EGL_HELPERS_H */
