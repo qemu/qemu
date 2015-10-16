b64d64de1a5b88de88146e3ad36e7b09b97837eb
Peter Crosthwaite <crosthwaitepeter@gmail.com>
2015-10-16 11:14:53 +0100

    arm: imx25-pdk: Fix machine name
    ARM uses dashes instead of underscores for machine names. Fix imx25_pdk
    which has not seen a release yet (so there is no legacy yet).
    
    Cc: Jean-Christophe Dubois <jcd@tribudubois.net>
    Signed-off-by: Peter Crosthwaite <crosthwaite.peter@gmail.com>
    Message-id: 1444445785-3648-1-git-send-email-crosthwaite.peter@gmail.com
    Reviewed-by: Peter Maydell <peter.maydell@linaro.org>
    [PMM: Added change to tests/ds1338-test.c to use new machine name]
    Signed-off-by: Peter Maydell <peter.maydell@linaro.org>

--- tests/ds1338-test.c
+++ tests/ds1338-test.c
@@ -61,7 +61,7 @@ int main(int argc, char **argv)
 
     g_test_init(&argc, &argv, NULL);
 
-    s = qtest_start("-display none -machine imx25_pdk");
+    s = qtest_start("-display none -machine imx25-pdk");
     i2c = imx_i2c_create(IMX25_I2C_0_BASE);
     addr = DS1338_ADDR;
 
