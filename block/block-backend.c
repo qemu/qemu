f1f57066573e832438cd87600310589fa9cee202
Max Reitz <mreitz@redhat.com>
2015-10-26 21:39:14 +0100

    block: Inquire tray state before tray-moved events
    blk_dev_change_media_cb() is called for all potential tray movements;
    however, it is possible to request closing the tray but nothing actually
    happening (on a floppy disk drive without a medium).
    
    Thus, the actual tray status should be inquired before sending a
    tray-moved event (and an event should be sent whenever the status
    changed).
    
    Checking @load is now superfluous; it was necessary because it was
    possible to change a medium without having explicitly opened the tray
    and closed it again (or it might have been possible, at least). This is
    no longer possible, though.
    
    Signed-off-by: Max Reitz <mreitz@redhat.com>
    Reviewed-by: Eric Blake <eblake@redhat.com>
    Reviewed-by: Kevin Wolf <kwolf@redhat.com>
    Signed-off-by: Kevin Wolf <kwolf@redhat.com>

--- block/block-backend.c
+++ block/block-backend.c
@@ -429,18 +429,15 @@ void blk_set_dev_ops(BlockBackend *blk, const BlockDevOps *ops,
 void blk_dev_change_media_cb(BlockBackend *blk, bool load)
 {
     if (blk->dev_ops && blk->dev_ops->change_media_cb) {
-        bool tray_was_closed = !blk_dev_is_tray_open(blk);
+        bool tray_was_open, tray_is_open;
 
+        tray_was_open = blk_dev_is_tray_open(blk);
         blk->dev_ops->change_media_cb(blk->dev_opaque, load);
-        if (tray_was_closed) {
-            /* tray open */
-            qapi_event_send_device_tray_moved(blk_name(blk),
-                                              true, &error_abort);
-        }
-        if (load) {
-            /* tray close */
-            qapi_event_send_device_tray_moved(blk_name(blk),
-                                              false, &error_abort);
+        tray_is_open = blk_dev_is_tray_open(blk);
+
+        if (tray_was_open != tray_is_open) {
+            qapi_event_send_device_tray_moved(blk_name(blk), tray_is_open,
+                                              &error_abort);
         }
     }
 }
