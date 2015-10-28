aece5edc96f211eec6febdafc9bbbb99315a2efd
Alberto Garcia <berto@igalia.com>
2015-10-28 17:33:18 +0200

    block: Update copyright of the accounting code
    Signed-off-by: Alberto Garcia <berto@igalia.com>
    Message-id: 80a2278e3ec2dafd5daab20a7cb2d6a9b83371e4.1446044838.git.berto@igalia.com
    Signed-off-by: Stefan Hajnoczi <stefanha@redhat.com>
    Signed-off-by: Kevin Wolf <kwolf@redhat.com>

--- include/block/accounting.h
+++ include/block/accounting.h
@@ -2,6 +2,7 @@
  * QEMU System Emulator block accounting
  *
  * Copyright (c) 2011 Christoph Hellwig
+ * Copyright (c) 2015 Igalia, S.L.
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
