680a4783dc13f1059c03d11da58193d76c19ead6
Paolo Bonzini <pbonzini@redhat.com>
2015-11-02 09:23:52 +0100

    memory: call begin, log_start and commit when registering a new listener
    This ensures that cpu_reload_memory_map() is called as soon as
    tcg_cpu_address_space_init() is called, and before cpu->memory_dispatch
    is used.  qemu-system-s390x never changes the address spaces after
    tcg_cpu_address_space_init() is called, and thus tcg_commit() is never
    called.  This causes a SIGSEGV.
    
    Because memory_map_init() will now call mem_commit(), we have to
    initialize io_mem_* before address_space_memory and friends.
    
    Reported-by: Philipp Kern <pkern@debian.org>
    Reviewed-by: Peter Maydell <peter.maydell@linaro.org>
    Fixes: 0a1c71cec63e95f9b8d0dc96d049d2daa00c5210
    Signed-off-by: Paolo Bonzini <pbonzini@redhat.com>

--- exec.c
+++ exec.c
@@ -2694,8 +2694,8 @@ void cpu_register_map_client(QEMUBH *bh)
 void cpu_exec_init_all(void)
 {
     qemu_mutex_init(&ram_list.mutex);
-    memory_map_init();
     io_mem_init();
+    memory_map_init();
     qemu_mutex_init(&map_client_list_lock);
 }
 
