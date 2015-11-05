172dfd4faf2b64720db8a2dad7048056f2b81d75
Dr. David Alan Gilbert <dgilbert@redhat.com>
2015-11-05 18:10:30 +0000

    Move configuration section writing
    The vmstate_configuration is currently written
    in 'qemu_savevm_state_begin', move it to
    'qemu_savevm_state_header' since it's got a hard
    requirement that it must be the 1st thing after
    the header.
    (In postcopy some 'command' sections get sent
    early before the saving of the main sections
    and hence before qemu_savevm_state_begin).
    
    Signed-off-by: Dr. David Alan Gilbert <dgilbert@redhat.com>
    Reviewed-by: Amit Shah <amit.shah@redhat.com>
    Reviewed-by: Juan Quintela <quintela@redhat.com>
    Signed-off-by: Juan Quintela <quintela@redhat.com>

--- migration/savevm.c
+++ migration/savevm.c
@@ -713,6 +713,12 @@ void qemu_savevm_state_header(QEMUFile *f)
     trace_savevm_state_header();
     qemu_put_be32(f, QEMU_VM_FILE_MAGIC);
     qemu_put_be32(f, QEMU_VM_FILE_VERSION);
+
+    if (!savevm_state.skip_configuration) {
+        qemu_put_byte(f, QEMU_VM_CONFIGURATION);
+        vmstate_save_state(f, &vmstate_configuration, &savevm_state, 0);
+    }
+
 }
 
 void qemu_savevm_state_begin(QEMUFile *f,
@@ -727,11 +733,6 @@ void qemu_savevm_state_begin(QEMUFile *f,
             continue;
         }
         se->ops->set_params(params, se->opaque);
-    }
-
-    if (!savevm_state.skip_configuration) {
-        qemu_put_byte(f, QEMU_VM_CONFIGURATION);
-        vmstate_save_state(f, &vmstate_configuration, &savevm_state, 0);
     }
 
     QTAILQ_FOREACH(se, &savevm_state.handlers, entry) {
