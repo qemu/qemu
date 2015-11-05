382e1737d3467b76e8ade34b96afaae91509002e
Gerd Hoffmann <kraxel@redhat.com>
2015-11-05 10:12:18 +0100

    vnc: fix mismerge
    Commit "4d77b1f vnc: fix bug: vnc server can't start when 'to' is
    specified" was rebased incorrectly, fix it.
    
    Reported-by: Markus Armbruster <armbru@redhat.com>
    Signed-off-by: Gerd Hoffmann <kraxel@redhat.com>
    Reviewed-by: Markus Armbruster <armbru@redhat.com>
    Reviewed-by: Yang Hongyang <hongyang.yang@easystack.cn>
    Message-id: 1446714738-22400-1-git-send-email-kraxel@redhat.com

--- ui/vnc.c
+++ ui/vnc.c
@@ -3617,8 +3617,6 @@ void vnc_display_open(const char *id, Error **errp)
 
             if (to) {
                 saddr->u.inet->has_to = true;
-                saddr->u.inet->to = to;
-                saddr->u.inet->has_to = true;
                 saddr->u.inet->to = to + 5900;
             }
             saddr->u.inet->ipv4 = saddr->u.inet->has_ipv4 = has_ipv4;
