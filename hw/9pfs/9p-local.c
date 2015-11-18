f00d4f596b1c85cf9449ef4b65fca46148bcc9bb
Wei Liu <wei.liu2@citrix.com>
2015-11-18 18:05:29 +0000

    9pfs: rename virtio-9p-local.c to 9p-local.c
    This file is not virtio specific. Rename it to use generic name.
    
    Fix comment and remove unneeded inclusion of virtio.h.
    
    Signed-off-by: Wei Liu <wei.liu2@citrix.com>
    Signed-off-by: Aneesh Kumar K.V <aneesh.kumar@linux.vnet.ibm.com>

--- hw/9pfs/9p-local.c
+++ hw/9pfs/9p-local.c
@@ -1,5 +1,5 @@
 /*
- * Virtio 9p Posix callback
+ * 9p Posix callback
  *
  * Copyright IBM, Corp. 2010
  *
@@ -11,7 +11,6 @@
  *
  */
 
-#include "hw/virtio/virtio.h"
 #include "virtio-9p.h"
 #include "virtio-9p-xattr.h"
 #include "fsdev/qemu-fsdev.h"   /* local_ops */
