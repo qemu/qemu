1613094766602bdb8cae337ceecd8ab68f956197
Markus Armbruster <armbru@redhat.com>
2015-11-24 18:06:27 +0100

    tests/ivshmem-test: Supply missing initializer in get_device()
    If the device isn't found, the assertion uses dev without
    initialization.  Fix that.
    
    Signed-off-by: Markus Armbruster <armbru@redhat.com>
    Message-Id: <1448384789-14830-4-git-send-email-armbru@redhat.com>
    Reviewed-by: Marc-André Lureau <marcandre.lureau@redhat.com>

--- tests/ivshmem-test.c
+++ tests/ivshmem-test.c
@@ -40,6 +40,7 @@ static QPCIDevice *get_device(void)
     QPCIBus *pcibus;
 
     pcibus = qpci_init_pc();
+    dev = NULL;
     qpci_device_foreach(pcibus, 0x1af4, 0x1110, save_fn, &dev);
     g_assert(dev != NULL);
 
