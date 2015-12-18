04a3615860d693cbf5da015ee136a313628a42be
Max Reitz <mreitz@redhat.com>
2015-12-18 16:04:26 +0100

    vhdx: Simplify vhdx_set_shift_bits()
    For values which are powers of two (and we do assume all of these to
    be), sizeof(x) * 8 - 1 - clz(x) == ctz(x). Therefore, use ctz().
    
    Signed-off-by: Max Reitz <mreitz@redhat.com>
    Message-id: 1450451066-13335-3-git-send-email-mreitz@redhat.com
    Signed-off-by: Jeff Cody <jcody@redhat.com>

--- block/vhdx.c
+++ block/vhdx.c
@@ -264,10 +264,10 @@ static void vhdx_region_unregister_all(BDRVVHDXState *s)
 
 static void vhdx_set_shift_bits(BDRVVHDXState *s)
 {
-    s->logical_sector_size_bits = 31 - clz32(s->logical_sector_size);
-    s->sectors_per_block_bits =   31 - clz32(s->sectors_per_block);
-    s->chunk_ratio_bits =         63 - clz64(s->chunk_ratio);
-    s->block_size_bits =          31 - clz32(s->block_size);
+    s->logical_sector_size_bits = ctz32(s->logical_sector_size);
+    s->sectors_per_block_bits =   ctz32(s->sectors_per_block);
+    s->chunk_ratio_bits =         ctz64(s->chunk_ratio);
+    s->block_size_bits =          ctz32(s->block_size);
 }
 
 /*
