1d939a68afe1673441250b495ed2e5f9b568a2fb
Peter Maydell <peter.maydell@linaro.org>
2016-01-21 14:15:07 +0000

    hw/arm/virt: Wire up memory region to CPUs explicitly
    Wire up the system memory region to the CPUs explicitly
    by setting the QOM property. This doesn't change anything
    over letting it default, but will be needed for adding
    a secure memory region later.
    
    Signed-off-by: Peter Maydell <peter.maydell@linaro.org>
    Reviewed-by: Edgar E. Iglesias <edgar.iglesias@xilinx.com>
    Acked-by: Edgar E. Iglesias <edgar.iglesias@xilinx.com>

--- hw/arm/virt.c
+++ hw/arm/virt.c
@@ -1093,6 +1093,9 @@ static void machvirt_init(MachineState *machine)
                                     "reset-cbar", &error_abort);
         }
 
+        object_property_set_link(cpuobj, OBJECT(sysmem), "memory",
+                                 &error_abort);
+
         object_property_set_bool(cpuobj, true, "realized", NULL);
     }
     g_strfreev(cpustr);
