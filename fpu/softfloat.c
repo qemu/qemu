7ceac86f49b564954f5bde477c4281f407be1399
Aurelien Jarno <aurelien@aurel32.net>
2016-01-22 15:09:21 +0000

    softfloat: fix return type of roundAndPackFloat16
    The roundAndPackFloat16 function should return a float16 value, not a
    float32 one. Fix that.
    
    Cc: Peter Maydell <peter.maydell@linaro.org>
    Signed-off-by: Aurelien Jarno <aurelien@aurel32.net>
    Reviewed-by: Peter Maydell <peter.maydell@linaro.org>
    Message-id: 1452700993-6570-1-git-send-email-aurelien@aurel32.net
    Signed-off-by: Peter Maydell <peter.maydell@linaro.org>

--- fpu/softfloat.c
+++ fpu/softfloat.c
@@ -3368,7 +3368,7 @@ static float16 packFloat16(flag zSign, int_fast16_t zExp, uint16_t zSig)
 | Binary Floating-Point Arithmetic.
 *----------------------------------------------------------------------------*/
 
-static float32 roundAndPackFloat16(flag zSign, int_fast16_t zExp,
+static float16 roundAndPackFloat16(flag zSign, int_fast16_t zExp,
                                    uint32_t zSig, flag ieee,
                                    float_status *status)
 {
