6a49a71cc652431538ba7ed00379fbaab1e7343c
Max Reitz <mreitz@redhat.com>
2016-01-29 20:49:11 +0100

    blockdev: Fix 'change' for slot devices
    'change' and related operations did not work when used on guest devices
    featuring removable media but no actual tray, because
    blk_dev_is_tray_open() always returned false for them and the
    blockdev-{insert,remove}-medium commands required it to return true.
    
    Fix this by making blockdev-{insert,remove}-medium work on tray-less
    devices. Also, blockdev-{open,close}-tray are now explicitly no-ops when
    invoked on such devices, and blk_dev_change_media_cb() is instead
    called by blockdev-{insert,remove}-medium (for tray-less devices only).
    
    Reported-by: Peter Maydell <peter.maydell@linaro.org>
    Cc: qemu-stable <qemu-stable@nongnu.org>
    Signed-off-by: Max Reitz <mreitz@redhat.com>
    Reviewed-by: Alberto Garcia <berto@igalia.com>
    Message-id: 1454096953-31773-3-git-send-email-mreitz@redhat.com
    Reviewed-by: Eric Blake <eblake@redhat.com>
    (cherry picked from commit 12c7ec87a7d88919b23736176eba3118d1521372)
    Signed-off-by: Michael Roth <mdroth@linux.vnet.ibm.com>

--- blockdev.c
+++ blockdev.c
@@ -2306,6 +2306,11 @@ void qmp_blockdev_open_tray(const char *device, bool has_force, bool force,
         return;
     }
 
+    if (!blk_dev_has_tray(blk)) {
+        /* Ignore this command on tray-less devices */
+        return;
+    }
+
     if (blk_dev_is_tray_open(blk)) {
         return;
     }
@@ -2333,6 +2338,11 @@ void qmp_blockdev_close_tray(const char *device, Error **errp)
 
     if (!blk_dev_has_removable_media(blk)) {
         error_setg(errp, "Device '%s' is not removable", device);
+        return;
+    }
+
+    if (!blk_dev_has_tray(blk)) {
+        /* Ignore this command on tray-less devices */
         return;
     }
 
@@ -2365,7 +2375,7 @@ void qmp_x_blockdev_remove_medium(const char *device, Error **errp)
         return;
     }
 
-    if (has_device && !blk_dev_is_tray_open(blk)) {
+    if (has_device && blk_dev_has_tray(blk) && !blk_dev_is_tray_open(blk)) {
         error_setg(errp, "Tray of device '%s' is not open", device);
         return;
     }
@@ -2389,6 +2399,14 @@ void qmp_x_blockdev_remove_medium(const char *device, Error **errp)
     }
 
     blk_remove_bs(blk);
+
+    if (!blk_dev_has_tray(blk)) {
+        /* For tray-less devices, blockdev-open-tray is a no-op (or may not be
+         * called at all); therefore, the medium needs to be ejected here.
+         * Do it after blk_remove_bs() so blk_is_inserted(blk) returns the @load
+         * value passed here (i.e. false). */
+        blk_dev_change_media_cb(blk, false);
+    }
 
 out:
     aio_context_release(aio_context);
@@ -2415,7 +2433,7 @@ static void qmp_blockdev_insert_anon_medium(const char *device,
         return;
     }
 
-    if (has_device && !blk_dev_is_tray_open(blk)) {
+    if (has_device && blk_dev_has_tray(blk) && !blk_dev_is_tray_open(blk)) {
         error_setg(errp, "Tray of device '%s' is not open", device);
         return;
     }
@@ -2428,6 +2446,15 @@ static void qmp_blockdev_insert_anon_medium(const char *device,
     blk_insert_bs(blk, bs);
 
     QTAILQ_INSERT_TAIL(&bdrv_states, bs, device_list);
+
+    if (!blk_dev_has_tray(blk)) {
+        /* For tray-less devices, blockdev-close-tray is a no-op (or may not be
+         * called at all); therefore, the medium needs to be pushed into the
+         * slot here.
+         * Do it after blk_insert_bs() so blk_is_inserted(blk) returns the @load
+         * value passed here (i.e. true). */
+        blk_dev_change_media_cb(blk, true);
+    }
 }
 
 void qmp_x_blockdev_insert_medium(const char *device, const char *node_name,
