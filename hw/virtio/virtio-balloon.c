9dbb8fa7eff6e5e4cfd83ea26dc67f8404b84aa2
Eric Blake <eblake@redhat.com>
2016-01-29 06:48:45 -0700

    balloon: Improve use of qapi visitor
    Rework the control flow of balloon_stats_get_all() to make it
    easier for a later patch to split visit_end_struct().  Also
    switch to the uint64 visitor to match the data type.
    
    Signed-off-by: Eric Blake <eblake@redhat.com>
    Message-Id: <1454075341-13658-10-git-send-email-eblake@redhat.com>
    Signed-off-by: Markus Armbruster <armbru@redhat.com>

--- hw/virtio/virtio-balloon.c
+++ hw/virtio/virtio-balloon.c
@@ -133,9 +133,11 @@ static void balloon_stats_get_all(Object *obj, struct Visitor *v,
     if (err) {
         goto out_end;
     }
-    for (i = 0; !err && i < VIRTIO_BALLOON_S_NR; i++) {
-        visit_type_int64(v, (int64_t *) &s->stats[i], balloon_stat_names[i],
-                         &err);
+    for (i = 0; i < VIRTIO_BALLOON_S_NR; i++) {
+        visit_type_uint64(v, &s->stats[i], balloon_stat_names[i], &err);
+        if (err) {
+            break;
+        }
     }
     error_propagate(errp, err);
     err = NULL;
