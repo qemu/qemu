15d61692da651fc79b3fc40050b986c5a73055c0
Greg Kurz <gkurz@linux.vnet.ibm.com>
2016-02-05 09:33:26 +0100

    migration: fix bad string passed to error_report()
    state->name does not contain a terminating '\0' and you may get:
    
    Machine type received is 'pseries-2.3y�?' and local is 'pseries-2.4'
    load of migration failed: Invalid argument
    
    Let's add a precision modifier to fix this.
    
    Reviewed-by: Amit Shah <amit.shah@redhat.com>
    Signed-off-by: Greg Kurz <gkurz@linux.vnet.ibm.com>
    Message-Id: <20160205083201.2201.76109.stgit@bahia.huguette.org>
    Signed-off-by: Amit Shah <amit.shah@redhat.com>

--- migration/savevm.c
+++ migration/savevm.c
@@ -299,8 +299,8 @@ static int configuration_post_load(void *opaque, int version_id)
     const char *current_name = MACHINE_GET_CLASS(current_machine)->name;
 
     if (strncmp(state->name, current_name, state->len) != 0) {
-        error_report("Machine type received is '%s' and local is '%s'",
-                     state->name, current_name);
+        error_report("Machine type received is '%.*s' and local is '%s'",
+                     (int) state->len, state->name, current_name);
         return -EINVAL;
     }
     return 0;
