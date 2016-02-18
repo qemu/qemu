b527db44ad32c9520698ff5121a614e968326900
Wei Huang <wei@redhat.com>
2016-02-18 14:16:17 +0000

    ARM: PL061: Clear PL061 device state after reset
    Current QEMU doesn't clear PL061 state after reset. This causes a
    weird issue with guest reboot via GPIO. Here is the device state
    with two reboot requests:
    
      (PL061State fields)           data   old_in_data   istate
    VM boot                         0      0             0
    After 1st ACPI reboot request   8      8             8
    After VM PL061 driver ACK       8      8             0
    After VM reboot                 8      8             0
    ------------------------------------------------------------
    2nd ACPI reboot request         8
    
    In the second reboot request above, because the old_in_data field is 8,
    QEMU decides that there is a pending edge IRQ already (see
    pl061_update()) in input; so it doesn't raise up IRQ again. As a result
    the second reboot request is lost. The correct way is to clear PL061
    device state after reset.
    
    The default reset state is found from the documents listed below. Per
    Peter's suggestion that QEMU automatically calls reset function after
    device initialization, this patch removes calling pl061_reset() from
    pl061_initfn().
    
    Reference:
    [1] PL061 Technical Reference Manual
    [2] Stellaris LM3S8962 Microcontroller Data Sheet
    [3] Stellaris LM3S5P31 Microcontroller Data Sheet
    
    Signed-off-by: Wei Huang <wei@redhat.com>
    Message-id: 1455729552-28026-2-git-send-email-wei@redhat.com
    Reviewed-by: Peter Maydell <peter.maydell@linaro.org>
    Signed-off-by: Peter Maydell <peter.maydell@linaro.org>

--- hw/gpio/pl061.c
+++ hw/gpio/pl061.c
@@ -282,10 +282,32 @@ static void pl061_write(void *opaque, hwaddr offset,
     pl061_update(s);
 }
 
-static void pl061_reset(PL061State *s)
+static void pl061_reset(DeviceState *dev)
 {
-  s->locked = 1;
-  s->cr = 0xff;
+    PL061State *s = PL061(dev);
+
+    /* reset values from PL061 TRM, Stellaris LM3S5P31 & LM3S8962 Data Sheet */
+    s->data = 0;
+    s->old_out_data = 0;
+    s->old_in_data = 0;
+    s->dir = 0;
+    s->isense = 0;
+    s->ibe = 0;
+    s->iev = 0;
+    s->im = 0;
+    s->istate = 0;
+    s->afsel = 0;
+    s->dr2r = 0xff;
+    s->dr4r = 0;
+    s->dr8r = 0;
+    s->odr = 0;
+    s->pur = 0;
+    s->pdr = 0;
+    s->slr = 0;
+    s->den = 0;
+    s->locked = 1;
+    s->cr = 0xff;
+    s->amsel = 0;
 }
 
 static void pl061_set_irq(void * opaque, int irq, int level)
@@ -318,7 +340,7 @@ static int pl061_initfn(SysBusDevice *sbd)
     sysbus_init_irq(sbd, &s->irq);
     qdev_init_gpio_in(dev, pl061_set_irq, 8);
     qdev_init_gpio_out(dev, s->out, 8);
-    pl061_reset(s);
+
     return 0;
 }
 
@@ -343,6 +365,7 @@ static void pl061_class_init(ObjectClass *klass, void *data)
 
     k->init = pl061_initfn;
     dc->vmsd = &vmstate_pl061;
+    dc->reset = &pl061_reset;
 }
 
 static const TypeInfo pl061_info = {
