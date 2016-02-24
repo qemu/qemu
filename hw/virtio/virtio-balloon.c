a0d06486b445985b8d128df172daefbae205bffd
Denis V. Lunev <den@openvz.org>
2016-02-24 10:50:48 +0300

    virtio-balloon: add 'available' counter
    The patch for the kernel part is in linux-next already:
    commit ac88e7c908b920866e529862f2b2f0129b254ab2
        Author: Igor Redko <redkoi@virtuozzo.com>
        Date:   Thu Feb 18 09:23:01 2016 +1100
    
        virtio_balloon: export 'available' memory to balloon statistics
    
        Add a new field, VIRTIO_BALLOON_S_AVAIL, to virtio_balloon memory
        statistics protocol, corresponding to 'Available' in /proc/meminfo.
    
    Signed-off-by: Denis V. Lunev <den@openvz.org>
    CC: Igor Redko <redkoi@virtuozzo.com>
    CC: Michael S. Tsirkin <mst@redhat.com>
    Reviewed-by: Michael S. Tsirkin <mst@redhat.com>
    Signed-off-by: Michael S. Tsirkin <mst@redhat.com>

--- hw/virtio/virtio-balloon.c
+++ hw/virtio/virtio-balloon.c
@@ -53,6 +53,7 @@ static const char *balloon_stat_names[] = {
    [VIRTIO_BALLOON_S_MINFLT] = "stat-minor-faults",
    [VIRTIO_BALLOON_S_MEMFREE] = "stat-free-memory",
    [VIRTIO_BALLOON_S_MEMTOT] = "stat-total-memory",
+   [VIRTIO_BALLOON_S_AVAIL] = "stat-available-memory",
    [VIRTIO_BALLOON_S_NR] = NULL
 };
 
