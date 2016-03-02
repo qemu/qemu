d0ee85b4e4c6cc2c8fac311d6df2ed412ed0df5f
Prasad J Pandit <pjp@fedoraproject.org>
2016-03-02 17:29:58 +0530

    net: check packet payload length
    While computing IP checksum, 'net_checksum_calculate' reads
    payload length from the packet. It could exceed the given 'data'
    buffer size. Add a check to avoid it.
    
    Reported-by: Liu Ling <liuling-it@360.cn>
    Signed-off-by: Prasad J Pandit <pjp@fedoraproject.org>
    Signed-off-by: Jason Wang <jasowang@redhat.com>
    (cherry picked from commit 362786f14a753d8a5256ef97d7c10ed576d6572b)
    Signed-off-by: Michael Roth <mdroth@linux.vnet.ibm.com>

--- net/checksum.c
+++ net/checksum.c
@@ -59,6 +59,11 @@ void net_checksum_calculate(uint8_t *data, int length)
     int hlen, plen, proto, csum_offset;
     uint16_t csum;
 
+    /* Ensure data has complete L2 & L3 headers. */
+    if (length < 14 + 20) {
+        return;
+    }
+
     if ((data[14] & 0xf0) != 0x40)
 	return; /* not IPv4 */
     hlen  = (data[14] & 0x0f) * 4;
@@ -76,8 +81,9 @@ void net_checksum_calculate(uint8_t *data, int length)
 	return;
     }
 
-    if (plen < csum_offset+2)
-	return;
+    if (plen < csum_offset + 2 || 14 + hlen + plen > length) {
+        return;
+    }
 
     data[14+hlen+csum_offset]   = 0;
     data[14+hlen+csum_offset+1] = 0;
