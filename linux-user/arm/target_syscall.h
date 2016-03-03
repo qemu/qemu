167e4cdc29985e69769452fade698c5b9df78b3d
Peter Maydell <peter.maydell@linaro.org>
2016-03-03 12:11:18 +0000

    linux-user: arm: Remove ARM_cpsr and similar #defines
    The #defines of ARM_cpsr and friends in linux-user/arm/target-syscall.h
    can clash with versions in the system headers if building on an
    ARM or AArch64 build (though this seems to be dependent on the version
    of the system headers). The QEMU defines are not very useful (it's
    not clear that they're intended for use with the target_pt_regs struct
    rather than (say) the CPUARMState structure) and we only use them in one
    function in elfload.c anyway. So just remove the #defines and directly
    access regs->uregs[].
    
    Reported-by: Christopher Covington <cov@codeaurora.org>
    Tested-by: Christopher Covington <cov@codeaurora.org>
    Signed-off-by: Peter Maydell <peter.maydell@linaro.org>
    Signed-off-by: Riku Voipio <riku.voipio@linaro.org>

--- linux-user/arm/target_syscall.h
+++ linux-user/arm/target_syscall.h
@@ -4,28 +4,10 @@
 /* this struct defines the way the registers are stored on the
    stack during a system call. */
 
+/* uregs[0..15] are r0 to r15; uregs[16] is CPSR; uregs[17] is ORIG_r0 */
 struct target_pt_regs {
     abi_long uregs[18];
 };
-
-#define ARM_cpsr	uregs[16]
-#define ARM_pc		uregs[15]
-#define ARM_lr		uregs[14]
-#define ARM_sp		uregs[13]
-#define ARM_ip		uregs[12]
-#define ARM_fp		uregs[11]
-#define ARM_r10		uregs[10]
-#define ARM_r9		uregs[9]
-#define ARM_r8		uregs[8]
-#define ARM_r7		uregs[7]
-#define ARM_r6		uregs[6]
-#define ARM_r5		uregs[5]
-#define ARM_r4		uregs[4]
-#define ARM_r3		uregs[3]
-#define ARM_r2		uregs[2]
-#define ARM_r1		uregs[1]
-#define ARM_r0		uregs[0]
-#define ARM_ORIG_r0	uregs[17]
 
 #define ARM_SYSCALL_BASE	0x900000
 #define ARM_THUMB_SYSCALL	0
