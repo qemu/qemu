10f759079e616a1cc4701fc26ab5e2a048ce912c
Eric Blake <eblake@redhat.com>
2016-03-03 09:16:50 -0700

    qapi: Avoid use of 'data' member of QAPI unions
    QAPI code generators currently create a 'void *data' member as
    part of the anonymous union embedded in the C struct corresponding
    to a QAPI union.  However, directly assigning to this member of
    the union feels a bit fishy, when we can assign to another member
    of the struct instead.
    
    Signed-off-by: Eric Blake <eblake@redhat.com>
    Reviewed-by: Daniel P. Berrange <berrange@redhat.com>
    Message-Id: <1457021813-10704-9-git-send-email-eblake@redhat.com>
    Signed-off-by: Markus Armbruster <armbru@redhat.com>

--- ui/input.c
+++ ui/input.c
@@ -470,7 +470,7 @@ InputEvent *qemu_input_event_new_move(InputEventKind kind,
     InputMoveEvent *move = g_new0(InputMoveEvent, 1);
 
     evt->type = kind;
-    evt->u.data = move;
+    evt->u.rel = move; /* evt->u.rel is the same as evt->u.abs */
     move->axis = axis;
     move->value = value;
     return evt;
