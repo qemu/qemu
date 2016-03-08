c10c9d96158ce4d05f4325e64c0ce6a5fcd64b8b
Kevin Wolf <kwolf@redhat.com>
2016-03-08 16:39:49 +0100

    block: Introduce blk_set_allow_write_beyond_eof()
    We check that the guest can't write beyond the end of its disk, but for
    other internal users it can make sense to allow growing a file.
    
    Signed-off-by: Kevin Wolf <kwolf@redhat.com>

--- include/sysemu/block-backend.h
+++ include/sysemu/block-backend.h
@@ -78,6 +78,7 @@ void blk_insert_bs(BlockBackend *blk, BlockDriverState *bs);
 
 void blk_hide_on_behalf_of_hmp_drive_del(BlockBackend *blk);
 
+void blk_set_allow_write_beyond_eof(BlockBackend *blk, bool allow);
 void blk_iostatus_enable(BlockBackend *blk);
 bool blk_iostatus_is_enabled(const BlockBackend *blk);
 BlockDeviceIoStatus blk_iostatus(const BlockBackend *blk);
