294bbbb4252ab5ff42d0e2c09f209c0bd7eb9748
Daniel P. Berrange <berrange@redhat.com>
2016-03-08 15:37:29 +0000

    io: assert errors before asserting content in I/O test
    When checking the results of an I/O operation test, assert that
    the error objects are NULL before asserting on the content. This
    is found to give more useful indication of the problem when
    diagnosing test failures.
    
    Reviewed-by: Paolo Bonzini <pbonzini@redhat.com>
    Signed-off-by: Daniel P. Berrange <berrange@redhat.com>

--- tests/io-channel-helpers.c
+++ tests/io-channel-helpers.c
@@ -233,11 +233,11 @@ void qio_channel_test_run_reader(QIOChannelTest *test,
 
 void qio_channel_test_validate(QIOChannelTest *test)
 {
+    g_assert(test->readerr == NULL);
+    g_assert(test->writeerr == NULL);
     g_assert_cmpint(memcmp(test->input,
                            test->output,
                            test->len), ==, 0);
-    g_assert(test->readerr == NULL);
-    g_assert(test->writeerr == NULL);
 
     g_free(test->inputv);
     g_free(test->outputv);
