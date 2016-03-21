6a9c4ef452c98060e919aa49db49c09ed8c37745
Benjamin Herrenschmidt <benh@kernel.crashing.org>
2016-03-21 13:52:36 +0100

    ppc: Initialize AMOR in PAPR mode
    Make sure we give the guest full authorization
    
    Signed-off-by: Benjamin Herrenschmidt <benh@kernel.crashing.org>
    Reviewed-by: Thomas Huth <thuth@redhat.com>
    Reviewed-by: David Gibson <david@gibson.dropbear.id.au>
    Signed-off-by: David Gibson <david@gibson.dropbear.id.au>

--- target-ppc/translate_init.c
+++ target-ppc/translate_init.c
@@ -8398,6 +8398,7 @@ POWERPC_FAMILY(POWER8)(ObjectClass *oc, void *data)
 void cpu_ppc_set_papr(PowerPCCPU *cpu)
 {
     CPUPPCState *env = &cpu->env;
+    ppc_spr_t *amor = &env->spr_cb[SPR_AMOR];
 
     /* PAPR always has exception vectors in RAM not ROM. To ensure this,
      * MSR[IP] should never be set.
@@ -8405,6 +8406,9 @@ void cpu_ppc_set_papr(PowerPCCPU *cpu)
      * We also disallow setting of MSR_HV
      */
     env->msr_mask &= ~((1ull << MSR_EP) | MSR_HVB);
+
+    /* Set a full AMOR so guest can use the AMR as it sees fit */
+    env->spr[SPR_AMOR] = amor->default_value = 0xffffffffffffffffull;
 
     /* Tell KVM that we're in PAPR mode */
     if (kvm_enabled()) {
