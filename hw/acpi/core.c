1beb99f787ba110a9de44254e7d62a1cb9117de8
Gerd Hoffmann <kraxel@redhat.com>
2016-04-15 08:43:29 +0200

    Revert "acpi: mark PMTIMER as unlocked"
    This reverts commit 7070e085d490c396f9237c8f10bf8b6e69cd0066.
    
    Commit message claims locking is not needed, but that appears
    to not be true, seabios ehci driver runs into timekeeping problems
    with this, see
    	https://bugzilla.redhat.com/show_bug.cgi?id=1322713
    
    Signed-off-by: Gerd Hoffmann <kraxel@redhat.com>
    Message-id: 1460702609-25971-1-git-send-email-kraxel@redhat.com
    Signed-off-by: Peter Maydell <peter.maydell@linaro.org>

--- hw/acpi/core.c
+++ hw/acpi/core.c
@@ -536,7 +536,6 @@ void acpi_pm_tmr_init(ACPIREGS *ar, acpi_update_sci_fn update_sci,
     ar->tmr.timer = timer_new_ns(QEMU_CLOCK_VIRTUAL, acpi_pm_tmr_timer, ar);
     memory_region_init_io(&ar->tmr.io, memory_region_owner(parent),
                           &acpi_pm_tmr_ops, ar, "acpi-tmr", 4);
-    memory_region_clear_global_locking(&ar->tmr.io);
     memory_region_add_subregion(parent, 8, &ar->tmr.io);
 }
 
