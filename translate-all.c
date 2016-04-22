835154b6e2200460f04719d0028716a37c178368
Emilio G. Cota <cota@braap.org>
2016-04-21 20:01:53 -0400

    translate-all: remove redundant setting of tcg_ctx.code_gen_buffer_size
    The setting of tcg_ctx.code_gen_buffer_size is done by the only caller of
    size_code_gen_buffer(), which is code_gen_alloc():
    
      $ git grep size_code_gen_buffer
      translate-all.c:static inline size_t size_code_gen_buffer(size_t tb_size)
      translate-all.c:    tcg_ctx.code_gen_buffer_size = size_code_gen_buffer(tb_size);
    
    Signed-off-by: Emilio G. Cota <cota@braap.org>
    Message-Id: <1461283314-2353-1-git-send-email-cota@braap.org>
    Signed-off-by: Richard Henderson <rth@twiddle.net>

--- translate-all.c
+++ translate-all.c
@@ -507,7 +507,6 @@ static inline size_t size_code_gen_buffer(size_t tb_size)
     if (tb_size > MAX_CODE_GEN_BUFFER_SIZE) {
         tb_size = MAX_CODE_GEN_BUFFER_SIZE;
     }
-    tcg_ctx.code_gen_buffer_size = tb_size;
     return tb_size;
 }
 
