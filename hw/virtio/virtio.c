14560d69e7c979d97975c3aa6e7bd1ab3249fe88
Fam Zheng <famz@redhat.com>
2016-04-22 21:53:54 +0800

    virtio: Mark host notifiers as external
    The effect of this change is the block layer drained section can work,
    for example when mirror job is being completed.
    
    Signed-off-by: Fam Zheng <famz@redhat.com>
    Reviewed-by: Michael S. Tsirkin <mst@redhat.com>
    Signed-off-by: Kevin Wolf <kwolf@redhat.com>

--- hw/virtio/virtio.c
+++ hw/virtio/virtio.c
@@ -1829,10 +1829,10 @@ void virtio_queue_set_host_notifier_fd_handler(VirtQueue *vq, bool assign,
                                                bool set_handler)
 {
     if (assign && set_handler) {
-        event_notifier_set_handler(&vq->host_notifier, false,
+        event_notifier_set_handler(&vq->host_notifier, true,
                                    virtio_queue_host_notifier_read);
     } else {
-        event_notifier_set_handler(&vq->host_notifier, false, NULL);
+        event_notifier_set_handler(&vq->host_notifier, true, NULL);
     }
     if (!assign) {
         /* Test and clear notifier before after disabling event,
