eded5bac3b2fa51f6165030b0e8ed718247af188
Alexey Kardashevskiy <aik@ozlabs.ru>
2016-05-04 16:52:18 +1000

    spapr_pci: Use correct DMA LIOBN when composing the device tree
    The user could have picked LIOBN via the CLI but the device tree
    rendering code would still use the value derived from the PHB index
    (which is the default fallback if LIOBN is not set in the CLI).
    
    This replaces SPAPR_PCI_LIOBN() with the actual DMA LIOBN value.
    
    Signed-off-by: Alexey Kardashevskiy <aik@ozlabs.ru>
    Signed-off-by: David Gibson <david@gibson.dropbear.id.au>

--- hw/ppc/spapr_pci.c
+++ hw/ppc/spapr_pci.c
@@ -1814,7 +1814,7 @@ int spapr_populate_pci_dt(sPAPRPHBState *phb,
     _FDT(fdt_setprop(fdt, bus_off, "interrupt-map", &interrupt_map,
                      sizeof(interrupt_map)));
 
-    tcet = spapr_tce_find_by_liobn(SPAPR_PCI_LIOBN(phb->index, 0));
+    tcet = spapr_tce_find_by_liobn(phb->dma_liobn);
     if (!tcet) {
         return -1;
     }
