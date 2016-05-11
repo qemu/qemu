c9e9e9c66cee9932fb28a41a4659aa421a7a3f78
Fam Zheng <famz@redhat.com>
2016-05-11 10:45:34 +0800

    block: Drop superfluous invalidating bs->file from drivers
    Now they are invalidated by the block layer, so it's not necessary to
    do this in block drivers' implementations of .bdrv_invalidate_cache.
    
    Signed-off-by: Fam Zheng <famz@redhat.com>
    Reviewed-by: Alberto Garcia <berto@igalia.com>
    Signed-off-by: Kevin Wolf <kwolf@redhat.com>

--- block/qcow2.c
+++ block/qcow2.c
@@ -1757,13 +1757,6 @@ static void qcow2_invalidate_cache(BlockDriverState *bs, Error **errp)
 
     qcow2_close(bs);
 
-    bdrv_invalidate_cache(bs->file->bs, &local_err);
-    if (local_err) {
-        error_propagate(errp, local_err);
-        bs->drv = NULL;
-        return;
-    }
-
     memset(s, 0, sizeof(BDRVQcow2State));
     options = qdict_clone_shallow(bs->options);
 
