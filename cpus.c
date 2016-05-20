1453e6627d19a8d6d54480c6980f5cef5dfc6833
Bandan Das <bsd@redhat.com>
2016-05-20 12:28:37 -0400

    cpus: call the core nmi injection function
    We can call the common function here directly since
    x86 specific actions will be taken care of by the arch
    specific nmi handler
    
    Signed-off-by: Bandan Das <bsd@redhat.com>
    Message-Id: <1463761717-26558-4-git-send-email-bsd@redhat.com>
    Signed-off-by: Paolo Bonzini <pbonzini@redhat.com>

--- cpus.c
+++ cpus.c
@@ -1693,21 +1693,7 @@ exit:
 
 void qmp_inject_nmi(Error **errp)
 {
-#if defined(TARGET_I386)
-    CPUState *cs;
-
-    CPU_FOREACH(cs) {
-        X86CPU *cpu = X86_CPU(cs);
-
-        if (!cpu->apic_state) {
-            cpu_interrupt(cs, CPU_INTERRUPT_NMI);
-        } else {
-            apic_deliver_nmi(cpu->apic_state);
-        }
-    }
-#else
     nmi_monitor_handle(monitor_get_cpu_index(), errp);
-#endif
 }
 
 void dump_drift_info(FILE *f, fprintf_function cpu_fprintf)
