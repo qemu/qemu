c9fb07ba56ae4d8f4eaa6e461179017624b23355
Fam Zheng <famz@redhat.com>
2016-05-26 14:15:05 +0800

    scsi-generic: Merge block max xfer len in INQUIRY response
    The rationale is similar to the above mode sense response interception:
    this is practically the only channel to communicate restraints from
    elsewhere such as host and block driver.
    
    The scsi bus we attach onto can have a larger max xfer len than what is
    accepted by the host file system (guarding between the host scsi LUN and
    QEMU), in which case the SG_IO we generate would get -EINVAL.
    
    Signed-off-by: Fam Zheng <famz@redhat.com>
    Message-Id: <1464243305-10661-3-git-send-email-famz@redhat.com>
    Signed-off-by: Paolo Bonzini <pbonzini@redhat.com>
    (cherry picked from commit 063143d5b1fde0fdcbae30bc7d6d14e76fa607d2)
    Signed-off-by: Michael Roth <mdroth@linux.vnet.ibm.com>

--- hw/scsi/scsi-generic.c
+++ hw/scsi/scsi-generic.c
@@ -222,6 +222,18 @@ static void scsi_read_complete(void * opaque, int ret)
             r->buf[3] |= 0x80;
         }
     }
+    if (s->type == TYPE_DISK &&
+        r->req.cmd.buf[0] == INQUIRY &&
+        r->req.cmd.buf[2] == 0xb0) {
+        uint32_t max_xfer_len = blk_get_max_transfer_length(s->conf.blk);
+        if (max_xfer_len) {
+            stl_be_p(&r->buf[8], max_xfer_len);
+            /* Also take care of the opt xfer len. */
+            if (ldl_be_p(&r->buf[12]) > max_xfer_len) {
+                stl_be_p(&r->buf[12], max_xfer_len);
+            }
+        }
+    }
     scsi_req_data(&r->req, len);
     scsi_req_unref(&r->req);
 }
