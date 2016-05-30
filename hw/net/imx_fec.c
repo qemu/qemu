ccdb81d3274d281d770703417257bd40bcdf4c0e
Jean-Christophe Dubois <jcd@tribudubois.net>
2016-05-30 19:25:56 +0200

    i.MX: Fix FEC code for ECR register reset value.
    According to the FEC chapter of i.MX25 reference manual ECR register is
    initialized at 0xf0000000 at reset time.
    
    We fix the value.
    
    Signed-off-by: Jean-Christophe Dubois <jcd@tribudubois.net>
    Signed-off-by: Jason Wang <jasowang@redhat.com>

--- hw/net/imx_fec.c
+++ hw/net/imx_fec.c
@@ -339,7 +339,7 @@ static void imx_fec_reset(DeviceState *d)
     s->eir = 0;
     s->eimr = 0;
     s->rx_enabled = 0;
-    s->ecr = 0;
+    s->ecr = 0xf0000000;
     s->mscr = 0;
     s->mibc = 0xc0000000;
     s->rcr = 0x05ee0001;
