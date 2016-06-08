4d5942332f42a17c9ebbf67ef335ec72fcd789ff
Olaf Hering <olaf@aepfle.de>
2016-06-08 21:43:52 +0000

    gtk: fix vte version check
    vte_terminal_set_encoding takes 3 args since 0.38.0.
    This fixes commit fba958c6 ("gtk: implement set_echo")
    
    Signed-off-by: Olaf Hering <olaf@aepfle.de>
    Message-id: 20160608214352.32669-1-olaf@aepfle.de
    Signed-off-by: Gerd Hoffmann <kraxel@redhat.com>

--- ui/gtk.c
+++ ui/gtk.c
@@ -1748,7 +1748,7 @@ static GSList *gd_vc_vte_init(GtkDisplayState *s, VirtualConsole *vc,
     /* The documentation says that the default is UTF-8, but actually it is
      * 7-bit ASCII at least in VTE 0.38.
      */
-#if VTE_CHECK_VERSION(0, 40, 0)
+#if VTE_CHECK_VERSION(0, 38, 0)
     vte_terminal_set_encoding(VTE_TERMINAL(vc->vte.terminal), "UTF-8", NULL);
 #else
     vte_terminal_set_encoding(VTE_TERMINAL(vc->vte.terminal), "UTF-8");
