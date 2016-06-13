ba4537805da7023ae48cef0cdc06e6724156c596
Peter Maydell <peter.maydell@linaro.org>
2016-06-13 11:57:18 +0100

    linux-user: Create a hostdep.h for each host architecture
    In commit 4d330cee37a21 a new hostdep.h file was added, with the intent
    that host architectures which needed one could provide it, and the
    build system would automatically fall back to a generic version if
    there was no version for the host architecture. Although this works,
    it has a flaw: if a subsequent commit switches an architecture from
    "uses generic/hostdep.h" to "uses its own hostdep.h" nothing in the
    makefile dependencies notices this and so doing a rebuild without
    a manual 'make clean' will fail.
    
    So we drop the idea of having a 'generic' version in favour of
    every architecture we support having its own hostdep.h, even if
    it doesn't have anything in it. (There are only thirteen of these.)
    
    If the dependency files claim that an object file depends on a
    nonexistent file, our dependency system means that make will
    rebuild the object file, and regenerate the dependencies in
    the process. So moving between trees prior to this commit and
    trees after this commit works without requiring a 'make clean'.
    
    Signed-off-by: Peter Maydell <peter.maydell@linaro.org>
    Reviewed-by: Laurent Vivier <laurent@vivier.eu>
    Reviewed-by: Richard Henderson <rth@twiddle.net>
    Signed-off-by: Riku Voipio <riku.voipio@linaro.org>

---
+++ linux-user/host/sparc64/hostdep.h
@@ -0,0 +1,15 @@
+/*
+ * hostdep.h : things which are dependent on the host architecture
+ *
+ *  * Written by Peter Maydell <peter.maydell@linaro.org>
+ *
+ * Copyright (C) 2016 Linaro Limited
+ *
+ * This work is licensed under the terms of the GNU GPL, version 2 or later.
+ * See the COPYING file in the top-level directory.
+ */
+
+#ifndef QEMU_HOSTDEP_H
+#define QEMU_HOSTDEP_H
+
+#endif
