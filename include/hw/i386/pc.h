d812b3d68ddf0efe91a088ecc8b177865b0bab8d
Efimov Vasily <real@ispras.ru>
2016-06-22 15:24:52 +0300

    port92: handle A20 IRQ as GPIO
    The port92 device has outgouing IRQ line A20. Currently the IRQ is referenced
    by a pointer which normally is set during machine initialization. The
    pointer is never changed at runtime. Hence, common GPIO model can be applied
    to A20 IRQ line. Note that checking for IRQ to be connected as in
    previous version of code is not required qemu_set_irq will do it.
    
    Signed-off-by: Efimov Vasily <real@ispras.ru>
    Signed-off-by: Paolo Bonzini <pbonzini@redhat.com>

--- include/hw/i386/pc.h
+++ include/hw/i386/pc.h
@@ -277,6 +277,8 @@ int cmos_get_fd_drive_type(FloppyDriveType fd0);
 
 #define FW_CFG_IO_BASE     0x510
 
+#define PORT92_A20_LINE "a20"
+
 /* acpi_piix.c */
 
 I2CBus *piix4_pm_init(PCIBus *bus, int devfn, uint32_t smb_io_base,
