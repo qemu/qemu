f11235b92065e06e789ee1b523dd71999bc6b3e6
Greg Kurz <groug@kaod.org>
2016-06-29 22:50:32 +0200

    spapr: do proper error propagation in spapr_cpu_core_realize_child()
    This patch changes spapr_cpu_core_realize_child() to have a local error
    pointer and use error_propagate() as it is supposed to be done.
    
    Signed-off-by: Greg Kurz <groug@kaod.org>
    Reviewed-by: Bharata B Rao <bharata@linux.vnet.ibm.com>
    Signed-off-by: David Gibson <david@gibson.dropbear.id.au>

--- hw/ppc/spapr_cpu_core.c
+++ hw/ppc/spapr_cpu_core.c
@@ -262,18 +262,20 @@ out:
 
 static int spapr_cpu_core_realize_child(Object *child, void *opaque)
 {
-    Error **errp = opaque;
+    Error **errp = opaque, *local_err = NULL;
     sPAPRMachineState *spapr = SPAPR_MACHINE(qdev_get_machine());
     CPUState *cs = CPU(child);
     PowerPCCPU *cpu = POWERPC_CPU(cs);
 
-    object_property_set_bool(child, true, "realized", errp);
-    if (*errp) {
+    object_property_set_bool(child, true, "realized", &local_err);
+    if (local_err) {
+        error_propagate(errp, local_err);
         return 1;
     }
 
-    spapr_cpu_init(spapr, cpu, errp);
-    if (*errp) {
+    spapr_cpu_init(spapr, cpu, &local_err);
+    if (local_err) {
+        error_propagate(errp, local_err);
         return 1;
     }
     return 0;
