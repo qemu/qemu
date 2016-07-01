4715856a68b3bf868aca05f34f8f9edde64d7d6f
Peter Maydell <peter.maydell@linaro.org>
2016-07-01 19:46:04 +0100

    linux-user: Add some new blk ioctls
    Add some new blk ioctls (these are 0x12,119 through
    to 0x12,127). Several of these are used by mke2fs; this silences
    the warnings:
    
    mke2fs 1.42.12 (29-Aug-2014)
    Unsupported ioctl: cmd=0x127b
    Unsupported ioctl: cmd=0x127a
    warning: Unable to get device geometry for /dev/loop5
    Unsupported ioctl: cmd=0x127c
    Unsupported ioctl: cmd=0x127c
    Unsupported ioctl: cmd=0x1277
    
    Signed-off-by: Peter Maydell <peter.maydell@linaro.org>
    Signed-off-by: Riku Voipio <riku.voipio@linaro.org>

--- linux-user/syscall_defs.h
+++ linux-user/syscall_defs.h
@@ -985,6 +985,17 @@ struct target_pollfd {
 #define TARGET_BLKGETSIZE64 TARGET_IOR(0x12,114,abi_ulong)
                                              /* return device size in bytes
                                                 (u64 *arg) */
+
+#define TARGET_BLKDISCARD TARGET_IO(0x12, 119)
+#define TARGET_BLKIOMIN TARGET_IO(0x12, 120)
+#define TARGET_BLKIOOPT TARGET_IO(0x12, 121)
+#define TARGET_BLKALIGNOFF TARGET_IO(0x12, 122)
+#define TARGET_BLKPBSZGET TARGET_IO(0x12, 123)
+#define TARGET_BLKDISCARDZEROES TARGET_IO(0x12, 124)
+#define TARGET_BLKSECDISCARD TARGET_IO(0x12, 125)
+#define TARGET_BLKROTATIONAL TARGET_IO(0x12, 126)
+#define TARGET_BLKZEROOUT TARGET_IO(0x12, 127)
+
 #define TARGET_FIBMAP     TARGET_IO(0x00,1)  /* bmap access */
 #define TARGET_FIGETBSZ   TARGET_IO(0x00,2)  /* get the block size used for bmap */
 #define TARGET_FS_IOC_FIEMAP TARGET_IOWR('f',11,struct fiemap)
