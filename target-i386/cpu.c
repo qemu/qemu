6816b1b3811e839540df22855d975b6d76ae438b
Igor Mammedov <imammedo@redhat.com>
2016-07-06 08:20:52 +0200

    target-i386: cpu: Do not ignore error and fix apic parent
    object_property_add_child() silently fails with error that it can't
    create duplicate propery 'apic' as we already have 'apic' property
    registered for 'apic' feature. As result generic device_realize puts
    apic into unattached container.
    
    As it's programming error, abort if name collision happens in future
    and fix property name for apic_state to 'lapic', this way apic is
    a child of cpu instance.
    
    Signed-off-by: Igor Mammedov <imammedo@redhat.com>
    Reviewed-by: Eduardo Habkost <ehabkost@redhat.com>
    Signed-off-by: Eduardo Habkost <ehabkost@redhat.com>

--- target-i386/cpu.c
+++ target-i386/cpu.c
@@ -2826,8 +2826,9 @@ static void x86_cpu_apic_create(X86CPU *cpu, Error **errp)
 
     cpu->apic_state = DEVICE(object_new(apic_type));
 
-    object_property_add_child(OBJECT(cpu), "apic",
-                              OBJECT(cpu->apic_state), NULL);
+    object_property_add_child(OBJECT(cpu), "lapic",
+                              OBJECT(cpu->apic_state), &error_abort);
+
     qdev_prop_set_uint8(cpu->apic_state, "id", cpu->apic_id);
     /* TODO: convert to link<> */
     apic = APIC_COMMON(cpu->apic_state);
