90e26f5aacd265257f7de58ee59f72dc36dff40e
Peter Maydell <peter.maydell@linaro.org>
2016-07-07 13:47:00 +0100

    target-arm/arm-semi.c: In SYS_HEAPINFO use correct type for 'limit'
    In commit f5666418c4 most of the SYS_HEAPINFO implementation was
    fixed to use target_ulong rather than uint32_t, but the 'limit'
    variable was not changed.
    
    Reported-by: Laurent Desnogues <laurent.desnogues@gmail.com>
    Reviewed-by: Laurent Desnogues <laurent.desnogues@gmail.com>
    Signed-off-by: Peter Maydell <peter.maydell@linaro.org>
    Message-id: 1467650942-28706-1-git-send-email-peter.maydell@linaro.org

--- target-arm/arm-semi.c
+++ target-arm/arm-semi.c
@@ -565,7 +565,7 @@ target_ulong do_arm_semihosting(CPUARMState *env)
     case TARGET_SYS_HEAPINFO:
         {
             target_ulong retvals[4];
-            uint32_t limit;
+            target_ulong limit;
             int i;
 
             GET_ARG(0);
