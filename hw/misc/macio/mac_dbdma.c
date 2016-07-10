3f0d4128dc641f082c3631d610f843b0cdbb6e61
Mark Cave-Ayland <mark.cave-ayland@ilande.co.uk>
2016-07-10 19:08:55 +0100

    dbdma: fix endian of DBDMA_CMDPTR_LO during branch
    The current DBDMA command is stored in little-endian format, so make sure
    we convert it to match our CPU when updating the DBDMA_CMDPTR_LO register.
    
    Signed-off-by: Mark Cave-Ayland <mark.cave-ayland@ilande.co.uk>
    Acked-by: Benjamin Herrenschmidt <benh@kernel.crashing.org>
    Signed-off-by: David Gibson <david@gibson.dropbear.id.au>

--- hw/misc/macio/mac_dbdma.c
+++ hw/misc/macio/mac_dbdma.c
@@ -213,7 +213,7 @@ static void branch(DBDMA_channel *ch)
 {
     dbdma_cmd *current = &ch->current;
 
-    ch->regs[DBDMA_CMDPTR_LO] = current->cmd_dep;
+    ch->regs[DBDMA_CMDPTR_LO] = le32_to_cpu(current->cmd_dep);
     ch->regs[DBDMA_STATUS] |= BT;
     dbdma_cmdptr_load(ch);
 }
