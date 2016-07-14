365aa1131fa61815eb1d672df6ba451bfe7f2cea
Igor Mammedov <imammedo@redhat.com>
2016-07-14 16:58:02 +0200

    apic: kvm-apic: Fix crash due to access to freed memory region
    kvm-apic.io_memory memory region had its parent set to NULL at
    memory_region_init_io() time, so it ended up as a child in
     /unattached contaner.
    As result when kvm-apic instance was deleted, the child property
     /unattached/kvm-apic-msi[XXX] contained a reference to
    kvm-apic.io_memory address which was freed as part of kvm-apic.
    
    Do the same as 'apic' and make kvm-apic instance the owner
    of the memory region so that it won't end up in /unattached
    and gets cleanly released along with related kvm-apic instance.
    
    Signed-off-by: Igor Mammedov <imammedo@redhat.com>
    Reviewed-by: Michael S. Tsirkin <mst@redhat.com>
    Signed-off-by: Eduardo Habkost <ehabkost@redhat.com>

--- hw/i386/kvm/apic.c
+++ hw/i386/kvm/apic.c
@@ -184,8 +184,8 @@ static void kvm_apic_realize(DeviceState *dev, Error **errp)
 {
     APICCommonState *s = APIC_COMMON(dev);
 
-    memory_region_init_io(&s->io_memory, NULL, &kvm_apic_io_ops, s, "kvm-apic-msi",
-                          APIC_SPACE_SIZE);
+    memory_region_init_io(&s->io_memory, OBJECT(s), &kvm_apic_io_ops, s,
+                          "kvm-apic-msi", APIC_SPACE_SIZE);
 
     if (kvm_has_gsi_routing()) {
         msi_nonbroken = true;
