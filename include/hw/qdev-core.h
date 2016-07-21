b3443f43f45e06971d87c985bb0316c1e40259c9
Greg Kurz <groug@kaod.org>
2016-07-21 23:58:37 +0200

    qdev: ignore GlobalProperty.errp for hotplugged devices
    This patch ensures QEMU won't terminate while hotplugging a device if the
    global property cannot be set and errp points to error_fatal or error_abort.
    
    While here, it also fixes indentation of the typename argument.
    
    Suggested-by: Eduardo Habkost <ehabkost@redhat.com>
    Signed-off-by: Greg Kurz <groug@kaod.org>
    Reviewed-by: Eduardo Habkost <ehabkost@redhat.com>
    Signed-off-by: Eduardo Habkost <ehabkost@redhat.com>

--- include/hw/qdev-core.h
+++ include/hw/qdev-core.h
@@ -261,7 +261,9 @@ struct PropertyInfo {
  * @used: Set to true if property was used when initializing a device.
  * @errp: Error destination, used like first argument of error_setg()
  *        in case property setting fails later. If @errp is NULL, we
- *        print warnings instead of ignoring errors silently.
+ *        print warnings instead of ignoring errors silently. For
+ *        hotplugged devices, errp is always ignored and warnings are
+ *        printed instead.
  */
 typedef struct GlobalProperty {
     const char *driver;
