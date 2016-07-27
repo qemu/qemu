98cb89af4df7e1776ce418ed6167b6e214a64435
Sascha Silbe <silbe@linux.vnet.ibm.com>
2016-07-27 11:24:26 +0200

    error: error_setg_errno(): errno gets preserved
    C11 allows errno to be clobbered by pretty much any library function
    call, so in general callers need to take care to save errno before
    calling other functions.
    
    However, for error reporting functions this is rather awkward and can
    make the code on the caller side more complicated than
    necessary. error_setg_errno() already takes care of preserving errno
    and some functions rely on that, so just promise that we continue to
    do so in the future.
    
    Signed-off-by: Sascha Silbe <silbe@linux.vnet.ibm.com>
    Message-Id: <1469611466-31574-1-git-send-email-silbe@linux.vnet.ibm.com>
    Reviewed-by: Markus Armbruster <armbru@redhat.com>
    Signed-off-by: Markus Armbruster <armbru@redhat.com>

--- include/qapi/error.h
+++ include/qapi/error.h
@@ -170,6 +170,9 @@ void error_setg_internal(Error **errp,
  * Just like error_setg(), with @os_error info added to the message.
  * If @os_error is non-zero, ": " + strerror(os_error) is appended to
  * the human-readable error message.
+ *
+ * The value of errno (which usually can get clobbered by almost any
+ * function call) will be preserved.
  */
 #define error_setg_errno(errp, os_error, fmt, ...)                      \
     error_setg_errno_internal((errp), __FILE__, __LINE__, __func__,     \
