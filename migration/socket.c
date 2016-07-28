474c624ddf0d390b850526a01026ef8737e67ca7
Cao jin <caoj.fnst@cn.fujitsu.com>
2016-07-28 16:54:34 +0800

    migration/socket: fix typo in file header
    Code of inet socket & unix socket is merged together.
    Also add some newlines, make code block well separated.
    
    Cc: Daniel P. Berrange <berrange@redhat.com>
    Cc: Juan Quintela <quintela@redhat.com>
    Cc: Amit Shah <amit.shah@redhat.com>
    
    Reviewed-by: Daniel P. Berrange <berrange@redhat.com>
    Signed-off-by: Cao jin <caoj.fnst@cn.fujitsu.com>
    Message-Id: <1469696074-12744-4-git-send-email-caoj.fnst@cn.fujitsu.com>
    Signed-off-by: Amit Shah <amit.shah@redhat.com>

--- migration/socket.c
+++ migration/socket.c
@@ -1,5 +1,5 @@
 /*
- * QEMU live migration via Unix Domain Sockets
+ * QEMU live migration via socket
  *
  * Copyright Red Hat, Inc. 2009-2016
  *
@@ -94,10 +94,12 @@ static void socket_start_outgoing_migration(MigrationState *s,
 {
     QIOChannelSocket *sioc = qio_channel_socket_new();
     struct SocketConnectData *data = g_new0(struct SocketConnectData, 1);
+
     data->s = s;
     if (saddr->type == SOCKET_ADDRESS_KIND_INET) {
         data->hostname = g_strdup(saddr->u.inet.data->host);
     }
+
     qio_channel_socket_connect_async(sioc,
                                      saddr,
                                      socket_outgoing_migration,
