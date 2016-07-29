0e8b3cdfbc167f4bb7790ef744eaa1ac0e6959f9
Evgeny Yakovlev <eyakovlev@virtuozzo.com>
2016-07-29 12:48:25 +0300

    migration: mmap error check fix
    mmap man page:
    "On success, mmap() returns a pointer to the mapped area. On error, the
    value MAP_FAILED (that is, (void *) -1) is returned, and errno  is  set
    to indicate the cause of the error."
    
    The check in postcopy_get_tmp_page is definitely wrong and should be
    fixed.
    
    Signed-off-by: Evgeny Yakovlev <eyakovlev@virtuozzo.com>
    Signed-off-by: Denis V. Lunev <den@openvz.org>
    Reviewed-by: Amit Shah <amit.shah@redhat.com>
    Reviewed-by: Dr. David Alan Gilbert <dgilbert@redhat.com>
    CC: Juan Quintela <quintela@redhat.com>
    CC: Amit Shah <amit.shah@redhat.com>
    Message-Id: <1469785705-16670-1-git-send-email-den@openvz.org>
    Signed-off-by: Amit Shah <amit.shah@redhat.com>

--- migration/postcopy-ram.c
+++ migration/postcopy-ram.c
@@ -604,7 +604,8 @@ void *postcopy_get_tmp_page(MigrationIncomingState *mis)
         mis->postcopy_tmp_page = mmap(NULL, getpagesize(),
                              PROT_READ | PROT_WRITE, MAP_PRIVATE |
                              MAP_ANONYMOUS, -1, 0);
-        if (!mis->postcopy_tmp_page) {
+        if (mis->postcopy_tmp_page == MAP_FAILED) {
+            mis->postcopy_tmp_page = NULL;
             error_report("%s: %s", __func__, strerror(errno));
             return NULL;
         }
