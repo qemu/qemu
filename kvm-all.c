7005f7f81cef31bda895d3274c13854c143d3d8d
Peter Xu <peterx@redhat.com>
2016-08-03 11:07:21 +0800

    kvm-irqchip: only commit route when irqchip is used
    Reported from Alexey Kardashevskiy:
    
    3f1fea0fb5bf "kvm-irqchip: do explicit commit when update irq" produces
    a crash on pseries guest running with VFIO on POWER8 machine as it does
    not support KVM_CAP_IRQCHIP (KVM_CAP_IRQ_XICS is there instead). At the
    result, KVMState::irq_routes is NULL when VFIO calls
    kvm_irqchip_commit_routes.
    
    This makes the routing update conditional.
    
    Reported-by: Alexey Kardashevskiy <aik@ozlabs.ru>
    Tested-by: Alexey Kardashevskiy <aik@ozlabs.ru>
    Signed-off-by: Peter Xu <peterx@redhat.com>
    Signed-off-by: David Gibson <david@gibson.dropbear.id.au>

--- kvm-all.c
+++ kvm-all.c
@@ -1047,6 +1047,14 @@ void kvm_irqchip_commit_routes(KVMState *s)
 {
     int ret;
 
+    if (kvm_gsi_direct_mapping()) {
+        return;
+    }
+
+    if (!kvm_gsi_routing_enabled()) {
+        return;
+    }
+
     s->irq_routes->flags = 0;
     trace_kvm_irqchip_commit_routes();
     ret = kvm_vm_ioctl(s, KVM_SET_GSI_ROUTING, s->irq_routes);
