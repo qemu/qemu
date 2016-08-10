980e66216ffc3e37903f979e02c5f63152b518c3
Kevin Wolf <kwolf@redhat.com>
2016-08-10 13:06:55 +0200

    test-coroutine: Fix coroutine pool corruption
    The test case overwrites the Coroutine object with 0xff as a way to
    assert that the coroutine isn't used any more. However, this means that
    the coroutine pool now contains a corrupted object and later test cases
    may get this corrupted object and crash.
    
    This patch saves the real content of the object and restores it after
    completing the test. The only use of the coroutine pool between those
    two points is the deletion of co2. As this only means an insertion at
    the head of an SLIST (release_pool or alloc_pool), it doesn't access the
    invalid list pointers that co1 has during this period.
    
    Signed-off-by: Kevin Wolf <kwolf@redhat.com>
    Reviewed-by: Stefan Hajnoczi <stefanha@redhat.com>

--- tests/test-coroutine.c
+++ tests/test-coroutine.c
@@ -139,13 +139,20 @@ static void test_co_queue(void)
 {
     Coroutine *c1;
     Coroutine *c2;
+    Coroutine tmp;
 
     c2 = qemu_coroutine_create(c2_fn, NULL);
     c1 = qemu_coroutine_create(c1_fn, c2);
 
     qemu_coroutine_enter(c1);
+
+    /* c1 shouldn't be used any more now; make sure we segfault if it is */
+    tmp = *c1;
     memset(c1, 0xff, sizeof(Coroutine));
     qemu_coroutine_enter(c2);
+
+    /* Must restore the coroutine now to avoid corrupted pool */
+    *c1 = tmp;
 }
 
 /*
