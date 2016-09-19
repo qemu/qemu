152fcbecad3775ba8950060616b59f6c23bc97e8
Igor Mammedov <imammedo@redhat.com>
2016-09-19 10:32:34 +0200

    target-i386: turn off CPU.l3-cache only for 2.7 and older machine types
    commit (14c985cff target-i386: present virtual L3 cache info for vcpus)
    misplaced compat property putting it in new 2.8 machine type
    which would effectively to disable feature until 2.9 is released.
    Intent of commit probably should be to disable feature for 2.7
    and older while allowing not yet released 2.8 to have feature
    enabled by default.
    
    Cc: qemu-stable@nongnu.org
    Signed-off-by: Igor Mammedov <imammedo@redhat.com>
    Reviewed-by: Marcel Apfelbaum <marcel@redhat.com>
    Reviewed-by: Eduardo Habkost <ehabkost@redhat.com>

--- include/hw/i386/pc.h
+++ include/hw/i386/pc.h
@@ -367,15 +367,14 @@ int e820_get_num_entries(void);
 bool e820_get_entry(int, uint32_t, uint64_t *, uint64_t *);
 
 #define PC_COMPAT_2_8 \
+
+#define PC_COMPAT_2_7 \
+    HW_COMPAT_2_7 \
     {\
         .driver   = TYPE_X86_CPU,\
         .property = "l3-cache",\
         .value    = "off",\
     },
-
-
-#define PC_COMPAT_2_7 \
-    HW_COMPAT_2_7
 
 #define PC_COMPAT_2_6 \
     HW_COMPAT_2_6 \
