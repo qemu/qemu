c3ff757d25115d6a530e8859d7287a862b1dc02d
Daniel P. Berrange <berrange@redhat.com>
2016-09-30 15:34:24 +0100

    main: set names for main loop sources created
    The main loop creates two generic sources for the AIO
    and IO handler systems.
    
    Acked-by: Paolo Bonzini <pbonzini@redhat.com>
    Reviewed-by: Stefan Hajnoczi <stefanha@redhat.com>
    Signed-off-by: Daniel P. Berrange <berrange@redhat.com>

--- main-loop.c
+++ main-loop.c
@@ -161,9 +161,11 @@ int qemu_init_main_loop(Error **errp)
     qemu_notify_bh = qemu_bh_new(notify_event_cb, NULL);
     gpollfds = g_array_new(FALSE, FALSE, sizeof(GPollFD));
     src = aio_get_g_source(qemu_aio_context);
+    g_source_set_name(src, "aio-context");
     g_source_attach(src, NULL);
     g_source_unref(src);
     src = iohandler_get_g_source();
+    g_source_set_name(src, "io-handler");
     g_source_attach(src, NULL);
     g_source_unref(src);
     return 0;
