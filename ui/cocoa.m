78e87797ba0b6612fc1c95216a0b81c744fb85b0
Paolo Bonzini <pbonzini@redhat.com>
2016-10-06 16:12:11 +0200

    qemu-doc: merge qemu-tech and qemu-doc
    Merge what is left of qemu-tech into the main manual as an appendix.
    Ultimately we should have a new internals manual built from docs/, and
    then the "Translator Internals" parts of qemu-tech could move to docs/
    as well.  The bits on limitation and features of CPU emulation should
    remain in qemu-doc.
    
    Reviewed-by: Emilio G. Cota <cota@braap.org>
    Signed-off-by: Paolo Bonzini <pbonzini@redhat.com>

--- ui/cocoa.m
+++ ui/cocoa.m
@@ -814,7 +814,6 @@ QemuCocoaView *cocoaView;
 - (void)doToggleFullScreen:(id)sender;
 - (void)toggleFullScreen:(id)sender;
 - (void)showQEMUDoc:(id)sender;
-- (void)showQEMUTec:(id)sender;
 - (void)zoomToFit:(id) sender;
 - (void)displayConsole:(id)sender;
 - (void)pauseQEMU:(id)sender;
@@ -996,13 +995,6 @@ QemuCocoaView *cocoaView;
     COCOA_DEBUG("QemuCocoaAppController: showQEMUDoc\n");
 
     [self openDocumentation: @"qemu-doc.html"];
-}
-
-- (void)showQEMUTec:(id)sender
-{
-    COCOA_DEBUG("QemuCocoaAppController: showQEMUTec\n");
-
-    [self openDocumentation: @"qemu-tech.html"];
 }
 
 /* Stretches video to fit host monitor size */
@@ -1335,7 +1327,6 @@ int main (int argc, const char * argv[]) {
     // Help menu
     menu = [[NSMenu alloc] initWithTitle:@"Help"];
     [menu addItem: [[[NSMenuItem alloc] initWithTitle:@"QEMU Documentation" action:@selector(showQEMUDoc:) keyEquivalent:@"?"] autorelease]]; // QEMU Help
-    [menu addItem: [[[NSMenuItem alloc] initWithTitle:@"QEMU Technology" action:@selector(showQEMUTec:) keyEquivalent:@""] autorelease]]; // QEMU Help
     menuItem = [[[NSMenuItem alloc] initWithTitle:@"Window" action:nil keyEquivalent:@""] autorelease];
     [menuItem setSubmenu:menu];
     [[NSApp mainMenu] addItem:menuItem];
