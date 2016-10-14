c1a900cf4a3925c2e4b313b82da5df955f17b70a
Michael Walle <michael@walle.cc>
2016-10-14 11:51:50 +0200

    milkymist-pfpu: fix potential integer overflow
    Since the lm32 is a 32 bit architecture, just return a 32 bit value which
    is then converted to a 64 bit value.
    
    Spotted by coverity, CID 1005506.
    
    Signed-off-by: Michael Walle <michael@walle.cc>
    Signed-off-by: Michael Tokarev <mjt@tls.msk.ru>

--- hw/misc/milkymist-pfpu.c
+++ hw/misc/milkymist-pfpu.c
@@ -137,7 +137,7 @@ struct MilkymistPFPUState {
 };
 typedef struct MilkymistPFPUState MilkymistPFPUState;
 
-static inline hwaddr
+static inline uint32_t
 get_dma_address(uint32_t base, uint32_t x, uint32_t y)
 {
     return base + 8 * (128 * y + x);
