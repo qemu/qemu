893bfc3cc893ed36cedc364e99cf483e9b08c294
Cao jin <caoj.fnst@cn.fujitsu.com>
2016-10-17 10:58:03 -0600

    vfio: fix duplicate function call
    When vfio device is reset(encounter FLR, or bus reset), if need to do
    bus reset(vfio_pci_hot_reset_one is called), vfio_pci_pre_reset &
    vfio_pci_post_reset will be called twice.
    
    Signed-off-by: Cao jin <caoj.fnst@cn.fujitsu.com>
    Signed-off-by: Alex Williamson <alex.williamson@redhat.com>

--- hw/vfio/pci.c
+++ hw/vfio/pci.c
@@ -1951,7 +1951,9 @@ static int vfio_pci_hot_reset(VFIOPCIDevice *vdev, bool single)
 
     trace_vfio_pci_hot_reset(vdev->vbasedev.name, single ? "one" : "multi");
 
-    vfio_pci_pre_reset(vdev);
+    if (!single) {
+        vfio_pci_pre_reset(vdev);
+    }
     vdev->vbasedev.needs_reset = false;
 
     info = g_malloc0(sizeof(*info));
@@ -2109,7 +2111,9 @@ out:
         }
     }
 out_single:
-    vfio_pci_post_reset(vdev);
+    if (!single) {
+        vfio_pci_post_reset(vdev);
+    }
     g_free(info);
 
     return ret;
