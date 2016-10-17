6868a420c519d74926ea814d48f6ce9beda35b98
Greg Kurz <groug@kaod.org>
2016-10-17 14:13:58 +0200

    9pfs: drop useless check in pdu_free()
    Out of the three users of pdu_free(), none ever passes a NULL pointer to
    this function.
    
    Signed-off-by: Greg Kurz <groug@kaod.org>

--- hw/9pfs/9p.c
+++ hw/9pfs/9p.c
@@ -626,16 +626,14 @@ V9fsPDU *pdu_alloc(V9fsState *s)
 
 void pdu_free(V9fsPDU *pdu)
 {
-    if (pdu) {
-        V9fsState *s = pdu->s;
-        /*
-         * Cancelled pdu are added back to the freelist
-         * by flush request .
-         */
-        if (!pdu->cancelled) {
-            QLIST_REMOVE(pdu, next);
-            QLIST_INSERT_HEAD(&s->free_list, pdu, next);
-        }
+    V9fsState *s = pdu->s;
+    /*
+     * Cancelled pdu are added back to the freelist
+     * by flush request .
+     */
+    if (!pdu->cancelled) {
+        QLIST_REMOVE(pdu, next);
+        QLIST_INSERT_HEAD(&s->free_list, pdu, next);
     }
 }
 
