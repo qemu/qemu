00d0f9fd6602a27b204f672ef5bc8e69736c7ff1
Igor Mammedov <imammedo@redhat.com>
2016-10-19 14:05:43 +0200

    pc: q35: Bump max_cpus to 288
    Along with it for machine versions 2.7 and older keep
    it at 255.
    
    Signed-off-by: Igor Mammedov <imammedo@redhat.com>
    Reviewed-by: Radim Krčmář <rkrcmar@redhat.com>
    Reviewed-by: Eduardo Habkost <ehabkost@redhat.com>
    Signed-off-by: Eduardo Habkost <ehabkost@redhat.com>

--- hw/i386/pc_q35.c
+++ hw/i386/pc_q35.c
@@ -291,6 +291,7 @@ static void pc_q35_machine_options(MachineClass *m)
     m->default_display = "std";
     m->no_floppy = 1;
     m->has_dynamic_sysbus = true;
+    m->max_cpus = 288;
 }
 
 static void pc_q35_2_8_machine_options(MachineClass *m)
@@ -306,6 +307,7 @@ static void pc_q35_2_7_machine_options(MachineClass *m)
 {
     pc_q35_2_8_machine_options(m);
     m->alias = NULL;
+    m->max_cpus = 255;
     SET_MACHINE_COMPAT(m, PC_COMPAT_2_7);
 }
 
