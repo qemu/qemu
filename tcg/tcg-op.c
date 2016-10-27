3ff91d7e85176f8b4b131163d7fd801757a2c949
Joseph Myers <joseph@codesourcery.com>
2016-10-27 23:35:04 +0000

    tcg: correct 32-bit tcg_gen_ld8s_i64 sign-extension
    The version of tcg_gen_ld8s_i64 for 32-bit systems does a load into
    the low part of the return value - then attempts a sign extension into
    the high part, but wrongly sets the high part to a sign extension of
    itself rather than of the low part.  This results in TCG internal
    errors from the use of the uninitialized high part (in some GCC tests
    of AArch64 NEON shift intrinsics, in particular).  This patch corrects
    the sign-extension logic, making it match other functions such as
    tcg_gen_ld16s_i64.
    
    Reviewed-by: Peter Maydell <peter.maydell@linaro.org>
    Signed-off-by: Joseph Myers <joseph@codesourcery.com>
    Message-Id: <alpine.DEB.2.20.1610272333560.22353@digraph.polyomino.org.uk>
    Signed-off-by: Richard Henderson <rth@twiddle.net>

--- tcg/tcg-op.c
+++ tcg/tcg-op.c
@@ -817,7 +817,7 @@ void tcg_gen_ld8u_i64(TCGv_i64 ret, TCGv_ptr arg2, tcg_target_long offset)
 void tcg_gen_ld8s_i64(TCGv_i64 ret, TCGv_ptr arg2, tcg_target_long offset)
 {
     tcg_gen_ld8s_i32(TCGV_LOW(ret), arg2, offset);
-    tcg_gen_sari_i32(TCGV_HIGH(ret), TCGV_HIGH(ret), 31);
+    tcg_gen_sari_i32(TCGV_HIGH(ret), TCGV_LOW(ret), 31);
 }
 
 void tcg_gen_ld16u_i64(TCGv_i64 ret, TCGv_ptr arg2, tcg_target_long offset)
