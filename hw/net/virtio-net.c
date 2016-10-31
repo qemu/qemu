68b5f314a269979b4f2014eb22873732d4745bcc
Yuri Benditovich <yuri.benditovich@daynix.com>
2016-11-01 00:01:17 +0200

    net: skip virtio-net config of deleted nic's peers
    https://bugzilla.redhat.com/show_bug.cgi?id=1373816
    qemu core dump happens during repetitive unpug-plug
    with multiple queues and Windows RSS-capable guest.
    If back-end delete requested during virtio-net device
    initialization, driver still can try configure the device
    for multiple queues. The virtio-net device is expected
    to be removed as soon as the initialization is done.
    
    Signed-off-by: Yuri Benditovich <yuri.benditovich@daynix.com>
    Signed-off-by: Jason Wang <jasowang@redhat.com>

--- hw/net/virtio-net.c
+++ hw/net/virtio-net.c
@@ -508,6 +508,10 @@ static void virtio_net_set_queues(VirtIONet *n)
     int i;
     int r;
 
+    if (n->nic->peer_deleted) {
+        return;
+    }
+
     for (i = 0; i < n->max_queues; i++) {
         if (i < n->curr_queues) {
             r = peer_attach(n, i);
