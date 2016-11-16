272694a29dc3ed2ed675e03194d33485a2902f0a
Richard Henderson <rth@twiddle.net>
2016-11-16 10:54:57 +0100

    target-cris: Use clz opcode
    Signed-off-by: Richard Henderson <rth@twiddle.net>

--- target/cris/helper.h
+++ target/cris/helper.h
@@ -7,7 +7,6 @@ DEF_HELPER_1(rfn, void, env)
 DEF_HELPER_3(movl_sreg_reg, void, env, i32, i32)
 DEF_HELPER_3(movl_reg_sreg, void, env, i32, i32)
 
-DEF_HELPER_FLAGS_1(lz, TCG_CALL_NO_SE, i32, i32)
 DEF_HELPER_FLAGS_4(btst, TCG_CALL_NO_SE, i32, env, i32, i32, i32)
 
 DEF_HELPER_FLAGS_4(evaluate_flags_muls, TCG_CALL_NO_SE, i32, env, i32, i32, i32)
