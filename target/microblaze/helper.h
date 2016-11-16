5318420c62b6f6ce0bb7cfcf115fc21055015f90
Richard Henderson <rth@twiddle.net>
2016-11-16 10:58:52 +0100

    target-microblaze: Use clz opcode
    Signed-off-by: Richard Henderson <rth@twiddle.net>

--- target/microblaze/helper.h
+++ target/microblaze/helper.h
@@ -3,7 +3,6 @@ DEF_HELPER_1(debug, void, env)
 DEF_HELPER_FLAGS_3(carry, TCG_CALL_NO_RWG_SE, i32, i32, i32, i32)
 DEF_HELPER_2(cmp, i32, i32, i32)
 DEF_HELPER_2(cmpu, i32, i32, i32)
-DEF_HELPER_FLAGS_1(clz, TCG_CALL_NO_RWG_SE, i32, i32)
 
 DEF_HELPER_3(divs, i32, env, i32, i32)
 DEF_HELPER_3(divu, i32, env, i32, i32)
