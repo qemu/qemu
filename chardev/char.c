e1f98fe48a049366e754c055cc3ea99dcf6cb688
Marc-André Lureau <marcandre.lureau@redhat.com>
2016-12-14 23:15:44 +0300

    char: useless NULL check
    g_strdup(NULL) returns NULL already.
    
    Signed-off-by: Marc-André Lureau <marcandre.lureau@redhat.com>
    Reviewed-by: Philippe Mathieu-Daudé <f4bug@amsat.org>

--- chardev/char.c
+++ chardev/char.c
@@ -764,7 +764,7 @@ void qemu_chr_parse_common(QemuOpts *opts, ChardevCommon *backend)
     const char *logfile = qemu_opt_get(opts, "logfile");
 
     backend->has_logfile = logfile != NULL;
-    backend->logfile = logfile ? g_strdup(logfile) : NULL;
+    backend->logfile = g_strdup(logfile);
 
     backend->has_logappend = true;
     backend->logappend = qemu_opt_get_bool(opts, "logappend", false);
