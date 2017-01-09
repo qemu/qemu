0f254b1ae04b36e2ab2d91528297ed60d40c8c08
Peter Xu <peterx@redhat.com>
2017-01-09 16:55:53 +0800

    x86: ioapic: fix fail migration when irqchip=split
    Split irqchip works based on the fact that we kept the first 24 gsi
    routing entries inside KVM for userspace ioapic's use. When system
    boot, we'll reserve these MSI routing entries before hand. However,
    after migration, we forgot to re-configure it up in the destination
    side. The result is, we'll get invalid gsi routing entries after
    migration (all empty), and we get interrupts with vector=0, then
    strange things happen, like keyboard hang.
    
    The solution is simple - we update them after migration, which is a
    one line fix.
    
    Signed-off-by: Peter Xu <peterx@redhat.com>
    Message-Id: <1483952153-7221-4-git-send-email-peterx@redhat.com>
    Cc: qemu-stable@nongnu.org
    Signed-off-by: Paolo Bonzini <pbonzini@redhat.com>

--- hw/intc/ioapic.c
+++ hw/intc/ioapic.c
@@ -439,6 +439,11 @@ static void ioapic_class_init(ObjectClass *klass, void *data)
     DeviceClass *dc = DEVICE_CLASS(klass);
 
     k->realize = ioapic_realize;
+    /*
+     * If APIC is in kernel, we need to update the kernel cache after
+     * migration, otherwise first 24 gsi routes will be invalid.
+     */
+    k->post_load = ioapic_update_kvm_routes;
     dc->reset = ioapic_reset_common;
     dc->props = ioapic_properties;
 }
