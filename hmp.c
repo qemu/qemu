dd39c544f4136ba390d1f976f520004a3ffad7e5
Igor Mammedov <imammedo@redhat.com>
2017-01-10 13:53:14 +0100

    monitor: reuse user_creatable_add_opts() instead of user_creatable_add()
    Simplify code by dropping ~57LOC by merging user_creatable_add()
    into user_creatable_add_opts() and using the later from monitor.
    Along with it allocate opts_visitor_new() once in user_creatable_add_opts().
    
    As result we have one less API func and a more readable/simple
    user_creatable_add_opts() vs user_creatable_add().
    
    Signed-off-by: Igor Mammedov <imammedo@redhat.com>
    Reviewed-by: Eric Blake <eblake@redhat.com>
    Message-Id: <1484052795-158195-3-git-send-email-imammedo@redhat.com>
    Signed-off-by: Eduardo Habkost <ehabkost@redhat.com>
    (cherry picked from commit 3a4641054e320d8ad7e780c30010b1c1dbfc56e8)
    * functional dep for 9a6d1ac
    Signed-off-by: Michael Roth <mdroth@linux.vnet.ibm.com>

--- hmp.c
+++ hmp.c
@@ -1809,7 +1809,6 @@ void hmp_object_add(Monitor *mon, const QDict *qdict)
 {
     Error *err = NULL;
     QemuOpts *opts;
-    Visitor *v;
     Object *obj = NULL;
 
     opts = qemu_opts_from_qdict(qemu_find_opts("object"), qdict, &err);
@@ -1818,9 +1817,7 @@ void hmp_object_add(Monitor *mon, const QDict *qdict)
         return;
     }
 
-    v = opts_visitor_new(opts);
-    obj = user_creatable_add(qdict, v, &err);
-    visit_free(v);
+    obj = user_creatable_add_opts(opts, &err);
     qemu_opts_del(opts);
 
     if (err) {
