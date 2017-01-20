5658ffa39aae034458231bc4abfee57637b88c6e
Thomas Huth <thuth@redhat.com>
2017-01-20 16:53:59 +0100

    hw/isa/isa-bus: Set category of the "isabus-bridge" device
    It has "bridge" in its name, so it should be in the category
    DEVICE_CATEGORY_BRIDGE.
    
    Signed-off-by: Thomas Huth <thuth@redhat.com>
    Reviewed-by: Markus Armbruster <armbru@redhat.com>
    Signed-off-by: Michael Tokarev <mjt@tls.msk.ru>

--- hw/isa/isa-bus.c
+++ hw/isa/isa-bus.c
@@ -219,6 +219,7 @@ static void isabus_bridge_class_init(ObjectClass *klass, void *data)
 {
     DeviceClass *dc = DEVICE_CLASS(klass);
 
+    set_bit(DEVICE_CATEGORY_BRIDGE, dc->categories);
     dc->fw_name = "isa";
 }
 
