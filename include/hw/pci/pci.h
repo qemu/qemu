f7d6f3fac8dd7b1d1ecb2662b1751e0ed3fef727
Marcel Apfelbaum <marcel@redhat.com>
2017-01-23 21:20:20 +0200

    hw/pcie: Introduce Generic PCI Express Root Port
    The Generic Root Port behaves almost the same as the
    Intel's IOH device with id 3420, without having
    Intel specific attributes.
    
    The device has two purposes:
     (1) Can be used on both X86 and ARM machines.
     (2) It will allow us to tweak the behaviour
        (e.g add vendor-specific PCI capabilities)
         - something that obviously cannot be done
           on a known device.
    
    Signed-off-by: Marcel Apfelbaum <marcel@redhat.com>
    Reviewed-by: Michael S. Tsirkin <mst@redhat.com>
    Signed-off-by: Michael S. Tsirkin <mst@redhat.com>
    Tested-by: Andrea Bolognani <abologna@redhat.com>

--- include/hw/pci/pci.h
+++ include/hw/pci/pci.h
@@ -96,6 +96,7 @@
 #define PCI_DEVICE_ID_REDHAT_PXB         0x0009
 #define PCI_DEVICE_ID_REDHAT_BRIDGE_SEAT 0x000a
 #define PCI_DEVICE_ID_REDHAT_PXB_PCIE    0x000b
+#define PCI_DEVICE_ID_REDHAT_PCIE_RP     0x000c
 #define PCI_DEVICE_ID_REDHAT_QXL         0x0100
 
 #define FMT_PCIBUS                      PRIx64
