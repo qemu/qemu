6516367fc0803d079384e0ad370856ac328bef30
Thomas Huth <thuth@redhat.com>
2017-01-25 21:45:17 +0100

    hw/core/generic-loader: Fix crash when running without CPU
    When running QEMU with "-M none -device loader,file=kernel.elf", it
    currently crashes with a segmentation fault, because the "none"-machine
    does not have any CPU by default and the generic loader code tries
    to dereference s->cpu. Fix it by adding an appropriate check for a
    NULL pointer.
    
    Reported-by: Laurent Vivier <laurent@vivier.eu>
    Signed-off-by: Thomas Huth <thuth@redhat.com>
    Reviewed-by: Laurent Vivier <laurent@vivier.eu>
    Reviewed-by: Alistair Francis <alistair.francis@xilinx.com>
    Signed-off-by: Michael Tokarev <mjt@tls.msk.ru>

--- hw/core/generic-loader.c
+++ hw/core/generic-loader.c
@@ -137,20 +137,21 @@ static void generic_loader_realize(DeviceState *dev, Error **errp)
 #endif
 
     if (s->file) {
+        AddressSpace *as = s->cpu ? s->cpu->as :  NULL;
+
         if (!s->force_raw) {
             size = load_elf_as(s->file, NULL, NULL, &entry, NULL, NULL,
-                               big_endian, 0, 0, 0, s->cpu->as);
+                               big_endian, 0, 0, 0, as);
 
             if (size < 0) {
                 size = load_uimage_as(s->file, &entry, NULL, NULL, NULL, NULL,
-                                      s->cpu->as);
+                                      as);
             }
         }
 
         if (size < 0 || s->force_raw) {
             /* Default to the maximum size being the machine's ram size */
-            size = load_image_targphys_as(s->file, s->addr, ram_size,
-                                          s->cpu->as);
+            size = load_image_targphys_as(s->file, s->addr, ram_size, as);
         } else {
             s->addr = entry;
         }
