6afff1ffa36909b164847ff276cc9cb575058f4c
Marc-André Lureau <marcandre.lureau@redhat.com>
2017-02-05 20:07:30 +0000

    tests: fix eepro100-test leak
    Spotted by ASAN.
    
    Signed-off-by: Marc-André Lureau <marcandre.lureau@redhat.com>
    Reviewed-by: Stefan Weil <sw@weilnetz.de>

--- tests/eepro100-test.c
+++ tests/eepro100-test.c
@@ -54,6 +54,7 @@ int main(int argc, char **argv)
 
         path = g_strdup_printf("eepro100/%s", models[i]);
         qtest_add_data_func(path, models[i], test_device);
+        g_free(path);
     }
 
     return g_test_run();
