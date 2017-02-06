2b880bcdbeb87b9cb4e325dd6e872373458f82d9
Marc-André Lureau <marcandre.lureau@redhat.com>
2017-02-06 09:31:41 +0000

    tests: fix virtio-9p-test leaks
    Spotted by ASAN.
    
    Cc: "Aneesh Kumar K.V" <aneesh.kumar@linux.vnet.ibm.com>
    Signed-off-by: Marc-André Lureau <marcandre.lureau@redhat.com>
    Reviewed-by: Greg Kurz <groug@kaod.org>

--- tests/virtio-9p-test.c
+++ tests/virtio-9p-test.c
@@ -80,7 +80,7 @@ static void qvirtio_9p_pci_stop(QVirtIO9P *v9p)
 {
     qvirtqueue_cleanup(v9p->dev->bus, v9p->vq, v9p->qs->alloc);
     qvirtio_pci_device_disable(container_of(v9p->dev, QVirtioPCIDevice, vdev));
-    g_free(v9p->dev);
+    qvirtio_pci_device_free((QVirtioPCIDevice *)v9p->dev);
     qvirtio_9p_stop(v9p);
 }
 
