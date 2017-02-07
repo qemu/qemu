9aecba0cde4222b25f70006155a73014493bed3c
zhangzhiming <zhangzhiming02@meituan.com>
2017-02-07 21:52:29 +0800

    add

--- qcow2-img.c
+++ qcow2-img.c
@@ -43,6 +43,8 @@ typedef enum OutputFormat {
     OFORMAT_HUMAN,
 } OutputFormat;
 
+#define BDRV_DEFAULT_CACHE "writeback"
+
 static QemuOptsList qemu_object_opts = {
     .name = "object",
     .implied_opt_name = "qom-type",
@@ -103,6 +105,11 @@ static char *generate_encoded_backingfile(const char* template, const char* laye
     char *backing_string = malloc(PATH_MAX*2);
     snprintf(backing_string, PATH_MAX*2, "qcow2://%s?layer=%s", template, layer_uuid);
     return backing_string;
+}
+
+static int get_encoded_backingfile(char* name, char* template, char* layer_uuid)
+{
+    return sscanf(name, "qcow2://%[^?]?layer=%s", template, layer_uuid);
 }
 
 static int img_open_password(BlockBackend *blk, const char *filename,
@@ -589,6 +596,204 @@ static int img_info(int argc, char **argv)
     return 0;
 }
 
+typedef struct CommonBlockJobCBInfo {
+    BlockDriverState *bs;
+    Error **errp;
+} CommonBlockJobCBInfo;
+
+static int img_commit(int argc, char **argv)
+{
+    int c, ret, flags;
+    const char *filename, *fmt, *cache, *base;
+    BlockBackend *blk;
+    BlockDriverState *bs, *base_bs;
+    bool progress = false, quiet = false, drop = false;
+    bool writethrough;
+    Error *local_err = NULL;
+    CommonBlockJobCBInfo cbi;
+    bool image_opts = false;
+    AioContext *aio_context;
+
+    fmt = "qcow2";
+    cache = BDRV_DEFAULT_CACHE;
+    base = NULL;
+    for(;;) {
+        static const struct option long_options[] = {
+            {"help", no_argument, 0, 'h'},
+            {"object", required_argument, 0, OPTION_OBJECT},
+            {"image-opts", no_argument, 0, OPTION_IMAGE_OPTS},
+            {0, 0, 0, 0}
+        };
+        c = getopt_long(argc, argv, "ht:b:dpq",
+                        long_options, NULL);
+        if (c == -1) {
+            break;
+        }
+        switch(c) {
+        case '?':
+        case 'h':
+            help();
+            break;
+        case 't':
+            cache = optarg;
+            break;
+        case 'b':
+            base = optarg;
+            /* -b implies -d */
+            drop = true;
+            break;
+        case 'd':
+            drop = true;
+            break;
+        case 'p':
+            progress = true;
+            break;
+        case 'q':
+            quiet = true;
+            break;
+        case OPTION_OBJECT: {
+            QemuOpts *opts;
+            opts = qemu_opts_parse_noisily(&qemu_object_opts,
+                                           optarg, true);
+            if (!opts) {
+                return 1;
+            }
+        }   break;
+        case OPTION_IMAGE_OPTS:
+            image_opts = true;
+            break;
+        }
+    }
+
+    /* Progress is not shown in Quiet mode */
+    if (quiet) {
+        progress = false;
+    }
+
+    if (optind != argc - 1) {
+        error_exit("Expecting one image file name");
+    }
+    filename = argv[optind++];
+
+    if (qemu_opts_foreach(&qemu_object_opts,
+                          user_creatable_add_opts_foreach,
+                          NULL, NULL)) {
+        return 1;
+    }
+
+    flags = BDRV_O_RDWR | BDRV_O_UNMAP | BDRV_O_NO_BACKING;
+    ret = bdrv_parse_cache_mode(cache, &flags, &writethrough);
+    if (ret < 0) {
+        error_report("Invalid cache option: %s", cache);
+        return 1;
+    }
+
+    blk = img_open(image_opts, filename, fmt, flags, writethrough, quiet);
+    if (!blk) {
+        return 1;
+    }
+    bs = blk_bs(blk);
+    ImageInfo *info;
+    bdrv_query_image_info(bs, &info, &local_err);
+    if (local_err) {
+        error_report_err(local_err);
+        goto done;
+    }
+
+    if (!info->has_backing_filename) {
+        error_setg_errno(&local_err, -1, "No backing file found, can't commit");
+        goto done;
+    }
+
+    char tmplate_name[PATH_MAX], layername[PATH_MAX];
+    ret = get_encoded_backingfile(info->backing_filename, tmplate_name, layername);
+    if (ret != 2) {
+        error_setg_errno(&local_err, -1, "error get get_encoded_backingfile, can't commit");
+        goto done;
+    }
+
+    base_bs = img_open(image_opts, tmplate_name, fmt, flags, writethrough, quiet);
+    if (!base_bs){
+        error_setg_errno(&local_err, -1, "error open backing file %s, can't commit", tmplate_name);
+        goto done;
+    }
+
+    if (base) {
+        base_bs = bdrv_find_backing_image(bs, base);
+        if (!base_bs) {
+            error_setg(&local_err, QERR_BASE_NOT_FOUND, base);
+            goto done;
+        }
+    } else {
+        /* This is different from QMP, which by default uses the deepest file in
+         * the backing chain (i.e., the very base); however, the traditional
+         * behavior of qemu-img commit is using the immediate backing file. */
+        base_bs = backing_bs(bs);
+        if (!base_bs) {
+            error_setg(&local_err, "Image does not have a backing file");
+            goto done;
+        }
+    }
+
+    cbi = (CommonBlockJobCBInfo){
+        .errp = &local_err,
+        .bs   = bs,
+    };
+
+    qemu_progress_init(progress, 1.f);
+    qemu_progress_print(0.f, 100);
+
+    aio_context = bdrv_get_aio_context(bs);
+    aio_context_acquire(aio_context);
+    commit_active_start("commit", bs, base_bs, BLOCK_JOB_DEFAULT, 0,
+                        BLOCKDEV_ON_ERROR_REPORT, common_block_job_cb, &cbi,
+                        &local_err, false);
+    aio_context_release(aio_context);
+    if (local_err) {
+        goto done;
+    }
+
+    /* When the block job completes, the BlockBackend reference will point to
+     * the old backing file. In order to avoid that the top image is already
+     * deleted, so we can still empty it afterwards, increment the reference
+     * counter here preemptively. */
+    if (!drop) {
+        bdrv_ref(bs);
+    }
+
+    run_block_job(bs->job, &local_err);
+    if (local_err) {
+        goto unref_backing;
+    }
+
+    if (!drop && bs->drv->bdrv_make_empty) {
+        ret = bs->drv->bdrv_make_empty(bs);
+        if (ret) {
+            error_setg_errno(&local_err, -ret, "Could not empty %s",
+                             filename);
+            goto unref_backing;
+        }
+    }
+
+unref_backing:
+    if (!drop) {
+        bdrv_unref(bs);
+    }
+
+done:
+    qemu_progress_end();
+
+    blk_unref(blk);
+
+    if (local_err) {
+        error_report_err(local_err);
+        return 1;
+    }
+
+    qprintf(quiet, "Image committed.\n");
+    return 0;
+}
+
 static int img_create(int argc, char **argv)
 {
     int c;
@@ -723,6 +928,7 @@ static const img_cmd_t img_cmds[] = {
     DEF("create", img_create, "create [-o options] {-t <template file> -l <layer UUID> -s <size>} filename")
     DEF("resize", img_resize, "resize filename [+ | -]size")
     DEF("info", img_info, "info filename")
+    DEF("commit", img_commit, "commit [-t <cache>] [-s <snapshot>] -m <commit-message> filename")
 #undef DEF
 #undef GEN_DOCS
     { NULL, NULL, },
