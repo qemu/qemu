a6e3707ece6b326e3fc5a086086a2ccac9710e67
Thomas Huth <thuth@redhat.com>
2017-02-08 18:57:38 +0100

    hw/core/register: Mark the device with cannot_instantiate_with_device_add_yet
    The "qemu,register" device needs to be wired up in source code, there
    is no way the user can make any real use of this device with the
    "-device" parameter or the "device_add" monitor command yet.
    
    Signed-off-by: Thomas Huth <thuth@redhat.com>
    Reviewed-by: Markus Armbruster <armbru@redhat.com>
    Reviewed-by: Alistair Francis <alistair.francis@xilinx.com>
    Signed-off-by: Michael Tokarev <mjt@tls.msk.ru>

--- hw/core/register.c
+++ hw/core/register.c
@@ -274,9 +274,18 @@ void register_finalize_block(RegisterInfoArray *r_array)
     g_free(r_array);
 }
 
+static void register_class_init(ObjectClass *oc, void *data)
+{
+    DeviceClass *dc = DEVICE_CLASS(oc);
+
+    /* Reason: needs to be wired up to work */
+    dc->cannot_instantiate_with_device_add_yet = true;
+}
+
 static const TypeInfo register_info = {
     .name  = TYPE_REGISTER,
     .parent = TYPE_DEVICE,
+    .class_init = register_class_init,
 };
 
 static void register_register_types(void)
