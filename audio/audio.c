1ffc266539d443f83d5eb487593be50ef496f09e
Pavel Dovgalyuk <pavel.dovgaluk@ispras.ru>
2017-02-14 10:15:10 +0300

    audio: make audio poll timer deterministic
    This patch changes resetting strategy of the audio polling timer.
    It does not change expiration time if the timer is already set.
    This patch is needed to make this timer deterministic and to use execution
    record/replay for audio devices.
    
    audio_reset_timer is used in the function audio_vm_change_state_handler.
    Therefore every time VM is stopped or restarted the timer will be reset
    to new timeout. Virtual clock does not proceed while VM is stopped.
    Therefore there is no need in resetting the timeout when VM restarts.
    
    v2: updated commit message
    v3: now using timer_mod_anticipate function (as suggested by Yurii Zubrytskyi)
    
    Signed-off-by: Pavel Dovgalyuk <pavel.dovgaluk@ispras.ru>
    Message-id: 20170214071510.6112.76764.stgit@PASHA-ISP
    Signed-off-by: Gerd Hoffmann <kraxel@redhat.com>

--- audio/audio.c
+++ audio/audio.c
@@ -1113,7 +1113,7 @@ static int audio_is_timer_needed (void)
 static void audio_reset_timer (AudioState *s)
 {
     if (audio_is_timer_needed ()) {
-        timer_mod (s->ts,
+        timer_mod_anticipate_ns(s->ts,
             qemu_clock_get_ns(QEMU_CLOCK_VIRTUAL) + conf.period.ticks);
     }
     else {
