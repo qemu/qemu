6244bb7e5811ff689688ab7ee8e8a7ced1c85010
Greg Kurz <gkurz@linux.vnet.ibm.com>
2017-02-24 15:55:31 +1100

    sysemu: support up to 1024 vCPUs
    Some systems can already provide more than 255 hardware threads.
    
    Bumping the QEMU limit to 1024 seems reasonable:
    - it has no visible overhead in top;
    - the limit itself has no effect on hot paths.
    
    Cc: Greg Kurz <gkurz@linux.vnet.ibm.com>
    Signed-off-by: Alexey Kardashevskiy <aik@ozlabs.ru>
    Signed-off-by: David Gibson <david@gibson.dropbear.id.au>

--- hw/ppc/spapr.c
+++ hw/ppc/spapr.c
@@ -2891,7 +2891,7 @@ static void spapr_machine_class_init(ObjectClass *oc, void *data)
     mc->init = ppc_spapr_init;
     mc->reset = ppc_spapr_reset;
     mc->block_default_type = IF_SCSI;
-    mc->max_cpus = 255;
+    mc->max_cpus = 1024;
     mc->no_parallel = 1;
     mc->default_boot_order = "";
     mc->default_ram_size = 512 * M_BYTE;
