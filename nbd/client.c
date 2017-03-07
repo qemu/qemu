8029d55a9362cbf6e210c9a33f62f5f30136ff50
Vladimir Sementsov-Ogievskiy <vsementsov@virtuozzo.com>
2017-03-07 09:16:27 -0600

    nbd/client: fix drop_sync [CVE-2017-2630]
    Comparison symbol is misused. It may lead to memory corruption.
    Introduced in commit 7d3123e.
    
    Signed-off-by: Vladimir Sementsov-Ogievskiy <vsementsov@virtuozzo.com>
    Message-Id: <20170203154757.36140-6-vsementsov@virtuozzo.com>
    [eblake: add CVE details, update conditional]
    Signed-off-by: Eric Blake <eblake@redhat.com>
    Reviewed-by: Marc-AndrÃ© Lureau <marcandre.lureau@redhat.com>
    Message-Id: <20170307151627.27212-1-eblake@redhat.com>
    Signed-off-by: Paolo Bonzini <pbonzini@redhat.com>
    
    (cherry picked from commit 2563c9c6b8670400c48e562034b321a7cf3d9a85)
    Signed-off-by: Michael Roth <mdroth@linux.vnet.ibm.com>

--- nbd/client.c
+++ nbd/client.c
@@ -94,7 +94,7 @@ static ssize_t drop_sync(QIOChannel *ioc, size_t size)
     char small[1024];
     char *buffer;
 
-    buffer = sizeof(small) < size ? small : g_malloc(MIN(65536, size));
+    buffer = sizeof(small) >= size ? small : g_malloc(MIN(65536, size));
     while (size > 0) {
         ssize_t count = read_sync(ioc, buffer, MIN(65536, size));
 
