3328c14e63f08fb07e8c6dec779c9d365e9e9864
hangaohuai <hangaohuai@huawei.com>
2017-03-14 14:39:19 +0800

    fix :cirrus_vga fix OOB read case qemu Segmentation fault
    check the validity of parameters in cirrus_bitblt_rop_fwd_transp_xxx
    and cirrus_bitblt_rop_fwd_xxx to avoid the OOB read which causes qemu Segmentation fault.
    
    After the fix, we will touch the assert in
    cirrus_invalidate_region:
    assert(off_cur_end >= off_cur);
    
    Signed-off-by: fangying <fangying1@huawei.com>
    Signed-off-by: hangaohuai <hangaohuai@huawei.com>
    Message-id: 20170314063919.16200-1-hangaohuai@huawei.com
    Signed-off-by: Gerd Hoffmann <kraxel@redhat.com>
    (cherry picked from commit 215902d7b6fb50c6fc216fc74f770858278ed904)
    Signed-off-by: Michael Roth <mdroth@linux.vnet.ibm.com>

--- hw/display/cirrus_vga_rop.h
+++ hw/display/cirrus_vga_rop.h
@@ -97,6 +97,11 @@ glue(glue(cirrus_bitblt_rop_fwd_transp_, ROP_NAME),_8)(CirrusVGAState *s,
     uint8_t p;
     dstpitch -= bltwidth;
     srcpitch -= bltwidth;
+
+    if (bltheight > 1 && (dstpitch < 0 || srcpitch < 0)) {
+        return;
+    }
+
     for (y = 0; y < bltheight; y++) {
         for (x = 0; x < bltwidth; x++) {
 	    p = *dst;
@@ -143,6 +148,11 @@ glue(glue(cirrus_bitblt_rop_fwd_transp_, ROP_NAME),_16)(CirrusVGAState *s,
     uint8_t p1, p2;
     dstpitch -= bltwidth;
     srcpitch -= bltwidth;
+
+    if (bltheight > 1 && (dstpitch < 0 || srcpitch < 0)) {
+        return;
+    }
+
     for (y = 0; y < bltheight; y++) {
         for (x = 0; x < bltwidth; x+=2) {
 	    p1 = *dst;
