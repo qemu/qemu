e048dac616748273c2153490e9fdf1da242f0cad
Gerd Hoffmann <kraxel@redhat.com>
2017-03-15 13:06:46 +0100

    cirrus: fix cirrus_invalidate_region
    off_cur_end is exclusive, so off_cur_end == cirrus_addr_mask is valid.
    Fix calculation to make sure to allow that, otherwise the assert added
    by commit f153b563f8cf121aebf5a2fff5f0110faf58ccb3 can trigger for valid
    blits.
    
    Test case: boot windows nt 4.0
    
    Signed-off-by: Gerd Hoffmann <kraxel@redhat.com>
    Message-id: 1489579606-26020-1-git-send-email-kraxel@redhat.com

--- hw/display/cirrus_vga.c
+++ hw/display/cirrus_vga.c
@@ -667,11 +667,11 @@ static void cirrus_invalidate_region(CirrusVGAState * s, int off_begin,
     }
 
     for (y = 0; y < lines; y++) {
-	off_cur = off_begin;
-	off_cur_end = (off_cur + bytesperline) & s->cirrus_addr_mask;
+        off_cur = off_begin;
+        off_cur_end = ((off_cur + bytesperline - 1) & s->cirrus_addr_mask) + 1;
         assert(off_cur_end >= off_cur);
         memory_region_set_dirty(&s->vga.vram, off_cur, off_cur_end - off_cur);
-	off_begin += off_pitch;
+        off_begin += off_pitch;
     }
 }
 
