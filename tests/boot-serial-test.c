7150d34a1d60851d73d6ab6783b12b1d25e68f86
Christian Borntraeger <borntraeger@de.ibm.com>
2017-03-24 14:19:30 +0100

    boot-serial-test: use -no-shutdown
    a qemu with an empty s390 guest will exit very quickly. This races
    against the testsuite reading from the console pipe leading to
    intermittent test suite failures. Using -no-shutdown will keep
    the guest running.
    
    Fixes: 864111f422ba (vl: exit qemu on guest panic if -no-shutdown is not set)
    Reported-by: Peter Maydell <peter.maydell@linaro.org>
    Signed-off-by: Christian Borntraeger <borntraeger@de.ibm.com>
    Message-id: 1490361570-288658-1-git-send-email-borntraeger@de.ibm.com
    Signed-off-by: Peter Maydell <peter.maydell@linaro.org>

--- tests/boot-serial-test.c
+++ tests/boot-serial-test.c
@@ -79,8 +79,8 @@ static void test_machine(const void *data)
     g_assert(fd != -1);
 
     args = g_strdup_printf("-M %s,accel=tcg -chardev file,id=serial0,path=%s"
-                           " -serial chardev:serial0 %s", test->machine,
-                           tmpname, test->extra);
+                           " -no-shutdown -serial chardev:serial0 %s",
+                           test->machine, tmpname, test->extra);
 
     qtest_start(args);
     unlink(tmpname);
