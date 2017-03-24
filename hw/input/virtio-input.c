57094547dfea4ce784923b8abb53ac3ab4e3961a
Ladi Prosek <lprosek@redhat.com>
2017-03-24 15:24:50 +0100

    virtio-input: fix eventq batching
    virtio_input_send buffers input events until it sees a SYNC. Then it
    either sends or drops the entire batch, depending on whether eventq
    has enough space available. The case to avoid here is partial sends
    where only part of the batch would get to the guest.
    
    Using virtqueue_get_avail_bytes to check the state of eventq was not
    correct. The queue may have a smaller number of larger buffers
    available so bytes may be enough but the batch would still not be
    possible to send, leading to the "Huh?  No vq elem available" error.
    
    Instead of checking available bytes, this patch optimistically pops
    buffers from the queue and puts them back in case it runs out of
    space and the batch needs to be dropped.
    
    Signed-off-by: Ladi Prosek <lprosek@redhat.com>
    Message-id: 1490365490-4854-3-git-send-email-lprosek@redhat.com
    Signed-off-by: Gerd Hoffmann <kraxel@redhat.com>

--- hw/input/virtio-input.c
+++ hw/input/virtio-input.c
@@ -22,7 +22,6 @@
 void virtio_input_send(VirtIOInput *vinput, virtio_input_event *event)
 {
     VirtQueueElement *elem;
-    unsigned have, need;
     int i, len;
 
     if (!vinput->active) {
@@ -33,9 +32,9 @@ void virtio_input_send(VirtIOInput *vinput, virtio_input_event *event)
     if (vinput->qindex == vinput->qsize) {
         vinput->qsize++;
         vinput->queue = g_realloc(vinput->queue, vinput->qsize *
-                                  sizeof(virtio_input_event));
+                                  sizeof(vinput->queue[0]));
     }
-    vinput->queue[vinput->qindex++] = *event;
+    vinput->queue[vinput->qindex++].event = *event;
 
     /* ... until we see a report sync ... */
     if (event->type != cpu_to_le16(EV_SYN) ||
@@ -44,24 +43,24 @@ void virtio_input_send(VirtIOInput *vinput, virtio_input_event *event)
     }
 
     /* ... then check available space ... */
-    need = sizeof(virtio_input_event) * vinput->qindex;
-    virtqueue_get_avail_bytes(vinput->evt, &have, NULL, need, 0);
-    if (have < need) {
-        vinput->qindex = 0;
-        trace_virtio_input_queue_full();
-        return;
+    for (i = 0; i < vinput->qindex; i++) {
+        elem = virtqueue_pop(vinput->evt, sizeof(VirtQueueElement));
+        if (!elem) {
+            while (--i >= 0) {
+                virtqueue_unpop(vinput->evt, vinput->queue[i].elem, 0);
+            }
+            vinput->qindex = 0;
+            trace_virtio_input_queue_full();
+            return;
+        }
+        vinput->queue[i].elem = elem;
     }
 
     /* ... and finally pass them to the guest */
     for (i = 0; i < vinput->qindex; i++) {
-        elem = virtqueue_pop(vinput->evt, sizeof(VirtQueueElement));
-        if (!elem) {
-            /* should not happen, we've checked for space beforehand */
-            fprintf(stderr, "%s: Huh?  No vq elem available ...\n", __func__);
-            return;
-        }
+        elem = vinput->queue[i].elem;
         len = iov_from_buf(elem->in_sg, elem->in_num,
-                           0, vinput->queue+i, sizeof(virtio_input_event));
+                           0, &vinput->queue[i].event, sizeof(virtio_input_event));
         virtqueue_push(vinput->evt, elem, len);
         g_free(elem);
     }
