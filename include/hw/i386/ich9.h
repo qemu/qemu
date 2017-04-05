8c9f42f3cf3598e8bb612b3f81cd12632efd3ca4
Paolo Bonzini <pbonzini@redhat.com>
2017-04-05 10:11:36 +0200

    tco: do not generate an NMI
    This behavior is not indicated in the datasheet and can confuse the OS.
    The TCO can trap NMIs from SERR# or IOCHK# and convert them to SMIs; but
    any other TCO event is either delivered as an SMI or completely disabled.
    
    Reviewed-by: Laszlo Ersek <lersek@redhat.com>
    Signed-off-by: Paolo Bonzini <pbonzini@redhat.com>

--- include/hw/i386/ich9.h
+++ include/hw/i386/ich9.h
@@ -21,7 +21,6 @@ void ich9_lpc_pm_init(PCIDevice *pci_lpc, bool smm_enabled);
 I2CBus *ich9_smb_init(PCIBus *bus, int devfn, uint32_t smb_io_base);
 
 void ich9_generate_smi(void);
-void ich9_generate_nmi(void);
 
 #define ICH9_CC_SIZE (16 * 1024) /* 16KB. Chipset configuration registers */
 
