bc70597a487a3bb8585c155a43d09dcff5e4ffd2
Max Reitz <mreitz@redhat.com>
2017-04-13 22:34:01 +0200

    qemu-img/convert: Move bs_n > 1 && -B check down
    It does not make much sense to use a backing image for the target when
    you concatenate multiple images (because then there is no correspondence
    between the source images' backing files and the target's); but it was
    still possible to give one by using -o backing_file=X instead of -B X.
    
    Fix this by moving the check.
    
    (Also, change the error message because -B is not the only way to
     specify the backing file, evidently.)
    
    Cc: qemu-stable <qemu-stable@nongnu.org>
    Signed-off-by: Max Reitz <mreitz@redhat.com>
    Reviewed-by: Eric Blake <eblake@redhat.com>
    * applied patch from v1 of series as suggested by author
    Signed-off-by: Michael Roth <mdroth@linux.vnet.ibm.com>

--- qemu-img.c
+++ qemu-img.c
@@ -2113,13 +2113,6 @@ static int img_convert(int argc, char **argv)
     }
 
 
-    if (bs_n > 1 && out_baseimg) {
-        error_report("-B makes no sense when concatenating multiple input "
-                     "images");
-        ret = -1;
-        goto out;
-    }
-
     src_flags = 0;
     ret = bdrv_parse_cache_mode(src_cache, &src_flags, &src_writethrough);
     if (ret < 0) {
@@ -2227,6 +2220,13 @@ static int img_convert(int argc, char **argv)
     out_baseimg_param = qemu_opt_get(opts, BLOCK_OPT_BACKING_FILE);
     if (out_baseimg_param) {
         out_baseimg = out_baseimg_param;
+    }
+
+    if (bs_n > 1 && out_baseimg) {
+        error_report("Having a backing file for the target makes no sense when "
+                     "concatenating multiple input images");
+        ret = -1;
+        goto out;
     }
 
     /* Check if compression is supported */
