05c6638b203fd7d8bbfa88ac6e6198e32ed0506f
Marc-André Lureau <marcandre.lureau@redhat.com>
2017-04-25 17:05:20 +0400

    input: don't queue delay if paused
    qemu_input_event_send() discards key event when the guest is paused,
    but not the delay.
    
    The delay ends up in the input queue, and qemu_input_event_send_key()
    will further fill the queue with upcoming events.
    
    VNC uses qemu_input_event_send_key_delay(), not SPICE, which results
    in a different input behaviour on pause: VNC will queue the events
    (except the first that is discarded), SPICE will discard all events.
    
    Don't queue delay if paused, and provide same behaviour on SPICE and
    VNC clients on resume (and potentially avoid over-allocating the
    buffer queue)
    
    Fixes:
    https://bugzilla.redhat.com/show_bug.cgi?id=1444326
    
    Signed-off-by: Marc-André Lureau <marcandre.lureau@redhat.com>
    Message-id: 20170425130520.31819-1-marcandre.lureau@redhat.com
    Signed-off-by: Gerd Hoffmann <kraxel@redhat.com>

--- ui/input.c
+++ ui/input.c
@@ -411,6 +411,10 @@ void qemu_input_event_send_key_qcode(QemuConsole *src, QKeyCode q, bool down)
 
 void qemu_input_event_send_key_delay(uint32_t delay_ms)
 {
+    if (!runstate_is_running() && !runstate_check(RUN_STATE_SUSPENDED)) {
+        return;
+    }
+
     if (!kbd_timer) {
         kbd_timer = timer_new_ms(QEMU_CLOCK_VIRTUAL, qemu_input_queue_process,
                                  &kbd_queue);
