d8586afd8bbda11130a1536d61820fe809af22c5
Juan Quintela <quintela@redhat.com>
2017-04-26 00:37:19 +0200

    audio: remove UINT16
    More modernitation.
    
    Signed-off-by: Juan Quintela <quintela@redhat.com>
    Message-id: 20170425223739.6703-7-quintela@redhat.com
    Signed-off-by: Gerd Hoffmann <kraxel@redhat.com>

--- hw/audio/fmopl.h
+++ hw/audio/fmopl.h
@@ -10,7 +10,6 @@
 /* compiler dependence */
 #ifndef OSD_CPU_H
 #define OSD_CPU_H
-typedef unsigned short	UINT16;  /* unsigned 16bit */
 typedef unsigned int	UINT32;  /* unsigned 32bit */
 typedef signed char		INT8;    /* signed  8bit   */
 typedef signed short	INT16;   /* signed 16bit   */
