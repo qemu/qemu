e78722368c721f3c5b8109ed525adac1653ae97b
Emilio G. Cota <cota@braap.org>
2017-04-28 14:57:41 -0400

    target/aarch64: optimize cross-page direct jumps in softmmu
    Perf numbers in next commit's log.
    
    Signed-off-by: Emilio G. Cota <cota@braap.org>
    Signed-off-by: Richard Henderson <rth@twiddle.net>

--- target/arm/translate-a64.c
+++ target/arm/translate-a64.c
@@ -379,7 +379,7 @@ static inline void gen_goto_tb(DisasContext *s, int n, uint64_t dest)
         } else if (s->singlestep_enabled) {
             gen_exception_internal(EXCP_DEBUG);
         } else {
-            tcg_gen_exit_tb(0);
+            tcg_gen_lookup_and_goto_ptr(cpu_pc);
             s->is_jmp = DISAS_TB_JUMP;
         }
     }
