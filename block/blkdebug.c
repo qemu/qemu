a1a3d603c4484ff06b4338e73713bdea8f298b45
Eric Blake <eblake@redhat.com>
2017-04-29 14:14:14 -0500

    blkdebug: Sanity check block layer guarantees
    Commits 04ed95f4 and 1a62d0ac updated the block layer to auto-fragment
    any I/O to fit within device boundaries. Additionally, when using a
    minimum alignment of 4k, we want to ensure the block layer does proper
    read-modify-write rather than requesting I/O on a slice of a sector.
    Let's enforce that the contract is obeyed when using blkdebug.  For
    now, blkdebug only allows alignment overrides, and just inherits other
    limits from whatever device it is wrapping, but a future patch will
    further enhance things.
    
    Signed-off-by: Eric Blake <eblake@redhat.com>
    Reviewed-by: Kevin Wolf <kwolf@redhat.com>
    Reviewed-by: Max Reitz <mreitz@redhat.com>
    Message-id: 20170429191419.30051-5-eblake@redhat.com
    Signed-off-by: Max Reitz <mreitz@redhat.com>
    (cherry picked from commit e0ef439588ce1ede747f82b77d893190c1cc9f4d)
    * prereq for 81c219a
    Signed-off-by: Michael Roth <mdroth@linux.vnet.ibm.com>

--- block/blkdebug.c
+++ block/blkdebug.c
@@ -429,6 +429,13 @@ blkdebug_co_preadv(BlockDriverState *bs, uint64_t offset, uint64_t bytes,
     BDRVBlkdebugState *s = bs->opaque;
     BlkdebugRule *rule = NULL;
 
+    /* Sanity check block layer guarantees */
+    assert(QEMU_IS_ALIGNED(offset, bs->bl.request_alignment));
+    assert(QEMU_IS_ALIGNED(bytes, bs->bl.request_alignment));
+    if (bs->bl.max_transfer) {
+        assert(bytes <= bs->bl.max_transfer);
+    }
+
     QSIMPLEQ_FOREACH(rule, &s->active_rules, active_next) {
         uint64_t inject_offset = rule->options.inject.offset;
 
@@ -452,6 +459,13 @@ blkdebug_co_pwritev(BlockDriverState *bs, uint64_t offset, uint64_t bytes,
 {
     BDRVBlkdebugState *s = bs->opaque;
     BlkdebugRule *rule = NULL;
+
+    /* Sanity check block layer guarantees */
+    assert(QEMU_IS_ALIGNED(offset, bs->bl.request_alignment));
+    assert(QEMU_IS_ALIGNED(bytes, bs->bl.request_alignment));
+    if (bs->bl.max_transfer) {
+        assert(bytes <= bs->bl.max_transfer);
+    }
 
     QSIMPLEQ_FOREACH(rule, &s->active_rules, active_next) {
         uint64_t inject_offset = rule->options.inject.offset;
