a081ab8f7b7b9eb72ead9201b5ece14d09fcc1c4
Eduardo Habkost <ehabkost@redhat.com>
2017-05-03 17:35:53 -0300

    sysbus-ahci: Remove user_creatable flag
    The sysbus-ahci devices are supposed to be created and wired by
    code from other devices, like calxeda_init() and
    xlnx_zynqmp_realize(), and won't work with -device. Remove the
    user_creatable flag from the device class.
    
    Cc: John Snow <jsnow@redhat.com>
    Cc: qemu-block@nongnu.org
    Cc: Rob Herring <robh@kernel.org>
    Cc: Peter Maydell <peter.maydell@linaro.org>
    Cc: Alistair Francis <alistair.francis@xilinx.com>
    Cc: "Edgar E. Iglesias" <edgar.iglesias@gmail.com>
    Cc: Marcel Apfelbaum <marcel@redhat.com>
    Reviewed-by: Alistair Francis <alistair.francis@xilinx.com>
    Acked-by: John Snow <jsnow@redhat.com>
    Acked-by: Marcel Apfelbaum <marcel@redhat.com>
    Signed-off-by: Eduardo Habkost <ehabkost@redhat.com>
    Message-Id: <20170503203604.31462-11-ehabkost@redhat.com>
    Signed-off-by: Eduardo Habkost <ehabkost@redhat.com>

--- hw/ide/ahci.c
+++ hw/ide/ahci.c
@@ -1721,11 +1721,6 @@ static void sysbus_ahci_class_init(ObjectClass *klass, void *data)
     dc->props = sysbus_ahci_properties;
     dc->reset = sysbus_ahci_reset;
     set_bit(DEVICE_CATEGORY_STORAGE, dc->categories);
-    /*
-     * FIXME: Set only because we are not sure yet if this device
-     * will be outside the q35 sysbus whitelist.
-     */
-    dc->user_creatable = true;
 }
 
 static const TypeInfo sysbus_ahci_info = {
