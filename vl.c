7af88279e4972dd6bf735b620876d54b7a355c4d
Eric Blake <eblake@redhat.com>
2017-05-15 16:41:10 -0500

    shutdown: Simplify shutdown_signal
    There is no signal 0 (kill(pid, 0) has special semantics to probe whether
    a process is alive), rather than actually sending a signal 0).  So we
    can use the simpler 0, instead of -1, for our sentinel of whether a
    shutdown request due to a signal has happened.
    
    Suggested-by: Markus Armbruster <armbru@redhat.com>
    Signed-off-by: Eric Blake <eblake@redhat.com>
    Reviewed-by: Markus Armbruster <armbru@redhat.com>
    Reviewed-by: Alistair Francis <alistair.francis@xilinx.com>
    Message-Id: <20170515214114.15442-2-eblake@redhat.com>
    Signed-off-by: Markus Armbruster <armbru@redhat.com>

--- vl.c
+++ vl.c
@@ -1598,7 +1598,7 @@ void vm_state_notify(int running, RunState state)
 }
 
 static int reset_requested;
-static int shutdown_requested, shutdown_signal = -1;
+static int shutdown_requested, shutdown_signal;
 static pid_t shutdown_pid;
 static int powerdown_requested;
 static int debug_requested;
@@ -1629,7 +1629,7 @@ static int qemu_shutdown_requested(void)
 
 static void qemu_kill_report(void)
 {
-    if (!qtest_driver() && shutdown_signal != -1) {
+    if (!qtest_driver() && shutdown_signal) {
         if (shutdown_pid == 0) {
             /* This happens for eg ^C at the terminal, so it's worth
              * avoiding printing an odd message in that case.
@@ -1643,7 +1643,7 @@ static void qemu_kill_report(void)
                          shutdown_cmd ? shutdown_cmd : "<unknown process>");
             g_free(shutdown_cmd);
         }
-        shutdown_signal = -1;
+        shutdown_signal = 0;
     }
 }
 
