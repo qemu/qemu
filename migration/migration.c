3482655bbc21d158ed0daaa294c890997238cc23
Peter Xu <peterx@redhat.com>
2017-05-19 14:43:29 +0800

    migration: fix leak of src file on dst
    The return path channel is possibly leaked. Fix it.
    
    Signed-off-by: Peter Xu <peterx@redhat.com>
    Reviewed-by: Juan Quintela <quintela@redhat.com>
    Signed-off-by: Juan Quintela <quintela@redhat.com>

--- migration/migration.c
+++ migration/migration.c
@@ -133,6 +133,11 @@ void migration_incoming_state_destroy(void)
 {
     struct MigrationIncomingState *mis = migration_incoming_get_current();
 
+    if (mis->to_src_file) {
+        qemu_fclose(mis->to_src_file);
+        mis->to_src_file = NULL;
+    }
+
     qemu_event_destroy(&mis->main_thread_load_event);
 }
 
