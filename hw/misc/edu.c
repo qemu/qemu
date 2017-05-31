c25a67f0c3d0c86231f9653267a222c4effa706f
Paolo Bonzini <pbonzini@redhat.com>
2017-05-31 14:56:37 +0200

    edu: fix memory leak on msi_broken platforms
    If msi_init fails, the thread has already been created and the
    mutex/condvar are not destroyed.  Initialize everything only
    after the point where pci_edu_realize cannot fail.
    
    Reported-by: Markus Armbruster <armbru@redhat.com>
    Cc: Peter Xu <peterx@redhat.com>
    Signed-off-by: Paolo Bonzini <pbonzini@redhat.com>

--- hw/misc/edu.c
+++ hw/misc/edu.c
@@ -343,18 +343,18 @@ static void pci_edu_realize(PCIDevice *pdev, Error **errp)
     EduState *edu = DO_UPCAST(EduState, pdev, pdev);
     uint8_t *pci_conf = pdev->config;
 
+    pci_config_set_interrupt_pin(pci_conf, 1);
+
+    if (msi_init(pdev, 0, 1, true, false, errp)) {
+        return;
+    }
+
     timer_init_ms(&edu->dma_timer, QEMU_CLOCK_VIRTUAL, edu_dma_timer, edu);
 
     qemu_mutex_init(&edu->thr_mutex);
     qemu_cond_init(&edu->thr_cond);
     qemu_thread_create(&edu->thread, "edu", edu_fact_thread,
                        edu, QEMU_THREAD_JOINABLE);
-
-    pci_config_set_interrupt_pin(pci_conf, 1);
-
-    if (msi_init(pdev, 0, 1, true, false, errp)) {
-        return;
-    }
 
     memory_region_init_io(&edu->mmio, OBJECT(edu), &edu_mmio_ops, edu,
                     "edu-mmio", 1 << 20);
