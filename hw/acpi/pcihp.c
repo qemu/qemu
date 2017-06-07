c03d83d55a27f11a4fe87ac7b0085bcbb440d9ac
Marc-André Lureau <marcandre.lureau@redhat.com>
2017-06-07 20:36:15 +0400

    pcihp: use get_uint() for "bsel" property
    The property is defined with object_property_add_uint32_ptr()
    
    Signed-off-by: Marc-André Lureau <marcandre.lureau@redhat.com>
    Message-Id: <20170607163635.17635-24-marcandre.lureau@redhat.com>
    Signed-off-by: Markus Armbruster <armbru@redhat.com>

--- hw/acpi/pcihp.c
+++ hw/acpi/pcihp.c
@@ -62,10 +62,10 @@ typedef struct AcpiPciHpFind {
 static int acpi_pcihp_get_bsel(PCIBus *bus)
 {
     Error *local_err = NULL;
-    int64_t bsel = object_property_get_int(OBJECT(bus), ACPI_PCIHP_PROP_BSEL,
-                                           &local_err);
+    uint64_t bsel = object_property_get_uint(OBJECT(bus), ACPI_PCIHP_PROP_BSEL,
+                                             &local_err);
 
-    if (local_err || bsel < 0 || bsel >= ACPI_PCIHP_MAX_HOTPLUG_BUS) {
+    if (local_err || bsel >= ACPI_PCIHP_MAX_HOTPLUG_BUS) {
         if (local_err) {
             error_free(local_err);
         }
