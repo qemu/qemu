21827917342bac938e462513afee6c7d39f28adc
Max Reitz <mreitz@redhat.com>
2017-06-13 19:20:06 +0200

    blkverify: Catch bs->exact_filename overflow
    The bs->exact_filename field may not be sufficient to store the full
    blkverify node filename. In this case, we should not generate a filename
    at all instead of an unusable one.
    
    Cc: qemu-stable@nongnu.org
    Reported-by: Qu Wenruo <quwenruo@cn.fujitsu.com>
    Signed-off-by: Max Reitz <mreitz@redhat.com>
    Message-id: 20170613172006.19685-3-mreitz@redhat.com
    Reviewed-by: Alberto Garcia <berto@igalia.com>
    Reviewed-by: Stefan Hajnoczi <stefanha@redhat.com>
    Signed-off-by: Max Reitz <mreitz@redhat.com>
    (cherry picked from commit 05cc758a3dfc79488d0a8eb7f5830a41871e78d0)
    Signed-off-by: Michael Roth <mdroth@linux.vnet.ibm.com>

--- block/blkverify.c
+++ block/blkverify.c
@@ -301,10 +301,14 @@ static void blkverify_refresh_filename(BlockDriverState *bs, QDict *options)
     if (bs->file->bs->exact_filename[0]
         && s->test_file->bs->exact_filename[0])
     {
-        snprintf(bs->exact_filename, sizeof(bs->exact_filename),
-                 "blkverify:%s:%s",
-                 bs->file->bs->exact_filename,
-                 s->test_file->bs->exact_filename);
+        int ret = snprintf(bs->exact_filename, sizeof(bs->exact_filename),
+                           "blkverify:%s:%s",
+                           bs->file->bs->exact_filename,
+                           s->test_file->bs->exact_filename);
+        if (ret >= sizeof(bs->exact_filename)) {
+            /* An overflow makes the filename unusable, so do not report any */
+            bs->exact_filename[0] = 0;
+        }
     }
 }
 
