40bf8e9aede0f9105a9e1e4aaf17b20aaa55f9a0
Roman Kagan <rkagan@virtuozzo.com>
2017-07-13 23:15:22 +0300

    update-linux-headers: prepare for hyperv.h removal
    All definitions related to Hyper-V emulation are now taken from the QEMU
    own header, so the one imported from the kernel is no longer needed.
    
    Unfortunately it's included by kvm_para.h.
    
    So, until this is fixed in the kernel, teach the header harvesting
    script to substitute kernel's hyperv.h with a dummy.
    
    Signed-off-by: Roman Kagan <rkagan@virtuozzo.com>
    Message-Id: <20170713201522.13765-3-rkagan@virtuozzo.com>
    Signed-off-by: Paolo Bonzini <pbonzini@redhat.com>

--- scripts/update-linux-headers.sh
+++ scripts/update-linux-headers.sh
@@ -104,7 +104,9 @@ for arch in $ARCHLIST; do
         cp "$tmpdir/include/asm/unistd-common.h" "$output/linux-headers/asm-arm/"
     fi
     if [ $arch = x86 ]; then
-        cp_portable "$tmpdir/include/asm/hyperv.h" "$output/include/standard-headers/asm-x86/"
+        cat <<-EOF >"$output/include/standard-headers/asm-x86/hyperv.h"
+        /* this is a temporary placeholder until kvm_para.h stops including it */
+        EOF
         cp "$tmpdir/include/asm/unistd_32.h" "$output/linux-headers/asm-x86/"
         cp "$tmpdir/include/asm/unistd_x32.h" "$output/linux-headers/asm-x86/"
         cp "$tmpdir/include/asm/unistd_64.h" "$output/linux-headers/asm-x86/"
