4c4414a4388f902b7ae2814f9a64898dd0e426a5
Peter Maydell <peter.maydell@linaro.org>
2017-07-17 17:37:42 +0100

    hw/display/sm501: Don't use vmstate_register_ram_global()
    The sm501 device uses vmstate_register_ram_global() to register its
    memory region for migration.  This means it gets a name that is
    assumed to be global to the whole system, which in turn means that if
    you create two of the device we assert because of the duplication:
    
    qemu-system-ppc -device sm501 -device sm501
    RAMBlock "sm501.local" already registered, abort!
    Aborted (core dumped)
    
    Changing this to just use memory_region_init_ram()'s automatic
    registration of the memory region with a device-local name fixes
    this.  The downside is that it breaks migration compatibility, but
    luckily we only added migration support to this device in the 2.10
    release cycle so we haven't released a QEMU version with the broken
    implementation.
    
    Signed-off-by: Peter Maydell <peter.maydell@linaro.org>
    Reviewed-by: Dr. David Alan Gilbert <dgilbert@redhat.com>
    Message-id: 1500309462-12792-1-git-send-email-peter.maydell@linaro.org

--- hw/display/sm501.c
+++ hw/display/sm501.c
@@ -1578,9 +1578,8 @@ static void sm501_init(SM501State *s, DeviceState *dev,
                   s->local_mem_size_index);
 
     /* local memory */
-    memory_region_init_ram_nomigrate(&s->local_mem_region, OBJECT(dev), "sm501.local",
+    memory_region_init_ram(&s->local_mem_region, OBJECT(dev), "sm501.local",
                            get_local_mem_size(s), &error_fatal);
-    vmstate_register_ram_global(&s->local_mem_region);
     memory_region_set_log(&s->local_mem_region, true, DIRTY_MEMORY_VGA);
     s->local_mem = memory_region_get_ram_ptr(&s->local_mem_region);
 
