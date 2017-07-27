b7b1e9dd6d56612d4656817b11e43d9fbefdc497
Philippe Mathieu-Daudé <f4bug@amsat.org>
2017-07-26 23:42:08 -0300

    ivshmem: fix incorrect error handling in ivshmem_recv_msg()
    Screwed up in commit 3a55fc0f, v2.6.0.
    
    If qemu_chr_fe_read_all() returns -EINTR the do {} statement continues and the
    n accumulator used to complete reads upto sizeof(msg) is decremented by 4 (the
    value of EINTR on Linux).
    To avoid that, use simpler if() statements and continue if EINTR occured.
    
    hw/misc/ivshmem.c:650:14: warning: Loss of sign in implicit conversion
        } while (n < sizeof(msg));
                 ^
    
    Reported-by: Clang Static Analyzer
    Signed-off-by: Philippe Mathieu-Daudé <f4bug@amsat.org>
    Reviewed-by: Markus Armbruster <armbru@redhat.com>
    Signed-off-by: Michael Tokarev <mjt@tls.msk.ru>

--- hw/misc/ivshmem.c
+++ hw/misc/ivshmem.c
@@ -642,7 +642,10 @@ static int64_t ivshmem_recv_msg(IVShmemState *s, int *pfd, Error **errp)
     do {
         ret = qemu_chr_fe_read_all(&s->server_chr, (uint8_t *)&msg + n,
                                    sizeof(msg) - n);
-        if (ret < 0 && ret != -EINTR) {
+        if (ret < 0) {
+            if (ret == -EINTR) {
+                continue;
+            }
             error_setg_errno(errp, -ret, "read from server failed");
             return INT64_MIN;
         }
