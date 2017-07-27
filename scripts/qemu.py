f0d20f34b9c7021c4c2e6bb247cfdafef4853d60
Amador Pahim <apahim@redhat.com>
2017-07-27 15:39:47 +0200

    qemu.py: use os.path.null instead of /dev/null
    For increased portability, let's use os.path.devnull.
    
    Signed-off-by: Amador Pahim <apahim@redhat.com>

--- scripts/qemu.py
+++ scripts/qemu.py
@@ -80,7 +80,7 @@ class QEMUMachine(object):
         fd_param = ["%s" % self._socket_scm_helper,
                     "%d" % self._qmp.get_sock_fd(),
                     "%s" % fd_file_path]
-        devnull = open('/dev/null', 'rb')
+        devnull = open(os.path.devnull, 'rb')
         p = subprocess.Popen(fd_param, stdin=devnull, stdout=sys.stdout,
                              stderr=sys.stderr)
         return p.wait()
@@ -137,7 +137,7 @@ class QEMUMachine(object):
 
     def launch(self):
         '''Launch the VM and establish a QMP connection'''
-        devnull = open('/dev/null', 'rb')
+        devnull = open(os.path.devnull, 'rb')
         qemulog = open(self._qemu_log_path, 'wb')
         try:
             self._pre_launch()
