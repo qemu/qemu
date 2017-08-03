808e668bbc8db6faa9ffaa802628711f36fe659b
Dong Jia Shi <bjsdjshi@linux.vnet.ibm.com>
2017-08-03 02:35:26 +0200

    s390x/css: use macro for event-information pending error recover code
    Let's use a macro for the ERC (error recover code) when generating a
    Channel Subsystem Event-information pending CRW (channel report word).
    
    While we are at it, let's also add all other ERCs.
    
    Signed-off-by: Dong Jia Shi <bjsdjshi@linux.vnet.ibm.com>
    Reviewed-by: Halil Pasic <pasic@linux.vnet.ibm.com>
    Message-Id: <20170803003527.86979-2-bjsdjshi@linux.vnet.ibm.com>
    Signed-off-by: Cornelia Huck <cohuck@redhat.com>

--- hw/s390x/css.c
+++ hw/s390x/css.c
@@ -2108,7 +2108,7 @@ void css_generate_chp_crws(uint8_t cssid, uint8_t chpid)
 void css_generate_css_crws(uint8_t cssid)
 {
     if (!channel_subsys.sei_pending) {
-        css_queue_crw(CRW_RSC_CSS, 0, 0, cssid);
+        css_queue_crw(CRW_RSC_CSS, CRW_ERC_EVENT, 0, cssid);
     }
     channel_subsys.sei_pending = true;
 }
