425b2fc501b3e51d60b57b1f8a0c0949bac109fe
Aaron Sierra <asierra@xes-inc.com>
2017-08-07 15:10:10 -0500

    linux-user: Support explicit targets for PowerPC
    Enable building PowerPC targets supporting a specific CPU, without
    having to set QEMU_CPU via the environment. For example these build
    targets (and many more) become available:
    
        qemu-ppc.e500mc
        qemu-ppc.e500v2
        qemu-ppc.e5500
        qemu-ppc.e600
        qemu-ppc.e6500
    
    These (statically compiled) binaries have proven useful for emulating
    PowerPC CPUs within Docker containers, where it's hard to reliably
    define environment variables that are available for every process.
    
    Signed-off-by: Aaron Sierra <asierra@xes-inc.com>

--- linux-user/main.c
+++ linux-user/main.c
@@ -4298,7 +4298,9 @@ int main(int argc, char **argv, char **envp)
     init_qemu_uname_release();
 
     if (cpu_model == NULL) {
-#if defined(TARGET_I386)
+#if defined(TARGET_EXPLICIT_CPU)
+        cpu_model = TARGET_EXPLICIT_CPU;
+#elif defined(TARGET_I386)
 #ifdef TARGET_X86_64
         cpu_model = "qemu64";
 #else
