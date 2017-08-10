5c0919d02066c3d0eb896c33265ad90101a6a84a
Richard W.M. Jones <rjones@redhat.com>
2017-08-10 17:52:55 +0100

    virtio-scsi: Add virtqueue_size parameter allowing virtqueue size to be set.
    Since Linux switched to blk-mq as the default in Linux commit
    5c279bd9e406 ("scsi: default to scsi-mq"), virtio-scsi LUNs consume
    about 10x as much guest kernel memory.
    
    This commit allows you to choose the virtqueue size for each
    virtio-scsi-pci controller like this:
    
      -device virtio-scsi-pci,id=scsi,virtqueue_size=16
    
    The default is still 128 as before.  Using smaller virtqueue_size
    allows many more disks to be added to small memory virtual machines.
    For a 1 vCPU, 500 MB, no swap VM I observed:
    
      With scsi-mq enabled (upstream kernel):              175 disks
        -"- ditto -"-   virtqueue_size=64:                 318 disks
        -"- ditto -"-   virtqueue_size=16:                 775 disks
      With scsi-mq disabled (kernel before 5c279bd9e406): 1755 disks
    
    Note that to have any effect, this requires a kernel patch:
    
      https://lkml.org/lkml/2017/8/10/689
    
    Signed-off-by: Richard W.M. Jones <rjones@redhat.com>
    Message-Id: <20170810165255.20865-1-rjones@redhat.com>
    Signed-off-by: Paolo Bonzini <pbonzini@redhat.com>

--- include/hw/virtio/virtio-scsi.h
+++ include/hw/virtio/virtio-scsi.h
@@ -32,7 +32,6 @@
 #define VIRTIO_SCSI(obj) \
         OBJECT_CHECK(VirtIOSCSI, (obj), TYPE_VIRTIO_SCSI)
 
-#define VIRTIO_SCSI_VQ_SIZE     128
 #define VIRTIO_SCSI_MAX_CHANNEL 0
 #define VIRTIO_SCSI_MAX_TARGET  255
 #define VIRTIO_SCSI_MAX_LUN     16383
@@ -48,6 +47,7 @@ typedef struct virtio_scsi_config VirtIOSCSIConfig;
 
 struct VirtIOSCSIConf {
     uint32_t num_queues;
+    uint32_t virtqueue_size;
     uint32_t max_sectors;
     uint32_t cmd_per_lun;
 #ifdef CONFIG_VHOST_SCSI
