480bc11e61693796debd3a9566be0ca1795bb7b7
Cornelia Huck <cohuck@redhat.com>
2017-08-11 13:03:31 +0200

    boot-serial-test: fallback to kvm accelerator
    Currently, at least x86_64 and s390x support building with --disable-tcg.
    Instead of forcing tcg (which causes the test to fail on such builds),
    allow to use kvm as well.
    
    Reviewed-by: Daniel P. Berrange <berrange@redhat.com>
    Reviewed-by: Thomas Huth <thuth@redhat.com>
    Signed-off-by: Cornelia Huck <cohuck@redhat.com>

--- tests/boot-serial-test.c
+++ tests/boot-serial-test.c
@@ -78,8 +78,9 @@ static void test_machine(const void *data)
     fd = mkstemp(tmpname);
     g_assert(fd != -1);
 
-    args = g_strdup_printf("-M %s,accel=tcg -chardev file,id=serial0,path=%s"
-                           " -no-shutdown -serial chardev:serial0 %s",
+    args = g_strdup_printf("-M %s,accel=kvm:tcg "
+                           "-chardev file,id=serial0,path=%s "
+                           "-no-shutdown -serial chardev:serial0 %s",
                            test->machine, tmpname, test->extra);
 
     qtest_start(args);
