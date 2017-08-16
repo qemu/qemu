d3f05848fc8f8240539c7d810f12a321f57a22f9
Thomas Huth <thuth@redhat.com>
2017-08-16 16:08:48 +0200

    watchdog/wdt_diag288: Mark diag288 watchdog as non-hotpluggable
    QEMU currently aborts when the user tries to hot-unplug a diag288
    device:
    
    $ qemu-system-s390x -nographic -nodefaults -S -monitor stdio
    QEMU 2.9.92 monitor - type 'help' for more information
    (qemu) device_add diag288,id=x
    (qemu) device_del x
    **
    ERROR:qemu/qdev-monitor.c:872:qdev_unplug: assertion failed: (hotplug_ctrl)
    Aborted (core dumped)
    
    The device is not designed as hot-pluggable (it should only be used
    via the "-watchdog" parameter), so let's simply remove the possibility
    to hotplug it to prevent that users can run into this ugly situation.
    
    Signed-off-by: Thomas Huth <thuth@redhat.com>
    Message-Id: <1502892528-22618-1-git-send-email-thuth@redhat.com>
    Reviewed-by: David Hildenbrand <david@redhat.com>
    Signed-off-by: Cornelia Huck <cohuck@redhat.com>
    (cherry picked from commit 84ebd3e8c7d4fe955b359b9aac84395907b0412e)
    Signed-off-by: Michael Roth <mdroth@linux.vnet.ibm.com>

--- hw/watchdog/wdt_diag288.c
+++ hw/watchdog/wdt_diag288.c
@@ -121,6 +121,7 @@ static void wdt_diag288_class_init(ObjectClass *klass, void *data)
     dc->realize = wdt_diag288_realize;
     dc->unrealize = wdt_diag288_unrealize;
     dc->reset = wdt_diag288_reset;
+    dc->hotpluggable = false;
     set_bit(DEVICE_CATEGORY_MISC, dc->categories);
     dc->vmsd = &vmstate_diag288;
     diag288->handle_timer = wdt_diag288_handle_timer;
