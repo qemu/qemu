c50f65118b429e6847d5c11b1a20a560d61c34b7
David Hildenbrand <david@redhat.com>
2017-08-18 13:43:39 +0200

    target/s390x: simplify gs_allowed()
    No need for kvm_enabled() as this function is only called from KVM and
    there is no reason why it shouldn't be allowed for tcg. It is simply not
    available under tcg.
    
    Also, there is no need to check for the machine type anymore. Just like
    ri_enabled(), we can directly use the stored flag, which results in
    "true" for the "none" machine.
    
    Signed-off-by: David Hildenbrand <david@redhat.com>
    Message-Id: <20170818114353.13455-5-david@redhat.com>
    Reviewed-by: Thomas Huth <thuth@redhat.com>
    Signed-off-by: Cornelia Huck <cohuck@redhat.com>

--- hw/s390x/s390-virtio-ccw.c
+++ hw/s390x/s390-virtio-ccw.c
@@ -290,18 +290,8 @@ bool cpu_model_allowed(void)
 
 bool gs_allowed(void)
 {
-    if (kvm_enabled()) {
-        MachineClass *mc = MACHINE_GET_CLASS(qdev_get_machine());
-        if (object_class_dynamic_cast(OBJECT_CLASS(mc),
-                                      TYPE_S390_CCW_MACHINE)) {
-            S390CcwMachineClass *s390mc = S390_MACHINE_CLASS(mc);
-
-            return s390mc->gs_allowed;
-        }
-        /* Make sure the "none" machine can have gs */
-        return true;
-    }
-    return false;
+    /* for "none" machine this results in true */
+    return get_machine_class()->gs_allowed;
 }
 
 static char *machine_get_loadparm(Object *obj, Error **errp)
