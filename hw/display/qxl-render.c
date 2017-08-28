79c5a10cdda1aed00d7ee4ef87de2ef8c854f4a5
Gerd Hoffmann <kraxel@redhat.com>
2017-08-28 14:39:32 +0200

    qxl: drop mono cursor support
    The chunk size sanity check in qxl_render_cursor works for
    SPICE_CURSOR_TYPE_ALPHA cursors only.  So support for
    SPICE_CURSOR_TYPE_MONO cursors must be broken for ages without anyone
    noticing.  Most likely it simply isn't used any more by guest drivers.
    Drop the dead code.
    
    Signed-off-by: Gerd Hoffmann <kraxel@redhat.com>
    Message-id: 20170828123933.30323-2-kraxel@redhat.com

--- hw/display/qxl-render.c
+++ hw/display/qxl-render.c
@@ -207,7 +207,6 @@ void qxl_render_update_area_done(PCIQXLDevice *qxl, QXLCookie *cookie)
 static QEMUCursor *qxl_cursor(PCIQXLDevice *qxl, QXLCursor *cursor)
 {
     QEMUCursor *c;
-    uint8_t *image, *mask;
     size_t size;
 
     c = cursor_alloc(cursor->header.width, cursor->header.height);
@@ -219,14 +218,6 @@ static QEMUCursor *qxl_cursor(PCIQXLDevice *qxl, QXLCursor *cursor)
         memcpy(c->data, cursor->chunk.data, size);
         if (qxl->debug > 2) {
             cursor_print_ascii_art(c, "qxl/alpha");
-        }
-        break;
-    case SPICE_CURSOR_TYPE_MONO:
-        mask  = cursor->chunk.data;
-        image = mask + cursor_get_mono_bpl(c) * c->width;
-        cursor_set_mono(c, 0xffffff, 0x000000, image, 1, mask);
-        if (qxl->debug > 2) {
-            cursor_print_ascii_art(c, "qxl/mono");
         }
         break;
     default:
