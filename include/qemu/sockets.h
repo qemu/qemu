bcd4dfd6852361361d6622de7e67f94e2b475d27
Mao Zhongyi <maozy.fnst@cn.fujitsu.com>
2017-09-04 22:35:39 +0800

    net/net: Convert parse_host_port() to Error
    Cc: berrange@redhat.com
    Cc: kraxel@redhat.com
    Cc: pbonzini@redhat.com
    Cc: jasowang@redhat.com
    Cc: armbru@redhat.com
    Cc: eblake@redhat.com
    Signed-off-by: Mao Zhongyi <maozy.fnst@cn.fujitsu.com>
    Reviewed-by: Markus Armbruster <armbru@redhat.com>
    Signed-off-by: Jason Wang <jasowang@redhat.com>

--- include/qemu/sockets.h
+++ include/qemu/sockets.h
@@ -45,7 +45,8 @@ void socket_listen_cleanup(int fd, Error **errp);
 int socket_dgram(SocketAddress *remote, SocketAddress *local, Error **errp);
 
 /* Old, ipv4 only bits.  Don't use for new code. */
-int parse_host_port(struct sockaddr_in *saddr, const char *str);
+int parse_host_port(struct sockaddr_in *saddr, const char *str,
+                    Error **errp);
 int socket_init(void);
 
 /**
--- net/socket.c
+++ net/socket.c
@@ -343,8 +343,7 @@ static NetSocketState *net_socket_fd_init_dgram(NetClientState *peer,
      */
 
     if (is_connected && mcast != NULL) {
-            if (parse_host_port(&saddr, mcast) < 0) {
-                error_setg(errp, "fd=%d failed parse_host_port()", fd);
+            if (parse_host_port(&saddr, mcast, errp) < 0) {
                 goto err;
             }
             /* must be bound */
@@ -496,9 +495,12 @@ static int net_socket_listen_init(NetClientState *peer,
     NetSocketState *s;
     struct sockaddr_in saddr;
     int fd, ret;
+    Error *err = NULL;
 
-    if (parse_host_port(&saddr, host_str) < 0)
+    if (parse_host_port(&saddr, host_str, &err) < 0) {
+        error_report_err(err);
         return -1;
+    }
 
     fd = qemu_socket(PF_INET, SOCK_STREAM, 0);
     if (fd < 0) {
@@ -543,8 +545,10 @@ static int net_socket_connect_init(NetClientState *peer,
     struct sockaddr_in saddr;
     Error *err = NULL;
 
-    if (parse_host_port(&saddr, host_str) < 0)
+    if (parse_host_port(&saddr, host_str, &err) < 0) {
+        error_report_err(err);
         return -1;
+    }
 
     fd = qemu_socket(PF_INET, SOCK_STREAM, 0);
     if (fd < 0) {
@@ -597,8 +601,10 @@ static int net_socket_mcast_init(NetClientState *peer,
     struct in_addr localaddr, *param_localaddr;
     Error *err = NULL;
 
-    if (parse_host_port(&saddr, host_str) < 0)
+    if (parse_host_port(&saddr, host_str, &err) < 0) {
+        error_report_err(err);
         return -1;
+    }
 
     if (localaddr_str != NULL) {
         if (inet_aton(localaddr_str, &localaddr) == 0)
@@ -640,11 +646,13 @@ static int net_socket_udp_init(NetClientState *peer,
     struct sockaddr_in laddr, raddr;
     Error *err = NULL;
 
-    if (parse_host_port(&laddr, lhost) < 0) {
+    if (parse_host_port(&laddr, lhost, &err) < 0) {
+        error_report_err(err);
         return -1;
     }
 
-    if (parse_host_port(&raddr, rhost) < 0) {
+    if (parse_host_port(&raddr, rhost, &err) < 0) {
+        error_report_err(err);
         return -1;
     }
 
