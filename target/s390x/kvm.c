01c36195d6ed421ab913937c03ae16d2e0cdff38
Yi Min Zhao <zyimin@linux.vnet.ibm.com>
2017-09-05 12:12:59 +0200

    s390x/pci: fixup ind_offset of msix routing entry
    The guest uses the mpcifc instruction to register the aibvo of a zpci
    device, which is the starting offset of indicators in the indicator
    area and thus remains constant. Each msix vector is an offset from the
    aibvo. When we map a msix route to an adapter route, we should not
    modify the starting offset, but instead add the vector to the starting
    offset to get the absolute offset in the specific route.
    
    Signed-off-by: Yi Min Zhao <zyimin@linux.vnet.ibm.com>
    Message-Id: <1504606380-49341-3-git-send-email-zyimin@linux.vnet.ibm.com>
    Signed-off-by: Cornelia Huck <cohuck@redhat.com>

--- target/s390x/kvm.c
+++ target/s390x/kvm.c
@@ -2436,14 +2436,12 @@ int kvm_arch_fixup_msi_route(struct kvm_irq_routing_entry *route,
         return -ENODEV;
     }
 
-    pbdev->routes.adapter.ind_offset = vec;
-
     route->type = KVM_IRQ_ROUTING_S390_ADAPTER;
     route->flags = 0;
     route->u.adapter.summary_addr = pbdev->routes.adapter.summary_addr;
     route->u.adapter.ind_addr = pbdev->routes.adapter.ind_addr;
     route->u.adapter.summary_offset = pbdev->routes.adapter.summary_offset;
-    route->u.adapter.ind_offset = pbdev->routes.adapter.ind_offset;
+    route->u.adapter.ind_offset = pbdev->routes.adapter.ind_offset + vec;
     route->u.adapter.adapter_id = pbdev->routes.adapter.adapter_id;
     return 0;
 }
