a75d6f07613af7ec5b016b31b117436e32ce7a5f
Brandon Carpenter <brandon.carpenter@cypherpath.com>
2017-09-12 08:21:47 -0700

    ui: Always remove an old VNC channel watch before adding a new one
    Also set saved handle to zero when removing without adding a new watch.
    
    Signed-off-by: Brandon Carpenter <brandon.carpenter@cypherpath.com>
    Reviewed-by: Paolo Bonzini <pbonzini@redhat.com>
    Reviewed-by: Daniel P. Berrange <berrange@redhat.com>

--- ui/vnc-auth-vencrypt.c
+++ ui/vnc-auth-vencrypt.c
@@ -75,6 +75,9 @@ static void vnc_tls_handshake_done(QIOTask *task,
         vnc_client_error(vs);
         error_free(err);
     } else {
+        if (vs->ioc_tag) {
+            g_source_remove(vs->ioc_tag);
+        }
         vs->ioc_tag = qio_channel_add_watch(
             vs->ioc, G_IO_IN | G_IO_OUT, vnc_client_io, vs, NULL);
         start_auth_vencrypt_subauth(vs);
