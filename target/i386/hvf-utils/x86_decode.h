996feed462fa7f249f03ffb0520c2ef0c5bc3312
Sergio Andres Gomez Del Real <sergio.g.delreal@gmail.com>
2017-09-13 04:05:11 -0500

    i386: hvf: fix licensing issues; isolate task handling code (GPL v2-only)
    This patch replaces the license header for those files that were either
    GPL v2-or-v3, or GPL v2-only; the replacing license is GPL v2-or-later.
    The code for task switching/handling, which is derived from KVM and
    hence is GPL v2-only, is isolated in the new files (with this license)
    x86_task.c/.h, and the corresponding compilation rule is added to
    target/i386/hvf-utils/Makefile.objs.
    
    Signed-off-by: Sergio Andres Gomez Del Real <Sergio.G.DelReal@gmail.com>
    Message-Id: <20170913090522.4022-4-Sergio.G.DelReal@gmail.com>
    Signed-off-by: Paolo Bonzini <pbonzini@redhat.com>

--- target/i386/hvf-utils/x86_decode.h
+++ target/i386/hvf-utils/x86_decode.h
@@ -2,17 +2,17 @@
  * Copyright (C) 2016 Veertu Inc,
  *
  * This program is free software; you can redistribute it and/or
- * modify it under the terms of the GNU General Public License as
- * published by the Free Software Foundation; either version 2 or
- * (at your option) version 3 of the License.
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2 of the License, or (at your option) any later version.
  *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
  *
- * You should have received a copy of the GNU General Public License along
- * with this program; if not, see <http://www.gnu.org/licenses/>.
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this program; if not, see <http://www.gnu.org/licenses/>.
  */
 
 #pragma once
