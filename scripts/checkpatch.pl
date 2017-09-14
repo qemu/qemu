825bfa0052a684f71f36693976fabad185e203c4
Greg Kurz <groug@kaod.org>
2017-09-14 11:12:07 +0200

    checkpatch: add hwaddr to @typeList
    The script doesn't know about all possible types and learn them as
    it parses the code. If it reaches a line with a type cast but the
    type isn't known yet, it is misinterpreted as an identifier.
    
    For example the following line:
    
        foo = (hwaddr) -1;
    
    results in the following false-positive to be reported:
    
    ERROR: spaces required around that '-' (ctx:VxV)
    
    Let's add this standard QEMU type to the list of pre-known types.
    
    Signed-off-by: Greg Kurz <groug@kaod.org>
    Message-Id: <150538015789.8149.10902725348939486674.stgit@bahia.lan>
    Signed-off-by: Paolo Bonzini <pbonzini@redhat.com>

--- scripts/checkpatch.pl
+++ scripts/checkpatch.pl
@@ -263,6 +263,7 @@ our @typeList = (
 	qr{${Ident}_handler},
 	qr{${Ident}_handler_fn},
 	qr{target_(?:u)?long},
+	qr{hwaddr},
 );
 
 # This can be modified by sub possible.  Since it can be empty, be careful
