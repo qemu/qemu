49958d37e7fcacb71512c781d21de2bb69e1c16b
Vladimir Sementsov-Ogievskiy <vsementsov@virtuozzo.com>
2017-09-29 18:22:55 +0300

    block/mirror: check backing in bdrv_mirror_top_flush
    Backing may be zero after failed bdrv_append in mirror_start_job,
    which leads to SIGSEGV.
    
    Signed-off-by: Vladimir Sementsov-Ogievskiy <vsementsov@virtuozzo.com>
    Message-id: 20170929152255.5431-1-vsementsov@virtuozzo.com
    Signed-off-by: Max Reitz <mreitz@redhat.com>
    (cherry picked from commit ce960aa9062a407d0ca15aee3dcd3bd84a4e24f9)
    Signed-off-by: Michael Roth <mdroth@linux.vnet.ibm.com>

--- block/mirror.c
+++ block/mirror.c
@@ -1056,6 +1056,10 @@ static int coroutine_fn bdrv_mirror_top_pwritev(BlockDriverState *bs,
 
 static int coroutine_fn bdrv_mirror_top_flush(BlockDriverState *bs)
 {
+    if (bs->backing == NULL) {
+        /* we can be here after failed bdrv_append in mirror_start_job */
+        return 0;
+    }
     return bdrv_co_flush(bs->backing->bs);
 }
 
