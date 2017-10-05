0255db239eba48669f93e12974ab3d68ca5d6a13
Igor Mammedov <imammedo@redhat.com>
2017-10-05 15:50:49 +0200

    moxie: cleanup cpu type name composition
    introduce MOXIE_CPU_TYPE_NAME macro and consistently use it
    to construct cpu type names. While at it replace dynamic
    cpu type name composition with static data.
    
    Signed-off-by: Igor Mammedov <imammedo@redhat.com>
    Reviewed-by: Philippe Mathieu-Daudé <f4bug@amsat.org>
    Message-Id: <1507211474-188400-16-git-send-email-imammedo@redhat.com>
    Signed-off-by: Eduardo Habkost <ehabkost@redhat.com>

--- target/moxie/cpu.h
+++ target/moxie/cpu.h
@@ -122,6 +122,9 @@ int cpu_moxie_signal_handler(int host_signum, void *pinfo,
 
 #define cpu_init(cpu_model) cpu_generic_init(TYPE_MOXIE_CPU, cpu_model)
 
+#define MOXIE_CPU_TYPE_SUFFIX "-" TYPE_MOXIE_CPU
+#define MOXIE_CPU_TYPE_NAME(model) model MOXIE_CPU_TYPE_SUFFIX
+
 #define cpu_signal_handler cpu_moxie_signal_handler
 
 static inline int cpu_mmu_index(CPUMoxieState *env, bool ifetch)
