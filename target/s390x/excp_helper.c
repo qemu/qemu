2bcf018340cbf233f7145e643fc1bb367f23fd90
David Hildenbrand <david@redhat.com>
2017-10-16 22:23:58 +0200

    s390x/tcg: low-address protection support
    This is a neat way to implement low address protection, whereby
    only the first 512 bytes of the first two pages (each 4096 bytes) of
    every address space are protected.
    
    Store a tec of 0 for the access exception, this is what is defined by
    Enhanced Suppression on Protection in case of a low address protection
    (Bit 61 set to 0, rest undefined).
    
    We have to make sure to to pass the access address, not the masked page
    address into mmu_translate*().
    
    Drop the check from testblock. So we can properly test this via
    kvm-unit-tests.
    
    This will check every access going through one of the MMUs.
    
    Reviewed-by: Richard Henderson <richard.henderson@linaro.org>
    Signed-off-by: David Hildenbrand <david@redhat.com>
    Message-Id: <20171016202358.3633-3-david@redhat.com>
    [CH: restored error message for access register mode]
    Signed-off-by: Cornelia Huck <cohuck@redhat.com>

--- target/s390x/excp_helper.c
+++ target/s390x/excp_helper.c
@@ -95,7 +95,6 @@ int s390_cpu_handle_mmu_fault(CPUState *cs, vaddr orig_vaddr,
     DPRINTF("%s: address 0x%" VADDR_PRIx " rw %d mmu_idx %d\n",
             __func__, orig_vaddr, rw, mmu_idx);
 
-    orig_vaddr &= TARGET_PAGE_MASK;
     vaddr = orig_vaddr;
 
     if (mmu_idx < MMU_REAL_IDX) {
@@ -127,7 +126,7 @@ int s390_cpu_handle_mmu_fault(CPUState *cs, vaddr orig_vaddr,
     qemu_log_mask(CPU_LOG_MMU, "%s: set tlb %" PRIx64 " -> %" PRIx64 " (%x)\n",
             __func__, (uint64_t)vaddr, (uint64_t)raddr, prot);
 
-    tlb_set_page(cs, orig_vaddr, raddr, prot,
+    tlb_set_page(cs, orig_vaddr & TARGET_PAGE_MASK, raddr, prot,
                  mmu_idx, TARGET_PAGE_SIZE);
 
     return 0;
