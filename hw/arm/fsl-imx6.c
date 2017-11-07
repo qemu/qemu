70fbd3c4bf9850fce733eea2c910c397905fb9a3
Thomas Huth <thuth@redhat.com>
2017-11-07 13:03:51 +0000

    hw/arm: Mark the "fsl,imx6" device with user_creatable = false
    This device causes QEMU to abort if the user tries to instantiate it:
    
    $ qemu-system-aarch64 -M sabrelite -smp 1,maxcpus=2 -device fsl,,imx6
    Unexpected error in qemu_chr_fe_init() at chardev/char-fe.c:222:
    qemu-system-aarch64: -device fsl,,imx6: Device 'serial0' is in use
    Aborted (core dumped)
    
    The device uses serial_hds[] directly in its realize function, so it
    can not be instantiated again by the user.
    
    Signed-off-by: Thomas Huth <thuth@redhat.com>
    Message-id: 1509519537-6964-2-git-send-email-thuth@redhat.com
    Reviewed-by: Peter Maydell <peter.maydell@linaro.org>
    Signed-off-by: Peter Maydell <peter.maydell@linaro.org>

--- hw/arm/fsl-imx6.c
+++ hw/arm/fsl-imx6.c
@@ -440,8 +440,9 @@ static void fsl_imx6_class_init(ObjectClass *oc, void *data)
     DeviceClass *dc = DEVICE_CLASS(oc);
 
     dc->realize = fsl_imx6_realize;
-
     dc->desc = "i.MX6 SOC";
+    /* Reason: Uses serial_hds[] in the realize() function */
+    dc->user_creatable = false;
 }
 
 static const TypeInfo fsl_imx6_type_info = {
