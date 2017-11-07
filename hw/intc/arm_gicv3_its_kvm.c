8a7348b5d62d7ea16807e6bea54b448a0184bb0f
Eric Auger <eric.auger@redhat.com>
2017-11-07 13:03:52 +0000

    hw/intc/arm_gicv3_its: Don't abort on table save failure
    The ITS is not fully properly reset at the moment. Caches are
    not emptied.
    
    After a reset, in case we attempt to save the state before
    the bound devices have registered their MSIs and after the
    1st level table has been allocated by the ITS driver
    (device BASER is valid), the first level entries are still
    invalid. If the device cache is not empty (devices registered
    before the reset), vgic_its_save_device_tables fails with -EINVAL.
    This causes a QEMU abort().
    
    Cc: qemu-stable@nongnu.org
    Signed-off-by: Eric Auger <eric.auger@redhat.com>
    Reported-by: wanghaibin <wanghaibin.wang@huawei.com>
    Reviewed-by: Peter Maydell <peter.maydell@linaro.org>
    Signed-off-by: Peter Maydell <peter.maydell@linaro.org>

--- hw/intc/arm_gicv3_its_kvm.c
+++ hw/intc/arm_gicv3_its_kvm.c
@@ -64,19 +64,15 @@ static void vm_change_state_handler(void *opaque, int running,
 {
     GICv3ITSState *s = (GICv3ITSState *)opaque;
     Error *err = NULL;
-    int ret;
 
     if (running) {
         return;
     }
 
-    ret = kvm_device_access(s->dev_fd, KVM_DEV_ARM_VGIC_GRP_CTRL,
-                            KVM_DEV_ARM_ITS_SAVE_TABLES, NULL, true, &err);
+    kvm_device_access(s->dev_fd, KVM_DEV_ARM_VGIC_GRP_CTRL,
+                      KVM_DEV_ARM_ITS_SAVE_TABLES, NULL, true, &err);
     if (err) {
         error_report_err(err);
-    }
-    if (ret < 0 && ret != -EFAULT) {
-        abort();
     }
 }
 
