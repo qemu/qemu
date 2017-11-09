777c5f1e436d334a57b650b6951c13d8d2799df0
Gerd Hoffmann <kraxel@redhat.com>
2017-11-09 11:51:54 +0100

    ui: fix dcl unregister
    register checks for dcl->ds being NULL, to avoid registering
    the same dcl twice.
    
    Therefore dcl->ds must be cleared on unregister, otherwise
    un-registering and re-registering doesn't work.
    
    Fixes: https://bugzilla.redhat.com/show_bug.cgi?id=1510809
    Signed-off-by: Gerd Hoffmann <kraxel@redhat.com>
    Reviewed-by: Marc-André Lureau <marcandre.lureau@redhat.com>
    Message-id: 20171109105154.29414-1-kraxel@redhat.com

--- ui/console.c
+++ ui/console.c
@@ -1471,6 +1471,7 @@ void unregister_displaychangelistener(DisplayChangeListener *dcl)
         dcl->con->dcls--;
     }
     QLIST_REMOVE(dcl, next);
+    dcl->ds = NULL;
     gui_setup_refresh(ds);
 }
 
