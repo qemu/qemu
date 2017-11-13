1342b0355ed943ca4ad4e67e03390b69476b8a6b
Emilio G. Cota <cota@braap.org>
2017-11-13 13:55:26 +0000

    xlnx-zcu102: Specify the max number of CPUs for the EP108
    Just like the zcu102, the ep108 can instantiate several CPUs.
    
    Signed-off-by: Emilio G. Cota <cota@braap.org>
    Reviewed-by: Alistair Francis <alistair.francis@xilinx.com>
    Message-id: 1510343626-25861-5-git-send-email-cota@braap.org
    Signed-off-by: Peter Maydell <peter.maydell@linaro.org>

--- hw/arm/xlnx-zcu102.c
+++ hw/arm/xlnx-zcu102.c
@@ -188,6 +188,7 @@ static void xlnx_ep108_machine_class_init(ObjectClass *oc, void *data)
     mc->block_default_type = IF_IDE;
     mc->units_per_default_bus = 1;
     mc->ignore_memory_transaction_failures = true;
+    mc->max_cpus = XLNX_ZYNQMP_NUM_APU_CPUS + XLNX_ZYNQMP_NUM_RPU_CPUS;
 }
 
 static const TypeInfo xlnx_ep108_machine_init_typeinfo = {
