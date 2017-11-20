1f4ad7d3b8f7162ec0471506d86f57a5d77b8f76
Kevin Wolf <kwolf@redhat.com>
2017-11-20 15:02:48 +0100

    block: Don't request I/O permission with BDRV_O_NO_IO
    'qemu-img info' makes sense even when BLK_PERM_CONSISTENT_READ cannot be
    granted because of a block job in a running qemu process. It already
    sets BDRV_O_NO_IO to indicate that it doesn't access the guest visible
    data at all.
    
    Check the BDRV_O_NO_IO flags in blk_new_open(), so that I/O related
    permissions are not unnecessarily requested and 'qemu-img info' can work
    even if BLK_PERM_CONSISTENT_READ cannot be granted.
    
    Signed-off-by: Kevin Wolf <kwolf@redhat.com>
    Reviewed-by: Fam Zheng <famz@redhat.com>
    Reviewed-by: Alberto Garcia <berto@igalia.com>

--- block/block-backend.c
+++ block/block-backend.c
@@ -299,7 +299,7 @@ BlockBackend *blk_new_open(const char *filename, const char *reference,
 {
     BlockBackend *blk;
     BlockDriverState *bs;
-    uint64_t perm;
+    uint64_t perm = 0;
 
     /* blk_new_open() is mainly used in .bdrv_create implementations and the
      * tools where sharing isn't a concern because the BDS stays private, so we
@@ -309,9 +309,11 @@ BlockBackend *blk_new_open(const char *filename, const char *reference,
      * caller of blk_new_open() doesn't make use of the permissions, but they
      * shouldn't hurt either. We can still share everything here because the
      * guest devices will add their own blockers if they can't share. */
-    perm = BLK_PERM_CONSISTENT_READ;
-    if (flags & BDRV_O_RDWR) {
-        perm |= BLK_PERM_WRITE;
+    if ((flags & BDRV_O_NO_IO) == 0) {
+        perm |= BLK_PERM_CONSISTENT_READ;
+        if (flags & BDRV_O_RDWR) {
+            perm |= BLK_PERM_WRITE;
+        }
     }
     if (flags & BDRV_O_RESIZE) {
         perm |= BLK_PERM_RESIZE;
