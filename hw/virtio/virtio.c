8fc47c876de638353bb635872f2c25bb7f4a3d6e
Michael S. Tsirkin <mst@redhat.com>
2017-12-13 21:59:54 +0200

    virtio_error: don't invoke status callbacks
    Backends don't need to know what frontend requested a reset,
    and notifying then from virtio_error is messy because
    virtio_error itself might be invoked from backend.
    
    Let's just set the status directly.
    
    Cc: qemu-stable@nongnu.org
    Reported-by: Ilya Maximets <i.maximets@samsung.com>
    Signed-off-by: Michael S. Tsirkin <mst@redhat.com>

--- hw/virtio/virtio.c
+++ hw/virtio/virtio.c
@@ -2469,7 +2469,7 @@ void GCC_FMT_ATTR(2, 3) virtio_error(VirtIODevice *vdev, const char *fmt, ...)
     va_end(ap);
 
     if (virtio_vdev_has_feature(vdev, VIRTIO_F_VERSION_1)) {
-        virtio_set_status(vdev, vdev->status | VIRTIO_CONFIG_S_NEEDS_RESET);
+        vdev->status = vdev->status | VIRTIO_CONFIG_S_NEEDS_RESET;
         virtio_notify_config(vdev);
     }
 
