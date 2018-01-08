db3b3c7281ca82e2647e072a1f97db111313dd73
Greg Kurz <groug@kaod.org>
2018-01-08 11:18:23 +0100

    9pfs: deprecate handle backend
    This backend raise some concerns:
    
    - doesn't support symlinks
    - fails +100 tests in the PJD POSIX file system test suite [1]
    - requires the QEMU process to run with the CAP_DAC_READ_SEARCH
      capability, which isn't recommended for security reasons
    
    This backend should not be used and wil be removed. The 'local'
    backend is the recommended alternative.
    
    [1] https://www.tuxera.com/community/posix-test-suite/
    
    Signed-off-by: Greg Kurz <groug@kaod.org>
    Reviewed-by: Daniel P. Berrange <berrange@redhat.com>
    Reviewed-by: Aneesh Kumar K.V <aneesh.kumar@linux.vnet.ibm.com>

--- hw/9pfs/9p-handle.c
+++ hw/9pfs/9p-handle.c
@@ -657,6 +657,8 @@ static int handle_parse_opts(QemuOpts *opts, FsDriverEntry *fse, Error **errp)
     const char *sec_model = qemu_opt_get(opts, "security_model");
     const char *path = qemu_opt_get(opts, "path");
 
+    warn_report("handle backend is deprecated");
+
     if (sec_model) {
         error_report("Invalid argument security_model specified with handle fsdriver");
         return -1;
