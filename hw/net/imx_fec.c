a6383e99ffc3b615d8465aebbd5d48f1fa9b2949
Andrey Smirnov <andrew.smirnov@gmail.com>
2018-01-11 13:25:35 +0000

    imx_fec: Refactor imx_eth_enable_rx()
    Refactor imx_eth_enable_rx() to have more meaningfull variable name
    than 'tmp' and to reduce number of logical negations done.
    
    Cc: Peter Maydell <peter.maydell@linaro.org>
    Cc: Jason Wang <jasowang@redhat.com>
    Cc: Philippe Mathieu-Daudé <f4bug@amsat.org>
    Cc: qemu-devel@nongnu.org
    Cc: qemu-arm@nongnu.org
    Cc: yurovsky@gmail.com
    Reviewed-by: Philippe Mathieu-Daudé <f4bug@amsat.org>
    Reviewed-by: Peter Maydell <peter.maydell@linaro.org>
    Signed-off-by: Andrey Smirnov <andrew.smirnov@gmail.com>
    Signed-off-by: Peter Maydell <peter.maydell@linaro.org>

--- hw/net/imx_fec.c
+++ hw/net/imx_fec.c
@@ -536,19 +536,19 @@ static void imx_eth_do_tx(IMXFECState *s)
 static void imx_eth_enable_rx(IMXFECState *s)
 {
     IMXFECBufDesc bd;
-    bool tmp;
+    bool rx_ring_full;
 
     imx_fec_read_bd(&bd, s->rx_descriptor);
 
-    tmp = ((bd.flags & ENET_BD_E) != 0);
+    rx_ring_full = !(bd.flags & ENET_BD_E);
 
-    if (!tmp) {
+    if (rx_ring_full) {
         FEC_PRINTF("RX buffer full\n");
     } else if (!s->regs[ENET_RDAR]) {
         qemu_flush_queued_packets(qemu_get_queue(s->nic));
     }
 
-    s->regs[ENET_RDAR] = tmp ? ENET_RDAR_RDAR : 0;
+    s->regs[ENET_RDAR] = rx_ring_full ? 0 : ENET_RDAR_RDAR;
 }
 
 static void imx_eth_reset(DeviceState *d)
