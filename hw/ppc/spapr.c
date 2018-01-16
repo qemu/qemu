ad484114d18d7d82cee83f870825cb395339d071
David Gibson <david@gibson.dropbear.id.au>
2018-01-16 15:37:37 +1100

    spapr: Allow some cases where we can't set VSMT mode in the kernel
    At present if we require a vsmt mode that's not equal to the kernel's
    default, and the kernel doesn't let us change it (e.g. because it's an old
    kernel without support) then we always fail.
    
    But in fact we can cope with the kernel having a different vsmt as long as
      a) it's >= the actual number of vthreads/vcore (so that guest threads
         that are supposed to be on the same core act like it)
      b) it's a submultiple of the requested vsmt mode (so that guest threads
         spaced by the vsmt value will act like they're on different cores)
    
    Allowing this case gives us a bit more freedom to adjust the vsmt behaviour
    without breaking existing cases.
    
    Signed-off-by: David Gibson <david@gibson.dropbear.id.au>
    Reviewed-by: Laurent Vivier <lvivier@redhat.com>
    Tested-by: Greg Kurz <groug@kaod.org>
    Reviewed-by: Greg Kurz <groug@kaod.org>
    (cherry picked from commit 1f20f2e0ee61d91abff4e86ed1cda1b5244647d3)
    Signed-off-by: Greg Kurz <groug@kaod.org>

--- hw/ppc/spapr.c
+++ hw/ppc/spapr.c
@@ -2291,17 +2291,29 @@ static void spapr_set_vsmt_mode(sPAPRMachineState *spapr, Error **errp)
     if (kvm_enabled() && (spapr->vsmt != kvm_smt)) {
         ret = kvmppc_set_smt_threads(spapr->vsmt);
         if (ret) {
+            /* Looks like KVM isn't able to change VSMT mode */
             error_setg(&local_err,
                        "Failed to set KVM's VSMT mode to %d (errno %d)",
                        spapr->vsmt, ret);
-            if (!vsmt_user) {
-                error_append_hint(&local_err, "On PPC, a VM with %d threads/"
-                             "core on a host with %d threads/core requires "
-                             " the use of VSMT mode %d.\n",
-                             smp_threads, kvm_smt, spapr->vsmt);
+            /* We can live with that if the default one is big enough
+             * for the number of threads, and a submultiple of the one
+             * we want.  In this case we'll waste some vcpu ids, but
+             * behaviour will be correct */
+            if ((kvm_smt >= smp_threads) && ((spapr->vsmt % kvm_smt) == 0)) {
+                warn_report_err(local_err);
+                local_err = NULL;
+                goto out;
+            } else {
+                if (!vsmt_user) {
+                    error_append_hint(&local_err,
+                                      "On PPC, a VM with %d threads/core"
+                                      " on a host with %d threads/core"
+                                      " requires the use of VSMT mode %d.\n",
+                                      smp_threads, kvm_smt, spapr->vsmt);
+                }
+                kvmppc_hint_smt_possible(&local_err);
+                goto out;
             }
-            kvmppc_hint_smt_possible(&local_err);
-            goto out;
         }
     }
     /* else TCG: nothing to do currently */
