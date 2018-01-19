d7aa3d0a0af9efbdfccb4db05418068a21603788
Suraj Jitindar Singh <sjitindarsingh@gmail.com>
2018-01-19 16:00:03 +1100

    target/ppc/spapr_caps: Add new tristate cap safe_bounds_check
    Add new tristate cap cap-sbbc to represent the speculation barrier
    bounds checking capability.
    
    Signed-off-by: Suraj Jitindar Singh <sjitindarsingh@gmail.com>
    Reviewed-by: David Gibson <david@gibson.dropbear.id.au>
    Signed-off-by: David Gibson <david@gibson.dropbear.id.au>
    (cherry picked from commit 09114fd8179977e4157b36aab2e3d68eaf08adca)
    Signed-off-by: Michael Roth <mdroth@linux.vnet.ibm.com>

--- hw/ppc/spapr.c
+++ hw/ppc/spapr.c
@@ -1766,6 +1766,7 @@ static const VMStateDescription vmstate_spapr = {
         &vmstate_spapr_cap_vsx,
         &vmstate_spapr_cap_dfp,
         &vmstate_spapr_cap_cfpc,
+        &vmstate_spapr_cap_sbbc,
         NULL
     }
 };
@@ -3711,6 +3712,7 @@ static void spapr_machine_class_init(ObjectClass *oc, void *data)
     smc->default_caps.caps[SPAPR_CAP_VSX] = SPAPR_CAP_ON;
     smc->default_caps.caps[SPAPR_CAP_DFP] = SPAPR_CAP_ON;
     smc->default_caps.caps[SPAPR_CAP_CFPC] = SPAPR_CAP_BROKEN;
+    smc->default_caps.caps[SPAPR_CAP_SBBC] = SPAPR_CAP_BROKEN;
     spapr_caps_add_properties(smc, &error_abort);
 }
 
