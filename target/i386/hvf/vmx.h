d781e24d05dfc7737af577ad9f03e9064015c337
Izik Eidus <izik@veertu.com>
2018-01-23 14:36:38 +0200

    Add missing hvdos public domain attribution:
    hvf.c and vmx.h contain code from hvdos.c that is released as public domain:
    
    from hvdos github: https://github.com/mist64/hvdos
    
    "License
    
    See LICENSE.txt (2-clause-BSD).
    
    In order to simplify use of this code as a template, you can consider any parts from "hvdos.c" and "interface.h" as being in the public domain."
    
    Signed-off-by: Izik Eidus <izik@veertu.com>
    Message-Id: <20180123123639.35255-2-izik@veertu.com>
    Signed-off-by: Paolo Bonzini <pbonzini@redhat.com>

--- target/i386/hvf/vmx.h
+++ target/i386/hvf/vmx.h
@@ -17,6 +17,9 @@
  *
  * You should have received a copy of the GNU Lesser General Public
  * License along with this program; if not, see <http://www.gnu.org/licenses/>.
+ *
+ * This file contain code under public domain from the hvdos project:
+ * https://github.com/mist64/hvdos
  */
 
 #ifndef VMX_H
