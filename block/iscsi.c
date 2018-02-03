83d11973fa78be5bf0fd0e00791245e974fe4af3
Stefan Hajnoczi <stefanha@redhat.com>
2018-02-03 07:16:20 +0100

    block/iscsi: take iscsilun->mutex in iscsi_timed_check_events()
    Commit d045c466d9e62b4321fadf586d024d54ddfd8bd4 ("iscsi: do not use
    aio_context_acquire/release") introduced iscsilun->mutex but appears to
    have overlooked iscsi_timed_check_events() when introducing the mutex.
    
    iscsi_service() and iscsi_set_events() must be called with
    iscsilun->mutex held.
    
    iscsi_timed_check_events() is invoked from the AioContext and does not
    take the mutex.
    
    Signed-off-by: Stefan Hajnoczi <stefanha@redhat.com>
    Message-Id: <20180203061621.7033-3-stefanha@redhat.com>
    Reviewed-by: Paolo Bonzini <pbonzini@redhat.com>
    Signed-off-by: Paolo Bonzini <pbonzini@redhat.com>

--- block/iscsi.c
+++ block/iscsi.c
@@ -344,6 +344,8 @@ static void iscsi_timed_check_events(void *opaque)
 {
     IscsiLun *iscsilun = opaque;
 
+    qemu_mutex_lock(&iscsilun->mutex);
+
     /* check for timed out requests */
     iscsi_service(iscsilun->iscsi, 0);
 
@@ -355,6 +357,8 @@ static void iscsi_timed_check_events(void *opaque)
     /* newer versions of libiscsi may return zero events. Ensure we are able
      * to return to service once this situation changes. */
     iscsi_set_events(iscsilun);
+
+    qemu_mutex_unlock(&iscsilun->mutex);
 
     timer_mod(iscsilun->event_timer,
               qemu_clock_get_ms(QEMU_CLOCK_REALTIME) + EVENT_INTERVAL);
