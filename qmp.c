1ef1cee7db805b97f000df2cb0712b50e75aef6c
Markus Armbruster <armbru@redhat.com>
2018-02-11 10:36:06 +0100

    Fix up dangling references to qmp-commands.* in comment and doc
    Fix up the reference to qmp-commands.hx in qmp.c.  Missed in commit
    5032a16d1d.
    
    Fix up the reference to qmp-commands.txt in
    docs/xen-save-devices-state.txt.  Missed in commit 4d8bb958fa.
    
    Signed-off-by: Markus Armbruster <armbru@redhat.com>
    Message-Id: <20180211093607.27351-29-armbru@redhat.com>
    Reviewed-by: Eric Blake <eblake@redhat.com>
    Reviewed-by: Marc-André Lureau <marcandre.lureau@redhat.com>
    Reviewed-by: Michael Roth <mdroth@linux.vnet.ibm.com>
    Signed-off-by: Eric Blake <eblake@redhat.com>

--- qmp.c
+++ qmp.c
@@ -147,13 +147,13 @@ VncInfo2List *qmp_query_vnc_servers(Error **errp)
 
 #ifndef CONFIG_SPICE
 /*
- * qmp-commands.hx ensures that QMP command query-spice exists only
- * #ifdef CONFIG_SPICE.  Necessary for an accurate query-commands
- * result.  However, the QAPI schema is blissfully unaware of that,
- * and the QAPI code generator happily generates a dead
- * qmp_marshal_query_spice() that calls qmp_query_spice().  Provide it
- * one, or else linking fails.  FIXME Educate the QAPI schema on
- * CONFIG_SPICE.
+ * qmp_unregister_commands_hack() ensures that QMP command query-spice
+ * exists only #ifdef CONFIG_SPICE.  Necessary for an accurate
+ * query-commands result.  However, the QAPI schema is blissfully
+ * unaware of that, and the QAPI code generator happily generates a
+ * dead qmp_marshal_query_spice() that calls qmp_query_spice().
+ * Provide it one, or else linking fails.  FIXME Educate the QAPI
+ * schema on CONFIG_SPICE.
  */
 SpiceInfo *qmp_query_spice(Error **errp)
 {
