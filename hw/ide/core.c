13471a40c13bc899c73f1879681da29da292547e
Stefan Hajnoczi <stefanha@redhat.com>
2018-02-16 16:50:15 +0000

    Revert "IDE: Do not flush empty CDROM drives"
    This reverts commit 4da97120d51a4383aa96d741a2b837f8c4bbcd0b.
    
    blk_aio_flush() now handles the blk->root == NULL case, so we no longer
    need this workaround.
    
    Cc: John Snow <jsnow@redhat.com>
    Signed-off-by: Stefan Hajnoczi <stefanha@redhat.com>
    Reviewed-by: Eric Blake <eblake@redhat.com>
    Signed-off-by: Kevin Wolf <kwolf@redhat.com>

--- hw/ide/core.c
+++ hw/ide/core.c
@@ -1087,15 +1087,7 @@ static void ide_flush_cache(IDEState *s)
     s->status |= BUSY_STAT;
     ide_set_retry(s);
     block_acct_start(blk_get_stats(s->blk), &s->acct, 0, BLOCK_ACCT_FLUSH);
-
-    if (blk_bs(s->blk)) {
-        s->pio_aiocb = blk_aio_flush(s->blk, ide_flush_cb, s);
-    } else {
-        /* XXX blk_aio_flush() crashes when blk_bs(blk) is NULL, remove this
-         * temporary workaround when blk_aio_*() functions handle NULL blk_bs.
-         */
-        ide_flush_cb(s, 0);
-    }
+    s->pio_aiocb = blk_aio_flush(s->blk, ide_flush_cb, s);
 }
 
 static void ide_cfata_metadata_inquiry(IDEState *s)
