1f4637953e29bd01360dbb97ee2312e8e72f2a9d
Mark Cave-Ayland <mark.cave-ayland@ilande.co.uk>
2018-02-23 11:10:17 +0000

    loader: don't perform overlapping address check for memory region ROM images
    All memory region ROM images have a base address of 0 which causes the overlapping
    address check to fail if more than one memory region ROM image is present, or an
    existing ROM image is loaded at address 0.
    
    Make sure that we ignore the overlapping address check in
    rom_check_and_register_reset() if this is a memory region ROM image. In particular
    this fixes the "rom: requested regions overlap" error on startup when trying to
    run qemu-system-sparc with a -kernel image since commit 7497638642: "tcx: switch to
    load_image_mr() and remove prom_addr hack".
    
    Suggested-by: Peter Maydell <peter.maydell@linaro.org>
    Reviewed-by: Peter Maydell <peter.maydell@linaro.org>
    Signed-off-by: Mark Cave-Ayland <mark.cave-ayland@ilande.co.uk>
    (cherry picked from commit ca316c11526a1bc221fb542bdce6bac7238dde69)
    Signed-off-by: Michael Roth <mdroth@linux.vnet.ibm.com>

--- hw/core/loader.c
+++ hw/core/loader.c
@@ -1104,20 +1104,22 @@ int rom_check_and_register_reset(void)
         if (rom->fw_file) {
             continue;
         }
-        if ((addr > rom->addr) && (as == rom->as)) {
-            fprintf(stderr, "rom: requested regions overlap "
-                    "(rom %s. free=0x" TARGET_FMT_plx
-                    ", addr=0x" TARGET_FMT_plx ")\n",
-                    rom->name, addr, rom->addr);
-            return -1;
+        if (!rom->mr) {
+            if ((addr > rom->addr) && (as == rom->as)) {
+                fprintf(stderr, "rom: requested regions overlap "
+                        "(rom %s. free=0x" TARGET_FMT_plx
+                        ", addr=0x" TARGET_FMT_plx ")\n",
+                        rom->name, addr, rom->addr);
+                return -1;
+            }
+            addr  = rom->addr;
+            addr += rom->romsize;
+            as = rom->as;
         }
-        addr  = rom->addr;
-        addr += rom->romsize;
         section = memory_region_find(rom->mr ? rom->mr : get_system_memory(),
                                      rom->addr, 1);
         rom->isrom = int128_nz(section.size) && memory_region_is_rom(section.mr);
         memory_region_unref(section.mr);
-        as = rom->as;
     }
     qemu_register_reset(rom_reset, NULL);
     roms_loaded = 1;
