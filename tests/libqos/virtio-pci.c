bb37a2c0b51f74e7bd9a1f87ca4ec1648e1f9398
Thomas Huth <thuth@redhat.com>
2018-02-27 07:44:44 +0100

    tests/libqos: Check for valid dev pointer when looking for PCI devices
    dev could be NULL if the PCI device can not be found due to some
    reasons, so we must not dereference the pointer in this case.
    
    Signed-off-by: Thomas Huth <thuth@redhat.com>
    Message-id: 1519713884-2346-1-git-send-email-thuth@redhat.com
    Signed-off-by: Stefan Hajnoczi <stefanha@redhat.com>

--- tests/libqos/virtio-pci.c
+++ tests/libqos/virtio-pci.c
@@ -315,7 +315,9 @@ QVirtioPCIDevice *qvirtio_pci_device_find(QPCIBus *bus, uint16_t device_type)
     qvirtio_pci_foreach(bus, device_type, false, 0,
                         qvirtio_pci_assign_device, &dev);
 
-    dev->vdev.bus = &qvirtio_pci;
+    if (dev) {
+        dev->vdev.bus = &qvirtio_pci;
+    }
 
     return dev;
 }
