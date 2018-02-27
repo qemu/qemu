08b824aa2d459f2822736ccaeb240811fac72957
Cornelia Huck <cohuck@redhat.com>
2018-02-27 18:25:41 +0100

    vfio-ccw: license text should indicate GPL v2 or later
    The license text currently specifies "any version" of the GPL. It
    is unlikely that GPL v1 was ever intended; change this to the
    standard "or any later version" text.
    
    Cc: Dong Jia Shi <bjsdjshi@linux.vnet.ibm.com>
    Cc: Xiao Feng Ren <renxiaof@linux.vnet.ibm.com>
    Cc: Pierre Morel <pmorel@linux.vnet.ibm.com>
    Acked-by: Christian Borntraeger <borntraeger@de.ibm.com>
    Reviewed-by: Dong Jia Shi <bjsdjshi@linux.vnet.ibm.com>
    Acked-by: Pierre Morel <pmorel@linux.vnet.ibm.com>
    Signed-off-by: Cornelia Huck <cohuck@redhat.com>

--- hw/vfio/ccw.c
+++ hw/vfio/ccw.c
@@ -6,8 +6,8 @@
  *            Xiao Feng Ren <renxiaof@linux.vnet.ibm.com>
  *            Pierre Morel <pmorel@linux.vnet.ibm.com>
  *
- * This work is licensed under the terms of the GNU GPL, version 2 or(at
- * your option) any version. See the COPYING file in the top-level
+ * This work is licensed under the terms of the GNU GPL, version 2 or (at
+ * your option) any later version. See the COPYING file in the top-level
  * directory.
  */
 
