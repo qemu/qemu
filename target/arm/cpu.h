6ad4d6187563f069fe5f11c3c1c9ccec1f69c2b7
Alex Bennée <alex.bennee@linaro.org>
2018-03-01 11:05:47 +0000

    target/arm/cpu64: introduce ARM_V8_FP16 feature bit
    Signed-off-by: Alex Bennée <alex.bennee@linaro.org>
    Reviewed-by: Richard Henderson <richard.henderson@linaro.org>
    Message-id: 20180227143852.11175-3-alex.bennee@linaro.org
    [PMM: postpone actually enabling feature until end of the
     patch series]
    Signed-off-by: Peter Maydell <peter.maydell@linaro.org>

--- target/arm/cpu.h
+++ target/arm/cpu.h
@@ -1408,6 +1408,7 @@ enum arm_features {
     ARM_FEATURE_V8_SHA3, /* implements SHA3 part of v8 Crypto Extensions */
     ARM_FEATURE_V8_SM3, /* implements SM3 part of v8 Crypto Extensions */
     ARM_FEATURE_V8_SM4, /* implements SM4 part of v8 Crypto Extensions */
+    ARM_FEATURE_V8_FP16, /* implements v8.2 half-precision float */
 };
 
 static inline int arm_feature(CPUARMState *env, int feature)
