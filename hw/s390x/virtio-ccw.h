24118af846868bb22e573be206c63e684ba9846a
Nia Alarie <nia.alarie@gmail.com>
2018-03-07 16:29:58 +0000

    s390x/virtio: Convert virtio-ccw from *_exit to *_unrealize
    Signed-off-by: Nia Alarie <nia.alarie@gmail.com>
    Message-Id: <20180307162958.11232-1-nia.alarie@gmail.com>
    Reviewed-by: Stefan Hajnoczi <stefanha@redhat.com>
    Signed-off-by: Cornelia Huck <cohuck@redhat.com>

--- hw/s390x/virtio-ccw.h
+++ hw/s390x/virtio-ccw.h
@@ -76,7 +76,7 @@ typedef struct VirtioCcwDevice VirtioCcwDevice;
 typedef struct VirtIOCCWDeviceClass {
     CCWDeviceClass parent_class;
     void (*realize)(VirtioCcwDevice *dev, Error **errp);
-    int (*exit)(VirtioCcwDevice *dev);
+    void (*unrealize)(VirtioCcwDevice *dev, Error **errp);
 } VirtIOCCWDeviceClass;
 
 /* Performance improves when virtqueue kick processing is decoupled from the
