aafb21a0b9cea5fa0fe52e68111bb6bd13837a02
Peter Xu <peterx@redhat.com>
2018-03-09 16:59:47 +0800

    qobject: let object_property_get_str() use new API
    We can simplify object_property_get_str() using the new
    qobject_get_try_str().
    
    Reviewed-by: Fam Zheng <famz@redhat.com>
    Reviewed-by: Stefan Hajnoczi <stefanha@redhat.com>
    Reviewed-by: Eric Blake <eblake@redhat.com>
    Signed-off-by: Peter Xu <peterx@redhat.com>
    Message-Id: <20180309090006.10018-5-peterx@redhat.com>
    [eblake: rebase context of qobject_to() macro]
    Signed-off-by: Eric Blake <eblake@redhat.com>

--- qom/object.c
+++ qom/object.c
@@ -1136,18 +1136,15 @@ char *object_property_get_str(Object *obj, const char *name,
                               Error **errp)
 {
     QObject *ret = object_property_get_qobject(obj, name, errp);
-    QString *qstring;
     char *retval;
 
     if (!ret) {
         return NULL;
     }
-    qstring = qobject_to(QString, ret);
-    if (!qstring) {
+
+    retval = g_strdup(qobject_get_try_str(ret));
+    if (!retval) {
         error_setg(errp, QERR_INVALID_PARAMETER_TYPE, name, "string");
-        retval = NULL;
-    } else {
-        retval = g_strdup(qstring_get_str(qstring));
     }
 
     qobject_decref(ret);
