f0d64cb72916b3086b51d5933de74e98c8586612
Vladimir Sementsov-Ogievskiy <vsementsov@virtuozzo.com>
2018-03-13 15:34:00 -0400

    migration/qemu-file: add qemu_put_counted_string()
    Add function opposite to qemu_get_counted_string.
    qemu_put_counted_string puts one-byte length of the string (string
    should not be longer than 255 characters), and then it puts the string,
    without last zero byte.
    
    Signed-off-by: Vladimir Sementsov-Ogievskiy <vsementsov@virtuozzo.com>
    Reviewed-by: John Snow <jsnow@redhat.com>
    Reviewed-by: Juan Quintela <quintela@redhat.com>
    Reviewed-by: Fam Zheng <famz@redhat.com>
    Message-id: 20180313180320.339796-9-vsementsov@virtuozzo.com

--- migration/qemu-file.c
+++ migration/qemu-file.c
@@ -734,6 +734,19 @@ size_t qemu_get_counted_string(QEMUFile *f, char buf[256])
 }
 
 /*
+ * Put a string with one preceding byte containing its length. The length of
+ * the string should be less than 256.
+ */
+void qemu_put_counted_string(QEMUFile *f, const char *str)
+{
+    size_t len = strlen(str);
+
+    assert(len < 256);
+    qemu_put_byte(f, len);
+    qemu_put_buffer(f, (const uint8_t *)str, len);
+}
+
+/*
  * Set the blocking state of the QEMUFile.
  * Note: On some transports the OS only keeps a single blocking state for
  *       both directions, and thus changing the blocking on the main
