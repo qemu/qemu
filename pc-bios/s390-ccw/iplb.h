e8c7ef288abb05b741a95418ee2de85c1071e0db
Viktor Mihajlovski <mihajlov@linux.vnet.ibm.com>
2018-04-05 17:07:24 +0200

    s390: Do not pass inofficial IPL type to the guest
    IPL over a virtio-scsi device requires special handling not
    available in the real architecture. For this purpose the IPL
    type 0xFF has been chosen as means of communication between
    QEMU and the pc-bios. However, a guest OS could be confused
    by seeing an unknown IPL type.
    
    This change sets the IPL parameter type to 0x02 (CCW) to prevent
    this. Pre-existing Linux has looked up the IPL parameters only in
    the case of FCP IPL. This means that the behavior should stay
    the same even if Linux checks for the IPL type unconditionally.
    
    Signed-off-by: Viktor Mihajlovski <mihajlov@linux.vnet.ibm.com>
    Message-Id: <1522940844-12336-4-git-send-email-mihajlov@linux.vnet.ibm.com>
    Reviewed-by: Christian Borntraeger <borntraeger@de.ibm.com>
    Signed-off-by: Cornelia Huck <cohuck@redhat.com>

--- pc-bios/s390-ccw/iplb.h
+++ pc-bios/s390-ccw/iplb.h
@@ -97,16 +97,27 @@ extern QemuIplParameters qipl;
 #define S390_IPL_TYPE_CCW 0x02
 #define S390_IPL_TYPE_QEMU_SCSI 0xff
 
-static inline bool store_iplb(IplParameterBlock *iplb)
+static inline bool manage_iplb(IplParameterBlock *iplb, bool store)
 {
     register unsigned long addr asm("0") = (unsigned long) iplb;
     register unsigned long rc asm("1") = 0;
 
     asm volatile ("diag %0,%2,0x308\n"
                   : "+d" (addr), "+d" (rc)
-                  : "d" (6)
+                  : "d" (store ? 6 : 5)
                   : "memory", "cc");
     return rc == 0x01;
+}
+
+
+static inline bool store_iplb(IplParameterBlock *iplb)
+{
+    return manage_iplb(iplb, true);
+}
+
+static inline bool set_iplb(IplParameterBlock *iplb)
+{
+    return manage_iplb(iplb, false);
 }
 
 #endif /* IPLB_H */
