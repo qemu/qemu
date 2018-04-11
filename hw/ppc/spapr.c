da9f80fbad21319194e73355dea8a1cff6a574e4
Serhii Popovych <spopovyc@redhat.com>
2018-04-11 14:41:59 -0400

    spapr: Add ibm,max-associativity-domains property
    Now recent kernels (i.e. since linux-stable commit a346137e9142
    ("powerpc/numa: Use ibm,max-associativity-domains to discover possible nodes")
    support this property to mark initially memory-less NUMA nodes as "possible"
    to allow further memory hot-add to them.
    
    Advertise this property for pSeries machines to let guest kernels detect
    maximum supported node configuration and benefit from kernel side change
    when hot-add memory to specific, possibly empty before, NUMA node.
    
    Signed-off-by: Serhii Popovych <spopovyc@redhat.com>
    Signed-off-by: David Gibson <david@gibson.dropbear.id.au>

--- hw/ppc/spapr.c
+++ hw/ppc/spapr.c
@@ -909,6 +909,13 @@ static void spapr_dt_rtas(sPAPRMachineState *spapr, void *fdt)
         0, cpu_to_be32(SPAPR_MEMORY_BLOCK_SIZE),
         cpu_to_be32(max_cpus / smp_threads),
     };
+    uint32_t maxdomains[] = {
+        cpu_to_be32(4),
+        cpu_to_be32(0),
+        cpu_to_be32(0),
+        cpu_to_be32(0),
+        cpu_to_be32(nb_numa_nodes ? nb_numa_nodes - 1 : 0),
+    };
 
     _FDT(rtas = fdt_add_subnode(fdt, 0, "rtas"));
 
@@ -944,6 +951,9 @@ static void spapr_dt_rtas(sPAPRMachineState *spapr, void *fdt)
 
     _FDT(fdt_setprop(fdt, rtas, "ibm,associativity-reference-points",
                      refpoints, sizeof(refpoints)));
+
+    _FDT(fdt_setprop(fdt, rtas, "ibm,max-associativity-domains",
+                     maxdomains, sizeof(maxdomains)));
 
     _FDT(fdt_setprop_cell(fdt, rtas, "rtas-error-log-max",
                           RTAS_ERROR_LOG_MAX));
