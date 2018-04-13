be73ef6423fbe1f06c912a67d6d066d257c11e18
Edgar E. Iglesias <edgar.iglesias@xilinx.com>
2018-04-13 18:10:00 +0200

    target-microblaze: Setup for 64bit addressing
    Setup MicroBlaze builds for 64bit addressing.
    No functional change since the translator does not yet
    emit 64bit addresses.
    
    Reviewed-by: Richard Henderson <richard.henderson@linaro.org>
    Signed-off-by: Edgar E. Iglesias <edgar.iglesias@xilinx.com>

--- target/microblaze/cpu.h
+++ target/microblaze/cpu.h
@@ -23,7 +23,7 @@
 #include "qemu-common.h"
 #include "cpu-qom.h"
 
-#define TARGET_LONG_BITS 32
+#define TARGET_LONG_BITS 64
 
 #define CPUArchState struct CPUMBState
 
@@ -340,8 +340,8 @@ int cpu_mb_signal_handler(int host_signum, void *pinfo,
 /* FIXME: MB uses variable pages down to 1K but linux only uses 4k.  */
 #define TARGET_PAGE_BITS 12
 
-#define TARGET_PHYS_ADDR_SPACE_BITS 32
-#define TARGET_VIRT_ADDR_SPACE_BITS 32
+#define TARGET_PHYS_ADDR_SPACE_BITS 64
+#define TARGET_VIRT_ADDR_SPACE_BITS 64
 
 #define CPU_RESOLVING_TYPE TYPE_MICROBLAZE_CPU
 
