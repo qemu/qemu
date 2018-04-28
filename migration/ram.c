701b1876c0fc0c583e4aff300ace5d33a1b97ed6
Xiao Guangrong <xiaoguangrong@tencent.com>
2018-04-28 16:10:45 +0800

    migration: fix saving normal page even if it's been compressed
    Fix the bug introduced by da3f56cb2e767016 (migration: remove
    ram_save_compressed_page()), It should be 'return' rather than
    'res'
    
    Sorry for this stupid mistake :(
    
    Signed-off-by: Xiao Guangrong <xiaoguangrong@tencent.com>
    Message-Id: <20180428081045.8878-1-xiaoguangrong@tencent.com>
    Signed-off-by: Juan Quintela <quintela@redhat.com>

--- migration/ram.c
+++ migration/ram.c
@@ -1490,7 +1490,7 @@ static int ram_save_target_page(RAMState *rs, PageSearchStatus *pss,
      * CPU resource.
      */
     if (block == rs->last_sent_block && save_page_use_compression(rs)) {
-        res = compress_page_with_multi_thread(rs, block, offset);
+        return compress_page_with_multi_thread(rs, block, offset);
     }
 
     return ram_save_page(rs, pss, last_stage);
