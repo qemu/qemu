83dc62f6ed6b4f2afce71d579a630441cca0957c
Thomas Huth <huth@tuxfamily.org>
2018-04-29 09:40:02 +0200

    hw/m68k/mcf5208: Fix trivial typo in board description
    It's the MCF5208 evaluation board, not the MCF5206 eval board.
    
    Signed-off-by: Thomas Huth <huth@tuxfamily.org>
    Reviewed-by: Laurent Vivier <laurent@vivier.eu>
    Message-Id: <20180429094002.3293c9de@thl530.multi.box>
    Signed-off-by: Laurent Vivier <laurent@vivier.eu>

--- hw/m68k/mcf5208.c
+++ hw/m68k/mcf5208.c
@@ -315,7 +315,7 @@ static void mcf5208evb_init(MachineState *machine)
 
 static void mcf5208evb_machine_init(MachineClass *mc)
 {
-    mc->desc = "MCF5206EVB";
+    mc->desc = "MCF5208EVB";
     mc->init = mcf5208evb_init;
     mc->is_default = 1;
     mc->default_cpu_type = M68K_CPU_TYPE_NAME("m5208");
