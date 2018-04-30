8b7bf2badac25c0a52aff1b181ad75fdb304dd0c
Dr. David Alan Gilbert <dgilbert@redhat.com>
2018-04-30 19:59:43 +0100

    Migration+TLS: Fix crash due to double cleanup
    During a TLS connect we see:
      migration_channel_connect calls
      migration_tls_channel_connect
      (calls after TLS setup)
      migration_channel_connect
    
    My previous error handling fix made migration_channel_connect
    call migrate_fd_connect in all cases; unfortunately the above
    means it gets called twice and crashes doing double cleanup.
    
    Fixes: 688a3dcba98
    
    Reported-by: Peter Krempa <pkrempa@redhat.com>
    Signed-off-by: Dr. David Alan Gilbert <dgilbert@redhat.com>
    Reviewed-by: Daniel P. Berrangé <berrange@redhat.com>
    Message-Id: <20180430185943.35714-1-dgilbert@redhat.com>
    Signed-off-by: Juan Quintela <quintela@redhat.com>

--- migration/channel.c
+++ migration/channel.c
@@ -71,6 +71,15 @@ void migration_channel_connect(MigrationState *s,
             !object_dynamic_cast(OBJECT(ioc),
                                  TYPE_QIO_CHANNEL_TLS)) {
             migration_tls_channel_connect(s, ioc, hostname, &error);
+
+            if (!error) {
+                /* tls_channel_connect will call back to this
+                 * function after the TLS handshake,
+                 * so we mustn't call migrate_fd_connect until then
+                 */
+
+                return;
+            }
         } else {
             QEMUFile *f = qemu_fopen_channel_output(ioc);
 
