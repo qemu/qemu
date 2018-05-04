f3be67812c226162f86ce92634bd913714445420
Igor Mammedov <imammedo@redhat.com>
2018-05-04 10:37:48 +0200

    qmp: add set-numa-node command
    Command is allowed to run only in preconfig stage and
    will allow to configure numa mapping for CPUs depending
    on possible CPUs layout (query-hotpluggable-cpus) for
    given machine instance.
    
    Example of configuration session:
    $QEMU -smp 2 --preconfig ...
    
    QMP:
    -> {'execute': 'query-hotpluggable-cpus' }
    <- {'return': [
           {'props': {'core-id': 0, 'thread-id': 0, 'socket-id': 1}, ... },
           {'props': {'core-id': 0, 'thread-id': 0, 'socket-id': 0}, ... }
       ]}
    
    -> {'execute': 'set-numa-node', 'arguments': { 'type': 'node', 'nodeid': 0 } }
    <- {'return': {}}
    -> {'execute': 'set-numa-node', 'arguments': { 'type': 'cpu',
           'node-id': 0, 'core-id': 0, 'thread-id': 0, 'socket-id': 1, }
       }
    <- {'return': {}}
    
    -> {'execute': 'set-numa-node', 'arguments': { 'type': 'node', 'nodeid': 1 } }
    -> {'execute': 'set-numa-node', 'arguments': { 'type': 'cpu',
           'node-id': 1, 'core-id': 0, 'thread-id': 0, 'socket-id': 0 }
       }
    <- {'return': {}}
    
    -> {'execute': 'query-hotpluggable-cpus' }
    <- {'return': [
           {'props': {'core-id': 0, 'thread-id': 0, 'node-id': 0, 'socket-id': 1}, ... },
           {'props': {'core-id': 0, 'thread-id': 0, 'node-id': 1, 'socket-id': 0}, ... }
       ]}
    
    Signed-off-by: Igor Mammedov <imammedo@redhat.com>
    Message-Id: <1525423069-61903-11-git-send-email-imammedo@redhat.com>
    Reviewed-by: Eduardo Habkost <ehabkost@redhat.com>
    [ehabkost: Changed "since 2.13" to "since 3.0"]
    Signed-off-by: Eduardo Habkost <ehabkost@redhat.com>

--- numa.c
+++ numa.c
@@ -444,6 +444,17 @@ void parse_numa_opts(MachineState *ms)
     }
 }
 
+void qmp_set_numa_node(NumaOptions *cmd, Error **errp)
+{
+    if (!runstate_check(RUN_STATE_PRECONFIG)) {
+        error_setg(errp, "The command is permitted only in '%s' state",
+                   RunState_str(RUN_STATE_PRECONFIG));
+         return;
+    }
+
+    set_numa_options(MACHINE(qdev_get_machine()), cmd, errp);
+}
+
 void numa_cpu_pre_plug(const CPUArchId *slot, DeviceState *dev, Error **errp)
 {
     int node_id = object_property_get_int(OBJECT(dev), "node-id", &error_abort);
