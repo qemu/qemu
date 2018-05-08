3372a3168a67cf756f78d378083733fdebec8c12
Thomas Huth <thuth@redhat.com>
2018-05-08 12:17:52 +0200

    pc-bios/s390-ccw: struct tpi_info must be declared as aligned(4)
    I've run into a compilation error today with the current version of GCC 8:
    
    In file included from s390-ccw.h:49,
                     from main.c:12:
    cio.h:128:1: error: alignment 1 of 'struct tpi_info' is less than 4 [-Werror=packed-not-aligned]
     } __attribute__ ((packed));
     ^
    cc1: all warnings being treated as errors
    
    Since the struct tpi_info contains an element ("struct subchannel_id schid")
    which is marked as aligned(4), we've got to mark the struct tpi_info as
    aligned(4), too.
    
    CC: qemu-stable@nongnu.org
    Signed-off-by: Thomas Huth <thuth@redhat.com>
    Message-Id: <1525774672-11913-1-git-send-email-thuth@redhat.com>
    Reviewed-by: Cornelia Huck <cohuck@redhat.com>
    Acked-by: Christian Borntraeger <borntraeger@de.ibm.com>
    Signed-off-by: Cornelia Huck <cohuck@redhat.com>
    (cherry picked from commit a6e4385dea94850d7b06b0542e7960c1063fdabd)
    Signed-off-by: Michael Roth <mdroth@linux.vnet.ibm.com>

--- pc-bios/s390-ccw/cio.h
+++ pc-bios/s390-ccw/cio.h
@@ -125,7 +125,7 @@ struct tpi_info {
     __u32 reserved3  : 12;
     __u32 int_type   : 3;
     __u32 reserved4  : 12;
-} __attribute__ ((packed));
+} __attribute__ ((packed, aligned(4)));
 
 /* channel command word (type 1) */
 struct ccw1 {
