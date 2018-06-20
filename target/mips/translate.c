0305d194be1dbda09fa7a7c883894030d07c355f
Yongbok Kim <yongbok.kim@mips.com>
2018-06-20 13:05:47 +0100

    target/mips: Fix microMIPS on reset
    Fix to activate microMIPS on reset when Config3.ISA == {1, 3}
    
    Signed-off-by: Yongbok Kim <yongbok.kim@mips.com>
    Reviewed-by: Aleksandar Markovic <aleksandar.markovic@mips.com>
    Signed-off-by: Aleksandar Markovic <aleksandar.markovic@mips.com>

--- target/mips/translate.c
+++ target/mips/translate.c
@@ -20713,6 +20713,11 @@ void cpu_state_reset(CPUMIPSState *env)
         env->CP0_Status |= (1 << CP0St_FR);
     }
 
+    if (env->CP0_Config3 & (1 << CP0C3_ISA)) {
+        /*  microMIPS on reset when Config3.ISA == {1, 3} */
+        env->hflags |= MIPS_HFLAG_M16;
+    }
+
     /* MSA */
     if (env->CP0_Config3 & (1 << CP0C3_MSAP)) {
         msa_reset(env);
