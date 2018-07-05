5131dc433df54b37e8e918d8fba7fe10344e7a7b
Robert Hoo <robert.hu@linux.intel.com>
2018-07-05 17:09:56 +0800

    i386: Add CPUID bit for PCONFIG
    PCONFIG: Platform configuration, enumerated by CPUID.(EAX=07H, ECX=0):
    EDX[bit18].
    
    Signed-off-by: Robert Hoo <robert.hu@linux.intel.com>
    Message-Id: <1530781798-183214-4-git-send-email-robert.hu@linux.intel.com>
    Signed-off-by: Eduardo Habkost <ehabkost@redhat.com>

--- target/i386/cpu.c
+++ target/i386/cpu.c
@@ -997,7 +997,7 @@ static FeatureWordInfo feature_word_info[FEATURE_WORDS] = {
             NULL, NULL, NULL, NULL,
             NULL, NULL, NULL, NULL,
             NULL, NULL, NULL, NULL,
-            NULL, NULL, NULL, NULL,
+            NULL, NULL, "pconfig", NULL,
             NULL, NULL, NULL, NULL,
             NULL, NULL, "spec-ctrl", NULL,
             NULL, "arch-capabilities", NULL, "ssbd",
