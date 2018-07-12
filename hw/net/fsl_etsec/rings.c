fd8e3381a00feb1e9878f6a3e2de11295f041f67
Michael Davidsaver <mdavidsaver@gmail.com>
2018-07-12 14:00:52 -0700

    etsec: fix IRQ (un)masking
    Interrupt conditions occurring while masked are not being
    signaled when later unmasked.
    The fix is to raise/lower IRQs when IMASK is changed.
    
    To avoid problems like this in future, consolidate
    IRQ pin update logic in one function.
    
    Also fix probable typo "IEVENT_TXF | IEVENT_TXF",
    and update IRQ pins on reset.
    
    Signed-off-by: Michael Davidsaver <mdavidsaver@gmail.com>
    Reviewed-by: Cédric Le Goater <clg@kaod.org>
    Signed-off-by: David Gibson <david@gibson.dropbear.id.au>

--- hw/net/fsl_etsec/rings.c
+++ hw/net/fsl_etsec/rings.c
@@ -152,17 +152,7 @@ static void ievent_set(eTSEC    *etsec,
 {
     etsec->regs[IEVENT].value |= flags;
 
-    if ((flags & IEVENT_TXB && etsec->regs[IMASK].value & IMASK_TXBEN)
-        || (flags & IEVENT_TXF && etsec->regs[IMASK].value & IMASK_TXFEN)) {
-        qemu_irq_raise(etsec->tx_irq);
-        RING_DEBUG("%s Raise Tx IRQ\n", __func__);
-    }
-
-    if ((flags & IEVENT_RXB && etsec->regs[IMASK].value & IMASK_RXBEN)
-        || (flags & IEVENT_RXF && etsec->regs[IMASK].value & IMASK_RXFEN)) {
-        qemu_irq_raise(etsec->rx_irq);
-        RING_DEBUG("%s Raise Rx IRQ\n", __func__);
-    }
+    etsec_update_irq(etsec);
 }
 
 static void tx_padding_and_crc(eTSEC *etsec, uint32_t min_frame_len)
