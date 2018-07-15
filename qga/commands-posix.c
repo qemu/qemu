d9c745c1768727aadd77e352c85114bc61e67bd4
Paolo Bonzini <pbonzini@redhat.com>
2018-07-15 18:21:56 +0200

    qga: fix file descriptor leak
    The file descriptor for /sys/power/state was never closed.  Reported
    by Coverity.
    
    Signed-off-by: Paolo Bonzini <pbonzini@redhat.com>
    Reviewed-by: Philippe Mathieu-Daudé <f4bug@amsat.org>
    Signed-off-by: Michael Roth <mdroth@linux.vnet.ibm.com>

--- qga/commands-posix.c
+++ qga/commands-posix.c
@@ -1652,6 +1652,7 @@ static bool linux_sys_state_supports_mode(SuspendMode mode, Error **errp)
     }
 
     ret = read(fd, buf, sizeof(buf) - 1);
+    close(fd);
     if (ret <= 0) {
         return false;
     }
