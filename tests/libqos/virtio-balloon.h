292410679b856e26665b3f734c0690379f3eb944
Emanuele Giuseppe Esposito <e.emanuelegiuseppe@gmail.com>
2018-07-25 23:39:13 +0200

    tests/libqos: virtio-balloon driver and interface nodes
    Add qgraph nodes for virtio-balloon-pci and virtio-balloon-device.
    Both nodes produce virtio-balloon, but virtio-balloon-pci receives
    a pci-bus and uses virtio-pci QOSGraphObject and functions,
    while virtio-balloon-device receives a virtio and implements
    its own functions
    
    Signed-off-by: Emanuele Giuseppe Esposito <e.emanuelegiuseppe@gmail.com>
    Signed-off-by: Paolo Bonzini <pbonzini@redhat.com>

---
+++ tests/libqos/virtio-balloon.h
@@ -0,0 +1,39 @@
+/*
+ * libqos driver framework
+ *
+ * Copyright (c) 2018 Emanuele Giuseppe Esposito <e.emanuelegiuseppe@gmail.com>
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License version 2 as published by the Free Software Foundation.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, see <http://www.gnu.org/licenses/>
+ */
+
+#include "libqos/qgraph.h"
+#include "libqos/virtio.h"
+#include "libqos/virtio-pci.h"
+
+typedef struct QVirtioBalloon QVirtioBalloon;
+typedef struct QVirtioBalloonPCI QVirtioBalloonPCI;
+typedef struct QVirtioBalloonDevice QVirtioBalloonDevice;
+
+struct QVirtioBalloon {
+    QVirtioDevice *vdev;
+};
+
+struct QVirtioBalloonPCI {
+    QVirtioPCIDevice pci_vdev;
+    QVirtioBalloon balloon;
+};
+
+struct QVirtioBalloonDevice {
+    QOSGraphObject obj;
+    QVirtioBalloon balloon;
+};
