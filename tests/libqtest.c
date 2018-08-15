f9e986d237881fda3ce599a89c26433531f5acbb
Markus Armbruster <armbru@redhat.com>
2018-08-15 16:19:45 +0200

    libqtest: Improve error reporting for bad read from QEMU
    When read() from the qtest socket or the QMP socket fails or EOFs, we
    report "Broken pipe" and exit(1).  This commonly happens when QEMU
    crashes.  It also happens when QEMU refuses to run because the test
    passed it bad arguments.  Sadly, we neglect to report either.
    
    Improve this by calling abort() instead of exit(1), so kill_qemu()
    runs, and reports how QEMU died.  This improves error reporting to
    something like
    
        /x86_64/device/introspect/list: Broken pipe
        tests/libqtest.c:129: kill_qemu() detected QEMU death from signal 6 (Aborted) (dumped core)
    
    Three exit() remain in libqtest.c:
    
    * In qmp_response(), when we can't parse a QMP reply read from the QMP
      socket.  Change to abort() for consistency.
    
    * In qtest_qemu_binary(), when QTEST_QEMU_BINARY isn't in the
      environment.  This can only happen before we start QEMU.  Leave
      alone.
    
    * In qtest_init_without_qmp_handshake(), when the fork()ed child fails
      to execlp().  Leave alone.
    
    exit() elsewhere are unlikely due to QEMU dying on us.  If that should
    turn out to be wrong, we can move kill_qemu() from @abrt_hooks to
    atexit() or something.
    
    Signed-off-by: Markus Armbruster <armbru@redhat.com>
    Message-Id: <20180815141945.10457-2-armbru@redhat.com>
    Reviewed-by: Philippe Mathieu-Daudé <f4bug@amsat.org>
    [Commit message tweaked slightly]

--- tests/libqtest.c
+++ tests/libqtest.c
@@ -367,7 +367,7 @@ static GString *qtest_recv_line(QTestState *s)
 
         if (len == -1 || len == 0) {
             fprintf(stderr, "Broken pipe\n");
-            exit(1);
+            abort();
         }
 
         g_string_append_len(s->rx, buffer, len);
@@ -454,7 +454,7 @@ static void qmp_response(JSONMessageParser *parser, GQueue *tokens)
     obj = json_parser_parse(tokens, NULL);
     if (!obj) {
         fprintf(stderr, "QMP JSON response parsing failed\n");
-        exit(1);
+        abort();
     }
 
     g_assert(!qmp->response);
@@ -480,7 +480,7 @@ QDict *qmp_fd_receive(int fd)
 
         if (len == -1 || len == 0) {
             fprintf(stderr, "Broken pipe\n");
-            exit(1);
+            abort();
         }
 
         if (log) {
