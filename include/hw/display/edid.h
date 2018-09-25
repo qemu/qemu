97917e9e02e04bbb5f3b14c0f2b58913ef5f09d2
Gerd Hoffmann <kraxel@redhat.com>
2018-09-25 09:56:44 +0200

    display/edid: add region helper.
    Create a io region for an EDID data block.
    
    Signed-off-by: Gerd Hoffmann <kraxel@redhat.com>
    Reviewed-by: Philippe Mathieu-Daudé <f4bug@amsat.org>
    Message-id: 20180925075646.25114-4-kraxel@redhat.com

--- include/hw/display/edid.h
+++ include/hw/display/edid.h
@@ -1,6 +1,8 @@
 #ifndef EDID_H
 #define EDID_H
 
+#include "hw/hw.h"
+
 typedef struct qemu_edid_info {
     const char *vendor;
     const char *name;
@@ -15,5 +17,7 @@ typedef struct qemu_edid_info {
 void qemu_edid_generate(uint8_t *edid, size_t size,
                         qemu_edid_info *info);
 size_t qemu_edid_size(uint8_t *edid);
+void qemu_edid_region_io(MemoryRegion *region, Object *owner,
+                         uint8_t *edid, size_t size);
 
 #endif /* EDID_H */
