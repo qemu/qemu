d41ca5afe3bc513ecf10b3ba5aa59523e3cd54aa
Paolo Bonzini <pbonzini@redhat.com>
2018-09-26 23:17:42 +0200

    virtio: do not take address of packed members
    The address of a packed member is not packed, which may cause accesses
    to unaligned pointers.  Avoid this by reading the packed value before
    passing it to another function.
    
    Cc: Jason Wang <jasowang@redhat.com>
    Cc: Peter Maydell <peter.maydell@linaro.org>
    Signed-off-by: Paolo Bonzini <pbonzini@redhat.com>

--- hw/char/virtio-serial-bus.c
+++ hw/char/virtio-serial-bus.c
@@ -667,9 +667,9 @@ static void virtio_serial_save_device(VirtIODevice *vdev, QEMUFile *f)
 
     /* The config space (ignored on the far end in current versions) */
     get_config(vdev, (uint8_t *)&config);
-    qemu_put_be16s(f, &config.cols);
-    qemu_put_be16s(f, &config.rows);
-    qemu_put_be32s(f, &config.max_nr_ports);
+    qemu_put_be16(f, config.cols);
+    qemu_put_be16(f, config.rows);
+    qemu_put_be32(f, config.max_nr_ports);
 
     /* The ports map */
     max_nr_ports = s->serial.max_virtserial_ports;
