6f8f015c0cd9f3ea44de4d73144db98beef97e3c
Alberto Garcia <berto@igalia.com>
2018-10-11 13:58:02 +0300

    qcow2: Get the request alignment for encrypted images from QCryptoBlock
    This doesn't have any practical effect at the moment because the
    values of BDRV_SECTOR_SIZE, QCRYPTO_BLOCK_LUKS_SECTOR_SIZE and
    QCRYPTO_BLOCK_QCOW_SECTOR_SIZE are all the same (512 bytes), but
    future encryption methods could have different requirements.
    
    Signed-off-by: Alberto Garcia <berto@igalia.com>
    Reviewed-by: Daniel P. Berrangé <berrange@redhat.com>
    Signed-off-by: Kevin Wolf <kwolf@redhat.com>

--- block/qcow2.c
+++ block/qcow2.c
@@ -1677,7 +1677,7 @@ static void qcow2_refresh_limits(BlockDriverState *bs, Error **errp)
 
     if (bs->encrypted) {
         /* Encryption works on a sector granularity */
-        bs->bl.request_alignment = BDRV_SECTOR_SIZE;
+        bs->bl.request_alignment = qcrypto_block_get_sector_size(s->crypto);
     }
     bs->bl.pwrite_zeroes_alignment = s->cluster_size;
     bs->bl.pdiscard_alignment = s->cluster_size;
