7f623d0834238ce6566afcbeeb325f3d943c2741
Gerd Hoffmann <kraxel@redhat.com>
2018-10-15 10:52:09 -0600

    stubs: add ramfb
    Needed to make sure code using ramfb (vfio) compiles properly even on
    platforms without fw_cfg (and therefore no ramfb) support.
    
    Signed-off-by: Gerd Hoffmann <kraxel@redhat.com>
    Signed-off-by: Alex Williamson <alex.williamson@redhat.com>

---
+++ stubs/ramfb.c
@@ -0,0 +1,13 @@
+#include "qemu/osdep.h"
+#include "qapi/error.h"
+#include "hw/display/ramfb.h"
+
+void ramfb_display_update(QemuConsole *con, RAMFBState *s)
+{
+}
+
+RAMFBState *ramfb_setup(Error **errp)
+{
+    error_setg(errp, "ramfb support not available");
+    return NULL;
+}
