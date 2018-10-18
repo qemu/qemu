d9c85b6cc5a2597e72299bc6644c966236378e50
Li Qiang <liq3ea@gmail.com>
2018-10-17 19:10:37 -0700

    qga: fix an off-by-one issue
    Signed-off-by: Li Qiang <liq3ea@gmail.com>
    Reviewed-by: Philippe Mathieu-Daudé <philmd@redhat.com>
    Signed-off-by: Michael Roth <mdroth@linux.vnet.ibm.com>

--- qga/commands-win32.c
+++ qga/commands-win32.c
@@ -485,7 +485,7 @@ static STORAGE_BUS_TYPE win2qemu[] = {
 
 static GuestDiskBusType find_bus_type(STORAGE_BUS_TYPE bus)
 {
-    if (bus > ARRAY_SIZE(win2qemu) || (int)bus < 0) {
+    if (bus >= ARRAY_SIZE(win2qemu) || (int)bus < 0) {
         return GUEST_DISK_BUS_TYPE_UNKNOWN;
     }
     return win2qemu[(int)bus];
