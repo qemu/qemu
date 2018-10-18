0a348b9a4e115deb28856e650b8fe5277e291c23
Aleksandar Markovic <amarkovic@wavecomp.com>
2018-10-18 14:40:20 +0200

    target/mips: Add and integrate MXU decoding engine placeholder
    Provide the placeholder and add the invocation logic for MXU
    decoding engine.
    
    Reviewed-by: Stefan Markovic <smarkovic@wavecomp.com>
    Signed-off-by: Aleksandar Markovic <amarkovic@wavecomp.com>

--- target/mips/translate.c
+++ target/mips/translate.c
@@ -23978,6 +23978,12 @@ static void decode_opc_special(CPUMIPSState *env, DisasContext *ctx)
     }
 }
 
+static void decode_opc_mxu(CPUMIPSState *env, DisasContext *ctx)
+{
+    MIPS_INVAL("decode_opc_mxu");
+    generate_exception_end(ctx, EXCP_RI);
+}
+
 static void decode_opc_special2_legacy(CPUMIPSState *env, DisasContext *ctx)
 {
     int rs, rt, rd;
@@ -26221,6 +26227,8 @@ static void decode_opc(CPUMIPSState *env, DisasContext *ctx)
     case OPC_SPECIAL2:
         if ((ctx->insn_flags & INSN_R5900) && (ctx->insn_flags & ASE_MMI)) {
             decode_tx79_mmi(env, ctx);
+        } else if (ctx->insn_flags & ASE_MXU) {
+            decode_opc_mxu(env, ctx);
         } else {
             decode_opc_special2_legacy(env, ctx);
         }
