20faf0f5f8aba33405a6206cd26f0d2b49ac174a
Li Qiang <liq3ea@gmail.com>
2018-10-28 23:29:40 -0700

    nvme: don't unref ctrl_mem when device unrealized
    Currently, when hotplug/unhotplug nvme device, it will cause an
    assert in object.c. Following is the backtrack:
    
    ERROR:qom/object.c:981:object_unref: assertion failed: (obj->ref > 0)
    
    Thread 2 "qemu-system-x86" received signal SIGABRT, Aborted.
    [Switching to Thread 0x7fffcbd32700 (LWP 18844)]
    0x00007fffdb9e4fff in raise () from /lib/x86_64-linux-gnu/libc.so.6
    (gdb) bt
    /lib/x86_64-linux-gnu/libglib-2.0.so.0
    /lib/x86_64-linux-gnu/libglib-2.0.so.0
    qom/object.c:981
    /home/liqiang02/qemu-upstream/qemu/memory.c:1732
    /home/liqiang02/qemu-upstream/qemu/memory.c:285
    util/qemu-thread-posix.c:504
    /lib/x86_64-linux-gnu/libpthread.so.0
    
    This is caused by memory_region_unref in nvme_exit.
    
    Remove it to make the PCIdevice refcount correct.
    
    Signed-off-by: Li Qiang <liq3ea@gmail.com>
    Reviewed-by: Igor Mammedov <imammedo@redhat.com>
    Signed-off-by: Kevin Wolf <kwolf@redhat.com>

--- hw/block/nvme.c
+++ hw/block/nvme.c
@@ -1331,9 +1331,6 @@ static void nvme_exit(PCIDevice *pci_dev)
     g_free(n->namespaces);
     g_free(n->cq);
     g_free(n->sq);
-    if (n->cmbsz) {
-        memory_region_unref(&n->ctrl_mem);
-    }
 
     msix_uninit_exclusive_bar(pci_dev);
 }
