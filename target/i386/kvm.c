d98f26073bebddcd3da0ba1b86c3a34e840c0fb8
Paolo Bonzini <pbonzini@redhat.com>
2018-11-14 10:38:13 +0100

    target/i386: kvm: add VMX migration blocker
    Nested VMX does not support live migration yet.  Add a blocker
    until that is worked out.
    
    Nested SVM only does not support it, but unfortunately it is
    enabled by default for -cpu host so we cannot really disable it.
    
    Signed-off-by: Paolo Bonzini <pbonzini@redhat.com>

--- target/i386/kvm.c
+++ target/i386/kvm.c
@@ -855,6 +855,7 @@ static int hyperv_init_vcpu(X86CPU *cpu)
 }
 
 static Error *invtsc_mig_blocker;
+static Error *vmx_mig_blocker;
 
 #define KVM_MAX_CPUID_ENTRIES  100
 
@@ -1245,6 +1246,17 @@ int kvm_arch_init_vcpu(CPUState *cs)
     if (c) {
         has_msr_feature_control = !!(c->ecx & CPUID_EXT_VMX) ||
                                   !!(c->ecx & CPUID_EXT_SMX);
+    }
+
+    if ((env->features[FEAT_1_ECX] & CPUID_EXT_VMX) && !vmx_mig_blocker) {
+        error_setg(&vmx_mig_blocker,
+                   "Nested VMX virtualization does not support live migration yet");
+        r = migrate_add_blocker(vmx_mig_blocker, &local_err);
+        if (local_err) {
+            error_report_err(local_err);
+            error_free(vmx_mig_blocker);
+            return r;
+        }
     }
 
     if (env->mcg_cap & MCG_LMCE_P) {
