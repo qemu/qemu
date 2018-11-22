2067d39e5e53b053bece6fa15711640123c119ed
Kevin Wolf <kwolf@redhat.com>
2018-11-22 15:52:20 +0100

    Revert "nvme: fix oob access issue(CVE-2018-16847)"
    This reverts commit 5e3c0220d7e4f0361c4d36c697a8842f2b583402.
    We have a better fix commited for this now.
    
    Signed-off-by: Kevin Wolf <kwolf@redhat.com>

--- hw/block/nvme.c
+++ hw/block/nvme.c
@@ -1177,10 +1177,6 @@ static void nvme_cmb_write(void *opaque, hwaddr addr, uint64_t data,
     unsigned size)
 {
     NvmeCtrl *n = (NvmeCtrl *)opaque;
-
-    if (addr + size > NVME_CMBSZ_GETSIZE(n->bar.cmbsz)) {
-        return;
-    }
     memcpy(&n->cmbuf[addr], &data, size);
 }
 
@@ -1189,9 +1185,6 @@ static uint64_t nvme_cmb_read(void *opaque, hwaddr addr, unsigned size)
     uint64_t val;
     NvmeCtrl *n = (NvmeCtrl *)opaque;
 
-    if (addr + size > NVME_CMBSZ_GETSIZE(n->bar.cmbsz)) {
-        return 0;
-    }
     memcpy(&val, &n->cmbuf[addr], size);
     return val;
 }
