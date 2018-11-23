b7ee9e4970bd76ca181ae6ea853c350e039eb71d
Wang Xin <wangxinxin.wang@huawei.com>
2018-11-23 14:46:46 +0800

    cirrus_vga/migration: update the bank offset before use
    The cirrus bank0/1 offset should be updated before we update the vram's alias
    offset.
    
    Signed-off-by: Wang Xin <wangxinxin.wang@huawei.com>
    Message-id: 20181123064646.23036-1-linzhecheng@huawei.com
    Signed-off-by: Gerd Hoffmann <kraxel@redhat.com>

--- hw/display/cirrus_vga.c
+++ hw/display/cirrus_vga.c
@@ -2746,11 +2746,12 @@ static int cirrus_post_load(void *opaque, int version_id)
     s->vga.gr[0x00] = s->cirrus_shadow_gr0 & 0x0f;
     s->vga.gr[0x01] = s->cirrus_shadow_gr1 & 0x0f;
 
+    cirrus_update_bank_ptr(s, 0);
+    cirrus_update_bank_ptr(s, 1);
     cirrus_update_memory_access(s);
     /* force refresh */
     s->vga.graphic_mode = -1;
-    cirrus_update_bank_ptr(s, 0);
-    cirrus_update_bank_ptr(s, 1);
+
     return 0;
 }
 
