2264faa55f0f06795e61365a6b4891e53b7716a4
ZhiPeng Lu <luzhipeng@uniudc.com>
2018-11-27 19:02:45 +0800

    hw/virt/arm: Add support for Cortex-A72 in virt
    Signed-off-by: ZhiPeng Lu <luzhipeng@uniudc.com>
    Message-id: 1543316565-1101590-1-git-send-email-luzhipeng@uniudc.com
    Reviewed-by: Peter Maydell <peter.maydell@linaro.org>
    Signed-off-by: Peter Maydell <peter.maydell@linaro.org>

--- hw/arm/virt.c
+++ hw/arm/virt.c
@@ -174,6 +174,7 @@ static const char *valid_cpus[] = {
     ARM_CPU_TYPE_NAME("cortex-a15"),
     ARM_CPU_TYPE_NAME("cortex-a53"),
     ARM_CPU_TYPE_NAME("cortex-a57"),
+    ARM_CPU_TYPE_NAME("cortex-a72"),
     ARM_CPU_TYPE_NAME("host"),
     ARM_CPU_TYPE_NAME("max"),
 };
