763f7e12090fa8a4f4d1902a51c7d5ac7c454090
Emilio G. Cota <cota@braap.org>
2018-12-07 20:53:09 -0500

    plugin-gen: add plugin_insn_append
    By adding it to plugin-gen's header file, we can export is as
    an inline, since tcg.h is included in the header (we need tcg_ctx).
    
    Signed-off-by: Emilio G. Cota <cota@braap.org>
    [AJB: use g_byte_array]
    Signed-off-by: Alex Bennée <alex.bennee@linaro.org>
    Reviewed-by: Richard Henderson <richard.henderson@linaro.org>

--- include/exec/plugin-gen.h
+++ include/exec/plugin-gen.h
@@ -27,6 +27,17 @@ void plugin_gen_insn_end(void);
 void plugin_gen_disable_mem_helpers(void);
 void plugin_gen_empty_mem_callback(TCGv addr, uint32_t info);
 
+static inline void plugin_insn_append(const void *from, size_t size)
+{
+    struct qemu_plugin_insn *insn = tcg_ctx->plugin_insn;
+
+    if (insn == NULL) {
+        return;
+    }
+
+    insn->data = g_byte_array_append(insn->data, from, size);
+}
+
 #else /* !CONFIG_PLUGIN */
 
 static inline
@@ -49,6 +60,9 @@ static inline void plugin_gen_disable_mem_helpers(void)
 { }
 
 static inline void plugin_gen_empty_mem_callback(TCGv addr, uint32_t info)
+{ }
+
+static inline void plugin_insn_append(const void *from, size_t size)
 { }
 
 #endif /* CONFIG_PLUGIN */
