bb1973aadb78bfb653287421d9df8ade1accc4af
Alistair Francis <Alistair.Francis@wdc.com>
2018-12-11 22:37:26 +0000

    hw/riscv/virt: Adjust memory layout spacing
    Signed-off-by: Alistair Francis <alistair.francis@wdc.com>
    Reviewed-by: Logan Gunthorpe <logang@deltatee.com>
    Tested-by: Guenter Roeck <linux@roeck-us.net>
    Tested-by: Andrea Bolognani <abologna@redhat.com>
    Signed-off-by: Palmer Dabbelt <palmer@sifive.com>

--- hw/riscv/virt.c
+++ hw/riscv/virt.c
@@ -47,14 +47,14 @@ static const struct MemmapEntry {
     hwaddr base;
     hwaddr size;
 } virt_memmap[] = {
-    [VIRT_DEBUG] =    {        0x0,      0x100 },
-    [VIRT_MROM] =     {     0x1000,    0x11000 },
-    [VIRT_TEST] =     {   0x100000,     0x1000 },
-    [VIRT_CLINT] =    {  0x2000000,    0x10000 },
-    [VIRT_PLIC] =     {  0xc000000,  0x4000000 },
-    [VIRT_UART0] =    { 0x10000000,      0x100 },
-    [VIRT_VIRTIO] =   { 0x10001000,     0x1000 },
-    [VIRT_DRAM] =     { 0x80000000,        0x0 },
+    [VIRT_DEBUG] =       {        0x0,         0x100 },
+    [VIRT_MROM] =        {     0x1000,       0x11000 },
+    [VIRT_TEST] =        {   0x100000,        0x1000 },
+    [VIRT_CLINT] =       {  0x2000000,       0x10000 },
+    [VIRT_PLIC] =        {  0xc000000,     0x4000000 },
+    [VIRT_UART0] =       { 0x10000000,         0x100 },
+    [VIRT_VIRTIO] =      { 0x10001000,        0x1000 },
+    [VIRT_DRAM] =        { 0x80000000,           0x0 },
 };
 
 static uint64_t load_kernel(const char *kernel_filename)
