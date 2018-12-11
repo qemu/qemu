c13c4153f76db23cac06a12044bf4dd346764059
Wei Wang <wei.w.wang@intel.com>
2018-12-11 16:24:53 +0800

    virtio-balloon: VIRTIO_BALLOON_F_FREE_PAGE_HINT
    The new feature enables the virtio-balloon device to receive hints of
    guest free pages from the free page vq.
    
    A notifier is registered to the migration precopy notifier chain. The
    notifier calls free_page_start after the migration thread syncs the dirty
    bitmap, so that the free page optimization starts to clear bits of free
    pages from the bitmap. It calls the free_page_stop before the migration
    thread syncs the bitmap, which is the end of the current round of ram
    save. The free_page_stop is also called to stop the optimization in the
    case when there is an error occurred in the process of ram saving.
    
    Note: balloon will report pages which were free at the time of this call.
    As the reporting happens asynchronously, dirty bit logging must be
    enabled before this free_page_start call is made. Guest reporting must be
    disabled before the migration dirty bitmap is synchronized.
    
    Signed-off-by: Wei Wang <wei.w.wang@intel.com>
    CC: Michael S. Tsirkin <mst@redhat.com>
    CC: Dr. David Alan Gilbert <dgilbert@redhat.com>
    CC: Juan Quintela <quintela@redhat.com>
    CC: Peter Xu <peterx@redhat.com>
    Message-Id: <1544516693-5395-8-git-send-email-wei.w.wang@intel.com>
    Reviewed-by: Michael S. Tsirkin <mst@redhat.com>
    Signed-off-by: Dr. David Alan Gilbert <dgilbert@redhat.com>
      dgilbert: Dropped kernel header update, fixed up CMD_ID_* name change

--- include/hw/virtio/virtio-balloon.h
+++ include/hw/virtio/virtio-balloon.h
@@ -17,10 +17,13 @@
 
 #include "standard-headers/linux/virtio_balloon.h"
 #include "hw/virtio/virtio.h"
+#include "sysemu/iothread.h"
 
 #define TYPE_VIRTIO_BALLOON "virtio-balloon-device"
 #define VIRTIO_BALLOON(obj) \
         OBJECT_CHECK(VirtIOBalloon, (obj), TYPE_VIRTIO_BALLOON)
+
+#define VIRTIO_BALLOON_FREE_PAGE_REPORT_CMD_ID_MIN 0x80000000
 
 typedef struct virtio_balloon_stat VirtIOBalloonStat;
 
@@ -32,15 +35,38 @@ typedef struct virtio_balloon_stat_modern {
 
 typedef struct PartiallyBalloonedPage PartiallyBalloonedPage;
 
+enum virtio_balloon_free_page_report_status {
+    FREE_PAGE_REPORT_S_STOP = 0,
+    FREE_PAGE_REPORT_S_REQUESTED = 1,
+    FREE_PAGE_REPORT_S_START = 2,
+    FREE_PAGE_REPORT_S_DONE = 3,
+};
+
 typedef struct VirtIOBalloon {
     VirtIODevice parent_obj;
-    VirtQueue *ivq, *dvq, *svq;
+    VirtQueue *ivq, *dvq, *svq, *free_page_vq;
+    uint32_t free_page_report_status;
     uint32_t num_pages;
     uint32_t actual;
+    uint32_t free_page_report_cmd_id;
     uint64_t stats[VIRTIO_BALLOON_S_NR];
     VirtQueueElement *stats_vq_elem;
     size_t stats_vq_offset;
     QEMUTimer *stats_timer;
+    IOThread *iothread;
+    QEMUBH *free_page_bh;
+    /*
+     * Lock to synchronize threads to access the free page reporting related
+     * fields (e.g. free_page_report_status).
+     */
+    QemuMutex free_page_lock;
+    QemuCond  free_page_cond;
+    /*
+     * Set to block iothread to continue reading free page hints as the VM is
+     * stopped.
+     */
+    bool block_iothread;
+    NotifierWithReturn free_page_report_notify;
     int64_t stats_last_update;
     int64_t stats_poll_interval;
     uint32_t host_features;
