1f79226d228b4d81fd93119e6c549cc87b394c1f
Thomas Huth <thuth@redhat.com>
2018-12-14 14:08:07 +0100

    hw/s390x: Fix bad mask in time2tod()
    Since "s390x/tcg: avoid overflows in time2tod/tod2time", the
    time2tod() function tries to deal with the 9 uppermost bits in the
    time value, but uses the wrong mask for this: 0xff80000000000000 should
    be used instead of 0xff10000000000000 here.
    
    Fixes: 14055ce53c2d901d826ffad7fb7d6bb8ab46bdfd
    Cc: qemu-stable@nongnu.org
    Signed-off-by: Thomas Huth <thuth@redhat.com>
    Message-Id: <1544792887-14575-1-git-send-email-thuth@redhat.com>
    Reviewed-by: David Hildenbrand <david@redhat.com>
    [CH: tweaked commit message]
    Signed-off-by: Cornelia Huck <cohuck@redhat.com>
    (cherry picked from commit aba7a5a2de3dba5917024df25441f715b9249e31)
    Signed-off-by: Michael Roth <mdroth@linux.vnet.ibm.com>

--- include/hw/s390x/tod.h
+++ include/hw/s390x/tod.h
@@ -50,7 +50,7 @@ typedef struct S390TODClass {
 /* Converts ns to s390's clock format */
 static inline uint64_t time2tod(uint64_t ns)
 {
-    return (ns << 9) / 125 + (((ns & 0xff10000000000000ull) / 125) << 9);
+    return (ns << 9) / 125 + (((ns & 0xff80000000000000ull) / 125) << 9);
 }
 
 /* Converts s390's clock format to ns */
