712f807e1965c8f1f1da5bbec2b92a8c540e6631
Robert Hoo <robert.hu@linux.intel.com>
2018-12-19 21:44:41 +0800

    Revert "i386: Add CPUID bit for PCONFIG"
    This reverts commit 5131dc433df54b37e8e918d8fba7fe10344e7a7b.
    For new instruction 'PCONFIG' will not be exposed to guest.
    
    Signed-off-by: Robert Hoo <robert.hu@linux.intel.com>
    Message-Id: <1545227081-213696-3-git-send-email-robert.hu@linux.intel.com>
    Signed-off-by: Paolo Bonzini <pbonzini@redhat.com>

--- target/i386/cpu.c
+++ target/i386/cpu.c
@@ -1077,7 +1077,7 @@ static FeatureWordInfo feature_word_info[FEATURE_WORDS] = {
             NULL, NULL, NULL, NULL,
             NULL, NULL, NULL, NULL,
             NULL, NULL, NULL, NULL,
-            NULL, NULL, "pconfig", NULL,
+            NULL, NULL, NULL, NULL,
             NULL, NULL, NULL, NULL,
             NULL, NULL, "spec-ctrl", "stibp",
             NULL, "arch-capabilities", NULL, "ssbd",
