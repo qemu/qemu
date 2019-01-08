4c7f4c4bbb515c5772fc15fc2192604ca7a0d197
Gerd Hoffmann <kraxel@redhat.com>
2019-01-08 11:23:01 +0100

    i2c-ddc: fix oob read
    Suggested-by: Michael Hanselmann <public@hansmi.ch>
    Signed-off-by: Gerd Hoffmann <kraxel@redhat.com>
    Reviewed-by: Michael Hanselmann <public@hansmi.ch>
    Reviewed-by: Philippe Mathieu-Daudé <philmd@redhat.com>
    Message-id: 20190108102301.1957-1-kraxel@redhat.com
    (cherry picked from commit b05b267840515730dbf6753495d5b7bd8b04ad1c)
    Signed-off-by: Michael Roth <mdroth@linux.vnet.ibm.com>

--- hw/i2c/i2c-ddc.c
+++ hw/i2c/i2c-ddc.c
@@ -56,7 +56,7 @@ static int i2c_ddc_rx(I2CSlave *i2c)
     I2CDDCState *s = I2CDDC(i2c);
 
     int value;
-    value = s->edid_blob[s->reg];
+    value = s->edid_blob[s->reg % sizeof(s->edid_blob)];
     s->reg++;
     return value;
 }
