051a0cde7b0a0dda9eef166f9aecd09dfc0a00cf
Chen Zhang <tgfbeta@me.com>
2019-01-25 15:47:23 +0800

    ui/egl-helpers: Augment parameter list of egl_texture_blend() to convey scales of viewport
    This would help gtk-egl display showing scaled DMABuf cursor images when
    gtk window was zoomed. A default scale of (1.0, 1.0) was presumed for
    call sites where no scaling is needed.
    
    Signed-off-by: Chen Zhang <tgfbeta@me.com>
    Message-id: 23B229B3-3095-4DFB-8369-866784808D30@me.com
    Signed-off-by: Gerd Hoffmann <kraxel@redhat.com>

--- ui/gtk-egl.c
+++ ui/gtk-egl.c
@@ -278,7 +278,8 @@ void gd_egl_scanout_flush(DisplayChangeListener *dcl,
                          vc->gfx.y0_top);
         egl_texture_blend(vc->gfx.gls, &vc->gfx.win_fb, &vc->gfx.cursor_fb,
                           vc->gfx.y0_top,
-                          vc->gfx.cursor_x, vc->gfx.cursor_y);
+                          vc->gfx.cursor_x, vc->gfx.cursor_y,
+                          vc->gfx.scale_x, vc->gfx.scale_y);
     } else {
         egl_fb_blit(&vc->gfx.win_fb, &vc->gfx.guest_fb, !vc->gfx.y0_top);
     }
