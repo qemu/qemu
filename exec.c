047be4ed24b3a408acccf9316d619477c06cca42
Stefan Hajnoczi <stefanha@redhat.com>
2019-01-29 11:46:04 +0000

    memory: add memory_region_flush_rom_device()
    ROM devices go via MemoryRegionOps->write() callbacks for write
    operations and do not dirty/invalidate that memory.  Device emulation
    must be able to mark memory ranges that have been modified internally
    (e.g. using memory_region_get_ram_ptr()).
    
    Introduce the memory_region_flush_rom_device() API for this purpose.
    
    Signed-off-by: Stefan Hajnoczi <stefanha@redhat.com>
    Message-id: 20190123212234.32068-2-stefanha@redhat.com
    Reviewed-by: Peter Maydell <peter.maydell@linaro.org>
    [PMM: fix block comment style]
    Signed-off-by: Peter Maydell <peter.maydell@linaro.org>

--- exec.c
+++ exec.c
@@ -3162,6 +3162,19 @@ static void invalidate_and_set_dirty(MemoryRegion *mr, hwaddr addr,
     cpu_physical_memory_set_dirty_range(addr, length, dirty_log_mask);
 }
 
+void memory_region_flush_rom_device(MemoryRegion *mr, hwaddr addr, hwaddr size)
+{
+    /*
+     * In principle this function would work on other memory region types too,
+     * but the ROM device use case is the only one where this operation is
+     * necessary.  Other memory regions should use the
+     * address_space_read/write() APIs.
+     */
+    assert(memory_region_is_romd(mr));
+
+    invalidate_and_set_dirty(mr, addr, size);
+}
+
 static int memory_access_size(MemoryRegion *mr, unsigned l, hwaddr addr)
 {
     unsigned access_size_max = mr->ops->valid.max_access_size;
