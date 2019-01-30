57d434407adc967a696d7afad0cc32f8f7eb4a4f
Philippe Mathieu-Daudé <philmd@redhat.com>
2019-01-30 13:00:04 +0100

    hw/display/milkymist-tmu2: Explicit the dependency to both X11 / OpenGL
    The TMU device requires both X11 and OpenGL.
    
    Signed-off-by: Philippe Mathieu-Daudé <philmd@redhat.com>
    Message-id: 20190130120005.23123-4-philmd@redhat.com
    Signed-off-by: Gerd Hoffmann <kraxel@redhat.com>

--- hw/lm32/milkymist-hw.h
+++ hw/lm32/milkymist-hw.h
@@ -88,7 +88,7 @@ static inline DeviceState *milkymist_pfpu_create(hwaddr base,
     return dev;
 }
 
-#ifdef CONFIG_OPENGL
+#if defined(CONFIG_X11) && defined(CONFIG_OPENGL)
 #include <X11/Xlib.h>
 #include <epoxy/gl.h>
 #include <epoxy/glx.h>
@@ -103,7 +103,7 @@ static const int glx_fbconfig_attr[] = {
 static inline DeviceState *milkymist_tmu2_create(hwaddr base,
         qemu_irq irq)
 {
-#ifdef CONFIG_OPENGL
+#if defined(CONFIG_X11) && defined(CONFIG_OPENGL)
     DeviceState *dev;
     Display *d;
     GLXFBConfig *configs;
