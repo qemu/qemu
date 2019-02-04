d5b661e6763ab69eb7e7bb3a16ede5497790a811
Philippe Mathieu-Daudé <philmd@redhat.com>
2019-02-04 22:04:31 +0100

    hw/sparc64/sun4u: Drop useless inclusion of "hw/i386/pc.h"
    In 47973a2dbf we split the last generic chipset out of the PC
    board, but forgot to remove the include of "hw/i386/pc.h".
    Since it is now unused, remove it.
    
    Signed-off-by: Philippe Mathieu-Daudé <philmd@redhat.com>
    Reviewed-by: Thomas Huth <thuth@redhat.com>
    Reviewed-by: Mark Cave-Ayland <mark.cave-ayland@ilande.co.uk>
    Message-Id: <20190204210433.26088-2-philmd@redhat.com>
    Signed-off-by: Laurent Vivier <laurent@vivier.eu>

--- hw/sparc64/sun4u.c
+++ hw/sparc64/sun4u.c
@@ -33,7 +33,6 @@
 #include "hw/pci/pci_bus.h"
 #include "hw/pci/pci_host.h"
 #include "hw/pci-host/sabre.h"
-#include "hw/i386/pc.h"
 #include "hw/char/serial.h"
 #include "hw/char/parallel.h"
 #include "hw/timer/m48t59.h"
