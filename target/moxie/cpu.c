70c9483a3041a4db9741d23ff75380e08216c3c6
Thomas Huth <thuth@redhat.com>
2019-02-04 08:54:18 +0100

    target/moxie: Fix LGPL information in the file headers
    It's either "GNU *Library* General Public License version 2" or "GNU
    Lesser General Public License version *2.1*", but there was no "version
    2.0" of the "Lesser" license. So assume that version 2.1 is meant here.
    Also the files mentioned the GPL instead of the LGPL after declaring
    that the files are licensed under the LGPL, so change these spots to
    use LGPL, too.
    
    Signed-off-by: Thomas Huth <thuth@redhat.com>
    Acked-by: Richard Henderson <richard.henderson@linaro.org>
    Message-Id: <1549266858-5043-1-git-send-email-thuth@redhat.com>
    Signed-off-by: Laurent Vivier <laurent@vivier.eu>

--- target/moxie/cpu.c
+++ target/moxie/cpu.c
@@ -13,7 +13,7 @@
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
  * Lesser General Public License for more details.
  *
- * You should have received a copy of the GNU General Public License
+ * You should have received a copy of the GNU Lesser General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
