44699e1c94bad41bbeb0c96f9ed344eb8e325413
Thomas Huth <thuth@redhat.com>
2019-02-06 13:41:33 +0100

    s390x: Fix the confusing contributions-after-2012 license statements
    The license information in these files is rather confusing. The text
    declares LGPL first, but then says that contributions after 2012 are
    licensed under the GPL instead. How should the average user who just
    downloaded the release tarball know which part is now GPL and which
    is LGPL?
    
    Looking at the text of the LGPL (see COPYING.LIB in the top directory),
    the license clearly states how this should be done instead:
    
    "3. You may opt to apply the terms of the ordinary GNU General Public
     License instead of this License to a given copy of the Library. To do
     this, you must alter all the notices that refer to this License, so
     that they refer to the ordinary GNU General Public License, version 2,
     instead of to this License."
    
    Thus let's clean up the confusing statements and use the proper GPL
    text only.
    
    Signed-off-by: Thomas Huth <thuth@redhat.com>
    Message-Id: <1549456893-16589-1-git-send-email-thuth@redhat.com>
    Acked-by: Laurent Vivier <laurent@vivier.eu>
    Acked-by: Christian Borntraeger <borntraeger@de.ibm.com>
    Signed-off-by: Cornelia Huck <cohuck@redhat.com>

--- linux-user/s390x/target_cpu.h
+++ linux-user/s390x/target_cpu.h
@@ -3,21 +3,18 @@
  *
  * Copyright (c) 2009 Ulrich Hecht
  *
- * This library is free software; you can redistribute it and/or
- * modify it under the terms of the GNU Lesser General Public
- * License as published by the Free Software Foundation; either
- * version 2 of the License, or (at your option) any later version.
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
  *
- * This library is distributed in the hope that it will be useful,
+ * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
- * Lesser General Public License for more details.
+ * General Public License for more details.
  *
- * Contributions after 2012-10-29 are licensed under the terms of the
- * GNU GPL, version 2 or (at your option) any later version.
- *
- * You should have received a copy of the GNU (Lesser) General Public
- * License along with this library; if not, see <http://www.gnu.org/licenses/>.
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, see <http://www.gnu.org/licenses/>.
  */
 #ifndef S390X_TARGET_CPU_H
 #define S390X_TARGET_CPU_H
