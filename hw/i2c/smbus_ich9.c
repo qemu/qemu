6086e300265391a02932b8acd8ba44a624cbc94f
Thomas Huth <thuth@redhat.com>
2019-02-06 17:33:38 +0100

    hw/i2c/smbus_ich9: Fix the confusing contributions-after-2012 statement
    The license information in this file is rather confusing. The text
    declares LGPL first, but then says that contributions after Jan 2012
    are licensed under the GPL instead. How should the average user who
    just downloaded the release tarball know which part is now GPL and
    which is LGPL? Also, as far as I can see, the file has been added to
    QEMU *after* January in 2012, so the whole file should be GPL by
    default instead.
    
    Furthermore, looking at the text of the LGPL (see COPYING.LIB in the
    top directory), the license clearly states in section "3." that one
    should rather replace the license information in such a case instead.
    Thus let's clean up the confusing statements and use the proper GPL
    text only.
    
    Message-Id: <1549471435-21887-1-git-send-email-thuth@redhat.com>
    Signed-off-by: Thomas Huth <thuth@redhat.com>

--- hw/i2c/smbus_ich9.c
+++ hw/i2c/smbus_ich9.c
@@ -6,23 +6,18 @@
  *               VA Linux Systems Japan K.K.
  * Copyright (C) 2012 Jason Baron <jbaron@redhat.com>
  *
- * This is based on acpi.c, but heavily rewritten.
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
  *
- * This library is free software; you can redistribute it and/or
- * modify it under the terms of the GNU Lesser General Public
- * License version 2 as published by the Free Software Foundation.
- *
- * This library is distributed in the hope that it will be useful,
+ * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
- * Lesser General Public License for more details.
+ * General Public License for more details.
  *
- * You should have received a copy of the GNU Lesser General Public
- * License along with this library; if not, see <http://www.gnu.org/licenses/>
- *
- * Contributions after 2012-01-13 are licensed under the terms of the
- * GNU GPL, version 2 or (at your option) any later version.
- *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, see <http://www.gnu.org/licenses/>
  */
 #include "qemu/osdep.h"
 #include "hw/hw.h"
