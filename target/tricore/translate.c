a00585eef28e388bd3022214ccd3c2486d6e3c7f
David Brenken <david.brenken@efs-auto.de>
2019-02-07 08:39:27 +0100

    tricore: fixed RCR_CADD instruction
    Signed-off-by: Christian Richter <christian.richter@efs-auto.de>
    Signed-off-by: David Brenken <david.brenken@efs-auto.de>
    Signed-off-by: Georg Hofstetter <georg.hofstetter@efs-auto.de>
    Signed-off-by: Robert Rasche <robert.rasche@efs-auto.de>
    Signed-off-by: Lars Biermanski <lars.biermanski@efs-auto.de>
    
    Message-Id: <20190207073928.4048-2-david.brenken@efs-auto.org>
    Signed-off-by: Bastian Koppelmann <kbastian@mail.uni-paderborn.de>

--- target/tricore/translate.c
+++ target/tricore/translate.c
@@ -5867,8 +5867,8 @@ static void decode_rcr_cond_select(CPUTriCoreState *env, DisasContext *ctx)
 
     switch (op2) {
     case OPC2_32_RCR_CADD:
-        gen_condi_add(TCG_COND_NE, cpu_gpr_d[r1], const9, cpu_gpr_d[r3],
-                      cpu_gpr_d[r4]);
+        gen_condi_add(TCG_COND_NE, cpu_gpr_d[r1], const9, cpu_gpr_d[r4],
+                      cpu_gpr_d[r3]);
         break;
     case OPC2_32_RCR_CADDN:
         gen_condi_add(TCG_COND_EQ, cpu_gpr_d[r1], const9, cpu_gpr_d[r3],
