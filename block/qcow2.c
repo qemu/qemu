b8968c875f4030dd6924d6971bb3d92dfa3d2f65
Andrey Shinkevich <andrey.shinkevich@virtuozzo.com>
2019-02-08 18:06:07 +0300

    qcow2: Add list of bitmaps to ImageInfoSpecificQCow2
    In the 'Format specific information' section of the 'qemu-img info'
    command output, the supplemental information about existing QCOW2
    bitmaps will be shown, such as a bitmap name, flags and granularity:
    
    image: /vz/vmprivate/VM1/harddisk.hdd
    file format: qcow2
    virtual size: 64G (68719476736 bytes)
    disk size: 3.0M
    cluster_size: 1048576
    Format specific information:
        compat: 1.1
        lazy refcounts: true
        bitmaps:
            [0]:
                flags:
                    [0]: in-use
                    [1]: auto
                name: back-up1
                granularity: 65536
            [1]:
                flags:
                    [0]: in-use
                    [1]: auto
                name: back-up2
                granularity: 65536
        refcount bits: 16
        corrupt: false
    
    Signed-off-by: Andrey Shinkevich <andrey.shinkevich@virtuozzo.com>
    Reviewed-by: Eric Blake <eblake@redhat.com>
    Reviewed-by: Vladimir Sementsov-Ogievskiy <vsementsov@virtuozzo.com>
    Message-Id: <1549638368-530182-3-git-send-email-andrey.shinkevich@virtuozzo.com>
    Signed-off-by: Eric Blake <eblake@redhat.com>

--- block/qcow2.c
+++ block/qcow2.c
@@ -4387,7 +4387,7 @@ static ImageInfoSpecific *qcow2_get_specific_info(BlockDriverState *bs,
     spec_info = g_new(ImageInfoSpecific, 1);
     *spec_info = (ImageInfoSpecific){
         .type  = IMAGE_INFO_SPECIFIC_KIND_QCOW2,
-        .u.qcow2.data = g_new(ImageInfoSpecificQCow2, 1),
+        .u.qcow2.data = g_new0(ImageInfoSpecificQCow2, 1),
     };
     if (s->qcow_version == 2) {
         *spec_info->u.qcow2.data = (ImageInfoSpecificQCow2){
@@ -4395,6 +4395,13 @@ static ImageInfoSpecific *qcow2_get_specific_info(BlockDriverState *bs,
             .refcount_bits      = s->refcount_bits,
         };
     } else if (s->qcow_version == 3) {
+        Qcow2BitmapInfoList *bitmaps;
+        bitmaps = qcow2_get_bitmap_info_list(bs, &local_err);
+        if (local_err) {
+            error_propagate(errp, local_err);
+            qapi_free_ImageInfoSpecific(spec_info);
+            return NULL;
+        }
         *spec_info->u.qcow2.data = (ImageInfoSpecificQCow2){
             .compat             = g_strdup("1.1"),
             .lazy_refcounts     = s->compatible_features &
@@ -4404,6 +4411,8 @@ static ImageInfoSpecific *qcow2_get_specific_info(BlockDriverState *bs,
                                   QCOW2_INCOMPAT_CORRUPT,
             .has_corrupt        = true,
             .refcount_bits      = s->refcount_bits,
+            .has_bitmaps        = !!bitmaps,
+            .bitmaps            = bitmaps,
         };
     } else {
         /* if this assertion fails, this probably means a new version was
