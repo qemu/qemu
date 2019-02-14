5c7ba877ef54e6a90982db729b4fda8c4790ef67
Igor Mammedov <imammedo@redhat.com>
2019-02-14 11:10:03 +0800

    hostmem-file: simplify ifdef-s in file_backend_memory_alloc()
    cleanup file_backend_memory_alloc() by using one CONFIG_POSIX ifdef
    instead of several ones within the function to make it simpler to follow.
    
    Signed-off-by: Igor Mammedov <imammedo@redhat.com>
    Suggested-by: Wei Yang <richardw.yang@linux.intel.com>
    Reviewed-by: Wei Yang <richardw.yang@linux.intel.com>
    Message-Id: <20190213123858.24620-1-imammedo@redhat.com>
    Signed-off-by: Stefan Hajnoczi <stefanha@redhat.com>
    Message-Id: <20190214031004.32522-2-stefanha@redhat.com>
    [lv: s/hostmem/hostmem-file/]
    Signed-off-by: Laurent Vivier <laurent@vivier.eu>

--- backends/hostmem-file.c
+++ backends/hostmem-file.c
@@ -41,10 +41,12 @@ struct HostMemoryBackendFile {
 static void
 file_backend_memory_alloc(HostMemoryBackend *backend, Error **errp)
 {
+#ifndef CONFIG_POSIX
+    error_setg(errp, "backend '%s' not supported on this host",
+               object_get_typename(OBJECT(backend)));
+#else
     HostMemoryBackendFile *fb = MEMORY_BACKEND_FILE(backend);
-#ifdef CONFIG_POSIX
     gchar *name;
-#endif
 
     if (!backend->size) {
         error_setg(errp, "can't create backend with size 0");
@@ -54,9 +56,6 @@ file_backend_memory_alloc(HostMemoryBackend *backend, Error **errp)
         error_setg(errp, "mem-path property not set");
         return;
     }
-#ifndef CONFIG_POSIX
-    error_setg(errp, "-mem-path not supported on this host");
-#else
     backend->force_prealloc = mem_prealloc;
     name = host_memory_backend_get_name(backend);
     memory_region_init_ram_from_file(&backend->mr, OBJECT(backend),
