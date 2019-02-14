5dc8ab361a6aed42560731eb9b4fdeff01ace948
Stefano Garzarella <sgarzare@redhat.com>
2019-02-14 19:02:16 +0100

    hw/i386/pc: run the multiboot loader before the PVH loader
    Some multiboot images could be in the ELF format. In the current
    implementation QEMU fails because we try to load these images
    as a PVH image.
    
    In order to fix this issue, we should try multiboot first (we
    already check the multiboot magic header before to load it).
    If it is not a multiboot image, we can try the PVH loader.
    
    Fixes: ab969087da6 ("pvh: Boot uncompressed kernel using direct boot ABI", 2019-01-15)
    Reported-by: Paolo Bonzini <pbonzini@redhat.com>
    Signed-off-by: Stefano Garzarella <sgarzare@redhat.com>
    Message-Id: <20190214180216.246707-1-sgarzare@redhat.com>
    Signed-off-by: Paolo Bonzini <pbonzini@redhat.com>

--- hw/i386/pc.c
+++ hw/i386/pc.c
@@ -1210,6 +1210,17 @@ static void load_linux(PCMachineState *pcms,
         protocol = lduw_p(header+0x206);
     } else {
         /*
+         * This could be a multiboot kernel. If it is, let's stop treating it
+         * like a Linux kernel.
+         * Note: some multiboot images could be in the ELF format (the same of
+         * PVH), so we try multiboot first since we check the multiboot magic
+         * header before to load it.
+         */
+        if (load_multiboot(fw_cfg, f, kernel_filename, initrd_filename,
+                           kernel_cmdline, kernel_size, header)) {
+            return;
+        }
+        /*
          * Check if the file is an uncompressed kernel file (ELF) and load it,
          * saving the PVH entry point used by the x86/HVM direct boot ABI.
          * If load_elfboot() is successful, populate the fw_cfg info.
@@ -1260,12 +1271,6 @@ static void load_linux(PCMachineState *pcms,
             option_rom[nb_option_roms].name = "pvh.bin";
             nb_option_roms++;
 
-            return;
-        }
-        /* This looks like a multiboot kernel. If it is, let's stop
-           treating it like a Linux kernel. */
-        if (load_multiboot(fw_cfg, f, kernel_filename, initrd_filename,
-                           kernel_cmdline, kernel_size, header)) {
             return;
         }
         protocol = 0;
