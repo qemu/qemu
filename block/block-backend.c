ae5a9592ce0d5c11e40667f217b2e2ceba33927f
Vladimir Sementsov-Ogievskiy <vsementsov@virtuozzo.com>
2019-02-18 17:09:12 +0300

    block/block-backend: use QEMU_IOVEC_INIT_BUF
    Use new QEMU_IOVEC_INIT_BUF() instead of
    qemu_iovec_init_external( ... , 1), which simplifies the code.
    
    Signed-off-by: Vladimir Sementsov-Ogievskiy <vsementsov@virtuozzo.com>
    Reviewed-by: Eric Blake <eblake@redhat.com>
    Reviewed-by: Stefan Hajnoczi <stefanha@redhat.com>
    Message-id: 20190218140926.333779-4-vsementsov@virtuozzo.com
    Message-Id: <20190218140926.333779-4-vsementsov@virtuozzo.com>
    Signed-off-by: Stefan Hajnoczi <stefanha@redhat.com>

--- block/block-backend.c
+++ block/block-backend.c
@@ -1204,17 +1204,8 @@ static int blk_prw(BlockBackend *blk, int64_t offset, uint8_t *buf,
                    int64_t bytes, CoroutineEntry co_entry,
                    BdrvRequestFlags flags)
 {
-    QEMUIOVector qiov;
-    struct iovec iov;
-    BlkRwCo rwco;
-
-    iov = (struct iovec) {
-        .iov_base = buf,
-        .iov_len = bytes,
-    };
-    qemu_iovec_init_external(&qiov, &iov, 1);
-
-    rwco = (BlkRwCo) {
+    QEMUIOVector qiov = QEMU_IOVEC_INIT_BUF(qiov, buf, bytes);
+    BlkRwCo rwco = {
         .blk    = blk,
         .offset = offset,
         .iobuf  = &qiov,
