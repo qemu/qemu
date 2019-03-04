a27382e2106c12a9761ca0954b4c0fba0e650682
Eric Auger <eric.auger@redhat.com>
2019-03-04 11:13:34 +0100

    kvm: add kvm_arm_get_max_vm_ipa_size
    Add the kvm_arm_get_max_vm_ipa_size() helper that returns the
    number of bits in the IPA address space supported by KVM.
    
    This capability needs to be known to create the VM with a
    specific IPA max size (kvm_type passed along KVM_CREATE_VM ioctl.
    
    Signed-off-by: Eric Auger <eric.auger@redhat.com>
    Reviewed-by: Igor Mammedov <imammedo@redhat.com>
    Message-id: 20190304101339.25970-6-eric.auger@redhat.com
    Signed-off-by: Peter Maydell <peter.maydell@linaro.org>

--- target/arm/kvm.c
+++ target/arm/kvm.c
@@ -18,6 +18,7 @@
 #include "qemu/error-report.h"
 #include "sysemu/sysemu.h"
 #include "sysemu/kvm.h"
+#include "sysemu/kvm_int.h"
 #include "kvm_arm.h"
 #include "cpu.h"
 #include "trace.h"
@@ -160,6 +161,15 @@ void kvm_arm_set_cpu_features_from_host(ARMCPU *cpu)
     cpu->dtb_compatible = arm_host_cpu_features.dtb_compatible;
     cpu->isar = arm_host_cpu_features.isar;
     env->features = arm_host_cpu_features.features;
+}
+
+int kvm_arm_get_max_vm_ipa_size(MachineState *ms)
+{
+    KVMState *s = KVM_STATE(ms->accelerator);
+    int ret;
+
+    ret = kvm_check_extension(s, KVM_CAP_ARM_VM_IPA_SIZE);
+    return ret > 0 ? ret : 40;
 }
 
 int kvm_arch_init(MachineState *ms, KVMState *s)
