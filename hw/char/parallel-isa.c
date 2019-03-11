3e3fdad6e1b980bd570790a8eaa6333023b4dcab
Thomas Huth <thuth@redhat.com>
2019-03-11 10:50:54 +0100

    hw/char/parallel: Make it possible to compile also without CONFIG_PARALLEL
    For the downstream distribution of QEMU, we want to compile without
    CONFIG_PARALLEL. Commit 9157eee1b1c076ff3 already moved the function
    parallel_hds_isa_init() (which is still required for linking) into a file
    that is included anyway, but commit bb3d5ea858e7f888563a moved it
    to a separate file which is only compiled again if CONFIG_PARALLEL is
    set. To be able to link QEMU again without CONFIG_PARALLEL, the file
    should be considered for linking for all targets that have CONFIG_ISA_BUS.
    And while we're at it, add a proper comment in there with the rationale
    for the separate file.
    
    Signed-off-by: Thomas Huth <thuth@redhat.com>
    Message-Id: <1552297854-25847-1-git-send-email-thuth@redhat.com>
    Signed-off-by: Paolo Bonzini <pbonzini@redhat.com>

--- hw/char/parallel-isa.c
+++ hw/char/parallel-isa.c
@@ -1,6 +1,9 @@
 /*
  * QEMU Parallel PORT (ISA bus helpers)
  *
+ * These functions reside in a separate file since they also might be
+ * required for linking when compiling QEMU without CONFIG_PARALLEL.
+ *
  * Copyright (c) 2003 Fabrice Bellard
  *
  * SPDX-License-Identifier: MIT
