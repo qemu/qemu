7918249416dddc5ab2d4bcb1e6c2742f3be0edac
Cleber Rosa <crosa@redhat.com>
2019-03-12 13:18:23 -0400

    tests/boot_linux_console: add a test for s390x + s390-ccw-virtio
    Just like the previous tests, boots a Linux kernel on a s390x target
    using the s390-ccw-virtio machine.
    
    Because it's not possible to have multiple VT220 consoles,
    '-nodefaults' is used, so that the one set with set_console() works
    correctly.
    
    Signed-off-by: Cleber Rosa <crosa@redhat.com>
    Reviewed-by: Cornelia Huck <cohuck@redhat.com>
    Reviewed-by: Caio Carrara <ccarrara@redhat.com>
    Message-Id: <20190312171824.5134-20-crosa@redhat.com>
    [ehabkost: Updated kernel URL to point to fedoraproject.org]
    Signed-off-by: Eduardo Habkost <ehabkost@redhat.com>

--- tests/acceptance/boot_linux_console.py
+++ tests/acceptance/boot_linux_console.py
@@ -175,3 +175,23 @@ class BootLinuxConsole(Test):
         self.vm.launch()
         console_pattern = 'Kernel command line: %s' % kernel_command_line
         self.wait_for_console_pattern(console_pattern)
+
+    def test_s390x_s390_ccw_virtio(self):
+        """
+        :avocado: tags=arch:s390x
+        :avocado: tags=machine:s390_ccw_virtio
+        """
+        kernel_url = ('https://download.fedoraproject.org/pub/fedora-secondary/'
+                      'releases/29/Everything/s390x/os/images/kernel.img')
+        kernel_hash = 'e8e8439103ef8053418ef062644ffd46a7919313'
+        kernel_path = self.fetch_asset(kernel_url, asset_hash=kernel_hash)
+
+        self.vm.set_machine('s390-ccw-virtio')
+        self.vm.set_console()
+        kernel_command_line = self.KERNEL_COMMON_COMMAND_LINE + 'console=sclp0'
+        self.vm.add_args('-nodefaults',
+                         '-kernel', kernel_path,
+                         '-append', kernel_command_line)
+        self.vm.launch()
+        console_pattern = 'Kernel command line: %s' % kernel_command_line
+        self.wait_for_console_pattern(console_pattern)
