08f7ad1b00a00eeb6c8f499b138f41d3b59f0ece
Jafar Abdi <cafer.abdi@gmail.com>
2019-03-23 17:26:35 +0300

    tests/libqos: fix usage of bool in pci-pc.c
    Clean up wrong usage of FALSE and TRUE in places that use "bool" from stdbool.h.
    
    FALSE and TRUE (with capital letters) are the constants defined by glib for
    being used with the "gboolean" type of glib. But some parts of the code also use
    TRUE and FALSE for variables that are declared as "bool" (the type from <stdbool.h>).
    
    Signed-off-by: Jafar Abdi <cafer.abdi@gmail.com>
    Reviewed-by: Eric Blake <eblake@redhat.com>
    Reviewed-by: Thomas Huth <thuth@redhat.com>
    Message-Id: <1553351197-14581-3-git-send-email-cafer.abdi@gmail.com>
    Signed-off-by: Thomas Huth <thuth@redhat.com>

--- tests/libqos/pci-pc.c
+++ tests/libqos/pci-pc.c
@@ -125,7 +125,7 @@ void qpci_init_pc(QPCIBusPC *qpci, QTestState *qts, QGuestAllocator *alloc)
     assert(qts);
 
     /* tests can use pci-bus */
-    qpci->bus.has_buggy_msi = FALSE;
+    qpci->bus.has_buggy_msi = false;
 
     qpci->bus.pio_readb = qpci_pc_pio_readb;
     qpci->bus.pio_readw = qpci_pc_pio_readw;
