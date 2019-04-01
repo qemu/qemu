1c685a9026349752c3627aa24b090973e35b83c3
Greg Kurz <groug@kaod.org>
2019-04-01 19:55:02 +0200

    pci: Allow PCI bus subtypes to support extended config space accesses
    Some PHB implementations, eg. PAPR used on pseries machine, act like
    a regular PCI bus rather than a PCIe bus, but allow access to the
    PCIe extended config space anyway.
    
    Introduce a new PCI bus class method to modelize this behaviour and
    use it when adjusting the config space size limit during accesses.
    
    No behaviour change for existing PCI bus types.
    
    Signed-off-by: Greg Kurz <groug@kaod.org>
    Message-Id: <155414130271.574858.4253514266378127489.stgit@bahia.lan>
    Signed-off-by: David Gibson <david@gibson.dropbear.id.au>

--- include/hw/pci/pci_bus.h
+++ include/hw/pci/pci_bus.h
@@ -18,6 +18,7 @@ typedef struct PCIBusClass {
     bool (*is_root)(PCIBus *bus);
     int (*bus_num)(PCIBus *bus);
     uint16_t (*numa_node)(PCIBus *bus);
+    bool (*allows_extended_config_space)(PCIBus *bus);
 } PCIBusClass;
 
 struct PCIBus {
