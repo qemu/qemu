958a01dab8e02fc49f4fd619fad8c82a1108afdb
Vitaly Kuznetsov <vkuznets@redhat.com>
2019-04-02 10:02:15 +0200

    ioapic: allow buggy guests mishandling level-triggered interrupts to make progress
    It was found that Hyper-V 2016 on KVM in some configurations (q35 machine +
    piix4-usb-uhci) hangs on boot. Root-cause was that one of Hyper-V
    level-triggered interrupt handler performs EOI before fixing the cause of
    the interrupt. This results in IOAPIC keep re-raising the level-triggered
    interrupt after EOI because irq-line remains asserted.
    
    Gory details: https://www.spinics.net/lists/kvm/msg184484.html
    (the whole thread).
    
    Turns out we were dealing with similar issues before; in-kernel IOAPIC
    implementation has commit 184564efae4d ("kvm: ioapic: conditionally delay
    irq delivery duringeoi broadcast") which describes a very similar issue.
    
    Steal the idea from the above mentioned commit for IOAPIC implementation in
    QEMU. SUCCESSIVE_IRQ_MAX_COUNT, delay and the comment are borrowed as well.
    
    Signed-off-by: Vitaly Kuznetsov <vkuznets@redhat.com>
    Message-Id: <20190402080215.10747-1-vkuznets@redhat.com>
    Signed-off-by: Paolo Bonzini <pbonzini@redhat.com>

--- include/hw/i386/ioapic_internal.h
+++ include/hw/i386/ioapic_internal.h
@@ -96,6 +96,7 @@ typedef struct IOAPICCommonClass {
     SysBusDeviceClass parent_class;
 
     DeviceRealize realize;
+    DeviceUnrealize unrealize;
     void (*pre_save)(IOAPICCommonState *s);
     void (*post_load)(IOAPICCommonState *s);
 } IOAPICCommonClass;
@@ -111,6 +112,8 @@ struct IOAPICCommonState {
     uint8_t version;
     uint64_t irq_count[IOAPIC_NUM_PINS];
     int irq_level[IOAPIC_NUM_PINS];
+    int irq_eoi[IOAPIC_NUM_PINS];
+    QEMUTimer *delayed_ioapic_service_timer;
 };
 
 void ioapic_reset_common(DeviceState *dev);
