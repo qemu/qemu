d013283a46e1f197594e76ed320f867067ab80b3
Juan Quintela <quintela@redhat.com>
2019-04-03 13:49:51 +0200

    migration: Fix migrate_set_parameter
    Otherwise we are setting err twice, what is wrong and causes an abort.
    
    Signed-off-by: Juan Quintela <quintela@redhat.com>
    Message-Id: <20190403114958.3705-2-quintela@redhat.com>
    Reviewed-by: Dr. David Alan Gilbert <dgilbert@redhat.com>
    Signed-off-by: Dr. David Alan Gilbert <dgilbert@redhat.com>

--- hmp.c
+++ hmp.c
@@ -1825,8 +1825,10 @@ void hmp_migrate_set_parameter(Monitor *mon, const QDict *qdict)
     case MIGRATION_PARAMETER_XBZRLE_CACHE_SIZE:
         p->has_xbzrle_cache_size = true;
         visit_type_size(v, param, &cache_size, &err);
-        if (err || cache_size > INT64_MAX
-            || (size_t)cache_size != cache_size) {
+        if (err) {
+            break;
+        }
+        if (cache_size > INT64_MAX || (size_t)cache_size != cache_size) {
             error_setg(&err, "Invalid size %s", valuestr);
             break;
         }
