1fb3e5cde8dcd9b5917aea9a0b2918e16be8be1e
Jason J. Herne <jjherne@linux.ibm.com>
2019-04-04 10:34:26 -0400

    s390-bios: ptr2u32 and u32toptr
    Introduce inline functions to convert between pointers and unsigned 32-bit
    ints. These are used to hide the ugliness required to  avoid compiler
    warnings.
    
    Signed-off-by: Jason J. Herne <jjherne@linux.ibm.com>
    Acked-by: Cornelia Huck <cohuck@redhat.com>
    Reviewed-by: Thomas Huth <thuth@redhat.com>
    Message-Id: <1554388475-18329-8-git-send-email-jjherne@linux.ibm.com>
    Signed-off-by: Thomas Huth <thuth@redhat.com>

---
+++ pc-bios/s390-ccw/helper.h
@@ -0,0 +1,31 @@
+/*
+ * Helper Functions
+ *
+ * Copyright (c) 2019 IBM Corp.
+ *
+ * Author(s): Jason J. Herne <jjherne@us.ibm.com>
+ *
+ * This work is licensed under the terms of the GNU GPL, version 2 or (at
+ * your option) any later version. See the COPYING file in the top-level
+ * directory.
+ */
+
+#ifndef S390_CCW_HELPER_H
+#define S390_CCW_HELPER_H
+
+#include "s390-ccw.h"
+
+/* Avoids compiler warnings when casting a pointer to a u32 */
+static inline uint32_t ptr2u32(void *ptr)
+{
+    IPL_assert((uint64_t)ptr <= 0xffffffff, "ptr2u32: ptr too large");
+    return (uint32_t)(uint64_t)ptr;
+}
+
+/* Avoids compiler warnings when casting a u32 to a pointer */
+static inline void *u32toptr(uint32_t n)
+{
+    return (void *)(uint64_t)n;
+}
+
+#endif
