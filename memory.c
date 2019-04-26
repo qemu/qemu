39adb536b3502060206c7515a50fb8faad22f51f
Wei Yang <richardw.yang@linux.intel.com>
2019-04-26 10:09:27 +0800

    memory: correct the comment to DIRTY_MEMORY_MIGRATION
    The dirty bit is DIRTY_MEMORY_MIGRATION. Correct the comment.
    
    Signed-off-by: Wei Yang <richardw.yang@linux.intel.com>
    Message-Id: <20190426020927.25470-1-richardw.yang@linux.intel.com>
    Signed-off-by: Paolo Bonzini <pbonzini@redhat.com>

--- memory.c
+++ memory.c
@@ -2584,7 +2584,7 @@ void memory_global_dirty_log_start(void)
 
     MEMORY_LISTENER_CALL_GLOBAL(log_global_start, Forward);
 
-    /* Refresh DIRTY_LOG_MIGRATION bit.  */
+    /* Refresh DIRTY_MEMORY_MIGRATION bit.  */
     memory_region_transaction_begin();
     memory_region_update_pending = true;
     memory_region_transaction_commit();
@@ -2594,7 +2594,7 @@ static void memory_global_dirty_log_do_stop(void)
 {
     global_dirty_log = false;
 
-    /* Refresh DIRTY_LOG_MIGRATION bit.  */
+    /* Refresh DIRTY_MEMORY_MIGRATION bit.  */
     memory_region_transaction_begin();
     memory_region_update_pending = true;
     memory_region_transaction_commit();
