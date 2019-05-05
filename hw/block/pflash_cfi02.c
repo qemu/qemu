aeaf6c20dbcfed5c459d9f6b39cb5fa2187f0b29
Philippe Mathieu-Daudé <philmd@redhat.com>
2019-05-05 22:42:08 +0200

    hw/block/pflash_cfi02: Add an enum to define the write cycles
    No change in functionality is intended with this commit.
    
    Signed-off-by: Stephen Checkoway <stephen.checkoway@oberlin.edu>
    Message-Id: <20190426162624.55977-3-stephen.checkoway@oberlin.edu>
    Reviewed-by: Philippe Mathieu-Daudé <philmd@redhat.com>
    Tested-by: Philippe Mathieu-Daudé <philmd@redhat.com>
    [PMD: Extracted from bigger patch]
    Reviewed-by: Alistair Francis <alistair.francis@wdc.com>
    Signed-off-by: Philippe Mathieu-Daudé <philmd@redhat.com>

--- hw/block/pflash_cfi02.c
+++ hw/block/pflash_cfi02.c
@@ -57,6 +57,11 @@ do {                                                       \
 
 #define PFLASH_LAZY_ROMD_THRESHOLD 42
 
+/* Special write cycles for CFI queries. */
+enum {
+    WCYCLE_CFI              = 7,
+};
+
 struct PFlashCFI02 {
     /*< private >*/
     SysBusDevice parent_obj;
@@ -286,7 +291,7 @@ static void pflash_write(PFlashCFI02 *pfl, hwaddr offset,
         if (boff == 0x55 && cmd == 0x98) {
         enter_CFI_mode:
             /* Enter CFI query mode */
-            pfl->wcycle = 7;
+            pfl->wcycle = WCYCLE_CFI;
             pfl->cmd = 0x98;
             return;
         }
@@ -458,7 +463,8 @@ static void pflash_write(PFlashCFI02 *pfl, hwaddr offset,
             goto reset_flash;
         }
         break;
-    case 7: /* Special value for CFI queries */
+    /* Special values for CFI queries */
+    case WCYCLE_CFI:
         DPRINTF("%s: invalid write in CFI query mode\n", __func__);
         goto reset_flash;
     default:
