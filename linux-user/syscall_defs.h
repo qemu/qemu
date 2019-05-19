c495a793409595c3f08ec5129ab7d8f267d73d28
Aleksandar Markovic <amarkovic@wavecomp.com>
2019-05-19 18:15:23 +0200

    linux-user: Add support for SIOCSPGRP ioctl for all targets
    Add support for setting the process (or process group) to receive SIGIO
    or SIGURG signals when I/O becomes possible or urgent data is available,
    using SIOCSPGRP ioctl.
    
    The ioctl numeric values for SIOCSPGRP are platform-dependent and are
    determined by following files in Linux kernel source tree:
    
    arch/ia64/include/uapi/asm/sockios.h:#define SIOCSPGRP    0x8902
    arch/mips/include/uapi/asm/sockios.h:#define SIOCSPGRP    _IOW('s', 8, pid_t)
    arch/parisc/include/uapi/asm/sockios.h:#define SIOCSPGRP  0x8902
    arch/sh/include/uapi/asm/sockios.h:#define SIOCSPGRP      _IOW('s', 8, pid_t)
    arch/xtensa/include/uapi/asm/sockios.h:#define SIOCSPGRP  _IOW('s', 8, pid_t)
    arch/alpha/include/uapi/asm/sockios.h:#define SIOCSPGRP   _IOW('s', 8, pid_t)
    arch/sparc/include/uapi/asm/sockios.h:#define SIOCSPGRP   0x8902
    include/uapi/asm-generic/sockios.h:#define SIOCSPGRP      0x8902
    
    Hence the different definition for alpha, mips, sh4, and xtensa.
    
    Signed-off-by: Aleksandar Markovic <amarkovic@wavecomp.com>
    Reviewed-by: Max Filippov <jcmvbkbc@gmail.com>
    Reviewed-by: Laurent Vivier <laurent@vivier.eu>
    Message-Id: <1558282527-22183-3-git-send-email-aleksandar.markovic@rt-rk.com>
    Signed-off-by: Laurent Vivier <laurent@vivier.eu>

--- linux-user/syscall_defs.h
+++ linux-user/syscall_defs.h
@@ -740,11 +740,14 @@ struct target_pollfd {
 #if defined(TARGET_ALPHA) || defined(TARGET_MIPS) || defined(TARGET_SH4) ||    \
        defined(TARGET_XTENSA)
 #define TARGET_SIOCATMARK      TARGET_IOR('s', 7, int)
+#define TARGET_SIOCSPGRP       TARGET_IOW('s', 8, pid_t)
 #define TARGET_SIOCGPGRP       TARGET_IOR('s', 9, pid_t)
 #else
 #define TARGET_SIOCATMARK      0x8905
+#define TARGET_SIOCSPGRP       0x8902
 #define TARGET_SIOCGPGRP       0x8904
 #endif
+
 #define TARGET_SIOCGSTAMP      0x8906          /* Get stamp (timeval) */
 #define TARGET_SIOCGSTAMPNS    0x8907          /* Get stamp (timespec) */
 
