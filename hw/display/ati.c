263807f4e85e94f2d4e988d61d2fc0319b1dd36b
Gerd Hoffmann <kraxel@redhat.com>
2019-06-20 17:11:04 +0200

    ati-vga: switch to vgabios-ati.bin
    Signed-off-by: Gerd Hoffmann <kraxel@redhat.com>
    Tested-by: BALATON Zoltan <balaton@eik.bme.hu>
    Message-id: 20190620151104.2678-4-kraxel@redhat.com
    Signed-off-by: Gerd Hoffmann <kraxel@redhat.com>

--- hw/display/ati.c
+++ hw/display/ati.c
@@ -910,7 +910,7 @@ static void ati_vga_class_init(ObjectClass *klass, void *data)
     k->class_id = PCI_CLASS_DISPLAY_VGA;
     k->vendor_id = PCI_VENDOR_ID_ATI;
     k->device_id = PCI_DEVICE_ID_ATI_RAGE128_PF;
-    k->romfile = "vgabios-stdvga.bin";
+    k->romfile = "vgabios-ati.bin";
     k->realize = ati_vga_realize;
     k->exit = ati_vga_exit;
 }
