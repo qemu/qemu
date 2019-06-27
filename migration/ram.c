a162b572e9e5a85c440656fe97d7a8b133257379
Wei Yang <richardw.yang@linux.intel.com>
2019-06-27 10:08:22 +0800

    migration/postcopy: discard_length must not be 0
    Since we break the loop when there is no more page to discard, we are
    sure the following process would find some page to discard.
    
    It is not necessary to check it again.
    
    Signed-off-by: Wei Yang <richardw.yang@linux.intel.com>
    Message-Id: <20190627020822.15485-4-richardw.yang@linux.intel.com>
    Reviewed-by: Dr. David Alan Gilbert <dgilbert@redhat.com>
    Signed-off-by: Dr. David Alan Gilbert <dgilbert@redhat.com>

--- migration/ram.c
+++ migration/ram.c
@@ -2877,9 +2877,7 @@ static int postcopy_send_discard_bm_ram(MigrationState *ms,
         } else {
             discard_length = zero - one;
         }
-        if (discard_length) {
-            postcopy_discard_send_range(ms, pds, one, discard_length);
-        }
+        postcopy_discard_send_range(ms, pds, one, discard_length);
         current = one + discard_length;
     }
 
