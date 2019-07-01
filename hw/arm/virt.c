4414942e7ea17e7769495b43010869ec4821ee40
Jan Kiszka <jan.kiszka@siemens.com>
2019-07-01 17:26:14 +0100

    hw/arm/virt: Add support for Cortex-A7
    Allow cortex-a7 to be used with the virt board; it supports
    the v7VE features and there is no reason to deny this type.
    
    Signed-off-by: Jan Kiszka <jan.kiszka@siemens.com>
    Reviewed-by: Philippe Mathieu-Daudé <philmd@redhat.com>
    Message-id: fc5404f7-4d1d-c28f-6e48-d8799c82acc0@web.de
    Reviewed-by: Peter Maydell <peter.maydell@linaro.org>
    Signed-off-by: Peter Maydell <peter.maydell@linaro.org>

--- hw/arm/virt.c
+++ hw/arm/virt.c
@@ -176,6 +176,7 @@ static const int a15irqmap[] = {
 };
 
 static const char *valid_cpus[] = {
+    ARM_CPU_TYPE_NAME("cortex-a7"),
     ARM_CPU_TYPE_NAME("cortex-a15"),
     ARM_CPU_TYPE_NAME("cortex-a53"),
     ARM_CPU_TYPE_NAME("cortex-a57"),
