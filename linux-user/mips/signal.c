4ced996ffe3f08883fa5bd423bcfafdfd6f13189
Aleksandar Markovic <amarkovic@wavecomp.com>
2019-07-12 13:37:46 +0200

    linux-user: Fix structure target_ucontext for MIPS
    Structure ucontext for MIPS is defined in the following way in
    Linux kernel:
    
    (arch/mips/include/uapi/asm/ucontext.h, lines 54-64)
    
    struct ucontext {
        /* Historic fields matching asm-generic */
        unsigned long       uc_flags;
        struct ucontext     *uc_link;
        stack_t             uc_stack;
        struct sigcontext   uc_mcontext;
        sigset_t            uc_sigmask;
    
        /* Extended context structures may follow ucontext */
        unsigned long long	uc_extcontext[0];
    };
    
    Fix the structure target_ucontext for MIPS to reflect the definition
    above, except the correction for field uc_extcontext, which will
    follow at some later time.
    
    Fixes: 94c5495d
    
    Reported-by: Dragan Mladjenovic <dmladjenovic@wavecomp.com>
    Signed-off-by: Aleksandar Markovic <amarkovic@wavecomp.com>
    Reviewed-by: Laurent Vivier <laurent@vivier.eu>
    Message-Id: <1562931470-3700-2-git-send-email-aleksandar.markovic@rt-rk.com>
    Signed-off-by: Laurent Vivier <laurent@vivier.eu>

--- linux-user/mips/signal.c
+++ linux-user/mips/signal.c
@@ -71,10 +71,9 @@ struct sigframe {
 };
 
 struct target_ucontext {
-    target_ulong tuc_flags;
-    target_ulong tuc_link;
+    abi_ulong tuc_flags;
+    abi_ulong tuc_link;
     target_stack_t tuc_stack;
-    target_ulong pad0;
     struct target_sigcontext tuc_mcontext;
     target_sigset_t tuc_sigmask;
 };
