57e23e78372d8b8426b650cde0ab92adc0fe1865
Philippe Mathieu-Daudé <philmd@redhat.com>
2019-08-19 00:54:05 +0200

    hw/i386/pc: Pass the apic_id_limit value by argument
    Pass the apic_id_limit value by argument, this will
    allow us to remove the PCMachineState argument later.
    
    Suggested-by: Samuel Ortiz <sameo@linux.intel.com>
    Signed-off-by: Philippe Mathieu-Daudé <philmd@redhat.com>
    Message-Id: <20190818225414.22590-7-philmd@redhat.com>
    Signed-off-by: Paolo Bonzini <pbonzini@redhat.com>

--- hw/i386/pc.c
+++ hw/i386/pc.c
@@ -937,7 +937,8 @@ static void pc_build_smbios(PCMachineState *pcms)
 }
 
 static FWCfgState *fw_cfg_arch_create(PCMachineState *pcms,
-                                      uint16_t boot_cpus)
+                                      uint16_t boot_cpus,
+                                      uint16_t apic_id_limit)
 {
     FWCfgState *fw_cfg;
     uint64_t *numa_fw_cfg;
@@ -1870,7 +1871,7 @@ void pc_memory_init(PCMachineState *pcms,
                                         option_rom_mr,
                                         1);
 
-    fw_cfg = fw_cfg_arch_create(pcms, pcms->boot_cpus);
+    fw_cfg = fw_cfg_arch_create(pcms, pcms->boot_cpus, pcms->apic_id_limit);
 
     rom_set_fw(fw_cfg);
 
