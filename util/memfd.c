9bdfa4d23f4395dab89a60eb6b05a0308792bd66
Shu-Chun Weng <scw@google.com>
2019-08-19 11:09:47 -0700

    linux-user: add memfd_create
    Add support for the memfd_create syscall. If the host does not have the
    libc wrapper, translate to a direct syscall with NC-macro.
    
    Buglink: https://bugs.launchpad.net/qemu/+bug/1734792
    Signed-off-by: Shu-Chun Weng <scw@google.com>
    Reviewed-by: Laurent Vivier <laurent@vivier.eu>
    Message-Id: <20190819180947.180725-1-scw@google.com>
    Signed-off-by: Laurent Vivier <laurent@vivier.eu>

--- util/memfd.c
+++ util/memfd.c
@@ -35,7 +35,7 @@
 #include <sys/syscall.h>
 #include <asm/unistd.h>
 
-static int memfd_create(const char *name, unsigned int flags)
+int memfd_create(const char *name, unsigned int flags)
 {
 #ifdef __NR_memfd_create
     return syscall(__NR_memfd_create, name, flags);
