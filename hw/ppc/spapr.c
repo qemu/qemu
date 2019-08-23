f767b1ac5766fcc48cc3939eeb7db7b95b98408b
Alexey Kardashevskiy <aik@ozlabs.ru>
2019-08-23 12:39:57 +1000

    spapr: Fixes a leak in CAS
    Add a missing g_free(fdt) if the resulting tree is bigger
    than the space allocated by SLOF.
    
    Signed-off-by: Alexey Kardashevskiy <aik@ozlabs.ru>
    Signed-off-by: David Gibson <david@gibson.dropbear.id.au>
    Reviewed-by: Greg Kurz <groug@kaod.org>
    Reviewed-by: Cédric Le Goater <clg@kaod.org>

--- hw/ppc/spapr.c
+++ hw/ppc/spapr.c
@@ -1026,6 +1026,7 @@ int spapr_h_cas_compose_response(SpaprMachineState *spapr,
     _FDT((fdt_pack(fdt)));
 
     if (fdt_totalsize(fdt) + sizeof(hdr) > size) {
+        g_free(fdt);
         trace_spapr_cas_failed(size);
         return -1;
     }
