58c46efa451caa3935224223f950216872e2eee3
Laurent Vivier <lvivier@redhat.com>
2019-08-30 18:13:45 +0200

    pseries: do not allow memory-less/cpu-less NUMA node
    When we hotplug a CPU on memory-less/cpu-less node, the linux kernel
    crashes.
    
    This happens because linux kernel needs to know the NUMA topology at
    start to be able to initialize the distance lookup table.
    
    On pseries, the topology is provided by the firmware via the existing
    CPUs and memory information. Thus a node without memory and CPU cannot be
    discovered by the kernel.
    
    To avoid the kernel crash, do not allow to start pseries with empty
    nodes.
    
    Signed-off-by: Laurent Vivier <lvivier@redhat.com>
    Message-Id: <20190830161345.22436-1-lvivier@redhat.com>
    [dwg: Rework to cope with movement of numa state from globals to MachineState]
    Signed-off-by: David Gibson <david@gibson.dropbear.id.au>

--- hw/ppc/spapr.c
+++ hw/ppc/spapr.c
@@ -2854,6 +2854,39 @@ static void spapr_machine_init(MachineState *machine)
     /* init CPUs */
     spapr_init_cpus(spapr);
 
+    /*
+     * check we don't have a memory-less/cpu-less NUMA node
+     * Firmware relies on the existing memory/cpu topology to provide the
+     * NUMA topology to the kernel.
+     * And the linux kernel needs to know the NUMA topology at start
+     * to be able to hotplug CPUs later.
+     */
+    if (machine->numa_state->num_nodes) {
+        for (i = 0; i < machine->numa_state->num_nodes; ++i) {
+            /* check for memory-less node */
+            if (machine->numa_state->nodes[i].node_mem == 0) {
+                CPUState *cs;
+                int found = 0;
+                /* check for cpu-less node */
+                CPU_FOREACH(cs) {
+                    PowerPCCPU *cpu = POWERPC_CPU(cs);
+                    if (cpu->node_id == i) {
+                        found = 1;
+                        break;
+                    }
+                }
+                /* memory-less and cpu-less node */
+                if (!found) {
+                    error_report(
+                       "Memory-less/cpu-less nodes are not supported (node %d)",
+                                 i);
+                    exit(1);
+                }
+            }
+        }
+
+    }
+
     if ((!kvm_enabled() || kvmppc_has_cap_mmu_radix()) &&
         ppc_type_check_compat(machine->cpu_type, CPU_POWERPC_LOGICAL_3_00, 0,
                               spapr->max_compat_pvr)) {
