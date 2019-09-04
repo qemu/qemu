e1b3d47751a420835cb0560fd029c39fea961a79
Gerd Hoffmann <kraxel@redhat.com>
2019-09-04 07:52:50 +0200

    vnc: fix websocket field in events
    Just need to fill VncClientInfo.websocket in vnc_client_cache_addr().
    
    Buglink: https://bugzilla.redhat.com/show_bug.cgi?id=1748175
    Signed-off-by: Gerd Hoffmann <kraxel@redhat.com>
    Reviewed-by: Stefan Hajnoczi <stefanha@redhat.com>
    Message-id: 20190904055250.22421-1-kraxel@redhat.com

--- ui/vnc.c
+++ ui/vnc.c
@@ -278,6 +278,7 @@ static void vnc_client_cache_addr(VncState *client)
     vnc_init_basic_info_from_remote_addr(client->sioc,
                                          qapi_VncClientInfo_base(client->info),
                                          &err);
+    client->info->websocket = client->websocket;
     if (err) {
         qapi_free_VncClientInfo(client->info);
         client->info = NULL;
