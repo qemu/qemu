1cc52740160e6b6762b1fafcd3a9fc0e498e45d0
Max Filippov <jcmvbkbc@gmail.com>
2019-09-06 09:57:36 -0700

    linux-user: drop redundant handling of environment variables
    QEMU_STRACE and QEMU_RAND_SEED are handled by the parse_args, no need to
    do it again in main.
    
    Signed-off-by: Max Filippov <jcmvbkbc@gmail.com>
    Reviewed-by: Laurent Vivier <laurent@vivier.eu>
    Message-Id: <20190906165736.5612-1-jcmvbkbc@gmail.com>
    Signed-off-by: Laurent Vivier <laurent@vivier.eu>

--- linux-user/main.c
+++ linux-user/main.c
@@ -706,13 +706,6 @@ int main(int argc, char **argv, char **envp)
         reserved_va = max_reserved_va & qemu_host_page_mask;
     }
 
-    if (getenv("QEMU_STRACE")) {
-        do_strace = 1;
-    }
-
-    if (seed_optarg == NULL) {
-        seed_optarg = getenv("QEMU_RAND_SEED");
-    }
     {
         Error *err = NULL;
         if (seed_optarg != NULL) {
