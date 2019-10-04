878b2b48ee49a24488ddf8654a59a568c864c815
Greg Kurz <groug@kaod.org>
2019-10-04 09:38:50 +0200

    xive: Make some device types not user creatable
    Some device types of the XIVE model are exposed to the QEMU command
    line:
    
    $ ppc64-softmmu/qemu-system-ppc64 -device help | grep xive
    name "xive-end-source", desc "XIVE END Source"
    name "xive-source", desc "XIVE Interrupt Source"
    name "xive-tctx", desc "XIVE Interrupt Thread Context"
    
    These are internal devices that shouldn't be instantiable by the
    user. By the way, they can't be because their respective realize
    functions expect link properties that can't be set from the command
    line:
    
    qemu-system-ppc64: -device xive-source: required link 'xive' not found:
     Property '.xive' not found
    qemu-system-ppc64: -device xive-end-source: required link 'xive' not found:
     Property '.xive' not found
    qemu-system-ppc64: -device xive-tctx: required link 'cpu' not found:
     Property '.cpu' not found
    
    Hide them by setting dc->user_creatable to false in their respective
    class init functions.
    
    Signed-off-by: Greg Kurz <groug@kaod.org>
    Message-Id: <157017473006.331610.2983143972519884544.stgit@bahia.lan>
    Message-Id: <157045578401.865784.6058183726552779559.stgit@bahia.lan>
    Reviewed-by: Cédric Le Goater <clg@kaod.org>
    [dwg: Folded comment update into base patch]
    Signed-off-by: David Gibson <david@gibson.dropbear.id.au>

--- hw/intc/xive.c
+++ hw/intc/xive.c
@@ -670,6 +670,11 @@ static void xive_tctx_class_init(ObjectClass *klass, void *data)
     dc->realize = xive_tctx_realize;
     dc->unrealize = xive_tctx_unrealize;
     dc->vmsd = &vmstate_xive_tctx;
+    /*
+     * Reason: part of XIVE interrupt controller, needs to be wired up
+     * by xive_tctx_create().
+     */
+    dc->user_creatable = false;
 }
 
 static const TypeInfo xive_tctx_info = {
@@ -1118,6 +1123,11 @@ static void xive_source_class_init(ObjectClass *klass, void *data)
     dc->props   = xive_source_properties;
     dc->realize = xive_source_realize;
     dc->vmsd    = &vmstate_xive_source;
+    /*
+     * Reason: part of XIVE interrupt controller, needs to be wired up,
+     * e.g. by spapr_xive_instance_init().
+     */
+    dc->user_creatable = false;
 }
 
 static const TypeInfo xive_source_info = {
@@ -1853,6 +1863,11 @@ static void xive_end_source_class_init(ObjectClass *klass, void *data)
     dc->desc    = "XIVE END Source";
     dc->props   = xive_end_source_properties;
     dc->realize = xive_end_source_realize;
+    /*
+     * Reason: part of XIVE interrupt controller, needs to be wired up,
+     * e.g. by spapr_xive_instance_init().
+     */
+    dc->user_creatable = false;
 }
 
 static const TypeInfo xive_end_source_info = {
