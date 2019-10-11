8010d3fce008dd13f155bc0babfe236ea44a2712
Michael S. Tsirkin <mst@redhat.com>
2019-10-11 15:58:03 +0200

    virtio: new post_load hook
    Post load hook in virtio vmsd is called early while device is processed,
    and when VirtIODevice core isn't fully initialized.  Most device
    specific code isn't ready to deal with a device in such state, and
    behaves weirdly.
    
    Add a new post_load hook in a device class instead.  Devices should use
    this unless they specifically want to verify the migration stream as
    it's processed, e.g. for bounds checking.
    
    Cc: qemu-stable@nongnu.org
    Suggested-by: "Dr. David Alan Gilbert" <dgilbert@redhat.com>
    Cc: Mikhail Sennikovsky <mikhail.sennikovskii@cloud.ionos.com>
    Signed-off-by: Michael S. Tsirkin <mst@redhat.com>
    Signed-off-by: Jason Wang <jasowang@redhat.com>
    (cherry picked from commit 1dd713837cac8ec5a97d3b8492d72ce5ac94803c)
    Signed-off-by: Michael Roth <mdroth@linux.vnet.ibm.com>

--- hw/virtio/virtio.c
+++ hw/virtio/virtio.c
@@ -2287,6 +2287,13 @@ int virtio_load(VirtIODevice *vdev, QEMUFile *f, int version_id)
     }
     rcu_read_unlock();
 
+    if (vdc->post_load) {
+        ret = vdc->post_load(vdev);
+        if (ret) {
+            return ret;
+        }
+    }
+
     return 0;
 }
 
