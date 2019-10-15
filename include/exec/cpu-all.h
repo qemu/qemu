f048b8a7cefcabb0e55210a76775f9be57c4d3f4
Richard Henderson <richard.henderson@linaro.org>
2019-10-15 11:10:27 -0700

    exec: Promote TARGET_PAGE_MASK to target_long
    There are some uint64_t uses that expect TARGET_PAGE_MASK to
    extend for a 32-bit, so this must continue to be a signed type.
    Define based on TARGET_PAGE_BITS not TARGET_PAGE_SIZE; this
    will make a following patch more clear.
    
    This should not have a functional effect so far.
    
    Reviewed-by: Alex Bennée <alex.bennee@linaro.org>
    Reviewed-by: Paolo Bonzini <pbonzini@redhat.com>
    Signed-off-by: Richard Henderson <richard.henderson@linaro.org>

--- include/exec/cpu-all.h
+++ include/exec/cpu-all.h
@@ -229,7 +229,7 @@ extern TargetPageBits target_page;
 #endif
 
 #define TARGET_PAGE_SIZE (1 << TARGET_PAGE_BITS)
-#define TARGET_PAGE_MASK ~(TARGET_PAGE_SIZE - 1)
+#define TARGET_PAGE_MASK ((target_long)-1 << TARGET_PAGE_BITS)
 #define TARGET_PAGE_ALIGN(addr) ROUND_UP((addr), TARGET_PAGE_SIZE)
 
 /* Using intptr_t ensures that qemu_*_page_mask is sign-extended even
