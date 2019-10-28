92d9361255a0862379866f156686180902bb63d2
Philippe Mathieu-Daudé <f4bug@amsat.org>
2019-10-28 19:04:04 -0400

    tests/boot_linux_console: Add a test for the Raspberry Pi 2
    Similar to the x86_64/pc test, it boots a Linux kernel on a raspi2
    board and verify the serial is working.
    
    The kernel image and DeviceTree blob are built by the Raspbian
    project (based on Debian):
    https://www.raspbian.org/RaspbianImages
    as recommended by the Raspberry Pi project:
    https://www.raspberrypi.org/downloads/raspbian/
    
    If ARM is a target being built, "make check-acceptance" will
    automatically include this test by the use of the "arch:arm" tags.
    
    Alternatively, this test can be run using:
    
        $ avocado run -t arch:arm tests/acceptance
        $ avocado run -t machine:raspi2 tests/acceptance
    
    Reviewed-by: Alex Bennée <alex.bennee@linaro.org>
    Reviewed-by: Cleber Rosa <crosa@redhat.com>
    Tested-by: Cleber Rosa <crosa@redhat.com>
    Signed-off-by: Philippe Mathieu-Daudé <f4bug@amsat.org>
    Message-Id: <20191028073441.6448-21-philmd@redhat.com>
    Signed-off-by: Cleber Rosa <crosa@redhat.com>

--- tests/acceptance/boot_linux_console.py
+++ tests/acceptance/boot_linux_console.py
@@ -292,6 +292,42 @@ class BootLinuxConsole(Test):
         self.vm.launch()
         self.wait_for_console_pattern('init started: BusyBox')
 
+    def do_test_arm_raspi2(self, uart_id):
+        """
+        The kernel can be rebuilt using the kernel source referenced
+        and following the instructions on the on:
+        https://www.raspberrypi.org/documentation/linux/kernel/building.md
+        """
+        serial_kernel_cmdline = {
+            0: 'earlycon=pl011,0x3f201000 console=ttyAMA0',
+        }
+        deb_url = ('http://archive.raspberrypi.org/debian/'
+                   'pool/main/r/raspberrypi-firmware/'
+                   'raspberrypi-kernel_1.20190215-1_armhf.deb')
+        deb_hash = 'cd284220b32128c5084037553db3c482426f3972'
+        deb_path = self.fetch_asset(deb_url, asset_hash=deb_hash)
+        kernel_path = self.extract_from_deb(deb_path, '/boot/kernel7.img')
+        dtb_path = self.extract_from_deb(deb_path, '/boot/bcm2709-rpi-2-b.dtb')
+
+        self.vm.set_machine('raspi2')
+        self.vm.set_console()
+        kernel_command_line = (self.KERNEL_COMMON_COMMAND_LINE +
+                               serial_kernel_cmdline[uart_id])
+        self.vm.add_args('-kernel', kernel_path,
+                         '-dtb', dtb_path,
+                         '-append', kernel_command_line)
+        self.vm.launch()
+        console_pattern = 'Kernel command line: %s' % kernel_command_line
+        self.wait_for_console_pattern(console_pattern)
+
+    def test_arm_raspi2_uart0(self):
+        """
+        :avocado: tags=arch:arm
+        :avocado: tags=machine:raspi2
+        :avocado: tags=device:pl011
+        """
+        self.do_test_arm_raspi2(0)
+
     def test_s390x_s390_ccw_virtio(self):
         """
         :avocado: tags=arch:s390x
