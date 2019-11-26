2a886794f1969020845d0085a41a884e01b357df
Greg Kurz <groug@kaod.org>
2019-11-26 17:46:17 +0100

    linux-headers: Update
    Update to mainline commit be2eca94d144 ("Merge tag 'for-linus-5.5-1'`
    of git://github.com/cminyard/linux-ipmi")
    
    Signed-off-by: Greg Kurz <groug@kaod.org>
    Message-Id: <157478677756.67101.11558821804418331832.stgit@bahia.tlslab.ibm.com>
    Signed-off-by: David Gibson <david@gibson.dropbear.id.au>

--- linux-headers/asm-mips/unistd_n64.h
+++ linux-headers/asm-mips/unistd_n64.h
@@ -340,6 +340,7 @@
 #define __NR_fsmount	(__NR_Linux + 432)
 #define __NR_fspick	(__NR_Linux + 433)
 #define __NR_pidfd_open	(__NR_Linux + 434)
+#define __NR_clone3	(__NR_Linux + 435)
 
 
 #endif /* _ASM_MIPS_UNISTD_N64_H */
