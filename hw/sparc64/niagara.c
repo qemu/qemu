bec6e07afde3bffb52f1a9d26a90ccbdfff13690
Philippe Mathieu-Daudé <f4bug@amsat.org>
2019-12-01 21:35:24 +0100

    hw/sparc64/niagara: Map the UART device unconditionally
    The UART is present on the machine regardless there is a
    character device connected to it. Map it unconditionally.
    
    Signed-off-by: Philippe Mathieu-Daudé <f4bug@amsat.org>
    Reviewed-by: Artyom Tarasenko <atar4qemu@gmail.com>
    Message-Id: <20200608172144.20461-4-f4bug@amsat.org>

--- hw/sparc64/niagara.c
+++ hw/sparc64/niagara.c
@@ -152,10 +152,8 @@ static void niagara_init(MachineState *machine)
             exit(1);
         }
     }
-    if (serial_hd(0)) {
-        serial_mm_init(sysmem, NIAGARA_UART_BASE, 0, NULL, 115200,
-                       serial_hd(0), DEVICE_BIG_ENDIAN);
-    }
+    serial_mm_init(sysmem, NIAGARA_UART_BASE, 0, NULL,
+                   115200, serial_hd(0), DEVICE_BIG_ENDIAN);
     create_unimplemented_device("sun4v-iob", NIAGARA_IOBBASE, NIAGARA_IOBSIZE);
     sun4v_rtc_init(NIAGARA_RTC_BASE);
 }
