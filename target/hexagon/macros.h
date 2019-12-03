32174c29ac64ccc61659aeb3c41d5fe9292eb4aa
Taylor Simpson <tsimpson@quicinc.com>
2019-12-03 11:17:22 -0600

    Add license to new files

--- target/hexagon/macros.h
+++ target/hexagon/macros.h
@@ -1,7 +1,22 @@
-/* Copyright (c) 2019 Qualcomm Innovation Center, Inc. All Rights Reserved. */
+/*
+ *  Copyright (c) 2019 Qualcomm Innovation Center, Inc. All Rights Reserved.
+ *
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program; if not, see <http://www.gnu.org/licenses/>.
+ */
 
-#ifndef _MACROS_H
-#define _MACROS_H
+#ifndef MACROS_H
+#define MACROS_H
 
 #include "imported/arch.h"
 #include "imported/iss_ver_registers.h"
