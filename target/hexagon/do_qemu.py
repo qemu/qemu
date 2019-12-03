d8ed4188a4560c21695b4808347eb4e0342f2955
Taylor Simpson <tsimpson@quicinc.com>
2019-12-03 11:17:22 -0600

    Add license to new files

--- target/hexagon/do_qemu.py
+++ target/hexagon/do_qemu.py
@@ -1,5 +1,21 @@
 #!/usr/bin/env python
-# Copyright (c) 2019 Qualcomm Innovation Center, Inc. All Rights Reserved.
+
+##
+##  Copyright (c) 2019 Qualcomm Innovation Center, Inc. All Rights Reserved.
+##
+##  This program is free software; you can redistribute it and/or modify
+##  it under the terms of the GNU General Public License as published by
+##  the Free Software Foundation; either version 2 of the License, or
+##  (at your option) any later version.
+##
+##  This program is distributed in the hope that it will be useful,
+##  but WITHOUT ANY WARRANTY; without even the implied warranty of
+##  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+##  GNU General Public License for more details.
+##
+##  You should have received a copy of the GNU General Public License
+##  along with this program; if not, see <http://www.gnu.org/licenses/>.
+##
 
 import sys
 import re
@@ -129,7 +145,8 @@ def MACROATTRIB(macname,beh,attribstring,ext=""):
 # read in file.  Evaluate each line: each line calls a function above
 
 for line in open(sys.argv[1]).readlines():
-    eval(line.strip())
+    if not line.startswith("#"):
+        eval(line.strip())
 
 
 calculate_attribs()
