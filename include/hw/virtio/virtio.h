d5a5d43e27308df63f4c7be4ec988c969ac45d1d
Michael S. Tsirkin <mst@redhat.com>
2019-12-09 11:46:13 -0500

    virtio: add ability to delete vq through a pointer
    Devices tend to maintain vq pointers, allow deleting them trough a vq pointer.
    
    Signed-off-by: Michael S. Tsirkin <mst@redhat.com>
    Reviewed-by: David Hildenbrand <david@redhat.com>
    Reviewed-by: David Hildenbrand <david@redhat.com>
    (cherry picked from commit 722f8c51d8af223751dfb1d02de40043e8ba067e)
    *prereq for 421afd2fe8
    Signed-off-by: Michael Roth <mdroth@linux.vnet.ibm.com>

--- include/hw/virtio/virtio.h
+++ include/hw/virtio/virtio.h
@@ -183,6 +183,8 @@ VirtQueue *virtio_add_queue(VirtIODevice *vdev, int queue_size,
 
 void virtio_del_queue(VirtIODevice *vdev, int n);
 
+void virtio_delete_queue(VirtQueue *vq);
+
 void virtqueue_push(VirtQueue *vq, const VirtQueueElement *elem,
                     unsigned int len);
 void virtqueue_flush(VirtQueue *vq, unsigned int count);
