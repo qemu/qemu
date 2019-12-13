1c8f85d93d261dc555a0aad6f54f2b5e8009d859
Tao Xu <tao3.xu@intel.com>
2019-12-13 09:19:29 +0800

    tests/bios-tables-test: add test cases for ACPI HMAT
    ACPI table HMAT has been introduced, QEMU now builds HMAT tables for
    Heterogeneous Memory with boot option '-numa node'.
    
    Add test cases on PC and Q35 machines with 2 numa nodes.
    Because HMAT is generated when system enable numa, the
    following tables need to be added for this test:
        tests/data/acpi/pc/APIC.acpihmat
        tests/data/acpi/pc/SRAT.acpihmat
        tests/data/acpi/pc/HMAT.acpihmat
        tests/data/acpi/pc/DSDT.acpihmat
        tests/data/acpi/q35/APIC.acpihmat
        tests/data/acpi/q35/SRAT.acpihmat
        tests/data/acpi/q35/HMAT.acpihmat
        tests/data/acpi/q35/DSDT.acpihmat
    
    Acked-by: Markus Armbruster <armbru@redhat.com>
    Reviewed-by: Igor Mammedov <imammedo@redhat.com>
    Reviewed-by: Daniel Black <daniel@linux.ibm.com>
    Reviewed-by: Jingqi Liu <Jingqi.liu@intel.com>
    Suggested-by: Igor Mammedov <imammedo@redhat.com>
    Signed-off-by: Tao Xu <tao3.xu@intel.com>
    Message-Id: <20191213011929.2520-9-tao3.xu@intel.com>
    Reviewed-by: Michael S. Tsirkin <mst@redhat.com>
    Signed-off-by: Michael S. Tsirkin <mst@redhat.com>

--- tests/bios-tables-test-allowed-diff.h
+++ tests/bios-tables-test-allowed-diff.h
@@ -1,1 +1,9 @@
 /* List of comma-separated changed AML files to ignore */
+"tests/data/acpi/pc/APIC.acpihmat",
+"tests/data/acpi/pc/SRAT.acpihmat",
+"tests/data/acpi/pc/HMAT.acpihmat",
+"tests/data/acpi/pc/DSDT.acpihmat",
+"tests/data/acpi/q35/APIC.acpihmat",
+"tests/data/acpi/q35/SRAT.acpihmat",
+"tests/data/acpi/q35/HMAT.acpihmat",
+"tests/data/acpi/q35/DSDT.acpihmat",
