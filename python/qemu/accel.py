1650c3e3043ac7e8f16bdfb746430a106e49c18f
Wainer dos Santos Moschetta <wainersm@redhat.com>
2019-12-16 16:14:35 -0300

    python/qemu: accel: Add list_accel() method
    Since commit cbe6d6365a48 the command `qemu -accel help` returns
    the list of accelerators enabled in the QEMU binary. This adds
    the list_accel() method which return that same list.
    
    Signed-off-by: Wainer dos Santos Moschetta <wainersm@redhat.com>
    Message-Id: <20191216191438.93418-3-wainersm@redhat.com>
    Reviewed-by: Cleber Rosa <crosa@redhat.com>
    Tested-by: Cleber Rosa <crosa@redhat.com>
    Signed-off-by: Cleber Rosa <crosa@redhat.com>

--- python/qemu/accel.py
+++ python/qemu/accel.py
@@ -14,7 +14,11 @@ accelerators.
 # the COPYING file in the top-level directory.
 #
 
+import logging
 import os
+import subprocess
+
+LOG = logging.getLogger(__name__)
 
 # Mapping host architecture to any additional architectures it can
 # support which often includes its 32 bit cousin.
@@ -22,6 +26,25 @@ ADDITIONAL_ARCHES = {
     "x86_64" : "i386",
     "aarch64" : "armhf"
 }
+
+def list_accel(qemu_bin):
+    """
+    List accelerators enabled in the QEMU binary.
+
+    @param qemu_bin (str): path to the QEMU binary.
+    @raise Exception: if failed to run `qemu -accel help`
+    @return a list of accelerator names.
+    """
+    if not qemu_bin:
+        return []
+    try:
+        out = subprocess.check_output([qemu_bin, '-accel', 'help'],
+                                      universal_newlines=True)
+    except:
+        LOG.debug("Failed to get the list of accelerators in %s", qemu_bin)
+        raise
+    # Skip the first line which is the header.
+    return [acc.strip() for acc in out.splitlines()[1:]]
 
 def kvm_available(target_arch=None):
     host_arch = os.uname()[4]
