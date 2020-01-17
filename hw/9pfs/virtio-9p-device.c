ad30a9e9043ac6ca386d5baca71e6c3d33bfcb9c
Pan Nengyuan <pannengyuan@huawei.com>
2020-01-17 14:09:27 +0800

    virtio-9p-device: convert to new virtio_delete_queue
    Use virtio_delete_queue to make it more clear.
    
    Signed-off-by: Pan Nengyuan <pannengyuan@huawei.com>
    Message-Id: <20200117060927.51996-3-pannengyuan@huawei.com>
    Reviewed-by: Michael S. Tsirkin <mst@redhat.com>
    Signed-off-by: Michael S. Tsirkin <mst@redhat.com>
    Acked-by: Christian Schoenebeck <qemu_oss@crudebyte.com>

--- hw/9pfs/virtio-9p-device.c
+++ hw/9pfs/virtio-9p-device.c
@@ -218,7 +218,7 @@ static void virtio_9p_device_unrealize(DeviceState *dev, Error **errp)
     V9fsVirtioState *v = VIRTIO_9P(dev);
     V9fsState *s = &v->state;
 
-    virtio_del_queue(vdev, 0);
+    virtio_delete_queue(v->vq);
     virtio_cleanup(vdev);
     v9fs_device_unrealize_common(s, errp);
 }
