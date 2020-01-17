9580d60e66d1543a0d79265c0085ee09b8782987
Pan Nengyuan <pannengyuan@huawei.com>
2020-01-17 14:09:26 +0800

    virtio-9p-device: fix memleak in virtio_9p_device_unrealize
    v->vq forgot to cleanup in virtio_9p_device_unrealize, the memory leak
    stack is as follow:
    
    Direct leak of 14336 byte(s) in 2 object(s) allocated from:
      #0 0x7f819ae43970 (/lib64/libasan.so.5+0xef970)  ??:?
      #1 0x7f819872f49d (/lib64/libglib-2.0.so.0+0x5249d)  ??:?
      #2 0x55a3a58da624 (./x86_64-softmmu/qemu-system-x86_64+0x2c14624)  /mnt/sdb/qemu/hw/virtio/virtio.c:2327
      #3 0x55a3a571bac7 (./x86_64-softmmu/qemu-system-x86_64+0x2a55ac7)  /mnt/sdb/qemu/hw/9pfs/virtio-9p-device.c:209
      #4 0x55a3a58e7bc6 (./x86_64-softmmu/qemu-system-x86_64+0x2c21bc6)  /mnt/sdb/qemu/hw/virtio/virtio.c:3504
      #5 0x55a3a5ebfb37 (./x86_64-softmmu/qemu-system-x86_64+0x31f9b37)  /mnt/sdb/qemu/hw/core/qdev.c:876
    
    Reported-by: Euler Robot <euler.robot@huawei.com>
    Signed-off-by: Pan Nengyuan <pannengyuan@huawei.com>
    Message-Id: <20200117060927.51996-2-pannengyuan@huawei.com>
    Reviewed-by: Michael S. Tsirkin <mst@redhat.com>
    Signed-off-by: Michael S. Tsirkin <mst@redhat.com>
    Reviewed-by: Christian Schoenebeck <qemu_oss@crudebyte.com>
    Acked-by: Greg Kurz <groug@kaod.org>

--- hw/9pfs/virtio-9p-device.c
+++ hw/9pfs/virtio-9p-device.c
@@ -218,6 +218,7 @@ static void virtio_9p_device_unrealize(DeviceState *dev, Error **errp)
     V9fsVirtioState *v = VIRTIO_9P(dev);
     V9fsState *s = &v->state;
 
+    virtio_del_queue(vdev, 0);
     virtio_cleanup(vdev);
     v9fs_device_unrealize_common(s, errp);
 }
