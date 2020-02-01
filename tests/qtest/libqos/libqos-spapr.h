63d57c8f91d0d0e62fc4d91db6340a662b36a3c0
Greg Kurz <groug@kaod.org>
2020-02-01 23:46:16 +0100

    tests: Silence various warnings with pseries
    Some default features of the pseries machine are only available with
    KVM. Warnings are printed when the pseries machine is used with another
    accelerator:
    
    qemu-system-ppc64: warning: TCG doesn't support requested feature,
    cap-ccf-assist=on
    qemu-system-ppc64: warning: Firmware Assisted Non-Maskable
    Interrupts(FWNMI) not supported in TCG
    qemu-system-ppc64: warning: TCG doesn't support requested feature,
    cap-ccf-assist=on
    qemu-system-ppc64: warning: Firmware Assisted Non-Maskable
    Interrupts(FWNMI) not supported in TCG
    qemu-system-ppc64: warning: TCG doesn't support requested feature,
    cap-ccf-assist=on
    qemu-system-ppc64: warning: Firmware Assisted Non-Maskable
    Interrupts(FWNMI) not supported in TCG
    
    This is annoying for CI since it usually runs without KVM. We already
    disable features that emit similar warnings thanks to properties of
    the pseries machine, but this is open-coded in various
    places. Consolidate the set of properties in a single place. Extend it
    to silence the above warnings. And use it in the various tests that
    start pseries machines.
    
    Reported-by: Peter Maydell <peter.maydell@linaro.org>
    Signed-off-by: Greg Kurz <groug@kaod.org>
    Message-Id: <158059697130.1820292.7823434132030453110.stgit@bahia.lan>
    Reviewed-by: Thomas Huth <thuth@redhat.com>
    [dwg: Correct minor grammatical error]
    Signed-off-by: David Gibson <david@gibson.dropbear.id.au>

--- tests/qtest/libqos/libqos-spapr.h
+++ tests/qtest/libqos/libqos-spapr.h
@@ -7,4 +7,12 @@ QOSState *qtest_spapr_vboot(const char *cmdline_fmt, va_list ap);
 QOSState *qtest_spapr_boot(const char *cmdline_fmt, ...);
 void qtest_spapr_shutdown(QOSState *qs);
 
+/* List of capabilities needed to silence warnings with TCG */
+#define PSERIES_DEFAULT_CAPABILITIES             \
+    "cap-cfpc=broken,"                           \
+    "cap-sbbc=broken,"                           \
+    "cap-ibs=broken,"                            \
+    "cap-ccf-assist=off,"                        \
+    "cap-fwnmi-mce=off"
+
 #endif
