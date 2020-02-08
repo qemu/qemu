af46a3b233787323417bbca7b1aa3290935a5bdf
Christian Schoenebeck <qemu_oss@crudebyte.com>
2020-02-08 09:24:19 +0100

    hw/9pfs/9p-synth: added directory for readdir test
    This will provide the following virtual files by the 9pfs
    synth driver:
    
      - /ReadDirDir/ReadDirFile99
      - /ReadDirDir/ReadDirFile98
      ...
      - /ReadDirDir/ReadDirFile1
      - /ReadDirDir/ReadDirFile0
    
    This virtual directory and its virtual 100 files will be
    used by the upcoming 9pfs readdir tests.
    
    Signed-off-by: Christian Schoenebeck <qemu_oss@crudebyte.com>
    Reviewed-by: Greg Kurz <groug@kaod.org>
    Message-Id: <5408c28c8de25dd575b745cef63bf785305ccef2.1579567020.git.qemu_oss@crudebyte.com>
    Signed-off-by: Greg Kurz <groug@kaod.org>

--- hw/9pfs/9p-synth.h
+++ hw/9pfs/9p-synth.h
@@ -55,6 +55,11 @@ int qemu_v9fs_synth_add_file(V9fsSynthNode *parent, int mode,
 #define QTEST_V9FS_SYNTH_LOPEN_FILE "LOPEN"
 #define QTEST_V9FS_SYNTH_WRITE_FILE "WRITE"
 
+/* for READDIR test */
+#define QTEST_V9FS_SYNTH_READDIR_DIR "ReadDirDir"
+#define QTEST_V9FS_SYNTH_READDIR_FILE "ReadDirFile%d"
+#define QTEST_V9FS_SYNTH_READDIR_NFILES 100
+
 /* Any write to the "FLUSH" file is handled one byte at a time by the
  * backend. If the byte is zero, the backend returns success (ie, 1),
  * otherwise it forces the server to try again forever. Thus allowing
