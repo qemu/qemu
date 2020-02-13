dbf65e873b2b4876fff5d94a906c2dfa2317d462
Aleksandar Markovic <amarkovic@wavecomp.com>
2020-02-13 13:29:11 +0100

    linux-user: sh4: Update syscall numbers to kernel 5.5 level
    Update sh4 syscall numbers based on Linux kernel v5.5.
    
    CC: Aurelien Jarno <aurelien@aurel32.net>
    Signed-off-by: Aleksandar Markovic <amarkovic@wavecomp.com>
    Reviewed-by: Laurent Vivier <laurent@vivier.eu>
    Message-Id: <1581596954-2305-7-git-send-email-aleksandar.markovic@rt-rk.com>
    Signed-off-by: Laurent Vivier <laurent@vivier.eu>

--- linux-user/sh4/syscall_nr.h
+++ linux-user/sh4/syscall_nr.h
@@ -389,5 +389,53 @@
 #define TARGET_NR_copy_file_range       380
 #define TARGET_NR_preadv2               381
 #define TARGET_NR_pwritev2              382
+#define TARGET_NR_statx                 383
+#define TARGET_NR_pkey_mprotect         384
+#define TARGET_NR_pkey_alloc            385
+#define TARGET_NR_pkey_free             386
+#define TARGET_NR_rseq                  387
+/* room for arch specific syscalls */
+#define TARGET_NR_semget                         393
+#define TARGET_NR_semctl                         394
+#define TARGET_NR_shmget                         395
+#define TARGET_NR_shmctl                         396
+#define TARGET_NR_shmat                          397
+#define TARGET_NR_shmdt                          398
+#define TARGET_NR_msgget                         399
+#define TARGET_NR_msgsnd                         400
+#define TARGET_NR_msgrcv                         401
+#define TARGET_NR_msgctl                         402
+#define TARGET_NR_clock_gettime64                403
+#define TARGET_NR_clock_settime64                404
+#define TARGET_NR_clock_adjtime64                405
+#define TARGET_NR_clock_getres_time64            406
+#define TARGET_NR_clock_nanosleep_time64         407
+#define TARGET_NR_timer_gettime64                408
+#define TARGET_NR_timer_settime64                409
+#define TARGET_NR_timerfd_gettime64              410
+#define TARGET_NR_timerfd_settime64              411
+#define TARGET_NR_utimensat_time64               412
+#define TARGET_NR_pselect6_time64                413
+#define TARGET_NR_ppoll_time64                   414
+#define TARGET_NR_io_pgetevents_time64           416
+#define TARGET_NR_recvmmsg_time64                417
+#define TARGET_NR_mq_timedsend_time64            418
+#define TARGET_NR_mq_timedreceive_time64         419
+#define TARGET_NR_semtimedop_time64              420
+#define TARGET_NR_rt_sigtimedwait_time64         421
+#define TARGET_NR_futex_time64                   422
+#define TARGET_NR_sched_rr_get_interval_time64   423
+#define TARGET_NR_pidfd_send_signal              424
+#define TARGET_NR_io_uring_setup                 425
+#define TARGET_NR_io_uring_enter                 426
+#define TARGET_NR_io_uring_register              427
+#define TARGET_NR_open_tree                      428
+#define TARGET_NR_move_mount                     429
+#define TARGET_NR_fsopen                         430
+#define TARGET_NR_fsconfig                       431
+#define TARGET_NR_fsmount                        432
+#define TARGET_NR_fspick                         433
+#define TARGET_NR_pidfd_open                     434
+/* 435 reserved for clone3 */
 
 #endif
