6ede81d576ddc8e1f745494a51345328bd3f4d76
Kevin Wolf <kwolf@redhat.com>
2020-02-24 15:30:02 +0100

    stubs: Update monitor stubs for qemu-storage-daemon
    Before we can add the monitor to qemu-storage-daemon, we need to add a
    stubs for monitor_fdsets_cleanup().
    
    We also need to make sure that stubs that are actually implemented in
    the monitor core aren't linked to qemu-storage-daemon so that we don't
    get linker errors because of duplicate symbols. This is achieved by
    moving the stubs in question to a new file stubs/monitor-core.c.
    
    Signed-off-by: Kevin Wolf <kwolf@redhat.com>
    Message-Id: <20200224143008.13362-15-kwolf@redhat.com>
    Acked-by: Stefan Hajnoczi <stefanha@redhat.com>
    Signed-off-by: Kevin Wolf <kwolf@redhat.com>

--- stubs/monitor.c
+++ stubs/monitor.c
@@ -1,14 +1,7 @@
 #include "qemu/osdep.h"
 #include "qapi/error.h"
-#include "qapi/qapi-emit-events.h"
 #include "monitor/monitor.h"
-
-__thread Monitor *cur_mon;
-
-int monitor_vprintf(Monitor *mon, const char *fmt, va_list ap)
-{
-    abort();
-}
+#include "../monitor/monitor-internal.h"
 
 int monitor_get_fd(Monitor *mon, const char *name, Error **errp)
 {
@@ -16,14 +9,10 @@ int monitor_get_fd(Monitor *mon, const char *name, Error **errp)
     return -1;
 }
 
-void monitor_init_qmp(Chardev *chr, bool pretty)
-{
-}
-
 void monitor_init_hmp(Chardev *chr, bool use_readline)
 {
 }
 
-void qapi_event_emit(QAPIEvent event, QDict *qdict)
+void monitor_fdsets_cleanup(void)
 {
 }
