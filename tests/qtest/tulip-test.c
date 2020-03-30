1153cf9f5b67fad41ca6f8571e9a26e2c7c70759
Li Qiang <liq3ea@163.com>
2020-03-30 07:52:01 -0700

    qtest: add tulip test case
    The tulip networking card emulation has an OOB issue in
    'tulip_copy_tx_buffers' when the guest provide malformed descriptor.
    This test will trigger a ASAN heap overflow crash. To trigger this
    issue we can construct the data as following:
    
    1. construct a 'tulip_descriptor'. Its control is set to
    '0x7ff | 0x7ff << 11', this will make the 'tulip_copy_tx_buffers's
    'len1' and 'len2' to 0x7ff(2047). So 'len1+len2' will overflow
    'TULIPState's 'tx_frame' field. This descriptor's 'buf_addr1' and
    'buf_addr2' should set to a guest address.
    
    2. write this descriptor to tulip device's CSR4 register. This will
    set the 'TULIPState's 'current_tx_desc' field.
    
    3. write 'CSR6_ST' to tulip device's CSR6 register. This will trigger
    'tulip_xmit_list_update' and finally calls 'tulip_copy_tx_buffers'.
    
    Following shows the backtrack of crash:
    
    ==31781==ERROR: AddressSanitizer: heap-buffer-overflow on address 0x628000007cd0 at pc 0x7fe03c5a077a bp 0x7fff05b46770 sp 0x7fff05b45f18
    WRITE of size 2047 at 0x628000007cd0 thread T0
        #0 0x7fe03c5a0779  (/usr/lib/x86_64-linux-gnu/libasan.so.4+0x79779)
        #1 0x5575fb6daa6a in flatview_read_continue /home/test/qemu/exec.c:3194
        #2 0x5575fb6daccb in flatview_read /home/test/qemu/exec.c:3227
        #3 0x5575fb6dae66 in address_space_read_full /home/test/qemu/exec.c:3240
        #4 0x5575fb6db0cb in address_space_rw /home/test/qemu/exec.c:3268
        #5 0x5575fbdfd460 in dma_memory_rw_relaxed /home/test/qemu/include/sysemu/dma.h:87
        #6 0x5575fbdfd4b5 in dma_memory_rw /home/test/qemu/include/sysemu/dma.h:110
        #7 0x5575fbdfd866 in pci_dma_rw /home/test/qemu/include/hw/pci/pci.h:787
        #8 0x5575fbdfd8a3 in pci_dma_read /home/test/qemu/include/hw/pci/pci.h:794
        #9 0x5575fbe02761 in tulip_copy_tx_buffers hw/net/tulip.c:585
        #10 0x5575fbe0366b in tulip_xmit_list_update hw/net/tulip.c:678
        #11 0x5575fbe04073 in tulip_write hw/net/tulip.c:783
    
    Signed-off-by: Li Qiang <liq3ea@163.com>
    Signed-off-by: Jason Wang <jasowang@redhat.com>

---
+++ tests/qtest/tulip-test.c
@@ -0,0 +1,91 @@
+/*
+ * QTest testcase for DEC/Intel Tulip 21143
+ *
+ * Copyright (c) 2020 Li Qiang <liq3ea@gmail.com>
+ *
+ * This work is licensed under the terms of the GNU GPL, version 2 or later.
+ * See the COPYING file in the top-level directory.
+ */
+
+#include "qemu/osdep.h"
+#include "libqtest.h"
+#include "qemu/module.h"
+#include "libqos/qgraph.h"
+#include "libqos/pci.h"
+#include "qemu/bitops.h"
+#include "hw/net/tulip.h"
+
+typedef struct QTulip_pci QTulip_pci;
+
+struct QTulip_pci {
+    QOSGraphObject obj;
+    QPCIDevice dev;
+};
+
+static void *tulip_pci_get_driver(void *obj, const char *interface)
+{
+    QTulip_pci *tulip_pci = obj;
+
+    if (!g_strcmp0(interface, "pci-device")) {
+        return &tulip_pci->dev;
+    }
+
+    fprintf(stderr, "%s not present in tulip_pci\n", interface);
+    g_assert_not_reached();
+}
+
+static void *tulip_pci_create(void *pci_bus, QGuestAllocator *alloc, void *addr)
+{
+    QTulip_pci *tulip_pci = g_new0(QTulip_pci, 1);
+    QPCIBus *bus = pci_bus;
+
+    qpci_device_init(&tulip_pci->dev, bus, addr);
+    tulip_pci->obj.get_driver = tulip_pci_get_driver;
+
+    return &tulip_pci->obj;
+}
+
+static void tulip_large_tx(void *obj, void *data, QGuestAllocator *alloc)
+{
+    QTulip_pci *tulip_pci = obj;
+    QPCIDevice *dev = &tulip_pci->dev;
+    QPCIBar bar;
+    struct tulip_descriptor context;
+    char guest_data[4096];
+    uint64_t context_pa;
+    uint64_t guest_pa;
+
+    qpci_device_enable(dev);
+    bar = qpci_iomap(dev, 0, NULL);
+    context_pa = guest_alloc(alloc, sizeof(context));
+    guest_pa = guest_alloc(alloc, 4096);
+    memset(guest_data, 'A', sizeof(guest_data));
+    context.status = TDES0_OWN;
+    context.control = TDES1_BUF2_SIZE_MASK << TDES1_BUF2_SIZE_SHIFT |
+                      TDES1_BUF1_SIZE_MASK << TDES1_BUF1_SIZE_SHIFT;
+    context.buf_addr2 = guest_pa;
+    context.buf_addr1 = guest_pa;
+
+    qtest_memwrite(dev->bus->qts, context_pa, &context, sizeof(context));
+    qtest_memwrite(dev->bus->qts, guest_pa, guest_data, sizeof(guest_data));
+    qpci_io_writel(dev, bar, 0x20, context_pa);
+    qpci_io_writel(dev, bar, 0x30, CSR6_ST);
+    guest_free(alloc, context_pa);
+    guest_free(alloc, guest_pa);
+}
+
+static void tulip_register_nodes(void)
+{
+    QOSGraphEdgeOptions opts = {
+        .extra_device_opts = "addr=04.0",
+    };
+    add_qpci_address(&opts, &(QPCIAddress) { .devfn = QPCI_DEVFN(4, 0) });
+
+    qos_node_create_driver("tulip", tulip_pci_create);
+    qos_node_consumes("tulip", "pci-bus", &opts);
+    qos_node_produces("tulip", "pci-device");
+
+    qos_add_test("tulip_large_tx", "tulip", tulip_large_tx, NULL);
+}
+
+libqos_init(tulip_register_nodes);
