122752d267cb43ee29b5f0acac1057198797c45c
Shameer Kolothum <shameerali.kolothum.thodi@huawei.com>
2020-04-21 13:59:32 +0100

    tests: Update ACPI tables list for upcoming arm/virt test changes
    This is in preparation to update test_acpi_virt_tcg_memhp()
    with pc-dimm and nvdimm. Update the bios-tables-test-allowed-diff.h
    with the affected ACPI tables so that "make check" doesn't fail.
    
    Also add empty files for new tables required for new test.
    
    Signed-off-by: Shameer Kolothum <shameerali.kolothum.thodi@huawei.com>
    Message-Id: <20200421125934.14952-6-shameerali.kolothum.thodi@huawei.com>
    Acked-by: Peter Maydell <peter.maydell@linaro.org>
    Tested-by: Eric Auger <eric.auger@redhat.com>
    Reviewed-by: Michael S. Tsirkin <mst@redhat.com>
    Signed-off-by: Michael S. Tsirkin <mst@redhat.com>

--- tests/qtest/bios-tables-test-allowed-diff.h
+++ tests/qtest/bios-tables-test-allowed-diff.h
@@ -1,3 +1,6 @@
 /* List of comma-separated changed AML files to ignore */
 "tests/data/acpi/pc/SSDT.dimmpxm",
 "tests/data/acpi/q35/SSDT.dimmpxm",
+"tests/data/acpi/virt/DSDT.memhp",
+"tests/data/acpi/virt/SSDT.memhp",
+"tests/data/acpi/virt/NFIT.memhp",
