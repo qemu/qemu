a13f20422dc657719c3fa335788300c7ccf464ee
Markus Armbruster <armbru@redhat.com>
2020-05-05 17:29:19 +0200

    e1000: Don't run e1000_instance_init() twice
    QOM object initialization runs .instance_init() for the type and all
    its supertypes; see object_init_with_type().
    
    Both TYPE_E1000_BASE and its concrete subtypes set .instance_init() to
    e1000_instance_init().  For the concrete subtypes, it duly gets run
    twice.  The second run fails, but the error gets ignored (a later
    commit will change that).
    
    Remove it from the subtypes.
    
    Cc: Jason Wang <jasowang@redhat.com>
    Signed-off-by: Markus Armbruster <armbru@redhat.com>
    Acked-by: Jason Wang <jasowang@redhat.com>
    Reviewed-by: Paolo Bonzini <pbonzini@redhat.com>
    Message-Id: <20200505152926.18877-12-armbru@redhat.com>

--- hw/net/e1000.c
+++ hw/net/e1000.c
@@ -1824,7 +1824,6 @@ static void e1000_register_types(void)
         type_info.parent = TYPE_E1000_BASE;
         type_info.class_data = (void *)info;
         type_info.class_init = e1000_class_init;
-        type_info.instance_init = e1000_instance_init;
 
         type_register(&type_info);
     }
