cdf9ebf18f32f4b29f99af58e154a9afe93bbc92
Vladimir Sementsov-Ogievskiy <vsementsov@virtuozzo.com>
2020-05-28 12:44:03 +0300

    block/vhdx: drop unallocated_blocks_are_zero
    vhdx doesn't have .bdrv_co_block_status handler, so DATA|ALLOCATED is
    always assumed for it in bdrv_co_block_status().
    unallocated_blocks_are_zero is useless (it doesn't affect the only user
    of the field: bdrv_co_block_status()), drop it.
    
    Signed-off-by: Vladimir Sementsov-Ogievskiy <vsementsov@virtuozzo.com>
    Reviewed-by: Eric Blake <eblake@redhat.com>
    Message-Id: <20200528094405.145708-9-vsementsov@virtuozzo.com>
    Signed-off-by: Max Reitz <mreitz@redhat.com>

--- block/vhdx.c
+++ block/vhdx.c
@@ -1164,9 +1164,6 @@ static int vhdx_get_info(BlockDriverState *bs, BlockDriverInfo *bdi)
 
     bdi->cluster_size = s->block_size;
 
-    bdi->unallocated_blocks_are_zero =
-        (s->params.data_bits & VHDX_PARAMS_HAS_PARENT) == 0;
-
     return 0;
 }
 
