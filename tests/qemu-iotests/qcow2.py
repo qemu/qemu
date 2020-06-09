3419ec713f04c323b030e0763459435335b25476
Eric Blake <eblake@redhat.com>
2020-06-09 15:59:44 -0500

    iotests: Add copyright line in qcow2.py
    The file qcow2.py was originally contributed in 2012 by Kevin Wolf,
    but was not given traditional boilerplate headers at the time.  The
    missing license was just rectified (commit 16306a7b39) using the
    project-default GPLv2+, but as Vladimir is not at Red Hat, he did not
    add a Copyright line.  All earlier contributions have come from CC'd
    authors, where all but Stefan used a Red Hat address at the time of
    the contribution, and that copyright carries over to the split to
    qcow2_format.py (d5262c7124).
    
    CC: Kevin Wolf <kwolf@redhat.com>
    CC: Stefan Hajnoczi <stefanha@redhat.com>
    CC: Eduardo Habkost <ehabkost@redhat.com>
    CC: Max Reitz <mreitz@redhat.com>
    CC: Philippe Mathieu-Daudé <philmd@redhat.com>
    CC: Paolo Bonzini <pbonzini@redhat.com>
    Signed-off-by: Eric Blake <eblake@redhat.com>
    Message-Id: <20200609205944.3549240-1-eblake@redhat.com>
    Acked-by: Stefan Hajnoczi <stefanha@redhat.com>
    Acked-by: Philippe Mathieu-Daudé <philmd@redhat.com>
    Signed-off-by: Kevin Wolf <kwolf@redhat.com>

--- tests/qemu-iotests/qcow2.py
+++ tests/qemu-iotests/qcow2.py
@@ -2,6 +2,8 @@
 #
 # Manipulations with qcow2 image
 #
+# Copyright (C) 2012 Red Hat, Inc.
+#
 # This program is free software; you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
 # the Free Software Foundation; either version 2 of the License, or
