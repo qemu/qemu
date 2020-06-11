e7b5dfd34f296db56aa5de31d1cf64dbe82674a3
Bin Meng <bin.meng@windriver.com>
2020-06-10 18:08:46 -0700

    riscv: Generalize CPU init routine for the base CPU
    There is no need to have two functions that have exactly the same
    codes for 32-bit and 64-bit base CPUs.
    
    Signed-off-by: Bin Meng <bin.meng@windriver.com>
    Reviewed-by: Alistair Francis <alistair.francis@wdc.com>
    Message-id: 1591837729-27486-1-git-send-email-bmeng.cn@gmail.com
    Message-Id: <1591837729-27486-1-git-send-email-bmeng.cn@gmail.com>
    Signed-off-by: Alistair Francis <alistair.francis@wdc.com>

--- target/riscv/cpu.c
+++ target/riscv/cpu.c
@@ -126,15 +126,15 @@ static void riscv_any_cpu_init(Object *obj)
     set_resetvec(env, DEFAULT_RSTVEC);
 }
 
-#if defined(TARGET_RISCV32)
-
-static void riscv_base32_cpu_init(Object *obj)
+static void riscv_base_cpu_init(Object *obj)
 {
     CPURISCVState *env = &RISCV_CPU(obj)->env;
     /* We set this in the realise function */
     set_misa(env, 0);
     set_resetvec(env, DEFAULT_RSTVEC);
 }
+
+#if defined(TARGET_RISCV32)
 
 static void rv32gcsu_priv1_10_0_cpu_init(Object *obj)
 {
@@ -172,14 +172,6 @@ static void rv32imafcu_nommu_cpu_init(Object *obj)
 }
 
 #elif defined(TARGET_RISCV64)
-
-static void riscv_base64_cpu_init(Object *obj)
-{
-    CPURISCVState *env = &RISCV_CPU(obj)->env;
-    /* We set this in the realise function */
-    set_misa(env, 0);
-    set_resetvec(env, DEFAULT_RSTVEC);
-}
 
 static void rv64gcsu_priv1_10_0_cpu_init(Object *obj)
 {
@@ -603,13 +595,13 @@ static const TypeInfo riscv_cpu_type_infos[] = {
     },
     DEFINE_CPU(TYPE_RISCV_CPU_ANY,              riscv_any_cpu_init),
 #if defined(TARGET_RISCV32)
-    DEFINE_CPU(TYPE_RISCV_CPU_BASE32,           riscv_base32_cpu_init),
+    DEFINE_CPU(TYPE_RISCV_CPU_BASE32,           riscv_base_cpu_init),
     DEFINE_CPU(TYPE_RISCV_CPU_IBEX,             rv32imcu_nommu_cpu_init),
     DEFINE_CPU(TYPE_RISCV_CPU_SIFIVE_E31,       rv32imacu_nommu_cpu_init),
     DEFINE_CPU(TYPE_RISCV_CPU_SIFIVE_E34,       rv32imafcu_nommu_cpu_init),
     DEFINE_CPU(TYPE_RISCV_CPU_SIFIVE_U34,       rv32gcsu_priv1_10_0_cpu_init),
 #elif defined(TARGET_RISCV64)
-    DEFINE_CPU(TYPE_RISCV_CPU_BASE64,           riscv_base64_cpu_init),
+    DEFINE_CPU(TYPE_RISCV_CPU_BASE64,           riscv_base_cpu_init),
     DEFINE_CPU(TYPE_RISCV_CPU_SIFIVE_E51,       rv64imacu_nommu_cpu_init),
     DEFINE_CPU(TYPE_RISCV_CPU_SIFIVE_U54,       rv64gcsu_priv1_10_0_cpu_init),
 #endif
