f476f17740ad42288d42dd8fedcdae8ca7007a16
LIU Zhiwei <zhiwei_liu@c-sky.com>
2020-07-01 23:24:53 +0800

    target/riscv: add an internals.h header
    The internals.h keeps things that are not relevant to the actual architecture,
    only to the implementation, separate.
    
    Signed-off-by: LIU Zhiwei <zhiwei_liu@c-sky.com>
    Reviewed-by: Alistair Francis <alistair.francis@wdc.com>
    Reviewed-by: Richard Henderson <richard.henderson@linaro.org>
    Message-Id: <20200701152549.1218-6-zhiwei_liu@c-sky.com>
    Signed-off-by: Alistair Francis <alistair.francis@wdc.com>

---
+++ target/riscv/internals.h
@@ -0,0 +1,24 @@
+/*
+ * QEMU RISC-V CPU -- internal functions and types
+ *
+ * Copyright (c) 2020 T-Head Semiconductor Co., Ltd. All rights reserved.
+ *
+ * This program is free software; you can redistribute it and/or modify it
+ * under the terms and conditions of the GNU General Public License,
+ * version 2 or later, as published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for
+ * more details.
+ *
+ * You should have received a copy of the GNU General Public License along with
+ * this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#ifndef RISCV_CPU_INTERNALS_H
+#define RISCV_CPU_INTERNALS_H
+
+#include "hw/registerfields.h"
+
+#endif
