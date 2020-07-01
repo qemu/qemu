1e0a84ea49b68b7cf60e229d91fd16333e0b7a90
Cindy Lu <lulu@redhat.com>
2020-07-01 22:55:38 +0800

    vhost-vdpa: introduce vhost-vdpa net client
    This patch set introduces a new net client type: vhost-vdpa.
    vhost-vdpa net client will set up a vDPA device which is specified
    by a "vhostdev" parameter.
    
    Signed-off-by: Lingshan Zhu <lingshan.zhu@intel.com>
    Signed-off-by: Tiwei Bie <tiwei.bie@intel.com>
    Signed-off-by: Cindy Lu <lulu@redhat.com>
    Signed-off-by: Jason Wang <jasowang@redhat.com>
    Message-Id: <20200701145538.22333-15-lulu@redhat.com>
    Reviewed-by: Michael S. Tsirkin <mst@redhat.com>
    Signed-off-by: Michael S. Tsirkin <mst@redhat.com>
    Acked-by: Jason Wang <jasowang@redhat.com>

--- net/net.c
+++ net/net.c
@@ -966,6 +966,9 @@ static int (* const net_client_init_fun[NET_CLIENT_DRIVER__MAX])(
 #ifdef CONFIG_VHOST_NET_USER
         [NET_CLIENT_DRIVER_VHOST_USER] = net_init_vhost_user,
 #endif
+#ifdef CONFIG_VHOST_NET_VDPA
+        [NET_CLIENT_DRIVER_VHOST_VDPA] = net_init_vhost_vdpa,
+#endif
 #ifdef CONFIG_L2TPV3
         [NET_CLIENT_DRIVER_L2TPV3]    = net_init_l2tpv3,
 #endif
