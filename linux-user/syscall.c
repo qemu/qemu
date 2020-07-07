71e2443e4ca1540db63c0bbf58ae78944c97e993
Josh Kunz <jkz@google.com>
2020-07-06 17:10:36 -0700

    linux-user: Use EPROTONOSUPPORT for unimplemented netlink protocols
    Linux uses the EPROTONOSUPPORT error code[1] if the users requests a
    netlink socket with an unsupported netlink protocol. This change
    switches linux-user to use the same code as Linux, instead of
    EPFNOSUPPORT (which AFAIK is just an anachronistic version of
    EAFNOSUPPORT).
    
    Tested by compiling all linux-user targets on x86.
    
    [1]:
    https://github.com/torvalds/linux/blob/bfe91da29bfad9941d5d703d45e29f0812a20724/net/netlink/af_netlink.c#L683
    
    Signed-off-by: Josh Kunz <jkz@google.com>
    Reviewed-by: Laurent Vivier <laurent@vivier.eu>
    Message-Id: <20200707001036.1671982-1-jkz@google.com>
    Signed-off-by: Laurent Vivier <laurent@vivier.eu>

--- linux-user/syscall.c
+++ linux-user/syscall.c
@@ -2990,7 +2990,7 @@ static abi_long do_socket(int domain, int type, int protocol)
 #endif
          protocol == NETLINK_KOBJECT_UEVENT ||
          protocol == NETLINK_AUDIT)) {
-        return -TARGET_EPFNOSUPPORT;
+        return -TARGET_EPROTONOSUPPORT;
     }
 
     if (domain == AF_PACKET ||
