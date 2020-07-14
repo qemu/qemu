5bd929d2fff068f829688f27b54f2f159ff06eb6
Markus Armbruster <armbru@redhat.com>
2020-07-14 18:02:01 +0200

    qom: Document object_get_canonical_path() returns malloced string
    Signed-off-by: Markus Armbruster <armbru@redhat.com>
    Message-Id: <20200714160202.3121879-5-armbru@redhat.com>
    Reviewed-by: Philippe Mathieu-Daudé <philmd@redhat.com>

--- include/qom/object.h
+++ include/qom/object.h
@@ -1467,8 +1467,9 @@ const char *object_get_canonical_path_component(const Object *obj);
 /**
  * object_get_canonical_path:
  *
- * Returns: The canonical path for a object.  This is the path within the
- * composition tree starting from the root.
+ * Returns: The canonical path for a object, newly allocated.  This is
+ * the path within the composition tree starting from the root.  Use
+ * g_free() to free it.
  */
 char *object_get_canonical_path(const Object *obj);
 
