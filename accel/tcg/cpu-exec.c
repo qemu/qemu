ba3c35d9c4026361fd380b269dc6def9510b7166
Richard Henderson <richard.henderson@linaro.org>
2020-07-17 09:26:59 -0700

    tcg/cpu-exec: precise single-stepping after an interrupt
    When single-stepping with a debugger attached to QEMU, and when an
    interrupt is raised, the debugger misses the first instruction after
    the interrupt.
    
    Tested-by: Luc Michel <luc.michel@greensocs.com>
    Reviewed-by: Luc Michel <luc.michel@greensocs.com>
    Buglink: https://bugs.launchpad.net/qemu/+bug/757702
    Message-Id: <20200717163029.2737546-1-richard.henderson@linaro.org>
    Signed-off-by: Richard Henderson <richard.henderson@linaro.org>

--- accel/tcg/cpu-exec.c
+++ accel/tcg/cpu-exec.c
@@ -588,7 +588,13 @@ static inline bool cpu_handle_interrupt(CPUState *cpu,
         else {
             if (cc->cpu_exec_interrupt(cpu, interrupt_request)) {
                 replay_interrupt();
-                cpu->exception_index = -1;
+                /*
+                 * After processing the interrupt, ensure an EXCP_DEBUG is
+                 * raised when single-stepping so that GDB doesn't miss the
+                 * next instruction.
+                 */
+                cpu->exception_index =
+                    (cpu->singlestep_enabled ? EXCP_DEBUG : -1);
                 *last_tb = NULL;
             }
             /* The target hook may have updated the 'cpu->interrupt_request';
