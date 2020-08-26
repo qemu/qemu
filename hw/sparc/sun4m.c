24f675cd3b1406f3af9f9b811bb6946d0e2e6ecf
Thiago Jung Bauermann <bauerman@linux.ibm.com>
2020-08-26 02:55:34 -0300

    sparc/sun4m: Use start-powered-off CPUState property
    Instead of setting CPUState::halted to 1 in secondary_cpu_reset(), use the
    start-powered-off property which makes cpu_common_reset() initialize it
    to 1 in common code.
    
    Now secondary_cpu_reset() becomes equivalent to main_cpu_reset() so rename
    the function to sun4m_cpu_reset().
    
    Also remove setting of cs->halted from cpu_devinit(), which seems out of
    place when compared to similar code in other architectures (e.g.,
    ppce500_init() in hw/ppc/e500.c).
    
    Finally, change creation of CPU object from cpu_create() to object_new()
    and qdev_realize_and_unref() because cpu_create() realizes the CPU and it's
    not possible to set a property after the object is realized.
    
    Suggested-by: Philippe Mathieu-Daudé <philmd@redhat.com>
    Signed-off-by: Thiago Jung Bauermann <bauerman@linux.ibm.com>
    Message-Id: <20200826055535.951207-8-bauerman@linux.ibm.com>
    Reviewed-by: Philippe Mathieu-Daudé <philmd@redhat.com>
    Signed-off-by: David Gibson <david@gibson.dropbear.id.au>

--- hw/sparc/sun4m.c
+++ hw/sparc/sun4m.c
@@ -218,21 +218,12 @@ static void dummy_cpu_set_irq(void *opaque, int irq, int level)
 {
 }
 
-static void main_cpu_reset(void *opaque)
+static void sun4m_cpu_reset(void *opaque)
 {
     SPARCCPU *cpu = opaque;
     CPUState *cs = CPU(cpu);
 
     cpu_reset(cs);
-}
-
-static void secondary_cpu_reset(void *opaque)
-{
-    SPARCCPU *cpu = opaque;
-    CPUState *cs = CPU(cpu);
-
-    cpu_reset(cs);
-    cs->halted = 1;
 }
 
 static void cpu_halt_signal(void *opaque, int irq, int level)
@@ -818,21 +809,17 @@ static const TypeInfo ram_info = {
 static void cpu_devinit(const char *cpu_type, unsigned int id,
                         uint64_t prom_addr, qemu_irq **cpu_irqs)
 {
-    CPUState *cs;
     SPARCCPU *cpu;
     CPUSPARCState *env;
 
-    cpu = SPARC_CPU(cpu_create(cpu_type));
+    cpu = SPARC_CPU(object_new(cpu_type));
     env = &cpu->env;
 
     cpu_sparc_set_id(env, id);
-    if (id == 0) {
-        qemu_register_reset(main_cpu_reset, cpu);
-    } else {
-        qemu_register_reset(secondary_cpu_reset, cpu);
-        cs = CPU(cpu);
-        cs->halted = 1;
-    }
+    qemu_register_reset(sun4m_cpu_reset, cpu);
+    object_property_set_bool(OBJECT(cpu), "start-powered-off", id != 0,
+                             &error_fatal);
+    qdev_realize_and_unref(DEVICE(cpu), NULL, &error_fatal);
     *cpu_irqs = qemu_allocate_irqs(cpu_set_irq, cpu, MAX_PILS);
     env->prom_addr = prom_addr;
 }
