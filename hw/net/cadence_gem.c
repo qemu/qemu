dfc388797cc413072e58a8f9a831633f29212448
Bin Meng <bin.meng@windriver.com>
2020-09-01 09:39:07 +0800

    hw/arm: xlnx: Set all boards' GEM 'phy-addr' property value to 23
    When cadence_gem model was created for Xilinx boards, the PHY address
    was hard-coded to 23 in the GEM model. Now that we have introduced a
    property we can use that to tell GEM model what our PHY address is.
    Change all boards' GEM 'phy-addr' property value to 23, and set the
    PHY address default value to 0 in the GEM model.
    
    Signed-off-by: Bin Meng <bin.meng@windriver.com>
    Reviewed-by: Philippe Mathieu-Daudé <f4bug@amsat.org>
    Reviewed-by: Alistair Francis <alistair.francis@wdc.com>
    Message-Id: <1598924352-89526-13-git-send-email-bmeng.cn@gmail.com>
    Signed-off-by: Alistair Francis <alistair.francis@wdc.com>

--- hw/net/cadence_gem.c
+++ hw/net/cadence_gem.c
@@ -250,7 +250,7 @@
 #define GEM_PHYMNTNC_REG_SHIFT 18
 
 /* Marvell PHY definitions */
-#define BOARD_PHY_ADDRESS    23 /* PHY address we will emulate a device at */
+#define BOARD_PHY_ADDRESS    0 /* PHY address we will emulate a device at */
 
 #define PHY_REG_CONTROL      0
 #define PHY_REG_STATUS       1
@@ -1446,7 +1446,7 @@ static uint64_t gem_read(void *opaque, hwaddr offset, unsigned size)
             uint32_t phy_addr, reg_num;
 
             phy_addr = (retval & GEM_PHYMNTNC_ADDR) >> GEM_PHYMNTNC_ADDR_SHFT;
-            if (phy_addr == s->phy_addr || phy_addr == 0) {
+            if (phy_addr == s->phy_addr) {
                 reg_num = (retval & GEM_PHYMNTNC_REG) >> GEM_PHYMNTNC_REG_SHIFT;
                 retval &= 0xFFFF0000;
                 retval |= gem_phy_read(s, reg_num);
@@ -1569,7 +1569,7 @@ static void gem_write(void *opaque, hwaddr offset, uint64_t val,
             uint32_t phy_addr, reg_num;
 
             phy_addr = (val & GEM_PHYMNTNC_ADDR) >> GEM_PHYMNTNC_ADDR_SHFT;
-            if (phy_addr == s->phy_addr || phy_addr == 0) {
+            if (phy_addr == s->phy_addr) {
                 reg_num = (val & GEM_PHYMNTNC_REG) >> GEM_PHYMNTNC_REG_SHIFT;
                 gem_phy_write(s, reg_num, val);
             }
