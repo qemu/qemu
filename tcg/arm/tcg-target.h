dbbeff77645242241fe2296b88a7b1d3b3614ffe
Richard Henderson <richard.henderson@linaro.org>
2020-09-05 12:44:06 -0700

    tcg/arm: Implement TCG_TARGET_HAS_minmax_vec
    This is minimum and maximum, signed and unsigned.
    
    Reviewed-by: Peter Maydell <peter.maydell@linaro.org>
    Signed-off-by: Richard Henderson <richard.henderson@linaro.org>

--- tcg/arm/tcg-target.h
+++ tcg/arm/tcg-target.h
@@ -168,7 +168,7 @@ extern bool use_neon_instructions;
 #define TCG_TARGET_HAS_shv_vec          0
 #define TCG_TARGET_HAS_mul_vec          1
 #define TCG_TARGET_HAS_sat_vec          1
-#define TCG_TARGET_HAS_minmax_vec       0
+#define TCG_TARGET_HAS_minmax_vec       1
 #define TCG_TARGET_HAS_bitsel_vec       0
 #define TCG_TARGET_HAS_cmpsel_vec       0
 
