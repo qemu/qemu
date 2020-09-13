a83e24ba1a52e2f0a00239c8b79edafaa61515aa
Philippe Mathieu-Daudé <philmd@redhat.com>
2020-09-13 21:53:43 +0200

    qapi: Restrict balloon-related commands to machine code
    Only qemu-system-FOO and qemu-storage-daemon provide QMP
    monitors, therefore such declarations and definitions are
    irrelevant for user-mode emulation.
    
    Restricting the balloon-related commands to machine.json pulls less
    QAPI-generated code into user-mode.
    
    Reviewed-by: David Hildenbrand <david@redhat.com>
    Acked-by: Markus Armbruster <armbru@redhat.com>
    Acked-by: Paolo Bonzini <pbonzini@redhat.com>
    Signed-off-by: Philippe Mathieu-Daudé <philmd@redhat.com>
    Message-Id: <20200913195348.1064154-4-philmd@redhat.com>
    [Commit message tweaked]
    Signed-off-by: Markus Armbruster <armbru@redhat.com>

--- monitor/hmp-cmds.c
+++ monitor/hmp-cmds.c
@@ -32,6 +32,7 @@
 #include "qapi/qapi-commands-block.h"
 #include "qapi/qapi-commands-char.h"
 #include "qapi/qapi-commands-control.h"
+#include "qapi/qapi-commands-machine.h"
 #include "qapi/qapi-commands-migration.h"
 #include "qapi/qapi-commands-misc.h"
 #include "qapi/qapi-commands-net.h"
