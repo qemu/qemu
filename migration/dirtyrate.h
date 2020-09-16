a2635f0a7508c3291c6bd2d88ea31b6ae7dfa341
Chuan Zheng <zhengchuan@huawei.com>
2020-09-16 14:21:58 +0800

    migration/dirtyrate: Add RamblockDirtyInfo to store sampled page info
    Add RamblockDirtyInfo to store sampled page info of each ramblock.
    
    Signed-off-by: Chuan Zheng <zhengchuan@huawei.com>
    Reviewed-by: Dr. David Alan Gilbert <dgilbert@redhat.com>
    Reviewed-by: David Edmondson <david.edmondson@oracle.com>
    Reviewed-by: Li Qiang <liq3ea@gmail.com>
    Message-Id: <1600237327-33618-4-git-send-email-zhengchuan@huawei.com>
    Signed-off-by: Dr. David Alan Gilbert <dgilbert@redhat.com>

--- migration/dirtyrate.h
+++ migration/dirtyrate.h
@@ -19,9 +19,27 @@
  */
 #define DIRTYRATE_DEFAULT_SAMPLE_PAGES            512
 
+/*
+ * Record ramblock idstr
+ */
+#define RAMBLOCK_INFO_MAX_LEN                     256
+
 struct DirtyRateConfig {
     uint64_t sample_pages_per_gigabytes; /* sample pages per GB */
     int64_t sample_period_seconds; /* time duration between two sampling */
+};
+
+/*
+ * Store dirtypage info for each ramblock.
+ */
+struct RamblockDirtyInfo {
+    char idstr[RAMBLOCK_INFO_MAX_LEN]; /* idstr for each ramblock */
+    uint8_t *ramblock_addr; /* base address of ramblock we measure */
+    uint64_t ramblock_pages; /* ramblock size in TARGET_PAGE_SIZE */
+    uint64_t *sample_page_vfn; /* relative offset address for sampled page */
+    uint64_t sample_pages_count; /* count of sampled pages */
+    uint64_t sample_dirty_count; /* count of dirty pages we measure */
+    uint32_t *hash_result; /* array of hash result for sampled pages */
 };
 
 void *get_dirtyrate_thread(void *arg);
