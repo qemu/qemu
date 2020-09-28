928dda43017681555a4f63196435881dbf7459ca
Gerd Hoffmann <kraxel@redhat.com>
2020-09-28 12:42:51 +0200

    tests/acpi: allow updates for expected data files
    Signed-off-by: Gerd Hoffmann <kraxel@redhat.com>
    Reviewed-by: Michael S. Tsirkin <mst@redhat.com>
    Message-id: 20200928104256.9241-8-kraxel@redhat.com

--- tests/qtest/bios-tables-test-allowed-diff.h
+++ tests/qtest/bios-tables-test-allowed-diff.h
@@ -1,1 +1,6 @@
 /* List of comma-separated changed AML files to ignore */
+"tests/data/acpi/microvm/DSDT.pcie32",
+"tests/data/acpi/microvm/DSDT.pcie64",
+"tests/data/acpi/virt/DSDT",
+"tests/data/acpi/virt/DSDT.numamem",
+"tests/data/acpi/virt/DSDT.memhp",
