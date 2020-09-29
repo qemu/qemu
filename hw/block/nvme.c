dcd1496132704ca16fc857a40370a6b9209341ae
Klaus Jensen <k.jensen@samsung.com>
2020-09-30 01:19:05 +0200

    hw/block/nvme: add trace event for requests with non-zero status code
    If a command results in a non-zero status code, trace it.
    
    Reviewed-by: Philippe Mathieu-Daudé <philmd@redhat.com>
    Reviewed-by: Dmitry Fomichev <dmitry.fomichev@wdc.com>
    Signed-off-by: Klaus Jensen <k.jensen@samsung.com>
    Signed-off-by: Keith Busch <kbusch@kernel.org>

--- hw/block/nvme.c
+++ hw/block/nvme.c
@@ -777,6 +777,12 @@ static void nvme_enqueue_req_completion(NvmeCQueue *cq, NvmeRequest *req)
     assert(cq->cqid == req->sq->cqid);
     trace_pci_nvme_enqueue_req_completion(nvme_cid(req), cq->cqid,
                                           req->status);
+
+    if (req->status) {
+        trace_pci_nvme_err_req_status(nvme_cid(req), nvme_nsid(req->ns),
+                                      req->status, req->cmd.opcode);
+    }
+
     QTAILQ_REMOVE(&req->sq->out_req_list, req, entry);
     QTAILQ_INSERT_TAIL(&cq->req_list, req, entry);
     timer_mod(cq->timer, qemu_clock_get_ns(QEMU_CLOCK_VIRTUAL) + 500);
