8db1efd3f30749d471a60e56dabc131c03e73282
Kevin Wolf <kwolf@redhat.com>
2020-10-07 18:49:03 +0200

    qemu-storage-daemon: Remove QemuOpts from --object parser
    The command line parser for --object parses the input twice: Once into
    QemuOpts just for detecting help options, and then again into a QDict
    using the keyval parser for actually creating the object.
    
    Now that the keyval parser can also detect help options, we can simplify
    this and remove the QemuOpts part.
    
    Signed-off-by: Kevin Wolf <kwolf@redhat.com>
    Reviewed-by: Eric Blake <eblake@redhat.com>
    Reviewed-by: Markus Armbruster <armbru@redhat.com>
    Message-Id: <20201007164903.282198-5-kwolf@redhat.com>
    Signed-off-by: Kevin Wolf <kwolf@redhat.com>

--- storage-daemon/qemu-storage-daemon.c
+++ storage-daemon/qemu-storage-daemon.c
@@ -264,21 +264,14 @@ static void process_options(int argc, char *argv[])
             }
         case OPTION_OBJECT:
             {
-                QemuOpts *opts;
-                const char *type;
                 QDict *args;
+                bool help;
 
-                /* FIXME The keyval parser rejects 'help' arguments, so we must
-                 * unconditionall try QemuOpts first. */
-                opts = qemu_opts_parse(&qemu_object_opts,
-                                       optarg, true, &error_fatal);
-                type = qemu_opt_get(opts, "qom-type");
-                if (type && user_creatable_print_help(type, opts)) {
+                args = keyval_parse(optarg, "qom-type", &help, &error_fatal);
+                if (help) {
+                    user_creatable_print_help_from_qdict(args);
                     exit(EXIT_SUCCESS);
                 }
-                qemu_opts_del(opts);
-
-                args = keyval_parse(optarg, "qom-type", NULL, &error_fatal);
                 user_creatable_add_dict(args, true, &error_fatal);
                 qobject_unref(args);
                 break;
