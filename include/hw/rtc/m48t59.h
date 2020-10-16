3e7e134d827790c3714cae1d5b8aff8612000116
Mark Cave-Ayland <mark.cave-ayland@ilande.co.uk>
2020-10-16 19:27:39 +0100

    m48t59: remove legacy m48t59_init() function
    Now that all of the callers of this function have been switched to use qdev
    properties, this legacy init function can now be removed.
    
    Signed-off-by: Mark Cave-Ayland <mark.cave-ayland@ilande.co.uk>
    Message-Id: <20201016182739.22875-6-mark.cave-ayland@ilande.co.uk>
    Reviewed-by: Hervé Poussineau <hpoussin@reactos.org>
    Reviewed-by: Philippe Mathieu-Daudé <f4bug@amsat.org>
    Signed-off-by: Mark Cave-Ayland <mark.cave-ayland@ilande.co.uk>

--- include/hw/rtc/m48t59.h
+++ include/hw/rtc/m48t59.h
@@ -47,8 +47,4 @@ struct NvramClass {
     void (*toggle_lock)(Nvram *obj, int lock);
 };
 
-Nvram *m48t59_init(qemu_irq IRQ, hwaddr mem_base,
-                   uint32_t io_base, uint16_t size, int base_year,
-                   int type);
-
 #endif /* HW_M48T59_H */
