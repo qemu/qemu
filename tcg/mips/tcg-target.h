0263330bce31a827295157627b32e9502cce693c
Richard Henderson <richard.henderson@linaro.org>
2020-10-17 09:26:17 -0700

    tcg/mips: Split out constraint sets to tcg-target-con-set.h
    Reviewed-by: Peter Maydell <peter.maydell@linaro.org>
    Signed-off-by: Richard Henderson <richard.henderson@linaro.org>

--- tcg/mips/tcg-target.h
+++ tcg/mips/tcg-target.h
@@ -207,5 +207,6 @@ void tb_target_set_jmp_target(uintptr_t, uintptr_t, uintptr_t, uintptr_t);
 #ifdef CONFIG_SOFTMMU
 #define TCG_TARGET_NEED_LDST_LABELS
 #endif
+#define TCG_TARGET_CON_SET_H
 
 #endif
