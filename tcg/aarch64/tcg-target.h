39e7522b4ac86636e09ccb43487b14fe690f1658
Richard Henderson <richard.henderson@linaro.org>
2020-10-16 22:25:58 -0700

    tcg/aarch64: Split out constraint sets to tcg-target-con-set.h
    Reviewed-by: Peter Maydell <peter.maydell@linaro.org>
    Signed-off-by: Richard Henderson <richard.henderson@linaro.org>

--- tcg/aarch64/tcg-target.h
+++ tcg/aarch64/tcg-target.h
@@ -155,5 +155,6 @@ void tb_target_set_jmp_target(uintptr_t, uintptr_t, uintptr_t, uintptr_t);
 #define TCG_TARGET_NEED_LDST_LABELS
 #endif
 #define TCG_TARGET_NEED_POOL_LABELS
+#define TCG_TARGET_CON_SET_H
 
 #endif /* AARCH64_TCG_TARGET_H */
