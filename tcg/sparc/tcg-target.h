0d11dc7c97e609e641892ab2f22f30f3d292f7b2
Richard Henderson <richard.henderson@linaro.org>
2020-10-17 11:23:30 -0700

    tcg/sparc: Split out constraint sets to tcg-target-con-set.h
    Reviewed-by: Peter Maydell <peter.maydell@linaro.org>
    Signed-off-by: Richard Henderson <richard.henderson@linaro.org>

--- tcg/sparc/tcg-target.h
+++ tcg/sparc/tcg-target.h
@@ -168,5 +168,6 @@ extern bool use_vis3_instructions;
 void tb_target_set_jmp_target(uintptr_t, uintptr_t, uintptr_t, uintptr_t);
 
 #define TCG_TARGET_NEED_POOL_LABELS
+#define TCG_TARGET_CON_SET_H
 
 #endif
