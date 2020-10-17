665be288ace26027bf941d6a2eafa0ecb92009e0
Richard Henderson <richard.henderson@linaro.org>
2020-10-17 11:08:21 -0700

    tcg/riscv: Split out constraint sets to tcg-target-con-set.h
    Reviewed-by: Peter Maydell <peter.maydell@linaro.org>
    Reviewed-by: Alistair Francis <alistair.francis@wdc.com>
    Signed-off-by: Richard Henderson <richard.henderson@linaro.org>

--- tcg/riscv/tcg-target.h
+++ tcg/riscv/tcg-target.h
@@ -171,5 +171,6 @@ void tb_target_set_jmp_target(uintptr_t, uintptr_t, uintptr_t, uintptr_t);
 #define TCG_TARGET_NEED_POOL_LABELS
 
 #define TCG_TARGET_HAS_MEMORY_BSWAP 0
+#define TCG_TARGET_CON_SET_H
 
 #endif
