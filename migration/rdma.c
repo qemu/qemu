cbfc71b52b067b3b70a7ace45ff223b58d63a540
Bihong Yu <yubihong@huawei.com>
2020-10-20 11:10:47 +0800

    migration: Open brace '{' following function declarations go on the next line
    Signed-off-by: Bihong Yu <yubihong@huawei.com>
    Reviewed-by: Chuan Zheng <zhengchuan@huawei.com>
    Reviewed-by: Dr. David Alan Gilbert <dgilbert@redhat.com>
    Message-Id: <1603163448-27122-8-git-send-email-yubihong@huawei.com>
    Signed-off-by: Dr. David Alan Gilbert <dgilbert@redhat.com>

--- migration/rdma.c
+++ migration/rdma.c
@@ -273,7 +273,8 @@ static uint64_t htonll(uint64_t v)
     return u.llv;
 }
 
-static uint64_t ntohll(uint64_t v) {
+static uint64_t ntohll(uint64_t v)
+{
     union { uint32_t lv[2]; uint64_t llv; } u;
     u.llv = v;
     return ((uint64_t)ntohl(u.lv[0]) << 32) | (uint64_t) ntohl(u.lv[1]);
