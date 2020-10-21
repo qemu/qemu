2153994e2efad92092a60bf6d1947f4a721ae46d
Vladimir Sementsov-Ogievskiy <vsementsov@virtuozzo.com>
2020-10-21 17:58:39 +0300

    block: simplify comment to BDRV_REQ_SERIALISING
    1. BDRV_REQ_NO_SERIALISING doesn't exist already, don't mention it.
    
    2. We are going to add one more user of BDRV_REQ_SERIALISING, so
       comment about backup becomes a bit confusing here. The use case in
       backup is documented in block/backup.c, so let's just drop
       duplication here.
    
    3. The fact that BDRV_REQ_SERIALISING is only for write requests is
       omitted. Add a note.
    
    Signed-off-by: Vladimir Sementsov-Ogievskiy <vsementsov@virtuozzo.com>
    Reviewed-by: Stefan Hajnoczi <stefanha@redhat.com>
    Reviewed-by: Alberto Garcia <berto@igalia.com>
    Message-Id: <20201021145859.11201-2-vsementsov@virtuozzo.com>
    Signed-off-by: Max Reitz <mreitz@redhat.com>

--- include/block/block.h
+++ include/block/block.h
@@ -63,16 +63,7 @@ typedef enum {
      * content. */
     BDRV_REQ_WRITE_UNCHANGED    = 0x40,
 
-    /*
-     * BDRV_REQ_SERIALISING forces request serialisation for writes.
-     * It is used to ensure that writes to the backing file of a backup process
-     * target cannot race with a read of the backup target that defers to the
-     * backing file.
-     *
-     * Note, that BDRV_REQ_SERIALISING is _not_ opposite in meaning to
-     * BDRV_REQ_NO_SERIALISING. A more descriptive name for the latter might be
-     * _DO_NOT_WAIT_FOR_SERIALISING, except that is too long.
-     */
+    /* Forces request serialisation. Use only with write requests. */
     BDRV_REQ_SERIALISING        = 0x80,
 
     /* Execute the request only if the operation can be offloaded or otherwise
