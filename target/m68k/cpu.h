4160d5e6bd347e5d27804912b61d02df0a90ba8e
Laurent Vivier <laurent@vivier.eu>
2020-10-22 22:29:59 +0200

    target/m68k: remove useless qregs array
    They are unused since the target has been converted to TCG.
    
    Fixes: e1f3808e03f7 ("Convert m68k target to TCG.")
    Signed-off-by: Laurent Vivier <laurent@vivier.eu>
    Reviewed-by: Philippe Mathieu-Daudé <f4bug@amsat.org>
    Reviewed-by: Thomas Huth <thuth@redhat.com>
    Message-Id: <20201022203000.1922749-2-laurent@vivier.eu>

--- target/m68k/cpu.h
+++ target/m68k/cpu.h
@@ -33,8 +33,6 @@
 #define OS_PACKED   6
 #define OS_UNSIZED  7
 
-#define MAX_QREGS 32
-
 #define EXCP_ACCESS         2   /* Access (MMU) error.  */
 #define EXCP_ADDRESS        3   /* Address error.  */
 #define EXCP_ILLEGAL        4   /* Illegal instruction.  */
@@ -138,8 +136,6 @@ typedef struct CPUM68KState {
 
     int pending_vector;
     int pending_level;
-
-    uint32_t qregs[MAX_QREGS];
 
     /* Fields up to this point are cleared by a CPU reset */
     struct {} end_reset_fields;
