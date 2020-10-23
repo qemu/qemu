00b46eccffd3e74f24906e8d251861625fa3222c
Philippe Mathieu-Daudé <philmd@redhat.com>
2020-10-23 17:19:23 +0200

    pci: Let pci_dma_write() propagate MemTxResult
    pci_dma_rw() returns a MemTxResult type.
    Do not discard it, return it to the caller.
    
    Reviewed-by: Li Qiang <liq3ea@gmail.com>
    Reviewed-by: Richard Henderson <richard.henderson@linaro.org>
    Signed-off-by: Philippe Mathieu-Daudé <philmd@redhat.com>
    Message-Id: <20201023151923.3243652-11-philmd@redhat.com>
    Signed-off-by: Paolo Bonzini <pbonzini@redhat.com>

--- include/hw/pci/pci.h
+++ include/hw/pci/pci.h
@@ -819,8 +819,20 @@ static inline MemTxResult pci_dma_read(PCIDevice *dev, dma_addr_t addr,
     return pci_dma_rw(dev, addr, buf, len, DMA_DIRECTION_TO_DEVICE);
 }
 
-static inline int pci_dma_write(PCIDevice *dev, dma_addr_t addr,
-                                const void *buf, dma_addr_t len)
+/**
+ * pci_dma_write: Write to address space from PCI device.
+ *
+ * Return a MemTxResult indicating whether the operation succeeded
+ * or failed (eg unassigned memory, device rejected the transaction,
+ * IOMMU fault).
+ *
+ * @dev: #PCIDevice doing the memory access
+ * @addr: address within the #PCIDevice address space
+ * @buf: buffer with the data transferred
+ * @len: the number of bytes to write
+ */
+static inline MemTxResult pci_dma_write(PCIDevice *dev, dma_addr_t addr,
+                                        const void *buf, dma_addr_t len)
 {
     return pci_dma_rw(dev, addr, (void *) buf, len, DMA_DIRECTION_FROM_DEVICE);
 }
