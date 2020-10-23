50f57e09fda4b7ffbc5ba62aad6cebf660824023
Chetan Pant <chetan4windows@gmail.com>
2020-10-23 12:29:13 +0000

    arm tcg cpus: Fix Lesser GPL version number
    There is no "version 2" of the "Lesser" General Public License.
    It is either "GPL version 2.0" or "Lesser GPL version 2.1".
    This patch replaces all occurrences of "Lesser GPL version 2" with
    "Lesser GPL version 2.1" in comment section.
    
    Signed-off-by: Chetan Pant <chetan4windows@gmail.com>
    Message-Id: <20201023122913.19561-1-chetan4windows@gmail.com>
    Reviewed-by: Thomas Huth <thuth@redhat.com>
    Signed-off-by: Thomas Huth <thuth@redhat.com>

--- target/arm/crypto_helper.c
+++ target/arm/crypto_helper.c
@@ -6,7 +6,7 @@
  * This library is free software; you can redistribute it and/or
  * modify it under the terms of the GNU Lesser General Public
  * License as published by the Free Software Foundation; either
- * version 2 of the License, or (at your option) any later version.
+ * version 2.1 of the License, or (at your option) any later version.
  */
 
 #include "qemu/osdep.h"
