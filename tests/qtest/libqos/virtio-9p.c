603cc76a60691b288947c88ad63b7b0d616f98b6
Greg Kurz <groug@kaod.org>
2020-10-30 13:46:34 +0100

    tests/9pfs: Force removing of local 9pfs test directory
    No need to get a complaint from "rm" if some path disappeared for some
    reason.
    
    Signed-off-by: Greg Kurz <groug@kaod.org>
    Message-Id: <160406199444.312256.8319835906008559151.stgit@bahia.lan>
    Signed-off-by: Christian Schoenebeck <qemu_oss@crudebyte.com>

--- tests/qtest/libqos/virtio-9p.c
+++ tests/qtest/libqos/virtio-9p.c
@@ -67,7 +67,7 @@ void virtio_9p_create_local_test_dir(void)
 void virtio_9p_remove_local_test_dir(void)
 {
     g_assert(local_test_path != NULL);
-    char *cmd = g_strdup_printf("rm -r '%s'\n", local_test_path);
+    char *cmd = g_strdup_printf("rm -fr '%s'\n", local_test_path);
     int res = system(cmd);
     if (res < 0) {
         /* ignore error, dummy check to prevent compiler error */
