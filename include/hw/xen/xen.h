97d351b476233ed8c80e61af79d6437fb3cef058
Alex Bennée <alex.bennee@linaro.org>
2020-11-10 19:23:10 +0000

    include/hw/xen.h: drop superfluous struct
    Chardev is already a typedef'ed struct.
    
    Signed-off-by: Alex Bennée <alex.bennee@linaro.org>
    Reviewed-by: Philippe Mathieu-Daudé <philmd@redhat.com>
    Message-Id: <20201110192316.26397-5-alex.bennee@linaro.org>

--- include/hw/xen/xen.h
+++ include/hw/xen/xen.h
@@ -28,7 +28,7 @@ int xen_is_pirq_msi(uint32_t msi_data);
 
 qemu_irq *xen_interrupt_controller_init(void);
 
-void xenstore_store_pv_console_info(int i, struct Chardev *chr);
+void xenstore_store_pv_console_info(int i, Chardev *chr);
 
 void xen_register_framebuffer(struct MemoryRegion *mr);
 
