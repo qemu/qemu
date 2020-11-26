dd5697b2f90c3c6aa4a97b247aa70635914a6041
Philippe Mathieu-Daudé <f4bug@amsat.org>
2020-11-26 11:54:30 +0100

    target/mips: Convert Rel6 LWLE/LWRE/SWLE/SWRE opcodes to decodetree
    LWLE/LWRE/SWLE/SWRE (EVA) opcodes have been removed from
    the Release 6. Add a single decodetree entry for the opcodes,
    triggering Reserved Instruction if ever used.
    
    Remove unreachable check_insn_opc_removed() calls.
    
    Signed-off-by: Philippe Mathieu-Daudé <f4bug@amsat.org>
    Reviewed-by: Richard Henderson <richard.henderson@linaro.org>
    Message-Id: <20201208203704.243704-11-f4bug@amsat.org>

--- target/mips/translate.c
+++ target/mips/translate.c
@@ -28121,8 +28121,6 @@ static void decode_opc_special3(CPUMIPSState *env, DisasContext *ctx)
         switch (op1) {
         case OPC_LWLE:
         case OPC_LWRE:
-            check_insn_opc_removed(ctx, ISA_MIPS_R6);
-            /* fall through */
         case OPC_LBUE:
         case OPC_LHUE:
         case OPC_LBE:
@@ -28134,8 +28132,6 @@ static void decode_opc_special3(CPUMIPSState *env, DisasContext *ctx)
             return;
         case OPC_SWLE:
         case OPC_SWRE:
-            check_insn_opc_removed(ctx, ISA_MIPS_R6);
-            /* fall through */
         case OPC_SBE:
         case OPC_SHE:
         case OPC_SWE:
