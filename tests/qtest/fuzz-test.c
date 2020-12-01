4bfb024bc76973d40a359476dc0291f46e435442
Paolo Bonzini <pbonzini@redhat.com>
2020-12-01 09:29:56 -0500

    memory: clamp cached translation in case it points to an MMIO region
    In using the address_space_translate_internal API, address_space_cache_init
    forgot one piece of advice that can be found in the code for
    address_space_translate_internal:
    
        /* MMIO registers can be expected to perform full-width accesses based only
         * on their address, without considering adjacent registers that could
         * decode to completely different MemoryRegions.  When such registers
         * exist (e.g. I/O ports 0xcf8 and 0xcf9 on most PC chipsets), MMIO
         * regions overlap wildly.  For this reason we cannot clamp the accesses
         * here.
         *
         * If the length is small (as is the case for address_space_ldl/stl),
         * everything works fine.  If the incoming length is large, however,
         * the caller really has to do the clamping through memory_access_size.
         */
    
    address_space_cache_init is exactly one such case where "the incoming length
    is large", therefore we need to clamp the resulting length---not to
    memory_access_size though, since we are not doing an access yet, but to
    the size of the resulting section.  This ensures that subsequent accesses
    to the cached MemoryRegionSection will be in range.
    
    With this patch, the enclosed testcase notices that the used ring does
    not fit into the MSI-X table and prints a "qemu-system-x86_64: Cannot map used"
    error.
    
    Signed-off-by: Paolo Bonzini <pbonzini@redhat.com>

--- tests/qtest/fuzz-test.c
+++ tests/qtest/fuzz-test.c
@@ -48,6 +48,55 @@ static void test_lp1878642_pci_bus_get_irq_level_assert(void)
     qtest_quit(s);
 }
 
+/*
+ * Here a MemoryRegionCache pointed to an MMIO region but had a
+ * larger size than the underlying region.
+ */
+static void test_mmio_oob_from_memory_region_cache(void)
+{
+    QTestState *s;
+
+    s = qtest_init("-M pc-q35-5.2 -display none -m 512M "
+		   "-device virtio-scsi,num_queues=8,addr=03.0 ");
+
+    qtest_outl(s, 0xcf8, 0x80001811);
+    qtest_outb(s, 0xcfc, 0x6e);
+    qtest_outl(s, 0xcf8, 0x80001824);
+    qtest_outl(s, 0xcf8, 0x80001813);
+    qtest_outl(s, 0xcfc, 0xa080000);
+    qtest_outl(s, 0xcf8, 0x80001802);
+    qtest_outl(s, 0xcfc, 0x5a175a63);
+    qtest_outb(s, 0x6e08, 0x9e);
+    qtest_writeb(s, 0x9f003, 0xff);
+    qtest_writeb(s, 0x9f004, 0x01);
+    qtest_writeb(s, 0x9e012, 0x0e);
+    qtest_writeb(s, 0x9e01b, 0x0e);
+    qtest_writeb(s, 0x9f006, 0x01);
+    qtest_writeb(s, 0x9f008, 0x01);
+    qtest_writeb(s, 0x9f00a, 0x01);
+    qtest_writeb(s, 0x9f00c, 0x01);
+    qtest_writeb(s, 0x9f00e, 0x01);
+    qtest_writeb(s, 0x9f010, 0x01);
+    qtest_writeb(s, 0x9f012, 0x01);
+    qtest_writeb(s, 0x9f014, 0x01);
+    qtest_writeb(s, 0x9f016, 0x01);
+    qtest_writeb(s, 0x9f018, 0x01);
+    qtest_writeb(s, 0x9f01a, 0x01);
+    qtest_writeb(s, 0x9f01c, 0x01);
+    qtest_writeb(s, 0x9f01e, 0x01);
+    qtest_writeb(s, 0x9f020, 0x01);
+    qtest_writeb(s, 0x9f022, 0x01);
+    qtest_writeb(s, 0x9f024, 0x01);
+    qtest_writeb(s, 0x9f026, 0x01);
+    qtest_writeb(s, 0x9f028, 0x01);
+    qtest_writeb(s, 0x9f02a, 0x01);
+    qtest_writeb(s, 0x9f02c, 0x01);
+    qtest_writeb(s, 0x9f02e, 0x01);
+    qtest_writeb(s, 0x9f030, 0x01);
+    qtest_outb(s, 0x6e10, 0x00);
+    qtest_quit(s);
+}
+
 int main(int argc, char **argv)
 {
     const char *arch = qtest_get_arch();
@@ -59,6 +108,8 @@ int main(int argc, char **argv)
                        test_lp1878263_megasas_zero_iov_cnt);
         qtest_add_func("fuzz/test_lp1878642_pci_bus_get_irq_level_assert",
                        test_lp1878642_pci_bus_get_irq_level_assert);
+        qtest_add_func("fuzz/test_mmio_oob_from_memory_region_cache",
+                       test_mmio_oob_from_memory_region_cache);
     }
 
     return g_test_run();
