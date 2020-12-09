cd42771a334434c5ca8a4a48a62e48570dbe8d9e
Klaus Jensen <k.jensen@samsung.com>
2020-12-09 23:55:06 +0100

    hw/block/nvme: add missing string representations for commands
    Add missing string representations for a couple of new commands.
    
    Signed-off-by: Klaus Jensen <k.jensen@samsung.com>
    Tested-by: Dmitry Fomichev <dmitry.fomichev@wdc.com>
    Reviewed-by: Dmitry Fomichev <dmitry.fomichev@wdc.com>

--- hw/block/nvme.h
+++ hw/block/nvme.h
@@ -64,8 +64,12 @@ static inline const char *nvme_io_opc_str(uint8_t opc)
     case NVME_CMD_FLUSH:            return "NVME_NVM_CMD_FLUSH";
     case NVME_CMD_WRITE:            return "NVME_NVM_CMD_WRITE";
     case NVME_CMD_READ:             return "NVME_NVM_CMD_READ";
+    case NVME_CMD_COMPARE:          return "NVME_NVM_CMD_COMPARE";
     case NVME_CMD_WRITE_ZEROES:     return "NVME_NVM_CMD_WRITE_ZEROES";
     case NVME_CMD_DSM:              return "NVME_NVM_CMD_DSM";
+    case NVME_CMD_ZONE_MGMT_SEND:   return "NVME_ZONED_CMD_MGMT_SEND";
+    case NVME_CMD_ZONE_MGMT_RECV:   return "NVME_ZONED_CMD_MGMT_RECV";
+    case NVME_CMD_ZONE_APPEND:      return "NVME_ZONED_CMD_ZONE_APPEND";
     default:                        return "NVME_NVM_CMD_UNKNOWN";
     }
 }
