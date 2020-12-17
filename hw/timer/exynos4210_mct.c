d97d9152c7baa9f3743aea7ffe124c7977a9b0fa
Gan Qixin <ganqixin@huawei.com>
2020-12-17 19:31:50 +0800

    exynos4210_mct: Use ptimer_free() in the finalize function to avoid memleaks
    When running device-introspect-test, a memory leak occurred in the
    exynos4210_mct_init function, so use ptimer_free() in the finalize function to
    avoid it.
    
    ASAN shows memory leak stack:
    
    Indirect leak of 96 byte(s) in 1 object(s) allocated from:
        #0 0xffffab97e1f0 in __interceptor_calloc (/lib64/libasan.so.5+0xee1f0)
        #1 0xffffab256800 in g_malloc0 (/lib64/libglib-2.0.so.0+0x56800)
        #2 0xaaabf555db78 in ptimer_init /qemu/hw/core/ptimer.c:432
        #3 0xaaabf56b01a0 in exynos4210_mct_init /qemu/hw/timer/exynos4210_mct.c:1505
        #4 0xaaabf6339f6c in object_initialize_with_type /qemu/qom/object.c:515
        #5 0xaaabf633a1e0 in object_new_with_type /qemu/qom/object.c:729
        #6 0xaaabf6375e40 in qmp_device_list_properties /qemu/qom/qom-qmp-cmds.c:153
        #7 0xaaabf653d8ec in qmp_marshal_device_list_properties /qemu/qapi/qapi-commands-qdev.c:59
        #8 0xaaabf6587d08 in do_qmp_dispatch_bh /qemu/qapi/qmp-dispatch.c:110
        #9 0xaaabf6552708 in aio_bh_call /qemu/util/async.c:136
        #10 0xaaabf6552708 in aio_bh_poll /qemu/util/async.c:164
        #11 0xaaabf655f19c in aio_dispatch /qemu/util/aio-posix.c:381
        #12 0xaaabf65523f4 in aio_ctx_dispatch /qemu/util/async.c:306
    
    Reported-by: Euler Robot <euler.robot@huawei.com>
    Signed-off-by: Gan Qixin <ganqixin@huawei.com>
    Reviewed-by: Peter Maydell <peter.maydell@linaro.org>
    Signed-off-by: Peter Maydell <peter.maydell@linaro.org>

--- hw/timer/exynos4210_mct.c
+++ hw/timer/exynos4210_mct.c
@@ -1530,6 +1530,19 @@ static void exynos4210_mct_init(Object *obj)
     sysbus_init_mmio(dev, &s->iomem);
 }
 
+static void exynos4210_mct_finalize(Object *obj)
+{
+    int i;
+    Exynos4210MCTState *s = EXYNOS4210_MCT(obj);
+
+    ptimer_free(s->g_timer.ptimer_frc);
+
+    for (i = 0; i < 2; i++) {
+        ptimer_free(s->l_timer[i].tick_timer.ptimer_tick);
+        ptimer_free(s->l_timer[i].ptimer_frc);
+    }
+}
+
 static void exynos4210_mct_class_init(ObjectClass *klass, void *data)
 {
     DeviceClass *dc = DEVICE_CLASS(klass);
@@ -1543,6 +1556,7 @@ static const TypeInfo exynos4210_mct_info = {
     .parent        = TYPE_SYS_BUS_DEVICE,
     .instance_size = sizeof(Exynos4210MCTState),
     .instance_init = exynos4210_mct_init,
+    .instance_finalize = exynos4210_mct_finalize,
     .class_init    = exynos4210_mct_class_init,
 };
 
