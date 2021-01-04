86635aa4e9d627d5142b81c57a33dd1f36627d07
Stefan Hajnoczi <stefanha@redhat.com>
2021-01-04 17:13:19 +0000

    hostmem-file: add readonly=on|off option
    Let -object memory-backend-file work on read-only files when the
    readonly=on option is given. This can be used to share the contents of a
    file between multiple guests while preventing them from consuming
    Copy-on-Write memory if guests dirty the pages, for example.
    
    Signed-off-by: Stefan Hajnoczi <stefanha@redhat.com>
    Reviewed-by: Philippe Mathieu-Daudé <philmd@redhat.com>
    Reviewed-by: Liam Merwick <liam.merwick@oracle.com>
    Acked-by: Michael S. Tsirkin <mst@redhat.com>
    Message-Id: <20210104171320.575838-3-stefanha@redhat.com>
    Signed-off-by: Eduardo Habkost <ehabkost@redhat.com>

--- qemu-options.hx
+++ qemu-options.hx
@@ -4426,7 +4426,7 @@ SRST
     they are specified. Note that the 'id' property must be set. These
     objects are placed in the '/objects' path.
 
-    ``-object memory-backend-file,id=id,size=size,mem-path=dir,share=on|off,discard-data=on|off,merge=on|off,dump=on|off,prealloc=on|off,host-nodes=host-nodes,policy=default|preferred|bind|interleave,align=align``
+    ``-object memory-backend-file,id=id,size=size,mem-path=dir,share=on|off,discard-data=on|off,merge=on|off,dump=on|off,prealloc=on|off,host-nodes=host-nodes,policy=default|preferred|bind|interleave,align=align,readonly=on|off``
         Creates a memory file backend object, which can be used to back
         the guest RAM with huge pages.
 
@@ -4508,6 +4508,9 @@ SRST
         requires support from both the host kernel (since Linux kernel
         4.15) and the filesystem of ``mem-path`` mounted with DAX
         option.
+
+        The ``readonly`` option specifies whether the backing file is opened
+        read-only or read-write (default).
 
     ``-object memory-backend-ram,id=id,merge=on|off,dump=on|off,share=on|off,prealloc=on|off,size=size,host-nodes=host-nodes,policy=default|preferred|bind|interleave``
         Creates a memory backend object, which can be used to back the
