5ca192dfc551c8a40871c4e30a8b8ceb879adc31
Rémi Denis-Courmont <remi.denis.courmont@huawei.com>
2021-01-12 12:44:58 +0200

    target/arm: Define isar_feature function to test for presence of SEL2
    Signed-off-by: Rémi Denis-Courmont <remi.denis.courmont@huawei.com>
    Reviewed-by: Richard Henderson <richard.henderson@linaro.org>
    Message-id: 20210112104511.36576-6-remi.denis.courmont@huawei.com
    [PMM: tweaked commit message to match reduced scope of patch
     following rebase]
    Signed-off-by: Peter Maydell <peter.maydell@linaro.org>

--- target/arm/cpu.h
+++ target/arm/cpu.h
@@ -4011,6 +4011,11 @@ static inline bool isar_feature_aa64_sve(const ARMISARegisters *id)
     return FIELD_EX64(id->id_aa64pfr0, ID_AA64PFR0, SVE) != 0;
 }
 
+static inline bool isar_feature_aa64_sel2(const ARMISARegisters *id)
+{
+    return FIELD_EX64(id->id_aa64pfr0, ID_AA64PFR0, SEL2) != 0;
+}
+
 static inline bool isar_feature_aa64_vh(const ARMISARegisters *id)
 {
     return FIELD_EX64(id->id_aa64mmfr1, ID_AA64MMFR1, VH) != 0;
