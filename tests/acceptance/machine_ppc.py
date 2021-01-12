46c647e69d6525c63bc235d96b5956130dc3f04f
Thomas Huth <thuth@redhat.com>
2021-01-12 17:40:44 +0100

    tests/acceptance: Test the mpc8544ds machine
    We can use the "Stupid creek" image to test the mpc8544ds ppc machine.
    
    Signed-off-by: Thomas Huth <thuth@redhat.com>
    Reviewed-by: Wainer dos Santos Moschetta <wainersm@redhat.com>
    Reviewed-by: Willian Rampazzo <willianr@redhat.com>
    Acked-by: David Gibson <david@gibson.dropbear.id.au>
    Message-Id: <20210112164045.98565-3-thuth@redhat.com>
    Signed-off-by: Philippe Mathieu-Daudé <philmd@redhat.com>

--- tests/acceptance/machine_ppc.py
+++ tests/acceptance/machine_ppc.py
@@ -5,6 +5,7 @@
 # This work is licensed under the terms of the GNU GPL, version 2 or
 # later.  See the COPYING file in the top-level directory.
 
+from avocado.utils import archive
 from avocado_qemu import Test
 from avocado_qemu import wait_for_console_pattern
 
@@ -32,3 +33,19 @@ class PpcMachine(Test):
         self.vm.launch()
         console_pattern = 'Kernel command line: %s' % kernel_command_line
         wait_for_console_pattern(self, console_pattern, self.panic_message)
+
+    def test_ppc_mpc8544ds(self):
+        """
+        :avocado: tags=arch:ppc
+        :avocado: tags=machine:mpc8544ds
+        """
+        tar_url = ('https://www.qemu-advent-calendar.org'
+                   '/2020/download/day17.tar.gz')
+        tar_hash = '7a5239542a7c4257aa4d3b7f6ddf08fb6775c494'
+        file_path = self.fetch_asset(tar_url, asset_hash=tar_hash)
+        archive.extract(file_path, self.workdir)
+        self.vm.set_console()
+        self.vm.add_args('-kernel', self.workdir + '/creek/creek.bin')
+        self.vm.launch()
+        wait_for_console_pattern(self, 'QEMU advent calendar 2020',
+                                 self.panic_message)
