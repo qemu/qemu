41da32471183d7ca4756ad3ed8bb11c1d0c37a32
Peter Maydell <peter.maydell@linaro.org>
2021-01-15 20:12:06 +0000

    hw/m68k/next-cube: Add missing header comment to next-cube.h
    The next-cube.h file is missing the usual copyright-and-license
    header; add it (same as the next-cube.c one).
    
    Signed-off-by: Peter Maydell <peter.maydell@linaro.org>
    Message-Id: <20210115201206.17347-12-peter.maydell@linaro.org>
    Signed-off-by: Thomas Huth <huth@tuxfamily.org>

--- include/hw/m68k/next-cube.h
+++ include/hw/m68k/next-cube.h
@@ -1,3 +1,13 @@
+/*
+ * NeXT Cube
+ *
+ * Copyright (c) 2011 Bryce Lanham
+ *
+ * This code is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published
+ * by the Free Software Foundation; either version 2 of the License,
+ * or (at your option) any later version.
+ */
 
 #ifndef NEXT_CUBE_H
 #define NEXT_CUBE_H
