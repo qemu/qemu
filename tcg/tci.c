cbec0754bed0d890a277746c68ce193cd3f1070d
Stefan Weil <sw@weilnetz.de>
2021-01-28 03:04:25 +0100

    tcg/tci: Implement INDEX_op_ld8s_i64
    That TCG opcode is used by debian-buster (arm64) running ffmpeg:
    
        qemu-aarch64 /usr/bin/ffmpeg -i theora.mkv theora.webm
    
    Tested-by: Alex Bennée <alex.bennee@linaro.org>
    Reviewed-by: Alex Bennée <alex.bennee@linaro.org>
    Reported-by: Alex Bennée <alex.bennee@linaro.org>
    Signed-off-by: Stefan Weil <sw@weilnetz.de>
    Message-Id: <20210128020425.2055454-1-sw@weilnetz.de>
    Signed-off-by: Richard Henderson <richard.henderson@linaro.org>

--- tcg/tci.c
+++ tcg/tci.c
@@ -883,7 +883,10 @@ uintptr_t QEMU_DISABLE_CFI tcg_qemu_tb_exec(CPUArchState *env,
             tci_write_reg8(regs, t0, *(uint8_t *)(t1 + t2));
             break;
         case INDEX_op_ld8s_i64:
-            TODO();
+            t0 = *tb_ptr++;
+            t1 = tci_read_r(regs, &tb_ptr);
+            t2 = tci_read_s32(&tb_ptr);
+            tci_write_reg(regs, t0, *(int8_t *)(t1 + t2));
             break;
         case INDEX_op_ld16u_i64:
             t0 = *tb_ptr++;
