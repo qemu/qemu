213f3b27dd099ef352181c48cd75c0f20a73e3f0
Andrea Fioraldi <andreafioraldi@gmail.com>
2021-02-15 10:27:05 +0100

    qasan shadow error on 32 bits hosts

--- qemuafl/asan-giovese-inl.h
+++ qemuafl/asan-giovese-inl.h
@@ -102,6 +102,11 @@ void* __ag_low_shadow = LOW_SHADOW_ADDR;
 
 void asan_giovese_init(void) {
 
+#if UINTPTR_MAX == 0xffffffff
+  fprintf(stderr, "ERROR: Cannot allocate sanitizer shadow memory on 32 bit "
+                  "platforms."):
+  exit(1);
+#else
   assert(mmap(__ag_high_shadow, HIGH_SHADOW_SIZE, PROT_READ | PROT_WRITE,
               MAP_PRIVATE | MAP_FIXED | MAP_NORESERVE | MAP_ANON, -1,
               0) != MAP_FAILED);
@@ -113,6 +118,7 @@ void asan_giovese_init(void) {
   assert(mmap(GAP_SHADOW_ADDR, GAP_SHADOW_SIZE, PROT_NONE,
               MAP_PRIVATE | MAP_FIXED | MAP_NORESERVE | MAP_ANON, -1,
               0) != MAP_FAILED);
+#endif
 
 }
 
