48c8c5e8862431235ec5ab553977ee5fe6ffbba7
Philippe Mathieu-Daudé <f4bug@amsat.org>
2021-03-03 22:46:57 +0100

    linux-user: Remove dead code
    We can not use watchpoints in user-mode emulation because we
    need the softmmu slow path to detect accesses to watchpointed
    memory. This code is expanded as empty stub in "hw/core/cpu.h"
    anyway, so we can drop it.
    
    Reviewed-by: Laurent Vivier <laurent@vivier.eu>
    Signed-off-by: Philippe Mathieu-Daudé <f4bug@amsat.org>
    Message-Id: <20210303214708.1727801-18-f4bug@amsat.org>
    Signed-off-by: Laurent Vivier <laurent@vivier.eu>

--- linux-user/main.c
+++ linux-user/main.c
@@ -205,7 +205,6 @@ CPUArchState *cpu_copy(CPUArchState *env)
     CPUState *new_cpu = cpu_create(cpu_type);
     CPUArchState *new_env = new_cpu->env_ptr;
     CPUBreakpoint *bp;
-    CPUWatchpoint *wp;
 
     /* Reset non arch specific state */
     cpu_reset(new_cpu);
@@ -217,12 +216,8 @@ CPUArchState *cpu_copy(CPUArchState *env)
        Note: Once we support ptrace with hw-debug register access, make sure
        BP_CPU break/watchpoints are handled correctly on clone. */
     QTAILQ_INIT(&new_cpu->breakpoints);
-    QTAILQ_INIT(&new_cpu->watchpoints);
     QTAILQ_FOREACH(bp, &cpu->breakpoints, entry) {
         cpu_breakpoint_insert(new_cpu, bp->pc, bp->flags, NULL);
-    }
-    QTAILQ_FOREACH(wp, &cpu->watchpoints, entry) {
-        cpu_watchpoint_insert(new_cpu, wp->vaddr, wp->len, wp->flags, NULL);
     }
 
     return new_env;
