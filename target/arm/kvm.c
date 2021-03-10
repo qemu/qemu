bcb902a1ed1ad5e0ceebb9536f392bf6d46219f9
Andrew Jones <drjones@redhat.com>
2021-03-10 14:52:18 +0100

    hw/arm/virt: KVM: The IPA lower bound is 32
    The virt machine already checks KVM_CAP_ARM_VM_IPA_SIZE to get the
    upper bound of the IPA size. If that bound is lower than the highest
    possible GPA for the machine, then QEMU will error out. However, the
    IPA is set to 40 when the highest GPA is less than or equal to 40,
    even when KVM may support an IPA limit as low as 32. This means KVM
    may fail the VM creation unnecessarily. Additionally, 40 is selected
    with the value 0, which means use the default, and that gets around
    a check in some versions of KVM, causing a difficult to debug fail.
    Always use the IPA size that corresponds to the highest possible GPA,
    unless it's lower than 32, in which case use 32. Also, we must still
    use 0 when KVM only supports the legacy fixed 40 bit IPA.
    
    Suggested-by: Marc Zyngier <maz@kernel.org>
    Signed-off-by: Andrew Jones <drjones@redhat.com>
    Reviewed-by: Eric Auger <eric.auger@redhat.com>
    Reviewed-by: Marc Zyngier <maz@kernel.org>
    Message-id: 20210310135218.255205-3-drjones@redhat.com
    Signed-off-by: Peter Maydell <peter.maydell@linaro.org>

--- target/arm/kvm.c
+++ target/arm/kvm.c
@@ -230,12 +230,14 @@ bool kvm_arm_pmu_supported(void)
     return kvm_check_extension(kvm_state, KVM_CAP_ARM_PMU_V3);
 }
 
-int kvm_arm_get_max_vm_ipa_size(MachineState *ms)
+int kvm_arm_get_max_vm_ipa_size(MachineState *ms, bool *fixed_ipa)
 {
     KVMState *s = KVM_STATE(ms->accelerator);
     int ret;
 
     ret = kvm_check_extension(s, KVM_CAP_ARM_VM_IPA_SIZE);
+    *fixed_ipa = ret <= 0;
+
     return ret > 0 ? ret : 40;
 }
 
