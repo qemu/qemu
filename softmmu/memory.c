7cac7fea708a1ca46f16c4e816d88b40da755de1
Alexander Bulekov <alxndr@bu.edu>
2021-03-15 10:05:12 -0400

    fuzz: move some DMA hooks
    For the sparse-mem device, we want the fuzzer to populate entire DMA
    reads from sparse-mem, rather than hooking into the individual MMIO
    memory_region_dispatch_read operations. Otherwise, the fuzzer will treat
    each sequential read separately (and populate it with a separate
    pattern). Work around this by rearranging some DMA hooks. Since the
    fuzzer has it's own logic to skip accidentally writing to MMIO regions,
    we can call the DMA cb, outside the flatview_translate loop.
    
    Signed-off-by: Alexander Bulekov <alxndr@bu.edu>
    Reviewed-by: Darren Kenny <darren.kenny@oracle.com>
    Signed-off-by: Paolo Bonzini <pbonzini@redhat.com>

--- softmmu/memory.c
+++ softmmu/memory.c
@@ -1440,7 +1440,6 @@ MemTxResult memory_region_dispatch_read(MemoryRegion *mr,
     unsigned size = memop_size(op);
     MemTxResult r;
 
-    fuzz_dma_read_cb(addr, size, mr);
     if (!memory_region_access_valid(mr, addr, size, false, attrs)) {
         *pval = unassigned_mem_read(mr, addr, size);
         return MEMTX_DECODE_ERROR;
