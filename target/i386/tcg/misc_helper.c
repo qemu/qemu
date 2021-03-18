da3f3b020f5346f59c30241ff6f3da60be87ed78
Paolo Bonzini <pbonzini@redhat.com>
2021-03-18 09:42:32 -0400

    target/i386: fail if toggling LA57 in 64-bit mode
    This fixes kvm-unit-tests access.flat with -cpu qemu64,la57.
    
    Signed-off-by: Paolo Bonzini <pbonzini@redhat.com>

--- target/i386/tcg/misc_helper.c
+++ target/i386/tcg/misc_helper.c
@@ -167,6 +167,10 @@ void helper_write_crN(CPUX86State *env, int reg, target_ulong t0)
         cpu_x86_update_cr3(env, t0);
         break;
     case 4:
+        if (((t0 ^ env->cr[4]) & CR4_LA57_MASK) &&
+            (env->hflags & HF_CS64_MASK)) {
+             raise_exception_ra(env, EXCP0D_GPF, GETPC());
+        }
         cpu_x86_update_cr4(env, t0);
         break;
     case 8:
