498114b37bc99fddcfc24b92bff7f1323fb32672
Joelle van Dyne <j@getutm.app>
2021-03-22 01:24:44 -0700

    hw/block/nvme: expose 'bootindex' property
    The check for `n->namespace.blkconf.blk` always fails because
    this is in the initialization function.
    
    Signed-off-by: Joelle van Dyne <j@getutm.app>
    Reviewed-by: Klaus Jensen <k.jensen@samsung.com>
    Signed-off-by: Klaus Jensen <k.jensen@samsung.com>

--- hw/block/nvme.c
+++ hw/block/nvme.c
@@ -6328,11 +6328,9 @@ static void nvme_instance_init(Object *obj)
 {
     NvmeCtrl *n = NVME(obj);
 
-    if (n->namespace.blkconf.blk) {
-        device_add_bootindex_property(obj, &n->namespace.blkconf.bootindex,
-                                      "bootindex", "/namespace@1,0",
-                                      DEVICE(obj));
-    }
+    device_add_bootindex_property(obj, &n->namespace.blkconf.bootindex,
+                                  "bootindex", "/namespace@1,0",
+                                  DEVICE(obj));
 
     object_property_add(obj, "smart_critical_warning", "uint8",
                         nvme_get_smart_warning,
