27eb9d65ff5121ed11979dd57d9ec2b6c2315c01
Richard Henderson <richard.henderson@linaro.org>
2021-03-22 12:24:25 +0100

    exec: Extract 'page-vary.h' header
    In the next commit we will extract the generic code out of
    page-vary.c, only keeping the target specific code. Both
    files will use the same TargetPageBits structure, so make
    its declaration in a shared header.
    
    As the common header can not use target specific types,
    use a uint64_t to hold the page mask value, and add a
    cast back to target_long in the TARGET_PAGE_MASK definitions.
    
    Signed-off-by: Philippe Mathieu-Daudé <f4bug@amsat.org>
    Message-Id: <20210322112427.4045204-3-f4bug@amsat.org>
    Signed-off-by: Richard Henderson <richard.henderson@linaro.org>

---
+++ include/exec/page-vary.h
@@ -0,0 +1,29 @@
+/*
+ * Definitions for cpus with variable page sizes.
+ *
+ *  Copyright (c) 2003 Fabrice Bellard
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, see <http://www.gnu.org/licenses/>.
+ */
+
+#ifndef EXEC_PAGE_VARY_H
+#define EXEC_PAGE_VARY_H
+
+typedef struct {
+    bool decided;
+    int bits;
+    uint64_t mask;
+} TargetPageBits;
+
+#endif /* EXEC_PAGE_VARY_H */
