bcad139192b0101e3d7ef593144c314bed4cb8c2
Philippe Mathieu-Daudé <f4bug@amsat.org>
2021-03-24 15:24:41 +0100

    hw/isa/piix4: Use qdev_get_gpio_in_named() to get ISA IRQ
    Since commit 078778c5a55 ("piix4: Add an i8259 Interrupt Controller")
    the TYPE_PIIX4_PCI_DEVICE exposes the ISA input IRQs as "isa" alias.
    
    Use this alias to get IRQ for the power management PCI function.
    
    Signed-off-by: Philippe Mathieu-Daudé <f4bug@amsat.org>
    Reviewed-by: Richard Henderson <richard.henderson@linaro.org>
    Message-Id: <20210324182902.692419-1-f4bug@amsat.org>

--- hw/isa/piix4.c
+++ hw/isa/piix4.c
@@ -268,8 +268,9 @@ DeviceState *piix4_create(PCIBus *pci_bus, ISABus **isa_bus, I2CBus **smbus)
     pci_create_simple(pci_bus, devfn + 2, "piix4-usb-uhci");
     if (smbus) {
         *smbus = piix4_pm_init(pci_bus, devfn + 3, 0x1100,
-                               isa_get_irq(NULL, 9), NULL, 0, NULL);
-   }
+                               qdev_get_gpio_in_named(dev, "isa", 9),
+                               NULL, 0, NULL);
+    }
 
     return dev;
 }
