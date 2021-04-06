03b3542ac93cb196bf6a6d85e92fa68a894f5256
Philippe Mathieu-Daudé <f4bug@amsat.org>
2021-04-06 10:48:42 +0200

    hw/ppc/mac_newworld: Restrict RAM to 2 GiB
    On Mac99 and newer machines, the Uninorth PCI host bridge maps
    the PCI hole region at 2GiB, so the RAM area beside 2GiB is not
    accessible by the CPU. Restrict the memory to 2GiB to avoid
    problems such the one reported in the buglink.
    
    Buglink: https://bugs.launchpad.net/qemu/+bug/1922391
    Reported-by: Håvard Eidnes <he@NetBSD.org>
    Signed-off-by: Philippe Mathieu-Daudé <f4bug@amsat.org>
    Message-Id: <20210406084842.2859664-1-f4bug@amsat.org>
    Reviewed-by: BALATON Zoltan <balaton@eik.bme.hu>
    Signed-off-by: David Gibson <david@gibson.dropbear.id.au>

--- hw/ppc/mac_newworld.c
+++ hw/ppc/mac_newworld.c
@@ -157,6 +157,10 @@ static void ppc_core99_init(MachineState *machine)
     }
 
     /* allocate RAM */
+    if (machine->ram_size > 2 * GiB) {
+        error_report("RAM size more than 2 GiB is not supported");
+        exit(1);
+    }
     memory_region_add_subregion(get_system_memory(), 0, machine->ram);
 
     /* allocate and load firmware ROM */
