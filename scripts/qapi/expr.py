4918bb7defbdcb1e27cc2adf4e1604486d778ece
John Snow <jsnow@redhat.com>
2021-04-21 14:20:22 -0400

    qapi/expr.py: Check type of union and alternate 'data' member
    Prior to this commit, specifying a non-object value here causes the QAPI
    parser to crash in expr.py with a stack trace with (likely) an
    AttributeError when we attempt to call that value's items() method.
    
    This member needs to be an object (Dict), and not anything else. Add a
    check for this with a nicer error message, and formalize that check with
    new test cases that exercise that error.
    
    Signed-off-by: John Snow <jsnow@redhat.com>
    Message-Id: <20210421182032.3521476-8-jsnow@redhat.com>
    Reviewed-by: Markus Armbruster <armbru@redhat.com>
    Signed-off-by: Markus Armbruster <armbru@redhat.com>

--- scripts/qapi/expr.py
+++ scripts/qapi/expr.py
@@ -283,6 +283,9 @@ def check_union(expr, info):
             raise QAPISemError(info, "'discriminator' requires 'base'")
         check_name_is_str(discriminator, info, "'discriminator'")
 
+    if not isinstance(members, dict):
+        raise QAPISemError(info, "'data' must be an object")
+
     for (key, value) in members.items():
         source = "'data' member '%s'" % key
         if discriminator is None:
@@ -298,6 +301,10 @@ def check_alternate(expr, info):
 
     if not members:
         raise QAPISemError(info, "'data' must not be empty")
+
+    if not isinstance(members, dict):
+        raise QAPISemError(info, "'data' must be an object")
+
     for (key, value) in members.items():
         source = "'data' member '%s'" % key
         check_name_lower(key, info, source)
