d80f54ce53167e38623b8aafe8317458a6d7a6cd
Dr. David Alan Gilbert <dgilbert@redhat.com>
2021-04-21 12:28:30 +0100

    channel-socket: Only set CLOEXEC if we have space for fds
    MSG_CMSG_CLOEXEC cleans up received fd's; it's really only for Unix
    sockets, but currently we enable it for everything; some socket types
    (IP_MPTCP) don't like this.
    
    Only enable it when we're giving the recvmsg room to receive fd's
    anyway.
    
    Signed-off-by: Dr. David Alan Gilbert <dgilbert@redhat.com>
    Reviewed-by: Daniel P. Berrangé <berrange@redhat.com>
    Message-Id: <20210421112834.107651-2-dgilbert@redhat.com>
    Signed-off-by: Dr. David Alan Gilbert <dgilbert@redhat.com>

--- io/channel-socket.c
+++ io/channel-socket.c
@@ -487,15 +487,15 @@ static ssize_t qio_channel_socket_readv(QIOChannel *ioc,
 
     memset(control, 0, CMSG_SPACE(sizeof(int) * SOCKET_MAX_FDS));
 
-#ifdef MSG_CMSG_CLOEXEC
-    sflags |= MSG_CMSG_CLOEXEC;
-#endif
-
     msg.msg_iov = (struct iovec *)iov;
     msg.msg_iovlen = niov;
     if (fds && nfds) {
         msg.msg_control = control;
         msg.msg_controllen = sizeof(control);
+#ifdef MSG_CMSG_CLOEXEC
+        sflags |= MSG_CMSG_CLOEXEC;
+#endif
+
     }
 
  retry:
