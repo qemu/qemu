78258debe03c6034365884cbfb05679d6938aaac
Andrew Melnychenko <andrew@daynix.com>
2021-05-14 14:48:29 +0300

    net/tap: Added TUNSETSTEERINGEBPF code.
    Additional code that will be used for eBPF setting steering routine.
    
    Signed-off-by: Andrew Melnychenko <andrew@daynix.com>
    Signed-off-by: Jason Wang <jasowang@redhat.com>

--- net/tap-linux.h
+++ net/tap-linux.h
@@ -31,6 +31,7 @@
 #define TUNSETQUEUE  _IOW('T', 217, int)
 #define TUNSETVNETLE _IOW('T', 220, int)
 #define TUNSETVNETBE _IOW('T', 222, int)
+#define TUNSETSTEERINGEBPF _IOR('T', 224, int)
 
 #endif
 
