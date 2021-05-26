7be3bf6c8429969f97728bb712d9a99997835607
Nicholas Piggin <npiggin@gmail.com>
2021-05-26 19:16:24 +1000

    spapr: Remove stale comment about power-saving LPCR bits
    Commit 47a9b551547 ("spapr: Clean up handling of LPCR power-saving exit
    bits") moved this logic but did not remove the comment from the
    previous location.
    
    Signed-off-by: Nicholas Piggin <npiggin@gmail.com>
    Message-Id: <20210526091626.3388262-2-npiggin@gmail.com>
    Reviewed-by: Cédric Le Goater <clg@kaod.org>
    Reviewed-by: Greg Kurz <groug@kaod.org>
    Signed-off-by: David Gibson <david@gibson.dropbear.id.au>

--- hw/ppc/spapr_rtas.c
+++ hw/ppc/spapr_rtas.c
@@ -164,7 +164,6 @@ static void rtas_start_cpu(PowerPCCPU *callcpu, SpaprMachineState *spapr,
     env->msr = (1ULL << MSR_SF) | (1ULL << MSR_ME);
     hreg_compute_hflags(env);
 
-    /* Enable Power-saving mode Exit Cause exceptions for the new CPU */
     lpcr = env->spr[SPR_LPCR];
     if (!pcc->interrupts_big_endian(callcpu)) {
         lpcr |= LPCR_ILE;
