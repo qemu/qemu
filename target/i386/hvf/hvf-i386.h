861457ce73bc9e0e56d866b5c06e2c745fe53448
Alexander Graf <agraf@csgraf.de>
2021-06-03 14:09:31 +0100

    hvf: Move hvf internal definitions into common header
    Until now, Hypervisor.framework has only been available on x86_64 systems.
    With Apple Silicon shipping now, it extends its reach to aarch64. To
    prepare for support for multiple architectures, let's start moving common
    code out into its own accel directory.
    
    This patch moves a few internal struct and constant defines over.
    
    Signed-off-by: Alexander Graf <agraf@csgraf.de>
    Reviewed-by: Sergio Lopez <slp@redhat.com>
    Message-id: 20210519202253.76782-5-agraf@csgraf.de
    Reviewed-by: Peter Maydell <peter.maydell@linaro.org>
    Signed-off-by: Peter Maydell <peter.maydell@linaro.org>

--- target/i386/hvf/hvf-i386.h
+++ target/i386/hvf/hvf-i386.h
@@ -18,38 +18,9 @@
 
 #include "qemu/accel.h"
 #include "sysemu/hvf.h"
+#include "sysemu/hvf_int.h"
 #include "cpu.h"
 #include "x86.h"
-
-/* hvf_slot flags */
-#define HVF_SLOT_LOG (1 << 0)
-
-typedef struct hvf_slot {
-    uint64_t start;
-    uint64_t size;
-    uint8_t *mem;
-    int slot_id;
-    uint32_t flags;
-    MemoryRegion *region;
-} hvf_slot;
-
-typedef struct hvf_vcpu_caps {
-    uint64_t vmx_cap_pinbased;
-    uint64_t vmx_cap_procbased;
-    uint64_t vmx_cap_procbased2;
-    uint64_t vmx_cap_entry;
-    uint64_t vmx_cap_exit;
-    uint64_t vmx_cap_preemption_timer;
-} hvf_vcpu_caps;
-
-struct HVFState {
-    AccelState parent;
-    hvf_slot slots[32];
-    int num_slots;
-
-    hvf_vcpu_caps *hvf_caps;
-};
-extern HVFState *hvf_state;
 
 void hvf_handle_io(CPUArchState *, uint16_t, void *, int, int, int);
 
