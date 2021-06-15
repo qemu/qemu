e542b71805dc4b7827e6c2f00a1170a61843345e
Stefan Berger <stefanb@linux.ibm.com>
2021-06-15 16:21:20 +0200

    sysemu: Make TPM structures inaccessible if CONFIG_TPM is not defined
    Signed-off-by: Stefan Berger <stefanb@linux.ibm.com>
    Reviewed-by: Philippe Mathieu-Daudé <philmd@redhat.com>
    Tested-by: Philippe Mathieu-Daudé <philmd@redhat.com>
    Message-Id: <20210614191335.1968807-5-stefanb@linux.ibm.com>
    [PMD: Remove tpm_init() / tpm_cleanup() stubs]
    Signed-off-by: Philippe Mathieu-Daudé <philmd@redhat.com>

--- stubs/tpm.c
+++ stubs/tpm.c
@@ -7,17 +7,7 @@
 
 #include "qemu/osdep.h"
 #include "qapi/qapi-commands-tpm.h"
-#include "sysemu/tpm.h"
 #include "hw/acpi/tpm.h"
-
-int tpm_init(void)
-{
-    return 0;
-}
-
-void tpm_cleanup(void)
-{
-}
 
 TPMInfoList *qmp_query_tpm(Error **errp)
 {
