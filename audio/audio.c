0c29b786e6b5276d43be2be255a8323c628ec790
Akihiko Odaki <akihiko.odaki@gmail.com>
2021-06-16 23:14:11 +0900

    audio: Fix format specifications of debug logs
    Signed-off-by: Akihiko Odaki <akihiko.odaki@gmail.com>
    Message-id: 20210616141411.53892-1-akihiko.odaki@gmail.com
    Message-Id: <20210616141411.53892-1-akihiko.odaki@gmail.com>
    Signed-off-by: Gerd Hoffmann <kraxel@redhat.com>

--- audio/audio.c
+++ audio/audio.c
@@ -705,7 +705,7 @@ static size_t audio_pcm_sw_write(SWVoiceOut *sw, void *buf, size_t size)
 
     if (live == hwsamples) {
 #ifdef DEBUG_OUT
-        dolog ("%s is full %d\n", sw->name, live);
+        dolog ("%s is full %zu\n", sw->name, live);
 #endif
         return 0;
     }
@@ -995,7 +995,7 @@ static size_t audio_get_avail (SWVoiceIn *sw)
     }
 
     ldebug (
-        "%s: get_avail live %d ret %" PRId64 "\n",
+        "%s: get_avail live %zu ret %" PRId64 "\n",
         SW_NAME (sw),
         live, (((int64_t) live << 32) / sw->ratio) * sw->info.bytes_per_frame
         );
@@ -1022,7 +1022,7 @@ static size_t audio_get_free(SWVoiceOut *sw)
     dead = sw->hw->mix_buf->size - live;
 
 #ifdef DEBUG_OUT
-    dolog ("%s: get_free live %d dead %d ret %" PRId64 "\n",
+    dolog ("%s: get_free live %zu dead %zu ret %" PRId64 "\n",
            SW_NAME (sw),
            live, dead, (((int64_t) dead << 32) / sw->ratio) *
            sw->info.bytes_per_frame);
