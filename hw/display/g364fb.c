8660df5ea25ea4e6ee94fca43559165fe7610199
Mark Cave-Ayland <mark.cave-ayland@ilande.co.uk>
2021-06-25 17:35:54 +0100

    g364fb: add VMStateDescription for G364SysBusState
    Currently when QEMU attempts to migrate the MIPS magnum machine it crashes due
    to a mistake in the g364fb VMStateDescription configuration which expects a
    G364SysBusState and not a G364State.
    
    Resolve the issue by adding a new VMStateDescription for G364SysBusState and
    embedding the existing vmstate_g364fb VMStateDescription inside it using
    VMSTATE_STRUCT.
    
    Signed-off-by: Mark Cave-Ayland <mark.cave-ayland@ilande.co.uk>
    Fixes: 97a3f6ffbba ("g364fb: convert to qdev")
    Reviewed-by: Philippe Mathieu-Daudé <f4bug@amsat.org>
    Message-Id: <20210625163554.14879-3-mark.cave-ayland@ilande.co.uk>
    Signed-off-by: Philippe Mathieu-Daudé <f4bug@amsat.org>

--- hw/display/g364fb.c
+++ hw/display/g364fb.c
@@ -517,6 +517,16 @@ static Property g364fb_sysbus_properties[] = {
     DEFINE_PROP_END_OF_LIST(),
 };
 
+static const VMStateDescription vmstate_g364fb_sysbus = {
+    .name = "g364fb-sysbus",
+    .version_id = 2,
+    .minimum_version_id = 2,
+    .fields = (VMStateField[]) {
+        VMSTATE_STRUCT(g364, G364SysBusState, 2, vmstate_g364fb, G364State),
+        VMSTATE_END_OF_LIST()
+    }
+};
+
 static void g364fb_sysbus_class_init(ObjectClass *klass, void *data)
 {
     DeviceClass *dc = DEVICE_CLASS(klass);
@@ -525,7 +535,7 @@ static void g364fb_sysbus_class_init(ObjectClass *klass, void *data)
     set_bit(DEVICE_CATEGORY_DISPLAY, dc->categories);
     dc->desc = "G364 framebuffer";
     dc->reset = g364fb_sysbus_reset;
-    dc->vmsd = &vmstate_g364fb;
+    dc->vmsd = &vmstate_g364fb_sysbus;
     device_class_set_props(dc, g364fb_sysbus_properties);
 }
 
