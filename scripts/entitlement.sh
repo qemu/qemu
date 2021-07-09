411ad8dd80077e98ed465775b044caf1a9482f6c
Akihiko Odaki <akihiko.odaki@gmail.com>
2021-07-09 10:25:33 +0900

    meson: Use input/output for entitlements target
    input/output parameters respect dependencies.
    
    Signed-off-by: Akihiko Odaki <akihiko.odaki@gmail.com>
    Message-Id: <20210709012533.58262-1-akihiko.odaki@gmail.com>
    Signed-off-by: Paolo Bonzini <pbonzini@redhat.com>

--- scripts/entitlement.sh
+++ scripts/entitlement.sh
@@ -8,10 +8,10 @@ if [ "$1" = --install ]; then
   in_place=false
 fi
 
-SRC="$1"
-DST="$2"
-ENTITLEMENT="$3"
-ICON="$4"
+DST="$1"
+SRC="$2"
+ICON="$3"
+ENTITLEMENT="$4"
 
 if $in_place; then
   trap 'rm "$DST.tmp"' exit
@@ -21,7 +21,7 @@ else
   cd "$MESON_INSTALL_DESTDIR_PREFIX"
 fi
 
-if test "$ENTITLEMENT" != '/dev/null'; then
+if test -n "$ENTITLEMENT"; then
   codesign --entitlements "$ENTITLEMENT" --force -s - "$SRC"
 fi
 
