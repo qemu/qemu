fe4bd9170c92e5557f9c49ac08acc54c0d752aba
Richard Henderson <richard.henderson@linaro.org>
2021-07-12 18:43:58 +0000

    net/checksum: Remove unused variable in net_checksum_add_iov
    From clang-13:
    ../qemu/net/checksum.c:189:23: error: variable 'buf_off' set but not used \
        [-Werror,-Wunused-but-set-variable]
    
    Reviewed-by: Eric Blake <eblake@redhat.com>
    Signed-off-by: Richard Henderson <richard.henderson@linaro.org>

--- net/checksum.c
+++ net/checksum.c
@@ -186,12 +186,11 @@ uint32_t
 net_checksum_add_iov(const struct iovec *iov, const unsigned int iov_cnt,
                      uint32_t iov_off, uint32_t size, uint32_t csum_offset)
 {
-    size_t iovec_off, buf_off;
+    size_t iovec_off;
     unsigned int i;
     uint32_t res = 0;
 
     iovec_off = 0;
-    buf_off = 0;
     for (i = 0; i < iov_cnt && size; i++) {
         if (iov_off < (iovec_off + iov[i].iov_len)) {
             size_t len = MIN((iovec_off + iov[i].iov_len) - iov_off , size);
@@ -200,7 +199,6 @@ net_checksum_add_iov(const struct iovec *iov, const unsigned int iov_cnt,
             res += net_checksum_add_cont(len, chunk_buf, csum_offset);
             csum_offset += len;
 
-            buf_off += len;
             iov_off += len;
             size -= len;
         }
