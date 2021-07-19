1ed1ccc5a429b3fb35d9756f911eb652a6ab00c5
Sean Christopherson <sean.j.christopherson@intel.com>
2021-07-19 19:21:21 +0800

    i386/pc: Add e820 entry for SGX EPC section(s)
    Note that SGX EPC is currently guaranteed to reside in a single
    contiguous chunk of memory regardless of the number of EPC sections.
    
    Signed-off-by: Sean Christopherson <sean.j.christopherson@intel.com>
    Signed-off-by: Yang Zhong <yang.zhong@intel.com>
    Message-Id: <20210719112136.57018-19-yang.zhong@intel.com>
    Signed-off-by: Paolo Bonzini <pbonzini@redhat.com>

--- hw/i386/pc.c
+++ hw/i386/pc.c
@@ -889,6 +889,10 @@ void pc_memory_init(PCMachineState *pcms,
         e820_add_entry(0x100000000ULL, x86ms->above_4g_mem_size, E820_RAM);
     }
 
+    if (pcms->sgx_epc.size != 0) {
+        e820_add_entry(pcms->sgx_epc.base, pcms->sgx_epc.size, E820_RESERVED);
+    }
+
     if (!pcmc->has_reserved_memory &&
         (machine->ram_slots ||
          (machine->maxram_size > machine->ram_size))) {
