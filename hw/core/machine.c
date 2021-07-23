a1d7e475beb5c9e7a8e1213f29b0d20a208a9ade
Christina Wang <christina.wang@windriver.com>
2021-07-23 15:55:10 +0800

    hw/net: e1000: Correct the initial value of VET register
    The initial value of VLAN Ether Type (VET) register is 0x8100, as per
    the manual and real hardware.
    
    While Linux e1000 driver always writes VET register to 0x8100, it is
    not always the case for everyone. Drivers relying on the reset value
    of VET won't be able to transmit and receive VLAN frames in QEMU.
    
    Reported-by: Markus Carlstedt <markus.carlstedt@windriver.com>
    Signed-off-by: Christina Wang <christina.wang@windriver.com>
    Signed-off-by: Bin Meng <bin.meng@windriver.com>
    Signed-off-by: Jason Wang <jasowang@redhat.com>

--- hw/core/machine.c
+++ hw/core/machine.c
@@ -41,6 +41,7 @@ GlobalProperty hw_compat_6_0[] = {
     { "gpex-pcihost", "allow-unmapped-accesses", "false" },
     { "i8042", "extended-state", "false"},
     { "nvme-ns", "eui64-default", "off"},
+    { "e1000", "init-vet", "off" },
 };
 const size_t hw_compat_6_0_len = G_N_ELEMENTS(hw_compat_6_0);
 
