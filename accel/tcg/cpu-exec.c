df3a2de51a07089a4a729fe1f792f658df9dade4
Peter Maydell <peter.maydell@linaro.org>
2021-07-25 18:44:04 +0100

    accel/tcg: Don't use CF_COUNT_MASK as the max value of icount_decr.u16.low
    In cpu_loop_exec_tb() we were bounding the number of insns we might
    try to execute in a TB using CF_COUNT_MASK.  This is incorrect,
    because we can validly put up to 0xffff into icount_decr.u16.low.  In
    particular, since commit 78ff82bb1b67c0d7 reduced CF_COUNT_MASK to
    511 this meant that we would incorrectly only try to execute 511
    instructions in a 512-instruction TB, which could result in QEMU
    hanging when in icount mode.
    
    Use the actual maximum value, which is 0xffff. (This brings this code
    in to line with the similar logic in icount_prepare_for_run() in
    tcg-accel-ops-icount.c.)
    
    Fixes: 78ff82bb1b67c0d7
    Signed-off-by: Peter Maydell <peter.maydell@linaro.org>
    Reviewed-by: Richard Henderson <richard.henderson@linaro.org>
    Reviewed-by: Philippe Mathieu-Daudé <f4bug@amsat.org>
    Resolves: https://gitlab.com/qemu-project/qemu/-/issues/499
    Message-Id: <20210725174405.24568-2-peter.maydell@linaro.org>
    Signed-off-by: Richard Henderson <richard.henderson@linaro.org>

--- accel/tcg/cpu-exec.c
+++ accel/tcg/cpu-exec.c
@@ -834,7 +834,7 @@ static inline void cpu_loop_exec_tb(CPUState *cpu, TranslationBlock *tb,
     /* Ensure global icount has gone forward */
     icount_update(cpu);
     /* Refill decrementer and continue execution.  */
-    insns_left = MIN(CF_COUNT_MASK, cpu->icount_budget);
+    insns_left = MIN(0xffff, cpu->icount_budget);
     cpu_neg(cpu)->icount_decr.u16.low = insns_left;
     cpu->icount_extra = cpu->icount_budget - insns_left;
 
