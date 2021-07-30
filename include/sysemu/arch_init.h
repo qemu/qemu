3669282cde01809edac8c91f61da50da2a83b067
Peter Maydell <peter.maydell@linaro.org>
2021-07-30 11:59:45 +0100

    arch_init.h: Move QEMU_ARCH_VIRTIO_* to qdev-monitor.c
    The QEMU_ARCH_VIRTIO_* defines are used only in one file,
    qdev-monitor.c. Move them to that file.
    
    Signed-off-by: Peter Maydell <peter.maydell@linaro.org>
    Reviewed-by: Richard Henderson <richard.henderson@linaro.org>
    Reviewed-by: Markus Armbruster <armbru@redhat.com>
    Message-id: 20210730105947.28215-7-peter.maydell@linaro.org

--- include/sysemu/arch_init.h
+++ include/sysemu/arch_init.h
@@ -30,13 +30,4 @@ enum {
 
 extern const uint32_t arch_type;
 
-/* default virtio transport per architecture */
-#define QEMU_ARCH_VIRTIO_PCI (QEMU_ARCH_ALPHA | QEMU_ARCH_ARM | \
-                              QEMU_ARCH_HPPA | QEMU_ARCH_I386 | \
-                              QEMU_ARCH_MIPS | QEMU_ARCH_PPC |  \
-                              QEMU_ARCH_RISCV | QEMU_ARCH_SH4 | \
-                              QEMU_ARCH_SPARC | QEMU_ARCH_XTENSA)
-#define QEMU_ARCH_VIRTIO_CCW (QEMU_ARCH_S390X)
-#define QEMU_ARCH_VIRTIO_MMIO (QEMU_ARCH_M68K)
-
 #endif
