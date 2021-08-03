62a4db5522cbbd8b5309a2949c22f00e5b0138e3
Michael S. Tsirkin <mst@redhat.com>
2021-08-03 16:20:22 -0400

    Drop _DSM 5 from expected DSDTs on ARM
    diff -rup /tmp/old/tests/data/acpi/microvm/DSDT.pcie.dsl /tmp/new/tests/data/acpi/microvm/DSDT.pcie.dsl
    --- /tmp/old/tests/data/acpi/microvm/DSDT.pcie.dsl	2021-08-03 16:22:52.289295442 -0400
    +++ /tmp/new/tests/data/acpi/microvm/DSDT.pcie.dsl	2021-08-03 16:22:40.102286317 -0400
    @@ -1302,14 +1302,9 @@ DefinitionBlock ("", "DSDT", 2, "BOCHS "
                         {
                             Return (Buffer (One)
                             {
    -                             0x21                                             // !
    +                             0x01                                             // .
                             })
                         }
    -
    -                    If ((Arg2 == 0x05))
    -                    {
    -                        Return (Zero)
    -                    }
                     }
    
                     Return (Buffer (One)
    
    Signed-off-by: Michael S. Tsirkin <mst@redhat.com>

--- tests/qtest/bios-tables-test-allowed-diff.h
+++ tests/qtest/bios-tables-test-allowed-diff.h
@@ -1,6 +1,1 @@
 /* List of comma-separated changed AML files to ignore */
-"tests/data/acpi/virt/DSDT",
-"tests/data/acpi/virt/DSDT.memhp",
-"tests/data/acpi/virt/DSDT.numamem",
-"tests/data/acpi/virt/DSDT.pxb",
-"tests/data/acpi/microvm/DSDT.pcie",
