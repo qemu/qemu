1c4c68593610e81fc3ba8d3919ec3fc9f26e063d
David Hildenbrand <david@redhat.com>
2021-08-05 11:23:50 +0200

    softmmu/physmem: fix wrong assertion in qemu_ram_alloc_internal()
    When adding RAM_NORESERVE, we forgot to remove the old assertion when
    adding the updated one, most probably when reworking the patches or
    rebasing. We can easily crash QEMU by adding
      -object memory-backend-ram,id=mem0,size=500G,reserve=off
    to the QEMU cmdline:
      qemu-system-x86_64: ../softmmu/physmem.c:2146: qemu_ram_alloc_internal:
      Assertion `(ram_flags & ~(RAM_SHARED | RAM_RESIZEABLE | RAM_PREALLOC))
      == 0' failed.
    
    Fix it by removing the old assertion.
    
    Fixes: 8dbe22c6868b ("memory: Introduce RAM_NORESERVE and wire it up in qemu_ram_mmap()")
    Reviewed-by: Peter Xu <peterx@redhat.com>
    Reviewed-by: Philippe Mathieu-Daudé <philmd@redhat.com>
    Tested-by: Philippe Mathieu-Daudé <philmd@redhat.com>
    Reviewed-by: Pankaj Gupta <pankaj.gupta@ionos.com>
    Signed-off-by: David Hildenbrand <david@redhat.com>
    Message-id: 20210805092350.31195-1-david@redhat.com
    Cc: Paolo Bonzini <pbonzini@redhat.com>
    Cc: Peter Xu <peterx@redhat.com>
    Cc: Philippe Mathieu-Daudé <philmd@redhat.com>
    Signed-off-by: David Hildenbrand <david@redhat.com>
    Signed-off-by: Peter Maydell <peter.maydell@linaro.org>

--- softmmu/physmem.c
+++ softmmu/physmem.c
@@ -2143,7 +2143,6 @@ RAMBlock *qemu_ram_alloc_internal(ram_addr_t size, ram_addr_t max_size,
     RAMBlock *new_block;
     Error *local_err = NULL;
 
-    assert((ram_flags & ~(RAM_SHARED | RAM_RESIZEABLE | RAM_PREALLOC)) == 0);
     assert((ram_flags & ~(RAM_SHARED | RAM_RESIZEABLE | RAM_PREALLOC |
                           RAM_NORESERVE)) == 0);
     assert(!host ^ (ram_flags & RAM_PREALLOC));
