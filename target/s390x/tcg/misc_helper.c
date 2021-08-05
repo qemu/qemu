6b01606f0e35827fb7b608b9e56e63ed4b88a0a7
David Hildenbrand <david@redhat.com>
2021-08-05 14:59:38 +0200

    s390x/tcg: fix and optimize SPX (SET PREFIX)
    We not only invalidate the translation of the range 0x0-0x2000, we also
    invalidate the translation of the new prefix range and the translation
    of the old prefix range -- because real2abs would return different
    results for all of these ranges when changing the prefix location.
    
    This fixes the kvm-unit-tests "edat" test that just hangs before this
    patch because we end up clearing the new prefix area instead of the old
    prefix area.
    
    While at it, let's not do anything in case the prefix doesn't change.
    
    Cc: Richard Henderson <richard.henderson@linaro.org>
    Cc: David Hildenbrand <david@redhat.com>
    Cc: Cornelia Huck <cohuck@redhat.com>
    Cc: Thomas Huth <thuth@redhat.com>
    Cc: Claudio Imbrenda <imbrenda@linux.ibm.com>
    Cc: qemu-s390x@nongnu.org
    Signed-off-by: David Hildenbrand <david@redhat.com>
    Reviewed-by: Thomas Huth <thuth@redhat.com>
    Reviewed-by: Claudio Imbrenda <imbrenda@linux.ibm.com>
    Message-Id: <20210805125938.74034-1-david@redhat.com>
    Signed-off-by: Cornelia Huck <cohuck@redhat.com>
    Signed-off-by: Thomas Huth <thuth@redhat.com>

--- target/s390x/tcg/misc_helper.c
+++ target/s390x/tcg/misc_helper.c
@@ -151,13 +151,26 @@ void HELPER(diag)(CPUS390XState *env, uint32_t r1, uint32_t r3, uint32_t num)
 /* Set Prefix */
 void HELPER(spx)(CPUS390XState *env, uint64_t a1)
 {
+    const uint32_t prefix = a1 & 0x7fffe000;
+    const uint32_t old_prefix = env->psa;
     CPUState *cs = env_cpu(env);
-    uint32_t prefix = a1 & 0x7fffe000;
+
+    if (prefix == old_prefix) {
+        return;
+    }
 
     env->psa = prefix;
     HELPER_LOG("prefix: %#x\n", prefix);
     tlb_flush_page(cs, 0);
     tlb_flush_page(cs, TARGET_PAGE_SIZE);
+    if (prefix != 0) {
+        tlb_flush_page(cs, prefix);
+        tlb_flush_page(cs, prefix + TARGET_PAGE_SIZE);
+    }
+    if (old_prefix != 0) {
+        tlb_flush_page(cs, old_prefix);
+        tlb_flush_page(cs, old_prefix + TARGET_PAGE_SIZE);
+    }
 }
 
 static void update_ckc_timer(CPUS390XState *env)
