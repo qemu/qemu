b116d6c319661b59d0461f771f407e47f54a8c80
Philippe Mathieu-Daudé <philmd@redhat.com>
2021-08-26 19:26:57 +0200

    hw/virtio: Remove NULL check in virtio_free_region_cache()
    virtio_free_region_cache() is called within call_rcu(),
    always with a non-NULL argument. Ensure new code keep it
    that way by replacing the NULL check by an assertion.
    Add a comment this function is called within call_rcu().
    
    Signed-off-by: Philippe Mathieu-Daudé <philmd@redhat.com>
    Message-Id: <20210826172658.2116840-3-philmd@redhat.com>
    Reviewed-by: Michael S. Tsirkin <mst@redhat.com>
    Signed-off-by: Michael S. Tsirkin <mst@redhat.com>
    Reviewed-by: Stefano Garzarella <sgarzare@redhat.com>
    Reviewed-by: Stefan Hajnoczi <stefanha@redhat.com>

--- hw/virtio/virtio.c
+++ hw/virtio/virtio.c
@@ -133,12 +133,10 @@ struct VirtQueue
     QLIST_ENTRY(VirtQueue) node;
 };
 
+/* Called within call_rcu().  */
 static void virtio_free_region_cache(VRingMemoryRegionCaches *caches)
 {
-    if (!caches) {
-        return;
-    }
-
+    assert(caches != NULL);
     address_space_cache_destroy(&caches->desc);
     address_space_cache_destroy(&caches->avail);
     address_space_cache_destroy(&caches->used);
