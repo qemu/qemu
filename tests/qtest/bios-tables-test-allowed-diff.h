c306cdb0cc5327f336fbf9a7411f319f0717ee75
Igor Mammedov <imammedo@redhat.com>
2021-09-02 07:35:39 -0400

    tests: acpi: whitelist expected tables for acpi/q35/xapic testcase
    Signed-off-by: Igor Mammedov <imammedo@redhat.com>
    Message-Id: <20210902113551.461632-4-imammedo@redhat.com>
    Reviewed-by: Michael S. Tsirkin <mst@redhat.com>
    Signed-off-by: Michael S. Tsirkin <mst@redhat.com>

--- tests/qtest/bios-tables-test-allowed-diff.h
+++ tests/qtest/bios-tables-test-allowed-diff.h
@@ -1,1 +1,5 @@
 /* List of comma-separated changed AML files to ignore */
+"tests/data/acpi/q35/DSDT.xapic",
+"tests/data/acpi/q35/SRAT.xapic",
+"tests/data/acpi/q35/FACP.xapic",
+"tests/data/acpi/q35/APIC.xapic",
