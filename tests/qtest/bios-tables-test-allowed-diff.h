d7830a9bdd9a2f298b0e38aeff1aef2614295402
Shashi Mallela <shashi.mallela@linaro.org>
2021-09-13 16:07:24 +0100

    tests/data/acpi/virt: Add IORT files for ITS
    Added expected IORT files applicable with latest GICv3
    ITS changes.Temporarily differences in these files are
    okay.
    
    Signed-off-by: Shashi Mallela <shashi.mallela@linaro.org>
    Acked-by: Igor Mammedov <imammedo@redhat.com>
    Reviewed-by: Peter Maydell <peter.maydell@linaro.org>
    Message-id: 20210910143951.92242-8-shashi.mallela@linaro.org
    Signed-off-by: Peter Maydell <peter.maydell@linaro.org>

--- tests/qtest/bios-tables-test-allowed-diff.h
+++ tests/qtest/bios-tables-test-allowed-diff.h
@@ -1,1 +1,5 @@
 /* List of comma-separated changed AML files to ignore */
+"tests/data/acpi/virt/IORT",
+"tests/data/acpi/virt/IORT.memhp",
+"tests/data/acpi/virt/IORT.numamem",
+"tests/data/acpi/virt/IORT.pxb",
