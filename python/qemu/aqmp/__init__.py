a093a65567c797ab0a53e8a6962043679e3ed2f2
John Snow <jsnow@redhat.com>
2021-09-15 12:29:29 -0400

    python/aqmp: add asynchronous QMP (AQMP) subpackage
    For now, it's empty! Soon, it won't be.
    
    Signed-off-by: John Snow <jsnow@redhat.com>
    Reviewed-by: Eric Blake <eblake@redhat.com>
    Message-id: 20210915162955.333025-2-jsnow@redhat.com
    Signed-off-by: John Snow <jsnow@redhat.com>

---
+++ python/qemu/aqmp/__init__.py
@@ -0,0 +1,27 @@
+"""
+QEMU Monitor Protocol (QMP) development library & tooling.
+
+This package provides a fairly low-level class for communicating
+asynchronously with QMP protocol servers, as implemented by QEMU, the
+QEMU Guest Agent, and the QEMU Storage Daemon.
+
+`QMPClient` provides the main functionality of this package. All errors
+raised by this library dervive from `AQMPError`, see `aqmp.error` for
+additional detail. See `aqmp.events` for an in-depth tutorial on
+managing QMP events.
+"""
+
+# Copyright (C) 2020, 2021 John Snow for Red Hat, Inc.
+#
+# Authors:
+#  John Snow <jsnow@redhat.com>
+#
+# Based on earlier work by Luiz Capitulino <lcapitulino@redhat.com>.
+#
+# This work is licensed under the terms of the GNU GPL, version 2.  See
+# the COPYING file in the top-level directory.
+
+
+# The order of these fields impact the Sphinx documentation order.
+__all__ = (
+)
