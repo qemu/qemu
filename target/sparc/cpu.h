9ac200acce8c27ef44da31246f337a2454e54e0d
Philippe Mathieu-Daudé <f4bug@amsat.org>
2021-09-16 10:40:02 +0200

    target/sparc: Make sparc_cpu_dump_state() static
    The sparc_cpu_dump_state() function is only called within
    the same file. Make it static.
    
    Signed-off-by: Philippe Mathieu-Daudé <f4bug@amsat.org>
    Reviewed-by: Laurent Vivier <laurent@vivier.eu>
    Reviewed-by: Mark Cave-Ayland <mark.cave-ayland@ilande.co.uk>
    Message-Id: <20210916084002.1918445-1-f4bug@amsat.org>
    Signed-off-by: Laurent Vivier <laurent@vivier.eu>

--- target/sparc/cpu.h
+++ target/sparc/cpu.h
@@ -571,7 +571,6 @@ extern const VMStateDescription vmstate_sparc_cpu;
 #endif
 
 void sparc_cpu_do_interrupt(CPUState *cpu);
-void sparc_cpu_dump_state(CPUState *cpu, FILE *f, int flags);
 hwaddr sparc_cpu_get_phys_page_debug(CPUState *cpu, vaddr addr);
 int sparc_cpu_gdb_read_register(CPUState *cpu, GByteArray *buf, int reg);
 int sparc_cpu_gdb_write_register(CPUState *cpu, uint8_t *buf, int reg);
