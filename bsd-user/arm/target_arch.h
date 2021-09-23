8c98705bb933f58bed743977ee897f67a27d449f
Warner Losh <imp@bsdimp.com>
2021-09-23 08:32:06 -0600

    bsd-user/arm/target_arch_cpu.c: Target specific TLS routines
    Target specific TLS routines to get and set the TLS values.
    
    Signed-off-by: Kyle Evans <kevans@FreeBSD.org>
    Signed-off-by: Stacey Son <sson@FreeBSD.org>
    Signed-off-by: Warner Losh <imp@bsdimp.com>
    Reviewed-by: Kyle Evans <kevans@FreeBSD.org>
    Reviewed-by: Richard Henderson <richard.henderson@linaro.org>

---
+++ bsd-user/arm/target_arch.h
@@ -0,0 +1,28 @@
+/*
+ * ARM 32-bit specific prototypes for bsd-user
+ *
+ *  Copyright (c) 2013 Stacey D. Son
+ *
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program; if not, see <http://www.gnu.org/licenses/>.
+ */
+
+#ifndef _TARGET_ARCH_H_
+#define _TARGET_ARCH_H_
+
+#include "qemu.h"
+
+void target_cpu_set_tls(CPUARMState *env, target_ulong newtls);
+target_ulong target_cpu_get_tls(CPUARMState *env);
+
+#endif /* !_TARGET_ARCH_H_ */
