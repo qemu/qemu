c5f0a81650ada7178865f74dd4b3c9e508b3018e
David Gibson <david@gibson.dropbear.id.au>
2021-09-27 14:48:02 +1000

    qemu: Split machine_ppc.py acceptance tests
    machine_ppc.py contains tests for 3 different ppc based machine types.  It
    is listed in MAINTAINERS along with the PPC TCG cpu code.  That's not
    really accurate though, since it's really more about testing those machines
    than the CPUs.
    
    Therefore, split it up into separate files for the separate machine types,
    and list those along with their machine types in MAINTAINERS.
    
    Suggested-by: Philippe Mathieu-Daudé <f4bug@amsat.org>
    Signed-off-by: David Gibson <david@gibson.dropbear.id.au>
    Reviewed-by: Philippe Mathieu-Daudé <f4bug@amsat.org>
    Signed-off-by: Philippe Mathieu-Daudé <philmd@redhat.com>
    Message-Id: <20210927044808.73391-2-david@gibson.dropbear.id.au>

---
+++ tests/acceptance/ppc_virtex_ml507.py
@@ -0,0 +1,34 @@
+# Test that Linux kernel boots on ppc machines and check the console
+#
+# Copyright (c) 2018, 2020 Red Hat, Inc.
+#
+# This work is licensed under the terms of the GNU GPL, version 2 or
+# later.  See the COPYING file in the top-level directory.
+
+from avocado.utils import archive
+from avocado_qemu import Test
+from avocado_qemu import wait_for_console_pattern
+
+class VirtexMl507Machine(Test):
+
+    timeout = 90
+    KERNEL_COMMON_COMMAND_LINE = 'printk.time=0 '
+    panic_message = 'Kernel panic - not syncing'
+
+    def test_ppc_virtex_ml507(self):
+        """
+        :avocado: tags=arch:ppc
+        :avocado: tags=machine:virtex-ml507
+        """
+        tar_url = ('https://www.qemu-advent-calendar.org'
+                   '/2020/download/hippo.tar.gz')
+        tar_hash = '306b95bfe7d147f125aa176a877e266db8ef914a'
+        file_path = self.fetch_asset(tar_url, asset_hash=tar_hash)
+        archive.extract(file_path, self.workdir)
+        self.vm.set_console()
+        self.vm.add_args('-kernel', self.workdir + '/hippo/hippo.linux',
+                         '-dtb', self.workdir + '/hippo/virtex440-ml507.dtb',
+                         '-m', '512')
+        self.vm.launch()
+        wait_for_console_pattern(self, 'QEMU advent calendar 2020',
+                                 self.panic_message)
