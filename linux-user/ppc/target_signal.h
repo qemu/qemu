c790e4ebfed8174f65d685f5a4f3262873c237a5
Richard Henderson <richard.henderson@linaro.org>
2021-09-29 09:05:46 -0400

    linux-user/ppc: Implement setup_sigtramp
    Create and record the two signal trampolines.
    
    Cc: qemu-ppc@nongnu.org
    Reviewed-by: Peter Maydell <peter.maydell@linaro.org>
    Signed-off-by: Richard Henderson <richard.henderson@linaro.org>
    Message-Id: <20210929130553.121567-20-richard.henderson@linaro.org>
    Signed-off-by: Laurent Vivier <laurent@vivier.eu>

--- linux-user/ppc/target_signal.h
+++ linux-user/ppc/target_signal.h
@@ -24,4 +24,6 @@ typedef struct target_sigaltstack {
 #if !defined(TARGET_PPC64)
 #define TARGET_ARCH_HAS_SETUP_FRAME
 #endif
+#define TARGET_ARCH_HAS_SIGTRAMP_PAGE 1
+
 #endif /* PPC_TARGET_SIGNAL_H */
