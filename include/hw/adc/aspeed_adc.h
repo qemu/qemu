5857974d5d1133455e3c33e7c740786722418588
Andrew Jeffery <andrew@aj.id.au>
2021-10-12 08:20:08 +0200

    hw/adc: Add basic Aspeed ADC model
    This model implements enough behaviour to do basic functionality tests
    such as device initialisation and read out of dummy sample values. The
    sample value generation strategy is similar to the STM ADC already in
    the tree.
    
    Signed-off-by: Andrew Jeffery <andrew@aj.id.au>
    [clg : support for multiple engines (AST2600) ]
    Signed-off-by: Cédric Le Goater <clg@kaod.org>
    [pdel : refactored engine register struct fields to regs[] array field]
    [pdel : added guest-error checking for upper-8 channel regs in AST2600]
    [pdel : allow 16-bit reads of the channel data registers]
    Signed-off-by: Peter Delevoryas <pdel@fb.com>
    Message-Id: <20211005052604.1674891-2-pdel@fb.com>
    Signed-off-by: Cédric Le Goater <clg@kaod.org>

---
+++ include/hw/adc/aspeed_adc.h
@@ -0,0 +1,55 @@
+/*
+ * Aspeed ADC
+ *
+ * Copyright 2017-2021 IBM Corp.
+ *
+ * Andrew Jeffery <andrew@aj.id.au>
+ *
+ * SPDX-License-Identifier: GPL-2.0-or-later
+ */
+
+#ifndef HW_ADC_ASPEED_ADC_H
+#define HW_ADC_ASPEED_ADC_H
+
+#include "hw/sysbus.h"
+
+#define TYPE_ASPEED_ADC "aspeed.adc"
+#define TYPE_ASPEED_2400_ADC TYPE_ASPEED_ADC "-ast2400"
+#define TYPE_ASPEED_2500_ADC TYPE_ASPEED_ADC "-ast2500"
+#define TYPE_ASPEED_2600_ADC TYPE_ASPEED_ADC "-ast2600"
+OBJECT_DECLARE_TYPE(AspeedADCState, AspeedADCClass, ASPEED_ADC)
+
+#define TYPE_ASPEED_ADC_ENGINE "aspeed.adc.engine"
+OBJECT_DECLARE_SIMPLE_TYPE(AspeedADCEngineState, ASPEED_ADC_ENGINE)
+
+#define ASPEED_ADC_NR_CHANNELS 16
+#define ASPEED_ADC_NR_REGS     (0xD0 >> 2)
+
+struct AspeedADCEngineState {
+    /* <private> */
+    SysBusDevice parent;
+
+    MemoryRegion mmio;
+    qemu_irq irq;
+    uint32_t engine_id;
+    uint32_t nr_channels;
+    uint32_t regs[ASPEED_ADC_NR_REGS];
+};
+
+struct AspeedADCState {
+    /* <private> */
+    SysBusDevice parent;
+
+    MemoryRegion mmio;
+    qemu_irq irq;
+
+    AspeedADCEngineState engines[2];
+};
+
+struct AspeedADCClass {
+    SysBusDeviceClass parent_class;
+
+    uint32_t nr_engines;
+};
+
+#endif /* HW_ADC_ASPEED_ADC_H */
