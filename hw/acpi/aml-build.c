099f2df2e6b0ec1b0f1d4e518f730594a66661a8
Andrew Jones <drjones@redhat.com>
2021-10-20 22:21:22 +0800

    hw/acpi/aml-build: Add PPTT table
    Add the Processor Properties Topology Table (PPTT) used to
    describe CPU topology information to ACPI guests.
    
    Note, a DT-boot Linux guest with a non-flat CPU topology will
    see socket and core IDs being sequential integers starting
    from zero, which is different from ACPI-boot Linux guest,
    e.g. with -smp 4,sockets=2,cores=2,threads=1
    
    a DT boot produces:
    
     cpu:  0 package_id:  0 core_id:  0
     cpu:  1 package_id:  0 core_id:  1
     cpu:  2 package_id:  1 core_id:  0
     cpu:  3 package_id:  1 core_id:  1
    
    an ACPI boot produces:
    
     cpu:  0 package_id: 36 core_id:  0
     cpu:  1 package_id: 36 core_id:  1
     cpu:  2 package_id: 96 core_id:  2
     cpu:  3 package_id: 96 core_id:  3
    
    This is due to several reasons:
    
     1) DT cpu nodes do not have an equivalent field to what the PPTT
        ACPI Processor ID must be, i.e. something equal to the MADT CPU
        UID or equal to the UID of an ACPI processor container. In both
        ACPI cases those are platform dependant IDs assigned by the
        vendor.
    
     2) While QEMU is the vendor for a guest, if the topology specifies
        SMT (> 1 thread), then, with ACPI, it is impossible to assign a
        core-id the same value as a package-id, thus it is not possible
        to have package-id=0 and core-id=0. This is because package and
        core containers must be in the same ACPI namespace and therefore
        must have unique UIDs.
    
     3) ACPI processor containers are not mandatorily required for PPTT
        tables to be used and, due to the limitations of which IDs are
        selected described above in (2), they are not helpful for QEMU,
        so we don't build them with this patch. In the absence of them,
        Linux assigns its own unique IDs. The maintainers have chosen not
        to use counters from zero, but rather ACPI table offsets, which
        explains why the numbers are so much larger than with DT.
    
     4) When there is no SMT (threads=1) the core IDs for ACPI boot guests
        match the logical CPU IDs, because these IDs must be equal to the
        MADT CPU UID (as no processor containers are present), and QEMU
        uses the logical CPU ID for these MADT IDs.
    
    So in summary, with QEMU as the vendor for the guests, we simply
    use sequential integers starting from zero for the non-leaf nodes
    but with ID-valid flag unset, so that guest will ignore them and
    use table offsets as unique container IDs. And we use logical CPU
    IDs for the leaf nodes with the ID-valid flag set, which will be
    consistent with MADT.
    
    Currently the implementation of PPTT generation complies with ACPI
    specification 5.2.29 (Revision 6.3). The 6.3 spec can be found at:
    https://uefi.org/sites/default/files/resources/ACPI_6_3_May16.pdf
    
    Reviewed-by: Eric Auger <eric.auger@redhat.com>
    Co-developed-by: Yanan Wang <wangyanan55@huawei.com>
    Signed-off-by: Andrew Jones <drjones@redhat.com>
    Signed-off-by: Yanan Wang <wangyanan55@huawei.com>
    Reviewed-by: Michael S. Tsirkin <mst@redhat.com>
    Message-Id: <20211020142125.7516-6-wangyanan55@huawei.com>
    Signed-off-by: Richard Henderson <richard.henderson@linaro.org>

--- hw/acpi/aml-build.c
+++ hw/acpi/aml-build.c
@@ -1994,6 +1994,65 @@ static void build_processor_hierarchy_node(GArray *tbl, uint32_t flags,
     }
 }
 
+/*
+ * ACPI spec, Revision 6.3
+ * 5.2.29 Processor Properties Topology Table (PPTT)
+ */
+void build_pptt(GArray *table_data, BIOSLinker *linker, MachineState *ms,
+                const char *oem_id, const char *oem_table_id)
+{
+    int pptt_start = table_data->len;
+    int uid = 0;
+    int socket;
+    AcpiTable table = { .sig = "PPTT", .rev = 2,
+                        .oem_id = oem_id, .oem_table_id = oem_table_id };
+
+    acpi_table_begin(&table, table_data);
+
+    for (socket = 0; socket < ms->smp.sockets; socket++) {
+        uint32_t socket_offset = table_data->len - pptt_start;
+        int core;
+
+        build_processor_hierarchy_node(
+            table_data,
+            /*
+             * Physical package - represents the boundary
+             * of a physical package
+             */
+            (1 << 0),
+            0, socket, NULL, 0);
+
+        for (core = 0; core < ms->smp.cores; core++) {
+            uint32_t core_offset = table_data->len - pptt_start;
+            int thread;
+
+            if (ms->smp.threads > 1) {
+                build_processor_hierarchy_node(
+                    table_data,
+                    (0 << 0), /* not a physical package */
+                    socket_offset, core, NULL, 0);
+
+                for (thread = 0; thread < ms->smp.threads; thread++) {
+                    build_processor_hierarchy_node(
+                        table_data,
+                        (1 << 1) | /* ACPI Processor ID valid */
+                        (1 << 2) | /* Processor is a Thread */
+                        (1 << 3),  /* Node is a Leaf */
+                        core_offset, uid++, NULL, 0);
+                }
+            } else {
+                build_processor_hierarchy_node(
+                    table_data,
+                    (1 << 1) | /* ACPI Processor ID valid */
+                    (1 << 3),  /* Node is a Leaf */
+                    socket_offset, uid++, NULL, 0);
+            }
+        }
+    }
+
+    acpi_table_end(linker, &table);
+}
+
 /* build rev1/rev3/rev5.1 FADT */
 void build_fadt(GArray *tbl, BIOSLinker *linker, const AcpiFadtData *f,
                 const char *oem_id, const char *oem_table_id)
