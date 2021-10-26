67f80eb4d0813788046a204595345d1195e767fb
Alex Bennée <alex.bennee@linaro.org>
2021-10-26 11:22:17 +0100

    tests/tcg: enable debian-nios2-cross for test building
    Now we have a nios2 test image we can start using it to build tests.
    However signal handling in nios2 is still broken so we disable the
    signals and linux-test tests that trigger the bug.
    
    Signed-off-by: Alex Bennée <alex.bennee@linaro.org>
    Reviewed-by: Philippe Mathieu-Daudé <f4bug@amsat.org>
    Reviewed-by: Richard Henderson <richard.henderson@linaro.org>
    Message-Id: <20211026102234.3961636-12-alex.bennee@linaro.org>

--- tests/tcg/configure.sh
+++ tests/tcg/configure.sh
@@ -60,6 +60,7 @@ fi
 : ${cross_cc_mips64="mips64-linux-gnuabi64-gcc"}
 : ${cross_cc_mipsel="mipsel-linux-gnu-gcc"}
 : ${cross_cc_mips="mips-linux-gnu-gcc"}
+: ${cross_cc_nios2="nios2-linux-gnu-gcc"}
 : ${cross_cc_ppc="powerpc-linux-gnu-gcc"}
 : ${cross_cc_cflags_ppc="-m32"}
 : ${cross_cc_ppc64="powerpc64-linux-gnu-gcc"}
@@ -158,6 +159,11 @@ for target in $target_list; do
       container_hosts=x86_64
       container_image=debian-mips-cross
       container_cross_cc=mips-linux-gnu-gcc
+      ;;
+    nios2-*)
+      container_hosts=x86_64
+      container_image=debian-nios2-cross
+      container_cross_cc=nios2-linux-gnu-gcc
       ;;
     ppc-*|ppc64abi32-*)
       container_hosts=x86_64
