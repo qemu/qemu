02abee3d5129e1430af35ba4a174980a5a22d64e
Juan Quintela <quintela@redhat.com>
2021-11-02 16:43:52 +0100

    migration: Zero migration compression counters
    Based on previous patch from yuxiating <yuxiating@huawei.com>
    
    Signed-off-by: Juan Quintela <quintela@redhat.com>

--- migration/savevm.c
+++ migration/savevm.c
@@ -1567,6 +1567,7 @@ static int qemu_savevm_state(QEMUFile *f, Error **errp)
 
     migrate_init(ms);
     memset(&ram_counters, 0, sizeof(ram_counters));
+    memset(&compression_counters, 0, sizeof(compression_counters));
     ms->to_dst_file = f;
 
     qemu_mutex_unlock_iothread();
