7d71e6bfb0a2f03bd35165d4fc11da64b7ccc0d7
Laurent Vivier <laurent@vivier.eu>
2021-11-06 20:41:57 +0100

    hw: m68k: virt: Add compat machine for 6.1
    Add the missing machine type for m68k/virt
    
    Cc: qemu-stable@nongnu.org
    Signed-off-by: Laurent Vivier <laurent@vivier.eu>
    Message-Id: <20211106194158.4068596-2-laurent@vivier.eu>
    Signed-off-by: Laurent Vivier <laurent@vivier.eu>
    (cherry picked from commit 6837f299762679429924242a63f16490862578e3)
    Signed-off-by: Michael Roth <michael.roth@amd.com>

--- hw/m68k/virt.c
+++ hw/m68k/virt.c
@@ -304,7 +304,14 @@ type_init(virt_machine_register_types)
     } \
     type_init(machvirt_machine_##major##_##minor##_init);
 
-static void virt_machine_6_0_options(MachineClass *mc)
+static void virt_machine_6_1_options(MachineClass *mc)
 {
 }
-DEFINE_VIRT_MACHINE(6, 0, true)
+DEFINE_VIRT_MACHINE(6, 1, true)
+
+static void virt_machine_6_0_options(MachineClass *mc)
+{
+    virt_machine_6_1_options(mc);
+    compat_props_add(mc->compat_props, hw_compat_6_0, hw_compat_6_0_len);
+}
+DEFINE_VIRT_MACHINE(6, 0, false)
