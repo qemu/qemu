71ba2adfeb4ca63015d4a51a665e67c46a06e822
Philippe Mathieu-Daudé <philmd@redhat.com>
2021-11-24 17:15:35 +0100

    hw/block/fdc: Kludge missing floppy drive to fix CVE-2021-20196
    Guest might select another drive on the bus by setting the
    DRIVE_SEL bit of the DIGITAL OUTPUT REGISTER (DOR).
    The current controller model doesn't expect a BlockBackend
    to be NULL. A simple way to fix CVE-2021-20196 is to create
    an empty BlockBackend when it is missing. All further
    accesses will be safely handled, and the controller state
    machines keep behaving correctly.
    
    Cc: qemu-stable@nongnu.org
    Fixes: CVE-2021-20196
    Reported-by: Gaoning Pan (Ant Security Light-Year Lab) <pgn@zju.edu.cn>
    Reviewed-by: Darren Kenny <darren.kenny@oracle.com>
    Reviewed-by: Hanna Reitz <hreitz@redhat.com>
    Signed-off-by: Philippe Mathieu-Daudé <philmd@redhat.com>
    Message-id: 20211124161536.631563-3-philmd@redhat.com
    BugLink: https://bugs.launchpad.net/qemu/+bug/1912780
    Resolves: https://gitlab.com/qemu-project/qemu/-/issues/338
    Reviewed-by: Darren Kenny <darren.kenny@oracle.com>
    Reviewed-by: Hanna Reitz <hreitz@redhat.com>
    Signed-off-by: Philippe Mathieu-Daudé <philmd@redhat.com>
    Signed-off-by: John Snow <jsnow@redhat.com>
    (cherry picked from commit 1ab95af033a419e7a64e2d58e67dd96b20af5233)
    Signed-off-by: Michael Roth <michael.roth@amd.com>

--- hw/block/fdc.c
+++ hw/block/fdc.c
@@ -1166,7 +1166,19 @@ static FDrive *get_drv(FDCtrl *fdctrl, int unit)
 
 static FDrive *get_cur_drv(FDCtrl *fdctrl)
 {
-    return get_drv(fdctrl, fdctrl->cur_drv);
+    FDrive *cur_drv = get_drv(fdctrl, fdctrl->cur_drv);
+
+    if (!cur_drv->blk) {
+        /*
+         * Kludge: empty drive line selected. Create an anonymous
+         * BlockBackend to avoid NULL deref with various BlockBackend
+         * API calls within this model (CVE-2021-20196).
+         * Due to the controller QOM model limitations, we don't
+         * attach the created to the controller device.
+         */
+        cur_drv->blk = blk_create_empty_drive();
+    }
+    return cur_drv;
 }
 
 /* Status A register : 0x00 (read-only) */
