4006a27c5e44734350009262efb0e2ec8da5ff09
Evan Miller <emmiller@gmail.com>
2021-11-30 12:30:12 -0500

    scripts/entitlement.sh: Use backward-compatible cp flags
    Older versions of Mac OS X do not support cp -a. The cp man page indicates
    that -a is equivalent to -pPR.
    
    Signed-off-by: Evan Miller <emmiller@gmail.com>
    Message-Id: <40635C6E-059A-4146-B1E2-F6376700EE85@gmail.com>
    [Leave out -R, these are files and not directories. - Paolo]
    Signed-off-by: Paolo Bonzini <pbonzini@redhat.com>

--- scripts/entitlement.sh
+++ scripts/entitlement.sh
@@ -15,7 +15,7 @@ ENTITLEMENT="$4"
 
 if $in_place; then
   trap 'rm "$DST.tmp"' exit
-  cp -af "$SRC" "$DST.tmp"
+  cp -pPf "$SRC" "$DST.tmp"
   SRC="$DST.tmp"
 else
   cd "$MESON_INSTALL_DESTDIR_PREFIX"
