5c4eb8fb5649f1fd137fb4d85a019332908fe066
Frank Chang <frank.chang@sifive.com>
2021-12-10 15:56:24 +0800

    target/riscv: rvv-1.0: floating-point scalar move instructions
    NaN-boxed the scalar floating-point register based on RVV 1.0's rules.
    
    Signed-off-by: Frank Chang <frank.chang@sifive.com>
    Acked-by: Alistair Francis <alistair.francis@wdc.com>
    Message-Id: <20211210075704.23951-39-frank.chang@sifive.com>
    Signed-off-by: Alistair Francis <alistair.francis@wdc.com>

--- target/riscv/internals.h
+++ target/riscv/internals.h
@@ -32,11 +32,6 @@ target_ulong fclass_h(uint64_t frs1);
 target_ulong fclass_s(uint64_t frs1);
 target_ulong fclass_d(uint64_t frs1);
 
-#define SEW8  0
-#define SEW16 1
-#define SEW32 2
-#define SEW64 3
-
 #ifndef CONFIG_USER_ONLY
 extern const VMStateDescription vmstate_riscv_cpu;
 #endif
