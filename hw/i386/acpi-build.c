5903646d3913af6544680f6645fcb7296d0b3a1c
Stefan Berger <stefanb@linux.ibm.com>
2022-01-04 12:58:05 -0500

    acpi: tpm: Add missing device identification objects
    Add missing TPM device identification objects _STR and _UID. They will
    appear as files 'description' and 'uid' under Linux sysfs.
    
    Following inspection of sysfs entries for hardware TPMs we chose
    uid '1'.
    
    Cc: Shannon Zhao <shannon.zhaosl@gmail.com>
    Cc: Michael S. Tsirkin <mst@redhat.com>
    Cc: Igor Mammedov <imammedo@redhat.com>
    Cc: Ani Sinha <ani@anisinha.ca>
    Resolves: https://gitlab.com/qemu-project/qemu/-/issues/708
    Signed-off-by: Stefan Berger <stefanb@linux.ibm.com>
    Reviewed-by: Ani Sinha <ani@anisinha.ca>
    Reviewed-by: Shannon Zhao <shannon.zhaosl@gmail.com>
    Message-id: 20211223022310.575496-3-stefanb@linux.ibm.com
    Message-Id: <20220104175806.872996-3-stefanb@linux.ibm.com>
    Reviewed-by: Michael S. Tsirkin <mst@redhat.com>
    Signed-off-by: Michael S. Tsirkin <mst@redhat.com>
    Reviewed-by: Igor Mammedov <imammedo@redhat.com>

--- hw/i386/acpi-build.c
+++ hw/i386/acpi-build.c
@@ -1812,11 +1812,15 @@ build_dsdt(GArray *table_data, BIOSLinker *linker,
                     dev = aml_device("TPM");
                     aml_append(dev, aml_name_decl("_HID",
                                                   aml_string("MSFT0101")));
+                    aml_append(dev,
+                               aml_name_decl("_STR",
+                                             aml_string("TPM 2.0 Device")));
                 } else {
                     dev = aml_device("ISA.TPM");
                     aml_append(dev, aml_name_decl("_HID",
                                                   aml_eisaid("PNP0C31")));
                 }
+                aml_append(dev, aml_name_decl("_UID", aml_int(1)));
 
                 aml_append(dev, aml_name_decl("_STA", aml_int(0xF)));
                 crs = aml_resource_template();
@@ -1844,12 +1848,15 @@ build_dsdt(GArray *table_data, BIOSLinker *linker,
     if (TPM_IS_CRB(tpm)) {
         dev = aml_device("TPM");
         aml_append(dev, aml_name_decl("_HID", aml_string("MSFT0101")));
+        aml_append(dev, aml_name_decl("_STR",
+                                      aml_string("TPM 2.0 Device")));
         crs = aml_resource_template();
         aml_append(crs, aml_memory32_fixed(TPM_CRB_ADDR_BASE,
                                            TPM_CRB_ADDR_SIZE, AML_READ_WRITE));
         aml_append(dev, aml_name_decl("_CRS", crs));
 
         aml_append(dev, aml_name_decl("_STA", aml_int(0xf)));
+        aml_append(dev, aml_name_decl("_UID", aml_int(1)));
 
         tpm_build_ppi_acpi(tpm, dev);
 
