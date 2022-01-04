7a5626a1d8a193106dc709608273ed48729967d5
Ahmed Abouzied <email@aabouzied.com>
2022-01-04 16:38:41 +0200

    linux-user/syscall.c: malloc to g_try_malloc
    Use g_try_malloc instead of malloc to alocate the target ifconfig.
    Also replace the corresponding free with g_free.
    
    Signed-off-by: Ahmed Abouzied <email@aabouzied.com>
    Message-Id: <20220104143841.25116-1-email@aabouzied.com>
    Signed-off-by: Laurent Vivier <laurent@vivier.eu>

--- linux-user/syscall.c
+++ linux-user/syscall.c
@@ -4915,7 +4915,7 @@ static abi_long do_ioctl_ifconf(const IOCTLEntry *ie, uint8_t *buf_temp,
              * We can't fit all the extents into the fixed size buffer.
              * Allocate one that is large enough and use it instead.
              */
-            host_ifconf = malloc(outbufsz);
+            host_ifconf = g_try_malloc(outbufsz);
             if (!host_ifconf) {
                 return -TARGET_ENOMEM;
             }
@@ -4963,7 +4963,7 @@ static abi_long do_ioctl_ifconf(const IOCTLEntry *ie, uint8_t *buf_temp,
     }
 
     if (free_buf) {
-        free(host_ifconf);
+        g_free(host_ifconf);
     }
 
     return ret;
