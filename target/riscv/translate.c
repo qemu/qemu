57c108b86461a050a58b1ca9d31fd6e4af32205c
Frédéric Pétrot <frederic.petrot@univ-grenoble-alpes.fr>
2022-01-06 22:01:01 +0100

    target/riscv: support for 128-bit U-type instructions
    Adding the 128-bit version of lui and auipc, and introducing to that end
    a "set register with immediat" function to handle extension on 128 bits.
    
    Signed-off-by: Frédéric Pétrot <frederic.petrot@univ-grenoble-alpes.fr>
    Co-authored-by: Fabien Portas <fabien.portas@grenoble-inp.org>
    Reviewed-by: Richard Henderson <richard.henderson@linaro.org>
    Reviewed-by: Alistair Francis <alistair.francis@wdc.com>
    Message-id: 20220106210108.138226-12-frederic.petrot@univ-grenoble-alpes.fr
    Signed-off-by: Alistair Francis <alistair.francis@wdc.com>

--- target/riscv/translate.c
+++ target/riscv/translate.c
@@ -322,6 +322,27 @@ static void gen_set_gpr(DisasContext *ctx, int reg_num, TCGv t)
     }
 }
 
+static void gen_set_gpri(DisasContext *ctx, int reg_num, target_long imm)
+{
+    if (reg_num != 0) {
+        switch (get_ol(ctx)) {
+        case MXL_RV32:
+            tcg_gen_movi_tl(cpu_gpr[reg_num], (int32_t)imm);
+            break;
+        case MXL_RV64:
+        case MXL_RV128:
+            tcg_gen_movi_tl(cpu_gpr[reg_num], imm);
+            break;
+        default:
+            g_assert_not_reached();
+        }
+
+        if (get_xl_max(ctx) == MXL_RV128) {
+            tcg_gen_movi_tl(cpu_gprh[reg_num], -(imm < 0));
+        }
+    }
+}
+
 static void gen_set_gpr128(DisasContext *ctx, int reg_num, TCGv rl, TCGv rh)
 {
     assert(get_ol(ctx) == MXL_RV128);
