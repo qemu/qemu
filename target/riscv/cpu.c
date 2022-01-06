dfdb46a3765bdab40a0b36722b4828d52ea8de96
Philipp Tomsich <philipp.tomsich@vrull.eu>
2022-01-06 14:40:20 +0100

    target/riscv: Fix position of 'experimental' comment
    When commit 0643c12e4b dropped the 'x-' prefix for Zb[abcs] and set
    them to be enabled by default, the comment about experimental
    extensions was kept in place above them.  This moves it down a few
    lines to only cover experimental extensions.
    
    References: 0643c12e4b ("target/riscv: Enable bitmanip Zb[abcs] instructions")
    
    Signed-off-by: Philipp Tomsich <philipp.tomsich@vrull.eu>
    Reviewed-by: Bin Meng <bmeng.cn@gmail.com>
    Reviewed-by: Philippe Mathieu-Daudé <f4bug@amsat.org>
    Reviewed-by: Alistair Francis <alistair.francis@wdc.com>
    Message-id: 20220106134020.1628889-1-philipp.tomsich@vrull.eu
    Signed-off-by: Alistair Francis <alistair.francis@wdc.com>

--- target/riscv/cpu.c
+++ target/riscv/cpu.c
@@ -641,11 +641,12 @@ static Property riscv_cpu_properties[] = {
     DEFINE_PROP_UINT16("vlen", RISCVCPU, cfg.vlen, 128),
     DEFINE_PROP_UINT16("elen", RISCVCPU, cfg.elen, 64),
 
-    /* These are experimental so mark with 'x-' */
     DEFINE_PROP_BOOL("zba", RISCVCPU, cfg.ext_zba, true),
     DEFINE_PROP_BOOL("zbb", RISCVCPU, cfg.ext_zbb, true),
     DEFINE_PROP_BOOL("zbc", RISCVCPU, cfg.ext_zbc, true),
     DEFINE_PROP_BOOL("zbs", RISCVCPU, cfg.ext_zbs, true),
+
+    /* These are experimental so mark with 'x-' */
     DEFINE_PROP_BOOL("x-j", RISCVCPU, cfg.ext_j, false),
     /* ePMP 0.9.3 */
     DEFINE_PROP_BOOL("x-epmp", RISCVCPU, cfg.epmp, false),
