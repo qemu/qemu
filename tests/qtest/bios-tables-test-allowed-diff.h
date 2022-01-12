5adc3aba875416b0e077d8a29ddd0357883746f4
Igor Mammedov <imammedo@redhat.com>
2022-01-12 08:03:32 -0500

    tests: acpi: update expected blobs
    Expected changes caused by previous commit:
    
    nvdimm ssdt (q35/pc/virt):
      - *     OEM Table ID     "NVDIMM  "
      + *     OEM Table ID     "NVDIMM"
    
    SLIC test FADT (tests/data/acpi/q35/FACP.slic):
      -[010h 0016   8]                 Oem Table ID : "ME      "
      +[010h 0016   8]                 Oem Table ID : "ME"
    
    Signed-off-by: Igor Mammedov <imammedo@redhat.com>
    Message-Id: <20220112130332.1648664-5-imammedo@redhat.com>
    Reviewed-by: Michael S. Tsirkin <mst@redhat.com>
    Signed-off-by: Michael S. Tsirkin <mst@redhat.com>

--- tests/qtest/bios-tables-test-allowed-diff.h
+++ tests/qtest/bios-tables-test-allowed-diff.h
@@ -1,5 +1,1 @@
 /* List of comma-separated changed AML files to ignore */
-"tests/data/acpi/virt/SSDT.memhp",
-"tests/data/acpi/pc/SSDT.dimmpxm",
-"tests/data/acpi/q35/SSDT.dimmpxm",
-"tests/data/acpi/q35/FACP.slic",
