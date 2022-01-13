a66a24585fae7036dc2f358c9addd1630be5c50c
Petr Pavlu <petr.pavlu@suse.com>
2022-01-13 16:19:15 +0100

    hw/intc/arm_gic: Implement read of GICC_IIDR
    Implement support for reading GICC_IIDR. This register is used by the
    Linux kernel to recognize that GICv2 with GICC_APRn is present.
    
    Signed-off-by: Petr Pavlu <petr.pavlu@suse.com>
    Message-id: 20220113151916.17978-2-ppavlu@suse.cz
    Reviewed-by: Peter Maydell <peter.maydell@linaro.org>
    Signed-off-by: Peter Maydell <peter.maydell@linaro.org>

--- hw/intc/arm_gic.c
+++ hw/intc/arm_gic.c
@@ -1662,6 +1662,15 @@ static MemTxResult gic_cpu_read(GICState *s, int cpu, int offset,
         }
         break;
     }
+    case 0xfc:
+        if (s->revision == REV_11MPCORE) {
+            /* Reserved on 11MPCore */
+            *data = 0;
+        } else {
+            /* GICv1 or v2; Arm implementation */
+            *data = (s->revision << 16) | 0x43b;
+        }
+        break;
     default:
         qemu_log_mask(LOG_GUEST_ERROR,
                       "gic_cpu_read: Bad offset %x\n", (int)offset);
