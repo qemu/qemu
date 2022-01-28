1afe57802ab0127b31b323be171ba9c943a04133
Fabiano Rosas <farosas@linux.ibm.com>
2022-01-28 13:15:05 +0100

    target/ppc: 405: Watchdog timer exception cleanup
    Remove references to BookE.
    
    Signed-off-by: Fabiano Rosas <farosas@linux.ibm.com>
    Reviewed-by: Cédric Le Goater <clg@kaod.org>
    Message-Id: <20220118184448.852996-15-farosas@linux.ibm.com>
    Signed-off-by: Cédric Le Goater <clg@kaod.org>

--- target/ppc/excp_helper.c
+++ target/ppc/excp_helper.c
@@ -396,7 +396,6 @@ static void powerpc_excp_40x(PowerPCCPU *cpu, int excp)
 {
     CPUState *cs = CPU(cpu);
     CPUPPCState *env = &cpu->env;
-    int excp_model = env->excp_model;
     target_ulong msr, new_msr, vector;
     int srr0, srr1;
 
@@ -515,14 +514,6 @@ static void powerpc_excp_40x(PowerPCCPU *cpu, int excp)
         break;
     case POWERPC_EXCP_WDT:       /* Watchdog timer interrupt                 */
         trace_ppc_excp_print("WDT");
-        switch (excp_model) {
-        case POWERPC_EXCP_BOOKE:
-            srr0 = SPR_BOOKE_CSRR0;
-            srr1 = SPR_BOOKE_CSRR1;
-            break;
-        default:
-            break;
-        }
         break;
     case POWERPC_EXCP_DTLB:      /* Data TLB error                           */
     case POWERPC_EXCP_ITLB:      /* Instruction TLB error                    */
