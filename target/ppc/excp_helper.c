66b5ad561552c5c77058502ea3c7f04316937b64
Fabiano Rosas <farosas@linux.ibm.com>
2022-01-28 13:15:04 +0100

    target/ppc: 405: Alignment exception cleanup
    There is no DSISR in the 405. It uses DEAR which we already set
    earlier at ppc_cpu_do_unaligned_access.
    
    Signed-off-by: Fabiano Rosas <farosas@linux.ibm.com>
    Reviewed-by: David Gibson <david@gibson.dropbear.id.au
    Message-Id: <20220118184448.852996-10-farosas@linux.ibm.com>
    Signed-off-by: Cédric Le Goater <clg@kaod.org>

--- target/ppc/excp_helper.c
+++ target/ppc/excp_helper.c
@@ -474,13 +474,6 @@ static void powerpc_excp_40x(PowerPCCPU *cpu, int excp)
     case POWERPC_EXCP_EXTERNAL:  /* External input                           */
         break;
     case POWERPC_EXCP_ALIGN:     /* Alignment exception                      */
-        /* Get rS/rD and rA from faulting opcode */
-        /*
-         * Note: the opcode fields will not be set properly for a
-         * direct store load/store, but nobody cares as nobody
-         * actually uses direct store segments.
-         */
-        env->spr[SPR_DSISR] |= (env->error_code & 0x03FF0000) >> 16;
         break;
     case POWERPC_EXCP_PROGRAM:   /* Program exception                        */
         switch (env->error_code & ~0xF) {
