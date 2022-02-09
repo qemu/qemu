9c9b67fe91a0e5ff3961d85d7d51dce4075a6f08
Fabiano Rosas <farosas@linux.ibm.com>
2022-02-09 09:08:55 +0100

    target/ppc: booke: Critical exception cleanup
    Remove 40x and G2 code.
    
    Signed-off-by: Fabiano Rosas <farosas@linux.ibm.com>
    Message-Id: <20220128224018.1228062-4-farosas@linux.ibm.com>
    Signed-off-by: Cédric Le Goater <clg@kaod.org>

--- target/ppc/excp_helper.c
+++ target/ppc/excp_helper.c
@@ -801,20 +801,8 @@ static void powerpc_excp_booke(PowerPCCPU *cpu, int excp)
 
     switch (excp) {
     case POWERPC_EXCP_CRITICAL:    /* Critical input                         */
-        switch (excp_model) {
-        case POWERPC_EXCP_40x:
-            srr0 = SPR_40x_SRR2;
-            srr1 = SPR_40x_SRR3;
-            break;
-        case POWERPC_EXCP_BOOKE:
-            srr0 = SPR_BOOKE_CSRR0;
-            srr1 = SPR_BOOKE_CSRR1;
-            break;
-        case POWERPC_EXCP_G2:
-            break;
-        default:
-            goto excp_invalid;
-        }
+        srr0 = SPR_BOOKE_CSRR0;
+        srr1 = SPR_BOOKE_CSRR1;
         break;
     case POWERPC_EXCP_MCHECK:    /* Machine check exception                  */
         if (msr_me == 0) {
@@ -1044,7 +1032,6 @@ static void powerpc_excp_booke(PowerPCCPU *cpu, int excp)
                   powerpc_excp_name(excp));
         break;
     default:
-    excp_invalid:
         cpu_abort(cs, "Invalid PowerPC exception %d. Aborting\n", excp);
         break;
     }
