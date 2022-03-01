acf7a705980d157735242cf3bedabd2ef22c5384
Volker Rümelin <vr_qemu@t-online.de>
2022-03-01 20:13:10 +0100

    paaudio: fix samples vs. frames mix-up
    Now that the mixing buffer size no longer adds to playback
    latency, fix the samples vs. frames mix-up in the mixing buffer
    size calculation. This change will go largely unnoticed as long
    as the user doesn't use a buffer-size smaller than timer-period.
    
    Signed-off-by: Volker Rümelin <vr_qemu@t-online.de>
    Message-Id: <20220301191311.26695-14-vr_qemu@t-online.de>
    Signed-off-by: Gerd Hoffmann <kraxel@redhat.com>

--- audio/paaudio.c
+++ audio/paaudio.c
@@ -549,11 +549,8 @@ static int qpa_init_out(HWVoiceOut *hw, struct audsettings *as,
     }
 
     audio_pcm_init_info (&hw->info, &obt_as);
-    /*
-     * This is wrong. hw->samples counts in frames. hw->samples will be
-     * number of channels times larger than expected.
-     */
-    hw->samples = audio_buffer_samples(
+    /* hw->samples counts in frames */
+    hw->samples = audio_buffer_frames(
         qapi_AudiodevPaPerDirectionOptions_base(ppdo), &obt_as, 46440);
 
     return 0;
@@ -601,11 +598,8 @@ static int qpa_init_in(HWVoiceIn *hw, struct audsettings *as, void *drv_opaque)
     }
 
     audio_pcm_init_info (&hw->info, &obt_as);
-    /*
-     * This is wrong. hw->samples counts in frames. hw->samples will be
-     * number of channels times larger than expected.
-     */
-    hw->samples = audio_buffer_samples(
+    /* hw->samples counts in frames */
+    hw->samples = audio_buffer_frames(
         qapi_AudiodevPaPerDirectionOptions_base(ppdo), &obt_as, 46440);
 
     return 0;
