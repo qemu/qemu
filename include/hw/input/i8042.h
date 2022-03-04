5334bf5703408b7948d9fd8bb8a599c97cdc84a3
Liav Albani <liavalb@gmail.com>
2022-03-04 21:10:30 +0530

    hw/acpi: add indication for i8042 in IA-PC boot flags of the FADT table
    This can allow the guest OS to determine more easily if i8042 controller
    is present in the system or not, so it doesn't need to do probing of the
    controller, but just initialize it immediately, before enumerating the
    ACPI AML namespace.
    
    The 8042 bit in IAPC_BOOT_ARCH was introduced from ACPI spec v2 (FADT
    revision 2 and above). Therefore, in this change, we only enable this bit for
    x86/q35 machine types since x86/i440fx machines use FADT ACPI table with
    revision 1.
    
    Signed-off-by: Liav Albani <liavalb@gmail.com>
    Signed-off-by: Ani Sinha <ani@anisinha.ca>
    Message-Id: <20220304154032.2071585-3-ani@anisinha.ca>
    Reviewed-by: Michael S. Tsirkin <mst@redhat.com>
    Signed-off-by: Michael S. Tsirkin <mst@redhat.com>

--- include/hw/input/i8042.h
+++ include/hw/input/i8042.h
@@ -23,4 +23,19 @@ void i8042_mm_init(qemu_irq kbd_irq, qemu_irq mouse_irq,
 void i8042_isa_mouse_fake_event(ISAKBDState *isa);
 void i8042_setup_a20_line(ISADevice *dev, qemu_irq a20_out);
 
+static inline bool i8042_present(void)
+{
+    bool amb = false;
+    return object_resolve_path_type("", TYPE_I8042, &amb) || amb;
+}
+
+/*
+ * ACPI v2, Table 5-10 - Fixed ACPI Description Table Boot Architecture
+ * Flags, bit offset 1 - 8042.
+ */
+static inline uint16_t iapc_boot_arch_8042(void)
+{
+    return i8042_present() ? 0x1 << 1 : 0x0 ;
+}
+
 #endif /* HW_INPUT_I8042_H */
