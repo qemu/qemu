43b6277ac2dc2a61acf303d9d515fafb0b876cc1
Liav Albani <liavalb@gmail.com>
2022-03-04 21:10:29 +0530

    tests/acpi: i386: allow FACP acpi table changes
    The FACP table is going to be changed for x86/q35 machines. To be sure
    the following changes are not breaking any QEMU test this change follows
    step 2 from the bios-tables-test.c guide on changes that affect ACPI
    tables.
    
    Signed-off-by: Liav Albani <liavalb@gmail.com>
    Acked-by: Ani Sinha <ani@anisinha.ca>
    Message-Id: <20220304154032.2071585-2-ani@anisinha.ca>
    Reviewed-by: Michael S. Tsirkin <mst@redhat.com>
    Signed-off-by: Michael S. Tsirkin <mst@redhat.com>

--- tests/qtest/bios-tables-test-allowed-diff.h
+++ tests/qtest/bios-tables-test-allowed-diff.h
@@ -1,1 +1,5 @@
 /* List of comma-separated changed AML files to ignore */
+"tests/data/acpi/q35/FACP",
+"tests/data/acpi/q35/FACP.nosmm",
+"tests/data/acpi/q35/FACP.slic",
+"tests/data/acpi/q35/FACP.xapic",
