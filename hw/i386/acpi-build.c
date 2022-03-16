9c2d83f5a0ef558f8882998af6cb800101243655
Andrew Deason <adeason@sinenomine.net>
2022-03-15 22:52:26 -0500

    hw/i386/acpi-build: Avoid 'sun' identifier
    On Solaris, 'sun' is #define'd to 1, which causes errors if a variable
    is named 'sun'. Slightly change the name of the var for the Slot User
    Number so we can build on Solaris.
    
    Reviewed-by: Ani Sinha <ani@anisinha.ca>
    Signed-off-by: Andrew Deason <adeason@sinenomine.net>
    Reviewed-by: Michael S. Tsirkin <mst@redhat.com>
    Message-id: 20220316035227.3702-3-adeason@sinenomine.net
    Signed-off-by: Peter Maydell <peter.maydell@linaro.org>

--- hw/i386/acpi-build.c
+++ hw/i386/acpi-build.c
@@ -583,7 +583,7 @@ Aml *aml_pci_device_dsm(void)
     Aml *bnum = aml_arg(4);
     Aml *func = aml_arg(2);
     Aml *rev = aml_arg(1);
-    Aml *sun = aml_arg(5);
+    Aml *sunum = aml_arg(5);
 
     method = aml_method("PDSM", 6, AML_SERIALIZED);
 
@@ -594,7 +594,7 @@ Aml *aml_pci_device_dsm(void)
     UUID = aml_touuid("E5C937D0-3553-4D7A-9117-EA4D19C3434D");
     ifctx = aml_if(aml_equal(aml_arg(0), UUID));
     {
-        aml_append(ifctx, aml_store(aml_call2("AIDX", bnum, sun), acpi_index));
+        aml_append(ifctx, aml_store(aml_call2("AIDX", bnum, sunum), acpi_index));
         ifctx1 = aml_if(aml_equal(func, zero));
         {
             uint8_t byte_list[1];
