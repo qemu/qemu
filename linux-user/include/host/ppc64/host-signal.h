9d1401b79463e74adbfac69d836789d4e103fb61
Khem Raj <raj.khem@gmail.com>
2022-03-20 23:35:27 +0100

    ppc64: Avoid pt_regs struct definition
    Remove pt_regs indirection and instead reference gp_regs directly, this
    makes it portable across musl/glibc
    
    Use PT_* constants defined in asm/ptrace.h
    
    Move the file to ppc64 subdir and leave ppc empty
    
    Fixes
    ../qemu-6.2.0/linux-user/host/ppc64/../ppc/host-signal.h:16:32: error: incomplete definition of type 'struct pt_regs'
        return uc->uc_mcontext.regs->nip;
               ~~~~~~~~~~~~~~~~~~~~^
    
    Signed-off-by: Khem Raj <raj.khem@gmail.com>
    Cc: Peter Maydell <peter.maydell@linaro.org>
    Cc: Philippe Mathieu-Daudé <f4bug@amsat.org>
    Cc: Richard Henderson <richard.henderson@linaro.org>
    Reviewed-by: Richard Henderson <richard.henderson@linaro.org>
    Message-Id: <20220315015740.847370-1-raj.khem@gmail.com>
    Signed-off-by: Cédric Le Goater <clg@kaod.org>

--- linux-user/include/host/ppc64/host-signal.h
+++ linux-user/include/host/ppc64/host-signal.h
@@ -1,1 +1,41 @@
-#include "../ppc/host-signal.h"
+/*
+ * host-signal.h: signal info dependent on the host architecture
+ *
+ * Copyright (c) 2003-2005 Fabrice Bellard
+ * Copyright (c) 2021 Linaro Limited
+ *
+ * This work is licensed under the terms of the GNU LGPL, version 2.1 or later.
+ * See the COPYING file in the top-level directory.
+ */
+
+#ifndef PPC_HOST_SIGNAL_H
+#define PPC_HOST_SIGNAL_H
+
+/* Needed for PT_* constants */
+#include <asm/ptrace.h>
+
+/* The third argument to a SA_SIGINFO handler is ucontext_t. */
+typedef ucontext_t host_sigcontext;
+
+static inline uintptr_t host_signal_pc(host_sigcontext *uc)
+{
+    return uc->uc_mcontext.gp_regs[PT_NIP];
+}
+
+static inline void host_signal_set_pc(host_sigcontext *uc, uintptr_t pc)
+{
+    uc->uc_mcontext.gp_regs[PT_NIP] = pc;
+}
+
+static inline void *host_signal_mask(host_sigcontext *uc)
+{
+    return &uc->uc_sigmask;
+}
+
+static inline bool host_signal_write(siginfo_t *info, host_sigcontext *uc)
+{
+    return uc->uc_mcontext.gp_regs[PT_TRAP] != 0x400
+        && (uc->uc_mcontext.gp_regs[PT_DSISR] & 0x02000000);
+}
+
+#endif
