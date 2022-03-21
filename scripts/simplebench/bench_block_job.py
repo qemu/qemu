28d4f06e0a10750bbf806977608930dd31b8add3
John Snow <jsnow@redhat.com>
2022-03-21 16:33:13 -0400

    scripts/bench-block-job: switch to AQMP
    For this commit, we only need to remove accommodations for the
    synchronous QMP library.
    
    Signed-off-by: John Snow <jsnow@redhat.com>
    Reviewed-by: Vladimir Sementsov-Ogievskiy <vsementsov@virtuozzo.com>
    Reviewed-by: Beraldo Leal <bleal@redhat.com>
    Acked-by: Hanna Reitz <hreitz@redhat.com>
    Message-id: 20220321203315.909411-3-jsnow@redhat.com
    Signed-off-by: John Snow <jsnow@redhat.com>

--- scripts/simplebench/bench_block_job.py
+++ scripts/simplebench/bench_block_job.py
@@ -27,7 +27,6 @@ import json
 
 sys.path.append(os.path.join(os.path.dirname(__file__), '..', '..', 'python'))
 from qemu.machine import QEMUMachine
-from qemu.qmp import QMPConnectError
 from qemu.aqmp import ConnectError
 
 
@@ -50,7 +49,7 @@ def bench_block_job(cmd, cmd_args, qemu_args):
         vm.launch()
     except OSError as e:
         return {'error': 'popen failed: ' + str(e)}
-    except (QMPConnectError, ConnectError, socket.timeout):
+    except (ConnectError, socket.timeout):
         return {'error': 'qemu failed: ' + str(vm.get_log())}
 
     try:
