d2293ebb042b7ca77cdadd1ed6e5d07fcc7a510f
Richard Henderson <richard.henderson@linaro.org>
2022-04-21 08:17:17 -0700

    target/nios2: Drop CR_STATUS_EH from tb->flags
    There's nothing about EH that affects translation,
    so there's no need to include it in tb->flags.
    
    Reviewed-by: Peter Maydell <peter.maydell@linaro.org>
    Signed-off-by: Richard Henderson <richard.henderson@linaro.org>
    Message-Id: <20220421151735.31996-47-richard.henderson@linaro.org>

--- target/nios2/cpu.h
+++ target/nios2/cpu.h
@@ -272,7 +272,7 @@ static inline void cpu_get_tb_cpu_state(CPUNios2State *env, target_ulong *pc,
 {
     *pc = env->pc;
     *cs_base = 0;
-    *flags = env->ctrl[CR_STATUS] & (CR_STATUS_EH | CR_STATUS_U);
+    *flags = env->ctrl[CR_STATUS] & CR_STATUS_U;
 }
 
 #endif /* NIOS2_CPU_H */
