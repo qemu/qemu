d8e81e3c18231e4dbab562f53e3e6cf7ad77735f
Weiwei Li <liweiwei@iscas.ac.cn>
2022-04-23 10:34:58 +0800

    target/riscv: rvk: add support for zbkb extension
     - reuse partial instructions of zbb extension, update extension check for them
     - add brev8, pack, packh, packw, unzip, zip instructions
    
    Signed-off-by: Weiwei Li <liweiwei@iscas.ac.cn>
    Signed-off-by: Junqiang Wang <wangjunqiang@iscas.ac.cn>
    Acked-by: Alistair Francis <alistair.francis@wdc.com>
    Reviewed-by: Richard Henderson <richard.henderson@linaro.org>
    Message-Id: <20220423023510.30794-3-liweiwei@iscas.ac.cn>
    Signed-off-by: Alistair Francis <alistair.francis@wdc.com>

--- target/riscv/helper.h
+++ target/riscv/helper.h
@@ -66,6 +66,9 @@ DEF_HELPER_FLAGS_1(fclass_d, TCG_CALL_NO_RWG_SE, tl, i64)
 /* Bitmanip */
 DEF_HELPER_FLAGS_2(clmul, TCG_CALL_NO_RWG_SE, tl, tl, tl)
 DEF_HELPER_FLAGS_2(clmulr, TCG_CALL_NO_RWG_SE, tl, tl, tl)
+DEF_HELPER_FLAGS_1(brev8, TCG_CALL_NO_RWG_SE, tl, tl)
+DEF_HELPER_FLAGS_1(unzip, TCG_CALL_NO_RWG_SE, tl, tl)
+DEF_HELPER_FLAGS_1(zip, TCG_CALL_NO_RWG_SE, tl, tl)
 
 /* Floating Point - Half Precision */
 DEF_HELPER_FLAGS_3(fadd_h, TCG_CALL_NO_RWG, i64, env, i64, i64)
