c7b1172026a03b914e3534eb2d1d6a4c9b211a58
Marc-André Lureau <marcandre.lureau@redhat.com>
2022-04-25 17:39:06 +0400

    ui: replace qemu_set_nonblock()
    The call is POSIX-specific. Use the dedicated GLib API.
    
    (this is a preliminary patch before renaming qemu_set_nonblock())
    
    Signed-off-by: Marc-André Lureau <marcandre.lureau@redhat.com>
    Reviewed-by: Daniel P. Berrangé <berrange@redhat.com>

--- ui/input-linux.c
+++ ui/input-linux.c
@@ -316,7 +316,10 @@ static void input_linux_complete(UserCreatable *uc, Error **errp)
         error_setg_file_open(errp, errno, il->evdev);
         return;
     }
-    qemu_set_nonblock(il->fd);
+    if (!g_unix_set_fd_nonblocking(il->fd, true, NULL)) {
+        error_setg_errno(errp, errno, "Failed to set FD nonblocking");
+        return;
+    }
 
     rc = ioctl(il->fd, EVIOCGVERSION, &ver);
     if (rc < 0) {
