14e275efcbe9cc8653a90f2085e2cdf0f7f7e24f
Mark Cave-Ayland <mark.cave-ayland@ilande.co.uk>
2022-05-04 10:25:21 +0100

    dino: split declarations from dino.c into dino.h
    This is to allow access to DinoState from outside dino.c. With the changes to
    the headers it is now possible to remove the duplicate definition for
    TYPE_DINO_PCI_HOST_BRIDGE from hppa_sys.h.
    
    Signed-off-by: Mark Cave-Ayland <mark.cave-ayland@ilande.co.uk>
    Acked-by: Helge Deller <deller@gmx.de>
    Message-Id: <20220504092600.10048-12-mark.cave-ayland@ilande.co.uk>
    Signed-off-by: Mark Cave-Ayland <mark.cave-ayland@ilande.co.uk>

--- hw/hppa/hppa_sys.h
+++ hw/hppa/hppa_sys.h
@@ -14,8 +14,6 @@ PCIBus *dino_init(MemoryRegion *, qemu_irq *, qemu_irq *);
 DeviceState *lasi_init(MemoryRegion *);
 #define enable_lasi_lan()       0
 
-#define TYPE_DINO_PCI_HOST_BRIDGE "dino-pcihost"
-
 /* hppa_pci.c.  */
 extern const MemoryRegionOps hppa_pci_ignore_ops;
 extern const MemoryRegionOps hppa_pci_conf1_ops;
