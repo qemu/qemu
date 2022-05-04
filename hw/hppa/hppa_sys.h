3c2ce5bba379aa87a3098bffc06101c8a9544940
Mark Cave-Ayland <mark.cave-ayland@ilande.co.uk>
2022-05-04 10:25:54 +0100

    hppa: remove hw/hppa/pci.c
    The functions and definitions in this file are not used anywhere within the
    generic hppa machine.
    
    Signed-off-by: Mark Cave-Ayland <mark.cave-ayland@ilande.co.uk>
    Acked-by: Helge Deller <deller@gmx.de>
    Message-Id: <20220504092600.10048-45-mark.cave-ayland@ilande.co.uk>
    Reviewed-by: Richard Henderson <richard.henderson@linaro.org>
    Signed-off-by: Mark Cave-Ayland <mark.cave-ayland@ilande.co.uk>

--- hw/hppa/hppa_sys.h
+++ hw/hppa/hppa_sys.h
@@ -3,14 +3,8 @@
 #ifndef HW_HPPA_SYS_H
 #define HW_HPPA_SYS_H
 
-#include "hw/boards.h"
-
 #include "hppa_hardware.h"
 
 #define enable_lasi_lan()       0
-
-/* hppa_pci.c.  */
-extern const MemoryRegionOps hppa_pci_conf1_ops;
-extern const MemoryRegionOps hppa_pci_iack_ops;
 
 #endif
