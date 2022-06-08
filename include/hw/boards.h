518f5f6ad6edfb17a35cc13368c4c5205fe75839
Jonathan Cameron <Jonathan.Cameron@huawei.com>
2022-06-08 15:54:40 +0100

    hw/machine: Drop cxl_supported flag as no longer useful
    As all the CXL elements have moved to boards that support
    CXL, there is no need to maintain a top level flag.
    
    Signed-off-by: Jonathan Cameron <Jonathan.Cameron@huawei.com>
    Reviewed-by: Ben Widawsky <ben@bwidawsk.net>
    Message-Id: <20220608145440.26106-9-Jonathan.Cameron@huawei.com>
    Reviewed-by: Michael S. Tsirkin <mst@redhat.com>
    Signed-off-by: Michael S. Tsirkin <mst@redhat.com>

--- include/hw/boards.h
+++ include/hw/boards.h
@@ -269,7 +269,6 @@ struct MachineClass {
     bool ignore_boot_device_suffixes;
     bool smbus_no_migration_support;
     bool nvdimm_supported;
-    bool cxl_supported;
     bool numa_mem_supported;
     bool auto_enable_numa;
     SMPCompatProps smp_props;
