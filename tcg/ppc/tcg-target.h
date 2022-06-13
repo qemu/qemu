4d5738222ff0a7f4d56fd4a2971b0605726a8fb2
Matheus Kowalczuk Ferst <matheus.ferst@eldorado.org.br>
2022-06-13 14:43:59 +0000

    tcg/ppc: implement rem[u]_i{32,64} with mod[su][wd]
    Power ISA v3.0 introduced mod[su][wd] insns that can be used to
    implement rem[u]_i{32,64}.
    
    Signed-off-by: Matheus Ferst <matheus.ferst@eldorado.org.br>
    Signed-off-by: Richard Henderson <richard.henderson@linaro.org>

--- tcg/ppc/tcg-target.h
+++ tcg/ppc/tcg-target.h
@@ -83,7 +83,7 @@ extern bool have_vsx;
 
 /* optional instructions */
 #define TCG_TARGET_HAS_div_i32          1
-#define TCG_TARGET_HAS_rem_i32          0
+#define TCG_TARGET_HAS_rem_i32          have_isa_3_00
 #define TCG_TARGET_HAS_rot_i32          1
 #define TCG_TARGET_HAS_ext8s_i32        1
 #define TCG_TARGET_HAS_ext16s_i32       1
@@ -117,7 +117,7 @@ extern bool have_vsx;
 #define TCG_TARGET_HAS_extrl_i64_i32    0
 #define TCG_TARGET_HAS_extrh_i64_i32    0
 #define TCG_TARGET_HAS_div_i64          1
-#define TCG_TARGET_HAS_rem_i64          0
+#define TCG_TARGET_HAS_rem_i64          have_isa_3_00
 #define TCG_TARGET_HAS_rot_i64          1
 #define TCG_TARGET_HAS_ext8s_i64        1
 #define TCG_TARGET_HAS_ext16s_i64       1
