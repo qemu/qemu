5e806fb00214142377bcbf7da8e79c62556d142c
Pavel Dovgalyuk <pavel.dovgalyuk@ispras.ru>
2022-06-20 15:05:27 +0300

    target/mips: implement Octeon-specific BBIT instructions
    This patch introduces Octeon-specific decoder and implements
    check-bit-and-jump instructions.
    
    Signed-off-by: Pavel Dovgalyuk <Pavel.Dovgalyuk@ispras.ru>
    Reviewed-by: Richard Henderson <richard.henderson@linaro.org>
    Message-Id: <165572672705.167724.16667636081912075906.stgit@pasha-ThinkPad-X280>
    Signed-off-by: Philippe Mathieu-Daudé <f4bug@amsat.org>

--- target/mips/tcg/octeon_translate.c
+++ target/mips/tcg/octeon_translate.c
@@ -14,3 +14,33 @@
 
 /* Include the auto-generated decoder.  */
 #include "decode-octeon.c.inc"
+
+static bool trans_BBIT(DisasContext *ctx, arg_BBIT *a)
+{
+    TCGv p;
+
+    if (ctx->hflags & MIPS_HFLAG_BMASK) {
+        LOG_DISAS("Branch in delay / forbidden slot at PC 0x"
+                  TARGET_FMT_lx "\n", ctx->base.pc_next);
+        generate_exception_end(ctx, EXCP_RI);
+        return true;
+    }
+
+    /* Load needed operands */
+    TCGv t0 = tcg_temp_new();
+    gen_load_gpr(t0, a->rs);
+
+    p = tcg_constant_tl(1ULL << a->p);
+    if (a->set) {
+        tcg_gen_and_tl(bcond, p, t0);
+    } else {
+        tcg_gen_andc_tl(bcond, p, t0);
+    }
+
+    ctx->hflags |= MIPS_HFLAG_BC;
+    ctx->btarget = ctx->base.pc_next + 4 + a->offset * 4;
+    ctx->hflags |= MIPS_HFLAG_BDS32;
+
+    tcg_temp_free(t0);
+    return true;
+}
