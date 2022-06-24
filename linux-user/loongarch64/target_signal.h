9d5cd6587a70d613087a284763d44ff6fe798149
Song Gao <gaosong@loongson.cn>
2022-06-24 11:10:38 +0800

    linux-user: Add LoongArch signal support
    Signed-off-by: Song Gao <gaosong@loongson.cn>
    Signed-off-by: Xiaojuan Yang <yangxiaojuan@loongson.cn>
    Message-Id: <20220624031049.1716097-3-gaosong@loongson.cn>
    [rth: Rework extctx frame allocation and locking;
          Properly read/write fcc from signal frame.]
    Signed-off-by: Richard Henderson <richard.henderson@linaro.org>

---
+++ linux-user/loongarch64/target_signal.h
@@ -0,0 +1,13 @@
+/* SPDX-License-Identifier: GPL-2.0-or-later */
+/*
+ * Copyright (c) 2021 Loongson Technology Corporation Limited
+ */
+
+#ifndef LOONGARCH_TARGET_SIGNAL_H
+#define LOONGARCH_TARGET_SIGNAL_H
+
+#include "../generic/signal.h"
+
+#define TARGET_ARCH_HAS_SIGTRAMP_PAGE 1
+
+#endif /* LOONGARCH_TARGET_SIGNAL_H */
