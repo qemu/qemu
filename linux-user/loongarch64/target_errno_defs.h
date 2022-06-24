070f7353339861e92efc44fbdc3103ccf3cae11e
Song Gao <gaosong@loongson.cn>
2022-06-24 11:10:37 +0800

    linux-user: Add LoongArch generic header files
    This includes:
    - sockbits.h
    - target_errno_defs.h
    - target_fcntl.h
    - termbits.h
    - target_resource.h
    - target_structs.h
    
    Signed-off-by: Song Gao <gaosong@loongson.cn>
    Signed-off-by: Xiaojuan Yang <yangxiaojuan@loongson.cn>
    Reviewed-by: Richard Henderson <richard.henderson@linaro.org>
    Reviewed-by: Philippe Mathieu-Daudé <f4bug@amsat.org>
    Reviewed-by: WANG Xuerui <git@xen0n.name>
    Message-Id: <20220624031049.1716097-2-gaosong@loongson.cn>
    Signed-off-by: Richard Henderson <richard.henderson@linaro.org>

---
+++ linux-user/loongarch64/target_errno_defs.h
@@ -0,0 +1,12 @@
+/* SPDX-License-Identifier: GPL-2.0-or-later */
+/*
+ * Copyright (c) 2021 Loongson Technology Corporation Limited
+ */
+
+#ifndef LOONGARCH_TARGET_ERRNO_DEFS_H
+#define LOONGARCH_TARGET_ERRNO_DEFS_H
+
+/* Target uses generic errno */
+#include "../generic/target_errno_defs.h"
+
+#endif
---
+++ linux-user/loongarch64/target_structs.h
@@ -0,0 +1,11 @@
+/* SPDX-License-Identifier: GPL-2.0-or-later */
+/*
+ * Copyright (c) 2021 Loongson Technology Corporation Limited
+ */
+
+#ifndef LOONGARCH_TARGET_STRUCTS_H
+#define LOONGARCH_TARGET_STRUCTS_H
+
+#include "../generic/target_structs.h"
+
+#endif
