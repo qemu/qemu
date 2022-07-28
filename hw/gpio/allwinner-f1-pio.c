c279acff8ab372cbbd3cc4cb2950aa3eed63b044
froloffw7 <69122526+froloffw7@users.noreply.github.com>
2022-07-28 22:27:36 +0200

    Update Copyright

--- hw/gpio/allwinner-f1-pio.c
+++ hw/gpio/allwinner-f1-pio.c
@@ -1,20 +1,17 @@
 /*
- * Allwinner F1 Clock Control Unit emulation
+ * Allwinner F1 PIO Unit emulation
  *
- * Copyright (C) 2019 Niek Linnenbank <nieklinnenbank@gmail.com>
+ * Copyright (C) 2022 froloff
  *
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 2 of the License, or
+ * This program is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License as published by the
+ * Free Software Foundation; either version 2 of the License, or
  * (at your option) any later version.
  *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License
+ * for more details.
  */
 
 #include "qemu/osdep.h"
