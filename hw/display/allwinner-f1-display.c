03e6230bc90ddeb0c3ed5f91de018c18538d5224
froloffw7 <69122526+froloffw7@users.noreply.github.com>
2022-07-28 22:25:42 +0200

    Add missing copyright

--- hw/display/allwinner-f1-display.c
+++ hw/display/allwinner-f1-display.c
@@ -1,3 +1,19 @@
+/*
+ * Allwinner F1 Display Engine Frontend Unit emulation
+ *
+ * Copyright (C) 2022 froloff
+ *
+ * This program is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License as published by the
+ * Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License
+ * for more details.
+ */
+
 #include "qemu/osdep.h"
 #include "qemu/units.h"
 #include "qemu/module.h"
