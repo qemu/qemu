3c210cdb8cbf559a395d03b213f5474412255180
froloffw7 <69122526+froloffw7@users.noreply.github.com>
2022-07-28 22:31:18 +0200

    Add missing Copyright

--- hw/gpio/fnirsi-1013d-tp.c
+++ hw/gpio/fnirsi-1013d-tp.c
@@ -1,5 +1,20 @@
 /*
- * FNIRSI I2C Touch Pad emulation
+ * FNIRSI-1013D I2C Touch Pad emulation
+ *
+ * Copyright (C) 2021 pecostm32
+ * Copyright (C) 2022 froloff
+ *
+ * This program is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License as published by the
+ * Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License
+ * for more details.
+ */
+ 
  */
 #include "qemu/osdep.h"
 #include "qemu/units.h"
