120fa5e0e6ebacd811e4d830cff8a405806d305c
Bin Meng <bin.meng@windriver.com>
2022-08-02 15:51:59 +0800

    chardev/char-socket: Update AF_UNIX for Windows
    Now that AF_UNIX has come to Windows, update the existing logic in
    qemu_chr_compute_filename() and qmp_chardev_open_socket() for Windows.
    
    Signed-off-by: Bin Meng <bin.meng@windriver.com>
    Reviewed-by: Marc-André Lureau <marcandre.lureau@redhat.com>
    Message-Id: <20220802075200.907360-4-bmeng.cn@gmail.com>

--- chardev/char-socket.c
+++ chardev/char-socket.c
@@ -557,12 +557,10 @@ static char *qemu_chr_compute_filename(SocketChardev *s)
     const char *left = "", *right = "";
 
     switch (ss->ss_family) {
-#ifndef _WIN32
     case AF_UNIX:
         return g_strdup_printf("unix:%s%s",
                                ((struct sockaddr_un *)(ss))->sun_path,
                                s->is_listen ? ",server=on" : "");
-#endif
     case AF_INET6:
         left  = "[";
         right = "]";
@@ -1372,10 +1370,12 @@ static void qmp_chardev_open_socket(Chardev *chr,
     }
 
     qemu_chr_set_feature(chr, QEMU_CHAR_FEATURE_RECONNECTABLE);
+#ifndef _WIN32
     /* TODO SOCKET_ADDRESS_FD where fd has AF_UNIX */
     if (addr->type == SOCKET_ADDRESS_TYPE_UNIX) {
         qemu_chr_set_feature(chr, QEMU_CHAR_FEATURE_FD_PASS);
     }
+#endif
 
     /*
      * In the chardev-change special-case, we shouldn't register a new yank
