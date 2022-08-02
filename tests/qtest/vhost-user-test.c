ff070f602af6163b6ef4d803d5c3bde735035fed
Alex Bennée <alex.bennee@linaro.org>
2022-08-02 10:50:08 +0100

    tests/qtest: implement stub for VHOST_USER_GET_CONFIG
    We don't implement the full solution because frankly none of the tests
    need to at the moment. We may end up re-implementing libvhostuser in
    the end.
    
    Signed-off-by: Alex Bennée <alex.bennee@linaro.org>
    Message-Id: <20220802095010.3330793-21-alex.bennee@linaro.org>
    Reviewed-by: Michael S. Tsirkin <mst@redhat.com>
    Signed-off-by: Michael S. Tsirkin <mst@redhat.com>

--- tests/qtest/vhost-user-test.c
+++ tests/qtest/vhost-user-test.c
@@ -79,6 +79,8 @@ typedef enum VhostUserRequest {
     VHOST_USER_SET_PROTOCOL_FEATURES = 16,
     VHOST_USER_GET_QUEUE_NUM = 17,
     VHOST_USER_SET_VRING_ENABLE = 18,
+    VHOST_USER_GET_CONFIG = 24,
+    VHOST_USER_SET_CONFIG = 25,
     VHOST_USER_MAX
 } VhostUserRequest;
 
@@ -370,6 +372,17 @@ static void chr_read(void *opaque, const uint8_t *buf, int size)
         if (s->vu_ops->get_protocol_features) {
             s->vu_ops->get_protocol_features(s, chr, &msg);
         }
+        break;
+
+    case VHOST_USER_GET_CONFIG:
+        /*
+         * Treat GET_CONFIG as a NOP and just reply and let the guest
+         * consider we have updated its memory. Tests currently don't
+         * require working configs.
+         */
+        msg.flags |= VHOST_USER_REPLY_MASK;
+        p = (uint8_t *) &msg;
+        qemu_chr_fe_write_all(chr, p, VHOST_USER_HDR_SIZE + msg.size);
         break;
 
     case VHOST_USER_SET_PROTOCOL_FEATURES:
