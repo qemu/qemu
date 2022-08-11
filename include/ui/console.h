df6322a8973b5e69bdc8931ff79d3bfe3901cab5
Cal Peake <cp@absolutedigital.net>
2022-08-11 18:01:38 -0400

    ui/console: Get tab completion working again in the SDL monitor vc
    Define a QEMU special key constant for the tab key and add an entry for
    it in the qcode_to_keysym table. This allows tab completion to work again
    in the SDL monitor virtual console, which has been broken ever since the
    migration from SDL1 to SDL2.
    
    Signed-off-by: Cal Peake <cp@absolutedigital.net>
    Message-Id: <7054816e-99c-7e2-6737-7cf98cc56e2@absolutedigital.net>
    Signed-off-by: Gerd Hoffmann <kraxel@redhat.com>

--- include/ui/console.h
+++ include/ui/console.h
@@ -70,6 +70,7 @@ void hmp_mouse_set(Monitor *mon, const QDict *qdict);
 /* keysym is a unicode code except for special keys (see QEMU_KEY_xxx
    constants) */
 #define QEMU_KEY_ESC1(c) ((c) | 0xe100)
+#define QEMU_KEY_TAB        0x0009
 #define QEMU_KEY_BACKSPACE  0x007f
 #define QEMU_KEY_UP         QEMU_KEY_ESC1('A')
 #define QEMU_KEY_DOWN       QEMU_KEY_ESC1('B')
