c891c24b1a4f5496cdb5f6afff9dae146334c7d3
Claudio Imbrenda <imbrenda@linux.ibm.com>
2022-08-12 15:34:53 +0200

    os-posix: asynchronous teardown for shutdown on Linux
    This patch adds support for asynchronously tearing down a VM on Linux.
    
    When qemu terminates, either naturally or because of a fatal signal,
    the VM is torn down. If the VM is huge, it can take a considerable
    amount of time for it to be cleaned up. In case of a protected VM, it
    might take even longer than a non-protected VM (this is the case on
    s390x, for example).
    
    Some users might want to shut down a VM and restart it immediately,
    without having to wait. This is especially true if management
    infrastructure like libvirt is used.
    
    This patch implements a simple trick on Linux to allow qemu to return
    immediately, with the teardown of the VM being performed
    asynchronously.
    
    If the new commandline option -async-teardown is used, a new process is
    spawned from qemu at startup, using the clone syscall, in such way that
    it will share its address space with qemu.The new process will have the
    name "cleanup/<QEMU_PID>". It will wait until qemu terminates
    completely, and then it will exit itself.
    
    This allows qemu to terminate quickly, without having to wait for the
    whole address space to be torn down. The cleanup process will exit
    after qemu, so it will be the last user of the address space, and
    therefore it will take care of the actual teardown. The cleanup
    process will share the same cgroups as qemu, so both memory usage and
    cpu time will be accounted properly.
    
    If possible, close_range will be used in the cleanup process to close
    all open file descriptors. If it is not available or if it fails, /proc
    will be used to determine which file descriptors to close.
    
    If the cleanup process is forcefully killed with SIGKILL before the
    main qemu process has terminated completely, the mechanism is defeated
    and the teardown will not be asynchronous.
    
    This feature can already be used with libvirt by adding the following
    to the XML domain definition to pass the parameter to qemu directly:
    
      <commandline xmlns="http://libvirt.org/schemas/domain/qemu/1.0">
      <arg value='-async-teardown'/>
      </commandline>
    
    Signed-off-by: Claudio Imbrenda <imbrenda@linux.ibm.com>
    Reviewed-by: Murilo Opsfelder Araujo <muriloo@linux.ibm.com>
    Tested-by: Murilo Opsfelder Araujo <muriloo@linux.ibm.com>
    Message-Id: <20220812133453.82671-1-imbrenda@linux.ibm.com>
    Signed-off-by: Paolo Bonzini <pbonzini@redhat.com>

---
+++ include/qemu/async-teardown.h
@@ -0,0 +1,22 @@
+/*
+ * Asynchronous teardown
+ *
+ * Copyright IBM, Corp. 2022
+ *
+ * Authors:
+ *  Claudio Imbrenda <imbrenda@linux.ibm.com>
+ *
+ * This work is licensed under the terms of the GNU GPL, version 2 or (at your
+ * option) any later version.  See the COPYING file in the top-level directory.
+ *
+ */
+#ifndef QEMU_ASYNC_TEARDOWN_H
+#define QEMU_ASYNC_TEARDOWN_H
+
+#include "config-host.h"
+
+#ifdef CONFIG_LINUX
+void init_async_teardown(void);
+#endif
+
+#endif
--- os-posix.c
+++ os-posix.c
@@ -39,6 +39,7 @@
 
 #ifdef CONFIG_LINUX
 #include <sys/prctl.h>
+#include "qemu/async-teardown.h"
 #endif
 
 /*
@@ -150,6 +151,11 @@ int os_parse_cmd_args(int index, const char *optarg)
     case QEMU_OPTION_daemonize:
         daemonize = 1;
         break;
+#if defined(CONFIG_LINUX)
+    case QEMU_OPTION_asyncteardown:
+        init_async_teardown();
+        break;
+#endif
     default:
         return -1;
     }
