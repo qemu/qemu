7fcc3f759536b9a6e07d979e73b81ccdc4751c01
Thomas Huth <thuth@redhat.com>
2022-09-14 16:59:22 +0100

    tests/avocado/boot_linux_console: Fix the test_aarch64_xlnx_versal_virt test
    The assets that this test tries to download have been removed from the
    server. Update to a newer version to get it working again.
    
    Signed-off-by: Thomas Huth <thuth@redhat.com>
    Reviewed-by: Alistair Francis <alistair.francis@wdc.com>
    Message-Id: <20220829080940.110831-1-thuth@redhat.com>
    Signed-off-by: Alex Bennée <alex.bennee@linaro.org>
    Message-Id: <20220914155950.804707-3-alex.bennee@linaro.org>

--- tests/avocado/boot_linux_console.py
+++ tests/avocado/boot_linux_console.py
@@ -335,13 +335,13 @@ class BootLinuxConsole(LinuxKernelTest):
         """
         images_url = ('http://ports.ubuntu.com/ubuntu-ports/dists/'
                       'bionic-updates/main/installer-arm64/'
-                      '20101020ubuntu543.15/images/')
+                      '20101020ubuntu543.19/images/')
         kernel_url = images_url + 'netboot/ubuntu-installer/arm64/linux'
-        kernel_hash = '5bfc54cf7ed8157d93f6e5b0241e727b6dc22c50'
+        kernel_hash = 'e167757620640eb26de0972f578741924abb3a82'
         kernel_path = self.fetch_asset(kernel_url, asset_hash=kernel_hash)
 
         initrd_url = images_url + 'netboot/ubuntu-installer/arm64/initrd.gz'
-        initrd_hash = 'd385d3e88d53e2004c5d43cbe668b458a094f772'
+        initrd_hash = 'cab5cb3fcefca8408aa5aae57f24574bfce8bdb9'
         initrd_path = self.fetch_asset(initrd_url, asset_hash=initrd_hash)
 
         self.vm.set_console()
