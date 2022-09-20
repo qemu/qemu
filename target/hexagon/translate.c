e2be9a5c5f65a2cbde4915e29a112439177656d7
Taylor Simpson <tsimpson@quicinc.com>
2022-09-20 01:07:46 -0700

    Hexagon (target/hexagon) Change decision to set pkt_has_store_s[01]
    We have found cases where pkt_has_store_s[01] is set incorrectly.
    This leads to generating an unnecessary store that is left over
    from a previous packet.
    
    Add an attribute to determine if an instruction is a scalar store
    The attribute is attached to the fSTORE macro (hex_common.py)
    Update the logic in decode.c that sets pkt_has_store_s[01]
    
    Signed-off-by: Taylor Simpson <tsimpson@quicinc.com>
    Reviewed-by: Richard Henderson <richard.henderson@linaro.org>
    Message-Id: <20220920080746.26791-4-tsimpson@quicinc.com>

--- target/hexagon/translate.c
+++ target/hexagon/translate.c
@@ -1,5 +1,5 @@
 /*
- *  Copyright(c) 2019-2021 Qualcomm Innovation Center, Inc. All Rights Reserved.
+ *  Copyright(c) 2019-2022 Qualcomm Innovation Center, Inc. All Rights Reserved.
  *
  *  This program is free software; you can redistribute it and/or modify
  *  it under the terms of the GNU General Public License as published by
@@ -499,10 +499,12 @@ static void process_store_log(DisasContext *ctx, Packet *pkt)
      *  slot 1 and then slot 0.  This will be important when
      *  the memory accesses overlap.
      */
-    if (pkt->pkt_has_store_s1 && !pkt->pkt_has_dczeroa) {
+    if (pkt->pkt_has_store_s1) {
+        g_assert(!pkt->pkt_has_dczeroa);
         process_store(ctx, pkt, 1);
     }
-    if (pkt->pkt_has_store_s0 && !pkt->pkt_has_dczeroa) {
+    if (pkt->pkt_has_store_s0) {
+        g_assert(!pkt->pkt_has_dczeroa);
         process_store(ctx, pkt, 0);
     }
 }
@@ -665,7 +667,7 @@ static void gen_commit_packet(CPUHexagonState *env, DisasContext *ctx,
          * The dczeroa will be the store in slot 0, check that we don't have
          * a store in slot 1 or an HVX store.
          */
-        g_assert(has_store_s0 && !has_store_s1 && !has_hvx_store);
+        g_assert(!has_store_s1 && !has_hvx_store);
         process_dczeroa(ctx, pkt);
     } else if (has_hvx_store) {
         TCGv mem_idx = tcg_constant_tl(ctx->mem_idx);
