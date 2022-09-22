617b4b17fbddb31742bb9a854e6acdbee65c4fc0
Paolo Bonzini <pbonzini@redhat.com>
2022-09-22 10:49:05 +0200

    blkverify: add missing coroutine_fn annotations
    Callers of coroutine_fn must be coroutine_fn themselves, or the call
    must be within "if (qemu_in_coroutine())".  Apply coroutine_fn to
    functions where this holds.
    
    Reviewed-by: Alberto Faria <afaria@redhat.com>
    Reviewed-by: Eric Blake <eblake@redhat.com>
    Signed-off-by: Paolo Bonzini <pbonzini@redhat.com>
    Message-Id: <20220922084924.201610-8-pbonzini@redhat.com>
    Reviewed-by: Kevin Wolf <kwolf@redhat.com>
    Signed-off-by: Kevin Wolf <kwolf@redhat.com>

--- block/blkverify.c
+++ block/blkverify.c
@@ -258,7 +258,7 @@ blkverify_co_pwritev(BlockDriverState *bs, int64_t offset, int64_t bytes,
     return blkverify_co_prwv(bs, &r, offset, bytes, qiov, qiov, flags, true);
 }
 
-static int blkverify_co_flush(BlockDriverState *bs)
+static int coroutine_fn blkverify_co_flush(BlockDriverState *bs)
 {
     BDRVBlkverifyState *s = bs->opaque;
 
