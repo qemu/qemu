5f1d731c08d86528bafce1d7bfe8b359bcbaeabf
Jean-Philippe Brucker <jean-philippe@linaro.org>
2022-09-27 11:03:42 +0100

    hw/arm/virt: Fix devicetree warning about the root node
    The devicetree specification requires a 'model' property in the root
    node. Fix the corresponding dt-validate warning:
    
      /: 'model' is a required property
      From schema: dtschema/schemas/root-node.yaml
    
    Use the same name for model as for compatible. The specification
    recommends that 'compatible' follows the format 'manufacturer,model' and
    'model' follows the format 'manufacturer,model-number'. Since our
    'compatible' doesn't observe this, 'model' doesn't really need to
    either.
    
    Signed-off-by: Jean-Philippe Brucker <jean-philippe@linaro.org>
    Reviewed-by: Peter Maydell <peter.maydell@linaro.org>
    Reviewed-by: Eric Auger <eric.auger@redhat.com>
    Message-id: 20220927100347.176606-2-jean-philippe@linaro.org
    Signed-off-by: Peter Maydell <peter.maydell@linaro.org>

--- hw/arm/virt.c
+++ hw/arm/virt.c
@@ -253,6 +253,7 @@ static void create_fdt(VirtMachineState *vms)
     qemu_fdt_setprop_string(fdt, "/", "compatible", "linux,dummy-virt");
     qemu_fdt_setprop_cell(fdt, "/", "#address-cells", 0x2);
     qemu_fdt_setprop_cell(fdt, "/", "#size-cells", 0x2);
+    qemu_fdt_setprop_string(fdt, "/", "model", "linux,dummy-virt");
 
     /* /chosen must exist for load_dtb to fill in necessary properties later */
     qemu_fdt_add_subnode(fdt, "/chosen");
