a12f1a7e56b7f21fee49f419dd4220c4b9e0ee7f
Michael Labiuk <michael.labiuk@virtuozzo.com>
2022-09-30 01:35:40 +0300

    tests/x86: Add subtest with 'q35' machine type to device-plug-test
    Configure pci bridge setting to plug pci device and unplug.
    
    Signed-off-by: Michael Labiuk <michael.labiuk@virtuozzo.com>
    Message-Id: <20220929223547.1429580-3-michael.labiuk@virtuozzo.com>
    Reviewed-by: Thomas Huth <thuth@redhat.com>
    Signed-off-by: Thomas Huth <thuth@redhat.com>

--- tests/qtest/device-plug-test.c
+++ tests/qtest/device-plug-test.c
@@ -79,6 +79,19 @@ static void test_pci_unplug_request(void)
     qtest_quit(qtest);
 }
 
+static void test_q35_pci_unplug_request(void)
+{
+
+    QTestState *qtest = qtest_initf("-machine q35 "
+                                    "-device pcie-root-port,id=p1 "
+                                    "-device pcie-pci-bridge,bus=p1,id=b1 "
+                                    "-device virtio-mouse-pci,bus=b1,id=dev0");
+
+    process_device_remove(qtest, "dev0");
+
+    qtest_quit(qtest);
+}
+
 static void test_pci_unplug_json_request(void)
 {
     const char *arch = qtest_get_arch();
@@ -91,6 +104,27 @@ static void test_pci_unplug_json_request(void)
     QTestState *qtest = qtest_initf(
         "%s -device \"{'driver': 'virtio-mouse-pci', 'id': 'dev0'}\"",
         machine_addition);
+
+    process_device_remove(qtest, "dev0");
+
+    qtest_quit(qtest);
+}
+
+static void test_q35_pci_unplug_json_request(void)
+{
+    const char *port = "-device '{\"driver\": \"pcie-root-port\", "
+                                      "\"id\": \"p1\"}'";
+
+    const char *bridge = "-device '{\"driver\": \"pcie-pci-bridge\", "
+                                   "\"id\": \"b1\", "
+                                   "\"bus\": \"p1\"}'";
+
+    const char *device = "-device '{\"driver\": \"virtio-mouse-pci\", "
+                                   "\"bus\": \"b1\", "
+                                   "\"id\": \"dev0\"}'";
+
+    QTestState *qtest = qtest_initf("-machine q35 %s %s %s",
+                                    port, bridge, device);
 
     process_device_remove(qtest, "dev0");
 
@@ -174,6 +208,13 @@ int main(int argc, char **argv)
                        test_spapr_memory_unplug_request);
         qtest_add_func("/device-plug/spapr-phb-unplug-request",
                        test_spapr_phb_unplug_request);
+    }
+
+    if (!strcmp(arch, "x86_64") && qtest_has_machine("q35")) {
+        qtest_add_func("/device-plug/q35-pci-unplug-request",
+                   test_q35_pci_unplug_request);
+        qtest_add_func("/device-plug/q35-pci-unplug-json-request",
+                   test_q35_pci_unplug_json_request);
     }
 
     return g_test_run();
