8cc5583abe6419e7faaebc9fbd109f34f4c850f2
Venu Busireddy <venu.busireddy@oracle.com>
2022-10-06 14:49:46 -0500

    virtio-scsi: Send "REPORTED LUNS CHANGED" sense data upon disk hotplug events
    Section 5.6.6.3 of VirtIO specification states, "Events will also
    be reported via sense codes..." However, no sense data is sent when
    VIRTIO_SCSI_EVT_RESET_RESCAN or VIRTIO_SCSI_EVT_RESET_REMOVED events
    are reported (when disk hotplug/hotunplug events occur). SCSI layer
    on Solaris depends on this sense data, and hence does not handle disk
    hotplug/hotunplug events.
    
    When the disk inventory changes, use the bus unit attention mechanism
    to return a CHECK_CONDITION status with sense data of 0x06/0x3F/0x0E
    (sense code REPORTED_LUNS_CHANGED).  The first device on the bus to
    execute a command successfully will report and consume the unit
    attention status.
    
    Signed-off-by: Venu Busireddy <venu.busireddy@oracle.com>
    Message-Id: <20221006194946.24134-1-venu.busireddy@oracle.com>
    Signed-off-by: Paolo Bonzini <pbonzini@redhat.com>

--- hw/scsi/virtio-scsi.c
+++ hw/scsi/virtio-scsi.c
@@ -956,6 +956,7 @@ static void virtio_scsi_hotplug(HotplugHandler *hotplug_dev, DeviceState *dev,
         virtio_scsi_push_event(s, sd,
                                VIRTIO_SCSI_T_TRANSPORT_RESET,
                                VIRTIO_SCSI_EVT_RESET_RESCAN);
+        scsi_bus_set_ua(&s->bus, SENSE_CODE(REPORTED_LUNS_CHANGED));
         virtio_scsi_release(s);
     }
 }
@@ -973,6 +974,7 @@ static void virtio_scsi_hotunplug(HotplugHandler *hotplug_dev, DeviceState *dev,
         virtio_scsi_push_event(s, sd,
                                VIRTIO_SCSI_T_TRANSPORT_RESET,
                                VIRTIO_SCSI_EVT_RESET_REMOVED);
+        scsi_bus_set_ua(&s->bus, SENSE_CODE(REPORTED_LUNS_CHANGED));
         virtio_scsi_release(s);
     }
 
