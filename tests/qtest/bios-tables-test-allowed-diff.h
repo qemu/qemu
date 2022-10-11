3cd0c8992f1c399191772e15177e2a57a9be8051
Miguel Luis <miguel.luis@oracle.com>
2022-10-11 18:17:27 +0000

    tests/acpi: virt: allow acpi MADT and FADT changes
    Step 3 from bios-tables-test.c documented procedure.
    
    Signed-off-by: Miguel Luis <miguel.luis@oracle.com>
    Message-Id: <20221011181730.10885-2-miguel.luis@oracle.com>
    Reviewed-by: Michael S. Tsirkin <mst@redhat.com>
    Signed-off-by: Michael S. Tsirkin <mst@redhat.com>
    Acked-by: Ani Sinha <ani@anisinha.ca>

--- tests/qtest/bios-tables-test-allowed-diff.h
+++ tests/qtest/bios-tables-test-allowed-diff.h
@@ -1,1 +1,7 @@
 /* List of comma-separated changed AML files to ignore */
+"tests/data/acpi/virt/FACP",
+"tests/data/acpi/virt/FACP.numamem",
+"tests/data/acpi/virt/FACP.memhp",
+"tests/data/acpi/virt/APIC",
+"tests/data/acpi/virt/APIC.memhp",
+"tests/data/acpi/virt/APIC.numamem",
