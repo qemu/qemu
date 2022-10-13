fc6e62d26e122d6530711a6abef875b1d36f12af
Andrew Fasano <fasano@mit.edu>
2022-10-12 23:12:43 -0400

    Add tiny OSI base plugin

---
+++ contrib/plugins/osi.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <qemu-plugin.h>
+#include <plugin-qpp.h>
+#include <gmodule.h>
+#include "osi.h"
+
+QEMU_PLUGIN_EXPORT int qemu_plugin_version = QEMU_PLUGIN_VERSION;
+
+QPP_CREATE_CB(on_get_process);
+QPP_CREATE_CB(on_get_current_process_handle);
+
+QEMU_PLUGIN_EXPORT OsiProc *get_process(const OsiProcHandle *h) {
+    OsiProc *p = NULL;
+    QPP_RUN_CB(on_get_process, h, &p);
+    return p;
+}
+
+QEMU_PLUGIN_EXPORT OsiProcHandle *get_current_process_handle(void) {
+    OsiProcHandle *h = NULL;
+    QPP_RUN_CB(on_get_current_process_handle, &h);
+    return h;
+}
+
+QEMU_PLUGIN_EXPORT int qemu_plugin_install(qemu_plugin_id_t id,
+                   const qemu_info_t *info, int argc, char **argv) {
+    qemu_plugin_outs("osi_stub loaded\n");
+
+    return 0;
+}
---
+++ contrib/plugins/osi.h
@@ -0,0 +1,7 @@
+#include "osi_linux/osi_types.h"
+
+QPP_CB_PROTOTYPE(void, on_get_process, const OsiProcHandle*, OsiProc**);
+QPP_CB_PROTOTYPE(void, on_get_current_process_handle, OsiProcHandle**);
+
+QPP_FUN_PROTOTYPE(osi, OsiProc*, get_process, const OsiProcHandle*);
+QPP_FUN_PROTOTYPE(osi, OsiProcHandle*, get_current_process_handle, void);
