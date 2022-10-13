6d998f3cbfebabea7882848a65c38f454d2a37c4
Stefan Hajnoczi <stefanha@redhat.com>
2022-10-13 14:59:05 -0400

    exec/cpu-common: add qemu_ram_get_fd()
    Add a function to get the file descriptor for a RAMBlock. Device
    emulation code typically uses the MemoryRegion APIs but vhost-style code
    may use RAMBlock directly for sharing guest memory with another process.
    
    This new API will be used by the libblkio block driver so it can share
    guest memory via .bdrv_register_buf().
    
    Signed-off-by: Stefan Hajnoczi <stefanha@redhat.com>
    Message-id: 20221013185908.1297568-11-stefanha@redhat.com
    Signed-off-by: Stefan Hajnoczi <stefanha@redhat.com>

--- include/exec/cpu-common.h
+++ include/exec/cpu-common.h
@@ -92,6 +92,7 @@ void qemu_ram_set_uf_zeroable(RAMBlock *rb);
 bool qemu_ram_is_migratable(RAMBlock *rb);
 void qemu_ram_set_migratable(RAMBlock *rb);
 void qemu_ram_unset_migratable(RAMBlock *rb);
+int qemu_ram_get_fd(RAMBlock *rb);
 
 size_t qemu_ram_pagesize(RAMBlock *block);
 size_t qemu_ram_pagesize_largest(void);
