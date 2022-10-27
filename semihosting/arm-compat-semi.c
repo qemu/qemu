3878d0c7d7de9fe201513b8ee31e38e53361a97d
Bin Meng <bin.meng@windriver.com>
2022-10-27 19:36:16 +0100

    semihosting/arm-compat-semi: Avoid using hardcoded /tmp
    Use g_get_tmp_dir() to get the directory to use for temporary files.
    
    Signed-off-by: Bin Meng <bin.meng@windriver.com>
    Reviewed-by: Alex Bennée <alex.bennee@linaro.org>
    Signed-off-by: Alex Bennée <alex.bennee@linaro.org>
    Reviewed-by: Philippe Mathieu-Daudé <philmd@linaro.org>
    Reviewed-by: Richard Henderson <richard.henderson@linaro.org>
    Message-Id: <20221006151927.2079583-2-bmeng.cn@gmail.com>
    Message-Id: <20221027183637.2772968-11-alex.bennee@linaro.org>

--- semihosting/arm-compat-semi.c
+++ semihosting/arm-compat-semi.c
@@ -503,7 +503,8 @@ void do_common_semihosting(CPUState *cs)
         GET_ARG(0);
         GET_ARG(1);
         GET_ARG(2);
-        len = asprintf(&s, "/tmp/qemu-%x%02x", getpid(), (int)arg1 & 0xff);
+        len = asprintf(&s, "%s/qemu-%x%02x", g_get_tmp_dir(),
+                       getpid(), (int)arg1 & 0xff);
         if (len < 0) {
             common_semi_set_ret(cs, -1);
             break;
