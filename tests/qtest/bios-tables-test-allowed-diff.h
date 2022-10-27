e7cb1ce2492ec9b51f8cc5d2829b62ed8e51d3cb
Brice Goglin <Brice.Goglin@inria.fr>
2022-10-27 11:00:31 +0100

    tests: acpi: add and whitelist *.hmat-noinitiator expected blobs
    .. which will be used by follow up hmat-noinitiator test-case.
    
    Signed-off-by: Brice Goglin <Brice.Goglin@inria.fr>
    Signed-off-by: Hesham Almatary <hesham.almatary@huawei.com>
    Message-Id: <20221027100037.251-3-hesham.almatary@huawei.com>
    Tested-by: Yicong Yang <yangyicong@hisilicon.com>
    Reviewed-by: Michael S. Tsirkin <mst@redhat.com>
    Signed-off-by: Michael S. Tsirkin <mst@redhat.com>

--- tests/qtest/bios-tables-test-allowed-diff.h
+++ tests/qtest/bios-tables-test-allowed-diff.h
@@ -1,1 +1,5 @@
 /* List of comma-separated changed AML files to ignore */
+"tests/data/acpi/q35/APIC.acpihmat-noinitiator",
+"tests/data/acpi/q35/DSDT.acpihmat-noinitiator",
+"tests/data/acpi/q35/HMAT.acpihmat-noinitiator",
+"tests/data/acpi/q35/SRAT.acpihmat-noinitiator",
