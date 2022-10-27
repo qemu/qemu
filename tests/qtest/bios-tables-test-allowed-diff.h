cfb4c059c6be477469c20db1808dd760f8993e66
Hesham Almatary <hesham.almatary@huawei.com>
2022-10-27 11:00:34 +0100

    tests: Add HMAT AArch64/virt empty table files
    Signed-off-by: Hesham Almatary <hesham.almatary@huawei.com>
    Message-Id: <20221027100037.251-6-hesham.almatary@huawei.com>
    Tested-by: Yicong Yang <yangyicong@hisilicon.com>
    Reviewed-by: Michael S. Tsirkin <mst@redhat.com>
    Signed-off-by: Michael S. Tsirkin <mst@redhat.com>

--- tests/qtest/bios-tables-test-allowed-diff.h
+++ tests/qtest/bios-tables-test-allowed-diff.h
@@ -1,1 +1,6 @@
 /* List of comma-separated changed AML files to ignore */
+"tests/data/acpi/virt/APIC.acpihmatvirt",
+"tests/data/acpi/virt/DSDT.acpihmatvirt",
+"tests/data/acpi/virt/HMAT.acpihmatvirt",
+"tests/data/acpi/virt/PPTT.acpihmatvirt",
+"tests/data/acpi/virt/SRAT.acpihmatvirt",
