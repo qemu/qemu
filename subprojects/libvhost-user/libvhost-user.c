266aaedc37d1e18af6c62f7f67c7e29887e32061
Stefan Weil via <qemu-devel@nongnu.org>
2022-11-26 16:25:02 +0100

    libvhost-user: Fix wrong type of argument to formatting function (reported by LGTM)
    Reviewed-by: Philippe Mathieu-Daudé <f4bug@amsat.org>
    Signed-off-by: Stefan Weil <sw@weilnetz.de>
    Message-Id: <20220422070144.1043697-2-sw@weilnetz.de>
    Signed-off-by: Laurent Vivier <laurent@vivier.eu>
    Signed-off-by: Stefan Hajnoczi <stefanha@redhat.com>
    Message-Id: <20221126152507.283271-2-sw@weilnetz.de>

--- subprojects/libvhost-user/libvhost-user.c
+++ subprojects/libvhost-user/libvhost-user.c
@@ -651,7 +651,7 @@ generate_faults(VuDev *dev) {
 
         if (ioctl(dev->postcopy_ufd, UFFDIO_REGISTER, &reg_struct)) {
             vu_panic(dev, "%s: Failed to userfault region %d "
-                          "@%p + size:%zx offset: %zx: (ufd=%d)%s\n",
+                          "@%" PRIx64 " + size:%zx offset: %zx: (ufd=%d)%s\n",
                      __func__, i,
                      dev_region->mmap_addr,
                      dev_region->size, dev_region->mmap_offset,
