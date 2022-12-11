5decd2c5218379ad8362b932461d139eab7205fa
Bin Meng <bmeng@tinylab.org>
2022-12-11 11:08:27 +0800

    hw/intc: sifive_plic: Change "priority-base" to start from interrupt source 0
    At present the SiFive PLIC model "priority-base" expects interrupt
    priority register base starting from source 1 instead source 0,
    that's why on most platforms "priority-base" is set to 0x04 except
    'opentitan' machine. 'opentitan' should have set "priority-base"
    to 0x04 too.
    
    Note the irq number calculation in sifive_plic_{read,write} is
    correct as the codes make up for the irq number by adding 1.
    
    Let's simply update "priority-base" to start from interrupt source
    0 and add a comment to make it crystal clear.
    
    Signed-off-by: Bin Meng <bmeng@tinylab.org>
    Reviewed-by: Alistair Francis <alistair.francis@wdc.com>
    Reviewed-by: Wilfred Mallawa <wilfred.mallawa@wdc.com>
    Message-Id: <20221211030829.802437-14-bmeng@tinylab.org>
    Signed-off-by: Alistair Francis <alistair.francis@wdc.com>

--- hw/intc/sifive_plic.c
+++ hw/intc/sifive_plic.c
@@ -140,7 +140,7 @@ static uint64_t sifive_plic_read(void *opaque, hwaddr addr, unsigned size)
     SiFivePLICState *plic = opaque;
 
     if (addr_between(addr, plic->priority_base, plic->num_sources << 2)) {
-        uint32_t irq = ((addr - plic->priority_base) >> 2) + 1;
+        uint32_t irq = (addr - plic->priority_base) >> 2;
 
         return plic->source_priority[irq];
     } else if (addr_between(addr, plic->pending_base, plic->num_sources >> 3)) {
@@ -187,7 +187,7 @@ static void sifive_plic_write(void *opaque, hwaddr addr, uint64_t value,
     SiFivePLICState *plic = opaque;
 
     if (addr_between(addr, plic->priority_base, plic->num_sources << 2)) {
-        uint32_t irq = ((addr - plic->priority_base) >> 2) + 1;
+        uint32_t irq = (addr - plic->priority_base) >> 2;
 
         if (((plic->num_priorities + 1) & plic->num_priorities) == 0) {
             /*
@@ -428,6 +428,7 @@ static Property sifive_plic_properties[] = {
     /* number of interrupt sources including interrupt source 0 */
     DEFINE_PROP_UINT32("num-sources", SiFivePLICState, num_sources, 1),
     DEFINE_PROP_UINT32("num-priorities", SiFivePLICState, num_priorities, 0),
+    /* interrupt priority register base starting from source 0 */
     DEFINE_PROP_UINT32("priority-base", SiFivePLICState, priority_base, 0),
     DEFINE_PROP_UINT32("pending-base", SiFivePLICState, pending_base, 0),
     DEFINE_PROP_UINT32("enable-base", SiFivePLICState, enable_base, 0),
