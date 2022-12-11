e8fe2bc11713897453580aa36085556cade63826
Bin Meng <bmeng@tinylab.org>
2022-12-11 11:08:22 +0800

    hw/intc: sifive_plic: Update "num-sources" property default value
    At present the default value of "num-sources" property is zero,
    which does not make a lot of sense, as in sifive_plic_realize()
    we see s->bitfield_words is calculated by:
    
      s->bitfield_words = (s->num_sources + 31) >> 5;
    
    if the we don't configure "num-sources" property its default value
    zero makes s->bitfield_words zero too, which isn't true because
    interrupt source 0 still occupies one word.
    
    Let's change the default value to 1 meaning that only interrupt
    source 0 is supported by default and a sanity check in realize().
    
    While we are here, add a comment to describe the exact meaning of
    this property that the number should include interrupt source 0.
    
    Signed-off-by: Bin Meng <bmeng@tinylab.org>
    Reviewed-by: Alistair Francis <alistair.francis@wdc.com>
    Message-Id: <20221211030829.802437-9-bmeng@tinylab.org>
    Signed-off-by: Alistair Francis <alistair.francis@wdc.com>

--- hw/intc/sifive_plic.c
+++ hw/intc/sifive_plic.c
@@ -363,6 +363,11 @@ static void sifive_plic_realize(DeviceState *dev, Error **errp)
 
     parse_hart_config(s);
 
+    if (!s->num_sources) {
+        error_setg(errp, "plic: invalid number of interrupt sources");
+        return;
+    }
+
     s->bitfield_words = (s->num_sources + 31) >> 5;
     s->num_enables = s->bitfield_words * s->num_addrs;
     s->source_priority = g_new0(uint32_t, s->num_sources);
@@ -420,7 +425,8 @@ static const VMStateDescription vmstate_sifive_plic = {
 static Property sifive_plic_properties[] = {
     DEFINE_PROP_STRING("hart-config", SiFivePLICState, hart_config),
     DEFINE_PROP_UINT32("hartid-base", SiFivePLICState, hartid_base, 0),
-    DEFINE_PROP_UINT32("num-sources", SiFivePLICState, num_sources, 0),
+    /* number of interrupt sources including interrupt source 0 */
+    DEFINE_PROP_UINT32("num-sources", SiFivePLICState, num_sources, 1),
     DEFINE_PROP_UINT32("num-priorities", SiFivePLICState, num_priorities, 0),
     DEFINE_PROP_UINT32("priority-base", SiFivePLICState, priority_base, 0),
     DEFINE_PROP_UINT32("pending-base", SiFivePLICState, pending_base, 0),
