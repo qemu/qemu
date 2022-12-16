712622385b6fd0c8cc6a5ff2dad8da7cbfdb0dd3
Bernhard Beschow <shentey@gmail.com>
2022-12-16 15:57:06 +0100

    hw/ppc/virtex_ml507: Prefer local over global variable
    Signed-off-by: Bernhard Beschow <shentey@gmail.com>
    Reviewed-by: Edgar E. Iglesias <edgar@zeroasic.com>
    Reviewed-by: Cédric Le Goater <clg@kaod.org>
    Reviewed-by: Philippe Mathieu-Daudé <philmd@linaro.org>
    Message-Id: <20221216145709.271940-4-shentey@gmail.com>
    Signed-off-by: Daniel Henrique Barboza <danielhb413@gmail.com>

--- hw/ppc/virtex_ml507.c
+++ hw/ppc/virtex_ml507.c
@@ -157,7 +157,7 @@ static int xilinx_load_device_tree(MachineState *machine,
     int r;
     const char *dtb_filename;
 
-    dtb_filename = current_machine->dtb;
+    dtb_filename = machine->dtb;
     if (dtb_filename) {
         fdt = load_device_tree(dtb_filename, &fdt_size);
         if (!fdt) {
