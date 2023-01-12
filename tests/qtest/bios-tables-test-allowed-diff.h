5d1aee56676c82c3bf2f678a816c0821d362a77b
Igor Mammedov <imammedo@redhat.com>
2023-01-12 15:02:56 +0100

    whitelist DSDT before adding endpoint devices to bridge testcases
    Signed-off-by: Igor Mammedov <imammedo@redhat.com>
    Message-Id: <20230112140312.3096331-25-imammedo@redhat.com>
    Reviewed-by: Michael S. Tsirkin <mst@redhat.com>
    Signed-off-by: Michael S. Tsirkin <mst@redhat.com>

--- tests/qtest/bios-tables-test-allowed-diff.h
+++ tests/qtest/bios-tables-test-allowed-diff.h
@@ -1,1 +1,5 @@
 /* List of comma-separated changed AML files to ignore */
+"tests/data/acpi/pc/DSDT.roothp",
+"tests/data/acpi/pc/DSDT.hpbrroot",
+"tests/data/acpi/q35/DSDT.bridge",
+"tests/data/acpi/q35/DSDT.multi-bridge",
