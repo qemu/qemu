9330847e6a3494a242a1180c8552ceab5d22b19d
Igor Mammedov <imammedo@redhat.com>
2023-01-12 15:03:07 +0100

    tests: acpi: whitelist DSDT blobs before removing dynamic _DSM on coldplugged bridges
    Signed-off-by: Igor Mammedov <imammedo@redhat.com>
    Message-Id: <20230112140312.3096331-36-imammedo@redhat.com>
    Reviewed-by: Michael S. Tsirkin <mst@redhat.com>
    Signed-off-by: Michael S. Tsirkin <mst@redhat.com>

--- tests/qtest/bios-tables-test-allowed-diff.h
+++ tests/qtest/bios-tables-test-allowed-diff.h
@@ -1,1 +1,5 @@
 /* List of comma-separated changed AML files to ignore */
+"tests/data/acpi/pc/DSDT.bridge",
+"tests/data/acpi/pc/DSDT.roothp",
+"tests/data/acpi/pc/DSDT.hpbridge",
+"tests/data/acpi/q35/DSDT.multi-bridge",
