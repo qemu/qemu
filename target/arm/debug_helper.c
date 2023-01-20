b3aa2f21284d1d2713d0eb0253001eeebad3d582
Evgeny Iakovlev <eiakovlev@linux.microsoft.com>
2023-01-20 16:59:29 +0100

    target/arm: provide stubs for more external debug registers
    Qemu doesn't implement Debug Communication Channel, as well as the rest
    of external debug interface. However, Microsoft Hyper-V in tries to
    access some of those registers during an EL2 context switch.
    
    Since there is no architectural way to not advertise support for external
    debug, provide RAZ/WI stubs for OSDTRRX_EL1, OSDTRTX_EL1 and OSECCR_EL1
    registers in the same way the rest of DCM is currently done. Do account
    for access traps though with access_tda.
    
    Signed-off-by: Evgeny Iakovlev <eiakovlev@linux.microsoft.com>
    Reviewed-by: Peter Maydell <peter.maydell@linaro.org>
    Message-id: 20230120155929.32384-3-eiakovlev@linux.microsoft.com
    Signed-off-by: Peter Maydell <peter.maydell@linaro.org>

--- target/arm/debug_helper.c
+++ target/arm/debug_helper.c
@@ -683,6 +683,27 @@ static const ARMCPRegInfo debug_cp_reginfo[] = {
       .access = PL0_R, .accessfn = access_tda,
       .type = ARM_CP_CONST, .resetvalue = 0 },
     /*
+     * OSDTRRX_EL1/OSDTRTX_EL1 are used for save and restore of DBGDTRRX_EL0.
+     * It is a component of the Debug Communications Channel, which is not implemented.
+     */
+    { .name = "OSDTRRX_EL1", .state = ARM_CP_STATE_BOTH, .cp = 14,
+      .opc0 = 2, .opc1 = 0, .crn = 0, .crm = 0, .opc2 = 2,
+      .access = PL1_RW, .accessfn = access_tda,
+      .type = ARM_CP_CONST, .resetvalue = 0 },
+    { .name = "OSDTRTX_EL1", .state = ARM_CP_STATE_BOTH, .cp = 14,
+      .opc0 = 2, .opc1 = 0, .crn = 0, .crm = 3, .opc2 = 2,
+      .access = PL1_RW, .accessfn = access_tda,
+      .type = ARM_CP_CONST, .resetvalue = 0 },
+    /*
+     * OSECCR_EL1 provides a mechanism for an operating system
+     * to access the contents of EDECCR. EDECCR is not implemented though,
+     * as is the rest of external device mechanism.
+     */
+    { .name = "OSECCR_EL1", .state = ARM_CP_STATE_BOTH, .cp = 14,
+      .opc0 = 2, .opc1 = 0, .crn = 0, .crm = 6, .opc2 = 2,
+      .access = PL1_RW, .accessfn = access_tda,
+      .type = ARM_CP_CONST, .resetvalue = 0 },
+    /*
      * DBGDSCRint[15,12,5:2] map to MDSCR_EL1[15,12,5:2].  Map all bits as
      * it is unlikely a guest will care.
      * We don't implement the configurable EL0 access.
