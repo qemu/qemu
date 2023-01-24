15b7646c7d0d17f41b78f3af5adb21a2e4e45a93
Markus Armbruster <armbru@redhat.com>
2023-01-24 13:19:15 +0100

    monitor: Drop unnecessary includes
    Signed-off-by: Markus Armbruster <armbru@redhat.com>
    Message-Id: <20230124121946.1139465-2-armbru@redhat.com>
    Reviewed-by: Stefan Berger <stefanb@linux.ibm.com>

--- monitor/hmp.c
+++ monitor/hmp.c
@@ -27,7 +27,6 @@
 #include "hw/qdev-core.h"
 #include "monitor-internal.h"
 #include "monitor/hmp.h"
-#include "qapi/error.h"
 #include "qapi/qmp/qdict.h"
 #include "qapi/qmp/qnum.h"
 #include "qemu/config-file.h"
@@ -37,7 +36,6 @@
 #include "qemu/option.h"
 #include "qemu/units.h"
 #include "sysemu/block-backend.h"
-#include "sysemu/runstate.h"
 #include "trace.h"
 
 static void monitor_command_cb(void *opaque, const char *cmdline,
