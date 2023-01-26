ed65e32cf52a91c620c925faa90049b8e35365e1
Marcin Juszkiewicz <marcin.juszkiewicz@linaro.org>
2023-01-26 12:44:16 +0100

    sbsa-ref: remove cortex-a76 from list of supported cpus
    Cortex-A76 supports 40bits of address space. sbsa-ref's memory
    starts above this limit.
    
    Signed-off-by: Marcin Juszkiewicz <marcin.juszkiewicz@linaro.org>
    Reviewed-by: Alex Bennée <alex.bennee@linaro.org>
    Reviewed-by: Richard Henderson <richard.henderson@linaro.org>
    Message-id: 20230126114416.2447685-1-marcin.juszkiewicz@linaro.org
    Signed-off-by: Peter Maydell <peter.maydell@linaro.org>

--- hw/arm/sbsa-ref.c
+++ hw/arm/sbsa-ref.c
@@ -146,7 +146,6 @@ static const int sbsa_ref_irqmap[] = {
 static const char * const valid_cpus[] = {
     ARM_CPU_TYPE_NAME("cortex-a57"),
     ARM_CPU_TYPE_NAME("cortex-a72"),
-    ARM_CPU_TYPE_NAME("cortex-a76"),
     ARM_CPU_TYPE_NAME("neoverse-n1"),
     ARM_CPU_TYPE_NAME("max"),
 };
