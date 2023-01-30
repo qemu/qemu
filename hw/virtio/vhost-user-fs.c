331acddc87b739c64b936ba4e58518f8491f1c6b
Akihiko Odaki <akihiko.odaki@daynix.com>
2023-01-30 23:02:25 +0900

    vhost-user-fs: Back up vqs before cleaning up vhost_dev
    vhost_dev_cleanup() clears vhost_dev so back up its vqs member to free
    the memory pointed by the member.
    
    Fixes: 98fc1ada4c ("virtio: add vhost-user-fs base device")
    Signed-off-by: Akihiko Odaki <akihiko.odaki@daynix.com>
    Signed-off-by: Stefan Hajnoczi <stefanha@redhat.com>
    Message-Id: <20230130140225.77964-1-akihiko.odaki@daynix.com>

--- hw/virtio/vhost-user-fs.c
+++ hw/virtio/vhost-user-fs.c
@@ -273,6 +273,7 @@ static void vuf_device_unrealize(DeviceState *dev)
 {
     VirtIODevice *vdev = VIRTIO_DEVICE(dev);
     VHostUserFS *fs = VHOST_USER_FS(dev);
+    struct vhost_virtqueue *vhost_vqs = fs->vhost_dev.vqs;
     int i;
 
     /* This will stop vhost backend if appropriate. */
@@ -288,8 +289,7 @@ static void vuf_device_unrealize(DeviceState *dev)
     }
     g_free(fs->req_vqs);
     virtio_cleanup(vdev);
-    g_free(fs->vhost_dev.vqs);
-    fs->vhost_dev.vqs = NULL;
+    g_free(vhost_vqs);
 }
 
 static struct vhost_dev *vuf_get_vhost(VirtIODevice *vdev)
