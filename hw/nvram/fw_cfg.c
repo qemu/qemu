99fb11df6f69a10b8b1be0b3bc2bb08e6bbd84b6
Michael S. Tsirkin <mst@redhat.com>
2023-02-08 16:04:40 -0500

    Revert "x86: return modified setup_data only if read as memory, not as file"
    This reverts commit e935b735085dfa61d8e6d276b6f9e7687796a3c7.
    
    Fixes: e935b73508 ("x86: return modified setup_data only if read as memory, not as file")
    Signed-off-by: Michael S. Tsirkin <mst@redhat.com>
    Tested-by: Nathan Chancellor <nathan@kernel.org>
    Tested-by: Dov Murik <dovmurik@linux.ibm.com>
    Reviewed-by: Michael S. Tsirkin <mst@redhat.com>
    Signed-off-by: Michael S. Tsirkin <mst@redhat.com>
    Reviewed-by: Daniel P. Berrangé <berrange@redhat.com>
    (cherry picked from commit ae80d81cfa865cbe443543679e013e7fa5fcd12c)
    Signed-off-by: Michael Tokarev <mjt@tls.msk.ru>

--- hw/nvram/fw_cfg.c
+++ hw/nvram/fw_cfg.c
@@ -693,12 +693,12 @@ static const VMStateDescription vmstate_fw_cfg = {
     }
 };
 
-void fw_cfg_add_bytes_callback(FWCfgState *s, uint16_t key,
-                               FWCfgCallback select_cb,
-                               FWCfgWriteCallback write_cb,
-                               void *callback_opaque,
-                               void *data, size_t len,
-                               bool read_only)
+static void fw_cfg_add_bytes_callback(FWCfgState *s, uint16_t key,
+                                      FWCfgCallback select_cb,
+                                      FWCfgWriteCallback write_cb,
+                                      void *callback_opaque,
+                                      void *data, size_t len,
+                                      bool read_only)
 {
     int arch = !!(key & FW_CFG_ARCH_LOCAL);
 
