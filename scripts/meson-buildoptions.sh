bc71d58fd7f149081f89fb3a414ceb79691049db
Ilya Leoshkevich <iii@linux.ibm.com>
2023-02-10 01:52:07 +0100

    meson: Add missing libdw knobs
    Add the missing meson infrastructure bits for the new libdw
    dependency. Model them after the existing capstone knobs.
    
    Fixes: 7c10cb38ccb8 ("accel/tcg: Add debuginfo support")
    Reported-by: Thomas Huth <thuth@redhat.com>
    Signed-off-by: Ilya Leoshkevich <iii@linux.ibm.com>
    Reviewed-by: Thomas Huth <thuth@redhat.com>
    Message-Id: <20230210005208.438142-1-iii@linux.ibm.com>
    Signed-off-by: Thomas Huth <thuth@redhat.com>

--- scripts/meson-buildoptions.sh
+++ scripts/meson-buildoptions.sh
@@ -109,6 +109,7 @@ meson_options_help() {
   printf "%s\n" '  kvm             KVM acceleration support'
   printf "%s\n" '  l2tpv3          l2tpv3 network backend support'
   printf "%s\n" '  libdaxctl       libdaxctl support'
+  printf "%s\n" '  libdw           debuginfo support'
   printf "%s\n" '  libiscsi        libiscsi userspace initiator'
   printf "%s\n" '  libnfs          libnfs block device driver'
   printf "%s\n" '  libpmem         libpmem support'
@@ -312,6 +313,8 @@ _meson_option_parse() {
     --enable-libdaxctl) printf "%s" -Dlibdaxctl=enabled ;;
     --disable-libdaxctl) printf "%s" -Dlibdaxctl=disabled ;;
     --libdir=*) quote_sh "-Dlibdir=$2" ;;
+    --enable-libdw) printf "%s" -Dlibdw=enabled ;;
+    --disable-libdw) printf "%s" -Dlibdw=disabled ;;
     --libexecdir=*) quote_sh "-Dlibexecdir=$2" ;;
     --enable-libiscsi) printf "%s" -Dlibiscsi=enabled ;;
     --disable-libiscsi) printf "%s" -Dlibiscsi=disabled ;;
