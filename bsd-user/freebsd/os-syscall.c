fb96f5dfed8a45f44356899c92d6fcba0e7658a5
Stacey Son <sson@FreeBSD.org>
2023-02-10 09:48:19 -0700

    bsd-user: Add sysarch syscall
    Connect up the sysarch system call.
    
    Signed-off-by: Juergen Lock <nox@jelal.kn-bremen.de>
    Co-authored-by: Juergen Lock <nox@jelal.kn-bremen.de>
    Signed-off-by: Stacey Son <sson@FreeBSD.org>
    Reviewed-by: Warner Losh <imp@bsdimp.com>
    Signed-off-by: Warner Losh <imp@bsdimp.com>
    Reviewed-by: Richard Henderson <richard.henderson@linaro.org>

--- bsd-user/freebsd/os-syscall.c
+++ bsd-user/freebsd/os-syscall.c
@@ -491,6 +491,13 @@ static abi_long freebsd_syscall(void *cpu_env, int num, abi_long arg1,
         ret = do_bsd_undelete(arg1);
         break;
 
+        /*
+         * sys{ctl, arch, call}
+         */
+    case TARGET_FREEBSD_NR_sysarch: /* sysarch(2) */
+        ret = do_freebsd_sysarch(cpu_env, arg1, arg2);
+        break;
+
     default:
         qemu_log_mask(LOG_UNIMP, "Unsupported syscall: %d\n", num);
         ret = -TARGET_ENOSYS;
