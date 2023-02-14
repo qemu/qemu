60d90bf43c169b9d1dbcb17ed794b7b02c6862b1
Stefano Garzarella <sgarzare@redhat.com>
2023-02-14 18:16:21 +0100

    block: temporarily hold the new AioContext of bs_top in bdrv_append()
    bdrv_append() is called with bs_top AioContext held, but
    bdrv_attach_child_noperm() could change the AioContext of bs_top.
    
    bdrv_replace_node_noperm() calls bdrv_drained_begin() starting from
    commit 2398747128 ("block: Don't poll in bdrv_replace_child_noperm()").
    bdrv_drained_begin() can call BDRV_POLL_WHILE that assumes the new lock
    is taken, so let's temporarily hold the new AioContext to prevent QEMU
    from failing in BDRV_POLL_WHILE when it tries to release the wrong
    AioContext.
    
    Buglink: https://bugzilla.redhat.com/show_bug.cgi?id=2168209
    Reported-by: Aihua Liang <aliang@redhat.com>
    Signed-off-by: Stefano Garzarella <sgarzare@redhat.com>
    Message-Id: <20230214171621.11574-1-sgarzare@redhat.com>
    Reviewed-by: Kevin Wolf <kwolf@redhat.com>
    Signed-off-by: Kevin Wolf <kwolf@redhat.com>

--- block.c
+++ block.c
@@ -5265,6 +5265,8 @@ int bdrv_drop_filter(BlockDriverState *bs, Error **errp)
  * child.
  *
  * This function does not create any image files.
+ *
+ * The caller must hold the AioContext lock for @bs_top.
  */
 int bdrv_append(BlockDriverState *bs_new, BlockDriverState *bs_top,
                 Error **errp)
@@ -5272,10 +5274,13 @@ int bdrv_append(BlockDriverState *bs_new, BlockDriverState *bs_top,
     int ret;
     BdrvChild *child;
     Transaction *tran = tran_new();
+    AioContext *old_context, *new_context = NULL;
 
     GLOBAL_STATE_CODE();
 
     assert(!bs_new->backing);
+
+    old_context = bdrv_get_aio_context(bs_top);
 
     child = bdrv_attach_child_noperm(bs_new, bs_top, "backing",
                                      &child_of_bds, bdrv_backing_role(bs_new),
@@ -5283,6 +5288,19 @@ int bdrv_append(BlockDriverState *bs_new, BlockDriverState *bs_top,
     if (!child) {
         ret = -EINVAL;
         goto out;
+    }
+
+    /*
+     * bdrv_attach_child_noperm could change the AioContext of bs_top.
+     * bdrv_replace_node_noperm calls bdrv_drained_begin, so let's temporarily
+     * hold the new AioContext, since bdrv_drained_begin calls BDRV_POLL_WHILE
+     * that assumes the new lock is taken.
+     */
+    new_context = bdrv_get_aio_context(bs_top);
+
+    if (old_context != new_context) {
+        aio_context_release(old_context);
+        aio_context_acquire(new_context);
     }
 
     ret = bdrv_replace_node_noperm(bs_top, bs_new, true, tran, errp);
@@ -5295,6 +5313,11 @@ out:
     tran_finalize(tran, ret);
 
     bdrv_refresh_limits(bs_top, NULL, NULL);
+
+    if (new_context && old_context != new_context) {
+        aio_context_release(new_context);
+        aio_context_acquire(old_context);
+    }
 
     return ret;
 }
