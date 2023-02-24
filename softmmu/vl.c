fffa36b68e2f266c8b03ef3fdd242aa9a9181a87
Thomas Huth <thuth@redhat.com>
2023-02-24 10:05:43 +0100

    Deprecate the "-no-acpi" command line switch
    Similar to "-no-hpet", the "-no-acpi" switch is a legacy command
    line option that should be replaced with the "acpi" machine parameter
    nowadays.
    
    Message-Id: <20230224090543.1129677-1-thuth@redhat.com>
    Reviewed-by: Philippe Mathieu-Daudé <philmd@linaro.org>
    Reviewed-by: Peter Krempa <pkrempa@redhat.com>
    Reviewed-by: Igor Mammedov <imammedo@redhat.com>
    Reviewed-by: Sunil V L <sunilvl@ventanamicro.com>
    Signed-off-by: Thomas Huth <thuth@redhat.com>

--- softmmu/vl.c
+++ softmmu/vl.c
@@ -3264,6 +3264,7 @@ void qemu_init(int argc, char **argv)
                 vnc_parse(optarg);
                 break;
             case QEMU_OPTION_no_acpi:
+                warn_report("-no-acpi is deprecated, use '-machine acpi=off' instead");
                 qdict_put_str(machine_opts_dict, "acpi", "off");
                 break;
             case QEMU_OPTION_no_hpet:
