ca647d4564c91b8f2ba48218d33aebcad7bff12b
Anton Johansson <anjo@rev.ng>
2023-02-27 14:51:55 +0100

    target/microblaze: Replace `tb_pc()` with `tb->pc`
    Signed-off-by: Anton Johansson <anjo@rev.ng>
    Reviewed-by: Philippe Mathieu-Daudé <philmd@linaro.org>
    Message-Id: <20230227135202.9710-21-anjo@rev.ng>
    Signed-off-by: Richard Henderson <richard.henderson@linaro.org>

--- target/microblaze/cpu.c
+++ target/microblaze/cpu.c
@@ -30,6 +30,7 @@
 #include "exec/exec-all.h"
 #include "exec/gdbstub.h"
 #include "fpu/softfloat-helpers.h"
+#include "tcg/tcg.h"
 
 static const struct {
     const char *name;
@@ -97,7 +98,8 @@ static void mb_cpu_synchronize_from_tb(CPUState *cs,
 {
     MicroBlazeCPU *cpu = MICROBLAZE_CPU(cs);
 
-    cpu->env.pc = tb_pc(tb);
+    tcg_debug_assert(!(cs->tcg_cflags & CF_PCREL));
+    cpu->env.pc = tb->pc;
     cpu->env.iflags = tb->flags & IFLAGS_TB_MASK;
 }
 
