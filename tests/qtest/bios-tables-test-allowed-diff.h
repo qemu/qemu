30216b3eaf3b5c3c36d90a5c7402a67ed6a5fcb9
Igor Mammedov <imammedo@redhat.com>
2023-03-02 17:15:19 +0100

    tests: acpi: update expected blobs
    BNUM numbering changes across DSDT due to addition of new bridges.
    
    Fixed missing PCI tree brunch (q35/DSDT.multi-bridge case):
    
      //  -device pcie-root-port,id=rpnohp,chassis=8,addr=0xA.0,hotplug=off
      +            Device (S50)
      +            {
      +                Name (_ADR, 0x000A0000)  // _ADR: Address
      //  -device pcie-root-port,id=rp3,chassis=9,bus=rpnohp
      +                Device (S00)
      +                {
      +                    Name (_ADR, Zero)  // _ADR: Address
      +                    Name (BSEL, Zero)
      +                    Device (S00)
      +                    {
      +                        Name (_ADR, Zero)  // _ADR: Address
      +                        Name (ASUN, Zero)
      +                        Method (_DSM, 4, Serialized)  // _DSM: Device-Specific Method
      +                        {
      +                            Local0 = Package (0x02)
      +                                {
      +                                    BSEL,
      +                                    ASUN
      +                                }
      +                            Return (PDSM (Arg0, Arg1, Arg2, Arg3, Local0))
      +                        }
      +
      +                        Name (_SUN, Zero)  // _SUN: Slot User Number
      +                        Method (_EJ0, 1, NotSerialized)  // _EJx: Eject Device
      +                        {
      +                            PCEJ (BSEL, _SUN)
      +                        }
      +                    }
      +
      +                    Method (DVNT, 2, NotSerialized)
      +                    {
      +                        If ((Arg0 & One))
      +                        {
      +                            Notify (S00, Arg1)
      +                        }
      +                    }
      +                }
      +            }
    
    Fixed hotplug notification for leaf root port (hotplug=on) attached to
    intermediate root port (hotplug=off) (q35/DSDT.multi-bridge case)
    
      //  -device pcie-root-port,id=rpnohp,chassis=8,addr=0xA.0,hotplug=off
      +        Scope (S50)
      +        {
      //  -device pcie-root-port,id=rp3,chassis=9,bus=rpnohp
      +            Scope (S00)
      +            {
      +                Method (PCNT, 0, NotSerialized)
      +                {
      +                    BNUM = Zero
      +                    DVNT (PCIU, One)
      +                    DVNT (PCID, 0x03)
      +                }
      +            }
      +
      +            Method (PCNT, 0, NotSerialized)
      +            {
      +                ^S00.PCNT ()
      +            }
      +        }
      ...
               Method (PCNT, 0, NotSerialized)
               {
      +            ^S50.PCNT ()
                   ^S13.PCNT ()
    
    Populated slots being described on coldplugged bridges even if
    ACPI bridge hotplug is disabled.
    (pc/DSDT.hpbridge and pc/DSDT.hpbrroot)
      ...
                   Device (S18)
                   {
                       Name (_ADR, 0x00030000)  // _ADR: Address
      +                Device (S08)
      +                {
      +                    Name (_ADR, 0x00010000)  // _ADR: Address
      +                }
      +
      +                Device (S10)
      +                {
      +                    Name (_ADR, 0x00020000)  // _ADR: Address
      +                }
                   }
      ...
                   Device (S18)
                   {
                       Name (_ADR, 0x00030000)  // _ADR: Address
      +                Device (S00)
      +                {
      +                    Name (_ADR, Zero)  // _ADR: Address
      +                }
                   }
    
    Signed-off-by: Igor Mammedov <imammedo@redhat.com>
    Message-Id: <20230302161543.286002-11-imammedo@redhat.com>

--- tests/qtest/bios-tables-test-allowed-diff.h
+++ tests/qtest/bios-tables-test-allowed-diff.h
@@ -1,5 +1,1 @@
 /* List of comma-separated changed AML files to ignore */
-"tests/data/acpi/q35/DSDT.multi-bridge",
-"tests/data/acpi/pc/DSDT.hpbridge",
-"tests/data/acpi/pc/DSDT.hpbrroot",
-"tests/data/acpi/q35/DSDT.noacpihp",
