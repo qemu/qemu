b804b35b1c8a0edfd127ac20819c234be55ac7fc
Juan Quintela <quintela@redhat.com>
2023-03-02 12:49:16 +0100

    migration: Create migrate_block_bitmap_mapping() function
    Notice that we changed the test of ->has_block_bitmap_mapping
    for the test that block_bitmap_mapping is not NULL.
    
    Signed-off-by: Juan Quintela <quintela@redhat.com>
    Reviewed-by: Vladimir Sementsov-Ogievskiy <vsementsov@yandex-team.ru>
    
    ---
    
    Make it return const (vladimir)

--- migration/options.c
+++ migration/options.c
@@ -455,6 +455,13 @@ void qmp_migrate_set_capabilities(MigrationCapabilityStatusList *params,
 
 /* parameters */
 
+const BitmapMigrationNodeAliasList *migrate_block_bitmap_mapping(void)
+{
+    MigrationState *s = migrate_get_current();
+
+    return s->parameters.block_bitmap_mapping;
+}
+
 bool migrate_block_incremental(void)
 {
     MigrationState *s = migrate_get_current();
