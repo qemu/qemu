9a6ef182c03eaa138bae553f0fbb5a123bef9a53
Jonathan Cameron <Jonathan.Cameron@huawei.com>
2023-03-02 13:37:03 +0000

    hw/pci/aer: Add missing routing for AER errors
    PCIe r6.0 Figure 6-3 "Pseudo Logic Diagram for Selected Error Message Control
    and Status Bits" includes a right hand branch under "All PCI Express devices"
    that allows for messages to be generated or sent onwards without SERR#
    being set as long as the appropriate per error class bit in the PCIe
    Device Control Register is set.
    
    Implement that branch thus enabling routing of ERR_COR, ERR_NONFATAL
    and ERR_FATAL under OSes that set these bits appropriately (e.g. Linux)
    
    Signed-off-by: Jonathan Cameron <Jonathan.Cameron@huawei.com>
    Reviewed-by: Dave Jiang <dave.jiang@intel.com>
    Message-Id: <20230302133709.30373-3-Jonathan.Cameron@huawei.com>
    Reviewed-by: Michael S. Tsirkin <mst@redhat.com>
    Signed-off-by: Michael S. Tsirkin <mst@redhat.com>
    Reviewed-by: Fan Ni <fan.ni@samsung.com>

--- hw/pci/pcie_aer.c
+++ hw/pci/pcie_aer.c
@@ -192,8 +192,16 @@ static void pcie_aer_update_uncor_status(PCIDevice *dev)
 static bool
 pcie_aer_msg_alldev(PCIDevice *dev, const PCIEAERMsg *msg)
 {
+    uint16_t devctl = pci_get_word(dev->config + dev->exp.exp_cap +
+                                   PCI_EXP_DEVCTL);
     if (!(pcie_aer_msg_is_uncor(msg) &&
-          (pci_get_word(dev->config + PCI_COMMAND) & PCI_COMMAND_SERR))) {
+          (pci_get_word(dev->config + PCI_COMMAND) & PCI_COMMAND_SERR)) &&
+        !((msg->severity == PCI_ERR_ROOT_CMD_NONFATAL_EN) &&
+          (devctl & PCI_EXP_DEVCTL_NFERE)) &&
+        !((msg->severity == PCI_ERR_ROOT_CMD_COR_EN) &&
+          (devctl & PCI_EXP_DEVCTL_CERE)) &&
+        !((msg->severity == PCI_ERR_ROOT_CMD_FATAL_EN) &&
+          (devctl & PCI_EXP_DEVCTL_FERE))) {
         return false;
     }
 
