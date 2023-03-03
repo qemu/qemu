a957a88416ecbec51e147cba9fe89b93f6646b3b
Jiaxi Chen <jiaxi.chen@linux.intel.com>
2023-03-03 14:59:10 +0800

    target/i386: Add support for AVX-IFMA in CPUID enumeration
    AVX-IFMA is a new instruction in the latest Intel platform Sierra
    Forest. This instruction packed multiplies unsigned 52-bit integers and
    adds the low/high 52-bit products to Qword Accumulators.
    
    The bit definition:
    CPUID.(EAX=7,ECX=1):EAX[bit 23]
    
    Add CPUID definition for AVX-IFMA.
    
    Signed-off-by: Jiaxi Chen <jiaxi.chen@linux.intel.com>
    Signed-off-by: Tao Su <tao1.su@linux.intel.com>
    Reviewed-by: Xiaoyao Li <xiaoyao.li@intel.com>
    Message-Id: <20230303065913.1246327-4-tao1.su@linux.intel.com>
    Signed-off-by: Paolo Bonzini <pbonzini@redhat.com>

--- target/i386/cpu.h
+++ target/i386/cpu.h
@@ -917,6 +917,8 @@ uint64_t x86_cpu_get_supported_feature_word(FeatureWord w,
 #define CPUID_7_1_EAX_FSRC              (1U << 12)
 /* Support Tile Computational Operations on FP16 Numbers */
 #define CPUID_7_1_EAX_AMX_FP16          (1U << 21)
+/* Support for VPMADD52[H,L]UQ */
+#define CPUID_7_1_EAX_AVX_IFMA          (1U << 23)
 
 /* XFD Extend Feature Disabled */
 #define CPUID_D_1_EAX_XFD               (1U << 4)
