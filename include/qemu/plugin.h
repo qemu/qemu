aa4cf6eb82954fa9d4bb7c5dfee9bad830bfa91d
Richard Henderson <richard.henderson@linaro.org>
2023-03-15 17:43:15 +0000

    include/qemu: Split out plugin-event.h
    The usage in hw/core/cpu.h only requires QEMU_PLUGIN_EV_MAX.
    
    Signed-off-by: Richard Henderson <richard.henderson@linaro.org>
    Reviewed-by: Philippe Mathieu-Daudé <philmd@linaro.org>
    Message-Id: <20230310195252.210956-7-richard.henderson@linaro.org>
    Signed-off-by: Alex Bennée <alex.bennee@linaro.org>
    Message-Id: <20230315174331.2959-17-alex.bennee@linaro.org>
    Reviewed-by: Emilio Cota <cota@braap.org>

--- include/qemu/plugin.h
+++ include/qemu/plugin.h
@@ -12,23 +12,8 @@
 #include "qemu/error-report.h"
 #include "qemu/queue.h"
 #include "qemu/option.h"
+#include "qemu/plugin-event.h"
 #include "exec/memopidx.h"
-
-/*
- * Events that plugins can subscribe to.
- */
-enum qemu_plugin_event {
-    QEMU_PLUGIN_EV_VCPU_INIT,
-    QEMU_PLUGIN_EV_VCPU_EXIT,
-    QEMU_PLUGIN_EV_VCPU_TB_TRANS,
-    QEMU_PLUGIN_EV_VCPU_IDLE,
-    QEMU_PLUGIN_EV_VCPU_RESUME,
-    QEMU_PLUGIN_EV_VCPU_SYSCALL,
-    QEMU_PLUGIN_EV_VCPU_SYSCALL_RET,
-    QEMU_PLUGIN_EV_FLUSH,
-    QEMU_PLUGIN_EV_ATEXIT,
-    QEMU_PLUGIN_EV_MAX, /* total number of plugin events we support */
-};
 
 /*
  * Option parsing/processing.
