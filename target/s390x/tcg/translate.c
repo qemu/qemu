0708220c5b41dc80c5bff3dbbd428ed473eab8b0
Ilya Leoshkevich <iii@linux.ibm.com>
2023-03-16 17:44:24 +0100

    target/s390x: Handle CLRL and CLGFRL with non-aligned addresses
    Use MO_ALIGN and let do_unaligned_access() generate a specification
    exception.
    
    Reported-by: Nina Schoetterl-Glausch <nsg@linux.ibm.com>
    Suggested-by: Nina Schoetterl-Glausch <nsg@linux.ibm.com>
    Signed-off-by: Ilya Leoshkevich <iii@linux.ibm.com>
    Reviewed-by: Richard Henderson <richard.henderson@linaro.org>
    Message-Id: <20230316164428.275147-9-iii@linux.ibm.com>
    Signed-off-by: Thomas Huth <thuth@redhat.com>

--- target/s390x/tcg/translate.c
+++ target/s390x/tcg/translate.c
@@ -5899,7 +5899,8 @@ static void in2_mri2_32s(DisasContext *s, DisasOps *o)
 static void in2_mri2_32u(DisasContext *s, DisasOps *o)
 {
     o->in2 = tcg_temp_new_i64();
-    tcg_gen_qemu_ld32u(o->in2, gen_ri2(s), get_mem_index(s));
+    tcg_gen_qemu_ld_tl(o->in2, gen_ri2(s), get_mem_index(s),
+                       MO_TEUL | MO_ALIGN);
 }
 #define SPEC_in2_mri2_32u 0
 
