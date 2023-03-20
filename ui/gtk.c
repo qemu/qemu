2f31663ed4b5631b5e1c79f5cdd6463e55410eb8
Erico Nunes <ernunes@redhat.com>
2023-03-20 17:08:55 +0100

    ui/gtk: use widget size for cursor motion event
    The gd_motion_event size has some calculations for the cursor position,
    which also take into account things like different size of the
    framebuffer compared to the window size.
    The use of window size makes things more difficult though, as at least
    in the case of Wayland includes the size of ui elements like a menu bar
    at the top of the window. This leads to a wrong position calculation by
    a few pixels.
    Fix it by using the size of the widget, which already returns the size
    of the actual space to render the framebuffer.
    
    Signed-off-by: Erico Nunes <ernunes@redhat.com>
    Reviewed-by: Marc-André Lureau <marcandre.lureau@redhat.com>
    Acked-by: Vivek Kasireddy <vivek.kasireddy@intel.com>
    Message-Id: <20230320160856.364319-1-ernunes@redhat.com>

--- ui/gtk.c
+++ ui/gtk.c
@@ -869,7 +869,6 @@ static gboolean gd_motion_event(GtkWidget *widget, GdkEventMotion *motion,
 {
     VirtualConsole *vc = opaque;
     GtkDisplayState *s = vc->s;
-    GdkWindow *window;
     int x, y;
     int mx, my;
     int fbh, fbw;
@@ -882,10 +881,9 @@ static gboolean gd_motion_event(GtkWidget *widget, GdkEventMotion *motion,
     fbw = surface_width(vc->gfx.ds) * vc->gfx.scale_x;
     fbh = surface_height(vc->gfx.ds) * vc->gfx.scale_y;
 
-    window = gtk_widget_get_window(vc->gfx.drawing_area);
-    ww = gdk_window_get_width(window);
-    wh = gdk_window_get_height(window);
-    ws = gdk_window_get_scale_factor(window);
+    ww = gtk_widget_get_allocated_width(widget);
+    wh = gtk_widget_get_allocated_height(widget);
+    ws = gtk_widget_get_scale_factor(widget);
 
     mx = my = 0;
     if (ww > fbw) {
