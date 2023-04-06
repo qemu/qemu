9d672e290475001fcecdcc9dc79ad088ff89d17f
Thomas De Schampheleire <thomas.de_schampheleire@nokia.com>
2023-04-06 10:00:45 +0200

    tracetool: use relative paths for '#line' preprocessor directives
    The event filename is an absolute path. Convert it to a relative path when
    writing '#line' directives, to preserve reproducibility of the generated
    output when different base paths are used.
    
    Signed-off-by: Thomas De Schampheleire <thomas.de_schampheleire@nokia.com>
    Signed-off-by: Stefan Hajnoczi <stefanha@redhat.com>
    Message-Id: <20230406080045.21696-1-thomas.de_schampheleire@nokia.com>

--- scripts/tracetool/backend/syslog.py
+++ scripts/tracetool/backend/syslog.py
@@ -12,6 +12,8 @@ __maintainer__ = "Stefan Hajnoczi"
 __email__      = "stefanha@redhat.com"
 
 
+import os.path
+
 from tracetool import out
 
 
@@ -41,7 +43,7 @@ def generate_h(event, group):
         '    }',
         cond=cond,
         event_lineno=event.lineno,
-        event_filename=event.filename,
+        event_filename=os.path.relpath(event.filename),
         name=event.name,
         fmt=event.fmt.rstrip("\n"),
         argnames=argnames)
