4caad79f8d60a5df20ceed1c396724af745c9512
Richard Henderson <richard.henderson@linaro.org>
2023-04-19 17:58:23 +0200

    tcg/s390x: Support 128-bit load/store
    Use LPQ/STPQ when 16-byte atomicity is required.
    Note that these instructions require 16-byte alignment.
    
    Reviewed-by: Peter Maydell <peter.maydell@linaro.org>
    Signed-off-by: Richard Henderson <richard.henderson@linaro.org>

--- tcg/s390x/tcg-target.h
+++ tcg/s390x/tcg-target.h
@@ -140,7 +140,7 @@ extern uint64_t s390_facilities[3];
 #define TCG_TARGET_HAS_muluh_i64      0
 #define TCG_TARGET_HAS_mulsh_i64      0
 
-#define TCG_TARGET_HAS_qemu_ldst_i128 0
+#define TCG_TARGET_HAS_qemu_ldst_i128 1
 
 #define TCG_TARGET_HAS_v64            HAVE_FACILITY(VECTOR)
 #define TCG_TARGET_HAS_v128           HAVE_FACILITY(VECTOR)
