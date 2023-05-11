62c5e181eef9482c081bed2cdcc61c1091a7739c
Juan Quintela <quintela@redhat.com>
2023-05-11 16:12:04 +0200

    softmmu: Create qemu_target_pages_to_MiB()
    Function that convert a number of target_pages into its size in MiB.
    
    Suggested-by: Richard Henderson <richard.henderson@linaro.org>
    Richard Henderson <richard.henderson@linaro.org>
    Signed-off-by: Juan Quintela <quintela@redhat.com>
    Message-Id: <20230511141208.17779-2-quintela@redhat.com>

--- softmmu/physmem.c
+++ softmmu/physmem.c
@@ -3357,6 +3357,17 @@ int qemu_target_page_bits_min(void)
     return TARGET_PAGE_BITS_MIN;
 }
 
+/* Convert target pages to MiB (2**20). */
+size_t qemu_target_pages_to_MiB(size_t pages)
+{
+    int page_bits = TARGET_PAGE_BITS;
+
+    /* So far, the largest (non-huge) page size is 64k, i.e. 16 bits. */
+    g_assert(page_bits < 20);
+
+    return pages >> (20 - page_bits);
+}
+
 bool cpu_physical_memory_is_io(hwaddr phys_addr)
 {
     MemoryRegion*mr;
