4fc28bf08a147f24c4817fa740e24d458ed29ec2
Yin Wang <yin.wang@intel.com>
2023-05-19 10:37:58 +0800

    hw/riscv: qemu crash when NUMA nodes exceed available CPUs
    Command "qemu-system-riscv64 -machine virt
    -m 2G -smp 1 -numa node,mem=1G -numa node,mem=1G"
    would trigger this problem.Backtrace with:
     #0  0x0000555555b5b1a4 in riscv_numa_get_default_cpu_node_id  at ../hw/riscv/numa.c:211
     #1  0x00005555558ce510 in machine_numa_finish_cpu_init  at ../hw/core/machine.c:1230
     #2  0x00005555558ce9d3 in machine_run_board_init  at ../hw/core/machine.c:1346
     #3  0x0000555555aaedc3 in qemu_init_board  at ../softmmu/vl.c:2513
     #4  0x0000555555aaf064 in qmp_x_exit_preconfig  at ../softmmu/vl.c:2609
     #5  0x0000555555ab1916 in qemu_init  at ../softmmu/vl.c:3617
     #6  0x000055555585463b in main  at ../softmmu/main.c:47
    This commit fixes the issue by adding parameter checks.
    
    Reviewed-by: Alistair Francis <alistair.francis@wdc.com>
    Reviewed-by: Daniel Henrique Barboza <dbarboza@ventanamicro.com>
    Reviewed-by: LIU Zhiwei <zhiwei_liu@linux.alibaba.com>
    Reviewed-by: Weiwei Li <liweiwei@iscas.ac.cn>
    Signed-off-by: Yin Wang <yin.wang@intel.com>
    Message-Id: <20230519023758.1759434-1-yin.wang@intel.com>
    Signed-off-by: Alistair Francis <alistair.francis@wdc.com>
    (cherry picked from commit b9cedbf19cb4be04908a3a623f0f237875483499)
    Signed-off-by: Michael Tokarev <mjt@tls.msk.ru>

--- hw/riscv/numa.c
+++ hw/riscv/numa.c
@@ -207,6 +207,12 @@ int64_t riscv_numa_get_default_cpu_node_id(const MachineState *ms, int idx)
 {
     int64_t nidx = 0;
 
+    if (ms->numa_state->num_nodes > ms->smp.cpus) {
+        error_report("Number of NUMA nodes (%d)"
+                     " cannot exceed the number of available CPUs (%d).",
+                     ms->numa_state->num_nodes, ms->smp.max_cpus);
+        exit(EXIT_FAILURE);
+    }
     if (ms->numa_state->num_nodes) {
         nidx = idx / (ms->smp.cpus / ms->numa_state->num_nodes);
         if (ms->numa_state->num_nodes <= nidx) {
