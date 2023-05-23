0f7ca2bf2c6476244113132539ff2d6ef1d47b34
Akihiko Odaki <akihiko.odaki@daynix.com>
2023-05-23 11:42:54 +0900

    e1000x: Fix BPRC and MPRC
    Before this change, e1000 and the common code updated BPRC and MPRC
    depending on the matched filter, but e1000e and igb decided to update
    those counters by deriving the packet type independently. This
    inconsistency caused a multicast packet to be counted twice.
    
    Updating BPRC and MPRC depending on are fundamentally flawed anyway as
    a filter can be used for different types of packets. For example, it is
    possible to filter broadcast packets with MTA.
    
    Always determine what counters to update by inspecting the packets.
    
    Fixes: 3b27430177 ("e1000: Implementing various counters")
    Signed-off-by: Akihiko Odaki <akihiko.odaki@daynix.com>
    Reviewed-by: Sriram Yagnaraman <sriram.yagnaraman@est.tech>
    Signed-off-by: Jason Wang <jasowang@redhat.com>
    (cherry picked from commit f3f9b726afba1f53663768603189e574f80b5907)
    Signed-off-by: Michael Tokarev <mjt@tls.msk.ru>

--- hw/net/e1000x_common.h
+++ hw/net/e1000x_common.h
@@ -91,8 +91,9 @@ e1000x_update_regs_on_link_up(uint32_t *mac, uint16_t *phy)
 }
 
 void e1000x_update_rx_total_stats(uint32_t *mac,
-                                  size_t data_size,
-                                  size_t data_fcs_size);
+                                  eth_pkt_types_e pkt_type,
+                                  size_t pkt_size,
+                                  size_t pkt_fcs_size);
 
 void e1000x_core_prepare_eeprom(uint16_t       *eeprom,
                                 const uint16_t *templ,
