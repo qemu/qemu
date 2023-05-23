14e066a7c440ce4c7c89ec119ba3b45b33f808d8
Bernhard Beschow <shentey@gmail.com>
2023-05-23 21:56:07 +0200

    hw/arm/omap: Remove unused omap_uart_attach()
    The function is unused since commit
    bdad3654d3c55f478e538037d9eccd204e5fc8ee ('hw/arm/nseries: Remove
    invalid/unnecessary n8x0_uart_setup()').
    
    Signed-off-by: Bernhard Beschow <shentey@gmail.com>
    Acked-by: Michael S. Tsirkin <mst@redhat.com>
    Reviewed-by: Mark Cave-Ayland <mark.cave-ayland@ilande.co.uk>
    Reviewed-by: Philippe Mathieu-Daudé <philmd@linaro.org>
    Message-Id: <20230523195608.125820-3-shentey@gmail.com>
    Signed-off-by: Mark Cave-Ayland <mark.cave-ayland@ilande.co.uk>

--- include/hw/arm/omap.h
+++ include/hw/arm/omap.h
@@ -724,7 +724,6 @@ struct omap_uart_s *omap2_uart_init(MemoryRegion *sysmem,
                 qemu_irq txdma, qemu_irq rxdma,
                 const char *label, Chardev *chr);
 void omap_uart_reset(struct omap_uart_s *s);
-void omap_uart_attach(struct omap_uart_s *s, Chardev *chr);
 
 struct omap_mpuio_s;
 qemu_irq *omap_mpuio_in_get(struct omap_mpuio_s *s);
