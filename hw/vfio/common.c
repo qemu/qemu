6fe4f6c941923608027e10af5dd30f18d481f9b9
Joao Martins <joao.m.martins@oracle.com>
2023-05-30 19:05:56 +0100

    hw/vfio: Add number of dirty pages to vfio_get_dirty_bitmap tracepoint
    Include the number of dirty pages on the vfio_get_dirty_bitmap tracepoint.
    These are fetched from the newly added return value in
    cpu_physical_memory_set_dirty_lebitmap().
    
    Signed-off-by: Joao Martins <joao.m.martins@oracle.com>
    Reviewed-by: Cédric Le Goater <clg@redhat.com>
    Reviewed-by: Philippe Mathieu-Daudé <philmd@linaro.org>
    Acked-by: Alex Williamson <alex.williamson@redhat.com>
    Message-Id: <20230530180556.24441-3-joao.m.martins@oracle.com>
    Signed-off-by: Philippe Mathieu-Daudé <philmd@linaro.org>

--- hw/vfio/common.c
+++ hw/vfio/common.c
@@ -1747,6 +1747,7 @@ static int vfio_get_dirty_bitmap(VFIOContainer *container, uint64_t iova,
 {
     bool all_device_dirty_tracking =
         vfio_devices_all_device_dirty_tracking(container);
+    uint64_t dirty_pages;
     VFIOBitmap vbmap;
     int ret;
 
@@ -1772,11 +1773,11 @@ static int vfio_get_dirty_bitmap(VFIOContainer *container, uint64_t iova,
         goto out;
     }
 
-    cpu_physical_memory_set_dirty_lebitmap(vbmap.bitmap, ram_addr,
-                                           vbmap.pages);
+    dirty_pages = cpu_physical_memory_set_dirty_lebitmap(vbmap.bitmap, ram_addr,
+                                                         vbmap.pages);
 
     trace_vfio_get_dirty_bitmap(container->fd, iova, size, vbmap.size,
-                                ram_addr);
+                                ram_addr, dirty_pages);
 out:
     g_free(vbmap.bitmap);
 
