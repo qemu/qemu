e73f27003e777fd9b77d13e71c5268015b8ed2b6
Richard Henderson <richard.henderson@linaro.org>
2023-06-03 14:55:03 -0700

    include/exec/user: Set ABI_LLONG_ALIGNMENT to 4 for microblaze
    Based on gcc's microblaze.h setting BIGGEST_ALIGNMENT to 32 bits.
    
    Signed-off-by: Richard Henderson <richard.henderson@linaro.org>

--- include/exec/user/abitypes.h
+++ include/exec/user/abitypes.h
@@ -15,7 +15,9 @@
 #define ABI_LLONG_ALIGNMENT 2
 #endif
 
-#if (defined(TARGET_I386) && !defined(TARGET_X86_64)) || defined(TARGET_SH4)
+#if (defined(TARGET_I386) && !defined(TARGET_X86_64)) \
+    || defined(TARGET_SH4) \
+    || defined(TARGET_MICROBLAZE)
 #define ABI_LLONG_ALIGNMENT 4
 #endif
 
