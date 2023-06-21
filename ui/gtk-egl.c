37802a24eb4e535d96d6fe0273505d2b5c6528c2
Dongwon Kim <dongwon.kim@intel.com>
2023-06-21 14:31:50 -0700

    ui/gtk: set the area of the scanout texture correctly
    x and y offsets and width and height of the scanout texture
    is not correctly configured in case guest scanout frame is
    dmabuf.
    
    Cc: Gerd Hoffmann <kraxel@redhat.com>
    Cc: Marc-André Lureau <marcandre.lureau@redhat.com>
    Cc: Vivek Kasireddy <vivek.kasireddy@intel.com>
    Signed-off-by: Dongwon Kim <dongwon.kim@intel.com>
    Message-ID: <20230621213150.29573-1-dongwon.kim@intel.com>

--- ui/gtk-egl.c
+++ ui/gtk-egl.c
@@ -257,7 +257,8 @@ void gd_egl_scanout_dmabuf(DisplayChangeListener *dcl,
 
     gd_egl_scanout_texture(dcl, dmabuf->texture,
                            dmabuf->y0_top, dmabuf->width, dmabuf->height,
-                           0, 0, dmabuf->width, dmabuf->height);
+                           dmabuf->x, dmabuf->y, dmabuf->scanout_width,
+                           dmabuf->scanout_height);
 
     if (dmabuf->allow_fences) {
         vc->gfx.guest_fb.dmabuf = dmabuf;
