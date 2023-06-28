cfff72b21ebd8bb893181bb1107848cf8903ebaa
Zhao Liu <zhao1.liu@intel.com>
2023-06-28 21:54:34 +0800

    machine: Add helpers to get cores/threads per socket
    The number of cores/threads per socket are needed for smbios, and are
    also useful for other modules.
    
    Provide the helpers to wrap the calculation of cores/threads per socket
    so that we can avoid calculation errors caused by other modules miss
    topology changes.
    
    Suggested-by: Igor Mammedov <imammedo@redhat.com>
    Signed-off-by: Zhao Liu <zhao1.liu@intel.com>
    Message-Id: <20230628135437.1145805-2-zhao1.liu@linux.intel.com>
    Reviewed-by: Michael S. Tsirkin <mst@redhat.com>
    Signed-off-by: Michael S. Tsirkin <mst@redhat.com>
    (cherry picked from commit a1d027be95bc375238e5b9292c6aa661a8ddef4c)
    Signed-off-by: Michael Tokarev <mjt@tls.msk.ru>

--- hw/core/machine-smp.c
+++ hw/core/machine-smp.c
@@ -197,3 +197,13 @@ void machine_parse_smp_config(MachineState *ms,
         return;
     }
 }
+
+unsigned int machine_topo_get_cores_per_socket(const MachineState *ms)
+{
+    return ms->smp.cores * ms->smp.clusters * ms->smp.dies;
+}
+
+unsigned int machine_topo_get_threads_per_socket(const MachineState *ms)
+{
+    return ms->smp.threads * machine_topo_get_cores_per_socket(ms);
+}
