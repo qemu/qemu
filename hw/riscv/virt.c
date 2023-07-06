49554856f0b6f622ab6afdcc275d4ab2ecb3625c
Guenter Roeck <linux@roeck-us.net>
2023-07-05 20:59:37 -0700

    riscv: Generate devicetree only after machine initialization is complete
    If the devicetree is created before machine initialization is complete,
    it misses dynamic devices. Specifically, the tpm device is not added
    to the devicetree file and is therefore not instantiated in Linux.
    Load/create devicetree in virt_machine_done() to solve the problem.
    
    Cc: Daniel Henrique Barboza <dbarboza@ventanamicro.com>
    Cc: Alistair Francis <alistair23@gmail.com>
    Cc: Daniel Henrique Barboza <dbarboza@ventanamicro.c>
    Fixes: 325b7c4e75 hw/riscv: Enable TPM backends
    Signed-off-by: Guenter Roeck <linux@roeck-us.net>
    Reviewed-by: Philippe Mathieu-Daudé <philmd@linaro.org>
    Reviewed-by: Alistair Francis <alistair.francis@wdc.com>
    Message-Id: <20230706035937.1870483-1-linux@roeck-us.net>
    Signed-off-by: Alistair Francis <alistair.francis@wdc.com>

--- hw/riscv/virt.c
+++ hw/riscv/virt.c
@@ -1249,6 +1249,17 @@ static void virt_machine_done(Notifier *notifier, void *data)
     uint64_t kernel_entry = 0;
     BlockBackend *pflash_blk0;
 
+    /* load/create device tree */
+    if (machine->dtb) {
+        machine->fdt = load_device_tree(machine->dtb, &s->fdt_size);
+        if (!machine->fdt) {
+            error_report("load_device_tree() failed");
+            exit(1);
+        }
+    } else {
+        create_fdt(s, memmap);
+    }
+
     /*
      * Only direct boot kernel is currently supported for KVM VM,
      * so the "-bios" parameter is not supported when KVM is enabled.
@@ -1508,17 +1519,6 @@ static void virt_machine_init(MachineState *machine)
                                   drive_get(IF_PFLASH, 0, i));
     }
     virt_flash_map(s, system_memory);
-
-    /* load/create device tree */
-    if (machine->dtb) {
-        machine->fdt = load_device_tree(machine->dtb, &s->fdt_size);
-        if (!machine->fdt) {
-            error_report("load_device_tree() failed");
-            exit(1);
-        }
-    } else {
-        create_fdt(s, memmap);
-    }
 
     s->machine_done.notify = virt_machine_done;
     qemu_add_machine_init_done_notifier(&s->machine_done);
