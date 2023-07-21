8c0e8ed327c8ff7b48a5f2b7f5295c0541fcdbf4
Bastian Koppelmann <kbastian@mail.uni-paderborn.de>
2023-07-21 08:06:05 +0200

    target/tricore: Rename tricore_feature
    this name is used by capstone and will lead to a build failure of QEMU,
    when capstone is enabled. So we rename it to tricore_has_feature(), to
    match has_feature() in translate.c.
    
    Resolves: https://gitlab.com/qemu-project/qemu/-/issues/1774
    Signed-off-by: Bastian Koppelmann <kbastian@mail.uni-paderborn.de>
    Reviewed-by: Thomas Huth <thuth@redhat.com>
    Reviewed-by: Philippe Mathieu-Daudé <philmd@linaro.org>
    Signed-off-by: Michael Tokarev <mjt@tls.msk.ru>

--- target/tricore/op_helper.c
+++ target/tricore/op_helper.c
@@ -2584,7 +2584,7 @@ void helper_ret(CPUTriCoreState *env)
     /* PCXI = new_PCXI; */
     env->PCXI = new_PCXI;
 
-    if (tricore_feature(env, TRICORE_FEATURE_131)) {
+    if (tricore_has_feature(env, TRICORE_FEATURE_131)) {
         /* PSW = {new_PSW[31:26], PSW[25:24], new_PSW[23:0]}; */
         psw_write(env, (new_PSW & ~(0x3000000)) + (psw & (0x3000000)));
     } else { /* TRICORE_FEATURE_13 only */
@@ -2695,7 +2695,7 @@ void helper_rfm(CPUTriCoreState *env)
     env->gpr_a[10] = cpu_ldl_data(env, env->DCX+8);
     env->gpr_a[11] = cpu_ldl_data(env, env->DCX+12);
 
-    if (tricore_feature(env, TRICORE_FEATURE_131)) {
+    if (tricore_has_feature(env, TRICORE_FEATURE_131)) {
         env->DBGTCR = 0;
     }
 }
