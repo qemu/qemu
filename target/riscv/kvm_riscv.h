9634ef7eda5f5b57f03924351a213b776f6b8a23
Yong-Xuan Wang <yongxuan.wang@sifive.com>
2023-07-27 10:24:35 +0000

    target/riscv: Create an KVM AIA irqchip
    We create a vAIA chip by using the KVM_DEV_TYPE_RISCV_AIA and then set up
    the chip with the KVM_DEV_RISCV_AIA_GRP_* APIs.
    We also extend KVM accelerator to specify the KVM AIA mode. The "riscv-aia"
    parameter is passed along with --accel in QEMU command-line.
    1) "riscv-aia=emul": IMSIC is emulated by hypervisor
    2) "riscv-aia=hwaccel": use hardware guest IMSIC
    3) "riscv-aia=auto": use the hardware guest IMSICs whenever available
                         otherwise we fallback to software emulation.
    
    Signed-off-by: Yong-Xuan Wang <yongxuan.wang@sifive.com>
    Reviewed-by: Jim Shu <jim.shu@sifive.com>
    Reviewed-by: Daniel Henrique Barboza <dbarboza@ventanamicro.com>
    Reviewed-by: Andrew Jones <ajones@ventanamicro.com>
    Message-ID: <20230727102439.22554-4-yongxuan.wang@sifive.com>
    Signed-off-by: Alistair Francis <alistair.francis@wdc.com>

--- target/riscv/kvm_riscv.h
+++ target/riscv/kvm_riscv.h
@@ -22,5 +22,9 @@
 void kvm_riscv_init_user_properties(Object *cpu_obj);
 void kvm_riscv_reset_vcpu(RISCVCPU *cpu);
 void kvm_riscv_set_irq(RISCVCPU *cpu, int irq, int level);
+void kvm_riscv_aia_create(MachineState *machine, uint64_t group_shift,
+                          uint64_t aia_irq_num, uint64_t aia_msi_num,
+                          uint64_t aplic_base, uint64_t imsic_base,
+                          uint64_t guest_num);
 
 #endif
