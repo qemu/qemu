9ea17007c4ae4420ccd917eb300c7db49483a5b8
Ard Biesheuvel <ardb@kernel.org>
2023-07-31 10:40:43 +0200

    target/riscv: Use existing lookup tables for MixColumns
    The AES MixColumns and InvMixColumns operations are relatively
    expensive 4x4 matrix multiplications in GF(2^8), which is why C
    implementations usually rely on precomputed lookup tables rather than
    performing the calculations on demand.
    
    Given that we already carry those tables in QEMU, we can just grab the
    right value in the implementation of the RISC-V AES32 instructions. Note
    that the tables in question are permuted according to the respective
    Sbox, so we can omit the Sbox lookup as well in this case.
    
    Cc: Richard Henderson <richard.henderson@linaro.org>
    Cc: Philippe Mathieu-Daudé <philmd@linaro.org>
    Cc: Zewen Ye <lustrew@foxmail.com>
    Cc: Weiwei Li <liweiwei@iscas.ac.cn>
    Cc: Junqiang Wang <wangjunqiang@iscas.ac.cn>
    Signed-off-by: Ard Biesheuvel <ardb@kernel.org>
    Reviewed-by: Richard Henderson <richard.henderson@linaro.org>
    Message-ID: <20230731084043.1791984-1-ardb@kernel.org>
    Signed-off-by: Alistair Francis <alistair.francis@wdc.com>

--- target/riscv/crypto_helper.c
+++ target/riscv/crypto_helper.c
@@ -25,29 +25,6 @@
 #include "crypto/aes-round.h"
 #include "crypto/sm4.h"
 
-#define AES_XTIME(a) \
-    ((a << 1) ^ ((a & 0x80) ? 0x1b : 0))
-
-#define AES_GFMUL(a, b) (( \
-    (((b) & 0x1) ? (a) : 0) ^ \
-    (((b) & 0x2) ? AES_XTIME(a) : 0) ^ \
-    (((b) & 0x4) ? AES_XTIME(AES_XTIME(a)) : 0) ^ \
-    (((b) & 0x8) ? AES_XTIME(AES_XTIME(AES_XTIME(a))) : 0)) & 0xFF)
-
-static inline uint32_t aes_mixcolumn_byte(uint8_t x, bool fwd)
-{
-    uint32_t u;
-
-    if (fwd) {
-        u = (AES_GFMUL(x, 3) << 24) | (x << 16) | (x << 8) |
-            (AES_GFMUL(x, 2) << 0);
-    } else {
-        u = (AES_GFMUL(x, 0xb) << 24) | (AES_GFMUL(x, 0xd) << 16) |
-            (AES_GFMUL(x, 0x9) << 8) | (AES_GFMUL(x, 0xe) << 0);
-    }
-    return u;
-}
-
 #define sext32_xlen(x) (target_ulong)(int32_t)(x)
 
 static inline target_ulong aes32_operation(target_ulong shamt,
@@ -55,23 +32,20 @@ static inline target_ulong aes32_operation(target_ulong shamt,
                                            bool enc, bool mix)
 {
     uint8_t si = rs2 >> shamt;
-    uint8_t so;
     uint32_t mixed;
     target_ulong res;
 
     if (enc) {
-        so = AES_sbox[si];
         if (mix) {
-            mixed = aes_mixcolumn_byte(so, true);
+            mixed = be32_to_cpu(AES_Te0[si]);
         } else {
-            mixed = so;
+            mixed = AES_sbox[si];
         }
     } else {
-        so = AES_isbox[si];
         if (mix) {
-            mixed = aes_mixcolumn_byte(so, false);
+            mixed = be32_to_cpu(AES_Td0[si]);
         } else {
-            mixed = so;
+            mixed = AES_isbox[si];
         }
     }
     mixed = rol32(mixed, shamt);
