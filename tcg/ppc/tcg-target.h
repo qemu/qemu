cba10bb3c8875f428821608f9dfb860a97a63aa1
Richard Henderson <richard.henderson@linaro.org>
2023-08-05 01:55:23 +0000

    tcg/ppc: Implement negsetcond_*
    In the general case we simply negate.  However with isel we
    may load -1 instead of 1 with no extra effort.
    
    Consolidate EQ0 and NE0 logic.  Replace the NE0 zero-extension
    with inversion+negation of EQ0, which is never worse and may
    eliminate one insn.  Provide a special case for -EQ0.
    
    Reviewed-by: Daniel Henrique Barboza <danielhb413@gmail.com>
    Signed-off-by: Richard Henderson <richard.henderson@linaro.org>

--- tcg/ppc/tcg-target.h
+++ tcg/ppc/tcg-target.h
@@ -97,7 +97,7 @@ typedef enum {
 #define TCG_TARGET_HAS_sextract_i32     0
 #define TCG_TARGET_HAS_extract2_i32     0
 #define TCG_TARGET_HAS_movcond_i32      1
-#define TCG_TARGET_HAS_negsetcond_i32   0
+#define TCG_TARGET_HAS_negsetcond_i32   1
 #define TCG_TARGET_HAS_mulu2_i32        0
 #define TCG_TARGET_HAS_muls2_i32        0
 #define TCG_TARGET_HAS_muluh_i32        1
@@ -135,7 +135,7 @@ typedef enum {
 #define TCG_TARGET_HAS_sextract_i64     0
 #define TCG_TARGET_HAS_extract2_i64     0
 #define TCG_TARGET_HAS_movcond_i64      1
-#define TCG_TARGET_HAS_negsetcond_i64   0
+#define TCG_TARGET_HAS_negsetcond_i64   1
 #define TCG_TARGET_HAS_add2_i64         1
 #define TCG_TARGET_HAS_sub2_i64         1
 #define TCG_TARGET_HAS_mulu2_i64        0
