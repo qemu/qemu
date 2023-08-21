0b36072786c1972d4876ff799ef611be153dfaa3
Song Gao <gaosong@loongson.cn>
2023-08-21 14:59:54 +0200

    target/loongarch: Fix loongarch_la464_initfn() misses setting LSPW
    Reviewed-by: Richard Henderson <richard.henderson@linaro.org>
    Signed-off-by: Song Gao <gaosong@loongson.cn>
    Reviewed-by: Philippe Mathieu-Daudé <philmd@linaro.org>
    Message-ID: <20230817093121.1053890-11-gaosong@loongson.cn>
    Signed-off-by: Philippe Mathieu-Daudé <philmd@linaro.org>
    Message-Id: <20230821125959.28666-4-philmd@linaro.org>

--- target/loongarch/cpu.c
+++ target/loongarch/cpu.c
@@ -391,6 +391,7 @@ static void loongarch_la464_initfn(Object *obj)
     data = FIELD_DP32(data, CPUCFG2, LSX, 1),
     data = FIELD_DP32(data, CPUCFG2, LLFTP, 1);
     data = FIELD_DP32(data, CPUCFG2, LLFTP_VER, 1);
+    data = FIELD_DP32(data, CPUCFG2, LSPW, 1);
     data = FIELD_DP32(data, CPUCFG2, LAM, 1);
     env->cpucfg[2] = data;
 
