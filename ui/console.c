dc6984bdc3ebe5357b0c1d983ba4e7689a985f2e
Marc-André Lureau <marcandre.lureau@redhat.com>
2023-08-30 13:37:44 +0400

    ui/console: new_console() cannot fail
    There is no code path that could allow a NULL return there.
    
    Signed-off-by: Marc-André Lureau <marcandre.lureau@redhat.com>
    Reviewed-by: Daniel P. Berrangé <berrange@redhat.com>
    Message-Id: <20230830093843.3531473-11-marcandre.lureau@redhat.com>

--- ui/console.c
+++ ui/console.c
@@ -2480,11 +2480,6 @@ static void vc_chr_open(Chardev *chr,
         s->surface = qemu_create_displaysurface(width, height);
     }
 
-    if (!s) {
-        error_setg(errp, "cannot create text console");
-        return;
-    }
-
     s->chr = chr;
     drv->console = s;
 
