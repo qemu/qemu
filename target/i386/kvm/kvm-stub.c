3adce820cf9c454b49cabf52b829001ecb98500e
Philippe Mathieu-Daudé <philmd@linaro.org>
2023-09-04 14:43:17 +0200

    target/i386: Remove unused KVM stubs
    All these functions:
    
     - kvm_arch_get_supported_cpuid()
     - kvm_has_smm(()
     - kvm_hyperv_expand_features()
     - kvm_set_max_apic_id()
    
    are called after checking for kvm_enabled(), which is
    false when KVM is not built. Since the compiler elides
    these functions, their stubs are not used and can be
    removed.
    
    Inspired-by: Daniel Henrique Barboza <dbarboza@ventanamicro.com>
    Signed-off-by: Philippe Mathieu-Daudé <philmd@linaro.org>
    Message-ID: <20230904124325.79040-7-philmd@linaro.org>
    Signed-off-by: Paolo Bonzini <pbonzini@redhat.com>

--- target/i386/kvm/kvm-stub.c
+++ target/i386/kvm/kvm-stub.c
@@ -10,42 +10,16 @@
  *
  */
 #include "qemu/osdep.h"
-#include "cpu.h"
 #include "kvm_i386.h"
 
 #ifndef __OPTIMIZE__
-bool kvm_has_smm(void)
-{
-    return 1;
-}
-
 bool kvm_enable_x2apic(void)
 {
     return false;
-}
-
-/* This function is only called inside conditionals which we
- * rely on the compiler to optimize out when CONFIG_KVM is not
- * defined.
- */
-uint32_t kvm_arch_get_supported_cpuid(KVMState *env, uint32_t function,
-                                      uint32_t index, int reg)
-{
-    abort();
 }
 #endif
 
 bool kvm_hv_vpindex_settable(void)
 {
     return false;
-}
-
-bool kvm_hyperv_expand_features(X86CPU *cpu, Error **errp)
-{
-    abort();
-}
-
-void kvm_set_max_apic_id(uint32_t max_apic_id)
-{
-    return;
 }
