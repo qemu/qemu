c8f2eb5d414b788420b938f2ffdde891aa6c3ae8
Shameer Kolothum <shameerali.kolothum.thodi@huawei.com>
2023-09-05 10:12:46 +0100

    arm/kvm: Enable support for KVM_CAP_ARM_EAGER_SPLIT_CHUNK_SIZE
    Now that we have Eager Page Split support added for ARM in the kernel,
    enable it in Qemu. This adds,
     -eager-split-size to -accel sub-options to set the eager page split chunk size.
     -enable KVM_CAP_ARM_EAGER_SPLIT_CHUNK_SIZE.
    
    The chunk size specifies how many pages to break at a time, using a
    single allocation. Bigger the chunk size, more pages need to be
    allocated ahead of time.
    
    Reviewed-by: Gavin Shan <gshan@redhat.com>
    Signed-off-by: Shameer Kolothum <shameerali.kolothum.thodi@huawei.com>
    Message-id: 20230905091246.1931-1-shameerali.kolothum.thodi@huawei.com
    Signed-off-by: Peter Maydell <peter.maydell@linaro.org>

--- accel/kvm/kvm-all.c
+++ accel/kvm/kvm-all.c
@@ -3763,6 +3763,7 @@ static void kvm_accel_instance_init(Object *obj)
     /* KVM dirty ring is by default off */
     s->kvm_dirty_ring_size = 0;
     s->kvm_dirty_ring_with_bitmap = false;
+    s->kvm_eager_split_size = 0;
     s->notify_vmexit = NOTIFY_VMEXIT_OPTION_RUN;
     s->notify_window = 0;
     s->xen_version = 0;
