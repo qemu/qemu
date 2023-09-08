c428b392590df6364a025d5841e3e8a589ebfd4a
Paolo Bonzini <pbonzini@redhat.com>
2023-09-08 09:54:58 +0200

    block: mark aio_poll as non-coroutine
    It is forbidden to block on the event loop during a coroutine, as that
    can cause deadlocks due to recursive locking.
    
    Signed-off-by: Paolo Bonzini <pbonzini@redhat.com>
    Message-ID: <20230908075458.527013-1-pbonzini@redhat.com>
    Reviewed-by: Michael Tokarev <mjt@tls.msk.ru>
    Reviewed-by: Kevin Wolf <kwolf@redhat.com>
    Signed-off-by: Kevin Wolf <kwolf@redhat.com>

--- include/block/aio.h
+++ include/block/aio.h
@@ -467,7 +467,7 @@ void aio_dispatch(AioContext *ctx);
  * or more AIO events have completed, to ensure something has moved
  * before returning.
  */
-bool aio_poll(AioContext *ctx, bool blocking);
+bool no_coroutine_fn aio_poll(AioContext *ctx, bool blocking);
 
 /* Register a file descriptor and associated callbacks.  Behaves very similarly
  * to qemu_set_fd_handler.  Unlike qemu_set_fd_handler, these callbacks will
