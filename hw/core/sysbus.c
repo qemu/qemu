491da0af99527bb27859e576b12fa53f21ee2a13
Philippe Mathieu-Daudé <philmd@linaro.org>
2023-09-22 19:02:39 +0200

    hw/sysbus: Remove now unused sysbus_address_space()
    sysbus_address_space() is not more used, remove it.
    
    Signed-off-by: Philippe Mathieu-Daudé <philmd@linaro.org>
    Reviewed-by: Richard Henderson <richard.henderson@linaro.org>
    Message-Id: <20240216153517.49422-7-philmd@linaro.org>

--- hw/core/sysbus.c
+++ hw/core/sysbus.c
@@ -298,11 +298,6 @@ static char *sysbus_get_fw_dev_path(DeviceState *dev)
     return g_strdup(qdev_fw_name(dev));
 }
 
-MemoryRegion *sysbus_address_space(SysBusDevice *dev)
-{
-    return get_system_memory();
-}
-
 static void sysbus_device_class_init(ObjectClass *klass, void *data)
 {
     DeviceClass *k = DEVICE_CLASS(klass);
