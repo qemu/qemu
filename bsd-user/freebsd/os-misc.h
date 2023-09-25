137d963cfb1c9f0d9e76a40229df2996809b746b
Stacey Son <sson@FreeBSD.org>
2023-09-25 21:26:50 +0300

    bsd-user: Introduce freebsd/os-misc.h to the source tree
    To preserve the copyright notice and help with the 'Author' info for
    subsequent changes to the file.
    
    Signed-off-by: Stacey Son <sson@FreeBSD.org>
    Signed-off-by: Karim Taha <kariem.taha2.7@gmail.com>
    Reviewed-by: Richard Henderson <richard.henderson@linaro.org>
    Reviewed-by: Warner Losh <imp@bsdimp.com>
    Message-Id: <20230925182709.4834-5-kariem.taha2.7@gmail.com>

---
+++ bsd-user/freebsd/os-misc.h
@@ -0,0 +1,28 @@
+/*
+ *  miscellaneous FreeBSD system call shims
+ *
+ *  Copyright (c) 2013-14 Stacey D. Son
+ *
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program; if not, see <http://www.gnu.org/licenses/>.
+ */
+
+#ifndef OS_MISC_H
+#define OS_MISC_H
+
+#include <sys/cpuset.h>
+#include <sys/random.h>
+#include <sched.h>
+
+
+#endif /* OS_MISC_H */
