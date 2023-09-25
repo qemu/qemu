1d4c4026b15045e45de5c6f64e4b6322274680e7
Stacey Son <sson@FreeBSD.org>
2023-09-25 21:26:49 +0300

    bsd-user: Declarations for ipc_perm and shmid_ds conversion functions
    Signed-off-by: Stacey Son <sson@FreeBSD.org>
    Signed-off-by: Karim Taha <kariem.taha2.7@gmail.com>
    Reviewed-by: Richard Henderson <richard.henderson@linaro.org>
    Reviewed-by: Warner Losh <imp@bsdimp.com>
    Message-Id: <20230925182709.4834-4-kariem.taha2.7@gmail.com>

--- bsd-user/qemu-bsd.h
+++ bsd-user/qemu-bsd.h
@@ -22,6 +22,16 @@
 
 #include <sys/types.h>
 #include <sys/resource.h>
+#include <sys/ipc.h>
+#include <sys/msg.h>
+#include <sys/resource.h>
+#include <sys/sem.h>
+#include <sys/shm.h>
+#include <sys/socket.h>
+#include <sys/un.h>
+#include <sys/uuid.h>
+#include <sys/wait.h>
+#include <netinet/in.h>
 
 /* bsd-proc.c */
 int target_to_host_resource(int code);
@@ -34,5 +44,15 @@ abi_long host_to_target_wrusage(abi_ulong target_addr,
 int host_to_target_waitstatus(int status);
 void h2g_rusage(const struct rusage *rusage,
         struct target_freebsd_rusage *target_rusage);
+
+/* bsd-mem.c */
+void target_to_host_ipc_perm__locked(struct ipc_perm *host_ip,
+        struct target_ipc_perm *target_ip);
+void host_to_target_ipc_perm__locked(struct target_ipc_perm *target_ip,
+        struct ipc_perm *host_ip);
+abi_long target_to_host_shmid_ds(struct shmid_ds *host_sd,
+        abi_ulong target_addr);
+abi_long host_to_target_shmid_ds(abi_ulong target_addr,
+        struct shmid_ds *host_sd);
 
 #endif /* QEMU_BSD_H */
