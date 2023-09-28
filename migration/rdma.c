3c03f21cb5ee0ded870987ea32217297f639afa0
Markus Armbruster <armbru@redhat.com>
2023-09-28 15:19:36 +0200

    migration/rdma: Put @errp parameter last
    include/qapi/error.h demands:
    
     * - Functions that use Error to report errors have an Error **errp
     *   parameter.  It should be the last parameter, except for functions
     *   taking variable arguments.
    
    qemu_rdma_connect() does not conform.  Clean it up.
    
    Signed-off-by: Markus Armbruster <armbru@redhat.com>
    Reviewed-by: Fabiano Rosas <farosas@suse.de>
    Reviewed-by: Li Zhijian <lizhijian@fujitsu.com>
    Reviewed-by: Juan Quintela <quintela@redhat.com>
    Signed-off-by: Juan Quintela <quintela@redhat.com>
    Message-ID: <20230928132019.2544702-11-armbru@redhat.com>

--- migration/rdma.c
+++ migration/rdma.c
@@ -2552,7 +2552,8 @@ static int qemu_get_cm_event_timeout(RDMAContext *rdma,
     }
 }
 
-static int qemu_rdma_connect(RDMAContext *rdma, Error **errp, bool return_path)
+static int qemu_rdma_connect(RDMAContext *rdma, bool return_path,
+                             Error **errp)
 {
     RDMACapabilities cap = {
                                 .version = RDMA_CONTROL_VERSION_CURRENT,
@@ -4183,7 +4184,7 @@ void rdma_start_outgoing_migration(void *opaque,
     }
 
     trace_rdma_start_outgoing_migration_after_rdma_source_init();
-    ret = qemu_rdma_connect(rdma, errp, false);
+    ret = qemu_rdma_connect(rdma, false, errp);
 
     if (ret) {
         goto err;
@@ -4204,7 +4205,7 @@ void rdma_start_outgoing_migration(void *opaque,
             goto return_path_err;
         }
 
-        ret = qemu_rdma_connect(rdma_return_path, errp, true);
+        ret = qemu_rdma_connect(rdma_return_path, true, errp);
 
         if (ret) {
             goto return_path_err;
