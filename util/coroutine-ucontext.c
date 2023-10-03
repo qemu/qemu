9afa888ce0f816d0f2cfc95eebe4f49244c518af
Daniel P. Berrangé <berrange@redhat.com>
2023-10-03 10:15:49 +0100

    osdep: set _FORTIFY_SOURCE=2 when optimization is enabled
    Currently we set _FORTIFY_SOURCE=2 as a compiler argument when the
    meson 'optimization' setting is non-zero, the compiler is GCC and
    the target is Linux.
    
    While the default QEMU optimization level is 2, user could override
    this by setting CFLAGS="-O0" or --extra-cflags="-O0" when running
    configure and this won't be reflected in the meson 'optimization'
    setting. As a result we try to enable _FORTIFY_SOURCE=2 and then the
    user gets compile errors as it only works with optimization.
    
    Rather than trying to improve detection in meson, it is simpler to
    just check the __OPTIMIZE__ define from osdep.h.
    
    The comment about being incompatible with clang appears to be
    outdated, as compilation works fine without excluding clang.
    
    In the coroutine code we must set _FORTIFY_SOURCE=0 to stop the
    logic in osdep.h then enabling it.
    
    Signed-off-by: Daniel P. Berrangé <berrange@redhat.com>
    Message-id: 20231003091549.223020-1-berrange@redhat.com
    Signed-off-by: Stefan Hajnoczi <stefanha@redhat.com>

--- util/coroutine-ucontext.c
+++ util/coroutine-ucontext.c
@@ -19,9 +19,9 @@
  */
 
 /* XXX Is there a nicer way to disable glibc's stack check for longjmp? */
-#ifdef _FORTIFY_SOURCE
 #undef _FORTIFY_SOURCE
-#endif
+#define _FORTIFY_SOURCE 0
+
 #include "qemu/osdep.h"
 #include <ucontext.h>
 #include "qemu/coroutine_int.h"
