6da9447929a88c05bc82a6ead0c967ce1e1e838c
Philippe Mathieu-Daudé <philmd@linaro.org>
2023-10-04 11:25:08 +0200

    target/i386/hvf: Remove unused includes in 'hvf-i386.h'
    The only non standard type -- CPUArchState -- is forward
    declared in "qemu/typedefs.h", so no particular header is
    required here.
    
    Signed-off-by: Philippe Mathieu-Daudé <philmd@linaro.org>
    Reviewed-by: Roman Bolshakov <roman@roolebo.dev>
    Tested-by: Roman Bolshakov <roman@roolebo.dev>
    Message-ID: <20231004092510.39498-2-philmd@linaro.org>
    Signed-off-by: Paolo Bonzini <pbonzini@redhat.com>

--- target/i386/hvf/hvf-i386.h
+++ target/i386/hvf/hvf-i386.h
@@ -16,12 +16,6 @@
 #ifndef HVF_I386_H
 #define HVF_I386_H
 
-#include "qemu/accel.h"
-#include "sysemu/hvf.h"
-#include "sysemu/hvf_int.h"
-#include "cpu.h"
-#include "x86.h"
-
 void hvf_handle_io(CPUArchState *, uint16_t, void *, int, int, int);
 
 /* Host specific functions */
