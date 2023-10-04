82f3346ff46e1bbbe768a3084e7052668d34c1fe
Philippe Mathieu-Daudé <philmd@linaro.org>
2023-10-04 14:00:08 +0200

    plugins/loader: Clean up global variable shadowing
    Fix:
    
      include/qemu/plugin.h:245:54: error: declaration shadows a variable in the global scope [-Werror,-Wshadow]
      static inline void qemu_plugin_opt_parse(const char *optarg,
                                                           ^
      /Library/Developer/CommandLineTools/SDKs/MacOSX.sdk/usr/include/getopt.h:77:14: note: previous declaration is here
      extern char *optarg;                    /* getopt(3) external variables */
                   ^
    
    Signed-off-by: Philippe Mathieu-Daudé <philmd@linaro.org>
    Message-ID: <20231004120019.93101-6-philmd@linaro.org>
    Reviewed-by: Richard Henderson <richard.henderson@linaro.org>
    Signed-off-by: Markus Armbruster <armbru@redhat.com>

--- plugins/loader.c
+++ plugins/loader.c
@@ -140,12 +140,12 @@ static int plugin_add(void *opaque, const char *name, const char *value,
     return 0;
 }
 
-void qemu_plugin_opt_parse(const char *optarg, QemuPluginList *head)
+void qemu_plugin_opt_parse(const char *optstr, QemuPluginList *head)
 {
     struct qemu_plugin_parse_arg arg;
     QemuOpts *opts;
 
-    opts = qemu_opts_parse_noisily(qemu_find_opts("plugin"), optarg, true);
+    opts = qemu_opts_parse_noisily(qemu_find_opts("plugin"), optstr, true);
     if (opts == NULL) {
         exit(1);
     }
