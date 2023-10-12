6cdd46f66ff91a9c13c5dc4d018ae53d2f28d74a
Dave Jiang <dave.jiang@intel.com>
2023-10-12 13:56:22 +0100

    hw/cxl: Add QTG _DSM support for ACPI0017 device
    Add a simple _DSM call support for the ACPI0017 device to return fake QTG
    ID values of 0 and 1 in all cases. This for _DSM plumbing testing from the OS.
    
    Following edited for readability
    
    Device (CXLM)
    {
        Name (_HID, "ACPI0017")  // _HID: Hardware ID
    ...
        Method (_DSM, 4, Serialized)  // _DSM: Device-Specific Method
        {
            If ((Arg0 == ToUUID ("f365f9a6-a7de-4071-a66a-b40c0b4f8e52")))
            {
                If ((Arg2 == Zero))
                {
                    Return (Buffer (One) { 0x01 })
                }
    
                If ((Arg2 == One))
                {
                    Return (Package (0x02)
                    {
                        One,
                        Package (0x02)
                        {
                            Zero,
                            One
                        }
                    })
                }
            }
        }
    
    Signed-off-by: Dave Jiang <dave.jiang@intel.com>
    Signed-off-by: Jonathan Cameron <Jonathan.Cameron@huawei.com>
    Message-Id: <20231012125623.21101-3-Jonathan.Cameron@huawei.com>
    Reviewed-by: Michael S. Tsirkin <mst@redhat.com>
    Signed-off-by: Michael S. Tsirkin <mst@redhat.com>

--- hw/i386/acpi-build.c
+++ hw/i386/acpi-build.c
@@ -1417,6 +1417,7 @@ static void build_acpi0017(Aml *table)
     method = aml_method("_STA", 0, AML_NOTSERIALIZED);
     aml_append(method, aml_return(aml_int(0x01)));
     aml_append(dev, method);
+    build_cxl_dsm_method(dev);
 
     aml_append(scope, dev);
     aml_append(table, scope);
