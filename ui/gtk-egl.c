7b15f81d637103ace40c0ff4dcbbf485d1ca7bd2
Antonio Caggiano <quic_acaggian@quicinc.com>
2023-10-16 14:32:15 +0200

    ui/gtk-egl: Check EGLSurface before doing scanout
    The first time gd_egl_scanout_texture() is called, there's a possibility
    that the GTK drawing area might not be realized yet, in which case its
    associated GdkWindow is NULL. This means gd_egl_init() was also skipped
    and the EGLContext and EGLSurface stored in the VirtualGfxConsole are
    not valid yet.
    
    Continuing with the scanout in this conditions would result in hitting
    an assert in libepoxy: "Couldn't find current GLX or EGL context".
    
    A possible workaround is to just ignore the scanout request, giving the
    the GTK drawing area some time to finish its realization. At that point,
    the gd_egl_init() will succeed and the EGLContext and EGLSurface stored
    in the VirtualGfxConsole will be valid.
    
    Signed-off-by: Antonio Caggiano <quic_acaggian@quicinc.com>
    Reviewed-by: Marc-André Lureau <marcandre.lureau@redhat.com>
    Message-Id: <20231016123215.2699269-1-quic_acaggian@quicinc.com>
    (cherry picked from commit 6f189a08c1b0085808af1bfbf4567f0da193ecc1)
    Signed-off-by: Michael Tokarev <mjt@tls.msk.ru>

--- ui/gtk-egl.c
+++ ui/gtk-egl.c
@@ -234,12 +234,19 @@ void gd_egl_scanout_texture(DisplayChangeListener *dcl,
     vc->gfx.h = h;
     vc->gfx.y0_top = backing_y_0_top;
 
-    eglMakeCurrent(qemu_egl_display, vc->gfx.esurface,
-                   vc->gfx.esurface, vc->gfx.ectx);
+    if (!vc->gfx.esurface) {
+        gd_egl_init(vc);
+        if (!vc->gfx.esurface) {
+            return;
+        }
 
-    gtk_egl_set_scanout_mode(vc, true);
-    egl_fb_setup_for_tex(&vc->gfx.guest_fb, backing_width, backing_height,
-                         backing_id, false);
+        eglMakeCurrent(qemu_egl_display, vc->gfx.esurface,
+                       vc->gfx.esurface, vc->gfx.ectx);
+
+        gtk_egl_set_scanout_mode(vc, true);
+        egl_fb_setup_for_tex(&vc->gfx.guest_fb, backing_width, backing_height,
+                             backing_id, false);
+    }
 }
 
 void gd_egl_scanout_dmabuf(DisplayChangeListener *dcl,
