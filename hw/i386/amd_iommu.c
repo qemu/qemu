f79a525d8d0fe9d6fe4c1925600aa69eb82acbff
Philippe Mathieu-Daudé <philmd@linaro.org>
2023-10-18 07:50:20 +0200

    hw/i386/amd_iommu: Do not use SysBus API to map local MMIO region
    There is no point in exposing an internal MMIO region via
    SysBus and directly mapping it in the very same device.
    
    Just map it without using the SysBus API.
    
    Transformation done using the following coccinelle script:
    
      @@
      expression sbdev;
      expression index;
      expression addr;
      expression subregion;
      @@
      -    sysbus_init_mmio(sbdev, subregion);
           ... when != sbdev
      -    sysbus_mmio_map(sbdev, index, addr);
      +    memory_region_add_subregion(get_system_memory(), addr, subregion);
    
    Signed-off-by: Philippe Mathieu-Daudé <philmd@linaro.org>
    Reviewed-by: Michael S. Tsirkin <mst@redhat.com>
    Reviewed-by: Zhao Liu <zhao1.liu@intel.com>
    Message-Id: <20231018141151.87466-2-philmd@linaro.org>

--- hw/i386/amd_iommu.c
+++ hw/i386/amd_iommu.c
@@ -1579,9 +1579,8 @@ static void amdvi_sysbus_realize(DeviceState *dev, Error **errp)
     /* set up MMIO */
     memory_region_init_io(&s->mmio, OBJECT(s), &mmio_mem_ops, s, "amdvi-mmio",
                           AMDVI_MMIO_SIZE);
-
-    sysbus_init_mmio(SYS_BUS_DEVICE(s), &s->mmio);
-    sysbus_mmio_map(SYS_BUS_DEVICE(s), 0, AMDVI_BASE_ADDR);
+    memory_region_add_subregion(get_system_memory(), AMDVI_BASE_ADDR,
+                                &s->mmio);
     pci_setup_iommu(bus, amdvi_host_dma_iommu, s);
     amdvi_init(s);
 }
