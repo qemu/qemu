7cb953ca1956d4ac5d8ad798321fe3fde765befc
Zhao Liu <zhao1.liu@intel.com>
2023-10-23 17:46:33 +0800

    tests: bios-tables-test: Prepare the ACPI table change for smbios type4 thread count2 test
    Following the guidelines in tests/qtest/bios-tables-test.c, this
    is step 1 - 3.
    
    List the ACPI tables that will be added to test the thread count2 field
    of smbios type4 table.
    
    Signed-off-by: Zhao Liu <zhao1.liu@intel.com>
    Reviewed-by: Michael S. Tsirkin <mst@redhat.com>
    Message-Id: <20231023094635.1588282-15-zhao1.liu@linux.intel.com>
    Reviewed-by: Michael S. Tsirkin <mst@redhat.com>
    Signed-off-by: Michael S. Tsirkin <mst@redhat.com>

--- tests/qtest/bios-tables-test-allowed-diff.h
+++ tests/qtest/bios-tables-test-allowed-diff.h
@@ -1,1 +1,4 @@
 /* List of comma-separated changed AML files to ignore */
+"tests/data/acpi/q35/APIC.thread-count2",
+"tests/data/acpi/q35/DSDT.thread-count2",
+"tests/data/acpi/q35/FACP.thread-count2",
