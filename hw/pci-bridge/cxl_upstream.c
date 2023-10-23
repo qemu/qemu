2710d49a7c8b9b117a46847c7ace5eb21d48e882
Jonathan Cameron <Jonathan.Cameron@huawei.com>
2023-10-23 17:07:54 +0100

    hw/pci-bridge/cxl_upstream: Move defintion of device to header.
    To avoid repetition of switch upstream port specific data in the
    CXLDeviceState structure it will be necessary to access the switch USP
    specific data from mailbox callbacks. Hence move it to cxl_device.h so it
    is no longer an opaque structure.
    
    Signed-off-by: Jonathan Cameron <Jonathan.Cameron@huawei.com>
    Reviewed-by: Fan Ni <fan.ni@samsung.com>
    Message-Id: <20231023160806.13206-6-Jonathan.Cameron@huawei.com>
    Reviewed-by: Michael S. Tsirkin <mst@redhat.com>
    Signed-off-by: Michael S. Tsirkin <mst@redhat.com>

--- hw/pci-bridge/cxl_upstream.c
+++ hw/pci-bridge/cxl_upstream.c
@@ -14,6 +14,7 @@
 #include "hw/pci/msi.h"
 #include "hw/pci/pcie.h"
 #include "hw/pci/pcie_port.h"
+#include "hw/pci-bridge/cxl_upstream_port.h"
 /*
  * Null value of all Fs suggested by IEEE RA guidelines for use of
  * EU, OUI and CID
@@ -29,16 +30,6 @@
     (CXL_UPSTREAM_PORT_AER_OFFSET + PCI_ERR_SIZEOF)
 #define CXL_UPSTREAM_PORT_DVSEC_OFFSET \
     (CXL_UPSTREAM_PORT_SN_OFFSET + PCI_EXT_CAP_DSN_SIZEOF)
-
-typedef struct CXLUpstreamPort {
-    /*< private >*/
-    PCIEPort parent_obj;
-
-    /*< public >*/
-    CXLComponentState cxl_cstate;
-    DOECap doe_cdat;
-    uint64_t sn;
-} CXLUpstreamPort;
 
 CXLComponentState *cxl_usp_to_cstate(CXLUpstreamPort *usp)
 {
