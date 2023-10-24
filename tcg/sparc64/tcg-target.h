b9ddaf5618adbb9d392ecd6b29b2cb76596de134
Richard Henderson <richard.henderson@linaro.org>
2023-10-24 14:27:38 -0700

    tcg/sparc64: Support TCG_COND_TST{EQ,NE}
    Reviewed-by: Philippe Mathieu-Daudé <philmd@linaro.org>
    Signed-off-by: Richard Henderson <richard.henderson@linaro.org>

--- tcg/sparc64/tcg-target.h
+++ tcg/sparc64/tcg-target.h
@@ -149,7 +149,7 @@ extern bool use_vis3_instructions;
 
 #define TCG_TARGET_HAS_qemu_ldst_i128   0
 
-#define TCG_TARGET_HAS_tst              0
+#define TCG_TARGET_HAS_tst              1
 
 #define TCG_AREG0 TCG_REG_I0
 
