ce59c54c4927d22e7d564f68f29c697d77ba9b86
Marc-André Lureau <marcandre.lureau@redhat.com>
2023-10-25 16:04:45 +0400

    build-sys: drop needless warning pragmas for old pixman
    Since commit 236f282c1c7 ("configure: check for pixman-1 version"), QEMU
    requires >= 0.21.8.
    
    Suggested-by: Thomas Huth <thuth@redhat.com>
    Signed-off-by: Marc-André Lureau <marcandre.lureau@redhat.com>
    Reviewed-by: Thomas Huth <thuth@redhat.com>
    Reviewed-by: Philippe Mathieu-Daudé <philmd@linaro.org>
    Tested-by: Philippe Mathieu-Daudé <philmd@linaro.org>

--- include/ui/qemu-pixman.h
+++ include/ui/qemu-pixman.h
@@ -7,11 +7,7 @@
 #define QEMU_PIXMAN_H
 
 #ifdef CONFIG_PIXMAN
-/* pixman-0.16.0 headers have a redundant declaration */
-#pragma GCC diagnostic push
-#pragma GCC diagnostic ignored "-Wredundant-decls"
 #include <pixman.h>
-#pragma GCC diagnostic pop
 #endif
 
 /*
