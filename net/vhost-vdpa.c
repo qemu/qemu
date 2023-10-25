556b67d413a699431eadb71642033864649ea934
Hawkins Jiawei <yin31149@gmail.com>
2023-10-25 09:02:25 +0800

    vdpa: Allow VIRTIO_NET_F_HASH_REPORT in SVQ
    Enable SVQ with VIRTIO_NET_F_HASH_REPORT feature.
    
    Signed-off-by: Hawkins Jiawei <yin31149@gmail.com>
    Message-Id: <d66b0aee501cdad7954231900c35a11cad1e13db.1698194366.git.yin31149@gmail.com>
    Acked-by: Eugenio Pérez <eperezma@redhat.com>
    Reviewed-by: Michael S. Tsirkin <mst@redhat.com>
    Signed-off-by: Michael S. Tsirkin <mst@redhat.com>

--- net/vhost-vdpa.c
+++ net/vhost-vdpa.c
@@ -121,6 +121,7 @@ static const uint64_t vdpa_svq_device_features =
     BIT_ULL(VIRTIO_NET_F_CTRL_MAC_ADDR) |
     /* VHOST_F_LOG_ALL is exposed by SVQ */
     BIT_ULL(VHOST_F_LOG_ALL) |
+    BIT_ULL(VIRTIO_NET_F_HASH_REPORT) |
     BIT_ULL(VIRTIO_NET_F_RSC_EXT) |
     BIT_ULL(VIRTIO_NET_F_STANDBY) |
     BIT_ULL(VIRTIO_NET_F_SPEED_DUPLEX);
