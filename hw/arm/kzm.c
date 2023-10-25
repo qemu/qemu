88e763dab4ea49bb177affeaa4596ee3270e3287
Philippe Mathieu-Daudé <philmd@linaro.org>
2023-10-25 08:53:11 +0200

    hw/arm/fsl-imx31: Remove 'hw/arm/boot.h' from header
    "hw/arm/boot.h" is only required on the source file.
    
    Signed-off-by: Philippe Mathieu-Daudé <philmd@linaro.org>
    Reviewed-by: Richard Henderson <richard.henderson@linaro.org>
    Reviewed-by: Luc Michel <luc.michel@amd.com>
    Message-id: 20231025065316.56817-6-philmd@linaro.org
    Signed-off-by: Peter Maydell <peter.maydell@linaro.org>

--- hw/arm/kzm.c
+++ hw/arm/kzm.c
@@ -16,6 +16,7 @@
 #include "qemu/osdep.h"
 #include "qapi/error.h"
 #include "hw/arm/fsl-imx31.h"
+#include "hw/arm/boot.h"
 #include "hw/boards.h"
 #include "qemu/error-report.h"
 #include "exec/address-spaces.h"
