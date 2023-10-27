97d3b2cd36d46b033a5f4219084ab929a5bac9d3
BALATON Zoltan <balaton@eik.bme.hu>
2023-10-27 13:54:49 +0200

    hw/pci-host: Add emulation of Mai Logic Articia S
    The Articia S is a generic chipset supporting several different CPUs
    that were among others used on some PPC boards. This is a minimal
    emulation of the parts needed for emulating the AmigaOne board.
    
    Signed-off-by: BALATON Zoltan <balaton@eik.bme.hu>
    Tested-by: Rene Engel <ReneEngel80@emailn.de>
    Acked-by: Daniel Henrique Barboza <danielhb413@gmail.com>
    Message-ID: <83822787431701cf4d460298d3e3845f362e5da1.1698406922.git.balaton@eik.bme.hu>
    Signed-off-by: Daniel Henrique Barboza <danielhb413@gmail.com>

---
+++ include/hw/pci-host/articia.h
@@ -0,0 +1,17 @@
+/*
+ * Mai Logic Articia S emulation
+ *
+ * Copyright (c) 2023 BALATON Zoltan
+ *
+ * This work is licensed under the GNU GPL license version 2 or later.
+ *
+ */
+
+#ifndef ARTICIA_H
+#define ARTICIA_H
+
+#define TYPE_ARTICIA "articia"
+#define TYPE_ARTICIA_PCI_HOST "articia-pci-host"
+#define TYPE_ARTICIA_PCI_BRIDGE "articia-pci-bridge"
+
+#endif
