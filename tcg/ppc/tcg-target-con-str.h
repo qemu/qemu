282ef7e8ef210c5582435f2d4cd9befc3130f5c6
Richard Henderson <richard.henderson@linaro.org>
2023-10-28 02:09:03 +0000

    tcg/ppc: Add TCG_CT_CONST_CMP
    Better constraint for tcg_out_cmp, based on the comparison.
    We can't yet remove the fallback to load constants into a
    scratch because of tcg_out_cmp2, but that path should not
    be as frequent.
    
    Reviewed-by: Philippe Mathieu-Daudé <philmd@linaro.org>
    Signed-off-by: Richard Henderson <richard.henderson@linaro.org>

--- tcg/ppc/tcg-target-con-str.h
+++ tcg/ppc/tcg-target-con-str.h
@@ -16,6 +16,7 @@ REGS('v', ALL_VECTOR_REGS)
  * Define constraint letters for constants:
  * CONST(letter, TCG_CT_CONST_* bit set)
  */
+CONST('C', TCG_CT_CONST_CMP)
 CONST('I', TCG_CT_CONST_S16)
 CONST('M', TCG_CT_CONST_MONE)
 CONST('T', TCG_CT_CONST_S32)
