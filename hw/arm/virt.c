1d675e59ea194fc918fe0f553eb79209b3fb3a8f
Jean-Philippe Brucker <jean-philippe@linaro.org>
2023-11-10 09:05:58 +0000

    hw/arm/virt: fix GIC maintenance IRQ registration
    Since commit 9036e917f8 ("{include/}hw/arm: refactor virt PPI logic"),
    GIC maintenance IRQ registration fails on arm64:
    
    [    0.979743] kvm [1]: Cannot register interrupt 9
    
    That commit re-defined VIRTUAL_PMU_IRQ to be a INTID but missed a case
    where the maintenance IRQ is actually referred by its PPI index. Just
    like commit fa68ecb330db ("hw/arm/virt: fix PMU IRQ registration"), use
    INITID_TO_PPI(). A search of "GIC_FDT_IRQ_TYPE_PPI" indicates that there
    shouldn't be more similar issues.
    
    Fixes: 9036e917f8 ("{include/}hw/arm: refactor virt PPI logic")
    Signed-off-by: Jean-Philippe Brucker <jean-philippe@linaro.org>
    Message-id: 20231110090557.3219206-2-jean-philippe@linaro.org
    Signed-off-by: Peter Maydell <peter.maydell@linaro.org>

--- hw/arm/virt.c
+++ hw/arm/virt.c
@@ -576,7 +576,8 @@ static void fdt_add_gic_node(VirtMachineState *vms)
 
         if (vms->virt) {
             qemu_fdt_setprop_cells(ms->fdt, nodename, "interrupts",
-                                   GIC_FDT_IRQ_TYPE_PPI, ARCH_GIC_MAINT_IRQ,
+                                   GIC_FDT_IRQ_TYPE_PPI,
+                                   INTID_TO_PPI(ARCH_GIC_MAINT_IRQ),
                                    GIC_FDT_IRQ_FLAGS_LEVEL_HI);
         }
     } else {
@@ -600,7 +601,8 @@ static void fdt_add_gic_node(VirtMachineState *vms)
                                          2, vms->memmap[VIRT_GIC_VCPU].base,
                                          2, vms->memmap[VIRT_GIC_VCPU].size);
             qemu_fdt_setprop_cells(ms->fdt, nodename, "interrupts",
-                                   GIC_FDT_IRQ_TYPE_PPI, ARCH_GIC_MAINT_IRQ,
+                                   GIC_FDT_IRQ_TYPE_PPI,
+                                   INTID_TO_PPI(ARCH_GIC_MAINT_IRQ),
                                    GIC_FDT_IRQ_FLAGS_LEVEL_HI);
         }
     }
