ca1b8762922f4fa859e082898ca567c2cc0269ca
Philippe Mathieu-Daudé <philmd@linaro.org>
2023-11-20 13:42:00 +0100

    hw/nvram: Simplify memory_region_init_rom_device() calls
    Mechanical change using the following coccinelle script:
    
    @@
    expression mr, owner, arg3, arg4, arg5, arg6, errp;
    @@
    -   memory_region_init_rom_device(mr, owner, arg3, arg4, arg5, arg6, &errp);
        if (
    -       errp
    +       !memory_region_init_rom_device(mr, owner, arg3, arg4, arg5, arg6, &errp)
        ) {
            ...
            return;
        }
    
    and removing the local Error variable.
    
    Signed-off-by: Philippe Mathieu-Daudé <philmd@linaro.org>
    Reviewed-by: Manos Pitsidianakis <manos.pitsidianakis@linaro.org>
    Reviewed-by: Gavin Shan <gshan@redhat.com>
    Message-Id: <20231120213301.24349-25-philmd@linaro.org>

--- hw/nvram/nrf51_nvm.c
+++ hw/nvram/nrf51_nvm.c
@@ -336,12 +336,9 @@ static void nrf51_nvm_init(Object *obj)
 static void nrf51_nvm_realize(DeviceState *dev, Error **errp)
 {
     NRF51NVMState *s = NRF51_NVM(dev);
-    Error *err = NULL;
 
-    memory_region_init_rom_device(&s->flash, OBJECT(dev), &flash_ops, s,
-        "nrf51_soc.flash", s->flash_size, &err);
-    if (err) {
-        error_propagate(errp, err);
+    if (!memory_region_init_rom_device(&s->flash, OBJECT(dev), &flash_ops, s,
+                                       "nrf51_soc.flash", s->flash_size, errp)) {
         return;
     }
 
