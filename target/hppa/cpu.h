3824e0d643f34ee09e0cc75190c0c4b60928b78c
Helge Deller <deller@gmx.de>
2024-01-11 22:50:11 +0100

    target/hppa: Export function hppa_set_ior_and_isr()
    Move functionality to set IOR and ISR on fault into own
    function. This will be used by follow-up patches.
    
    Signed-off-by: Helge Deller <deller@gmx.de>
    Reviewed-by: Richard Henderson <richard.henderson@linaro.org>

--- target/hppa/cpu.h
+++ target/hppa/cpu.h
@@ -385,6 +385,7 @@ void hppa_cpu_dump_state(CPUState *cs, FILE *f, int);
 #ifndef CONFIG_USER_ONLY
 void hppa_ptlbe(CPUHPPAState *env);
 hwaddr hppa_cpu_get_phys_page_debug(CPUState *cs, vaddr addr);
+void hppa_set_ior_and_isr(CPUHPPAState *env, vaddr addr, bool mmu_disabled);
 bool hppa_cpu_tlb_fill(CPUState *cs, vaddr address, int size,
                        MMUAccessType access_type, int mmu_idx,
                        bool probe, uintptr_t retaddr);
