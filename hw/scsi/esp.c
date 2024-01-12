5fd4de76048966bb3b6c768e6ca5008600ea07d4
Mark Cave-Ayland <mark.cave-ayland@ilande.co.uk>
2024-01-12 12:53:03 +0000

    esp.c: remove unused case from esp_pdma_read()
    The do_cmd variable is only set for the MESSAGE OUT and COMMAND phases i.e.
    those which involve transfers from the host to the SCSI bus, and so the unused
    case can be removed.
    
    Signed-off-by: Mark Cave-Ayland <mark.cave-ayland@ilande.co.uk>
    Tested-by: Helge Deller <deller@gmx.de>
    Tested-by: Thomas Huth <thuth@redhat.com>
    Message-Id: <20240112125420.514425-12-mark.cave-ayland@ilande.co.uk>
    Signed-off-by: Mark Cave-Ayland <mark.cave-ayland@ilande.co.uk>

--- hw/scsi/esp.c
+++ hw/scsi/esp.c
@@ -187,12 +187,7 @@ static uint8_t esp_pdma_read(ESPState *s)
 {
     uint8_t val;
 
-    if (s->do_cmd) {
-        val = esp_fifo_pop(&s->cmdfifo);
-    } else {
-        val = esp_fifo_pop(&s->fifo);
-    }
-
+    val = esp_fifo_pop(&s->fifo);
     return val;
 }
 
