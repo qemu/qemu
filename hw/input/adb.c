5c9ca5d72b312b53ed85e434e381608129d632c9
Peter Maydell <peter.maydell@linaro.org>
2024-01-19 16:35:10 +0000

    adb: Switch bus reset to 3-phase-reset
    Switch the ADB bus from using BusClass::reset to the Resettable
    interface.
    
    This has no behavioural change, because the BusClass code to support
    subclasses that use the legacy BusClass::reset will call that method
    in the hold phase of 3-phase reset.
    
    Signed-off-by: Peter Maydell <peter.maydell@linaro.org>
    Acked-by: Michael S. Tsirkin <mst@redhat.com>
    Acked-by: Cédric Le Goater <clg@redhat.com>
    Acked-by: Maciej S. Szmigiero <maciej.szmigiero@oracle.com>
    Tested-by: Cédric Le Goater <clg@redhat.com>
    Reviewed-by: Mark Cave-Ayland <mark.cave-ayland@ilande.co.uk>
    Reviewed-by: Zhao Liu <zhao1.liu@intel.com>
    Message-id: 20240119163512.3810301-4-peter.maydell@linaro.org

--- hw/input/adb.c
+++ hw/input/adb.c
@@ -231,9 +231,9 @@ static const VMStateDescription vmstate_adb_bus = {
     }
 };
 
-static void adb_bus_reset(BusState *qbus)
+static void adb_bus_reset_hold(Object *obj)
 {
-    ADBBusState *adb_bus = ADB_BUS(qbus);
+    ADBBusState *adb_bus = ADB_BUS(obj);
 
     adb_bus->autopoll_enabled = false;
     adb_bus->autopoll_mask = 0xffff;
@@ -262,10 +262,11 @@ static void adb_bus_unrealize(BusState *qbus)
 static void adb_bus_class_init(ObjectClass *klass, void *data)
 {
     BusClass *k = BUS_CLASS(klass);
+    ResettableClass *rc = RESETTABLE_CLASS(klass);
 
     k->realize = adb_bus_realize;
     k->unrealize = adb_bus_unrealize;
-    k->reset = adb_bus_reset;
+    rc->phases.hold = adb_bus_reset_hold;
 }
 
 static const TypeInfo adb_bus_type_info = {
