eb04c35da2c063515667e513028d64e27178365f
Ninad Palsule <ninad@linux.ibm.com>
2024-01-26 04:49:52 -0600

    hw/fsi: Aspeed APB2OPB & On-chip peripheral bus
    This is a part of patchset where IBM's Flexible Service Interface is
    introduced.
    
    An APB-to-OPB bridge enabling access to the OPB from the ARM core in
    the AST2600. Hardware limitations prevent the OPB from being directly
    mapped into APB, so all accesses are indirect through the bridge.
    
    The On-Chip Peripheral Bus (OPB): A low-speed bus typically found in
    POWER processors. This now makes an appearance in the ASPEED SoC due
    to tight integration of the FSI master IP with the OPB, mainly the
    existence of an MMIO-mapping of the CFAM address straight onto a
    sub-region of the OPB address space.
    
    Signed-off-by: Andrew Jeffery <andrew@aj.id.au>
    Signed-off-by: Ninad Palsule <ninad@linux.ibm.com>
    Reviewed-by: Cédric Le Goater <clg@kaod.org>
    [ clg: - moved FSIMasterState under AspeedAPB2OPBState
           - modified fsi_opb_fsi_master_address() and
             fsi_opb_opb2fsi_address()
           - instroduced fsi_aspeed_apb2opb_init()
           - reworked fsi_aspeed_apb2opb_realize()
           - removed FSIMasterState object and fsi_opb_realize()
           - simplified OPBus
           - introduced fsi_aspeed_apb2opb_rw to fix endianness issue ]
    Signed-off-by: Cédric Le Goater <clg@kaod.org>

---
+++ include/hw/fsi/aspeed_apb2opb.h
@@ -0,0 +1,46 @@
+/*
+ * SPDX-License-Identifier: GPL-2.0-or-later
+ * Copyright (C) 2024 IBM Corp.
+ *
+ * ASPEED APB2OPB Bridge
+ * IBM On-Chip Peripheral Bus
+ */
+#ifndef FSI_ASPEED_APB2OPB_H
+#define FSI_ASPEED_APB2OPB_H
+
+#include "exec/memory.h"
+#include "hw/fsi/fsi-master.h"
+#include "hw/sysbus.h"
+
+#define TYPE_FSI_OPB "fsi.opb"
+
+#define TYPE_OP_BUS "opb"
+OBJECT_DECLARE_SIMPLE_TYPE(OPBus, OP_BUS)
+
+typedef struct OPBus {
+    BusState bus;
+
+    MemoryRegion mr;
+    AddressSpace as;
+} OPBus;
+
+#define TYPE_ASPEED_APB2OPB "aspeed.apb2opb"
+OBJECT_DECLARE_SIMPLE_TYPE(AspeedAPB2OPBState, ASPEED_APB2OPB)
+
+#define ASPEED_APB2OPB_NR_REGS ((0xe8 >> 2) + 1)
+
+#define ASPEED_FSI_NUM 2
+
+typedef struct AspeedAPB2OPBState {
+    SysBusDevice parent_obj;
+
+    MemoryRegion iomem;
+
+    uint32_t regs[ASPEED_APB2OPB_NR_REGS];
+    qemu_irq irq;
+
+    OPBus opb[ASPEED_FSI_NUM];
+    FSIMasterState fsi[ASPEED_FSI_NUM];
+} AspeedAPB2OPBState;
+
+#endif /* FSI_ASPEED_APB2OPB_H */
