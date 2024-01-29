258293b564d00d8c34419442da3fd7b8108eaee4
Akihiko Odaki <akihiko.odaki@daynix.com>
2024-01-29 17:03:07 +0900

    hw/smbios: Fix OEM strings table option validation
    qemu_smbios_type11_opts did not have the list terminator and that
    resulted in out-of-bound memory access. It also needs to have an element
    for the type option.
    
    Cc: qemu-stable@nongnu.org
    Fixes: 2d6dcbf93fb0 ("smbios: support setting OEM strings table")
    Signed-off-by: Akihiko Odaki <akihiko.odaki@daynix.com>
    Reviewed-by: Michael Tokarev <mjt@tls.msk.ru>
    Reviewed-by: Ani Sinha <anisinha@redhat.com>
    Reviewed-by: Michael S. Tsirkin <mst@redhat.com>
    Signed-off-by: Michael Tokarev <mjt@tls.msk.ru>
    (cherry picked from commit cd8a35b913c24248267c682cb9a348461c106139)

--- hw/smbios/smbios.c
+++ hw/smbios/smbios.c
@@ -369,6 +369,11 @@ static const QemuOptDesc qemu_smbios_type8_opts[] = {
 
 static const QemuOptDesc qemu_smbios_type11_opts[] = {
     {
+        .name = "type",
+        .type = QEMU_OPT_NUMBER,
+        .help = "SMBIOS element type",
+    },
+    {
         .name = "value",
         .type = QEMU_OPT_STRING,
         .help = "OEM string data",
@@ -378,6 +383,7 @@ static const QemuOptDesc qemu_smbios_type11_opts[] = {
         .type = QEMU_OPT_STRING,
         .help = "OEM string data from file",
     },
+    { /* end of list */ }
 };
 
 static const QemuOptDesc qemu_smbios_type17_opts[] = {
