066804029f089e43fe77ddbf1ba45b0dc0ba574c
Philippe Mathieu-Daudé <philmd@linaro.org>
2024-02-19 11:05:42 +0100

    hw/acpi/ich9_tco: Include missing 'migration/vmstate.h' header
    We need the VMStateDescription structure definition from
    "migration/vmstate.h" in order to declare vmstate_tco_io_sts.
    
    Signed-off-by: Philippe Mathieu-Daudé <philmd@linaro.org>
    Reviewed-by: Zhao Liu <zhao1.liu@intel.com>
    Message-Id: <20240219141412.71418-4-philmd@linaro.org>

--- include/hw/acpi/ich9_tco.h
+++ include/hw/acpi/ich9_tco.h
@@ -11,6 +11,7 @@
 #define HW_ACPI_TCO_H
 
 #include "exec/memory.h"
+#include "migration/vmstate.h"
 
 /* As per ICH9 spec, the internal timer has an error of ~0.6s on every tick */
 #define TCO_TICK_NSEC 600000000LL
