cda5a7d640d9aa0260a9c21939a30059096715a0
Sergey Kambalin <serg.oker@gmail.com>
2024-02-25 18:02:57 -0600

    hw/misc/bcm2835_property: Add missed BCM2835 properties
    Our model of the bcm2835 mailbox is missing a few properties
    that we need for the raspi4 kernel:
     * RPI_FWREQ_GET_CLOCKS
     * RPI_FWREQ_GET_THROTTLED
     * RPI_FWREQ_VCHIQ_INIT
    
    Add minimal implementations of them.
    
    Signed-off-by: Sergey Kambalin <sergey.kambalin@auriga.com>
    Message-id: 20240226000259.2752893-40-sergey.kambalin@auriga.com
    Signed-off-by: Peter Maydell <peter.maydell@linaro.org>
    [PMM: improved commit message]
    Reviewed-by: Peter Maydell <peter.maydell@linaro.org>

--- include/hw/arm/raspberrypi-fw-defs.h
+++ include/hw/arm/raspberrypi-fw-defs.h
@@ -159,4 +159,15 @@ enum rpi_firmware_clk_id {
     RPI_FIRMWARE_NUM_CLK_ID,
 };
 
+struct rpi_firmware_property_tag_header {
+    uint32_t tag;
+    uint32_t buf_size;
+    uint32_t req_resp_size;
+};
+
+typedef struct rpi_firmware_prop_request {
+    struct rpi_firmware_property_tag_header hdr;
+    uint8_t payload[0];
+} rpi_firmware_prop_request_t;
+
 #endif /* INCLUDE_HW_MISC_RASPBERRYPI_FW_DEFS_H_ */
