e8c44363fbf25c2601fa2fbf3ec0d6d37c5f1a02
Cédric Le Goater <clg@redhat.com>
2024-03-04 13:28:24 +0100

    migration: Report error when shutdown fails
    This will help detect issues regarding I/O channels usage.
    
    Reviewed-by: Philippe Mathieu-Daudé <philmd@linaro.org>
    Reviewed-by: Peter Xu <peterx@redhat.com>
    Signed-off-by: Cédric Le Goater <clg@redhat.com>
    Link: https://lore.kernel.org/r/20240304122844.1888308-7-clg@redhat.com
    Signed-off-by: Peter Xu <peterx@redhat.com>

--- migration/qemu-file.c
+++ migration/qemu-file.c
@@ -63,6 +63,8 @@ struct QEMUFile {
  */
 int qemu_file_shutdown(QEMUFile *f)
 {
+    Error *err = NULL;
+
     /*
      * We must set qemufile error before the real shutdown(), otherwise
      * there can be a race window where we thought IO all went though
@@ -91,7 +93,8 @@ int qemu_file_shutdown(QEMUFile *f)
         return -ENOSYS;
     }
 
-    if (qio_channel_shutdown(f->ioc, QIO_CHANNEL_SHUTDOWN_BOTH, NULL) < 0) {
+    if (qio_channel_shutdown(f->ioc, QIO_CHANNEL_SHUTDOWN_BOTH, &err) < 0) {
+        error_report_err(err);
         return -EIO;
     }
 
