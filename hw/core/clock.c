52405b7f69d03428950c33ebe823e2256e6a4a70
Philippe Mathieu-Daudé <philmd@linaro.org>
2024-03-22 16:48:17 +0100

    hw/clock: Let clock_set_mul_div() return a boolean value
    Let clock_set_mul_div() return a boolean value whether the
    clock has been updated or not, similarly to clock_set().
    
    Return early when clock_set_mul_div() is called with
    same mul/div values the clock has.
    
    Acked-by: Luc Michel <luc@lmichel.fr>
    Signed-off-by: Philippe Mathieu-Daudé <philmd@linaro.org>
    Reviewed-by: Alistair Francis <alistair.francis@wdc.com>
    Message-Id: <20240325152827.73817-2-philmd@linaro.org>

--- hw/core/clock.c
+++ hw/core/clock.c
@@ -143,14 +143,20 @@ char *clock_display_freq(Clock *clk)
     return freq_to_str(clock_get_hz(clk));
 }
 
-void clock_set_mul_div(Clock *clk, uint32_t multiplier, uint32_t divider)
+bool clock_set_mul_div(Clock *clk, uint32_t multiplier, uint32_t divider)
 {
     assert(divider != 0);
+
+    if (clk->multiplier == multiplier && clk->divider == divider) {
+        return false;
+    }
 
     trace_clock_set_mul_div(CLOCK_PATH(clk), clk->multiplier, multiplier,
                             clk->divider, divider);
     clk->multiplier = multiplier;
     clk->divider = divider;
+
+    return true;
 }
 
 static void clock_initfn(Object *obj)
