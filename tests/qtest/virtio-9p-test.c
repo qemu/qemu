981eb1060336ee0e371369088c55f81f5a577ce8
Daniel Henrique Barboza <dbarboza@ventanamicro.com>
2024-03-27 11:20:10 -0300

    qtest/virtio-9p-test.c: create/remove temp dirs after each test
    The local 9p driver in virtio-9p-test.c its temporary dir right at the
    start of qos-test (via virtio_9p_create_local_test_dir()) and only
    deletes it after qos-test is finished (via
    virtio_9p_remove_local_test_dir()).
    
    This means that any qos-test machine that ends up running virtio-9p-test
    local tests more than once will end up re-using the same temp dir. This
    is what's happening in [1] after we introduced the riscv machine nodes:
    if we enable slow tests with the '-m slow' flag using
    qemu-system-riscv64, this is what happens:
    
    - a temp dir is created;
    
    - virtio-9p-device tests will run virtio-9p-test successfully;
    
    - virtio-9p-pci tests will run virtio-9p-test, and fail right at the
      first slow test at fs_create_dir() because the "01" file was already
    created by fs_create_dir() test when running with the virtio-9p-device.
    
    The root cause is that we're creating a single temporary dir, via the
    construct/destruct callbacks, and this temp dir is kept for the entire
    qos-test run.
    
    We can change each test to clean after themselves. This approach would
    make the 'create' tests obsolete since we would need to create and
    delete dirs/files/symlinks for the cleanup, turning them into the
    'unlinkat' tests that comes right after.
    
    We chose a different approach that handles the root cause: do not use
    constructor/destructor to create the temp dir. Create one temp dir for
    each test, and remove it after the test is complete. This is the
    approach taken for other qtests like vhost-user-test.c where each test
    requires a setup() and a subsequent cleanup(), all of those instantiated
    in the .before callback.
    
    [1] https://mail.gnu.org/archive/html/qemu-devel/2024-03/msg05807.html
    
    Reported-by: Thomas Huth <thuth@redhat.com>
    Signed-off-by: Daniel Henrique Barboza <dbarboza@ventanamicro.com>
    Message-Id: <20240327142011.805728-2-dbarboza@ventanamicro.com>
    Reviewed-by: Greg Kurz <groug@kaod.org>
    Reviewed-by: Christian Schoenebeck <qemu_oss@crudebyte.com>
    Tested-by: Thomas Huth <thuth@redhat.com>
    Signed-off-by: Christian Schoenebeck <qemu_oss@crudebyte.com>

--- tests/qtest/virtio-9p-test.c
+++ tests/qtest/virtio-9p-test.c
@@ -693,9 +693,20 @@ static void fs_unlinkat_hardlink(void *obj, void *data,
     g_assert(stat(real_file, &st_real) == 0);
 }
 
+static void cleanup_9p_local_driver(void *data)
+{
+    /* remove previously created test dir when test is completed */
+    virtio_9p_remove_local_test_dir();
+}
+
 static void *assign_9p_local_driver(GString *cmd_line, void *arg)
 {
+    /* make sure test dir for the 'local' tests exists */
+    virtio_9p_create_local_test_dir();
+
     virtio_9p_assign_local_driver(cmd_line, "security_model=mapped-xattr");
+
+    g_test_queue_destroy(cleanup_9p_local_driver, NULL);
     return arg;
 }
 
@@ -759,15 +770,3 @@ static void register_virtio_9p_test(void)
 }
 
 libqos_init(register_virtio_9p_test);
-
-static void __attribute__((constructor)) construct_9p_test(void)
-{
-    /* make sure test dir for the 'local' tests exists */
-    virtio_9p_create_local_test_dir();
-}
-
-static void __attribute__((destructor)) destruct_9p_test(void)
-{
-    /* remove previously created test dir when test suite completed */
-    virtio_9p_remove_local_test_dir();
-}
