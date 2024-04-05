66f3b79e85149f42465a580c2530638e27c4a4bb
Richard Henderson <richard.henderson@linaro.org>
2024-04-04 15:56:39 -1000

    accel/tcg: Hide in_same_page outside of a target-specific context
    While there are other methods that could be used to replace
    TARGET_PAGE_MASK, the function is not really required outside
    the context of target-specific translation.
    
    This makes the header usable by target independent code.
    
    Reviewed-by: Philippe Mathieu-Daudé <philmd@linaro.org>
    Signed-off-by: Richard Henderson <richard.henderson@linaro.org>

--- include/exec/translator.h
+++ include/exec/translator.h
@@ -232,6 +232,7 @@ translator_ldq_swap(CPUArchState *env, DisasContextBase *db,
  */
 void translator_fake_ldb(uint8_t insn8, vaddr pc);
 
+#ifdef COMPILING_PER_TARGET
 /*
  * Return whether addr is on the same page as where disassembly started.
  * Translators can use this to enforce the rule that only single-insn
@@ -241,5 +242,6 @@ static inline bool is_same_page(const DisasContextBase *db, vaddr addr)
 {
     return ((addr ^ db->pc_first) & TARGET_PAGE_MASK) == 0;
 }
+#endif
 
 #endif /* EXEC__TRANSLATOR_H */
