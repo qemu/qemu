1b1138cabeb6fd55438721c6d5fe373ba4fd3cd4
Richard Henderson <richard.henderson@linaro.org>
2024-04-04 23:17:02 -1000

    target/cris: Use cris_fetch in translate_v10.c.inc
    Reviewed-by: Edgar E. Iglesias <edgar.iglesias@amd.com>
    Reviewed-by: Philippe Mathieu-Daudé <philmd@linaro.org>
    Signed-off-by: Richard Henderson <richard.henderson@linaro.org>

--- target/cris/translate.c
+++ target/cris/translate.c
@@ -29,7 +29,6 @@
 #include "tcg/tcg-op.h"
 #include "exec/helper-proto.h"
 #include "mmu.h"
-#include "exec/cpu_ldst.h"
 #include "exec/translator.h"
 #include "crisv32-decode.h"
 #include "qemu/qemu-print.h"
