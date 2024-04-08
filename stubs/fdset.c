748e62dbf5065c7d166c827425c7797389b5f9fe
Paolo Bonzini <pbonzini@redhat.com>
2024-04-08 17:53:30 +0200

    stubs: move monitor_fdsets_cleanup with other fdset stubs
    Even though monitor_get_fd() has to remain separate because it is mocked by
    tests/unit/test-util-sockets, monitor_fdsets_cleanup() is logically part
    of the stubs for monitor/fds.c, so move it there.
    
    Signed-off-by: Paolo Bonzini <pbonzini@redhat.com>
    Reviewed-by: Richard Henderson <richard.henderson@linaro.org>
    Message-ID: <20240408155330.522792-19-pbonzini@redhat.com>
    Signed-off-by: Paolo Bonzini <pbonzini@redhat.com>

--- stubs/fdset.c
+++ stubs/fdset.c
@@ -1,5 +1,7 @@
 #include "qemu/osdep.h"
+#include "qapi/error.h"
 #include "monitor/monitor.h"
+#include "../monitor/monitor-internal.h"
 
 int monitor_fdset_dup_fd_add(int64_t fdset_id, int flags)
 {
@@ -13,5 +15,9 @@ int64_t monitor_fdset_dup_fd_find(int dup_fd)
 }
 
 void monitor_fdset_dup_fd_remove(int dupfd)
+{
+}
+
+void monitor_fdsets_cleanup(void)
 {
 }
--- stubs/monitor-internal.c
+++ stubs/monitor-internal.c
@@ -1,7 +1,6 @@
 #include "qemu/osdep.h"
 #include "qapi/error.h"
 #include "monitor/monitor.h"
-#include "../monitor/monitor-internal.h"
 
 int monitor_get_fd(Monitor *mon, const char *name, Error **errp)
 {
@@ -10,9 +9,5 @@ int monitor_get_fd(Monitor *mon, const char *name, Error **errp)
 }
 
 void monitor_init_hmp(Chardev *chr, bool use_readline, Error **errp)
-{
-}
-
-void monitor_fdsets_cleanup(void)
 {
 }
