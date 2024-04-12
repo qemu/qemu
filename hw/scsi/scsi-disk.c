00a17d803d0931b00bffdb3b3e8a3e81251de9fa
Philippe Mathieu-Daudé <philmd@linaro.org>
2024-04-12 00:33:27 -0700

    hw/scsi/scsi-disk: Use qemu_hexdump_line to avoid sprintf
    sprintf() is deprecated on Darwin since macOS 13.0 / XCode 14.1.
    Using qemu_hexdump_line both fixes the deprecation warning and
    simplifies the code base.
    
    Note that this drops the "0x" prefix to every byte, which should
    be of no consequence to tracing.
    
    Signed-off-by: Philippe Mathieu-Daudé <philmd@linaro.org>
    Signed-off-by: Richard Henderson <richard.henderson@linaro.org>
    Reviewed-by: Philippe Mathieu-Daudé <philmd@linaro.org>
    Message-Id: <20240412073346.458116-9-richard.henderson@linaro.org>

--- hw/scsi/scsi-disk.c
+++ hw/scsi/scsi-disk.c
@@ -2648,19 +2648,12 @@ static const SCSIReqOps *const scsi_disk_reqops_dispatch[256] = {
 
 static void scsi_disk_new_request_dump(uint32_t lun, uint32_t tag, uint8_t *buf)
 {
-    int i;
     int len = scsi_cdb_length(buf);
-    char *line_buffer, *p;
+    g_autoptr(GString) str = NULL;
 
     assert(len > 0 && len <= 16);
-    line_buffer = g_malloc(len * 5 + 1);
-
-    for (i = 0, p = line_buffer; i < len; i++) {
-        p += sprintf(p, " 0x%02x", buf[i]);
-    }
-    trace_scsi_disk_new_request(lun, tag, line_buffer);
-
-    g_free(line_buffer);
+    str = qemu_hexdump_line(NULL, buf, len, 1, 0);
+    trace_scsi_disk_new_request(lun, tag, str->str);
 }
 
 static SCSIRequest *scsi_new_request(SCSIDevice *d, uint32_t tag, uint32_t lun,
