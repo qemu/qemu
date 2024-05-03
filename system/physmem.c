a5bdc451c7d10056acd9b49f6028895451b37df5
Edgar E. Iglesias <edgar.iglesias@amd.com>
2024-05-03 03:44:46 +0200

    physmem: Replace check for RAMBlock offset 0 with xen_mr_is_memory
    For xen, when checking for the first RAM (xen_memory), use
    xen_mr_is_memory() rather than checking for a RAMBlock with
    offset 0.
    
    All Xen machines create xen_memory first so this has no
    functional change for existing machines.
    
    Signed-off-by: Edgar E. Iglesias <edgar.iglesias@amd.com>
    Reviewed-by: Stefano Stabellini <sstabellini@kernel.org>
    Reviewed-by: David Hildenbrand <david@redhat.com>
    Message-ID: <20240529140739.1387692-6-edgar.iglesias@gmail.com>
    Signed-off-by: Philippe Mathieu-Daudé <philmd@linaro.org>

--- system/physmem.c
+++ system/physmem.c
@@ -2229,7 +2229,7 @@ static void *qemu_ram_ptr_length(RAMBlock *block, ram_addr_t addr,
          * because we don't want to map the entire memory in QEMU.
          * In that case just map the requested area.
          */
-        if (block->offset == 0) {
+        if (xen_mr_is_memory(block->mr)) {
             return xen_map_cache(block->mr, block->offset + addr,
                                  len, lock, lock,
                                  is_write);
