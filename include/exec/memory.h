a0aa6db7ce72a08703774107185e639e73e7754c
Xiaoyao Li <xiaoyao.li@intel.com>
2024-05-30 06:16:15 -0500

    memory: Introduce memory_region_init_ram_guest_memfd()
    Introduce memory_region_init_ram_guest_memfd() to allocate private
    guset memfd on the MemoryRegion initialization. It's for the use case of
    TDVF, which must be private on TDX case.
    
    Signed-off-by: Xiaoyao Li <xiaoyao.li@intel.com>
    Signed-off-by: Michael Roth <michael.roth@amd.com>
    Signed-off-by: Pankaj Gupta <pankaj.gupta@amd.com>
    Message-ID: <20240530111643.1091816-4-pankaj.gupta@amd.com>
    Signed-off-by: Paolo Bonzini <pbonzini@redhat.com>

--- include/exec/memory.h
+++ include/exec/memory.h
@@ -1638,6 +1638,12 @@ bool memory_region_init_ram(MemoryRegion *mr,
                             uint64_t size,
                             Error **errp);
 
+bool memory_region_init_ram_guest_memfd(MemoryRegion *mr,
+                                        Object *owner,
+                                        const char *name,
+                                        uint64_t size,
+                                        Error **errp);
+
 /**
  * memory_region_init_rom: Initialize a ROM memory region.
  *
