eb350d1d01d9b9df0ce174e2e1681699b071bab3
Paolo Bonzini <pbonzini@redhat.com>
2024-06-25 13:12:20 +0200

    include: move typeof_strip_qual to compiler.h, use it in QAPI_LIST_LENGTH()
    The typeof_strip_qual() is most useful for the atomic fetch-and-modify
    operations in atomic.h, but it can be used elsewhere as well.  For example,
    QAPI_LIST_LENGTH() assumes that the argument is not const, which is not a
    requirement.
    
    Move the macro to compiler.h and, while at it, move it under #ifndef
    __cplusplus to emphasize that it uses C-only constructs.  A C++ version
    of typeof_strip_qual() using type traits is possible[1], but beyond the
    scope of this patch because the little C++ code that is in QEMU does not
    use QAPI.
    
    The patch was tested by changing the declaration of strv_from_str_list()
    in qapi/qapi-type-helpers.c to:
    
        char **strv_from_str_list(const strList *const list)
    
    This is valid C code, and it fails to compile without this change.
    
    [1] https://lore.kernel.org/qemu-devel/20240624205647.112034-1-flwu@google.com/
    
    Reviewed-by: Richard Henderson <richard.henderson@linaro.org>
    Reviewed-by: Manos Pitsidianakis <manos.pitsidianakis@linaro.org>
    Tested-by: Manos Pitsidianakis <manos.pitsidianakis@linaro.org>
    Signed-off-by: Paolo Bonzini <pbonzini@redhat.com>

--- include/qemu/compiler.h
+++ include/qemu/compiler.h
@@ -227,4 +227,50 @@
 #define SECOND_ARG(first, second, ...) second
 #define IS_EMPTY_(junk_maybecomma)     SECOND_ARG(junk_maybecomma 1, 0)
 
+#ifndef __cplusplus
+/*
+ * Useful in macros that need to declare temporary variables.  For example,
+ * the variable that receives the old value of an atomically-accessed
+ * variable must be non-qualified, because atomic builtins return values
+ * through a pointer-type argument as in __atomic_load(&var, &old, MODEL).
+ *
+ * This macro has to handle types smaller than int manually, because of
+ * implicit promotion.  int and larger types, as well as pointers, can be
+ * converted to a non-qualified type just by applying a binary operator.
+ */
+#define typeof_strip_qual(expr)                                                    \
+  typeof(                                                                          \
+    __builtin_choose_expr(                                                         \
+      __builtin_types_compatible_p(typeof(expr), bool) ||                          \
+        __builtin_types_compatible_p(typeof(expr), const bool) ||                  \
+        __builtin_types_compatible_p(typeof(expr), volatile bool) ||               \
+        __builtin_types_compatible_p(typeof(expr), const volatile bool),           \
+        (bool)1,                                                                   \
+    __builtin_choose_expr(                                                         \
+      __builtin_types_compatible_p(typeof(expr), signed char) ||                   \
+        __builtin_types_compatible_p(typeof(expr), const signed char) ||           \
+        __builtin_types_compatible_p(typeof(expr), volatile signed char) ||        \
+        __builtin_types_compatible_p(typeof(expr), const volatile signed char),    \
+        (signed char)1,                                                            \
+    __builtin_choose_expr(                                                         \
+      __builtin_types_compatible_p(typeof(expr), unsigned char) ||                 \
+        __builtin_types_compatible_p(typeof(expr), const unsigned char) ||         \
+        __builtin_types_compatible_p(typeof(expr), volatile unsigned char) ||      \
+        __builtin_types_compatible_p(typeof(expr), const volatile unsigned char),  \
+        (unsigned char)1,                                                          \
+    __builtin_choose_expr(                                                         \
+      __builtin_types_compatible_p(typeof(expr), signed short) ||                  \
+        __builtin_types_compatible_p(typeof(expr), const signed short) ||          \
+        __builtin_types_compatible_p(typeof(expr), volatile signed short) ||       \
+        __builtin_types_compatible_p(typeof(expr), const volatile signed short),   \
+        (signed short)1,                                                           \
+    __builtin_choose_expr(                                                         \
+      __builtin_types_compatible_p(typeof(expr), unsigned short) ||                \
+        __builtin_types_compatible_p(typeof(expr), const unsigned short) ||        \
+        __builtin_types_compatible_p(typeof(expr), volatile unsigned short) ||     \
+        __builtin_types_compatible_p(typeof(expr), const volatile unsigned short), \
+        (unsigned short)1,                                                         \
+      (expr)+0))))))
+#endif
+
 #endif /* COMPILER_H */
