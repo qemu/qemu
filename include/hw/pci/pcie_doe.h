78cc8c69475042ce6b6f720f8c81920fead0d86e
Alistair Francis <alistair23@gmail.com>
2024-07-03 19:20:25 +1000

    hw/pci: Add all Data Object Types defined in PCIe r6.0
    Add all of the defined protocols/features from the PCIe-SIG r6.0
    "Table 6-32 PCI-SIG defined Data Object Types (Vendor ID = 0001h)"
    table.
    
    Signed-off-by: Alistair Francis <alistair.francis@wdc.com>
    Reviewed-by: Jonathan Cameron <Jonathan.Cameron@huawei.com>
    Reviewed-by: Wilfred Mallawa <wilfred.mallawa@wdc.com>
    Message-Id: <20240703092027.644758-2-alistair.francis@wdc.com>
    Reviewed-by: Michael S. Tsirkin <mst@redhat.com>
    Signed-off-by: Michael S. Tsirkin <mst@redhat.com>

--- include/hw/pci/pcie_doe.h
+++ include/hw/pci/pcie_doe.h
@@ -46,6 +46,8 @@ REG32(PCI_DOE_CAP_STATUS, 0)
 
 /* PCI-SIG defined Data Object Types - r6.0 Table 6-32 */
 #define PCI_SIG_DOE_DISCOVERY       0x00
+#define PCI_SIG_DOE_CMA             0x01
+#define PCI_SIG_DOE_SECURED_CMA     0x02
 
 #define PCI_DOE_DW_SIZE_MAX         (1 << 18)
 #define PCI_DOE_PROTOCOL_NUM_MAX    256
