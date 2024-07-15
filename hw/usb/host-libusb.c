a3c45ef33e5c0205c270b716593bcc5f6718d501
Zhao Liu <zhao1.liu@intel.com>
2024-07-15 16:21:50 +0800

    hw/usb/host-libusb: Get rid of qemu_open_old()
    For qemu_open_old(), osdep.h said:
    
    > Don't introduce new usage of this function, prefer the following
    > qemu_open/qemu_create that take an "Error **errp".
    
    So replace qemu_open_old() with qemu_open().
    
    Signed-off-by: Zhao Liu <zhao1.liu@intel.com>
    Reviewed-by: Philippe Mathieu-Daudé <philmd@linaro.org>
    Reviewed-by: Michael Tokarev <mjt@tls.msk.ru>
    Signed-off-by: Michael Tokarev <mjt@tls.msk.ru>

--- hw/usb/host-libusb.c
+++ hw/usb/host-libusb.c
@@ -1212,9 +1212,8 @@ static void usb_host_realize(USBDevice *udev, Error **errp)
     if (s->hostdevice) {
         int fd;
         s->needs_autoscan = false;
-        fd = qemu_open_old(s->hostdevice, O_RDWR);
+        fd = qemu_open(s->hostdevice, O_RDWR, errp);
         if (fd < 0) {
-            error_setg_errno(errp, errno, "failed to open %s", s->hostdevice);
             return;
         }
         rc = usb_host_open(s, NULL, fd);
