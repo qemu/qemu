06f1f4958be70f14f527d1f03d1e9a141650bbc5
Salil Mehta <salil.mehta@huawei.com>
2024-07-16 12:14:58 +0100

    hw/acpi: Update ACPI GED framework to support vCPU Hotplug
    ACPI GED (as described in the ACPI 6.4 spec) uses an interrupt listed in the
    _CRS object of GED to intimate OSPM about an event. Later then demultiplexes the
    notified event by evaluating ACPI _EVT method to know the type of event. Use
    ACPI GED to also notify the guest kernel about any CPU hot(un)plug events.
    
    Note, GED interface is used by many hotplug events like memory hotplug, NVDIMM
    hotplug and non-hotplug events like system power down event. Each of these can
    be selected using a bit in the 32 bit GED IO interface. A bit has been reserved
    for the CPU hotplug event.
    
    ACPI CPU hotplug related initialization should only happen if ACPI_CPU_HOTPLUG
    support has been enabled for particular architecture. Add cpu_hotplug_hw_init()
    stub to avoid compilation break.
    
    Co-developed-by: Keqian Zhu <zhukeqian1@huawei.com>
    Signed-off-by: Keqian Zhu <zhukeqian1@huawei.com>
    Signed-off-by: Salil Mehta <salil.mehta@huawei.com>
    Reviewed-by: Jonathan Cameron <Jonathan.Cameron@huawei.com>
    Reviewed-by: Gavin Shan <gshan@redhat.com>
    Reviewed-by: David Hildenbrand <david@redhat.com>
    Reviewed-by: Shaoqin Huang <shahuang@redhat.com>
    Tested-by: Vishnu Pajjuri <vishnu@os.amperecomputing.com>
    Tested-by: Xianglai Li <lixianglai@loongson.cn>
    Tested-by: Miguel Luis <miguel.luis@oracle.com>
    Reviewed-by: Vishnu Pajjuri <vishnu@os.amperecomputing.com>
    Tested-by: Zhao Liu <zhao1.liu@intel.com>
    Reviewed-by: Zhao Liu <zhao1.liu@intel.com>
    Message-Id: <20240716111502.202344-4-salil.mehta@huawei.com>
    Reviewed-by: Michael S. Tsirkin <mst@redhat.com>
    Signed-off-by: Michael S. Tsirkin <mst@redhat.com>
    Acked-by: Igor Mammedov <imammedo@redhat.com>

--- include/hw/acpi/generic_event_device.h
+++ include/hw/acpi/generic_event_device.h
@@ -62,6 +62,7 @@
 #include "hw/sysbus.h"
 #include "hw/acpi/memory_hotplug.h"
 #include "hw/acpi/ghes.h"
+#include "hw/acpi/cpu.h"
 #include "qom/object.h"
 
 #define ACPI_POWER_BUTTON_DEVICE "PWRB"
@@ -95,6 +96,7 @@ OBJECT_DECLARE_SIMPLE_TYPE(AcpiGedState, ACPI_GED)
 #define ACPI_GED_MEM_HOTPLUG_EVT   0x1
 #define ACPI_GED_PWR_DOWN_EVT      0x2
 #define ACPI_GED_NVDIMM_HOTPLUG_EVT 0x4
+#define ACPI_GED_CPU_HOTPLUG_EVT    0x8
 
 typedef struct GEDState {
     MemoryRegion evt;
@@ -106,6 +108,8 @@ struct AcpiGedState {
     SysBusDevice parent_obj;
     MemHotplugState memhp_state;
     MemoryRegion container_memhp;
+    CPUHotplugState cpuhp_state;
+    MemoryRegion container_cpuhp;
     GEDState ged_state;
     uint32_t ged_event_bitmap;
     qemu_irq irq;
