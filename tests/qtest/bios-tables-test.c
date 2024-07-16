329b327924f7e1a7a735c91b31f722708599e10f
Sunil V L <sunilvl@ventanamicro.com>
2024-07-16 20:13:03 +0530

    tests/qtest/bios-tables-test.c: Remove the fall back path
    The expected ACPI AML files are moved now under ${arch}/{machine} path.
    Hence, there is no need to search in old path which didn't have ${arch}.
    Remove the code which searches for the expected AML files under old path
    as well.
    
    Suggested-by: Igor Mammedov <imammedo@redhat.com>
    Signed-off-by: Sunil V L <sunilvl@ventanamicro.com>
    Acked-by: Alistair Francis <alistair.francis@wdc.com>
    Reviewed-by: Igor Mammedov <imammedo@redhat.com>
    Message-Id: <20240716144306.2432257-7-sunilvl@ventanamicro.com>
    Reviewed-by: Michael S. Tsirkin <mst@redhat.com>
    Signed-off-by: Michael S. Tsirkin <mst@redhat.com>

--- tests/qtest/bios-tables-test.c
+++ tests/qtest/bios-tables-test.c
@@ -267,15 +267,6 @@ static void dump_aml_files(test_data *data, bool rebuild)
                                        data->arch, data->machine,
                                        sdt->aml, ext);
 
-            /*
-             * To keep test cases not failing before the DATA files are moved to
-             * ${arch}/${machine} folder, add this check as well.
-             */
-            if (!g_file_test(aml_file, G_FILE_TEST_EXISTS)) {
-                aml_file = g_strdup_printf("%s/%s/%.4s%s", data_dir,
-                                           data->machine, sdt->aml, ext);
-            }
-
             if (!g_file_test(aml_file, G_FILE_TEST_EXISTS) &&
                 sdt->aml_len == exp_sdt->aml_len &&
                 !memcmp(sdt->aml, exp_sdt->aml, sdt->aml_len)) {
@@ -412,11 +403,6 @@ static GArray *load_expected_aml(test_data *data)
 try_again:
         aml_file = g_strdup_printf("%s/%s/%s/%.4s%s", data_dir, data->arch,
                                    data->machine, sdt->aml, ext);
-        if (!g_file_test(aml_file, G_FILE_TEST_EXISTS)) {
-            aml_file = g_strdup_printf("%s/%s/%.4s%s", data_dir, data->machine,
-                                       sdt->aml, ext);
-        }
-
         if (verbosity_level >= 2) {
             fprintf(stderr, "Looking for expected file '%s'\n", aml_file);
         }
