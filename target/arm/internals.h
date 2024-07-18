e8122a7118eb22ace9e60c91bfbf2d4bbfc6f15a
Alex Bennée <alex.bennee@linaro.org>
2024-07-18 10:45:11 +0100

    gdbstub: Re-factor gdb command extensions
    Coverity reported a memory leak (CID 1549757) in this code and its
    admittedly rather clumsy handling of extending the command table.
    Instead of handing over a full array of the commands lets use the
    lighter weight GPtrArray and simply test for the presence of each
    entry as we go. This avoids complications of transferring ownership of
    arrays and keeps the final command entries as static entries in the
    target code.
    
    Cc: Akihiko Odaki <akihiko.odaki@daynix.com>
    Cc: Gustavo Bueno Romero <gustavo.romero@linaro.org>
    Cc: Peter Maydell <peter.maydell@linaro.org>
    Reviewed-by: Gustavo Romero <gustavo.romero@linaro.org>
    Signed-off-by: Alex Bennée <alex.bennee@linaro.org>
    Message-Id: <20240718094523.1198645-4-alex.bennee@linaro.org>

--- target/arm/internals.h
+++ target/arm/internals.h
@@ -359,8 +359,8 @@ void arm_cpu_register_gdb_regs_for_features(ARMCPU *cpu);
 void arm_translate_init(void);
 
 void arm_cpu_register_gdb_commands(ARMCPU *cpu);
-void aarch64_cpu_register_gdb_commands(ARMCPU *cpu, GString *, GArray *,
-                                       GArray *);
+void aarch64_cpu_register_gdb_commands(ARMCPU *cpu, GString *,
+                                       GPtrArray *, GPtrArray *);
 
 void arm_restore_state_to_opc(CPUState *cs,
                               const TranslationBlock *tb,
