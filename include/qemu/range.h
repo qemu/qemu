007643bddcebf9e7fb8460a4e8068c4f80afa17e
Yao Xingtao <yaoxt.fnst@fujitsu.com>
2024-07-22 00:07:30 -0400

    util/range: Make ranges_overlap() return bool
    Just like range_overlaps_range(), use the returned bool value
    to check whether 2 given ranges overlap.
    
    Signed-off-by: Yao Xingtao <yaoxt.fnst@fujitsu.com>
    Reviewed-by: Philippe Mathieu-Daudé <philmd@linaro.org>
    Message-ID: <20240722040742.11513-2-yaoxt.fnst@fujitsu.com>
    Signed-off-by: Philippe Mathieu-Daudé <philmd@linaro.org>

--- include/qemu/range.h
+++ include/qemu/range.h
@@ -210,8 +210,8 @@ static inline int range_covers_byte(uint64_t offset, uint64_t len,
 
 /* Check whether 2 given ranges overlap.
  * Undefined if ranges that wrap around 0. */
-static inline int ranges_overlap(uint64_t first1, uint64_t len1,
-                                 uint64_t first2, uint64_t len2)
+static inline bool ranges_overlap(uint64_t first1, uint64_t len1,
+                                  uint64_t first2, uint64_t len2)
 {
     uint64_t last1 = range_get_last(first1, len1);
     uint64_t last2 = range_get_last(first2, len2);
