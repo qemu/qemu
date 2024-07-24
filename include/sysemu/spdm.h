80ddf450c312305e2efa614e7694d33990679eb8
htafr <offreitas@usp.br>
2024-07-24 17:55:54 -0300

    libspdm: create spdm common files that use libspdm api

---
+++ include/sysemu/spdm.h
@@ -0,0 +1,173 @@
+/**
+ *  Author: 
+ *      htafr
+ **/
+
+#ifndef QEMU_SPDM_H
+#define QEMU_SPDM_H
+
+#include <glib.h>
+#include "hal/base.h"
+#include "hal/library/memlib.h"
+#include "industry_standard/spdm.h"
+#include "library/spdm_responder_lib.h"
+#include "library/spdm_requester_lib.h"
+#include "library/spdm_transport_mctp_lib.h"
+#include "library/spdm_transport_pcidoe_lib.h"
+
+typedef struct SpdmDev SpdmDev;
+
+struct SpdmDev {
+    bool isResponder;
+    bool isRequester;
+
+    void *spdm_context;
+
+    void *scratch_buffer;
+    size_t scratch_buffer_size;
+
+    void *requester_cert_chain_buffer;
+
+    uint32_t use_transport_layer;
+    uint32_t use_tcp_handshake;
+    uint8_t use_version;
+    uint8_t use_secured_message_version;
+    uint32_t use_requester_capability_flags;
+    uint32_t use_responder_capability_flags;
+    uint32_t use_capability_flags;
+    uint32_t use_peer_capability_flags;
+
+    uint8_t use_basic_mut_auth;
+    uint8_t use_mut_auth;
+    uint8_t use_measurement_summary_hash_type;
+    uint8_t use_measurement_operation;
+    uint8_t use_measurement_attribute;
+    uint8_t use_slot_id;
+    uint8_t use_slot_count;
+    bool g_private_key_mode;
+
+    libspdm_key_update_action_t use_key_update_action;
+
+    uint32_t use_hash_algo;
+    uint32_t use_measurement_hash_algo;
+    uint32_t use_asym_algo;
+    uint16_t use_req_asym_algo;
+
+    uint8_t support_measurement_spec;
+    uint8_t support_mel_spec;
+    uint32_t support_measurement_hash_algo;
+    uint32_t support_hash_algo;
+    uint32_t support_asym_algo;
+    uint16_t support_req_asym_algo;
+    uint16_t support_dhe_algo;
+    uint16_t support_aead_algo;
+    uint16_t support_key_schedule_algo;
+    uint8_t support_other_params_support;
+
+    uint8_t session_policy;
+    uint8_t end_session_attributes;
+
+    char *load_state_file_name;
+    char *save_state_file_name;
+
+    uint32_t exe_mode;
+
+    uint32_t exe_connection;
+
+    uint32_t exe_session;
+    
+    /**/ 
+    libspdm_return_t (*spdm_device_send_message)(void *spdm_context,
+                                                 size_t response_size,
+                                                 const void *response,
+                                                 uint64_t timeout);
+    libspdm_return_t (*spdm_device_receive_message)(void *spdm_context,
+                                                    size_t *request_size,
+                                                    void **request,
+                                                    uint64_t timeout);
+
+    libspdm_return_t (*spdm_device_acquire_sender_buffer)(void *context,
+                                                          void **msg_buffer_ptr);
+    void (*spdm_device_release_sender_buffer)(void *context,
+                                              const void *msg_buf_ptr);
+    libspdm_return_t (*spdm_device_acquire_receiver_buffer)(void *context,
+                                                            void **msg_buf_ptr);
+    void (*spdm_device_release_receiver_buffer)(void *context,
+                                                const void *msg_buf_ptr);
+};
+
+void *spdm_responder_init(SpdmDev *spdm_dev);
+void *spdm_requester_init(SpdmDev *spdm_dev);
+
+#define SOCKET_TRANSPORT_TYPE_NONE 0x00
+#define SOCKET_TRANSPORT_TYPE_MCTP 0x01
+#define SOCKET_TRANSPORT_TYPE_PCI_DOE 0x02
+
+#define EXE_MODE_SHUTDOWN 0
+#define EXE_MODE_CONTINUE 1
+
+#define EXE_CONNECTION_VERSION_ONLY 0x1
+#define EXE_CONNECTION_DIGEST 0x2
+#define EXE_CONNECTION_CERT 0x4
+#define EXE_CONNECTION_CHAL 0x8
+#define EXE_CONNECTION_MEAS 0x10
+#define EXE_CONNECTION_SET_CERT 0x20
+#define EXE_CONNECTION_GET_CSR 0x40
+#define EXE_CONNECTION_MEL 0x80
+
+#define EXE_SESSION_KEY_EX 0x1
+#define EXE_SESSION_PSK 0x2
+#define EXE_SESSION_NO_END 0x4
+#define EXE_SESSION_KEY_UPDATE 0x8
+#define EXE_SESSION_HEARTBEAT 0x10
+#define EXE_SESSION_MEAS 0x20
+#define EXE_SESSION_SET_CERT 0x40
+#define EXE_SESSION_GET_CSR 0x80
+#define EXE_SESSION_DIGEST 0x100
+#define EXE_SESSION_CERT 0x200
+#define EXE_SESSION_APP 0x400
+#define EXE_SESSION_MEL 0x800
+
+#define LIBSPDM_TRANSPORT_HEADER_SIZE 64
+#define LIBSPDM_TRANSPORT_TAIL_SIZE 64
+
+/* define common LIBSPDM_TRANSPORT_ADDITIONAL_SIZE. It should be the biggest one. */
+#define LIBSPDM_TRANSPORT_ADDITIONAL_SIZE \
+    (LIBSPDM_TRANSPORT_HEADER_SIZE + LIBSPDM_TRANSPORT_TAIL_SIZE)
+
+#ifndef LIBSPDM_SENDER_BUFFER_SIZE
+#define LIBSPDM_SENDER_BUFFER_SIZE (0x1100 + \
+                                    LIBSPDM_TRANSPORT_ADDITIONAL_SIZE)
+#endif
+#ifndef LIBSPDM_RECEIVER_BUFFER_SIZE
+#define LIBSPDM_RECEIVER_BUFFER_SIZE (0x1200 + \
+                                      LIBSPDM_TRANSPORT_ADDITIONAL_SIZE)
+#endif
+
+/* Maximum size of a single SPDM message.
+ * It matches DataTransferSize in SPDM specification. */
+#define LIBSPDM_SENDER_DATA_TRANSFER_SIZE (LIBSPDM_SENDER_BUFFER_SIZE - \
+                                           LIBSPDM_TRANSPORT_ADDITIONAL_SIZE)
+#define LIBSPDM_RECEIVER_DATA_TRANSFER_SIZE (LIBSPDM_RECEIVER_BUFFER_SIZE - \
+                                             LIBSPDM_TRANSPORT_ADDITIONAL_SIZE)
+#define LIBSPDM_DATA_TRANSFER_SIZE LIBSPDM_RECEIVER_DATA_TRANSFER_SIZE
+
+#if (LIBSPDM_SENDER_BUFFER_SIZE > LIBSPDM_RECEIVER_BUFFER_SIZE)
+#define LIBSPDM_MAX_SENDER_RECEIVER_BUFFER_SIZE LIBSPDM_SENDER_BUFFER_SIZE
+#else
+#define LIBSPDM_MAX_SENDER_RECEIVER_BUFFER_SIZE LIBSPDM_RECEIVER_BUFFER_SIZE
+#endif
+
+/* Maximum size of a large SPDM message.
+ * If chunk is unsupported, it must be same as DATA_TRANSFER_SIZE.
+ * If chunk is supported, it must be larger than DATA_TRANSFER_SIZE.
+ * It matches MaxSPDMmsgSize in SPDM specification. */
+#ifndef LIBSPDM_MAX_SPDM_MSG_SIZE
+#define LIBSPDM_MAX_SPDM_MSG_SIZE 0x1200
+#endif
+
+#ifndef LIBSPDM_MAX_CSR_SIZE
+#define LIBSPDM_MAX_CSR_SIZE 0xffff
+#endif
+
+#endif /* QEMU_SPDM_H */
---
+++ system/spdm.c
@@ -0,0 +1,209 @@
+/*
+ * Utility functions to use LibSPDM
+ */
+
+#include "sysemu/spdm.h"
+
+void *spdm_responder_init(SpdmDev *spdm_dev)
+{
+    libspdm_data_parameter_t parameter;
+    uint8_t data8;
+    uint16_t data16;
+    uint32_t data32;
+    spdm_version_number_t spdm_version;
+
+    /* Check if device is set to be responder */ 
+    if (!spdm_dev->isResponder) {
+        return NULL;
+    }
+
+    /*
+     * Allocate and initialize the SPDM context, then the context 
+     * data is configured using SpdmDev data
+     */
+    spdm_dev->spdm_context = g_malloc(libspdm_get_context_size());
+    libspdm_init_context(spdm_dev->spdm_context);
+
+    libspdm_register_device_io_func(spdm_dev->spdm_context,
+                                    spdm_dev->spdm_device_send_message,
+                                    spdm_dev->spdm_device_receive_message);
+
+    if (spdm_dev->use_transport_layer == SOCKET_TRANSPORT_TYPE_MCTP) {
+        libspdm_register_transport_layer_func(
+            spdm_dev->spdm_context,
+            LIBSPDM_MAX_SPDM_MSG_SIZE,
+            LIBSPDM_TRANSPORT_HEADER_SIZE,
+            LIBSPDM_TRANSPORT_TAIL_SIZE,
+            libspdm_transport_mctp_encode_message,
+            libspdm_transport_mctp_decode_message);
+    } else if (spdm_dev->use_transport_layer == SOCKET_TRANSPORT_TYPE_PCI_DOE) {
+        libspdm_register_transport_layer_func(
+            spdm_dev->spdm_context,
+            LIBSPDM_MAX_SPDM_MSG_SIZE,
+            LIBSPDM_TRANSPORT_HEADER_SIZE,
+            LIBSPDM_TRANSPORT_TAIL_SIZE,
+            libspdm_transport_pci_doe_encode_message,
+            libspdm_transport_pci_doe_decode_message);
+    } else {
+        g_free(spdm_dev->spdm_context);
+        spdm_dev->spdm_context = NULL;
+        return NULL;
+    }
+    libspdm_register_device_buffer_func(spdm_dev->spdm_context,
+                                        LIBSPDM_SENDER_BUFFER_SIZE,
+                                        LIBSPDM_RECEIVER_BUFFER_SIZE,
+                                        spdm_dev->spdm_device_acquire_sender_buffer,
+                                        spdm_dev->spdm_device_release_sender_buffer,
+                                        spdm_dev->spdm_device_acquire_receiver_buffer,
+                                        spdm_dev->spdm_device_release_receiver_buffer);
+
+    spdm_dev->scratch_buffer_size = libspdm_get_sizeof_required_scratch_buffer(spdm_dev->spdm_context);
+    spdm_dev->scratch_buffer = g_malloc(spdm_dev->scratch_buffer_size);
+    libspdm_set_scratch_buffer(spdm_dev->spdm_context,
+                               spdm_dev->scratch_buffer,
+                               spdm_dev->scratch_buffer_size);
+
+    spdm_dev->requester_cert_chain_buffer = g_malloc(SPDM_MAX_CERTIFICATE_CHAIN_SIZE);
+    libspdm_register_cert_chain_buffer(spdm_dev->spdm_context,
+                                       spdm_dev->requester_cert_chain_buffer,
+                                       SPDM_MAX_CERTIFICATE_CHAIN_SIZE);
+
+    if (!libspdm_check_context(spdm_dev->spdm_context)) {
+        return NULL;
+    }
+
+    if (spdm_dev->use_version != 0) {
+        libspdm_zero_mem(&parameter, sizeof(parameter));
+        parameter.location = LIBSPDM_DATA_LOCATION_LOCAL;
+        spdm_version = spdm_dev->use_version << SPDM_VERSION_NUMBER_SHIFT_BIT;
+        libspdm_set_data(spdm_dev->spdm_context,
+                         LIBSPDM_DATA_SPDM_VERSION,
+                         &parameter,
+                         &spdm_version,
+                         sizeof(spdm_version));
+    }
+
+    if (spdm_dev->use_secured_message_version != 0) {
+        libspdm_zero_mem(&parameter, sizeof(parameter));
+        parameter.location = LIBSPDM_DATA_LOCATION_LOCAL;
+        spdm_version = spdm_dev->use_secured_message_version << SPDM_VERSION_NUMBER_SHIFT_BIT;
+        libspdm_set_data(spdm_dev->spdm_context,
+                         LIBSPDM_DATA_SECURED_MESSAGE_VERSION,
+                         &parameter,
+                         &spdm_version,
+                         sizeof(spdm_version));
+    }
+
+    libspdm_zero_mem(&parameter, sizeof(parameter));
+    parameter.location = LIBSPDM_DATA_LOCATION_LOCAL;
+
+    data8 = 0;
+    libspdm_set_data(spdm_dev->spdm_context,
+                     LIBSPDM_DATA_CAPABILITY_CT_EXPONENT,
+                     &parameter,
+                     &data8,
+                     sizeof(data8));
+
+    data32 = spdm_dev->use_responder_capability_flags;
+    if (spdm_dev->use_slot_id == 0xFF) {
+        data32 |= SPDM_GET_CAPABILITIES_RESPONSE_FLAGS_PUB_KEY_ID_CAP;
+        data32 &= ~SPDM_GET_CAPABILITIES_RESPONSE_FLAGS_CERT_CAP;
+        data32 &= ~SPDM_GET_CAPABILITIES_RESPONSE_FLAGS_ALIAS_CERT_CAP;
+        data32 &= ~SPDM_GET_CAPABILITIES_RESPONSE_FLAGS_SET_CERT_CAP;
+        data32 &= ~SPDM_GET_CAPABILITIES_RESPONSE_FLAGS_CSR_CAP;
+        data32 &= ~SPDM_GET_CAPABILITIES_RESPONSE_FLAGS_CERT_INSTALL_RESET_CAP;
+        data32 &= ~SPDM_GET_CAPABILITIES_RESPONSE_FLAGS_MULTI_KEY_CAP;
+        data32 &= ~SPDM_GET_CAPABILITIES_RESPONSE_FLAGS_GET_KEY_PAIR_INFO_CAP;
+        data32 &= ~SPDM_GET_CAPABILITIES_RESPONSE_FLAGS_SET_KEY_PAIR_INFO_CAP;
+    }
+    if (spdm_dev->use_capability_flags != 0) {
+        data32 = spdm_dev->use_capability_flags;
+        spdm_dev->use_responder_capability_flags = spdm_dev->use_capability_flags;
+    }
+    libspdm_set_data(spdm_dev->spdm_context,
+                     LIBSPDM_DATA_CAPABILITY_FLAGS,
+                     &parameter,
+                     &data32,
+                     sizeof(data32));
+
+    data8 = spdm_dev->support_measurement_spec;
+    libspdm_set_data(spdm_dev->spdm_context,
+                     LIBSPDM_DATA_MEASUREMENT_SPEC,
+                     &parameter,
+                     &data8,
+                     sizeof(data8));
+    data32 = spdm_dev->support_measurement_hash_algo;
+    libspdm_set_data(spdm_dev->spdm_context,
+                     LIBSPDM_DATA_MEASUREMENT_HASH_ALGO,
+                     &parameter,
+                     &data32,
+                     sizeof(data32));
+    data32 = spdm_dev->support_asym_algo;
+    libspdm_set_data(spdm_dev->spdm_context,
+                     LIBSPDM_DATA_BASE_ASYM_ALGO,
+                     &parameter,
+                     &data32,
+                     sizeof(data32));
+    data32 = spdm_dev->support_hash_algo;
+    libspdm_set_data(spdm_dev->spdm_context,
+                     LIBSPDM_DATA_BASE_HASH_ALGO,
+                     &parameter,
+                     &data32,
+                     sizeof(data32));
+    data16 = spdm_dev->support_dhe_algo;
+    libspdm_set_data(spdm_dev->spdm_context,
+                     LIBSPDM_DATA_DHE_NAME_GROUP,
+                     &parameter,
+                     &data16,
+                     sizeof(data16));
+    data16 = spdm_dev->support_aead_algo;
+    libspdm_set_data(spdm_dev->spdm_context,
+                     LIBSPDM_DATA_AEAD_CIPHER_SUITE,
+                     &parameter,
+                     &data16,
+                     sizeof(data16));
+    data16 = spdm_dev->support_req_asym_algo;
+    libspdm_set_data(spdm_dev->spdm_context,
+                     LIBSPDM_DATA_REQ_BASE_ASYM_ALG,
+                     &parameter,
+                     &data16,
+                     sizeof(data16));
+    data16 = spdm_dev->support_key_schedule_algo;
+    libspdm_set_data(spdm_dev->spdm_context,
+                     LIBSPDM_DATA_KEY_SCHEDULE,
+                     &parameter,
+                     &data16,
+                     sizeof(data16));
+    data8 = spdm_dev->support_other_params_support;
+    libspdm_set_data(spdm_dev->spdm_context,
+                     LIBSPDM_DATA_OTHER_PARAMS_SUPPORT,
+                     &parameter,
+                     &data8,
+                     sizeof(data8));
+    data8 = spdm_dev->support_mel_spec;
+    libspdm_set_data(spdm_dev->spdm_context,
+                     LIBSPDM_DATA_MEL_SPEC,
+                     &parameter,
+                     &data8,
+                     sizeof(data8));
+
+    data8 = 0xF0;
+    libspdm_set_data(spdm_dev->spdm_context,
+                     LIBSPDM_DATA_HEARTBEAT_PERIOD,
+                     &parameter,
+                     &data8,
+                     sizeof(data8));
+
+    if (spdm_dev->use_version != 0) {
+        libspdm_zero_mem(&parameter, sizeof(parameter));
+        parameter.location = LIBSPDM_DATA_LOCATION_LOCAL;
+        spdm_version = spdm_dev->use_version << SPDM_VERSION_NUMBER_SHIFT_BIT;
+        libspdm_set_data(spdm_dev->spdm_context,
+                         LIBSPDM_DATA_SPDM_VERSION,
+                         &parameter,
+                         &spdm_version,
+                         sizeof(spdm_version));
+    }
+
+    return spdm_dev->spdm_context;
+}
