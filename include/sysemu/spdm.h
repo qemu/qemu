e3dbe5bc335cbee78ce8f6a1e2b25b7749528173
htafr <offreitas@usp.br>
2024-07-25 12:36:14 -0300

    libspdm: add structures for I/O functions

--- include/sysemu/spdm.h
+++ include/sysemu/spdm.h
@@ -15,90 +15,6 @@
 #include "library/spdm_transport_mctp_lib.h"
 #include "library/spdm_transport_pcidoe_lib.h"
 
-typedef struct SpdmDev SpdmDev;
-
-struct SpdmDev {
-    bool isResponder;
-    bool isRequester;
-
-    void *spdm_context;
-
-    void *scratch_buffer;
-    size_t scratch_buffer_size;
-
-    void *requester_cert_chain_buffer;
-
-    uint32_t use_transport_layer;
-    uint32_t use_tcp_handshake;
-    uint8_t use_version;
-    uint8_t use_secured_message_version;
-    uint32_t use_requester_capability_flags;
-    uint32_t use_responder_capability_flags;
-    uint32_t use_capability_flags;
-    uint32_t use_peer_capability_flags;
-
-    uint8_t use_basic_mut_auth;
-    uint8_t use_mut_auth;
-    uint8_t use_measurement_summary_hash_type;
-    uint8_t use_measurement_operation;
-    uint8_t use_measurement_attribute;
-    uint8_t use_slot_id;
-    uint8_t use_slot_count;
-    bool g_private_key_mode;
-
-    libspdm_key_update_action_t use_key_update_action;
-
-    uint32_t use_hash_algo;
-    uint32_t use_measurement_hash_algo;
-    uint32_t use_asym_algo;
-    uint16_t use_req_asym_algo;
-
-    uint8_t support_measurement_spec;
-    uint8_t support_mel_spec;
-    uint32_t support_measurement_hash_algo;
-    uint32_t support_hash_algo;
-    uint32_t support_asym_algo;
-    uint16_t support_req_asym_algo;
-    uint16_t support_dhe_algo;
-    uint16_t support_aead_algo;
-    uint16_t support_key_schedule_algo;
-    uint8_t support_other_params_support;
-
-    uint8_t session_policy;
-    uint8_t end_session_attributes;
-
-    char *load_state_file_name;
-    char *save_state_file_name;
-
-    uint32_t exe_mode;
-
-    uint32_t exe_connection;
-
-    uint32_t exe_session;
-    
-    /**/ 
-    libspdm_return_t (*spdm_device_send_message)(void *spdm_context,
-                                                 size_t response_size,
-                                                 const void *response,
-                                                 uint64_t timeout);
-    libspdm_return_t (*spdm_device_receive_message)(void *spdm_context,
-                                                    size_t *request_size,
-                                                    void **request,
-                                                    uint64_t timeout);
-
-    libspdm_return_t (*spdm_device_acquire_sender_buffer)(void *context,
-                                                          void **msg_buffer_ptr);
-    void (*spdm_device_release_sender_buffer)(void *context,
-                                              const void *msg_buf_ptr);
-    libspdm_return_t (*spdm_device_acquire_receiver_buffer)(void *context,
-                                                            void **msg_buf_ptr);
-    void (*spdm_device_release_receiver_buffer)(void *context,
-                                                const void *msg_buf_ptr);
-};
-
-void *spdm_responder_init(SpdmDev *spdm_dev);
-void *spdm_requester_init(SpdmDev *spdm_dev);
-
 #define SOCKET_TRANSPORT_TYPE_NONE 0x00
 #define SOCKET_TRANSPORT_TYPE_MCTP 0x01
 #define SOCKET_TRANSPORT_TYPE_PCI_DOE 0x02
@@ -169,5 +85,108 @@ void *spdm_requester_init(SpdmDev *spdm_dev);
 #ifndef LIBSPDM_MAX_CSR_SIZE
 #define LIBSPDM_MAX_CSR_SIZE 0xffff
 #endif
+
+typedef struct SpdmDev SpdmDev;
+typedef struct SpdmIO SpdmIO;
+typedef struct SpdmBufferIO SpdmBufferIO;
+
+struct SpdmIO {
+    libspdm_return_t (*spdm_device_send_message)(void *spdm_context,
+                                                 size_t response_size,
+                                                 const void *response,
+                                                 uint64_t timeout);
+    libspdm_return_t (*spdm_device_receive_message)(void *spdm_context,
+                                                    size_t *request_size,
+                                                    void **request,
+                                                    uint64_t timeout);
+};
+
+struct SpdmBufferIO {
+    libspdm_return_t (*spdm_device_acquire_sender_buffer)(void *context, void **msg_buffer_ptr);
+    void (*spdm_device_release_sender_buffer)(void *context, const void *msg_buf_ptr);
+
+    libspdm_return_t (*spdm_device_acquire_receiver_buffer)(void *context, void **msg_buf_ptr);
+    void (*spdm_device_release_receiver_buffer)(void *context, const void *msg_buf_ptr);
+};
+
+struct SpdmDev {
+    bool isResponder;
+    bool isRequester;
+
+    void *spdm_context;
+
+    void *scratch_buffer;
+    size_t scratch_buffer_size;
+
+    void *requester_cert_chain_buffer;
+
+    /* The developer can choose to use only a buffer or to separate them */
+    uint8_t sender_buffer[LIBSPDM_SENDER_BUFFER_SIZE];
+    uint8_t receiver_buffer[LIBSPDM_RECEIVER_BUFFER_SIZE];
+    uint8_t sender_receiver_buffer[LIBSPDM_MAX_SENDER_RECEIVER_BUFFER_SIZE];
+    bool sender_buffer_acquired;
+    bool receiver_buffer_acquired;
+    bool sender_receiver_buffer_acquired;
+
+    uint32_t use_transport_layer;
+    uint32_t use_tcp_handshake;
+    uint8_t use_version;
+    uint8_t use_secured_message_version;
+    uint32_t use_requester_capability_flags;
+    uint32_t use_responder_capability_flags;
+    uint32_t use_capability_flags;
+    uint32_t use_peer_capability_flags;
+
+    uint8_t use_basic_mut_auth;
+    uint8_t use_mut_auth;
+    uint8_t use_measurement_summary_hash_type;
+    uint8_t use_measurement_operation;
+    uint8_t use_measurement_attribute;
+    uint8_t use_slot_id;
+    uint8_t use_slot_count;
+    bool g_private_key_mode;
+
+    libspdm_key_update_action_t use_key_update_action;
+
+    uint32_t use_hash_algo;
+    uint32_t use_measurement_hash_algo;
+    uint32_t use_asym_algo;
+    uint16_t use_req_asym_algo;
+
+    uint8_t support_measurement_spec;
+    uint8_t support_mel_spec;
+    uint32_t support_measurement_hash_algo;
+    uint32_t support_hash_algo;
+    uint32_t support_asym_algo;
+    uint16_t support_req_asym_algo;
+    uint16_t support_dhe_algo;
+    uint16_t support_aead_algo;
+    uint16_t support_key_schedule_algo;
+    uint8_t support_other_params_support;
+
+    uint8_t session_policy;
+    uint8_t end_session_attributes;
+
+    char *load_state_file_name;
+    char *save_state_file_name;
+
+    uint32_t exe_mode;
+
+    uint32_t exe_connection;
+
+    uint32_t exe_session;
+    
+    /**/ 
+    SpdmIO spdm_io;
+    SpdmBufferIO spdm_buffer_io;
+
+    libspdm_return_t (*spdm_get_response_vendor_defined_request)(
+        void *spdm_context, const uint32_t *session_id, bool is_app_message,
+        size_t request_size, const void *request, size_t *response_size,
+        void *response);
+};
+
+void *spdm_responder_init(SpdmDev *spdm_dev);
+void *spdm_requester_init(SpdmDev *spdm_dev);
 
 #endif /* QEMU_SPDM_H */
--- system/spdm.c
+++ system/spdm.c
@@ -6,6 +6,8 @@
 
 void *spdm_responder_init(SpdmDev *spdm_dev)
 {
+    SpdmIO *spdm_io = &spdm_dev->spdm_io;
+    SpdmBufferIO *spdm_buffer_io = &spdm_dev->spdm_buffer_io;
     libspdm_data_parameter_t parameter;
     uint8_t data8;
     uint16_t data16;
@@ -25,8 +27,8 @@ void *spdm_responder_init(SpdmDev *spdm_dev)
     libspdm_init_context(spdm_dev->spdm_context);
 
     libspdm_register_device_io_func(spdm_dev->spdm_context,
-                                    spdm_dev->spdm_device_send_message,
-                                    spdm_dev->spdm_device_receive_message);
+                                    spdm_io->spdm_device_send_message,
+                                    spdm_io->spdm_device_receive_message);
 
     if (spdm_dev->use_transport_layer == SOCKET_TRANSPORT_TYPE_MCTP) {
         libspdm_register_transport_layer_func(
@@ -52,10 +54,10 @@ void *spdm_responder_init(SpdmDev *spdm_dev)
     libspdm_register_device_buffer_func(spdm_dev->spdm_context,
                                         LIBSPDM_SENDER_BUFFER_SIZE,
                                         LIBSPDM_RECEIVER_BUFFER_SIZE,
-                                        spdm_dev->spdm_device_acquire_sender_buffer,
-                                        spdm_dev->spdm_device_release_sender_buffer,
-                                        spdm_dev->spdm_device_acquire_receiver_buffer,
-                                        spdm_dev->spdm_device_release_receiver_buffer);
+                                        spdm_buffer_io->spdm_device_acquire_sender_buffer,
+                                        spdm_buffer_io->spdm_device_release_sender_buffer,
+                                        spdm_buffer_io->spdm_device_acquire_receiver_buffer,
+                                        spdm_buffer_io->spdm_device_release_receiver_buffer);
 
     spdm_dev->scratch_buffer_size = libspdm_get_sizeof_required_scratch_buffer(spdm_dev->spdm_context);
     spdm_dev->scratch_buffer = g_malloc(spdm_dev->scratch_buffer_size);
@@ -76,33 +78,24 @@ void *spdm_responder_init(SpdmDev *spdm_dev)
         libspdm_zero_mem(&parameter, sizeof(parameter));
         parameter.location = LIBSPDM_DATA_LOCATION_LOCAL;
         spdm_version = spdm_dev->use_version << SPDM_VERSION_NUMBER_SHIFT_BIT;
-        libspdm_set_data(spdm_dev->spdm_context,
-                         LIBSPDM_DATA_SPDM_VERSION,
-                         &parameter,
-                         &spdm_version,
-                         sizeof(spdm_version));
+        libspdm_set_data(spdm_dev->spdm_context, LIBSPDM_DATA_SPDM_VERSION,
+                         &parameter, &spdm_version, sizeof(spdm_version));
     }
 
     if (spdm_dev->use_secured_message_version != 0) {
         libspdm_zero_mem(&parameter, sizeof(parameter));
         parameter.location = LIBSPDM_DATA_LOCATION_LOCAL;
         spdm_version = spdm_dev->use_secured_message_version << SPDM_VERSION_NUMBER_SHIFT_BIT;
-        libspdm_set_data(spdm_dev->spdm_context,
-                         LIBSPDM_DATA_SECURED_MESSAGE_VERSION,
-                         &parameter,
-                         &spdm_version,
-                         sizeof(spdm_version));
+        libspdm_set_data(spdm_dev->spdm_context, LIBSPDM_DATA_SECURED_MESSAGE_VERSION,
+                         &parameter, &spdm_version, sizeof(spdm_version));
     }
 
     libspdm_zero_mem(&parameter, sizeof(parameter));
     parameter.location = LIBSPDM_DATA_LOCATION_LOCAL;
 
     data8 = 0;
-    libspdm_set_data(spdm_dev->spdm_context,
-                     LIBSPDM_DATA_CAPABILITY_CT_EXPONENT,
-                     &parameter,
-                     &data8,
-                     sizeof(data8));
+    libspdm_set_data(spdm_dev->spdm_context, LIBSPDM_DATA_CAPABILITY_CT_EXPONENT,
+                     &parameter, &data8, sizeof(data8));
 
     data32 = spdm_dev->use_responder_capability_flags;
     if (spdm_dev->use_slot_id == 0xFF) {
@@ -120,90 +113,54 @@ void *spdm_responder_init(SpdmDev *spdm_dev)
         data32 = spdm_dev->use_capability_flags;
         spdm_dev->use_responder_capability_flags = spdm_dev->use_capability_flags;
     }
-    libspdm_set_data(spdm_dev->spdm_context,
-                     LIBSPDM_DATA_CAPABILITY_FLAGS,
-                     &parameter,
-                     &data32,
-                     sizeof(data32));
+    libspdm_set_data(spdm_dev->spdm_context, LIBSPDM_DATA_CAPABILITY_FLAGS,
+                     &parameter,&data32, sizeof(data32));
 
     data8 = spdm_dev->support_measurement_spec;
-    libspdm_set_data(spdm_dev->spdm_context,
-                     LIBSPDM_DATA_MEASUREMENT_SPEC,
-                     &parameter,
-                     &data8,
-                     sizeof(data8));
+    libspdm_set_data(spdm_dev->spdm_context, LIBSPDM_DATA_MEASUREMENT_SPEC,
+                     &parameter, &data8, sizeof(data8));
     data32 = spdm_dev->support_measurement_hash_algo;
-    libspdm_set_data(spdm_dev->spdm_context,
-                     LIBSPDM_DATA_MEASUREMENT_HASH_ALGO,
-                     &parameter,
-                     &data32,
-                     sizeof(data32));
+    libspdm_set_data(spdm_dev->spdm_context, LIBSPDM_DATA_MEASUREMENT_HASH_ALGO,
+                     &parameter, &data32, sizeof(data32));
     data32 = spdm_dev->support_asym_algo;
-    libspdm_set_data(spdm_dev->spdm_context,
-                     LIBSPDM_DATA_BASE_ASYM_ALGO,
-                     &parameter,
-                     &data32,
-                     sizeof(data32));
+    libspdm_set_data(spdm_dev->spdm_context, LIBSPDM_DATA_BASE_ASYM_ALGO,
+                     &parameter, &data32, sizeof(data32));
     data32 = spdm_dev->support_hash_algo;
-    libspdm_set_data(spdm_dev->spdm_context,
-                     LIBSPDM_DATA_BASE_HASH_ALGO,
-                     &parameter,
-                     &data32,
-                     sizeof(data32));
+    libspdm_set_data(spdm_dev->spdm_context, LIBSPDM_DATA_BASE_HASH_ALGO,
+                     &parameter, &data32, sizeof(data32));
     data16 = spdm_dev->support_dhe_algo;
-    libspdm_set_data(spdm_dev->spdm_context,
-                     LIBSPDM_DATA_DHE_NAME_GROUP,
-                     &parameter,
-                     &data16,
-                     sizeof(data16));
+    libspdm_set_data(spdm_dev->spdm_context, LIBSPDM_DATA_DHE_NAME_GROUP,
+                     &parameter, &data16, sizeof(data16));
     data16 = spdm_dev->support_aead_algo;
-    libspdm_set_data(spdm_dev->spdm_context,
-                     LIBSPDM_DATA_AEAD_CIPHER_SUITE,
-                     &parameter,
-                     &data16,
-                     sizeof(data16));
+    libspdm_set_data(spdm_dev->spdm_context, LIBSPDM_DATA_AEAD_CIPHER_SUITE,
+                     &parameter, &data16, sizeof(data16));
     data16 = spdm_dev->support_req_asym_algo;
-    libspdm_set_data(spdm_dev->spdm_context,
-                     LIBSPDM_DATA_REQ_BASE_ASYM_ALG,
-                     &parameter,
-                     &data16,
-                     sizeof(data16));
+    libspdm_set_data(spdm_dev->spdm_context, LIBSPDM_DATA_REQ_BASE_ASYM_ALG,
+                     &parameter, &data16, sizeof(data16));
     data16 = spdm_dev->support_key_schedule_algo;
-    libspdm_set_data(spdm_dev->spdm_context,
-                     LIBSPDM_DATA_KEY_SCHEDULE,
-                     &parameter,
-                     &data16,
-                     sizeof(data16));
+    libspdm_set_data(spdm_dev->spdm_context, LIBSPDM_DATA_KEY_SCHEDULE,
+                     &parameter, &data16, sizeof(data16));
     data8 = spdm_dev->support_other_params_support;
-    libspdm_set_data(spdm_dev->spdm_context,
-                     LIBSPDM_DATA_OTHER_PARAMS_SUPPORT,
-                     &parameter,
-                     &data8,
-                     sizeof(data8));
+    libspdm_set_data(spdm_dev->spdm_context, LIBSPDM_DATA_OTHER_PARAMS_SUPPORT,
+                     &parameter, &data8, sizeof(data8));
     data8 = spdm_dev->support_mel_spec;
-    libspdm_set_data(spdm_dev->spdm_context,
-                     LIBSPDM_DATA_MEL_SPEC,
-                     &parameter,
-                     &data8,
-                     sizeof(data8));
+    libspdm_set_data(spdm_dev->spdm_context, LIBSPDM_DATA_MEL_SPEC,
+                     &parameter, &data8, sizeof(data8));
 
     data8 = 0xF0;
-    libspdm_set_data(spdm_dev->spdm_context,
-                     LIBSPDM_DATA_HEARTBEAT_PERIOD,
-                     &parameter,
-                     &data8,
-                     sizeof(data8));
+    libspdm_set_data(spdm_dev->spdm_context, LIBSPDM_DATA_HEARTBEAT_PERIOD,
+                     &parameter, &data8, sizeof(data8));
 
     if (spdm_dev->use_version != 0) {
         libspdm_zero_mem(&parameter, sizeof(parameter));
         parameter.location = LIBSPDM_DATA_LOCATION_LOCAL;
         spdm_version = spdm_dev->use_version << SPDM_VERSION_NUMBER_SHIFT_BIT;
-        libspdm_set_data(spdm_dev->spdm_context,
-                         LIBSPDM_DATA_SPDM_VERSION,
-                         &parameter,
-                         &spdm_version,
-                         sizeof(spdm_version));
+        libspdm_set_data(spdm_dev->spdm_context, LIBSPDM_DATA_SPDM_VERSION,
+                         &parameter, &spdm_version, sizeof(spdm_version));
     }
+
+    libspdm_register_get_response_func(spdm_dev->spdm_context,
+                                       spdm_dev->spdm_get_response_vendor_defined_request);
 
     return spdm_dev->spdm_context;
 }
