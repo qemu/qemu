273db89bcaf42102580a7994fdf7f0651e35092c
Aditya Gupta <adityag@linux.ibm.com>
2024-07-31 11:20:22 +0530

    ppc/pseries: Add Power11 cpu type
    Add sPAPR CPU Core definition for Power11
    
    Reviewed-by: Harsh Prateek Bora <harshpb@linux.ibm.com>
    Reviewed-by: Nicholas Piggin <npiggin@gmail.com>
    Signed-off-by: Aditya Gupta <adityag@linux.ibm.com>
    Tested-by: Amit Machhiwal <amachhiw@linux.ibm.com>
    Signed-off-by: Nicholas Piggin <npiggin@gmail.com>

--- hw/ppc/spapr_cpu_core.c
+++ hw/ppc/spapr_cpu_core.c
@@ -405,6 +405,7 @@ static const TypeInfo spapr_cpu_core_type_infos[] = {
     DEFINE_SPAPR_CPU_CORE_TYPE("power9_v2.0"),
     DEFINE_SPAPR_CPU_CORE_TYPE("power9_v2.2"),
     DEFINE_SPAPR_CPU_CORE_TYPE("power10_v2.0"),
+    DEFINE_SPAPR_CPU_CORE_TYPE("power11_v2.0"),
 #ifdef CONFIG_KVM
     DEFINE_SPAPR_CPU_CORE_TYPE("host"),
 #endif
