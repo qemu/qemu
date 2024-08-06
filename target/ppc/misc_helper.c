a0f6b02792a3332c6d108f6eef002b97e30e4ab5
Nicholas Piggin <npiggin@gmail.com>
2024-08-06 23:13:13 +1000

    target/ppc: Fix mtDPDES targeting SMT siblings
    A typo in the loop over SMT threads to set irq level for doorbells
    when storing to DPDES meant everything was aimed at the CPU executing
    the instruction.
    
    Cc: qemu-stable@nongnu.org
    Fixes: d24e80b2ae ("target/ppc: Add msgsnd/p and DPDES SMT support")
    Reviewed-by: Philippe Mathieu-Daudé <philmd@linaro.org>
    Reviewed-by: Richard Henderson <richard.henderson@linaro.org>
    Signed-off-by: Nicholas Piggin <npiggin@gmail.com>
    (cherry picked from commit 0324d236d2918c18a9ad4a1081b1083965a1433b)
    Signed-off-by: Michael Tokarev <mjt@tls.msk.ru>

--- target/ppc/misc_helper.c
+++ target/ppc/misc_helper.c
@@ -283,7 +283,7 @@ void helper_store_dpdes(CPUPPCState *env, target_ulong val)
         PowerPCCPU *ccpu = POWERPC_CPU(ccs);
         uint32_t thread_id = ppc_cpu_tir(ccpu);
 
-        ppc_set_irq(cpu, PPC_INTERRUPT_DOORBELL, val & (0x1 << thread_id));
+        ppc_set_irq(ccpu, PPC_INTERRUPT_DOORBELL, val & (0x1 << thread_id));
     }
     qemu_mutex_unlock_iothread();
 }
