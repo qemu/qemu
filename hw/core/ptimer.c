75e6a93d18dba4412459c984776d4d4cfbec12bb
Jianzhou Yue <JianZhou.Yue@verisilicon.com>
2024-08-09 17:37:56 +0100

    hw/core/ptimer: fix timer zero period condition for freq > 1GHz
    The real period is zero when both period and period_frac are zero.
    Check the method ptimer_set_freq, if freq is larger than 1000 MHz,
    the period is zero, but the period_frac is not, in this case, the
    ptimer will work but the current code incorrectly recognizes that
    the ptimer is disabled.
    
    Resolves: https://gitlab.com/qemu-project/qemu/-/issues/2306
    Signed-off-by: JianZhou Yue <JianZhou.Yue@verisilicon.com>
    Message-id: 3DA024AEA8B57545AF1B3CAA37077D0FB75E82C8@SHASXM03.verisilicon.com
    Reviewed-by: Peter Maydell <peter.maydell@linaro.org>
    Signed-off-by: Peter Maydell <peter.maydell@linaro.org>
    (cherry picked from commit 446e5e8b4515e9a7be69ef6a29852975289bb6f0)
    Signed-off-by: Michael Tokarev <mjt@tls.msk.ru>

--- hw/core/ptimer.c
+++ hw/core/ptimer.c
@@ -83,7 +83,7 @@ static void ptimer_reload(ptimer_state *s, int delta_adjust)
         delta = s->delta = s->limit;
     }
 
-    if (s->period == 0) {
+    if (s->period == 0 && s->period_frac == 0) {
         if (!qtest_enabled()) {
             fprintf(stderr, "Timer with period zero, disabling\n");
         }
@@ -309,7 +309,7 @@ void ptimer_run(ptimer_state *s, int oneshot)
 
     assert(s->in_transaction);
 
-    if (was_disabled && s->period == 0) {
+    if (was_disabled && s->period == 0 && s->period_frac == 0) {
         if (!qtest_enabled()) {
             fprintf(stderr, "Timer with period zero, disabling\n");
         }
