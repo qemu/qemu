253ec604a84686231817443ea107a636bf1fc09a
Nicholas Piggin <npiggin@gmail.com>
2024-08-13 21:23:22 +0100

    tests/avocado: replay_kernel.py add x86-64 q35 machine test
    The x86-64 pc machine is flaky with record/replay, but q35 is more
    stable. Add a q35 test to replay_kernel.py.
    
    Reviewed-by: Alex Bennée <alex.bennee@linaro.org>
    Tested-by: Alex Bennée <alex.bennee@linaro.org>
    Signed-off-by: Nicholas Piggin <npiggin@gmail.com>
    Message-Id: <20240813050638.446172-7-npiggin@gmail.com>
    Signed-off-by: Alex Bennée <alex.bennee@linaro.org>
    Message-Id: <20240813202329.1237572-15-alex.bennee@linaro.org>

--- tests/avocado/replay_kernel.py
+++ tests/avocado/replay_kernel.py
@@ -110,12 +110,28 @@ class ReplayKernelNormal(ReplayKernelBase):
         self.run_rr(kernel_path, kernel_command_line, console_pattern, shift=5)
 
     # See https://gitlab.com/qemu-project/qemu/-/issues/2094
-    @skipUnless(os.getenv('QEMU_TEST_FLAKY_TESTS'), 'Test sometimes gets stuck')
+    @skipUnless(os.getenv('QEMU_TEST_FLAKY_TESTS'), 'pc machine is unstable with replay')
     def test_x86_64_pc(self):
         """
         :avocado: tags=arch:x86_64
         :avocado: tags=machine:pc
         :avocado: tags=flaky
+        """
+        kernel_url = ('https://archives.fedoraproject.org/pub/archive/fedora'
+                      '/linux/releases/29/Everything/x86_64/os/images/pxeboot'
+                      '/vmlinuz')
+        kernel_hash = '23bebd2680757891cf7adedb033532163a792495'
+        kernel_path = self.fetch_asset(kernel_url, asset_hash=kernel_hash)
+
+        kernel_command_line = self.KERNEL_COMMON_COMMAND_LINE + 'console=ttyS0'
+        console_pattern = 'VFS: Cannot open root device'
+
+        self.run_rr(kernel_path, kernel_command_line, console_pattern, shift=5)
+
+    def test_x86_64_q35(self):
+        """
+        :avocado: tags=arch:x86_64
+        :avocado: tags=machine:q35
         """
         kernel_url = ('https://archives.fedoraproject.org/pub/archive/fedora'
                       '/linux/releases/29/Everything/x86_64/os/images/pxeboot'
