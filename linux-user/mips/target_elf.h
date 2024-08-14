8a9322814e9704001ed8fb6dd086abf11494912d
Philippe Mathieu-Daudé <philmd@linaro.org>
2024-08-14 12:01:18 +0200

    linux-user/mips: Do not try to use removed R5900 CPU
    R5900 emulation was removed in commit 823f2897bd.
    Remove it from ELF parsing in order to avoid:
    
      $ qemu-mipsn32 ./test5900
      qemu-mipsn32: unable to find CPU model 'R5900'
    
    This reverts commit 4d9e5a0eb7df6e98ac6cf5e16029f35dd05b9537.
    
    Fixes: 823f2897bd ("target/mips: Disable R5900 support")
    Signed-off-by: Philippe Mathieu-Daudé <philmd@linaro.org>
    Reviewed-by: Richard Henderson <richard.henderson@linaro.org>
    Message-Id: <20240814133928.6746-2-philmd@linaro.org>
    (cherry picked from commit f7e3d7521b41ada97c5344914d3c9bc6ed04c82a)
    Signed-off-by: Michael Tokarev <mjt@tls.msk.ru>

--- linux-user/mips/target_elf.h
+++ linux-user/mips/target_elf.h
@@ -12,9 +12,6 @@ static inline const char *cpu_get_model(uint32_t eflags)
     if ((eflags & EF_MIPS_ARCH) == EF_MIPS_ARCH_32R6) {
         return "mips32r6-generic";
     }
-    if ((eflags & EF_MIPS_MACH) == EF_MIPS_MACH_5900) {
-        return "R5900";
-    }
     if (eflags & EF_MIPS_NAN2008) {
         return "P5600";
     }
