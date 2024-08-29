1d8abce868fa6ef8cab76f152c812f300141d2b3
Brian Cain <bcain@quicinc.com>
2024-08-28 17:56:18 -0700

    target/hexagon: Add representation to count cycles
    The PCYCLE register can be enabled to indicate accumulated clock cycles.
    
    Signed-off-by: Brian Cain <brian.cain@oss.qualcomm.com>

--- target/hexagon/cpu.c
+++ target/hexagon/cpu.c
@@ -329,6 +329,7 @@ static void hexagon_cpu_reset_hold(Object *obj, ResetType type)
 
     if (cs->cpu_index == 0) {
         arch_set_system_reg(env, HEX_SREG_MODECTL, 0x1);
+        *(env->g_pcycle_base) = 0;
     }
     mmu_reset(env);
     arch_set_system_reg(env, HEX_SREG_HTID, cs->cpu_index);
@@ -391,10 +392,12 @@ static void hexagon_cpu_realize(DeviceState *dev, Error **errp)
 #ifndef CONFIG_USER_ONLY
     if (cs->cpu_index == 0) {
         env->g_sreg = g_new0(target_ulong, NUM_SREGS);
+        env->g_pcycle_base = g_malloc0(sizeof(*env->g_pcycle_base));
     } else {
         CPUState *cpu0 = qemu_get_cpu(0);
         CPUHexagonState *env0 = cpu_env(cpu0);
         env->g_sreg = env0->g_sreg;
+        env->g_pcycle_base = env0->g_pcycle_base;
     }
 #endif
 
