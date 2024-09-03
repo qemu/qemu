9ef4d9d51aa0baa580a4b9694a8de1a93473c50d
Peter Maydell <peter.maydell@linaro.org>
2024-09-03 17:07:15 +0100

    hw/input: Remove pxa2xx_keypad
    Remove the pxa2xx-specific pxa2xx_keypad device.
    
    Signed-off-by: Peter Maydell <peter.maydell@linaro.org>
    Reviewed-by: Philippe Mathieu-Daudé <philmd@linaro.org>
    Tested-by: Philippe Mathieu-Daudé <philmd@linaro.org>
    Message-id: 20240903160751.4100218-18-peter.maydell@linaro.org

--- include/hw/arm/pxa.h
+++ include/hw/arm/pxa.h
@@ -94,16 +94,4 @@ int pxa2xx_pcmcia_attach(void *opaque, PCMCIACardState *card);
 int pxa2xx_pcmcia_detach(void *opaque);
 void pxa2xx_pcmcia_set_irq_cb(void *opaque, qemu_irq irq, qemu_irq cd_irq);
 
-/* pxa2xx_keypad.c */
-struct  keymap {
-    int8_t column;
-    int8_t row;
-};
-typedef struct PXA2xxKeyPadState PXA2xxKeyPadState;
-PXA2xxKeyPadState *pxa27x_keypad_init(MemoryRegion *sysmem,
-                                      hwaddr base,
-                                      qemu_irq irq);
-void pxa27x_register_keypad(PXA2xxKeyPadState *kp,
-                            const struct keymap *map, int size);
-
 #endif /* PXA_H */
