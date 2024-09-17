e87ced60a3aa9ce77dfff4ef38837bfad1a9a6eb
Philippe Mathieu-Daudé <philmd@linaro.org>
2024-09-17 14:07:56 +0200

    ui/cocoa: Temporarily ignore annoying deprecated declaration warnings
    These warnings are breaking some build configurations since 2 months
    now (https://gitlab.com/qemu-project/qemu/-/issues/2575):
    
      ui/cocoa.m:662:14: error: 'CVDisplayLinkCreateWithCGDisplay' is deprecated: first deprecated in macOS 15.0 - use NSView.displayLink(target:selector:), NSWindow.displayLink(target:selector:), or NSScreen.displayLink(target:selector:)  [-Werror,-Wdeprecated-declarations]
        662 |         if (!CVDisplayLinkCreateWithCGDisplay(display, &displayLink)) {
            |              ^
      /Library/Developer/CommandLineTools/SDKs/MacOSX.sdk/System/Library/Frameworks/CoreVideo.framework/Headers/CVDisplayLink.h:89:20: note: 'CVDisplayLinkCreateWithCGDisplay' has been explicitly marked deprecated here
         89 | CV_EXPORT CVReturn CVDisplayLinkCreateWithCGDisplay(
            |                    ^
      ui/cocoa.m:663:29: error: 'CVDisplayLinkGetNominalOutputVideoRefreshPeriod' is deprecated: first deprecated in macOS 15.0 - use NSView.displayLink(target:selector:), NSWindow.displayLink(target:selector:), or NSScreen.displayLink(target:selector:)  [-Werror,-Wdeprecated-declarations]
        663 |             CVTime period = CVDisplayLinkGetNominalOutputVideoRefreshPeriod(displayLink);
            |                             ^
      /Library/Developer/CommandLineTools/SDKs/MacOSX.sdk/System/Library/Frameworks/CoreVideo.framework/Headers/CVDisplayLink.h:182:18: note: 'CVDisplayLinkGetNominalOutputVideoRefreshPeriod' has been explicitly marked deprecated here
        182 | CV_EXPORT CVTime CVDisplayLinkGetNominalOutputVideoRefreshPeriod( CVDisplayLinkRef CV_NONNULL displayLink );
            |                  ^
      ui/cocoa.m:664:13: error: 'CVDisplayLinkRelease' is deprecated: first deprecated in macOS 15.0 - use NSView.displayLink(target:selector:), NSWindow.displayLink(target:selector:), or NSScreen.displayLink(target:selector:)  [-Werror,-Wdeprecated-declarations]
        664 |             CVDisplayLinkRelease(displayLink);
            |             ^
      /Library/Developer/CommandLineTools/SDKs/MacOSX.sdk/System/Library/Frameworks/CoreVideo.framework/Headers/CVDisplayLink.h:249:16: note: 'CVDisplayLinkRelease' has been explicitly marked deprecated here
        249 | CV_EXPORT void CVDisplayLinkRelease( CV_RELEASES_ARGUMENT CVDisplayLinkRef CV_NULLABLE displayLink );
            |                ^
      3 errors generated.
    
    For the next release, ignore the warnings using #pragma directives.
    At least until we figure the correct new API usage.
    
    Signed-off-by: Philippe Mathieu-Daudé <philmd@linaro.org>
    Reviewed-by: Phil Dennis-Jordan <phil@philjordan.eu>
    Tested-by: Phil Dennis-Jordan <phil@philjordan.eu>
    Message-Id: <20241121131954.98949-1-philmd@linaro.org>
    (cherry picked from commit 9cf6e41fe293dd56089faac94c36ff5cb3d96726)
    Signed-off-by: Michael Tokarev <mjt@tls.msk.ru>

--- ui/cocoa.m
+++ ui/cocoa.m
@@ -549,6 +549,9 @@ static CGEventRef handleTapEvent(CGEventTapProxy proxy, CGEventType type, CGEven
     }
 }
 
+#pragma clang diagnostic push
+#pragma clang diagnostic ignored "-Wdeprecated-declarations"
+
 - (void) updateUIInfoLocked
 {
     /* Must be called with the iothread lock, i.e. via updateUIInfo */
@@ -593,6 +596,8 @@ static CGEventRef handleTapEvent(CGEventTapProxy proxy, CGEventType type, CGEven
 
     dpy_set_ui_info(dcl.con, &info, TRUE);
 }
+
+#pragma clang diagnostic pop
 
 - (void) updateUIInfo
 {
