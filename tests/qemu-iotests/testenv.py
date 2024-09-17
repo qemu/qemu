fd7294335c9ce7f9cb5e4b5e3b3148d9e8cce3bd
Thomas Huth <thuth@redhat.com>
2024-09-17 21:43:50 +0200

    tests/qemu-iotests/testenv: Use the "r2d" machine for sh4/sh4eb
    Commit 0ea0538fae516f9b4 removed the default machine of the sh4
    binaries, so a lot of iotests are failing now without such a default
    machine. Teach the iotest harness to use the "r2d" machine instead
    to fix this problem.
    
    Signed-off-by: Thomas Huth <thuth@redhat.com>
    Signed-off-by: Michael Tokarev <mjt@tls.msk.ru>

--- tests/qemu-iotests/testenv.py
+++ tests/qemu-iotests/testenv.py
@@ -244,6 +244,8 @@ class TestEnv(ContextManager['TestEnv']):
             ('riscv32', 'virt'),
             ('riscv64', 'virt'),
             ('rx', 'gdbsim-r5f562n8'),
+            ('sh4', 'r2d'),
+            ('sh4eb', 'r2d'),
             ('tricore', 'tricore_testboard')
         )
         for suffix, machine in machine_map:
