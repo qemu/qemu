5375bc1674590b19e2c138d06a954420ac9b5c4d
Philippe Mathieu-Daudé <philmd@linaro.org>
2024-09-26 19:06:02 +0200

    target/mips: Declare mips_env_is_bigendian() in 'internal.h'
    In order to re-use cpu_is_bigendian(), declare it on "internal.h"
    after renaming it as mips_env_is_bigendian().
    
    Reviewed-by: Jiaxun Yang <jiaxun.yang@flygoat.com>
    Tested-by: Jiaxun Yang <jiaxun.yang@flygoat.com>
    Signed-off-by: Philippe Mathieu-Daudé <philmd@linaro.org>
    Reviewed-by: Richard Henderson <richard.henderson@linaro.org>
    Message-Id: <20241010215015.44326-2-philmd@linaro.org>

--- target/mips/internal.h
+++ target/mips/internal.h
@@ -225,6 +225,11 @@ static inline void mips_env_set_pc(CPUMIPSState *env, target_ulong value)
     }
 }
 
+static inline bool mips_env_is_bigendian(CPUMIPSState *env)
+{
+    return extract32(env->CP0_Config0, CP0C0_BE, 1);
+}
+
 static inline void restore_pamask(CPUMIPSState *env)
 {
     if (env->hflags & MIPS_HFLAG_ELPA) {
