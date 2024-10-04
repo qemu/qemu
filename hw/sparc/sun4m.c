3b3033a0bfba40a751ec79ab67ed62023910f383
Philippe Mathieu-Daudé <philmd@linaro.org>
2024-10-04 11:44:50 +0200

    hw/sparc: Use explicit big-endian LD/ST API
    The SPARC architecture uses big endianness. Directly use
    the big-endian LD/ST API.
    
    Mechanical change running:
    
      $ for a in uw w l q; do \
          sed -i -e "s/ld${a}_p(/ld${a}_be_p(/" \
            $(git grep -wlE '(ld|st)u?[wlq]_p' hw/sparc*/);
        done
    
    Signed-off-by: Philippe Mathieu-Daudé <philmd@linaro.org>
    Reviewed-by: Mark Cave-Ayland <mark.cave-ayland@ilande.co.uk>
    Reviewed-by: Chigot Clément <chigot@adacore.com>
    Reviewed-by: Manos Pitsidianakis <manos.pitsidianakis@linaro.org>
    Message-ID: <20251218221457.73341-2-philmd@linaro.org>

--- hw/sparc/sun4m.c
+++ hw/sparc/sun4m.c
@@ -268,9 +268,9 @@ static unsigned long sun4m_load_kernel(const char *kernel_filename,
         if (*initrd_size > 0) {
             for (i = 0; i < 64 * TARGET_PAGE_SIZE; i += TARGET_PAGE_SIZE) {
                 ptr = rom_ptr(KERNEL_LOAD_ADDR + i, 24);
-                if (ptr && ldl_p(ptr) == 0x48647253) { /* HdrS */
-                    stl_p(ptr + 16, INITRD_LOAD_ADDR);
-                    stl_p(ptr + 20, *initrd_size);
+                if (ptr && ldl_be_p(ptr) == 0x48647253) { /* HdrS */
+                    stl_be_p(ptr + 16, INITRD_LOAD_ADDR);
+                    stl_be_p(ptr + 20, *initrd_size);
                     break;
                 }
             }
