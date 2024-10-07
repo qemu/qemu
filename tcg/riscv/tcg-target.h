101c1ef56221926718eb70d33ac2844d139d55e0
TANG Tiancheng <tangtiancheng.ttc@alibaba-inc.com>
2024-10-07 10:56:56 +0800

    tcg/riscv: Implement vector sat/mul ops
    Signed-off-by: TANG Tiancheng <tangtiancheng.ttc@alibaba-inc.com>
    Reviewed-by: Liu Zhiwei <zhiwei_liu@linux.alibaba.com>
    Reviewed-by: Richard Henderson <richard.henderson@linaro.org>
    Message-ID: <20241007025700.47259-9-zhiwei_liu@linux.alibaba.com>
    Signed-off-by: Richard Henderson <richard.henderson@linaro.org>

--- tcg/riscv/tcg-target.h
+++ tcg/riscv/tcg-target.h
@@ -160,8 +160,8 @@ typedef enum {
 #define TCG_TARGET_HAS_shi_vec          0
 #define TCG_TARGET_HAS_shs_vec          0
 #define TCG_TARGET_HAS_shv_vec          0
-#define TCG_TARGET_HAS_mul_vec          0
-#define TCG_TARGET_HAS_sat_vec          0
+#define TCG_TARGET_HAS_mul_vec          1
+#define TCG_TARGET_HAS_sat_vec          1
 #define TCG_TARGET_HAS_minmax_vec       0
 #define TCG_TARGET_HAS_bitsel_vec       0
 #define TCG_TARGET_HAS_cmpsel_vec       1
