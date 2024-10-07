c283c0748a01ec05f24d5aa24409f42b459e8938
TANG Tiancheng <tangtiancheng.ttc@alibaba-inc.com>
2024-10-07 10:56:55 +0800

    tcg/riscv: Implement vector neg ops
    Signed-off-by: TANG Tiancheng <tangtiancheng.ttc@alibaba-inc.com>
    Reviewed-by: Liu Zhiwei <zhiwei_liu@linux.alibaba.com>
    Reviewed-by: Richard Henderson <richard.henderson@linaro.org>
    Message-ID: <20241007025700.47259-8-zhiwei_liu@linux.alibaba.com>
    Signed-off-by: Richard Henderson <richard.henderson@linaro.org>

--- tcg/riscv/tcg-target.h
+++ tcg/riscv/tcg-target.h
@@ -152,7 +152,7 @@ typedef enum {
 #define TCG_TARGET_HAS_nor_vec          0
 #define TCG_TARGET_HAS_eqv_vec          0
 #define TCG_TARGET_HAS_not_vec          1
-#define TCG_TARGET_HAS_neg_vec          0
+#define TCG_TARGET_HAS_neg_vec          1
 #define TCG_TARGET_HAS_abs_vec          0
 #define TCG_TARGET_HAS_roti_vec         0
 #define TCG_TARGET_HAS_rots_vec         0
