24a6271ebb2dd253952e7dcfb5420a37ffde86ba
Alejandro Zeise <alejandro.zeise@seagate.com>
2024-10-08 09:57:22 +0200

    crypto/hashpriv: Remove old hash API function
    Remove old hash_bytesv function, as it was replaced by the 4
    new functions.
    
    Signed-off-by: Alejandro Zeise <alejandro.zeise@seagate.com>
    Reviewed-by: Daniel P. Berrangé <berrange@redhat.com>
    Signed-off-by: Daniel P. Berrangé <berrange@redhat.com>

--- crypto/hashpriv.h
+++ crypto/hashpriv.h
@@ -21,12 +21,6 @@
 typedef struct QCryptoHashDriver QCryptoHashDriver;
 
 struct QCryptoHashDriver {
-    int (*hash_bytesv)(QCryptoHashAlgo alg,
-                       const struct iovec *iov,
-                       size_t niov,
-                       uint8_t **result,
-                       size_t *resultlen,
-                       Error **errp);
     QCryptoHash *(*hash_new)(QCryptoHashAlgo alg, Error **errp);
     int (*hash_update)(QCryptoHash *hash,
                        const struct iovec *iov,
