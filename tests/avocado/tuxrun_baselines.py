4007fc948702f8f6ee5edc0522304c96f7bc10bc
Thomas Huth <thuth@redhat.com>
2024-10-11 15:19:26 +0200

    tests/functional: Convert the Avocado i386 tuxrun test
    Move the tests to a new file so that they can be run via
    qemu-system-i386 in the functional framework.
    
    Signed-off-by: Thomas Huth <thuth@redhat.com>
    Message-ID: <20241011131937.377223-10-thuth@redhat.com>

--- tests/avocado/tuxrun_baselines.py
+++ tests/avocado/tuxrun_baselines.py
@@ -223,22 +223,6 @@ class TuxRunBaselineTest(QemuSystemTest):
                  "e6ffd8813c8a335bc15728f2835f90539c84be7f8f5f691a8b01451b47fb4bd7"}
         self.common_tuxrun(csums=sums)
 
-    def test_i386(self):
-        """
-        :avocado: tags=arch:i386
-        :avocado: tags=cpu:coreduo
-        :avocado: tags=machine:q35
-        :avocado: tags=tuxboot:i386
-        :avocado: tags=image:bzImage
-        :avocado: tags=shutdown:nowait
-        """
-        sums = {"bzImage" :
-                "a3e5b32a354729e65910f5a1ffcda7c14a6c12a55e8213fb86e277f1b76ed956",
-                "rootfs.ext4.zst" :
-                "f15e66b2bf673a210ec2a4b2e744a80530b36289e04f5388aab812b97f69754a" }
-
-        self.common_tuxrun(csums=sums, drive="virtio-blk-pci")
-
     def test_mips32(self):
         """
         :avocado: tags=arch:mips
