9d41f02760f3093d3b51cee5900ddf9ea0a4778a
thomasw04 <35061939+thomasw04@users.noreply.github.com>
2024-10-15 19:06:57 +0200

    rng: more progress.

--- hw/misc/stm32l4r5_rng.c
+++ hw/misc/stm32l4r5_rng.c
@@ -4,6 +4,7 @@
 #include "qemu/guest-random.h"
 #include "qemu/units.h"
 #include "qemu/module.h"
+#include "qemu/timer.h"
 #include "hw/misc/stm32l4r5_rng.h"
 #include "hw/qdev-properties.h"
 #include "trace.h"
@@ -38,9 +39,20 @@ static uint64_t stm32l4r5_rng_read(void *opaque, hwaddr offset, unsigned size)
         value = s->regs[RNG_SR];
         break;
     case RNG_DR:
-        if (s->regs[RNG_SR] & 0x1) {
+        if (s->data_read_cnt != 0)
+        {
             qemu_guest_getrandom(&value, sizeof(value), NULL);
-        } else {
+            s->data_read_cnt--;
+
+            if (s->data_read_cnt == 0)
+            {
+                // Reset DRDY bit.
+                s->regs[RNG_SR] &= 0xfffffffe;
+                timer_mod(s->timer, qemu_clock_get_ns(QEMU_CLOCK_VIRTUAL) + 1);
+            }
+        } 
+        else 
+        {
             qemu_log_mask(LOG_GUEST_ERROR, "%s: No valid data available in DR register.\n", __func__);
         }
         break;
@@ -54,6 +66,12 @@ static uint64_t stm32l4r5_rng_read(void *opaque, hwaddr offset, unsigned size)
     return value;
 }
 
+static void stm32_iwdg_reload(STM32IWDGState *s)
+{
+    uint64_t reload = 1;
+    timer_mod(s->timer, qemu_clock_get_ns(QEMU_CLOCK_VIRTUAL) + reload);
+}
+
 static void stm32l4r5_rng_write(void *opaque, hwaddr offset, uint64_t value,
                                unsigned size)
 {
@@ -63,12 +81,8 @@ static void stm32l4r5_rng_write(void *opaque, hwaddr offset, uint64_t value,
     case RNG_CR:
         s->regs[RNG_CR] = value;
 
-        // Set DRDY bit if the RNG is enabled.
-        if (value & 0x4) {
-            s->regs[RNG_SR] |= 0x1;
-        } else {
-            s->regs[RNG_SR] &= ~0x1;
-        }
+        timer_mod(s->timer, qemu_clock_get_ns(QEMU_CLOCK_VIRTUAL) + 1);
+        
         break;
     case RNG_SR:
         // Warn when bit 2 1 or 0 is set.
@@ -109,14 +123,31 @@ static const MemoryRegionOps stm32l4r5_rng_ops = {
     .valid.unaligned = false,
 };
 
+static void stm32l4r5_data_ready(void* opaque)
+{
+    STM32L4R5RNGState *s = opaque;
+
+    // Set DRDY bit when the data is ready.
+    s->regs[RNG_SR] |= 0x1;
+    s->data_read_cnt = 4;
+
+    // Raise interrupt if the IE flag is set.
+    if (value & 0x8) {
+        qemu_set_irq(s->irq, 1);
+    }
+}
+
 
 static void stm32l4r5_rng_realize(DeviceState *dev, Error **errp)
 {
     STM32L4R5RNGState *s = STM32L4R5_RNG(dev);
 
+    s->timer = timer_new_ns(QEMU_CLOCK_VIRTUAL, stm32l4r5_data_ready, s);
+
     memory_region_init_io(&s->iomem, OBJECT(s), &stm32l4r5_rng_ops, s,
                           TYPE_STM32L4R5_RNG, STM32L4R5_RNG_REGS_SIZE);
     sysbus_init_mmio(SYS_BUS_DEVICE(dev), &s->iomem);
+    sysbus_init_irq(SYS_BUS_DEVICE(dev), s->irq);
 }
 
 static void stm32l4r5_rng_class_init(ObjectClass *klass, void *data)
--- include/hw/misc/stm32l4r5_rng.h
+++ include/hw/misc/stm32l4r5_rng.h
@@ -14,8 +14,13 @@ OBJECT_DECLARE_TYPE(STM32L4R5RNGState, STM32L4R5RNGClass, STM32L4R5_RNG)
 struct STM32L4R5RNGState {
     SysBusDevice parent_obj;
 
+    QEMUTimer *timer;
+    uint8_t data_read_cnt = 0;
+
     MemoryRegion iomem;
     uint32_t regs[STM32L4R5_RNG_REGS_NUM];
+
+    qemu_irq irq;
 };
 
 
