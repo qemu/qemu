b278b60d517688b5f136be9d5f2f499eaf804002
Manos Pitsidianakis <manos.pitsidianakis@linaro.org>
2024-10-27 15:57:06 +0200

    rust/wrapper.h: define memory_order enum
    Add stub definition of memory_order enum in wrapper.h.
    
    Creating Rust bindings from C code is done by passing the wrapper.h
    header to `bindgen`. This fails when library dependencies that use
    compiler headers are enabled, and the libclang that bindgen detects does
    not match the expected clang version. So far this has only been observed
    with the memory_order enum symbols from stdatomic.h. If we add the enum
    definition to wrapper.h ourselves, the error does not happen.
    
    Before this commit, if the mismatch happened the following error could
    come up:
    
      /usr/include/liburing/barrier.h:72:10: error: use of undeclared identifier 'memory_order_release'
      /usr/include/liburing/barrier.h:75:9: error: use of undeclared identifier 'memory_order_acquire'
      /usr/include/liburing/barrier.h:75:9: error: use of undeclared identifier 'memory_order_acquire'
      /usr/include/liburing/barrier.h:68:9: error: use of undeclared identifier 'memory_order_relaxed'
      /usr/include/liburing/barrier.h:65:17: error: use of undeclared identifier 'memory_order_relaxed'
      /usr/include/liburing/barrier.h:75:9: error: use of undeclared identifier 'memory_order_acquire'
      /usr/include/liburing/barrier.h:75:9: error: use of undeclared identifier 'memory_order_acquire'
      /usr/include/liburing/barrier.h:72:10: error: use of undeclared identifier 'memory_order_release'
      panicked at [..]/.cargo/registry/src/index.crates.io-6f17d22bba15001f/bindgen-cli-0.70.1/main.rs:45:36:
      Unable to generate bindings
    
    To fix this (on my system) I would have to export CLANG_PATH and
    LIBCLANG_PATH:
    
      export CLANG_PATH=/bin/clang-17
      export LIBCLANG_PATH=/usr/lib/llvm-17/lib
    
    With these changes applied, bindgen is successful with both the
    environment variables set and unset.
    
    Since we're not using those symbols in the bindings (they are only used
    by dependencies) this does not affect the generated bindings in any way.
    
    Signed-off-by: Manos Pitsidianakis <manos.pitsidianakis@linaro.org>
    Link: https://lore.kernel.org/r/20241027-rust-wrapper-stdatomic-v2-1-dab27bbf93ea@linaro.org
    Signed-off-by: Paolo Bonzini <pbonzini@redhat.com>

--- rust/wrapper.h
+++ rust/wrapper.h
@@ -30,6 +30,23 @@
  * in order to generate C FFI compatible Rust bindings.
  */
 
+#ifndef __CLANG_STDATOMIC_H
+#define __CLANG_STDATOMIC_H
+/*
+ * Fix potential missing stdatomic.h error in case bindgen does not insert the
+ * correct libclang header paths on its own. We do not use stdatomic.h symbols
+ * in QEMU code, so it's fine to declare dummy types instead.
+ */
+typedef enum memory_order {
+  memory_order_relaxed,
+  memory_order_consume,
+  memory_order_acquire,
+  memory_order_release,
+  memory_order_acq_rel,
+  memory_order_seq_cst,
+} memory_order;
+#endif /* __CLANG_STDATOMIC_H */
+
 #include "qemu/osdep.h"
 #include "qemu/module.h"
 #include "qemu-io.h"
