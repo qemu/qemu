e30319cddd744055757999e45512898cdf2d190c
Jan Luebbe <jlu@pengutronix.de>
2024-10-28 17:23:08 +0100

    hw/sd/sdcard: Fix calculation of size when using eMMC boot partitions
    The sd_bootpart_offset() function calculates the *runtime* offset which
    changes as the guest switches between accessing the main user data area
    and the boot partitions by writing to the EXT_CSD_PART_CONFIG_ACC_MASK
    bits, so it shouldn't be used to calculate the main user data area size.
    
    Instead, subtract the boot_part_size directly (twice, as there are two
    identical boot partitions defined by the eMMC spec).
    
    Suggested-by: Cédric Le Goater <clg@kaod.org>
    Signed-off-by: Jan Luebbe <jlu@pengutronix.de>
    Fixes: c8cb19876d3e ("hw/sd/sdcard: Support boot area in emmc image")
    Tested-by: Guenter Roeck <linux@roeck-us.net>
    Reviewed-by: Cédric Le Goater <clg@redhat.com>
    (cherry picked from commit c078298301a8c72fe12da85d94372689196628bc)
    Signed-off-by: Michael Tokarev <mjt@tls.msk.ru>

--- hw/sd/sd.c
+++ hw/sd/sd.c
@@ -834,7 +834,9 @@ static void sd_reset(DeviceState *dev)
         sect = 0;
     }
     size = sect << HWBLOCK_SHIFT;
-    size -= sd_bootpart_offset(sd);
+    if (sd_is_emmc(sd)) {
+        size -= sd->boot_part_size * 2;
+    }
 
     sect = sd_addr_to_wpnum(size) + 1;
 
