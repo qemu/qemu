dbeafa453baec78a9f77580f83177f4fdfad6852
Zhao Liu <zhao1.liu@intel.com>
2024-10-29 16:59:24 +0800

    hw/scsi: Replace type_register() with type_register_static()
    Replace type_register() with type_register_static() because\
    type_register() will be deprecated.
    
    Signed-off-by: Zhao Liu <zhao1.liu@intel.com>
    Signed-off-by: Paolo Bonzini <pbonzini@redhat.com>
    Link: https://lore.kernel.org/r/20241029085934.2799066-7-zhao1.liu@intel.com

--- hw/scsi/mptsas.c
+++ hw/scsi/mptsas.c
@@ -1450,7 +1450,7 @@ static const TypeInfo mptsas_info = {
 
 static void mptsas_register_types(void)
 {
-    type_register(&mptsas_info);
+    type_register_static(&mptsas_info);
 }
 
 type_init(mptsas_register_types)
