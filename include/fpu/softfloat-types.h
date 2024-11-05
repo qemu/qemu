8a132968b2ce3f7078c8643dbc00e5c642eef0f6
Peter Maydell <peter.maydell@linaro.org>
2024-11-05 10:09:52 +0000

    softfloat: Allow 2-operand NaN propagation rule to be set at runtime
    IEEE 758 does not define a fixed rule for which NaN to pick as the
    result if both operands of a 2-operand operation are NaNs.  As a
    result different architectures have ended up with different rules for
    propagating NaNs.
    
    QEMU currently hardcodes the NaN propagation logic into the binary
    because pickNaN() has an ifdef ladder for different targets.  We want
    to make the propagation rule instead be selectable at runtime,
    because:
     * this will let us have multiple targets in one QEMU binary
     * the Arm FEAT_AFP architectural feature includes letting
       the guest select a NaN propagation rule at runtime
     * x86 specifies different propagation rules for x87 FPU ops
       and for SSE ops, and specifying the rule in the float_status
       would let us emulate this, instead of wrongly using the
       x87 rules everywhere
    
    In this commit we add an enum for the propagation rule, the field in
    float_status, and the corresponding getters and setters.  We change
    pickNaN to honour this, but because all targets still leave this
    field at its default 0 value, the fallback logic will pick the rule
    type with the old ifdef ladder.
    
    It's valid not to set a propagation rule if default_nan_mode is
    enabled, because in that case there's no need to pick a NaN; all the
    callers of pickNaN() catch this case and skip calling it.  So we can
    already assert that we don't get into the "no rule defined" codepath
    for our four targets which always set default_nan_mode: Hexagon,
    RiscV, SH4 and Tricore, and for the one target which does not have FP
    at all: avr.  These targets will not need to be updated to call
    set_float_2nan_prop_rule().
    
    Signed-off-by: Peter Maydell <peter.maydell@linaro.org>
    Reviewed-by: Richard Henderson <richard.henderson@linaro.org>
    Message-id: 20241025141254.2141506-2-peter.maydell@linaro.org

--- include/fpu/softfloat-types.h
+++ include/fpu/softfloat-types.h
@@ -171,6 +171,47 @@ typedef enum __attribute__((__packed__)) {
 } FloatX80RoundPrec;
 
 /*
+ * 2-input NaN propagation rule. Individual architectures have
+ * different rules for which input NaN is propagated to the output
+ * when there is more than one NaN on the input.
+ *
+ * If default_nan_mode is enabled then it is valid not to set a
+ * NaN propagation rule, because the softfloat code guarantees
+ * not to try to pick a NaN to propagate in default NaN mode.
+ *
+ * For transition, currently the 'none' rule will cause us to
+ * fall back to picking the propagation rule based on the existing
+ * ifdef ladder. When all targets are converted it will be an error
+ * not to set the rule in float_status unless in default_nan_mode,
+ * and we will assert if we need to handle an input NaN and no
+ * rule was selected.
+ */
+typedef enum __attribute__((__packed__)) {
+    /* No propagation rule specified */
+    float_2nan_prop_none = 0,
+    /* Prefer SNaN over QNaN, then operand A over B */
+    float_2nan_prop_s_ab,
+    /* Prefer SNaN over QNaN, then operand B over A */
+    float_2nan_prop_s_ba,
+    /* Prefer A over B regardless of SNaN vs QNaN */
+    float_2nan_prop_ab,
+    /* Prefer B over A regardless of SNaN vs QNaN */
+    float_2nan_prop_ba,
+    /*
+     * This implements x87 NaN propagation rules:
+     * SNaN + QNaN => return the QNaN
+     * two SNaNs => return the one with the larger significand, silenced
+     * two QNaNs => return the one with the larger significand
+     * SNaN and a non-NaN => return the SNaN, silenced
+     * QNaN and a non-NaN => return the QNaN
+     *
+     * If we get down to comparing significands and they are the same,
+     * return the NaN with the positive sign bit (if any).
+     */
+    float_2nan_prop_x87,
+} Float2NaNPropRule;
+
+/*
  * Floating Point Status. Individual architectures may maintain
  * several versions of float_status for different functions. The
  * correct status for the operation is then passed by reference to
@@ -181,6 +222,7 @@ typedef struct float_status {
     uint16_t float_exception_flags;
     FloatRoundMode float_rounding_mode;
     FloatX80RoundPrec floatx80_rounding_precision;
+    Float2NaNPropRule float_2nan_prop_rule;
     bool tininess_before_rounding;
     /* should denormalised results go to zero and set the inexact flag? */
     bool flush_to_zero;
