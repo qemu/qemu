6de3c4917fcfe46f4ae9621c78f768c37a428fda
Peter Xu <peterx@redhat.com>
2024-11-21 14:21:54 -0500

    ppc/e500: Avoid abuse of container_get()
    container_get() is going to become strict on not allowing to return a
    non-container.
    
    Switch the e500 user to use object_resolve_path_component() explicitly.
    
    Cc: Bharat Bhushan <r65777@freescale.com>
    Cc: qemu-ppc@nongnu.org
    Reviewed-by: Cédric Le Goater <clg@redhat.com>
    Reviewed-by: Daniel P. Berrangé <berrange@redhat.com>
    Signed-off-by: Peter Xu <peterx@redhat.com>
    Message-ID: <20241121192202.4155849-6-peterx@redhat.com>
    Signed-off-by: Philippe Mathieu-Daudé <philmd@linaro.org>

--- hw/pci-host/ppce500.c
+++ hw/pci-host/ppce500.c
@@ -418,8 +418,8 @@ static const VMStateDescription vmstate_ppce500_pci = {
 static void e500_pcihost_bridge_realize(PCIDevice *d, Error **errp)
 {
     PPCE500PCIBridgeState *b = PPC_E500_PCI_BRIDGE(d);
-    PPCE500CCSRState *ccsr = CCSR(container_get(qdev_get_machine(),
-                                  "/e500-ccsr"));
+    PPCE500CCSRState *ccsr = CCSR(
+        object_resolve_path_component(qdev_get_machine(), "e500-ccsr"));
 
     memory_region_init_alias(&b->bar0, OBJECT(ccsr), "e500-pci-bar0", &ccsr->ccsr_space,
                              0, int128_get64(ccsr->ccsr_space.size));
