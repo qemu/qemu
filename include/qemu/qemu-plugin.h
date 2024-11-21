cfa3a6c54511374e9ccee26d9c38ac1698fc7af2
Pierrick Bouvier <pierrick.bouvier@linaro.org>
2024-11-21 16:58:02 +0000

    plugins: add missing export for qemu_plugin_num_vcpus
    Fixes: 4a448b148ca ("plugins: add qemu_plugin_num_vcpus function")
    Reviewed-by: Philippe Mathieu-Daudé <philmd@linaro.org>
    Signed-off-by: Pierrick Bouvier <pierrick.bouvier@linaro.org>
    Message-Id: <20241112212622.3590693-2-pierrick.bouvier@linaro.org>
    Reviewed-by: Richard Henderson <richard.henderson@linaro.org>
    Signed-off-by: Alex Bennée <alex.bennee@linaro.org>
    Message-Id: <20241121165806.476008-36-alex.bennee@linaro.org>

--- include/qemu/qemu-plugin.h
+++ include/qemu/qemu-plugin.h
@@ -803,6 +803,7 @@ void qemu_plugin_register_atexit_cb(qemu_plugin_id_t id,
                                     qemu_plugin_udata_cb_t cb, void *userdata);
 
 /* returns how many vcpus were started at this point */
+QEMU_PLUGIN_API
 int qemu_plugin_num_vcpus(void);
 
 /**
