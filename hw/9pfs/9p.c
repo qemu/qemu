321d19fb057ee2a41945767eb5bba5d8101ef9b2
Christian Schoenebeck <qemu_oss@crudebyte.com>
2024-11-24 16:06:40 +0100

    9pfs: remove obsolete comment in v9fs_getattr()
    The comment claims that we'd only support basic Tgetattr fields. This is
    no longer true, so remove this comment.
    
    Fixes: e06a765efbe3 ("hw/9pfs: Add st_gen support in getattr reply")
    Signed-off-by: Christian Schoenebeck <qemu_oss@crudebyte.com>
    Reviewed-by: Greg Kurz <groug@kaod.org>
    Message-Id: <fb364d12045217a4c6ccd0dd6368103ddb80698b.1732465720.git.qemu_oss@crudebyte.com>
    (cherry picked from commit 3bc4db44430f53387d17145bb52b330a830a03fe)
    Signed-off-by: Michael Tokarev <mjt@tls.msk.ru>
    (Mjt: pick it to stable so the next commit applies cleanly)

--- hw/9pfs/9p.c
+++ hw/9pfs/9p.c
@@ -1596,10 +1596,6 @@ static void coroutine_fn v9fs_getattr(void *opaque)
         retval = -ENOENT;
         goto out_nofid;
     }
-    /*
-     * Currently we only support BASIC fields in stat, so there is no
-     * need to look at request_mask.
-     */
     retval = v9fs_co_lstat(pdu, &fidp->path, &stbuf);
     if (retval < 0) {
         goto out;
