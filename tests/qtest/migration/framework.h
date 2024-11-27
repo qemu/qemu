8a645544565c14cf1ac1b57ad2ef4f77a9446b7d
Fabiano Rosas <farosas@suse.de>
2024-11-27 15:28:59 -0300

    tests/qtest/migration: Split precopy tests
    Split the precopy tests from migration-test.c. This is the largest
    group of tests and the more difficult one to break into smaller
    groups, so move all of it.
    
    Reviewed-by: Peter Xu <peterx@redhat.com>
    Signed-off-by: Fabiano Rosas <farosas@suse.de>

--- tests/qtest/migration/framework.h
+++ tests/qtest/migration/framework.h
@@ -223,5 +223,6 @@ static inline void migration_test_add_tls(MigrationTestEnv *env) {};
 void migration_test_add_compression(MigrationTestEnv *env);
 void migration_test_add_postcopy(MigrationTestEnv *env);
 void migration_test_add_file(MigrationTestEnv *env);
+void migration_test_add_precopy(MigrationTestEnv *env);
 
 #endif /* TEST_FRAMEWORK_H */
