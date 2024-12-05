86131e0e657e523fe7624a2dcfac7fd0cffd6463
Luke Craig <lacraig3@gmail.com>
2024-12-05 10:25:10 -0500

    update build

--- build.sh
+++ build.sh
@@ -1,5 +1,6 @@
 PANDA_DIR_REL="$(dirname $0)"
+TARGETS="x86_64-softmmu,x86_64-linux-user,i386-softmmu,i386-linux-user,arm-softmmu,arm-linux-user,aarch64-softmmu,aarch64-linux-user,ppc-softmmu,ppc-linux-user,mips-softmmu,mips-linux-user,mipsel-softmmu,mipsel-linux-user,mips64-softmmu,mips64-linux-user,mips64el-softmmu,mips64el-linux-user"
 
-"${PANDA_DIR_REL}/configure" --enable-plugins
+"${PANDA_DIR_REL}/configure" --enable-plugins \
+    --target-list=$TARGETS
 ninja
-make -j`nproc` plugins
