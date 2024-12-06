3a25075ecef85adca5ab5b4e2486955b9d7a23ce
Philippe Mathieu-Daudé <philmd@linaro.org>
2024-12-06 18:10:20 +0100

    hw/nvram/fw_cfg: Remove fw_cfg_add_extra_pci_roots()
    Now that all uses of fw_cfg_add_extra_pci_roots() have been
    converted to the newer pci_bus_add_fw_cfg_extra_pci_roots(),
    we can remove that bogus method. hw/nvram/fw_cfg must
    stay generic. Device specific entries have to be implemented
    using TYPE_FW_CFG_DATA_GENERATOR_INTERFACE.
    
    This mostly reverts commit 0abd38885ac0fcdb08653922f339849cad387961
    ("fw_cfg: Refactor extra pci roots addition").
    
    Signed-off-by: Philippe Mathieu-Daudé <philmd@linaro.org>
    Reviewed-by: Daniel P. Berrangé <berrange@redhat.com>
    Message-Id: <20241206181352.6836-7-philmd@linaro.org>

--- include/hw/nvram/fw_cfg.h
+++ include/hw/nvram/fw_cfg.h
@@ -315,15 +315,6 @@ bool fw_cfg_add_file_from_generator(FWCfgState *s,
                                     Object *parent, const char *part,
                                     const char *filename, Error **errp);
 
-/**
- * fw_cfg_add_extra_pci_roots:
- * @bus: main pci root bus to be scanned from
- * @s: fw_cfg device being modified
- *
- * Add a new fw_cfg item...
- */
-void fw_cfg_add_extra_pci_roots(PCIBus *bus, FWCfgState *s);
-
 FWCfgState *fw_cfg_init_io_dma(uint32_t iobase, uint32_t dma_iobase,
                                 AddressSpace *dma_as);
 FWCfgState *fw_cfg_init_mem(hwaddr ctl_addr, hwaddr data_addr);
