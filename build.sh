78b82634eaba559146de31b1b60e77310eadce8f
Luke Craig <lacraig3@gmail.com>
2024-12-11 21:12:03 -0500

    build: drop softmmu

--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
 PANDA_DIR_REL="$(dirname $0)"
-TARGETS="x86_64-softmmu,x86_64-linux-user,i386-softmmu,i386-linux-user,arm-softmmu,arm-linux-user,aarch64-softmmu,aarch64-linux-user,ppc-softmmu,ppc-linux-user,mips-softmmu,mips-linux-user,mipsel-softmmu,mipsel-linux-user,mips64-softmmu,mips64-linux-user,mips64el-softmmu,mips64el-linux-user"
+TARGETS="x86_64-softmmu,i386-softmmu,arm-softmmu,aarch64-softmmu,ppc-softmmu,mips-softmmu,mipsel-softmmu,mips64-softmmu,mips64el-softmmu"
 
 "${PANDA_DIR_REL}/configure" --enable-plugins \
     --target-list=$TARGETS
