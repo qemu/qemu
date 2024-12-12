ad0642467ced43f2e2aa6c2467f2113fd632ba96
Luke Craig <lacraig3@gmail.com>
2024-12-11 22:05:15 -0500

    panda plugins: add cskeleton

---
+++ panda/plugins/cskeleton/cskeleton.c
@@ -0,0 +1,36 @@
+/* PANDABEGINCOMMENT
+ * 
+ * Authors:
+ *  Tim Leek               tleek@ll.mit.edu
+ *  Ryan Whelan            rwhelan@ll.mit.edu
+ *  Joshua Hodosh          josh.hodosh@ll.mit.edu
+ *  Michael Zhivich        mzhivich@ll.mit.edu
+ *  Brendan Dolan-Gavitt   brendandg@gatech.edu
+ * 
+ * This work is licensed under the terms of the GNU GPL, version 2. 
+ * See the COPYING file in the top-level directory. 
+ * 
+PANDAENDCOMMENT */
+// This needs to be defined before anything is included in order to get
+// the PRIx64 macro
+#define __STDC_FORMAT_MACROS
+
+#include "panda/plugin.h"
+
+bool init_plugin(void *);
+void uninit_plugin(void *);
+
+void before_block_exec(CPUState *env, TranslationBlock *tb);
+
+void before_block_exec(CPUState *env, TranslationBlock *tb) {
+    printf("before_block_exec\n");
+}
+
+bool init_plugin(void *self) {
+    panda_cb pcb = { .before_block_exec = before_block_exec };
+    panda_register_callback(self, PANDA_CB_START_BLOCK_EXEC, pcb);
+
+    return true;
+}
+
+void uninit_plugin(void *self) { }
