93dcc9390e5ad0696ae7e9b7b3a5b08c2d1b6de6
Han Han <hhan@redhat.com>
2024-12-19 16:51:38 +0800

    target/i386/cpu: Fix notes for CPU models
    Fixes: 644e3c5d812 ("missing vmx features for Skylake-Server and Cascadelake-Server")
    Signed-off-by: Han Han <hhan@redhat.com>
    Reviewed-by: Chenyi Qiang <chenyi.qiang@intel.com>
    Reviewed-by: Michael Tokarev <mjt@tls.msk.ru>
    Signed-off-by: Michael Tokarev <mjt@tls.msk.ru>

--- target/i386/cpu.c
+++ target/i386/cpu.c
@@ -3692,6 +3692,7 @@ static const X86CPUDefinition builtin_x86_defs[] = {
             },
             {
                 .version = 4,
+                .note = "IBRS, EPT switching, no TSX",
                 .props = (PropValue[]) {
                     { "vmx-eptp-switching", "on" },
                     { /* end of list */ }
@@ -3826,7 +3827,7 @@ static const X86CPUDefinition builtin_x86_defs[] = {
               },
             },
             { .version = 4,
-              .note = "ARCH_CAPABILITIES, no TSX",
+              .note = "ARCH_CAPABILITIES, EPT switching, no TSX",
               .props = (PropValue[]) {
                   { "vmx-eptp-switching", "on" },
                   { /* end of list */ }
