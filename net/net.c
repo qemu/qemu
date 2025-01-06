255a8c6e30fcde1f2e2a71c26f9390a228ff1f2c
Eugenio Pérez <eperezma@redhat.com>
2025-01-06 10:57:35 -0500

    net: move backend cleanup to NIC cleanup
    Commit a0d7215e33 ("vhost-vdpa: do not cleanup the vdpa/vhost-net
    structures if peer nic is present") effectively delayed the backend
    cleanup, allowing the frontend or the guest to access it resources as
    long as the frontend is still visible to the guest.
    
    However it does not clean up the resources until the qemu process is
    over.  This causes an effective leak if the device is deleted with
    device_del, as there is no way to close the vdpa device.  This makes
    impossible to re-add that device to this or other QEMU instances until
    the first instance of QEMU is finished.
    
    Move the cleanup from qemu_cleanup to the NIC deletion and to
    net_cleanup.
    
    Fixes: a0d7215e33 ("vhost-vdpa: do not cleanup the vdpa/vhost-net structures if peer nic is present")
    Reported-by: Lei Yang <leiyang@redhat.com>
    Signed-off-by: Eugenio Pérez <eperezma@redhat.com>
    Signed-off-by: Jonah Palmer <jonah.palmer@oracle.com>
    Signed-off-by: Jason Wang <jasowang@redhat.com>
    (cherry picked from commit e7891c575fb294618b172119a91c892b8f4384a2)
    Signed-off-by: Michael Tokarev <mjt@tls.msk.ru>

--- net/net.c
+++ net/net.c
@@ -422,7 +422,13 @@ void qemu_del_net_client(NetClientState *nc)
         object_unparent(OBJECT(nf));
     }
 
-    /* If there is a peer NIC, delete and cleanup client, but do not free. */
+    /*
+     * If there is a peer NIC, transfer ownership to it.  Delete the client
+     * from net_client list but do not cleanup nor free.  This way NIC can
+     * still access to members of the backend.
+     *
+     * The cleanup and free will be done when the NIC is free.
+     */
     if (nc->peer && nc->peer->info->type == NET_CLIENT_DRIVER_NIC) {
         NICState *nic = qemu_get_nic(nc->peer);
         if (nic->peer_deleted) {
@@ -432,14 +438,11 @@ void qemu_del_net_client(NetClientState *nc)
 
         for (i = 0; i < queues; i++) {
             ncs[i]->peer->link_down = true;
+            QTAILQ_REMOVE(&net_clients, ncs[i], next);
         }
 
         if (nc->peer->info->link_status_changed) {
             nc->peer->info->link_status_changed(nc->peer);
-        }
-
-        for (i = 0; i < queues; i++) {
-            qemu_cleanup_net_client(ncs[i], true);
         }
 
         return;
@@ -459,8 +462,12 @@ void qemu_del_nic(NICState *nic)
 
     for (i = 0; i < queues; i++) {
         NetClientState *nc = qemu_get_subqueue(nic, i);
-        /* If this is a peer NIC and peer has already been deleted, free it now. */
+        /*
+         * If this is a peer NIC and peer has already been deleted, clean it up
+         * and free it now.
+         */
         if (nic->peer_deleted) {
+            qemu_cleanup_net_client(nc->peer, false);
             qemu_free_net_client(nc->peer);
         } else if (nc->peer) {
             /* if there are RX packets pending, complete them */
@@ -1536,6 +1543,9 @@ void net_cleanup(void)
      * of the latest NET_CLIENT_DRIVER_NIC, and operate on *p as we walk
      * the list.
      *
+     * However, the NIC may have peers that trust to be clean beyond this
+     * point.  For example, if they have been removed with device_del.
+     *
      * The 'nc' variable isn't part of the list traversal; it's purely
      * for convenience as too much '(*p)->' has a tendency to make the
      * readers' eyes bleed.
@@ -1543,6 +1553,17 @@ void net_cleanup(void)
     while (*p) {
         nc = *p;
         if (nc->info->type == NET_CLIENT_DRIVER_NIC) {
+            NICState *nic = qemu_get_nic(nc);
+
+            if (nic->peer_deleted) {
+                int queues = MAX(nic->conf->peers.queues, 1);
+
+                for (int i = 0; i < queues; i++) {
+                    nc = qemu_get_subqueue(nic, i);
+                    qemu_cleanup_net_client(nc->peer, false);
+                }
+            }
+
             /* Skip NET_CLIENT_DRIVER_NIC entries */
             p = &QTAILQ_NEXT(nc, next);
         } else {
