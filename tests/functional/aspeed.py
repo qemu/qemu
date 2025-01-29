01050d97e1f342590f5f2db4a6d2b557a8244f44
Cédric Le Goater <clg@redhat.com>
2025-01-29 08:18:18 +0100

    tests/functional: Update OpenBMC image of romulus machine
    Use the new do_test_arm_aspeed_openbmc() routine to run the latest
    OpenBMC firmware build of the romulus BMC. Remove the older routine
    which is now unused.
    
    Reviewed-by: Thomas Huth <thuth@redhat.com>
    Link: https://lore.kernel.org/qemu-devel/20250129071820.1258133-4-clg@redhat.com
    Signed-off-by: Cédric Le Goater <clg@redhat.com>

--- tests/functional/aspeed.py
+++ tests/functional/aspeed.py
@@ -7,22 +7,6 @@ from qemu_test import LinuxKernelTest
 
 class AspeedTest(LinuxKernelTest):
 
-    def do_test_arm_aspeed(self, machine, image):
-        self.set_machine(machine)
-        self.vm.set_console()
-        self.vm.add_args('-drive', 'file=' + image + ',if=mtd,format=raw',
-                         '-net', 'nic', '-snapshot')
-        self.vm.launch()
-
-        self.wait_for_console_pattern("U-Boot 2016.07")
-        self.wait_for_console_pattern("## Loading kernel from FIT Image at 20080000")
-        self.wait_for_console_pattern("Starting kernel ...")
-        self.wait_for_console_pattern("Booting Linux on physical CPU 0x0")
-        self.wait_for_console_pattern(
-                "aspeed-smc 1e620000.spi: read control register: 203b0641")
-        self.wait_for_console_pattern("ftgmac100 1e660000.ethernet eth0: irq ")
-        self.wait_for_console_pattern("systemd[1]: Set hostname to")
-
     def do_test_arm_aspeed_openbmc(self, machine, image, uboot='2019.04',
                                    cpu_id='0x0', soc='AST2500 rev A1'):
         hostname = machine.removesuffix('-bmc')
