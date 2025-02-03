ff12e6a5ff904e68e222b0ba567938943e0f4b68
Eric Blake <eblake@redhat.com>
2025-02-03 16:26:07 -0600

    nbd/server: Allow users to adjust handshake limit in QMP
    Although defaulting the handshake limit to 10 seconds was a nice QoI
    change to weed out intentionally slow clients, it can interfere with
    integration testing done with manual NBD_OPT commands over 'nbdsh
    --opt-mode'.  Expose a QMP knob 'handshake-max-secs' to allow the user
    to alter the timeout away from the default.
    
    The parameter name here intentionally matches the spelling of the
    constant added in commit fb1c2aaa98, and not the command-line spelling
    added in the previous patch for qemu-nbd; that's because in QMP,
    longer names serve as good self-documentation, and unlike the command
    line, machines don't have problems generating longer spellings.
    
    Signed-off-by: Eric Blake <eblake@redhat.com>
    Message-ID: <20250203222722.650694-6-eblake@redhat.com>
    [eblake: s/max-secs/max-seconds/ in QMP]
    Acked-by: Markus Armbruster <armbru@redhat.com>
    Reviewed-by: Vladimir Sementsov-Ogievskiy <vsementsov@yandex-team.ru>

--- block/monitor/block-hmp-cmds.c
+++ block/monitor/block-hmp-cmds.c
@@ -402,8 +402,8 @@ void hmp_nbd_server_start(Monitor *mon, const QDict *qdict)
         goto exit;
     }
 
-    nbd_server_start(addr, NULL, NULL, NBD_DEFAULT_MAX_CONNECTIONS,
-                     &local_err);
+    nbd_server_start(addr, NBD_DEFAULT_HANDSHAKE_MAX_SECS, NULL, NULL,
+                     NBD_DEFAULT_MAX_CONNECTIONS, &local_err);
     qapi_free_SocketAddress(addr);
     if (local_err != NULL) {
         goto exit;
