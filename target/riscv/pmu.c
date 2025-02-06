abe9b81ee41b607eab1928f337837a19acae3208
Atish Patra <atishp@rivosinc.com>
2025-02-06 01:58:47 -0800

    target/riscv: Mask out upper sscofpmf bits during validation
    As per the ISA definition, the upper 8 bits in hpmevent are defined
    by Sscofpmf for privilege mode filtering and overflow bits while the
    lower 56 bits are desginated for platform specific hpmevent values.
    For the reset case, mhpmevent value should have zero in lower 56 bits.
    Software may set the OF bit to indicate disable interrupt.
    
    Ensure that correct value is checked after masking while clearing the
    event encodings.
    
    Reviewed-by: Daniel Henrique Barboza <dbarboza@ventanamicro.com>
    Acked-by: Alistair Francis <alistair.francis@wdc.com>
    Signed-off-by: Atish Patra <atishp@rivosinc.com>
    Message-ID: <20250206-pmu_minor_fixes-v2-2-1bb0f4aeb8b4@rivosinc.com>
    Signed-off-by: Alistair Francis <alistair.francis@wdc.com>

--- target/riscv/pmu.c
+++ target/riscv/pmu.c
@@ -390,7 +390,7 @@ int riscv_pmu_update_event_map(CPURISCVState *env, uint64_t value,
      * Expected mhpmevent value is zero for reset case. Remove the current
      * mapping.
      */
-    if (!value) {
+    if (!(value & MHPMEVENT_IDX_MASK)) {
         g_hash_table_foreach_remove(cpu->pmu_event_ctr_map,
                                     pmu_remove_event_map,
                                     GUINT_TO_POINTER(ctr_idx));
