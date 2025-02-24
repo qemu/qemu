c5d4173fcf04f6de9b9bb0959d1fdfc08254381a
Peter Maydell <peter.maydell@linaro.org>
2025-02-24 11:15:23 +0000

    fpu: Don't compile-time disable hardfloat for PPC targets
    We happen to know that for the PPC target the FP status flags (and in
    particular float_flag_inexact) will always be cleared before a
    floating point operation, and so can_use_fpu() will always return
    false.  So we speed things up a little by forcing QEMU_NO_HARDFLOAT
    to true on that target.
    
    We would like to build softfloat once for all targets; that means
    removing target-specific ifdefs.  Remove the check for TARGET_PPC;
    this won't change behaviour because can_use_fpu() will see that
    float_flag_inexact is clear and take the softfloat path anyway.
    
    Signed-off-by: Peter Maydell <peter.maydell@linaro.org>
    Reviewed-by: Richard Henderson <richard.henderson@linaro.org>
    Message-id: 20250224111524.1101196-12-peter.maydell@linaro.org
    Message-id: 20250217125055.160887-10-peter.maydell@linaro.org

--- fpu/softfloat.c
+++ fpu/softfloat.c
@@ -220,11 +220,9 @@ GEN_INPUT_FLUSH3(float64_input_flush3, float64)
  * the use of hardfloat, since hardfloat relies on the inexact flag being
  * already set.
  */
-#if defined(TARGET_PPC) || defined(__FAST_MATH__)
 # if defined(__FAST_MATH__)
 #  warning disabling hardfloat due to -ffast-math: hardfloat requires an exact \
     IEEE implementation
-# endif
 # define QEMU_NO_HARDFLOAT 1
 # define QEMU_SOFTFLOAT_ATTR QEMU_FLATTEN
 #else
