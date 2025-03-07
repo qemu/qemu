8241d6f8d5b7a423dd4289b8140934d96819d019
Jamin Lin <jamin_lin@aspeedtech.com>
2025-03-07 11:59:35 +0800

    tests/functional/aspeed: Update temperature hwmon path
    Modified the temperature hwmon path to use a wildcard to handle different SDK
    versions: "cat /sys/bus/i2c/devices/1-004d/hwmon/hwmon*/temp1_input".
    
    Signed-off-by: Jamin Lin <jamin_lin@aspeedtech.com>
    Reviewed-by: Cédric Le Goater <clg@redhat.com>
    Link: https://lore.kernel.org/qemu-devel/20250307035945.3698802-27-jamin_lin@aspeedtech.com
    Signed-off-by: Cédric Le Goater <clg@redhat.com>

--- tests/functional/test_aarch64_aspeed.py
+++ tests/functional/test_aarch64_aspeed.py
@@ -83,11 +83,11 @@ class AST2x00MachineSDK(QemuSystemTest):
             'echo lm75 0x4d > /sys/class/i2c-dev/i2c-1/device/new_device ',
             'i2c i2c-1: new_device: Instantiated device lm75 at 0x4d');
         exec_command_and_wait_for_pattern(self,
-            'cat /sys/class/hwmon/hwmon20/temp1_input', '0')
+            'cat /sys/bus/i2c/devices/1-004d/hwmon/hwmon*/temp1_input', '0')
         self.vm.cmd('qom-set', path='/machine/peripheral/tmp-test',
                     property='temperature', value=18000)
         exec_command_and_wait_for_pattern(self,
-            'cat /sys/class/hwmon/hwmon20/temp1_input', '18000')
+            'cat /sys/bus/i2c/devices/1-004d/hwmon/hwmon*/temp1_input', '18000')
 
     def test_aarch64_ast2700_evb_sdk_v09_03(self):
         self.set_machine('ast2700-evb')
