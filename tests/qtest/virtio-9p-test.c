610dc187e52605c8ea8d14c5e7d8e7384f8af290
Greg Kurz <groug@kaod.org>
2025-03-12 16:29:32 +0100

    tests/9p: Test `Tsetattr` can truncate unlinked file
    Enhance the `use-after-unlink` test with a new check for the
    case where the client wants to alter the size of an unlinked
    file for which it still has an active fid.
    
    Suggested-by: Christian Schoenebeck <qemu_oss@crudebyte.com>
    Signed-off-by: Greg Kurz <groug@kaod.org>
    Reviewed-by: Christian Schoenebeck <qemu_oss@crudebyte.com>
    Message-Id: <20250312152933.383967-7-groug@kaod.org>
    Signed-off-by: Christian Schoenebeck <qemu_oss@crudebyte.com>

--- tests/qtest/virtio-9p-test.c
+++ tests/qtest/virtio-9p-test.c
@@ -736,6 +736,20 @@ static void fs_use_after_unlink(void *obj, void *data,
         .data = buf
     }).count;
     g_assert_cmpint(count, ==, write_count);
+
+    /* truncate file to (arbitrarily chosen) size 2001 */
+    tsetattr({
+        .client = v9p, .fid = fid_file, .attr = (v9fs_attr) {
+            .valid = P9_SETATTR_SIZE,
+            .size = 2001
+        }
+     });
+    /* truncate apparently succeeded, let's double-check the size */
+    tgetattr({
+        .client = v9p, .fid = fid_file, .request_mask = P9_GETATTR_BASIC,
+        .rgetattr.attr = &attr
+    });
+    g_assert_cmpint(attr.size, ==, 2001);
 }
 
 static void cleanup_9p_local_driver(void *data)
