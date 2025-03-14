3a031e395dc65239d031890d038bc354af61dc35
Akihiko Odaki <akihiko.odaki@daynix.com>
2025-03-14 15:14:50 +0900

    hw/pci: Do not add ROM BAR for SR-IOV VF
    A SR-IOV VF cannot have a ROM BAR.
    
    Co-developed-by: Yui Washizu <yui.washidu@gmail.com>
    Signed-off-by: Akihiko Odaki <akihiko.odaki@daynix.com>
    Message-Id: <20250314-sriov-v9-1-57dae8ae3ab5@daynix.com>
    Tested-by: Yui Washizu <yui.washidu@gmail.com>
    Tested-by: Pasha Tatashin <pasha.tatashin@soleen.com>
    Reviewed-by: Michael S. Tsirkin <mst@redhat.com>
    Signed-off-by: Michael S. Tsirkin <mst@redhat.com>

--- hw/pci/pci.c
+++ hw/pci/pci.c
@@ -2522,6 +2522,14 @@ static void pci_add_option_rom(PCIDevice *pdev, bool is_default_rom,
         return;
     }
 
+    if (pci_is_vf(pdev)) {
+        if (pdev->rom_bar > 0) {
+            error_setg(errp, "ROM BAR cannot be enabled for SR-IOV VF");
+        }
+
+        return;
+    }
+
     if (load_file || pdev->romsize == UINT32_MAX) {
         path = qemu_find_file(QEMU_FILE_TYPE_BIOS, pdev->romfile);
         if (path == NULL) {
