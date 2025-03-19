4c1a39eebc6a9f1db816ff6c23e3d42ba52e2601
Sebastian Huber <sebastian.huber@embedded-brains.de>
2025-03-19 07:13:33 +0100

    hw/misc: Add MPFS system reset support
    Signed-off-by: Sebastian Huber <sebastian.huber@embedded-brains.de>
    Acked-by: Alistair Francis <alistair.francis@wdc.com>
    Message-ID: <20250319061342.26435-2-sebastian.huber@embedded-brains.de>
    Signed-off-by: Alistair Francis <alistair.francis@wdc.com>

--- hw/misc/mchp_pfsoc_sysreg.c
+++ hw/misc/mchp_pfsoc_sysreg.c
@@ -27,7 +27,9 @@
 #include "hw/irq.h"
 #include "hw/sysbus.h"
 #include "hw/misc/mchp_pfsoc_sysreg.h"
+#include "system/runstate.h"
 
+#define MSS_RESET_CR    0x18
 #define ENVM_CR         0xb8
 #define MESSAGE_INT     0x118c
 
@@ -56,6 +58,11 @@ static void mchp_pfsoc_sysreg_write(void *opaque, hwaddr offset,
 {
     MchpPfSoCSysregState *s = opaque;
     switch (offset) {
+    case MSS_RESET_CR:
+        if (value == 0xdead) {
+            qemu_system_reset_request(SHUTDOWN_CAUSE_GUEST_RESET);
+        }
+        break;
     case MESSAGE_INT:
         qemu_irq_lower(s->irq);
         break;
