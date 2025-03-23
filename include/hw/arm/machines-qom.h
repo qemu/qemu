5de9216064d32eaac125dac4de1878486c16d73e
Philippe Mathieu-Daudé <philmd@linaro.org>
2025-03-23 22:50:46 +0100

    hw/arm: Register TYPE_TARGET_ARM/AARCH64_MACHINE QOM interfaces
    Define the TYPE_TARGET_ARM_MACHINE and TYPE_TARGET_AARCH64_MACHINE
    QOM interface names to allow machines to implement them.
    
    Register these interfaces in common code in target_info-qom.c used
    by all binaries because QOM interfaces must be registered before
    being checked (see next commit with the 'none' machine).
    
    Signed-off-by: Philippe Mathieu-Daudé <philmd@linaro.org>
    Reviewed-by: Pierrick Bouvier <pierrick.bouvier@linaro.org>
    Reviewed-by: Richard Henderson <richard.henderson@linaro.org>
    Acked-by: Peter Maydell <peter.maydell@linaro.org>
    Message-Id: <20251021205741.57109-2-philmd@linaro.org>

---
+++ include/hw/arm/machines-qom.h
@@ -0,0 +1,18 @@
+/*
+ * QOM type definitions for ARM / Aarch64 machines
+ *
+ *  Copyright (c) Linaro
+ *
+ * SPDX-License-Identifier: GPL-2.0-or-later
+ */
+
+#ifndef HW_ARM_MACHINES_QOM_H
+#define HW_ARM_MACHINES_QOM_H
+
+#define TYPE_TARGET_ARM_MACHINE \
+        "target-info-arm-machine"
+
+#define TYPE_TARGET_AARCH64_MACHINE \
+        "target-info-aarch64-machine"
+
+#endif
