eba24b60a72115e21e850977b3019aaf037c66c9
Richard Henderson <richard.henderson@linaro.org>
2025-03-25 15:32:50 -0700

    target/avr: Increase TARGET_PAGE_BITS to 10
    Now that we can handle the MCU allocating only a portion of the
    first page to i/o, increase the page size.  Choose 10 as larger
    than the i/o on every MCU, just so that this path is tested.
    
    Reviewed-by: Pierrick Bouvier <pierrick.bouvier@linaro.org>
    Signed-off-by: Richard Henderson <richard.henderson@linaro.org>

--- target/avr/cpu-param.h
+++ target/avr/cpu-param.h
@@ -21,13 +21,7 @@
 #ifndef AVR_CPU_PARAM_H
 #define AVR_CPU_PARAM_H
 
-/*
- * TARGET_PAGE_BITS cannot be more than 8 bits because
- * 1.  all IO registers occupy [0x0000 .. 0x00ff] address range, and they
- *     should be implemented as a device and not memory
- * 2.  SRAM starts at the address 0x0100
- */
-#define TARGET_PAGE_BITS 8
+#define TARGET_PAGE_BITS 10
 #define TARGET_PHYS_ADDR_SPACE_BITS 24
 #define TARGET_VIRT_ADDR_SPACE_BITS 24
 
