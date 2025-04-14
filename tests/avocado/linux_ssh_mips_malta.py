902fc01dc232b9f32aff26bec25f4ea4a61ecb37
Thomas Huth <thuth@redhat.com>
2025-04-14 13:30:21 +0200

    tests/functional: Convert the 32-bit little endian Wheezy mips test
    Reuse the test function from the big endian test to easily
    convert the 32-bit little endian Wheezy mips test.
    
    Reviewed-by: Daniel P. Berrangé <berrange@redhat.com>
    Message-ID: <20250414113031.151105-8-thuth@redhat.com>
    Signed-off-by: Thomas Huth <thuth@redhat.com>
    (cherry picked from commit 689a8b56a6f3d16458004006b604950295da87df)
    Signed-off-by: Michael Tokarev <mjt@tls.msk.ru>

--- tests/avocado/linux_ssh_mips_malta.py
+++ tests/avocado/linux_ssh_mips_malta.py
@@ -172,14 +172,6 @@ class LinuxSSH(QemuSystemTest, LinuxSSHMixIn):
         # Wait for VM to shut down gracefully
         self.vm.wait()
 
-    def test_mips_malta32el_kernel3_2_0(self):
-        """
-        :avocado: tags=arch:mipsel
-        :avocado: tags=endian:little
-        :avocado: tags=device:pcnet32
-        """
-        self.check_mips_malta('mips', 'le')
-
     def test_mips_malta64eb_kernel3_2_0(self):
         """
         :avocado: tags=arch:mips64
