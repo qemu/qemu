ad63158bdb33dab5704ea1cf740d2ea0387175df
Ziqiao Kong <ziqiaokong@gmail.com>
2025-04-15 16:02:54 +0800

    target/riscv: fix endless translation loop on big endian systems
    On big endian systems, pte and updated_pte hold big endian host data
    while pte_pa points to little endian target data. This means the branch
    at cpu_helper.c:1669 will be always satisfied and restart translation,
    causing an endless translation loop.
    
    The correctness of this patch can be deduced by:
    
    old_pte will hold value either from cpu_to_le32/64(pte) or
    cpu_to_le32/64(updated_pte), both of wich is litte endian. After that,
    an in-place conversion by le32/64_to_cpu(old_pte) ensures that old_pte
    now is in native endian, same with pte. Therefore, the endianness of the
    both side of if (old_pte != pte) is correct.
    
    Signed-off-by: Ziqiao Kong <ziqiaokong@gmail.com>
    Reviewed-by: Alistair Francis <alistair.francis@wdc.com>
    Reviewed-by: Richard Henderson <richard.henderson@linaro.org>
    Message-ID: <20250415080254.3667878-2-ziqiaokong@gmail.com>
    Signed-off-by: Alistair Francis <alistair.francis@wdc.com>
    Cc: qemu-stable@nongnu.org

--- target/riscv/cpu_helper.c
+++ target/riscv/cpu_helper.c
@@ -1566,9 +1566,11 @@ static int get_physical_address(CPURISCVState *env, hwaddr *physical,
             target_ulong *pte_pa = qemu_map_ram_ptr(mr->ram_block, addr1);
             target_ulong old_pte;
             if (riscv_cpu_sxl(env) == MXL_RV32) {
-                old_pte = qatomic_cmpxchg((uint32_t *)pte_pa, pte, updated_pte);
+                old_pte = qatomic_cmpxchg((uint32_t *)pte_pa, cpu_to_le32(pte), cpu_to_le32(updated_pte));
+                old_pte = le32_to_cpu(old_pte);
             } else {
-                old_pte = qatomic_cmpxchg(pte_pa, pte, updated_pte);
+                old_pte = qatomic_cmpxchg(pte_pa, cpu_to_le64(pte), cpu_to_le64(updated_pte));
+                old_pte = le64_to_cpu(old_pte);
             }
             if (old_pte != pte) {
                 goto restart;
