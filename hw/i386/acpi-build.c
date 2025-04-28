43edad68ca5bdbfb4164b46624f3b5ceb2395772
Eric Auger <eric.auger@redhat.com>
2025-04-28 12:25:29 +0200

    hw/i386/acpi-build: Fix build_append_notfication_callback typo
    Rename build_append_notfication_callback into
    build_append_notification_callback
    
    Signed-off-by: Eric Auger <eric.auger@redhat.com>
    Reviewed-by: Gustavo Romero <gustavo.romero@linaro.org>
    Message-ID: <20250428102628.378046-4-eric.auger@redhat.com>
    Signed-off-by: Philippe Mathieu-Daudé <philmd@linaro.org>

--- hw/i386/acpi-build.c
+++ hw/i386/acpi-build.c
@@ -589,8 +589,8 @@ void build_append_pci_bus_devices(Aml *parent_scope, PCIBus *bus)
     }
 }
 
-static bool build_append_notfication_callback(Aml *parent_scope,
-                                              const PCIBus *bus)
+static bool build_append_notification_callback(Aml *parent_scope,
+                                               const PCIBus *bus)
 {
     Aml *method;
     PCIBus *sec;
@@ -604,7 +604,7 @@ static bool build_append_notfication_callback(Aml *parent_scope,
             continue;
         }
         nr_notifiers = nr_notifiers +
-                       build_append_notfication_callback(br_scope, sec);
+                       build_append_notification_callback(br_scope, sec);
         /*
          * add new child scope to parent
          * and keep track of bus that have PCNT,
@@ -1773,7 +1773,7 @@ build_dsdt(GArray *table_data, BIOSLinker *linker,
         PCIBus *b = PCI_HOST_BRIDGE(pci_host)->bus;
 
         scope = aml_scope("\\_SB.PCI0");
-        has_pcnt = build_append_notfication_callback(scope, b);
+        has_pcnt = build_append_notification_callback(scope, b);
         if (has_pcnt) {
             aml_append(dsdt, scope);
         }
