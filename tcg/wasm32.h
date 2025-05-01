29a4d456d7e7c3e26c1386db639f571263ad5a86
Kohei Tokunaga <ktokunaga.mail@gmail.com>
2025-05-01 17:34:58 +0900

    tcg/wasm32: Write wasm binary to TB
    This commit updates tcg_out_tb_start and tcg_out_tb_end to emit Wasm
    binaries into the TB code buffer. The generated Wasm binary defines a
    function of type wasm_tb_func which takes a wasmContext, executes the TB,
    and returns a result. In the Wasm backend, each TB starts with a
    wasmTBHeader, followed by the following data:
    
    - TCI code
    - Wasm code
    - Array of function indices imported into the Wasm instance
    
    The wasmTBHeader contains pointers to each of these elements.
    
    tcg_out_tb_start writes the wasmTBHeader to the code buffer. tcg_out_tb_end
    generates the full Wasm executable binary by creating the Wasm module header
    following the spec[1][2] and copying the Wasm code body from sub_buf to the
    code buffer. Wasm binary is placed after the TCI code which was emitted
    earlier.
    
    Additionally, an array of imported function pointers is appended to the TB.
    They are used during Wasm module instantiation. Function are imported to
    Wasm with names like "helper.0", "helper.1", etc., where the number
    corresponds to the assigned function IDs.
    
    Each function's type signature must also be encoded in the Wasm module header.
    To support this, each call, qemu_ld and qemu_st operation records the target
    function's type information to a buffer.
    
    Memory is shared between QEMU and the TBs and is imported to the Wasm module
    with the name "env.buffer".
    
    [1] https://webassembly.github.io/spec/core/binary/modules.html
    [2] https://github.com/WebAssembly/threads/blob/b2567bff61ee6fbe731934f0ed17a5d48dc9ab01/proposals/threads/Overview.md
    
    Signed-off-by: Kohei Tokunaga <ktokunaga.mail@gmail.com>

--- tcg/wasm32.h
+++ tcg/wasm32.h
@@ -34,4 +34,30 @@ struct wasmContext {
     uint64_t *stack;
 };
 
+/* Instantiated Wasm function of a TB */
+typedef int32_t (*wasm_tb_func)(struct wasmContext *);
+
+/*
+ * TB of wasm backend starts from a header which stores pointers for each data
+ * stored in the following region in the TB.
+ */
+struct wasmTBHeader {
+    /*
+     * Pointer to the region containing TCI instructions.
+     */
+    void *tci_ptr;
+
+    /*
+     * Pointer to the region containing Wasm instructions.
+     */
+    void *wasm_ptr;
+    int wasm_size;
+
+    /*
+     * Pointer to the array containing imported function pointers.
+     */
+    void *import_ptr;
+    int import_size;
+};
+
 #endif
