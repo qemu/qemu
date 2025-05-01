711de639d571b6ac6981020536ff816130768f58
Philippe Mathieu-Daudé <philmd@linaro.org>
2025-05-02 01:01:28 +0200

    hw/display/vga-pci: Do not expose the 'global-vmstate' property
    The "global-vmstate" property is 'false' by default, and was only
    set to 'true' in the hw_compat_2_12[] array. We removed all machines
    using that array. Stop exposing that property on the PCI devices.
    
    Signed-off-by: Philippe Mathieu-Daudé <philmd@linaro.org>
    Message-ID: <20250501230129.2596-11-philmd@linaro.org>
    Reviewed-by: Thomas Huth <thuth@redhat.com>
    Signed-off-by: Thomas Huth <thuth@redhat.com>

--- hw/display/vmware_vga.c
+++ hw/display/vmware_vga.c
@@ -1335,8 +1335,6 @@ static void pci_vmsvga_realize(PCIDevice *dev, Error **errp)
 static const Property vga_vmware_properties[] = {
     DEFINE_PROP_UINT32("vgamem_mb", struct pci_vmsvga_state_s,
                        chip.vga.vram_size_mb, 16),
-    DEFINE_PROP_BOOL("global-vmstate", struct pci_vmsvga_state_s,
-                     chip.vga.global_vmstate, false),
 };
 
 static void vmsvga_class_init(ObjectClass *klass, const void *data)
