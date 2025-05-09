8efe15927b9692bf7202c20522c9c93267232e0d
Fabiano Rosas <farosas@suse.de>
2025-05-09 14:49:38 -0300

    s390x: Fix leak in machine_set_loadparm
    ASAN spotted a leaking string in machine_set_loadparm():
    
    Direct leak of 9 byte(s) in 1 object(s) allocated from:
        #0 0x560ffb5bb379 in malloc ../projects/compiler-rt/lib/asan/asan_malloc_linux.cpp:69:3
        #1 0x7f1aca926518 in g_malloc ../glib/gmem.c:106
        #2 0x7f1aca94113e in g_strdup ../glib/gstrfuncs.c:364
        #3 0x560ffc8afbf9 in qobject_input_type_str ../qapi/qobject-input-visitor.c:542:12
        #4 0x560ffc8a80ff in visit_type_str ../qapi/qapi-visit-core.c:349:10
        #5 0x560ffbe6053a in machine_set_loadparm ../hw/s390x/s390-virtio-ccw.c:802:10
        #6 0x560ffc0c5e52 in object_property_set ../qom/object.c:1450:5
        #7 0x560ffc0d4175 in object_property_set_qobject ../qom/qom-qobject.c:28:10
        #8 0x560ffc0c6004 in object_property_set_str ../qom/object.c:1458:15
        #9 0x560ffbe2ae60 in update_machine_ipl_properties ../hw/s390x/ipl.c:569:9
        #10 0x560ffbe2aa65 in s390_ipl_update_diag308 ../hw/s390x/ipl.c:594:5
        #11 0x560ffbdee132 in handle_diag_308 ../target/s390x/diag.c:147:9
        #12 0x560ffbebb956 in helper_diag ../target/s390x/tcg/misc_helper.c:137:9
        #13 0x7f1a3c51c730  (/memfd:tcg-jit (deleted)+0x39730)
    
    Cc: qemu-stable@nongnu.org
    Signed-off-by: Fabiano Rosas <farosas@suse.de>
    Message-ID: <20250509174938.25935-1-farosas@suse.de>
    Fixes: 1fd396e3228 ("s390x: Register TYPE_S390_CCW_MACHINE properties as class properties")
    Reviewed-by: Thomas Huth <thuth@redhat.com>
    Reviewed-by: Philippe Mathieu-Daudé <philmd@linaro.org>
    Signed-off-by: Thomas Huth <thuth@redhat.com>
    (cherry picked from commit bdf12f2a56bf3f13c52eb51f0a994bbfe40706b2)
    Signed-off-by: Michael Tokarev <mjt@tls.msk.ru>

--- hw/s390x/s390-virtio-ccw.c
+++ hw/s390x/s390-virtio-ccw.c
@@ -802,6 +802,7 @@ static void machine_set_loadparm(Object *obj, Visitor *v,
     }
 
     s390_ipl_fmt_loadparm(ms->loadparm, val, errp);
+    g_free(val);
 }
 
 static void ccw_machine_class_init(ObjectClass *oc, void *data)
