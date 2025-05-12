f9fba6ddfc854f427e00d62c1e4888ce9a1a7c34
Pierrick Bouvier <pierrick.bouvier@linaro.org>
2025-05-12 11:04:30 -0700

    target/arm/debug_helper: only include common helpers
    Avoid pulling helper.h which contains TARGET_AARCH64.
    
    Reviewed-by: Richard Henderson <richard.henderson@linaro.org>
    Signed-off-by: Pierrick Bouvier <pierrick.bouvier@linaro.org>
    Message-id: 20250512180502.2395029-17-pierrick.bouvier@linaro.org
    Signed-off-by: Peter Maydell <peter.maydell@linaro.org>

--- target/arm/debug_helper.c
+++ target/arm/debug_helper.c
@@ -11,9 +11,11 @@
 #include "internals.h"
 #include "cpu-features.h"
 #include "cpregs.h"
-#include "exec/helper-proto.h"
 #include "exec/watchpoint.h"
 #include "system/tcg.h"
+
+#define HELPER_H "tcg/helper.h"
+#include "exec/helper-proto.h.inc"
 
 #ifdef CONFIG_TCG
 /* Return the Exception Level targeted by debug exceptions. */
