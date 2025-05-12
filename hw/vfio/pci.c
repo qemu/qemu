a1330277308e492180c2d1f64bb49d4987adfde1
Steve Sistare <steven.sistare@oracle.com>
2025-05-12 08:32:26 -0700

    vfio-pci: skip reset during cpr
    Do not reset a vfio-pci device during CPR, and do not complain if the
    kernel's PCI config space changes for non-emulated bits between the
    vmstate save and load, which can happen due to ongoing interrupt activity.
    
    Signed-off-by: Steve Sistare <steven.sistare@oracle.com>
    Link: https://lore.kernel.org/qemu-devel/1747063973-124548-16-git-send-email-steven.sistare@oracle.com
    Signed-off-by: Cédric Le Goater <clg@redhat.com>

--- hw/vfio/pci.c
+++ hw/vfio/pci.c
@@ -3344,6 +3344,11 @@ static void vfio_pci_reset(DeviceState *dev)
 {
     VFIOPCIDevice *vdev = VFIO_PCI_BASE(dev);
 
+    /* Do not reset the device during qemu_system_reset prior to cpr load */
+    if (vdev->vbasedev.cpr.reused) {
+        return;
+    }
+
     trace_vfio_pci_reset(vdev->vbasedev.name);
 
     vfio_pci_pre_reset(vdev);
@@ -3513,6 +3518,7 @@ static void vfio_pci_dev_class_init(ObjectClass *klass, const void *data)
 #ifdef CONFIG_IOMMUFD
     object_class_property_add_str(klass, "fd", NULL, vfio_pci_set_fd);
 #endif
+    dc->vmsd = &vfio_cpr_pci_vmstate;
     dc->desc = "VFIO-based PCI device assignment";
     pdc->realize = vfio_realize;
 
