6ad3a47f15624176434490e895ac244375322607
Thomas Huth <thuth@redhat.com>
2025-05-19 07:47:44 +0200

    hw/s390x/s390-virtio-ccw: Remove the deprecated 4.1 machine type
    With the upcoming release of QEMU 10.1, the s390-ccw-virtio-4.1 machine
    will be older than 6 years, so according to our machine support policy,
    it can be removed now. The V4_1 CPU feature group gets merged into the
    minimum CPU feature group now.
    
    Signed-off-by: Thomas Huth <thuth@redhat.com>
    Message-ID: <20250519054744.36715-1-thuth@redhat.com>

--- hw/s390x/s390-virtio-ccw.c
+++ hw/s390x/s390-virtio-ccw.c
@@ -1145,20 +1145,6 @@ static void ccw_machine_4_2_class_options(MachineClass *mc)
 }
 DEFINE_CCW_MACHINE(4, 2);
 
-static void ccw_machine_4_1_instance_options(MachineState *machine)
-{
-    static const S390FeatInit qemu_cpu_feat = { S390_FEAT_LIST_QEMU_V4_1 };
-    ccw_machine_4_2_instance_options(machine);
-    s390_set_qemu_cpu_model(0x2964, 13, 2, qemu_cpu_feat);
-}
-
-static void ccw_machine_4_1_class_options(MachineClass *mc)
-{
-    ccw_machine_4_2_class_options(mc);
-    compat_props_add(mc->compat_props, hw_compat_4_1, hw_compat_4_1_len);
-}
-DEFINE_CCW_MACHINE(4, 1);
-
 static void ccw_machine_register_types(void)
 {
     type_register_static(&ccw_machine_info);
