a400720365ea86602044b78dd8654911d0fe3977
Eugenio Pérez <eperezma@redhat.com>
2025-05-22 10:58:38 -0400

    vdpa: move iova_tree allocation to net_vhost_vdpa_init
    As we are moving to keep the mapping through all the vdpa device life
    instead of resetting it at VirtIO reset, we need to move all its
    dependencies to the initialization too.  In particular devices with
    x-svq=on need a valid iova_tree from the beginning.
    
    Simplify the code also consolidating the two creation points: the first
    data vq in case of SVQ active and CVQ start in case only CVQ uses it.
    
    Tested-by: Lei Yang <leiyang@redhat.com>
    Reviewed-by: Si-Wei Liu <si-wei.liu@oracle.com>
    Acked-by: Jason Wang <jasowang@redhat.com>
    Suggested-by: Si-Wei Liu <si-wei.liu@oracle.com>
    Signed-off-by: Eugenio Pérez <eperezma@redhat.com>
    Signed-off-by: Jonah Palmer <jonah.palmer@oracle.com>
    Message-Id: <20250522145839.59974-7-jonah.palmer@oracle.com>
    Reviewed-by: Michael S. Tsirkin <mst@redhat.com>
    Signed-off-by: Michael S. Tsirkin <mst@redhat.com>

--- include/hw/virtio/vhost-vdpa.h
+++ include/hw/virtio/vhost-vdpa.h
@@ -43,7 +43,21 @@ typedef struct vhost_vdpa_shared {
     struct vhost_vdpa_iova_range iova_range;
     QLIST_HEAD(, vdpa_iommu) iommu_list;
 
-    /* IOVA mapping used by the Shadow Virtqueue */
+    /*
+     * IOVA mapping used by the Shadow Virtqueue
+     *
+     * It is shared among all ASID for simplicity, whether CVQ shares ASID with
+     * guest or not:
+     * - Memory listener need access to guest's memory addresses allocated in
+     *   the IOVA tree.
+     * - There should be plenty of IOVA address space for both ASID not to
+     *   worry about collisions between them.  Guest's translations are still
+     *   validated with virtio virtqueue_pop so there is no risk for the guest
+     *   to access memory that it shouldn't.
+     *
+     * To allocate a iova tree per ASID is doable but it complicates the code
+     * and it is not worth it for the moment.
+     */
     VhostIOVATree *iova_tree;
 
     /* Copy of backend features */
