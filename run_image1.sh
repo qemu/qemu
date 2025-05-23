bb53fd657f5fb7bc57907215bd97b86ed2e713f8
JothamWong <jwys6747@gmail.com>
2025-05-24 04:36:55 +0800

    util: update the run scripts
    They now use the cxl-switch-client instead of the replicas

--- run_image1.sh
+++ run_image1.sh
@@ -1,13 +1,9 @@
 #!/usr/bin/env bash
 
-img1="ubuntu-22.04-image0.img" # Renamed for clarity, was 'img'
+img1="ubuntu-22.04-image0.img"
 user_data1="user-data0.img"
 REPLICA_SIZE="256M"
-
-# --- QEMU Execution (Example for Image 1 - adapt as needed) ---
-# You'll need to decide how you want to run QEMU for each image.
-# This is the original QEMU command, now pointing to img1 and user_data1.
-# You would duplicate and modify this section for img2.
+SERVER_SOCKET_PATH="/tmp/cxl_switch_server.sock"
 
 ./build/qemu-system-x86_64 \
     -m 16G \
@@ -18,8 +14,5 @@ REPLICA_SIZE="256M"
     -enable-kvm \
     -vga std \
     -device virtio-net-pci,netdev=net0 -netdev user,id=net0 \
-    -object memory-backend-file,id=my-memdev0,size=${REPLICA_SIZE},mem-path=/home/jotham/qemu-cxl-shm/replica0.img,share=on \
-    -object memory-backend-file,id=my-memdev1,size=${REPLICA_SIZE},mem-path=/home/jotham/qemu-cxl-shm/replica1.img,share=on \
-    -object memory-backend-file,id=my-memdev2,size=${REPLICA_SIZE},mem-path=/home/jotham/qemu-cxl-shm/replica2.img,share=on \
-    -device cxl-switch,id=cxlsw0,mem-size=${REPLICA_SIZE},memdev0=my-memdev0,memdev1=my-memdev1,memdev2=my-memdev2 \
+    -device cxl-switch-client,id=cxlsw0,socket-path="${SERVER_SOCKET_PATH}" \
     -virtfs local,path=qemu_share,mount_tag=shared,security_model=mapped-xattr
