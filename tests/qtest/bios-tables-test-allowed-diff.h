85240876b2f6f9edd72a6f324cac0ee9ee28ab0a
Bibo Mao <maobibo@loongson.cn>
2025-06-12 17:03:21 +0800

    tests/acpi: Remove stale allowed tables
    Remove stale allowed tables for LoongArch virt machine.
    
    Signed-off-by: Bibo Mao <maobibo@loongson.cn>
    Message-Id: <20250612090321.3416594-6-maobibo@loongson.cn>
    Reviewed-by: Michael S. Tsirkin <mst@redhat.com>
    Signed-off-by: Michael S. Tsirkin <mst@redhat.com>

--- tests/qtest/bios-tables-test-allowed-diff.h
+++ tests/qtest/bios-tables-test-allowed-diff.h
@@ -1,9 +1,1 @@
 /* List of comma-separated changed AML files to ignore */
-"tests/data/acpi/loongarch64/virt/APIC",
-"tests/data/acpi/loongarch64/virt/DSDT",
-"tests/data/acpi/loongarch64/virt/FACP",
-"tests/data/acpi/loongarch64/virt/MCFG",
-"tests/data/acpi/loongarch64/virt/PPTT",
-"tests/data/acpi/loongarch64/virt/SLIT",
-"tests/data/acpi/loongarch64/virt/SPCR",
-"tests/data/acpi/loongarch64/virt/SRAT",
