49b91833cc671c30fd2a074a92c50c858d6ec055
Sean Wei <me@sean.taipei>
2025-06-13 12:50:20 -0400

    hw: replace FSF postal address with licenses URL
    The GPLv2 boiler-plate in vmxnet3.h and vmw_pvscsi.h still
    contained the obsolete "51 Franklin Street" postal address.
    
    Replace it with the canonical GNU licenses URL recommended by the FSF:
    https://www.gnu.org/licenses/
    
    Signed-off-by: Sean Wei <me@sean.taipei>
    Reviewed-by: Thomas Huth <thuth@redhat.com>
    Message-ID: <20250613.qemu.patch.08@sean.taipei>
    Signed-off-by: Thomas Huth <thuth@redhat.com>

--- hw/scsi/vmw_pvscsi.h
+++ hw/scsi/vmw_pvscsi.h
@@ -14,8 +14,8 @@
  * details.
  *
  * You should have received a copy of the GNU General Public License
- * along with this program; if not, write to the Free Software
- * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA.
+ * along with this program; if not, see
+ * <https://www.gnu.org/licenses/>.
  *
  * Maintained by: Arvind Kumar <arvindkumar@vmware.com>
  *
