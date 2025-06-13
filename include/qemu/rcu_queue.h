c61b807e625846871e3d142a9f101d58f7ee2522
Sean Wei <me@sean.taipei>
2025-06-13 12:46:15 -0400

    include/qemu: replace FSF postal address with licenses URL
    The LGPLv2.1 boiler-plate in rcu.h and rcu_queue.h still
    contained the obsolete "51 Franklin Street" postal address.
    
    Replace it with the canonical GNU licenses URL recommended by the FSF:
    https://www.gnu.org/licenses/
    
    Signed-off-by: Sean Wei <me@sean.taipei>
    Reviewed-by: Thomas Huth <thuth@redhat.com>
    Message-ID: <20250613.qemu.patch.06@sean.taipei>
    Signed-off-by: Thomas Huth <thuth@redhat.com>

--- include/qemu/rcu_queue.h
+++ include/qemu/rcu_queue.h
@@ -17,8 +17,8 @@
  * Lesser General Public License for more details.
  *
  * You should have received a copy of the GNU Lesser General Public
- * License along with this library; if not, write to the Free Software
- * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
+ * License along with this library; if not, see
+ * <https://www.gnu.org/licenses/>.
  *
  * Copyright (c) 2013 Mike D. Day, IBM Corporation.
  *
