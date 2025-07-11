cb58ff30c53e14a01371f7c8064949903ce1898c
minglei.liu <minglei.liu@smartx.com>
2025-07-11 10:17:14 +0800

    qga: Fix truncated output handling in guest-exec status reporting
    Signed-off-by: minglei.liu <minglei.liu@smartx.com>
    Fixes: a1853dca743
    Reviewed-by: Daniel P. Berrangé <berrange@redhat.com>
    Reviewed-by: Kostiantyn Kostiuk <kkostiuk@redhat.com>
    Link: https://lore.kernel.org/qemu-devel/20250711021714.91258-1-minglei.liu@smartx.com
    Signed-off-by: Kostiantyn Kostiuk <kkostiuk@redhat.com>
    (cherry picked from commit 28c5d27dd4dc4100a96ff4c9e5871dd23c6b02ec)
    Signed-off-by: Michael Tokarev <mjt@tls.msk.ru>

--- qga/commands.c
+++ qga/commands.c
@@ -205,13 +205,15 @@ GuestExecStatus *qmp_guest_exec_status(int64_t pid, Error **errp)
 #endif
         if (gei->out.length > 0) {
             ges->out_data = g_base64_encode(gei->out.data, gei->out.length);
-            ges->has_out_truncated = gei->out.truncated;
+            ges->has_out_truncated = true;
+            ges->out_truncated = gei->out.truncated;
         }
         g_free(gei->out.data);
 
         if (gei->err.length > 0) {
             ges->err_data = g_base64_encode(gei->err.data, gei->err.length);
-            ges->has_err_truncated = gei->err.truncated;
+            ges->has_err_truncated = true;
+            ges->err_truncated = gei->err.truncated;
         }
         g_free(gei->err.data);
 
