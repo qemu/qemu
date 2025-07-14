ce1b67acd6fa6b3cc959340b2b45a96b1b809c76
Yicong Yang <yangyicong@hisilicon.com>
2025-07-14 18:31:43 +0100

    tests: virt: Allow changes to PPTT test table
    Allow changes to PPTT test table, preparing for adding identical
    implementation flags support and for adding a root node for all
    the system.
    
    This is related to both loongarch64 and aarch64.
    
    Signed-off-by: Yicong Yang <yangyicong@hisilicon.com>
    Reviewed-by: Jonathan Cameron <Jonathan.Cameron@huawei.com>
    Reviewed-by: Zhao Liu <zhao1.liu@intel.com>
    Signed-off-by: Alireza Sanaee <alireza.sanaee@huawei.com>
    Message-Id: <20250714173146.511-2-alireza.sanaee@huawei.com>
    Reviewed-by: Michael S. Tsirkin <mst@redhat.com>
    Signed-off-by: Michael S. Tsirkin <mst@redhat.com>

--- tests/qtest/bios-tables-test-allowed-diff.h
+++ tests/qtest/bios-tables-test-allowed-diff.h
@@ -1,1 +1,6 @@
 /* List of comma-separated changed AML files to ignore */
+"tests/data/acpi/aarch64/virt/PPTT",
+"tests/data/acpi/aarch64/virt/PPTT.acpihmatvirt",
+"tests/data/acpi/aarch64/virt/PPTT.topology",
+"tests/data/acpi/loongarch64/virt/PPTT",
+"tests/data/acpi/loongarch64/virt/PPTT.topology",
