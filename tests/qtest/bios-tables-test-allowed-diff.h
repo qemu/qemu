9748673735d93da3b1da12041f2525e0fbca5dc6
Gustavo Romero <gustavo.romero@linaro.org>
2025-07-14 10:04:48 +0200

    tests/qtest/bios-tables-test: Prepare for changes in the DSDT table
    This commit adds DSDT blobs to the whilelist in the prospect to
    allow changes in the GPEX _OSC method.
    
    Signed-off-by: Gustavo Romero <gustavo.romero@linaro.org>
    Signed-off-by: Eric Auger <eric.auger@redhat.com>
    Reviewed-by: Jonathan Cameron <jonathan.cameron@huawei.com>
    Acked-by: Igor Mammedov <imammedo@redhat.com>
    Message-Id: <20250714080639.2525563-5-eric.auger@redhat.com>
    Reviewed-by: Michael S. Tsirkin <mst@redhat.com>
    Signed-off-by: Michael S. Tsirkin <mst@redhat.com>

--- tests/qtest/bios-tables-test-allowed-diff.h
+++ tests/qtest/bios-tables-test-allowed-diff.h
@@ -1,1 +1,12 @@
 /* List of comma-separated changed AML files to ignore */
+"tests/data/acpi/aarch64/virt/DSDT",
+"tests/data/acpi/aarch64/virt/DSDT.acpihmatvirt",
+"tests/data/acpi/aarch64/virt/DSDT.memhp",
+"tests/data/acpi/aarch64/virt/DSDT.pxb",
+"tests/data/acpi/aarch64/virt/DSDT.topology",
+"tests/data/acpi/loongarch64/virt/DSDT.memhp",
+"tests/data/acpi/loongarch64/virt/DSDT.topology",
+"tests/data/acpi/loongarch64/virt/DSDT.numamem",
+"tests/data/acpi/loongarch64/virt/DSDT",
+"tests/data/acpi/x86/microvm/DSDT.pcie",
+"tests/data/acpi/riscv64/virt/DSDT",
