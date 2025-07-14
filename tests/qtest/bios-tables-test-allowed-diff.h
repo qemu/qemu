4dfb1418278a43f3acbf177dfcafe955da8ffaf6
Eric Auger <eric.auger@redhat.com>
2025-07-14 10:05:06 +0200

    tests/qtest/bios-tables-test: Update ARM DSDT reference blobs
        Changes relate to the introduction of pieces related to
        acpi-index static support along with root ports with no hotplug.
    
    +
    +    Scope (\_SB.PCI0)
    +    {
    +        Method (EDSM, 5, Serialized)
    +        {
    +            If ((Arg2 == Zero))
    +            {
    +                Local0 = Buffer (One)
    +                    {
    +                         0x00                                             // .
    +                    }
    +                If ((Arg0 != ToUUID ("e5c937d0-3553-4d7a-9117-ea4d19c3434d") /* Device Labeling Interface */))
    +                {
    +                    Return (Local0)
    +                }
    +
    +                If ((Arg1 < 0x02))
    +                {
    +                    Return (Local0)
    +                }
    +
    +                Local0 [Zero] = 0x81
    +                Return (Local0)
    +            }
    +
    +            If ((Arg2 == 0x07))
    +            {
    +                Local0 = Package (0x02)
    +                    {
    +                        Zero,
    +                        ""
    +                    }
    +                Local1 = DerefOf (Arg4 [Zero])
    +                Local0 [Zero] = Local1
    +                Return (Local0)
    +            }
    +        }
    +
    +        Device (S00)
    +        {
    +            Name (_ADR, Zero)  // _ADR: Address
    +        }
    +
    +        Device (S08)
    +        {
    +            Name (_ADR, 0x00010000)  // _ADR: Address
    +        }
    +
    +        Device (S10)
    +        {
    +            Name (_ADR, 0x00020000)  // _ADR: Address
    +        }
    +    }
     }
    
    Signed-off-by: Eric Auger <eric.auger@redhat.com>
    Reviewed-by: Jonathan Cameron <jonathan.cameron@huawei.com>
    Reviewed-by: Igor Mammedov <imammedo@redhat.com>
    Message-Id: <20250714080639.2525563-23-eric.auger@redhat.com>
    Reviewed-by: Michael S. Tsirkin <mst@redhat.com>
    Signed-off-by: Michael S. Tsirkin <mst@redhat.com>

--- tests/qtest/bios-tables-test-allowed-diff.h
+++ tests/qtest/bios-tables-test-allowed-diff.h
@@ -1,7 +1,1 @@
 /* List of comma-separated changed AML files to ignore */
-"tests/data/acpi/aarch64/virt/DSDT",
-"tests/data/acpi/aarch64/virt/DSDT.acpihmatvirt",
-"tests/data/acpi/aarch64/virt/DSDT.memhp",
-"tests/data/acpi/aarch64/virt/DSDT.pxb",
-"tests/data/acpi/aarch64/virt/DSDT.topology",
-"tests/data/acpi/aarch64/virt/DSDT.viot",
