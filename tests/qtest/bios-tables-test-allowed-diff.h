1983d552999a68dec60246e3e2fd122ecf25fa97
Gustavo Romero <gustavo.romero@linaro.org>
2025-07-14 10:05:04 +0200

    tests/qtest/bios-tables-test: Prepare for changes in the arm virt DSDT table
    This commit adds DSDT blobs to the whilelist in the prospect to
    allow changes in the arm virt DSDT method.
    
    Signed-off-by: Gustavo Romero <gustavo.romero@linaro.org>
    Signed-off-by: Eric Auger <eric.auger@redhat.com>
    Reviewed-by: Jonathan Cameron <jonathan.cameron@huawei.com>
    Reviewed-by: Igor Mammedov <imammedo@redhat.com>
    Message-Id: <20250714080639.2525563-21-eric.auger@redhat.com>
    Reviewed-by: Michael S. Tsirkin <mst@redhat.com>
    Signed-off-by: Michael S. Tsirkin <mst@redhat.com>

--- tests/qtest/bios-tables-test-allowed-diff.h
+++ tests/qtest/bios-tables-test-allowed-diff.h
@@ -1,1 +1,7 @@
 /* List of comma-separated changed AML files to ignore */
+"tests/data/acpi/aarch64/virt/DSDT",
+"tests/data/acpi/aarch64/virt/DSDT.acpihmatvirt",
+"tests/data/acpi/aarch64/virt/DSDT.memhp",
+"tests/data/acpi/aarch64/virt/DSDT.pxb",
+"tests/data/acpi/aarch64/virt/DSDT.topology",
+"tests/data/acpi/aarch64/virt/DSDT.viot",
