67adbc8b6f40f6a2370d4a0ba479d3778d147cbc
Thomas Huth <thuth@redhat.com>
2025-08-19 13:23:50 +0200

    tests/functional: Move microblaze tests into architecture specific folder
    The tests/functional folder has become quite crowded, thus move the
    microblaze tests into a target-specific subfolder.
    
    Reviewed-by: Pierrick Bouvier <pierrick.bouvier@linaro.org>
    Reviewed-by: Philippe Mathieu-Daudé <philmd@linaro.org>
    Signed-off-by: Thomas Huth <thuth@redhat.com>
    Message-ID: <20250819112403.432587-14-thuth@redhat.com>

--- tests/functional/microblazeel/test_s3adsp1800.py
+++ tests/functional/microblazeel/test_s3adsp1800.py
@@ -7,7 +7,7 @@
 # This work is licensed under the terms of the GNU GPL, version 2 or
 # later. See the COPYING file in the top-level directory.
 
-from test_microblaze_s3adsp1800 import MicroblazeMachine
+from microblaze.test_s3adsp1800 import MicroblazeMachine
 
 
 class MicroblazeLittleEndianMachine(MicroblazeMachine):
