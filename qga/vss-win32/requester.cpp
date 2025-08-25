edf3780a7dad4658ab7b72ea37e310a2be9b16d3
Kostiantyn Kostiuk <kkostiuk@redhat.com>
2025-08-25 16:53:11 +0300

    qga-vss: Write hex value of error in log
    QGA-VSS writes error using error_setg_win32_internal,
    which call g_win32_error_message.
    
    g_win32_error_message - translate a Win32 error code
    (as returned by GetLastError()) into the corresponding message.
    
    In the same time, we call error_setg_win32_internal with
    error codes from different Windows componets like VSS or
    Performance monitor that provides different codes and
    can't be converted with g_win32_error_message. In this
    case, the empty suffix will be returned so error will be
    masked.
    
    This commit directly add hex value of error code.
    
    Reproduce:
     - Run QGA command: {"execute": "guest-fsfreeze-freeze-list", "arguments": {"mountpoints": ["D:"]}}
    
    QGA error example:
     - before changes:
      {"error": {"class": "GenericError", "desc": "failed to add D: to snapshot set: "}}
     - after changes:
      {"error": {"class": "GenericError", "desc": "failed to add D: to snapshot set: Windows error 0x8004230e: "}}
    
    Reviewed-by: Yan Vugenfirer <yvugenfi@redhat.com>
    Link: https://lore.kernel.org/qemu-devel/20250825135311.138330-1-kkostiuk@redhat.com
    Signed-off-by: Kostiantyn Kostiuk <kkostiuk@redhat.com>

--- qga/vss-win32/requester.cpp
+++ qga/vss-win32/requester.cpp
@@ -28,8 +28,9 @@
 
 #define err_set(e, err, fmt, ...) {                                         \
     (e)->error_setg_win32_wrapper((e)->errp, __FILE__, __LINE__, __func__,  \
-                                   err, fmt, ## __VA_ARGS__);               \
-    qga_debug(fmt, ## __VA_ARGS__);                                         \
+                                   err, fmt ": Windows error 0x%lx",        \
+                                   ## __VA_ARGS__, err);                    \
+    qga_debug(fmt ": Windows error 0x%lx", ## __VA_ARGS__, err);            \
 }
 /* Bad idea, works only when (e)->errp != NULL: */
 #define err_is_set(e) ((e)->errp && *(e)->errp)
