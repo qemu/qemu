57ac646a2ecb2967c46febcfd0f40d396868b4dc
CLEMENT MATHIEU--DRIF <clement.mathieu--drif@eviden.com>
2025-09-01 11:17:20 +0000

    intel_iommu: Bypass barrier wait descriptor
    wait_desc with SW=0,IF=0,FN=1 must not be considered as an
    invalid descriptor as it is used to implement section 7.10 of
    the VT-d spec.
    
    Signed-off-by: Clement Mathieu--Drif <clement.mathieu--drif@eviden.com>
    Reviewed-by: Michael S. Tsirkin <mst@redhat.com>
    Message-ID: <20250901111630.1018573-3-clement.mathieu--drif@eviden.com>
    Signed-off-by: Michael S. Tsirkin <mst@redhat.com>

--- hw/i386/intel_iommu.c
+++ hw/i386/intel_iommu.c
@@ -2857,7 +2857,13 @@ static bool vtd_process_wait_desc(IntelIOMMUState *s, VTDInvDesc *inv_desc)
         vtd_generate_completion_event(s);
     }
 
-    if (!(inv_desc->lo & (VTD_INV_DESC_WAIT_IF | VTD_INV_DESC_WAIT_SW))) {
+    /*
+     * SW=0, IF=0, FN=1 is also a valid descriptor (VT-d 7.10)
+     * Nothing to do as we process the descriptors in order
+     */
+
+    if (!(inv_desc->lo & (VTD_INV_DESC_WAIT_IF | VTD_INV_DESC_WAIT_SW |
+                                                 VTD_INV_DESC_WAIT_FN))) {
         error_report_once("%s: invalid wait desc: hi=%"PRIx64", lo=%"PRIx64
                           " (unknown type)", __func__, inv_desc->hi,
                           inv_desc->lo);
