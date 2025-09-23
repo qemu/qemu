ebf7a5d294a336c7701018884587646d89030073
Richard Henderson <richard.henderson@linaro.org>
2025-09-23 13:26:53 -0700

    accel/tcg: Improve buffer overflow in tb_gen_code
    If we only have one vcpu, or within cpu_exec_step_atomic,
    we needn't jump all the way back out to the cpu loop to
    empty the code gen buffer.
    
    Signed-off-by: Richard Henderson <richard.henderson@linaro.org>

--- accel/tcg/translate-all.c
+++ accel/tcg/translate-all.c
@@ -289,6 +289,10 @@ TranslationBlock *tb_gen_code(CPUState *cpu, TCGTBCPUState s)
     tb = tcg_tb_alloc(tcg_ctx);
     if (unlikely(!tb)) {
         /* flush must be done */
+        if (cpu_in_serial_context(cpu)) {
+            tb_flush__exclusive_or_serial();
+            goto buffer_overflow;
+        }
         queue_tb_flush(cpu);
         mmap_unlock();
         /* Make the execution loop process the flush as soon as possible.  */
