cdcb1d39eb72410bee55b6d3344f176c86224447
Huacai Chen <chenhuacai@kernel.org>
2025-09-23 22:35:40 +0800

    bios-tables-test-allowed-diff.h: Allow LoongArch DSDT.*
    Signed-off-by: Huacai Chen <chenhuacai@kernel.org>
    Reviewed-by: <maobibo@loongson.cn>
    Message-ID: <20250923143542.2391576-2-chenhuacai@kernel.org>
    Signed-off-by: Song Gao <gaosong@loongson.cn>

--- tests/qtest/bios-tables-test-allowed-diff.h
+++ tests/qtest/bios-tables-test-allowed-diff.h
@@ -1,1 +1,5 @@
 /* List of comma-separated changed AML files to ignore */
+"tests/data/acpi/loongarch64/virt/DSDT",
+"tests/data/acpi/loongarch64/virt/DSDT.memhp",
+"tests/data/acpi/loongarch64/virt/DSDT.numamem",
+"tests/data/acpi/loongarch64/virt/DSDT.topology",
