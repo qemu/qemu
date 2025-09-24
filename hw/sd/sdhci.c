0c6d897e38f5a6a864d6ae6dc42f521b84011107
Akihiko Odaki <odaki@rsg.ci.i.u-tokyo.ac.jp>
2025-09-24 13:37:24 +0900

    hw/sd/sdhci: Do not unparent in instance_finalize()
    Children are automatically unparented so manually unparenting is
    unnecessary.
    
    Worse, automatic unparenting happens before the instance_finalize()
    callback of the parent gets called, so object_unparent() calls in
    the callback will refer to objects that are already unparented, which
    is semantically incorrect.
    
    Signed-off-by: Akihiko Odaki <odaki@rsg.ci.i.u-tokyo.ac.jp>
    Reviewed-by: Daniel P. Berrangé <berrange@redhat.com>
    Link: https://lore.kernel.org/r/20250924-use-v4-5-07c6c598f53d@rsg.ci.i.u-tokyo.ac.jp
    Signed-off-by: Paolo Bonzini <pbonzini@redhat.com>

--- hw/sd/sdhci.c
+++ hw/sd/sdhci.c
@@ -1578,10 +1578,6 @@ static void sdhci_sysbus_finalize(Object *obj)
 {
     SDHCIState *s = SYSBUS_SDHCI(obj);
 
-    if (s->dma_mr) {
-        object_unparent(OBJECT(s->dma_mr));
-    }
-
     sdhci_uninitfn(s);
 }
 
