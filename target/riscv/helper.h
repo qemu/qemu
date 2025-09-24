0c21b32947b2ffa2f1027a6edea5e9f47284248e
Jim Shu <jim.shu@sifive.com>
2025-09-24 15:48:18 +0800

    target/riscv: Fix ssamoswap error handling
    Follow the RISC-V CFI v1.0 spec [1] to fix the exception type
    when ssamoswap is disabled by xSSE.
    
    [1] RISC-V CFI spec v1.0, ch2.7 Atomic Swap from a Shadow Stack Location
    
    Signed-off-by: Jim Shu <jim.shu@sifive.com>
    Reviewed-by: Alistair Francis <alistair.francis@wdc.com>
    Message-ID: <20250924074818.230010-4-jim.shu@sifive.com>
    Signed-off-by: Alistair Francis <alistair.francis@wdc.com>
    (cherry picked from commit 0b16c7b6a854d461cdfd418769b51d58e43dd92a)
    Signed-off-by: Michael Tokarev <mjt@tls.msk.ru>

--- target/riscv/helper.h
+++ target/riscv/helper.h
@@ -1284,3 +1284,8 @@ DEF_HELPER_4(vgmul_vv, void, ptr, ptr, env, i32)
 DEF_HELPER_5(vsm4k_vi, void, ptr, ptr, i32, env, i32)
 DEF_HELPER_4(vsm4r_vv, void, ptr, ptr, env, i32)
 DEF_HELPER_4(vsm4r_vs, void, ptr, ptr, env, i32)
+
+/* CFI (zicfiss) helpers */
+#ifndef CONFIG_USER_ONLY
+DEF_HELPER_1(ssamoswap_disabled, void, env)
+#endif
