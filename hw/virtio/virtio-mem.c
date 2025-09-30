97480ca692e94bb790190a43bb122bd0752b8f62
Philippe Mathieu-Daudé <philmd@linaro.org>
2025-09-30 09:20:38 +0200

    hw: Remove unnecessary 'system/ram_addr.h' header
    None of these files require definition exposed by "system/ram_addr.h",
    remove its inclusion.
    
    Signed-off-by: Philippe Mathieu-Daudé <philmd@linaro.org>
    Reviewed-by: Richard Henderson <richard.henderson@linaro.org>
    Reviewed-by: Jagannathan Raman <jag.raman@oracle.com>
    Reviewed-by: Cédric Le Goater <clg@redhat.com>
    Reviewed-by: Eric Farman <farman@linux.ibm.com>
    Reviewed-by: Thomas Huth <thuth@redhat.com>
    Acked-by: Michael S. Tsirkin <mst@redhat.com>
    Message-Id: <20251001175448.18933-7-philmd@linaro.org>

--- hw/virtio/virtio-mem.c
+++ hw/virtio/virtio-mem.c
@@ -25,7 +25,6 @@
 #include "hw/virtio/virtio-mem.h"
 #include "qapi/error.h"
 #include "qapi/visitor.h"
-#include "system/ram_addr.h"
 #include "migration/misc.h"
 #include "hw/boards.h"
 #include "hw/qdev-properties.h"
