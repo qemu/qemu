3c17823e2940c0a7756246eb57b5971ecfe197b6
Cédric Le Goater <clg@redhat.com>
2025-10-07 16:16:03 +0200

    test/functional/aarch64: Remove test for the ast2700a0-evb machine
    The 'ast2700a0-evb' machine was deprecated in commit 6888a4a9c860 and
    removal is scheduled in the QEMU 11.0 release. This change removes
    the corresponding tests ahead of time to save CI resources.
    
    Cc: Thomas Huth <thuth@redhat.com>
    Reviewed-by: Thomas Huth <thuth@redhat.com>
    Reviewed-by: Jamin Lin <jamin_lin@aspeedtech.com>
    Link: https://lore.kernel.org/qemu-devel/20251007141604.761686-5-clg@redhat.com
    Signed-off-by: Cédric Le Goater <clg@redhat.com>

--- tests/functional/aarch64/test_aspeed_ast2700.py
+++ tests/functional/aarch64/test_aspeed_ast2700.py
@@ -46,10 +46,6 @@ class AST2x00MachineSDK(QemuSystemTest):
         exec_command_and_wait_for_pattern(self, 'root', 'Password:')
         exec_command_and_wait_for_pattern(self, '0penBmc', f'root@{name}:~#')
 
-    ASSET_SDK_V906_AST2700 = Asset(
-            'https://github.com/AspeedTech-BMC/openbmc/releases/download/v09.06/ast2700-a0-default-obmc.tar.gz',
-            '7247b6f19dbfb700686f8d9f723ac23f3eb229226c0589cb9b06b80d1b61f3cb')
-
     ASSET_SDK_V908_AST2700A1 = Asset(
             'https://github.com/AspeedTech-BMC/openbmc/releases/download/v09.08/ast2700-default-obmc.tar.gz',
             'eac3dc409b7ea3cd4b03d4792d3cebd469792ad893cb51e1d15f0fc20bd1e2cd')
@@ -123,14 +119,6 @@ class AST2x00MachineSDK(QemuSystemTest):
         self.vm.add_args('-bios', 'ast27x0_bootrom.bin')
         self.do_test_aarch64_aspeed_sdk_start(
                 self.scratch_file(name, 'image-bmc'))
-
-    def test_aarch64_ast2700a0_evb_sdk_v09_06(self):
-        self.set_machine('ast2700a0-evb')
-
-        self.archive_extract(self.ASSET_SDK_V906_AST2700)
-        self.start_ast2700_test('ast2700-a0-default')
-        self.verify_openbmc_boot_and_login('ast2700-a0-default')
-        self.do_ast2700_i2c_test()
 
     def test_aarch64_ast2700a1_evb_sdk_v09_08(self):
         self.set_machine('ast2700a1-evb')
