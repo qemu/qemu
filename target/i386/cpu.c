7917dee17a4cc82102a21f148d0ae845b446774f
Michael Tokarev <mjt@tls.msk.ru>
2025-10-08 16:42:20 +0300

    Revert "i386/cpu: Move adjustment of CPUID_EXT_PDCM before feature_dependencies[] check"
    This reverts commit 3d26cb65c27190e57637644ecf6c96b8c3d246a3
    (v10.0.3-34-g3d26cb65c271) from 10.0.x branch.
    
    The problem is that the change makes qemu 10.0.x non-migratable
    to subsequent qemu versions, since it requires introducing a new
    machine type.  This change should not have been picked up for 10.0.x.
    
    Reviewed-by: Zhao Liu <zhao1.liu@intel.com>
    Reopens: https://gitlab.com/qemu-project/qemu/-/issues/3061 (for 10.0.x)
    Signed-off-by: Michael Tokarev <mjt@tls.msk.ru>

--- target/i386/cpu.c
+++ target/i386/cpu.c
@@ -6851,6 +6851,9 @@ void cpu_x86_cpuid(CPUX86State *env, uint32_t index, uint32_t count,
             /* Fixup overflow: max value for bits 23-16 is 255. */
             *ebx |= MIN(num, 255) << 16;
         }
+        if (!cpu->enable_pmu) {
+            *ecx &= ~CPUID_EXT_PDCM;
+        }
         break;
     case 2:
         /* cache info: needed for Pentium Pro compatibility */
@@ -7838,10 +7841,6 @@ void x86_cpu_expand_features(X86CPU *cpu, Error **errp)
         if (!IS_INTEL_CPU(env) && !IS_ZHAOXIN_CPU(env)) {
             env->features[FEAT_8000_0001_ECX] |= CPUID_EXT3_CMP_LEG;
         }
-    }
-
-    if (!cpu->enable_pmu) {
-        env->features[FEAT_1_ECX] &= ~CPUID_EXT_PDCM;
     }
 
     for (i = 0; i < ARRAY_SIZE(feature_dependencies); i++) {
