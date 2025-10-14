555a090ed079614cf8898e43451eb9b1516ea77d
Soumyajyotii Ssarkar <soumyajyotisarkar23@gmail.com>
2025-10-14 22:44:16 +0200

    lasi: Forward LASI SCSI ports to NCR 53c710 driver
    Signed-off-by: Soumyajyotii Ssarkar <soumyajyotisarkar23@gmail.com>
    Reviewed-by: Helge Deller <deller@gmx.de>
    Signed-off-by: Helge Deller <deller@gmx.de>

--- include/hw/misc/lasi.h
+++ include/hw/misc/lasi.h
@@ -28,7 +28,9 @@ OBJECT_DECLARE_SIMPLE_TYPE(LasiState, LASI_CHIP)
 #define LASI_LPT        0x02000
 #define LASI_AUDIO      0x04000
 #define LASI_UART       0x05000
+#define LASI_SCSI       0x06000
 #define LASI_LAN        0x07000
+#define LASI_PS2        0x08000
 #define LASI_RTC        0x09000
 #define LASI_FDC        0x0A000
 
