77a5b02b5b3a3053414560737a41151005623b50
Vishal Chourasia <vishalc@linux.ibm.com>
2025-10-24 18:35:53 +0530

    hw/core/loader: Use qemu_open() instead of open() in get_image_size()
    Replace open() with qemu_open() which provides better error handling
    via the Error object, automatically sets O_CLOEXEC, and supports FD
    passing with /dev/fdset.
    
    Currently pass errp argument as NULL.
    
    Suggested-by: Daniel P. Berrangé <berrange@redhat.com>
    Reviewed-by: Philippe Mathieu-Daudé <philmd@linaro.org>
    Signed-off-by: Vishal Chourasia <vishalc@linux.ibm.com>
    Message-ID: <20251024130556.1942835-4-vishalc@linux.ibm.com>
    Signed-off-by: Philippe Mathieu-Daudé <philmd@linaro.org>

--- hw/core/loader.c
+++ hw/core/loader.c
@@ -74,7 +74,7 @@ int64_t get_image_size(const char *filename)
 {
     int fd;
     int64_t size;
-    fd = open(filename, O_RDONLY | O_BINARY);
+    fd = qemu_open(filename, O_RDONLY | O_BINARY, NULL);
     if (fd < 0)
         return -1;
     size = lseek(fd, 0, SEEK_END);
