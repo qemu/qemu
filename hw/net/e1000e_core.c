28efd5e5dd204810250d19e10fb89f4aa0c5161a
Peter Maydell <peter.maydell@linaro.org>
2025-11-03 17:58:51 +0000

    hw/net/e1000e_core: Adjust e1000e_write_payload_frag_to_rx_buffers() assert
    An assertion in e1000e_write_payload_frag_to_rx_buffers() attempts to
    guard against the calling code accidentally trying to write too much
    data to a single RX descriptor, such that the E1000EBAState::cur_idx
    indexes off the end of the EB1000BAState::written[] array.
    
    Unfortunately it is overzealous: it asserts that cur_idx is in
    range after it has been incremented. This will fire incorrectly
    for the case where the guest configures four buffers and exactly
    enough bytes are written to fill all four of them.
    
    The only places where we use cur_idx and index in to the written[]
    array are the functions e1000e_write_hdr_frag_to_rx_buffers() and
    e1000e_write_payload_frag_to_rx_buffers(), so we can rewrite this to
    assert before doing the array dereference, rather than asserting
    after updating cur_idx.
    
    Cc: qemu-stable@nongnu.org
    Reviewed-by: Akihiko Odaki <odaki@rsg.ci.i.u-tokyo.ac.jp>
    Signed-off-by: Peter Maydell <peter.maydell@linaro.org>
    Signed-off-by: Jason Wang <jasowang@redhat.com>
    (cherry picked from commit bab496a18358643b686f69e2b97d73fb98d37e79)
    (Mjt: in 7.2.x it is e1000e_write_to_rx_buffers, not
     e1000e_write_payload_frag_to_rx_buffers, due to missing in 7.2.x
     v8.1.0-693-g17ccd0164796 "igb: RX payload guest writting refactoring")
    Signed-off-by: Michael Tokarev <mjt@tls.msk.ru>

--- hw/net/e1000e_core.c
+++ hw/net/e1000e_core.c
@@ -1411,10 +1411,13 @@ e1000e_write_to_rx_buffers(E1000ECore *core,
                            dma_addr_t data_len)
 {
     while (data_len > 0) {
-        uint32_t cur_buf_len = core->rxbuf_sizes[bastate->cur_idx];
-        uint32_t cur_buf_bytes_left = cur_buf_len -
-                                      bastate->written[bastate->cur_idx];
-        uint32_t bytes_to_write = MIN(data_len, cur_buf_bytes_left);
+        uint32_t cur_buf_len, cur_buf_bytes_left, bytes_to_write;
+
+        assert(bastate->cur_idx < MAX_PS_BUFFERS);
+
+        cur_buf_len = core->rxbuf_sizes[bastate->cur_idx];
+        cur_buf_bytes_left = cur_buf_len - bastate->written[bastate->cur_idx];
+        bytes_to_write = MIN(data_len, cur_buf_bytes_left);
 
         trace_e1000e_rx_desc_buff_write(bastate->cur_idx,
                                         (*ba)[bastate->cur_idx],
@@ -1433,8 +1436,6 @@ e1000e_write_to_rx_buffers(E1000ECore *core,
         if (bastate->written[bastate->cur_idx] == cur_buf_len) {
             bastate->cur_idx++;
         }
-
-        assert(bastate->cur_idx < MAX_PS_BUFFERS);
     }
 }
 
