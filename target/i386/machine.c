bb9f99e9baffb050fcddd324b31b20dd0a61adbc
Zhao Liu <zhao1.liu@intel.com>
2025-12-11 15:09:35 +0800

    i386/machine: Use VMSTATE_UINTTL_SUB_ARRAY for vmstate of CPUX86State.regs
    Before expanding the number of elements in the CPUX86State.regs array,
    first use VMSTATE_UINTTL_SUB_ARRAY for the regs' vmstate to avoid the
    type_check_array failure.
    
    VMSTATE_UINTTL_SUB_ARRAY will also be used for subsequently added elements
    in regs array.
    
    Tested-by: Xudong Hao <xudong.hao@intel.com>
    Signed-off-by: Zhao Liu <zhao1.liu@intel.com>
    Link: https://lore.kernel.org/r/20251211070942.3612547-3-zhao1.liu@intel.com
    Signed-off-by: Paolo Bonzini <pbonzini@redhat.com>

--- target/i386/machine.c
+++ target/i386/machine.c
@@ -1748,7 +1748,7 @@ const VMStateDescription vmstate_x86_cpu = {
     .pre_save = cpu_pre_save,
     .post_load = cpu_post_load,
     .fields = (const VMStateField[]) {
-        VMSTATE_UINTTL_ARRAY(env.regs, X86CPU, CPU_NB_REGS),
+        VMSTATE_UINTTL_SUB_ARRAY(env.regs, X86CPU, 0, CPU_NB_REGS),
         VMSTATE_UINTTL(env.eip, X86CPU),
         VMSTATE_UINTTL(env.eflags, X86CPU),
         VMSTATE_UINT32(env.hflags, X86CPU),
