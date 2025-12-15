881536e2a53dad9b528bf135c8835256f997737e
Fabiano Rosas <farosas@suse.de>
2025-12-15 18:59:48 -0300

    migration: Fix leak of cpr_exec_command
    Signed-off-by: Fabiano Rosas <farosas@suse.de>
    Reviewed-by: Peter Xu <peterx@redhat.com>
    Link: https://lore.kernel.org/r/20251215220041.12657-3-farosas@suse.de
    Signed-off-by: Peter Xu <peterx@redhat.com>

--- migration/migration.c
+++ migration/migration.c
@@ -4140,6 +4140,7 @@ static void migration_instance_finalize(Object *obj)
     MigrationState *ms = MIGRATION_OBJ(obj);
 
     qapi_free_BitmapMigrationNodeAliasList(ms->parameters.block_bitmap_mapping);
+    qapi_free_strList(ms->parameters.cpr_exec_command);
     qemu_mutex_destroy(&ms->error_mutex);
     qemu_mutex_destroy(&ms->qemu_file_lock);
     qemu_sem_destroy(&ms->wait_unplug_sem);
