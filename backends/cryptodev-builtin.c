3464e88bc98d72acc3a9674054b9ed0c3d4e9b90
zhenwei pi <pizhenwei@tensorfer.com>
2025-12-21 10:43:21 +0800

    cryptodev-builtin: Limit the maximum size
    This backend driver is used for demonstration purposes only, unlimited
    size leads QEMU OOM.
    
    Fixes: CVE-2025-14876
    Fixes: 1653a5f3fc7 ("cryptodev: introduce a new cryptodev backend")
    Reported-by: 이재영 <nakamurajames123@gmail.com>
    Signed-off-by: zhenwei pi <zhenwei.pi@linux.dev>
    Reviewed-by: Michael S. Tsirkin <mst@redhat.com>
    Signed-off-by: Michael S. Tsirkin <mst@redhat.com>
    Message-Id: <20251221024321.143196-3-zhenwei.pi@linux.dev>
    (cherry picked from commit 7b913094c703641a0442bb1d1165323a019c591c)
    Signed-off-by: Michael Tokarev <mjt@tls.msk.ru>

--- backends/cryptodev-builtin.c
+++ backends/cryptodev-builtin.c
@@ -53,6 +53,8 @@ typedef struct CryptoDevBackendBuiltinSession {
 
 #define CRYPTODEV_BUITLIN_MAX_AUTH_KEY_LEN    512
 #define CRYPTODEV_BUITLIN_MAX_CIPHER_KEY_LEN  64
+/* demonstration purposes only, use a limited size to avoid QEMU OOM */
+#define CRYPTODEV_BUITLIN_MAX_REQUEST_SIZE  (1024 * 1024)
 
 struct CryptoDevBackendBuiltin {
     CryptoDevBackend parent_obj;
@@ -98,12 +100,7 @@ static void cryptodev_builtin_init(
                          1u << QCRYPTODEV_BACKEND_SERVICE_TYPE_MAC;
     backend->conf.cipher_algo_l = 1u << VIRTIO_CRYPTO_CIPHER_AES_CBC;
     backend->conf.hash_algo = 1u << VIRTIO_CRYPTO_HASH_SHA1;
-    /*
-     * Set the Maximum length of crypto request.
-     * Why this value? Just avoid to overflow when
-     * memory allocation for each crypto request.
-     */
-    backend->conf.max_size = LONG_MAX - sizeof(CryptoDevBackendOpInfo);
+    backend->conf.max_size = CRYPTODEV_BUITLIN_MAX_REQUEST_SIZE;
     backend->conf.max_cipher_key_len = CRYPTODEV_BUITLIN_MAX_CIPHER_KEY_LEN;
     backend->conf.max_auth_key_len = CRYPTODEV_BUITLIN_MAX_AUTH_KEY_LEN;
     cryptodev_builtin_init_akcipher(backend);
