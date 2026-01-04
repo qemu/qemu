014c06435f96c82d0e0be67832d40be43133dc71
Jason Wang <jasowang@redhat.com>
2026-01-04 15:54:12 +0800

    tests/qtest: Add test for filter-redirector rx event opened
    Add a new test case 'test_redirector_rx_event_opened' to verify the
    handling of the CHR_EVENT_OPENED event in filter-redirector.
    
    The test simulates a scenario where the backend character device (socket)
    is disconnected and then reconnected. It works by:
    1. Connecting to the redirector's socket (triggers CHR_EVENT_OPENED).
    2. Sending a packet to verify initial connectivity.
    3. Disconnecting (triggers CHR_EVENT_CLOSED).
    4. Reconnecting (triggers CHR_EVENT_OPENED again).
    5. Sending another packet to verify that the redirector correctly
       re-registers its handlers and resumes passing traffic.
    
    This ensures that the filter-redirector can recover and function correctly
    after a backend reconnection.
    
    Reviewed-by: Zhang Chen <zhangckid@gmail.com>
    Signed-off-by: Jason Wang <jasowang@redhat.com>

--- tests/qtest/test-filter-redirector.c
+++ tests/qtest/test-filter-redirector.c
@@ -385,6 +385,100 @@ static void test_redirector_init_status_off(void)
     qtest_quit(qts);
 }
 
+static void test_redirector_rx_event_opened(void)
+{
+    int backend_sock[2], send_sock;
+    uint32_t ret = 0, len = 0;
+    char send_buf[] = "Hello!!";
+    char send_buf2[] = "Hello2!!";
+    char sock_path0[] = "filter-redirector0.XXXXXX";
+    char *recv_buf;
+    uint32_t size = sizeof(send_buf);
+    uint32_t size2 = sizeof(send_buf2);
+    size = htonl(size);
+    size2 = htonl(size2);
+    QTestState *qts;
+
+    ret = socketpair(PF_UNIX, SOCK_STREAM, 0, backend_sock);
+    g_assert_cmpint(ret, !=, -1);
+
+    ret = mkstemp(sock_path0);
+    g_assert_cmpint(ret, !=, -1);
+
+    qts = qtest_initf(
+        "-nic socket,id=qtest-bn0,fd=%d "
+        "-chardev socket,id=redirector0,path=%s,server=on,wait=off "
+        "-object filter-redirector,id=qtest-f0,netdev=qtest-bn0,"
+        "queue=rx,indev=redirector0 ",
+        backend_sock[1], sock_path0);
+
+    struct iovec iov[] = {
+        {
+            .iov_base = &size,
+            .iov_len = sizeof(size),
+        }, {
+            .iov_base = send_buf,
+            .iov_len = sizeof(send_buf),
+        },
+    };
+
+    struct iovec iov2[] = {
+        {
+            .iov_base = &size2,
+            .iov_len = sizeof(size2),
+        }, {
+            .iov_base = send_buf2,
+            .iov_len = sizeof(send_buf2),
+        },
+    };
+
+    /* First connection */
+    send_sock = unix_connect(sock_path0, NULL);
+    g_assert_cmpint(send_sock, !=, -1);
+    qtest_qmp_assert_success(qts, "{ 'execute' : 'query-status'}");
+
+    ret = iov_send(send_sock, iov, 2, 0, sizeof(size) + sizeof(send_buf));
+    g_assert_cmpint(ret, ==, sizeof(send_buf) + sizeof(size));
+
+    ret = recv(backend_sock[0], &len, sizeof(len), 0);
+    g_assert_cmpint(ret, ==, sizeof(len));
+    len = ntohl(len);
+    g_assert_cmpint(len, ==, sizeof(send_buf));
+    recv_buf = g_malloc(len);
+    ret = recv(backend_sock[0], recv_buf, len, 0);
+    g_assert_cmpint(ret, ==, len);
+    g_assert_cmpstr(recv_buf, ==, send_buf);
+    g_free(recv_buf);
+
+    close(send_sock);
+
+    /* Verify disconnection handling if needed, but mainly we want to test Reconnection */
+    qtest_qmp_assert_success(qts, "{ 'execute' : 'query-status'}");
+
+    /* Second connection */
+    send_sock = unix_connect(sock_path0, NULL);
+    g_assert_cmpint(send_sock, !=, -1);
+    qtest_qmp_assert_success(qts, "{ 'execute' : 'query-status'}");
+
+    ret = iov_send(send_sock, iov2, 2, 0, sizeof(size2) + sizeof(send_buf2));
+    g_assert_cmpint(ret, ==, sizeof(send_buf2) + sizeof(size2));
+
+    ret = recv(backend_sock[0], &len, sizeof(len), 0);
+    g_assert_cmpint(ret, ==, sizeof(len));
+    len = ntohl(len);
+    g_assert_cmpint(len, ==, sizeof(send_buf2));
+    recv_buf = g_malloc(len);
+    ret = recv(backend_sock[0], recv_buf, len, 0);
+    g_assert_cmpint(ret, ==, len);
+    g_assert_cmpstr(recv_buf, ==, send_buf2);
+    g_free(recv_buf);
+
+    close(send_sock);
+    unlink(sock_path0);
+    qtest_quit(qts);
+    close(backend_sock[0]);
+}
+
 int main(int argc, char **argv)
 {
     g_test_init(&argc, &argv, NULL);
@@ -393,5 +487,7 @@ int main(int argc, char **argv)
     qtest_add_func("/netfilter/redirector_status", test_redirector_status);
     qtest_add_func("/netfilter/redirector_init_status_off",
                    test_redirector_init_status_off);
+    qtest_add_func("/netfilter/redirector_rx_event_opened",
+                   test_redirector_rx_event_opened);
     return g_test_run();
 }
