0369fac6c86a9a375ae1f1d83b0d8f646798f5be
Vladimir Sementsov-Ogievskiy <vsementsov@yandex-team.ru>
2026-01-14 09:47:06 +0300

    vmstate-types: get_fd(): handle qemu_file_get_fd() failure
    qemu_file_get_fd() may fail, and return -1 in this case.
    We don't support passing "invalid" file descriptors (like -1),
    so we should not interpret error here like invalid descriptor.
    
    Signed-off-by: Vladimir Sementsov-Ogievskiy <vsementsov@yandex-team.ru>
    Reviewed-by: Fabiano Rosas <farosas@suse.de>
    Link: https://lore.kernel.org/qemu-devel/20260114064710.176268-2-vsementsov@yandex-team.ru
    Signed-off-by: Fabiano Rosas <farosas@suse.de>

--- migration/vmstate-types.c
+++ migration/vmstate-types.c
@@ -321,12 +321,21 @@ const VMStateInfo vmstate_info_uint64 = {
 static int get_fd(QEMUFile *f, void *pv, size_t size,
                   const VMStateField *field)
 {
+    int fd;
     int32_t *v = pv;
+
     if (migrate_mode() == MIG_MODE_CPR_EXEC) {
         qemu_get_sbe32s(f, v);
         return 0;
     }
-    *v = qemu_file_get_fd(f);
+
+    fd = qemu_file_get_fd(f);
+    if (fd < 0) {
+        return -1;
+    }
+
+    *v = fd;
+
     return 0;
 }
 
