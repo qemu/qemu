f48bddafa251503a2aa4a6eb9dd612ec9c0781f7
Shameer Kolothum <skolothumtho@nvidia.com>
2026-01-29 13:32:05 +0000

    tests/qtest/bios-tables-test: Update IORT blobs after revision upgrade
    Update the reference IORT blobs after revision upgrade for RMR node
    support. This affects the aarch64 'virt' IORT tests.
    
    IORT diff is the same for all the tests:
    
     /*
      * Intel ACPI Component Architecture
      * AML/ASL+ Disassembler version 20230628 (64-bit version)
      * Copyright (c) 2000 - 2023 Intel Corporation
      *
    - * Disassembly of tests/data/acpi/aarch64/virt/IORT, Mon Oct 20 14:42:41 2025
    + * Disassembly of /tmp/aml-B4ZRE3, Mon Oct 20 14:42:41 2025
      *
      * ACPI Data Table [IORT]
      *
      * Format: [HexOffset DecimalOffset ByteLength]  FieldName : FieldValue (in hex)
      */
    
     [000h 0000 004h]                   Signature : "IORT"    [IO Remapping Table]
     [004h 0004 004h]                Table Length : 00000080
    -[008h 0008 001h]                    Revision : 03
    -[009h 0009 001h]                    Checksum : B3
    +[008h 0008 001h]                    Revision : 05
    +[009h 0009 001h]                    Checksum : B1
     [00Ah 0010 006h]                      Oem ID : "BOCHS "
     [010h 0016 008h]                Oem Table ID : "BXPC    "
     [018h 0024 004h]                Oem Revision : 00000001
     [01Ch 0028 004h]             Asl Compiler ID : "BXPC"
     [020h 0032 004h]       Asl Compiler Revision : 00000001
     ...
    
    Reviewed-by: Jonathan Cameron <jonathan.cameron@huawei.com>
    Reviewed-by: Eric Auger <eric.auger@redhat.com>
    Tested-by: Eric Auger <eric.auger@redhat.com>
    Tested-by: Zhangfei Gao <zhangfei.gao@linaro.org>
    Signed-off-by: Shameer Kolothum <skolothumtho@nvidia.com>
    Message-id: 20260126104342.253965-27-skolothumtho@nvidia.com
    Signed-off-by: Peter Maydell <peter.maydell@linaro.org>

--- tests/qtest/bios-tables-test-allowed-diff.h
+++ tests/qtest/bios-tables-test-allowed-diff.h
@@ -1,5 +1,1 @@
 /* List of comma-separated changed AML files to ignore */
-"tests/data/acpi/aarch64/virt/IORT",
-"tests/data/acpi/aarch64/virt/IORT.its_off",
-"tests/data/acpi/aarch64/virt/IORT.smmuv3-legacy",
-"tests/data/acpi/aarch64/virt/IORT.smmuv3-dev",
