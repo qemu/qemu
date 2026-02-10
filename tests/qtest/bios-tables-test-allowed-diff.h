eb1c60997c4dff601f3cc0d6a24918e2068cf3c9
Mohamed Mediouni <mohamed@unpredictable.fr>
2026-02-10 11:34:48 +0000

    qtest: hw/arm: virt: skip ACPI test for IORT with GICv2
    Signed-off-by: Mohamed Mediouni <mohamed@unpredictable.fr>
    Reviewed-by: Peter Maydell <peter.maydell@linaro.org>
    Signed-off-by: Peter Maydell <peter.maydell@linaro.org>

--- tests/qtest/bios-tables-test-allowed-diff.h
+++ tests/qtest/bios-tables-test-allowed-diff.h
@@ -1,1 +1,4 @@
 /* List of comma-separated changed AML files to ignore */
+"tests/data/acpi/aarch64/virt/IORT",
+"tests/data/acpi/aarch64/virt/IORT.smmuv3-dev",
+"tests/data/acpi/aarch64/virt/IORT.smmuv3-legacy",
