fb45807fffb1d3f4d591cc1852fb9039b2011263
Mohamed Mediouni <mohamed@unpredictable.fr>
2026-02-24 00:39:45 +0100

    target/i386: emulate: remove fetch_instruction helper too
    Not used anymore.
    Link: https://lore.kernel.org/r/20260223233950.96076-24-mohamed@unpredictable.fr
    
    Signed-off-by: Paolo Bonzini <pbonzini@redhat.com>

--- target/i386/emulate/x86_emu.h
+++ target/i386/emulate/x86_emu.h
@@ -25,8 +25,6 @@
 #include "cpu.h"
 
 struct x86_emul_ops {
-    void (*fetch_instruction)(CPUState *cpu, void *data, target_ulong addr,
-                              int bytes);
     MMUTranslateResult (*mmu_gva_to_gpa) (CPUState *cpu, target_ulong gva, uint64_t *gpa, MMUTranslateFlags flags);
     void (*read_segment_descriptor)(CPUState *cpu, struct x86_segment_descriptor *desc,
                                     enum X86Seg seg);
