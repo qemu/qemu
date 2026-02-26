0e2c5647d9203c51edbadc5d1f678862f87198d5
Thomas Huth <thuth@redhat.com>
2026-02-26 09:46:03 +0100

    tests/functional: Remove the microblazeel test
    We are going to remove the microblazeel target, so the test is not
    required anymore. The little endian mode is tested already via the
    "microblaze" target, so we don't lose any test coverage here.
    
    While we're at it, simplify the "microblaze" target test now (in the
    file tests/functional/microblaze/test_s3adsp1800.py) since we don't
    need the separate super-class here anymore.
    
    Reviewed-by: Philippe Mathieu-Daudé <philmd@linaro.org>
    Signed-off-by: Thomas Huth <thuth@redhat.com>
    Message-ID: <20260226084608.11251-2-thuth@redhat.com>

--- tests/functional/microblaze/test_s3adsp1800.py
+++ tests/functional/microblaze/test_s3adsp1800.py
@@ -14,8 +14,6 @@ from qemu_test import wait_for_console_pattern
 
 class MicroblazeMachine(QemuSystemTest):
 
-    timeout = 90
-
     ASSET_IMAGE_BE = Asset(
         ('https://qemu-advcal.gitlab.io/qac-best-of-multiarch/download/'
          'day17.tar.xz'),
@@ -25,14 +23,12 @@ class MicroblazeMachine(QemuSystemTest):
         ('http://www.qemu-advent-calendar.org/2023/download/day13.tar.gz'),
         'b9b3d43c5dd79db88ada495cc6e0d1f591153fe41355e925d791fbf44de50c22')
 
-    def do_ballerina_be_test(self, force_endianness=False):
+    def test_microblaze_s3adsp1800_big_endian(self):
         self.set_machine('petalogix-s3adsp1800')
         self.archive_extract(self.ASSET_IMAGE_BE)
         self.vm.set_console()
         self.vm.add_args('-kernel',
                          self.scratch_file('day17', 'ballerina.bin'))
-        if force_endianness:
-            self.vm.add_args('-M', 'endianness=big')
         self.vm.launch()
         wait_for_console_pattern(self, 'This architecture does not have '
                                        'kernel memory protection')
@@ -41,14 +37,13 @@ class MicroblazeMachine(QemuSystemTest):
         # message, that's why we don't test for a later string here. This
         # needs some investigation by a microblaze wizard one day...
 
-    def do_xmaton_le_test(self, force_endianness=False):
+    def test_microblaze_s3adsp1800_little_endian(self):
         self.require_netdev('user')
         self.set_machine('petalogix-s3adsp1800')
         self.archive_extract(self.ASSET_IMAGE_LE)
         self.vm.set_console()
         self.vm.add_args('-kernel', self.scratch_file('day13', 'xmaton.bin'))
-        if force_endianness:
-            self.vm.add_args('-M', 'endianness=little')
+        self.vm.add_args('-M', 'endianness=little')
         tftproot = self.scratch_file('day13')
         self.vm.add_args('-nic', f'user,tftp={tftproot}')
         self.vm.launch()
@@ -58,18 +53,6 @@ class MicroblazeMachine(QemuSystemTest):
         exec_command_and_wait_for_pattern(self,
                 'tftp -g -r xmaton.png 10.0.2.2 ; md5sum xmaton.png',
                 '821cd3cab8efd16ad6ee5acc3642a8ea')
-
-
-class MicroblazeBigEndianMachine(MicroblazeMachine):
-
-    ASSET_IMAGE_BE = MicroblazeMachine.ASSET_IMAGE_BE
-    ASSET_IMAGE_LE = MicroblazeMachine.ASSET_IMAGE_LE
-
-    def test_microblaze_s3adsp1800_legacy_be(self):
-        self.do_ballerina_be_test()
-
-    def test_microblaze_s3adsp1800_legacy_le(self):
-        self.do_xmaton_le_test(force_endianness=True)
 
 
 if __name__ == '__main__':
