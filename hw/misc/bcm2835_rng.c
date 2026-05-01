baa047808a492497079450a34ebcf2279946b5ef
Peter Maydell <peter.maydell@linaro.org>
2026-05-01 17:27:00 +0100

    hw/misc/bcm2835_rng: Specify valid memory access sizes
    The BCM2835 RNG has 32-bit registers only; specify this in
    the MemoryRegionOps so wrong-sized accesses are rejected rather
    than getting to the assertions in the read and write functions,
    and for clarity add the matching .impl constraints.
    
    Cc: qemu-stable@nongnu.org
    Resolves: https://gitlab.com/qemu-project/qemu/-/work_items/3394
    Fixes: 54a5ba13a9f ("target-arm: Implement BCM2835 hardware RNG")
    Signed-off-by: Peter Maydell <peter.maydell@linaro.org>
    Reviewed-by: Philippe Mathieu-Daudé <philmd@linaro.org>
    Message-id: 20260501162700.4092512-1-peter.maydell@linaro.org
    (cherry picked from commit 18b664c90085b0d2be9c2ad8c747e00a7a733402)
    Signed-off-by: Michael Tokarev <mjt@tls.msk.ru>

--- hw/misc/bcm2835_rng.c
+++ hw/misc/bcm2835_rng.c
@@ -93,6 +93,10 @@ static const MemoryRegionOps bcm2835_rng_ops = {
     .read = bcm2835_rng_read,
     .write = bcm2835_rng_write,
     .endianness = DEVICE_NATIVE_ENDIAN,
+    .impl.min_access_size = 4,
+    .impl.max_access_size = 4,
+    .valid.min_access_size = 4,
+    .valid.max_access_size = 4,
 };
 
 static const VMStateDescription vmstate_bcm2835_rng = {
