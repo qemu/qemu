c5dcf59006b7beec59c0d38f109e6ce45b4c0f9d
James Hilliard <james.hilliard1@gmail.com>
2026-05-08 09:12:24 -0600

    tests/tcg/mips: add Octeon instruction smoke test
    Add a mips64/mips64el linux-user TCG smoke test for representative
    Octeon instruction paths.
    
    Run the test with -cpu Octeon68XX and share the source between the
    mips64 and mips64el target directories.
    
    Signed-off-by: James Hilliard <james.hilliard1@gmail.com>
    Reviewed-by: Philippe Mathieu-Daudé <philmd@linaro.org>
    Signed-off-by: Philippe Mathieu-Daudé <philmd@linaro.org>
    Reviewed-by: Richard Henderson <richard.henderson@linaro.org>
    Message-Id: <20260520172313.23777-6-philmd@linaro.org>

---
+++ tests/tcg/mips/user/isa/octeon/octeon-insns.c
@@ -0,0 +1,13 @@
+/*
+ * Test Octeon-specific user-mode instructions.
+ *
+ * SPDX-License-Identifier: GPL-2.0-or-later
+ */
+
+#include <assert.h>
+#include <stdint.h>
+
+int main(void)
+{
+    return 0;
+}
