fa7c684371ea28e869ed2e745868d98ad909c6ca
Daniel P. Berrangé <berrange@redhat.com>
2026-05-21 11:33:52 +0100

    ui/vnc: fix OOB write in lossy rect worker code
    Incorrect calculation of the boundary condition when tracking lossy
    rectangles in the worker thread will result in an OOB write which
    can corrupt further worker state, and/or trigger any guard pages
    that may lie beyond the VncWorker struct. This can be triggered
    through careful choice of the display resolution in the guest
    OS by an unprivileged user.
    
    Fixes: CVE-2026-48002
    Reported-by: Marc-André Lureau <marcandre.lureau@redhat.com>
    Signed-off-by: Daniel P. Berrangé <berrange@redhat.com>
    Message-ID: <20260521103353.1645561-4-berrange@redhat.com>
    [Marc-André - added assert() suggest by philmd@linaro.org]
    Signed-off-by: Marc-André Lureau <marcandre.lureau@redhat.com>
    (cherry picked from commit 46ee49034d26d04d95ba8f3183d4fbfa9d2b89b4)
    (Mjt: context fixup for 10.0.x)
    Signed-off-by: Michael Tokarev <mjt@tls.msk.ru>

--- ui/vnc.c
+++ ui/vnc.c
@@ -2968,13 +2968,15 @@ void vnc_sent_lossy_rect(VncState *vs, int x, int y, int w, int h)
 {
     int i, j;
 
-    w = (x + w) / VNC_STAT_RECT;
-    h = (y + h) / VNC_STAT_RECT;
+    w = DIV_ROUND_UP((x + w), VNC_STAT_RECT);
+    h = DIV_ROUND_UP((y + h), VNC_STAT_RECT);
+    assert(h <= VNC_STAT_ROWS);
+    assert(w <= VNC_STAT_COLS);
     x /= VNC_STAT_RECT;
     y /= VNC_STAT_RECT;
 
-    for (j = y; j <= h; j++) {
-        for (i = x; i <= w; i++) {
+    for (j = y; j < h; j++) {
+        for (i = x; i < w; i++) {
             vs->lossy_rect[j][i] = 1;
         }
     }
