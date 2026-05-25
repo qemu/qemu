3e39f23a6d38f1c6d11db34766cc28fe25547198
Kane Chen <kane_chen@aspeedtech.com>
2026-05-25 04:41:58 +0000

    hw/net/ftgmac100: convert to use Resettable interface
    Replace the legacy reset callback registered via
    device_class_set_legacy_reset() with the Resettable interface.
    
    Signed-off-by: Kane Chen <kane_chen@aspeedtech.com>
    Reviewed-by: Jamin Lin <jamin_lin@aspeedtech.com>
    Link: https://lore.kernel.org/qemu-devel/20260525044129.3133916-20-kane_chen@aspeedtech.com
    Signed-off-by: Cédric Le Goater <clg@redhat.com>

--- hw/net/ftgmac100.c
+++ hw/net/ftgmac100.c
@@ -723,9 +723,9 @@ static void ftgmac100_do_reset(FTGMAC100State *s, bool sw_reset)
     phy_reset(s);
 }
 
-static void ftgmac100_reset(DeviceState *d)
+static void ftgmac100_reset_hold(Object *obj, ResetType type)
 {
-    ftgmac100_do_reset(FTGMAC100(d), false);
+    ftgmac100_do_reset(FTGMAC100(obj), false);
 }
 
 static uint64_t ftgmac100_read(void *opaque, hwaddr addr, unsigned size)
@@ -1269,9 +1269,10 @@ static const Property ftgmac100_properties[] = {
 static void ftgmac100_class_init(ObjectClass *klass, const void *data)
 {
     DeviceClass *dc = DEVICE_CLASS(klass);
+    ResettableClass *rc = RESETTABLE_CLASS(klass);
 
     dc->vmsd = &vmstate_ftgmac100;
-    device_class_set_legacy_reset(dc, ftgmac100_reset);
+    rc->phases.hold = ftgmac100_reset_hold;
     device_class_set_props(dc, ftgmac100_properties);
     set_bit(DEVICE_CATEGORY_NETWORK, dc->categories);
     dc->realize = ftgmac100_realize;
@@ -1387,9 +1388,9 @@ static const MemoryRegionOps aspeed_mii_ops = {
     .endianness = DEVICE_LITTLE_ENDIAN,
 };
 
-static void aspeed_mii_reset(DeviceState *dev)
+static void aspeed_mii_reset_hold(Object *obj, ResetType type)
 {
-    AspeedMiiState *s = ASPEED_MII(dev);
+    AspeedMiiState *s = ASPEED_MII(obj);
 
     s->phycr = 0;
     s->phydata = 0;
@@ -1428,9 +1429,10 @@ static const Property aspeed_mii_properties[] = {
 static void aspeed_mii_class_init(ObjectClass *klass, const void *data)
 {
     DeviceClass *dc = DEVICE_CLASS(klass);
+    ResettableClass *rc = RESETTABLE_CLASS(klass);
 
     dc->vmsd = &vmstate_aspeed_mii;
-    device_class_set_legacy_reset(dc, aspeed_mii_reset);
+    rc->phases.hold = aspeed_mii_reset_hold;
     dc->realize = aspeed_mii_realize;
     dc->desc = "Aspeed MII controller";
     device_class_set_props(dc, aspeed_mii_properties);
