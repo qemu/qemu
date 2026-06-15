d6bb1d656cba9cf49210eab04671f2cfcfca8b66
Pierrick Bouvier <pierrick.bouvier@oss.qualcomm.com>
2026-06-15 12:35:24 -0700

    scripts/checkpatch: remove check on forbidden space before '[' in C++
    Lambdas are very confusing for checkpatch, so just relax this check.
    
    Reviewed-by: Chao Liu <chao.liu.zevorn@gmail.com>
    Reviewed-by: Manos Pitsidianakis <manos.pitsidianakis@linaro.org>
    Reviewed-by: Philippe Mathieu-Daudé <philmd@mailo.com>
    Link: https://lore.kernel.org/qemu-devel/20260615193526.2883349-26-pierrick.bouvier@oss.qualcomm.com
    Signed-off-by: Pierrick Bouvier <pierrick.bouvier@oss.qualcomm.com>

--- scripts/checkpatch.pl
+++ scripts/checkpatch.pl
@@ -2423,7 +2423,8 @@ sub process {
 #  3. inside a curly brace -- = { [0...10] = 5 }
 #  4. after a comma -- [1] = 5, [2] = 6
 #  5. in a macro definition -- #define abc(x) [x] = y
-		while ($line =~ /(.*?\s)\[/g) {
+		my $cpp = $realfile =~ /(\.cpp)$/;
+		while (!$cpp && $line =~ /(.*?\s)\[/g) {
 			my ($where, $prefix) = ($-[1], $1);
 			if ($prefix !~ /$Type\s+$/ &&
 			    ($where != 0 || $prefix !~ /^.\s+$/) &&
