2335b8dfad235e30441b87735c7c918a4e68ea17
Akihiko Odaki <odaki@rsg.ci.i.u-tokyo.ac.jp>
2026-07-25 16:11:46 +0900

    hw/display/virtio-gpu: Initialize blob mapping for ATTACH_BACKING
    It is necessary for blob cursor and scanout.
    
    Fixes: e0933d91b1cd ("virtio-gpu: Add virtio_gpu_resource_create_blob")
    Signed-off-by: Akihiko Odaki <odaki@rsg.ci.i.u-tokyo.ac.jp>
    Reviewed-by: Marc-André Lureau <marcandre.lureau@redhat.com>
    Message-ID: <20260725-backing-v1-1-5b584bf7df5a@rsg.ci.i.u-tokyo.ac.jp>
    (cherry picked from commit 4ae1c5c7d6f33d1e9c030ec4d24ab272609188cc)
    Signed-off-by: Michael Tokarev <mjt@tls.msk.ru>

--- hw/display/virtio-gpu.c
+++ hw/display/virtio-gpu.c
@@ -992,6 +992,10 @@ virtio_gpu_resource_attach_backing(VirtIOGPU *g,
         cmd->error = VIRTIO_GPU_RESP_ERR_UNSPEC;
         return;
     }
+
+    if (!res->image) {
+        virtio_gpu_init_udmabuf(res);
+    }
 }
 
 static void
