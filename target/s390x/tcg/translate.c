a1b99e787d956b7f0fb19de30cfade43d9bfb59d
Richard Henderson <richard.henderson@linaro.org>
2026-07-31 11:56:18 -0700

    target/s390x/tcg: Convert op_ld32s to op_ld
    Note that op_ld32s already or'd in insn->data, with the only
    use being MO_ALIGN.  We now put the entire MemOp in insn->data.
    
    Reviewed-by: Philippe Mathieu-Daudé <philmd@oss.qualcomm.com>
    Reviewed-by: Ilya Leoshkevich <iii@linux.ibm.com>
    Signed-off-by: Richard Henderson <richard.henderson@linaro.org>
    Message-ID: <20260731185628.23161-6-richard.henderson@linaro.org>

--- target/s390x/tcg/translate.c
+++ target/s390x/tcg/translate.c
@@ -2755,13 +2755,6 @@ static DisasJumpType op_ld(DisasContext *s, DisasOps *o)
     return DISAS_NEXT;
 }
 
-static DisasJumpType op_ld32s(DisasContext *s, DisasOps *o)
-{
-    tcg_gen_qemu_ld_i64(o->out, o->in2, get_mem_index(s),
-                        MO_BESL | s->insn->data);
-    return DISAS_NEXT;
-}
-
 static DisasJumpType op_ld32u(DisasContext *s, DisasOps *o)
 {
     tcg_gen_qemu_ld_i64(o->out, o->in2, get_mem_index(s),
