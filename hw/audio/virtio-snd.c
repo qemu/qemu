4772110c6a3b88b12ea16b17fd33eb9d942aa435
Volker Rümelin <vr_qemu@t-online.de>
2026-08-03 10:11:58 +0200

    hw/audio/virtio-sound: introduce virtio_snd_set_active()
    Split out the function virtio_snd_pcm_set_active() from
    virtio_snd_pcm_start_stop(). A later patch also needs this
    new funcion. There is no functional change.
    
    Signed-off-by: Volker Rümelin <vr_qemu@t-online.de>
    [AM: trivial rebase changes]
    Signed-off-by: Alexander Mikhalitsyn <aleksandr.mikhalitsyn@futurfusion.io>
    Reviewed-by: Marc-André Lureau <marcandre.lureau@redhat.com>
    Message-ID: <20260803081159.91981-10-alexander@mihalicyn.com>
    Signed-off-by: Manos Pitsidianakis <manos.pitsidianakis@linaro.org>

--- hw/audio/virtio-snd.c
+++ hw/audio/virtio-snd.c
@@ -461,6 +461,21 @@ static void virtio_snd_pcm_open(VirtIOSoundPCMStream *stream)
 }
 
 /*
+ * Activate/deactivate a stream.
+ *
+ * @stream: VirtIOSoundPCMStream *stream
+ * @active: whether to activate or deactivate the stream
+ */
+static void virtio_snd_pcm_set_active(VirtIOSoundPCMStream *stream, bool active)
+{
+    if (stream->info.direction == VIRTIO_SND_D_OUTPUT) {
+        audio_be_set_active_out(stream->s->audio_be, stream->voice.out, active);
+    } else {
+        audio_be_set_active_in(stream->s->audio_be, stream->voice.in, active);
+    }
+}
+
+/*
  * Close a stream and free all its resources.
  *
  * @stream: VirtIOSoundPCMStream *stream
@@ -606,11 +621,7 @@ static uint32_t virtio_snd_pcm_start_stop(VirtIOSound *s,
         stream->state = VIRTIO_SND_PCM_STATE_STOPPED;
     }
 
-    if (stream->info.direction == VIRTIO_SND_D_OUTPUT) {
-        audio_be_set_active_out(s->audio_be, stream->voice.out, start);
-    } else {
-        audio_be_set_active_in(s->audio_be, stream->voice.in, start);
-    }
+    virtio_snd_pcm_set_active(stream, start);
 
     return cpu_to_le32(VIRTIO_SND_S_OK);
 }
