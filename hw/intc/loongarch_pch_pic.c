5a7483c4bf9405d4d8c807f03e74f5dd8a7f0c7c
Bin Guo <guobin@linux.alibaba.com>
2026-08-04 15:34:45 +0800

    hw/intc/loongarch_pch_pic: Validate htmsi_vector before indexing parent_irq
    pch_pic_update_irq() used the guest-writable htmsi_vector[irq] value as an
    index into parent_irq[] without checking bounds.  A value >= irq_num (64 in
    the array, but only 32 are used by the virt machine) causes an out-of-bounds
    read and a guest-triggerable QEMU crash.
    
    Validate the vector before calling qemu_set_irq() in both the raise and lower
    paths and log a guest error if it is out of range.
    
    Resolves: https://gitlab.com/qemu-project/qemu/-/work_items/4114
    Cc: qemu-stable@nongnu.org
    Signed-off-by: Bin Guo <guobin@linux.alibaba.com>
    Signed-off-by: Bibo Mao <maobibo@loongson.cn>
    Reviewed-by: Bibo Mao <maobibo@loongson.cn>
    (cherry picked from commit 80776c4df92c3d58f89608b61e355092a7d524ed)
    (Mjt: #include qemu/log.h)
    Signed-off-by: Michael Tokarev <mjt@tls.msk.ru>

--- hw/intc/loongarch_pch_pic.c
+++ hw/intc/loongarch_pch_pic.c
@@ -7,6 +7,7 @@
 
 #include "qemu/osdep.h"
 #include "qemu/bitops.h"
+#include "qemu/log.h"
 #include "hw/irq.h"
 #include "hw/intc/loongarch_pch_pic.h"
 #include "trace.h"
@@ -17,13 +18,21 @@ static void pch_pic_update_irq(LoongArchPICCommonState *s, uint64_t mask,
 {
     uint64_t val;
     int irq;
+    uint8_t vector;
 
     if (level) {
         val = mask & s->intirr & ~s->int_mask;
         if (val) {
             irq = ctz64(val);
+            vector = s->htmsi_vector[irq];
+            if (vector >= s->irq_num) {
+                qemu_log_mask(LOG_GUEST_ERROR,
+                              "%s: htmsi_vector[%d]=%u out of range\n",
+                              __func__, irq, vector);
+                return;
+            }
             s->intisr |= MAKE_64BIT_MASK(irq, 1);
-            qemu_set_irq(s->parent_irq[s->htmsi_vector[irq]], 1);
+            qemu_set_irq(s->parent_irq[vector], 1);
         }
     } else {
         /*
@@ -33,8 +42,15 @@ static void pch_pic_update_irq(LoongArchPICCommonState *s, uint64_t mask,
         val = mask & s->intisr & ~s->intirr;
         if (val) {
             irq = ctz64(val);
+            vector = s->htmsi_vector[irq];
+            if (vector >= s->irq_num) {
+                qemu_log_mask(LOG_GUEST_ERROR,
+                              "%s: htmsi_vector[%d]=%u out of range\n",
+                              __func__, irq, vector);
+                return;
+            }
             s->intisr &= ~MAKE_64BIT_MASK(irq, 1);
-            qemu_set_irq(s->parent_irq[s->htmsi_vector[irq]], 0);
+            qemu_set_irq(s->parent_irq[vector], 0);
         }
     }
 }
