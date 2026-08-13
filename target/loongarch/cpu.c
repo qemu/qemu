11563efaba79377d2004041da514f857c876a151
gaosong <gaosong@loongson.cn>
2026-08-13 19:10:00 +0800

    target/loongarch: KVM disable msgint
    In KVM mode, msgint is disabled by default; please enable it manually.
    
    e.g
       ... -cpu max,msgint=on ...
    
    Signed-off-by: gaosong <gaosong@loongson.cn>
    Reviewed-by: Bibo Mao <maobibo@loongson.cn>
    Message-ID: <20260813111000.446232-4-gaosong@loongson.cn>
    Signed-off-by: Song Gao <gaosong@loongson.cn>

--- target/loongarch/cpu.c
+++ target/loongarch/cpu.c
@@ -450,6 +450,9 @@ static void loongarch_max_initfn(Object *obj)
     loongarch_la464_initfn(obj);
 
     cpu->ptw = ON_OFF_AUTO_AUTO;
+    if (kvm_enabled()){
+        cpu->msgint=ON_OFF_AUTO_OFF;
+    }
     if (tcg_enabled()) {
         uint32_t data = cpu->env.cpucfg[2];
         data = FIELD_DP32(data, CPUCFG2, HPTW, 1);
@@ -464,9 +467,9 @@ static void loongarch_max_initfn(Object *obj)
         data = cpu->env.cpucfg[3];
         data = FIELD_DP32(data, CPUCFG3, DBAR_HINTS, 1);
         cpu->env.cpucfg[3] = data;
+	 cpu->env.cpucfg[1] = FIELD_DP32(cpu->env.cpucfg[1], CPUCFG1, MSG_INT, 1);
+        cpu->msgint = ON_OFF_AUTO_AUTO;
     }
-    cpu->env.cpucfg[1] = FIELD_DP32(cpu->env.cpucfg[1], CPUCFG1, MSG_INT, 1);
-    cpu->msgint = ON_OFF_AUTO_AUTO;
 }
 
 #if defined(CONFIG_KVM)
