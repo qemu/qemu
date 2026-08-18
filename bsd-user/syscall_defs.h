cc638a9f4b8ec1e6ab59f4b2153f24ec4496411a
Warner Losh <imp@bsdimp.com>
2026-08-17 21:32:59 -0600

    bsd-user: move sys/shm.h lines
    Diff reduction with blitz: move these lines to the same place in the
    blitz branch.
    
    Reviewed-by: Pierrick Bouvier <pierrick.bouvier@oss.qualcomm.com>
    Acked-by: Richard Henderson <richard.henderson@linaro.org>
    Signed-off-by: Warner Losh <imp@bsdimp.com>

--- bsd-user/syscall_defs.h
+++ bsd-user/syscall_defs.h
@@ -43,26 +43,6 @@ struct target_ipc_perm {
 #define TARGET_IPC_STAT 2   /* get options */
 
 /*
- * sys/shm.h
- */
-struct target_shmid_ds {
-    struct  target_ipc_perm shm_perm; /* peration permission structure */
-    abi_ulong   shm_segsz;  /* size of segment in bytes */
-    int32_t     shm_lpid;   /* process ID of last shared memory op */
-    int32_t     shm_cpid;   /* process ID of creator */
-    int32_t     shm_nattch; /* number of current attaches */
-    target_time_t shm_atime;  /* time of last shmat() */
-    target_time_t shm_dtime;  /* time of last shmdt() */
-    target_time_t shm_ctime;  /* time of last change by shmctl() */
-};
-
-#define N_BSD_SHM_REGIONS   32
-struct bsd_shm_regions {
-    abi_long start;
-    abi_long size;
-};
-
-/*
  * sys/sem.h
  */
 #define TARGET_GETNCNT  3   /* Return the value of semncnt {READ} */
@@ -91,6 +71,26 @@ struct target_semid_ds {
     abi_ushort  sem_nsems;  /* number of sems in set */
     target_time_t   sem_otime;  /* last operation time */
     target_time_t   sem_ctime;  /* times measured in secs */
+};
+
+/*
+ * sys/shm.h
+ */
+struct target_shmid_ds {
+    struct  target_ipc_perm shm_perm; /* peration permission structure */
+    abi_ulong   shm_segsz;  /* size of segment in bytes */
+    int32_t     shm_lpid;   /* process ID of last shared memory op */
+    int32_t     shm_cpid;   /* process ID of creator */
+    int32_t     shm_nattch; /* number of current attaches */
+    target_time_t shm_atime;  /* time of last shmat() */
+    target_time_t shm_dtime;  /* time of last shmdt() */
+    target_time_t shm_ctime;  /* time of last change by shmctl() */
+};
+
+#define N_BSD_SHM_REGIONS   32
+struct bsd_shm_regions {
+    abi_long start;
+    abi_long size;
 };
 
 /*
