2a15a5d2f2378858af0dbbd03a397cf9f093f905
Amit Machhiwal <amachhiw@linux.ibm.com>
2026-08-19 10:29:24 +0530

    linux-headers: Update to include KVM_CAP_PPC_COMPAT_CAPS
    Sync linux headers from Linux kernel commit 1200d84f4c0a
    ("Merge tag 'powerpc-7.3-1' of git://git.kernel.org/pub/scm/linux/kernel/git/powerpc/linux").
    
    This picks up the new KVM_CAP_PPC_COMPAT_CAPS capability for powerpc,
    which introduces:
    - struct kvm_ppc_compat_caps: reports processor compatibility modes
      supported by the host (POWER9, POWER10, POWER11)
    - KVM_PPC_GET_COMPAT_CAPS ioctl (KVMIO 0xb8)
    - KVM_CAP_PPC_COMPAT_CAPS (cap #250) in linux/kvm.h
    
    The sync is needed to get the corresponding Qemu series[1] merged.
    
    [1]: https://lore.kernel.org/all/20260812170854.58709-1-amachhiw@linux.ibm.com/
    
    Signed-off-by: Amit Machhiwal <amachhiw@linux.ibm.com>
    Link: https://lore.kernel.org/qemu-devel/20260819045924.11732-1-amachhiw@linux.ibm.com
    Signed-off-by: Harsh Prateek Bora <harshpb@linux.ibm.com>

--- linux-headers/asm-x86/unistd_x32.h
+++ linux-headers/asm-x86/unistd_x32.h
@@ -339,6 +339,7 @@
 #define __NR_file_setattr (__X32_SYSCALL_BIT + 469)
 #define __NR_listns (__X32_SYSCALL_BIT + 470)
 #define __NR_rseq_slice_yield (__X32_SYSCALL_BIT + 471)
+#define __NR_fchroot (__X32_SYSCALL_BIT + 472)
 #define __NR_rt_sigaction (__X32_SYSCALL_BIT + 512)
 #define __NR_rt_sigreturn (__X32_SYSCALL_BIT + 513)
 #define __NR_ioctl (__X32_SYSCALL_BIT + 514)
