4fc85110efe00bbfed17f90969dcc5e58fb89fc5
Harald Freudenberger <freude@linux.ibm.com>
2026-08-24 10:31:59 +0200

    target/s390x: Support protected key AES CBC for cpacf kmc instruction
    Support the subfunctions CPACF_KMC_PAES_128, CPACF_KMC_PAES_192
    and CPACF_KMC_PAES_256 for the cpacf kmc instruction.
    
    Signed-off-by: Harald Freudenberger <freude@linux.ibm.com>
    Tested-by: Holger Dengler <dengler@linux.ibm.com>
    Reviewed-by: Finn Callies <fcallies@linux.ibm.com>
    Reviewed-by: Ilya Leoshkevich <iii@linux.ibm.com>
    Link: https://lore.kernel.org/qemu-devel/20260824083204.40877-16-freude@linux.ibm.com
    Signed-off-by: Eric Farman <farman@linux.ibm.com>

--- target/s390x/tcg/crypto_helper.c
+++ target/s390x/tcg/crypto_helper.c
@@ -146,6 +146,13 @@ static int cpacf_kmc(CPUS390XState *env, const int mmu_idx, uintptr_t ra,
                            &env->regs[r1], &env->regs[r2], &env->regs[r2 + 1],
                            S390_FEAT_TYPE_KMC, fc, mod);
         break;
+    case CPACF_KMC_PAES_128:
+    case CPACF_KMC_PAES_192:
+    case CPACF_KMC_PAES_256:
+        rc = cpacf_paes_cbc(env, mmu_idx, ra, env->regs[1],
+                            &env->regs[r1], &env->regs[r2], &env->regs[r2 + 1],
+                            S390_FEAT_TYPE_KMC, fc, mod);
+        break;
     default:
         tcg_s390_program_interrupt(env, PGM_SPECIFICATION, ra);
     }
