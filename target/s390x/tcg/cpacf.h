a69a321658a90ef46b650040546690704e9ef2eb
Harald Freudenberger <freude@linux.ibm.com>
2026-08-24 10:31:47 +0200

    target/s390x: Move cpacf sha512 code into a new file
    Move the cpacf sha512 implementation into a new file
    cpacf_sha512.c. Add this new file to the build. Introduce a new header
    file cpacf.h which holds function prototypes and other things around
    cpacf local to the tcg directory.
    
    Signed-off-by: Harald Freudenberger <freude@linux.ibm.com>
    Tested-by: Holger Dengler <dengler@linux.ibm.com>
    Reviewed-by: Finn Callies <fcallies@linux.ibm.com>
    Reviewed-by: Ilya Leoshkevich <iii@linux.ibm.com>
    Reviewed-by: Holger Dengler <dengler@linux.ibm.com>
    Link: https://lore.kernel.org/qemu-devel/20260824083204.40877-4-freude@linux.ibm.com
    Signed-off-by: Eric Farman <farman@linux.ibm.com>

---
+++ target/s390x/tcg/cpacf.h
@@ -0,0 +1,16 @@
+/*
+ * SPDX-License-Identifier: GPL-2.0-or-later
+ *
+ * s390x cpacf
+ *
+ */
+
+#ifndef S390X_CPACF_H
+#define S390X_CPACF_H
+
+/* from cpacf_sha512.c */
+int cpacf_sha512(CPUS390XState *env, const int mmu_idx, uintptr_t ra,
+                 uint64_t param_addr, uint64_t *message_reg, uint64_t *len_reg,
+                 uint32_t type);
+
+#endif /* S390X_CPACF_H */
