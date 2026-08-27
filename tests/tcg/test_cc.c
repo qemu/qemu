e5d084d622b1b6ba770b998dfdff08352ef805a3
Pierrick Bouvier <pierrick.bouvier@oss.qualcomm.com>
2026-08-27 22:13:50 +0000

    tests/tcg/meson.build: check host cross cc is working
    On Fedora, cross compilers are provided without runtime or header files.
    Thus, even if program is present, it can't be used to compile files
    directly. Check this, and fallback on docker if not working.
    
    Reviewed-by: Philippe Mathieu-Daudé <philmd@oss.qualcomm.com>
    Tested-by: Aniket Sahu <asahu1x@linux.ibm.com>
    Signed-off-by: Pierrick Bouvier <pierrick.bouvier@oss.qualcomm.com>
    Message-ID: <20260827221506.91574-30-pierrick.bouvier@oss.qualcomm.com>
    Signed-off-by: Alex Bennée <alex.bennee@linaro.org>

---
+++ tests/tcg/test_cc.c
@@ -0,0 +1,9 @@
+/* SPDX-License-Identifier: GPL-2.0-or-later */
+
+/* Include a standard header to make sure cross compiler provides them */
+#include <stdint.h>
+
+int main(void)
+{
+    return 0;
+}
