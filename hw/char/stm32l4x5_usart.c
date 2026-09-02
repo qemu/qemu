03e7b6262e9c099a53f75feca28def91883b0208
Alex Bennée <alex.bennee@linaro.org>
2026-09-02 17:59:55 +0100

    hw/char/stm32l4x5_usart: Accept character input upon RDR read
    Looking through existing serial devices this seems to be a common
    pattern so it makes sense to me this is the right fix and matches the
    API description.
    
    Resolves: https://gitlab.com/qemu-project/qemu/-/issues/4389
    Reported-by: Jan Brittenson (@bson)
    Signed-off-by: Alex Bennée <alex.bennee@linaro.org>
    Message-id: 20260902165956.582901-1-alex.bennee@linaro.org
    Suggested-by: Jan Brittenson (@bson)
    AI-used-for: fetch issue, apply proposed patch, verify build and qtests
    Signed-off-by: Alex Bennée <alex.bennee@linaro.org>
    
    Reviewed-by: Peter Maydell <peter.maydell@linaro.org>
    Signed-off-by: Peter Maydell <peter.maydell@linaro.org>

--- hw/char/stm32l4x5_usart.c
+++ hw/char/stm32l4x5_usart.c
@@ -441,6 +441,7 @@ static uint64_t stm32l4x5_usart_base_read(void *opaque, hwaddr addr,
         /* Reset RXNE flag */
         s->isr &= ~R_ISR_RXNE_MASK;
         stm32l4x5_update_irq(s);
+        qemu_chr_fe_accept_input(&s->chr);
         break;
     case A_TDR:
         retvalue = FIELD_EX32(s->tdr, TDR, TDR);
