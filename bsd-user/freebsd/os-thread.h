d9ab6943edd4d124d37347dd6a024e02421b0759
Rick Richard <rick@sloservers.com>
2026-09-16 03:46:16 -0700

    bsd-user: HOST_BIG_ENDIAN and TARGET_BIG_ENDIAN are always
    TARGET_BIG_ENDIAN and HOST_BIG_ENDIAN are always defined.
    
    lseek and target_offset64() transpose upper and lower halves
    on 32-bit ABIs, -strace prints wrong off_t.
    
    Signed-off-by: Rick Richard <rick@sloservers.com>
    [ fixed defined() too and moved to C if() instead of ifdef ]
    Reviewed-by: Warner Losh <imp@bsdimp.com>
    Signed-off-by: Warner Losh <imp@bsdimp.com>

--- bsd-user/freebsd/os-thread.h
+++ bsd-user/freebsd/os-thread.h
@@ -18,7 +18,7 @@
 int safe_thr_suspend(struct timespec *timeout);
 int safe__umtx_op(void *, int, unsigned long, void *, void *);
 
-#if defined(HOST_BIG_ENDIAN) == defined(TARGET_BIG_ENDIAN) && \
+#if HOST_BIG_ENDIAN == TARGET_BIG_ENDIAN && \
     (TARGET_ABI_BITS == HOST_LONG_BITS || defined(UMTX_OP__32BIT))
 #define _UMTX_OPTIMIZED
 #if defined(TARGET_ABI32)
