f115b35ed817df6ddbc96f47cc0a537ea76e545a
Pierrick Bouvier <pierrick.bouvier@oss.qualcomm.com>
2026-09-17 15:11:17 -0700

    tests/docker/docker.py: do not add-current-user if $UID == 0
    root already exists, so this step fails with:
    useradd: UID 0 is not unique
    
    The setup where it happens is when working on QEMU using a dev container
    with podman, with default root user being mapped to host user. In this
    case, uid in container is 0 but effective uid on host is user's id.
    
    Tested-by: John Snow <jsnow@redhat.com>
    Reviewed-by: Pierrick Bouvier <pierrick.bouvier@oss.qualcomm.com>
    Link: https://lore.kernel.org/qemu-devel/20260917221117.274619-4-pierrick.bouvier@oss.qualcomm.com
    Signed-off-by: Pierrick Bouvier <pierrick.bouvier@oss.qualcomm.com>

--- tests/docker/docker.py
+++ tests/docker/docker.py
@@ -300,9 +300,10 @@ class Docker(object):
 
         if user:
             uid = os.getuid()
-            uname = getpass.getuser()
-            build_args += ["--build-arg", "USER=%s" % uname,
-                           "--build-arg", "UID=%s" % uid]
+            if uid != 0:
+                uname = getpass.getuser()
+                build_args += ["--build-arg", "USER=%s" % uname,
+                               "--build-arg", "UID=%s" % uid]
 
         if registry is not None:
             pull_args = ["pull", "%s/%s" % (registry, tag)]
